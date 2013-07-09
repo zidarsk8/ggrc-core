cbd143025930097db947972f52aa38fcebc4bfc9
Vraj Mohan <vraj@reciprocitylabs.com>
2013-07-09 07:44:41 -0400

    Update newly added columns to sensible defaults

--- src/ggrc/migrations/versions/20130703190904_26641df89c2c_rebase_event.py
+++ src/ggrc/migrations/versions/20130703190904_26641df89c2c_rebase_event.py
@@ -16,13 +16,17 @@ import sqlalchemy as sa
 
 def upgrade():
     op.add_column('events', sa.Column('updated_at', sa.DateTime(), nullable=False))
+    op.execute('UPDATE events SET updated_at = created_at')
     op.drop_constraint('events_ibfk_1', 'events', type_='foreignkey')
     op.alter_column('events', 'person_id', existing_type = sa.INTEGER, new_column_name = 'modified_by_id')
     op.create_foreign_key('events_modified_by', 'events', 'people', ['modified_by_id'], ['id'])
     op.add_column('revisions', sa.Column('created_at', sa.DateTime(), nullable=False))
     op.add_column('revisions', sa.Column('updated_at', sa.DateTime(), nullable=False))
     op.add_column('revisions', sa.Column('modified_by_id', sa.INTEGER, nullable=False))
-    op.execute('''UPDATE revisions, events SET revisions.modified_by_id = events.modified_by_id
+    op.execute('''UPDATE revisions, events SET 
+                    revisions.created_at = events.created_at,
+                    revisions.updated_at = events.updated_at,
+                    revisions.modified_by_id = events.modified_by_id
                 WHERE revisions.event_id = events.id''')
     op.create_foreign_key('revisions_modified_by', 'revisions', 'people', ['modified_by_id'], ['id'])
 
