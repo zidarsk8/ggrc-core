7c0e8af8482f95f0d6c790b9ab109de269d381e2
Dan Ring <dfring@gmail.com>
2013-08-14 18:31:23 -0700

    Add test to verify query count for each service

--- src/service_specs/steps/service_description.py
+++ src/service_specs/steps/service_description.py
@@ -20,6 +20,7 @@ def validate_service_description(context, resource_name):
           resource_name, service_description_obj)
   for endpoint_row in context.table:
     endpoint_name = endpoint_row['endpoint']
+    max_query_count = int(endpoint_row['max_query_count'])
     endpoint_obj = endpoints.get(unicode(endpoint_name), None)
     assert endpoint_obj is not None, \
         'Expected to find service_description.endpoints.{0} in {1}\n{2}'.format(
@@ -28,7 +29,12 @@ def validate_service_description(context, resource_name):
     assert href is not None, \
         'Expected endpoint {0} to contain "href" value'.format(
             endpoint_name)
+    query_count = context.query_count
     response = get_resource(context, href)
+    if max_query_count > 0:
+      assert (context.query_count - query_count) <= max_query_count, \
+          'GET of {0} required {1} queries, which is more than {2}'\
+            .format(href, context.query_count - query_count, max_query_count)
     assert response.status_code == 200, \
         'Expected status code 200 on GET of endpoint {0} URL {1}, received {2}'\
           .format(response.status_code)
