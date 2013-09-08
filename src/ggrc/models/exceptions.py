28af216bec77570485eff074480139ec73bbbc37
Vraj Mohan <vraj@reciprocitylabs.com>
2013-09-08 16:02:21 -0400

    Translate db messages to a user-understandable form

--- src/ggrc/models/exceptions.py
+++ src/ggrc/models/exceptions.py
@@ -3,5 +3,24 @@
 # Created By: vraj@reciprocitylabs.com
 # Maintained By: vraj@reciprocitylabs.com
 
+import re
+from sqlalchemy.exc import IntegrityError
+
+def translate_message(exception):
+  """
+  Translates db exceptions to something a user can understand.
+  """
+  message = exception.message
+  if isinstance(exception, IntegrityError):
+    # TODO: Handle not null, foreign key errors, uniqueness errors with compound keys
+    duplicate_entry_pattern = re.compile(r'\(1062, "(Duplicate entry \'[^\']*\')')
+    matches = duplicate_entry_pattern.search(message)
+    if matches:
+      return matches.group(1)
+    else:
+      return message
+  else:
+    return message
+
 class ValidationError(Exception):
   pass
