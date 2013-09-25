c5058b85b936d6da6f8fb5e8a7f3c60e71a717e4
Dan Ring <dfring@gmail.com>
2013-09-25 11:19:22 -0700

    Add Control-to-Control mappings
    * also ensures consistency in mappings for the case where source and
      target models are the same

--- src/ggrc/assets/javascripts/controllers/modal_selector_controller.js
+++ src/ggrc/assets/javascripts/controllers/modal_selector_controller.js
@@ -1190,14 +1190,13 @@
         , extra_options = modal_descriptor_view_options[option_model_name]
         ;
 
-      /*if (option_descriptors[option_model_name])
-        console.debug(
-            "Found duplicate option descriptors for"
-          , option_model_name
-          , join_descriptors);*/
-
       if (!option_set.default_option_descriptor)
         option_set.default_option_descriptor = option_model_name;
+
+      //  If we have duplicate options, we want to use the first, so return
+      //    early.
+      if (option_descriptors[option_model_name])
+        return;
 
       if (!extra_options)
         extra_options = {
