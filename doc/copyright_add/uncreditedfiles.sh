74fea60fad3d1960612ad40f3db1cf1e34377cd9
Silas Barta <sbarta@gmail.com>
2013-09-30 11:38:32 -0700

    first front-end addition of credit

--- doc/copyright_add/uncreditedfiles.sh
+++ doc/copyright_add/uncreditedfiles.sh
@@ -1,8 +1,15 @@
-counter=1
-for x in `grep -rlI --exclude-dir=tmp "Created By: *$" . | grep -v --file=doc/copyright_add/excludes.grep | grep --file=$1`
+#for x in `grep -rlI --exclude-dir=tmp "Created By: *$" . | grep -v --file=doc/copyright_add/excludes.grep | grep --file=$1`
+#do
+#    cp $x temporary_file
+#    sed "s/Created By: *$/Created By: $2/" temporary_file > $x
+#    rm temporary_file
+#    break
+#done
+
+for x in `grep -rlI --exclude-dir=tmp "Maintained By: *$" . | grep -v --file=doc/copyright_add/excludes.grep | grep --file=$1`
 do
-				cp $x temporary_file
-				cat $x | sed "s/Created By: *$/Created By: $2/" > $x
-				rm temporary_file
-				break
+    cp $x temporary_file
+    sed "s/Maintained By: *$/Maintained By: $2/" temporary_file > $x
+    rm temporary_file
+    break
 done
