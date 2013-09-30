8052a7bd680272b0be019a9e4d9e0d7d0bc4226f
Silas Barta <sbarta@gmail.com>
2013-09-29 17:09:17 -0700

    notice to haml files

--- doc/copyright_add/badfiles.sh
+++ doc/copyright_add/badfiles.sh
@@ -1,8 +1,9 @@
 # note: initially found 628 non-compliant files
-for x in `grep -rLI --exclude-dir=tmp "Copyright (C)" . | grep -v --file=doc/copyright_add/excludes.grep | grep "\.scss$"`
+# Did the above search for file endings .scss, .feature, .html. js, .py, .haml, .mako and applied the appropriate header.
+for x in `grep -rLI --exclude-dir=tmp "Copyright (C)" . | grep -v --file=doc/copyright_add/excludes.grep | grep "\.haml$"`
 do
 				#echo $x
 				cp $x temporary_file
-				cat doc/copyright_add/js_header.js temporary_file > $x
+				cat doc/copyright_add/haml_header.haml temporary_file > $x
 				rm temporary_file
 done
