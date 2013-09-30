effe7a7ba531d1557ea65a0511ce2d91e289e3b6
Silas Barta <sbarta@gmail.com>
2013-09-30 13:16:51 -0700

    add dan as create/maintainer for backend files

--- src/ggrc/__main__.py
+++ src/ggrc/__main__.py
@@ -1,8 +1,8 @@
 
 # Copyright (C) 2013 Google Inc., authors, and contributors <see AUTHORS file>
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
-# Created By:
-# Maintained By:
+# Created By: dan@reciprocitylabs.com
+# Maintained By: dan@reciprocitylabs.com
 
 if __name__ == "__main__" and (__package__ is None or __package__ == ""):
   import os, sys
--- src/ggrc/migrations/versions/20130728030007_3a5ff1d71b9f_create_objectobjecti.py
+++ src/ggrc/migrations/versions/20130728030007_3a5ff1d71b9f_create_objectobjecti.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2013 Google Inc., authors, and contributors <see AUTHORS file>
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
-# Created By: 
-# Maintained By: 
+# Created By: dan@reciprocitylabs.com
+# Maintained By: dan@reciprocitylabs.com
 
 """Create ObjectObjective, ObjectSection, ObjectControl
 
