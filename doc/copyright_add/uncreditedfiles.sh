0c0e6e0319b4ef6f941e2bff2e5eb0cd938c8710
Silas Barta <sbarta@gmail.com>
2013-09-30 13:12:26 -0700

    first backend addition of credit

--- doc/copyright_add/uncreditedfiles.sh
+++ doc/copyright_add/uncreditedfiles.sh
@@ -1,10 +1,12 @@
-#for x in `grep -rlI --exclude-dir=tmp "Created By: *$" . | grep -v --file=doc/copyright_add/excludes.grep | grep --file=$1`
-#do
-#    cp $x temporary_file
-#    sed "s/Created By: *$/Created By: $2/" temporary_file > $x
-#    rm temporary_file
-#    break
-#done
+# arg1 is grep file that lists regexes for files you want to match
+# arg2 is email address of person you want to credit
+for x in `grep -rlI --exclude-dir=tmp "Created By: *$" . | grep -v --file=doc/copyright_add/excludes.grep | grep --file=$1`
+do
+    cp $x temporary_file
+    sed "s/Created By: *$/Created By: $2/" temporary_file > $x
+    rm temporary_file
+    break
+done
 
 for x in `grep -rlI --exclude-dir=tmp "Maintained By: *$" . | grep -v --file=doc/copyright_add/excludes.grep | grep --file=$1`
 do
