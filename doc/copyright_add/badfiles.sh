bd4d6cfb4b0ae4cdac8ab792eaea1ad71c12595b
Silas Barta <sbarta@gmail.com>
2013-09-29 17:07:28 -0700

    add notice to scss files

--- doc/copyright_add/badfiles.sh
+++ doc/copyright_add/badfiles.sh
@@ -1,8 +1,8 @@
 # note: initially found 628 non-compliant files
-for x in `grep -rLI --exclude-dir=tmp "Copyright (C)" . | grep -v --file=doc/copyright_add/excludes.grep | grep "\.feature$"`
+for x in `grep -rLI --exclude-dir=tmp "Copyright (C)" . | grep -v --file=doc/copyright_add/excludes.grep | grep "\.scss$"`
 do
 				#echo $x
 				cp $x temporary_file
-				cat doc/copyright_add/py_header.py temporary_file > $x
+				cat doc/copyright_add/js_header.js temporary_file > $x
 				rm temporary_file
 done
