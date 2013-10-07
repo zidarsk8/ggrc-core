504b63d7f05745ff074d79f9b5d003d44f180b72
Silas Barta <sbarta@gmail.com>
2013-10-07 12:20:05 -0700

    Remove old commented-outline.

--- doc/copyright_add/mustache_fix.sh
+++ doc/copyright_add/mustache_fix.sh
@@ -1,5 +1,4 @@
 # good test file: src/ggrc/static/mustache/sections/control_selector.mustache
-#for x in "src/ggrc/static/mustache/sections/control_selector.mustache"
 for x in `grep -rlI --include=*.mustache --exclude-dir=tmp "<\!--" . | grep -v --file=doc/copyright_add/excludes.grep`
 # save the seventh line on (tail -n +7), do swap on lines before that (head -6)
 do
