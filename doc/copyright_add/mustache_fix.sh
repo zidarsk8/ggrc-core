2c087be57468aa998e8d2e551f5dd04e7b93028b
Silas Barta <sbarta@gmail.com>
2013-10-07 12:19:05 -0700

    Switched mustached copyright comment to use mustache comment syntax.

--- doc/copyright_add/mustache_fix.sh
+++ doc/copyright_add/mustache_fix.sh
@@ -1,15 +1,14 @@
-# arg1 is grep file that lists regexes for files you want to match
 # good test file: src/ggrc/static/mustache/sections/control_selector.mustache
-#for x in `grep -rlI --exclude-dir=tmp "Created By: *$" . | grep -v --file=doc/copyright_add/excludes.grep | egrep --file=$1`
+#for x in "src/ggrc/static/mustache/sections/control_selector.mustache"
+for x in `grep -rlI --include=*.mustache --exclude-dir=tmp "<\!--" . | grep -v --file=doc/copyright_add/excludes.grep`
 # save the seventh line on (tail -n +7), do swap on lines before that (head -6)
-for x in "src/ggrc/static/mustache/sections/control_selector.mustache"
 do
     tail -n +7 $x > temporary_file
     head -6 $x \
     | sed -e 's/<!--/{{!/' \
     | sed -e 's/-->/}}/' \
     > temporary_file2
-    cat temporary_file2 temporary_file
+    cat temporary_file2 temporary_file > $x
     rm temporary_file
     rm temporary_file2
 done
