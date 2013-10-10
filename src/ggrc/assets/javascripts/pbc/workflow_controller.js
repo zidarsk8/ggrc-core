3e79ca69174d200258f816218e389f73844f875a
Bradley Momberger <brad@reciprocitylabs.com>
2013-10-09 17:42:10 -0700

    Supporting auto-generation of requests/responses from existing objectives as well

--- src/ggrc/assets/javascripts/pbc/workflow_controller.js
+++ src/ggrc/assets/javascripts/pbc/workflow_controller.js
@@ -13,14 +13,35 @@ can.Control("GGRC.Controllers.PbcWorkflows", {
 }, {
 
   "{CMS.Models.Audit} created" : function(model, ev, instance) {
+    var refresh_queue
+    , that = this
     if(instance instanceof CMS.Models.Audit) {
-      this.create_objective(instance)
-      .then(this.proxy("map_objective_to_program", instance.program.reify()))
-      .then(function(object_objective) {
-        return object_objective.objective.reify();
-      })
-      .then(this.proxy("create_request", instance))
-      .then(this.proxy("create_response"));
+      if(instance.auto_generate) {
+        refresh_queue = new RefreshQueue();
+
+        instance.program.reify().refresh()
+        .then(function(program) {
+          can.each(program.objectives.reify(), function(obj) {
+            refresh_queue.enqueue(obj);
+          });
+          return refresh_queue.trigger();
+        })
+        .then(function(objectives) {
+          can.each(objectives, function(objective) {
+            that.create_request(instance, objective)
+            .then(that.proxy("create_response"));
+          });
+        });
+      } else {
+        this.create_objective(instance)
+        .then(this.proxy("map_objective_to_program", instance.program.reify()))
+        .then(function(object_objective) {
+          return object_objective.objective.reify();
+        })
+        .then(that.proxy("create_request", instance))
+        .done(that.proxy("update_objective_title"))
+        .done(this.proxy("create_response"));
+      }
     }
   }
 
@@ -49,10 +70,15 @@ can.Control("GGRC.Controllers.PbcWorkflows", {
       , objective : objective.stub()
       , request_type : "documentation"
       , context : audit.context
-    }).save().done(function(req) {
-      objective.attr("title", objective.title + " " + req.attr("id")).save();
-      return req;
+    }).save();
+  }
+
+  , update_objective_title : function(request) {
+    var objective = request.objective.reify();
+    objective.refresh().then(function() {
+      objective.attr("title", objective.title + " " + request.attr("id")).save();
     });
+    return request;
   }
 
   , create_response : function(request) {
