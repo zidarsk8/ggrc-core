7fdc272deda2c8599638100e08683d784a5a92ac
Andy Kant <andy@andykant.com>
2013-10-10 23:56:16 -0500

    Added nested sticky headers support

--- src/ggrc/assets/javascripts/sticky_header.js
+++ src/ggrc/assets/javascripts/sticky_header.js
@@ -10,14 +10,16 @@
 
 can.Control("StickyHeader", {
     defaults: {
-        header: "header"
-      , scroll_area: ".object-area"
-      , mode: "fixed" // transform | fixed
+        scroll_area: ".object-area"
+      , header_selector: "header, .tree-open > .item-open > .item-main"
+      , depth_selector: ".tree-open > .item-open"
+      , root_header_selector: "header"
+      // , margin: 50
     }
 }, {
     init : function() {
       this.options = new can.Observe(can.extend(this.options, {
-          header: typeof this.options.header === 'string' ? this.element.find(this.options.header) : this.options.header
+          header: this.element.find(this.options.root_header_selector)
         , scroll_area: typeof this.options.scroll_area === 'string' ? this.element.closest(this.options.scroll_area) : this.options.scroll_area
       }));
       this.on();
@@ -27,37 +29,83 @@ can.Control("StickyHeader", {
     if (!this.options.header.is(":visible"))
       return;
 
-    // Initialize on first visibility
-    if (!this._origin && !this._margin) {
-      this._origin = this.options.scroll_area[0].scrollTop;
-      this._margin = this.options.header.position().top;
-      if (this.options.mode === "fixed") {
-        this._clone_min = this.options.header.position().top;
-        this._clone = this.options.header.clone(true, true).css({
-            position: 'fixed'
-          , top: this.options.scroll_area.css("top")
-          , left: this.options.header.offset().left + 'px'
-          , width: (this.options.header[0].getBoundingClientRect().width
-              - parseFloat(this.options.header.css('paddingLeft')) 
-              - parseFloat(this.options.header.css('paddingRight'))) 
-              + 'px'
-        });
-      }
-      else if (this.options.mode === "transform")
-        this.options.header.css('transform-origin', '0 0');
-    }
+    // Update the header positions
+    var headers = this.find_headers();
+    for (var i = headers.length - 1; i >= 0; i--) {
+      var el = headers.eq(i)
+        , clone = el.data('sticky').clone
+        ;
 
-    // Update the position
-    if (this.options.mode === "fixed") {
-      if (el[0].scrollTop >= this._margin && !this._clone[0].parentNode)
-        this.options.header.parent().append(this._clone);
-      else if (el[0].scrollTop < this._margin)
-        this._clone.remove();
+      if (this.in_viewport(el)) {
+        !clone[0].parentNode && el.parent().append(clone);
+      }
+      else {
+        clone[0].parentNode && clone.remove();
+        $.removeData(el, 'sticky');
+      }
     }
-    else if (this.options.mode === "transform") {
-      var y_position = Math.max(0, el[0].scrollTop - this._origin - this._margin);
-      this.options.header.transition({ 'translate': [0, y_position] }, 0, 'cubic-bezier(0.33, 0.66, 0.66, 1)');
-    }
+  }
+
+  , find_headers : function() {
+    var headers = this.element.find(this.options.header_selector).filter(':not(.sticky):visible')
+      , self = this
+      ;
+    headers.each(function(i) {
+      var $this = $(this);
+      if (!$this.data('sticky')) {
+        var data = {
+          level: $this.parents(self.options.depth_selector).length
+        };
+        $this.data('sticky', data);
+        data.clone = self.clone($this, headers.slice(0, i));
+      }
+    });
+
+    return headers;
+  }
+
+  , position_header : function(el) {
+    var level = el.data('sticky').level
+      , pos = el.position()
+      ;
+  }
+
+  , in_viewport : function(el) {
+    var parent = el.parent()
+      , pos = parent.position()
+      , top = this.options.scroll_area[0].scrollTop
+      , bottom = top + this.options.scroll_area.outerHeight() 
+      , offset = el.data('sticky').offset
+      ;
+
+    return pos.top < offset && (pos.top + parent.outerHeight() - el.outerHeight()) > offset;
+  }
+
+    // Clones and prepares a header
+  , clone : function(el, headers) {
+    // Compute heights of above headers
+    var offset = 0
+      , level = el.data('sticky').level
+      ;
+    headers.each(function() {
+      var $this = $(this);
+      if ($this.data('sticky').level < level)
+        offset += $this.outerHeight();
+    })
+
+    el.data('sticky').offset = offset;
+    return el
+      .clone(true, true)
+      .addClass("sticky")
+      .css({
+          position: 'fixed'
+        , top: (offset + parseFloat(this.options.scroll_area.css("top"))) + 'px'
+        , left: el.offset().left + 'px'
+        , width: (el[0].getBoundingClientRect().width
+            - parseFloat(el.css('paddingLeft')) 
+            - parseFloat(el.css('paddingRight'))) 
+            + 'px'
+      });
   }
 });
 
