97d01df808b6c45049fb7bbe1c764b82fa5a7dd8
Andy Kant <andy@andykant.com>
2013-10-11 11:36:05 -0500

    Adding more documentation and cleaning up unused code

--- src/ggrc/assets/javascripts/sticky_header.js
+++ src/ggrc/assets/javascripts/sticky_header.js
@@ -10,23 +10,28 @@
 
 can.Control("StickyHeader", {
     defaults: {
-        scroll_area: ".object-area"
+        // A selector for the scrollable area ancestor
+        scroll_area_selector: ".object-area"
+        // A selector for all sticky-able headers
       , header_selector: "header, .tree-open > .item-open > .item-main"
+        // A selector for counting the depth
+        // Generally this should be header_selector with the final element in each selector removed
       , depth_selector: ".tree-open > .item-open"
-      , root_header_selector: "header"
+        // The amount of space at the bottom of the content when the header should start scrolling away
       , margin: 30
     }
 }, {
     init : function() {
-      this.options = new can.Observe(can.extend(this.options, {
-          header: this.element.find(this.options.root_header_selector)
-        , scroll_area: typeof this.options.scroll_area === 'string' ? this.element.closest(this.options.scroll_area) : this.options.scroll_area
-      }));
+      this.options = can.extend(this.options, {
+        scroll_area: this.element.closest(this.options.scroll_area_selector)
+      });
       this.on();
     }
 
+    // Handle window scrolling
   , "{scroll_area} scroll" : function(el, ev) {
-    if (!this.options.header.is(":visible"))
+    // Only process if this section is visible
+    if (!this.element.is(":visible"))
       return;
 
     // Update the header positions
@@ -37,26 +42,33 @@ can.Control("StickyHeader", {
         , margin = this.in_viewport(el)
         ;
 
+      // Remove the clone if its content no longer inside the viewport
       if (margin === false) {
         clone[0].parentNode && clone.remove();
         $.removeData(el, 'sticky');
       }
+      // Otherwise inject the clone
       else {
         !clone[0].parentNode && el.parent().append(clone);
+
+        // When the content is close to scrolling away, also scroll the header away
         clone.css('marginTop', margin + 'px');
       }
     }
   }
 
+    // Find all sticky-able headers in the document
   , find_headers : function() {
     var headers = this.element.find(this.options.header_selector).filter(':not(.sticky):visible')
       , self = this
       ;
+
+    // Generate the depth and clone for each header
     headers.each(function(i) {
       var $this = $(this);
       if (!$this.data('sticky')) {
         var data = {
-          level: $this.parents(self.options.depth_selector).length
+          depth: $this.parents(self.options.depth_selector).length
         };
         $this.data('sticky', data);
         data.clone = self.clone($this, headers.slice(0, i));
@@ -66,12 +78,7 @@ can.Control("StickyHeader", {
     return headers;
   }
 
-  , position_header : function(el) {
-    var level = el.data('sticky').level
-      , pos = el.position()
-      ;
-  }
-
+    // Determine whether a header's content section is within the scrolling viewport
   , in_viewport : function(el) {
     var parent = el.parent()
       , pos = parent.position()
@@ -81,7 +88,9 @@ can.Control("StickyHeader", {
       , margin = pos.top + parent.outerHeight() - el.outerHeight() - offset
       ;
 
+    // If the content is in the viewport...
     if (pos.top < offset && (pos.top + parent.outerHeight() - el.outerHeight()) > offset) {
+      // Return zero or the amount that the header should start scrolling away if applicable
       return margin <= this.options.margin ? -Math.max(0, this.options.margin - margin) : 0;
     }
     else
@@ -92,15 +101,17 @@ can.Control("StickyHeader", {
   , clone : function(el, headers) {
     // Compute heights of above headers
     var offset = 0
-      , level = el.data('sticky').level
+      , depth = el.data('sticky').depth
       ;
+
+    // Determine the depth
     headers.each(function() {
       var $this = $(this);
-      if ($this.data('sticky').level < level)
+      if ($this.data('sticky').depth < depth)
         offset += $this.outerHeight();
     })
-
     el.data('sticky').offset = offset;
+
     return el
       .clone(true, true)
       .addClass("sticky")
