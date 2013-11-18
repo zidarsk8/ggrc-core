f362ba99a34d1c0e64856b416b087a2fefd2fdc6
Bradley Momberger <brad@reciprocitylabs.com>
2013-11-18 11:22:59 -0800

    Fixed env var for the admin group

--- src/ggrc_gdrive_integration/settings/development.py
+++ src/ggrc_gdrive_integration/settings/development.py
@@ -9,5 +9,5 @@ EXTENSIONS = ['ggrc_gdrive_integration']
 GAPI_KEY = os.environ.get('GGRC_GAPI_KEY', "AIzaSyAndAzs1E-8brJdESH7YSuvrj3A8Y-MZCg")
 GAPI_CLIENT_ID = os.environ.get('GGRC_GAPI_CLIENT_ID', "831270113958.apps.googleusercontent.com")
 #Admin group gets writer access to all
-GAPI_ADMIN_GROUP = os.environ.get('GGRC_GAPI_CLIENT_ID', "ggrc-dev@google.com")
+GAPI_ADMIN_GROUP = os.environ.get('GGRC_GAPI_ADMIN_GROUP', "ggrc-dev@google.com")
 exports = ["GAPI_KEY", "GAPI_CLIENT_ID", "GAPI_ADMIN_GROUP"]
