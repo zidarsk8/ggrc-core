5740f9e7c4f91516a73bc04d29c60b2c203cdc10
Dan Ring <dfring@gmail.com>
2013-11-22 12:03:58 -0800

    Bump version to s17

--- src/ggrc/settings/default.py
+++ src/ggrc/settings/default.py
@@ -20,7 +20,7 @@ EXTENSIONS = []
 # Deployment-specific variables
 COMPANY = "Company, Inc."
 COMPANY_LOGO_TEXT = "Company GRC"
-VERSION = "s16.2"
+VERSION = "s17"
 
 # Initialize from environment if present
 import os
