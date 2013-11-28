zidarsk8/ggrc-core

commit a20be8cd4910f2566beb89daf8fdb8192f9aa243

Hide LHN if user cannot view pages in the public context
* Move LHN initialization into `CMS.Controllers.LHN`
* Move LHS template code into Mustache templates (template path
  configurable in `templates/layouts/dashboard.haml`
* Hide LHN if user has no `view_object_page` permissions in the public
  context; otherwise it initializes as normal
* Handle `resize_areas` when the resize bar is not needed
* Add `with_model_as` mustache helper to get model information in
  mustache templates

src/ggrc/assets/javascripts/controllers/quick_search_controller.js
@@ -195,6 +195,149 @@ CMS.Controllers.Filterable("CMS.Controllers.QuickSearch", {
   }
 });
 
+can.Control("CMS.Controllers.LHN", {
+    defaults: {
+    }
+}, {
+    init: function() {
+      var self = this
+        ;
+
+      this.obs = new can.Observe();
+
+      if (this.should_show_lhn()) {
+        this.init_lhn();
+      }
+      else {
+        this.hide_lhn();
+      }
+    }
+
+  , should_show_lhn: function() {
+      return Permission.is_allowed("view_object_page", "__GGRC_ALL__", null);
+    }
+
+  , "input.widgetsearch keypress": function(el, ev) {
+      if(ev.which === 13)
+        this.obs.attr("value", $(ev.target).val());
+    }
+
+  , "input.my-work click": function(el, ev) {
+      var target = $(ev.target);
+      if (target.is('input.my-work')) {
+        var checked = target.prop("checked");
+        this.obs.attr("my_work", checked);
+        target.closest('.btn')[checked ? 'addClass' : 'removeClass']('btn-success');
+        CMS.Models.DisplayPrefs.findAll().done(function(prefs) {
+          if(prefs.length < 1) {
+            prefs.push(new CMS.Models.DisplayPrefs());
+            prefs[0].save();
+          }
+          prefs[0].setGlobal("lhs", { my_work: checked });
+        });
+      }
+    }
+
+  , init_lhn: function() {
+      var self = this;
+
+      CMS.Models.DisplayPrefs.findAll().done(function(prefs) {
+        var settings, checked
+          , target = $('#lhs input.my-work')
+          ;
+
+        if(prefs.length < 1) {
+          prefs.push(new CMS.Models.DisplayPrefs());
+          prefs[0].save();
+        }
+
+        settings = prefs[0].getGlobal("lhs");
+        checked = (settings && 'my_work' in settings) ? !!settings.my_work : true;
+        target.prop('checked', checked);
+        self.obs.attr("my_work", checked);
+        target.closest('.btn')[checked ? 'addClass' : 'removeClass']('btn-success');
+
+        $("#lhs").cms_controllers_lhn_search({ observer: self.obs });
+        $("#lhs").cms_controllers_lhn_tooltips();
+        // Delay Recently Viewed until after LHN is initialized
+        setTimeout(function() {
+          $(".recent").ggrc_controllers_recently_viewed();
+        }, 200);
+
+        // Collapse the LHN if they did it on a previous page
+        var collapsed = prefs[0].getCollapsed(null, "lhs");
+        collapsed && $(".bar-v").trigger('click');
+      });
+    }
+
+  , hide_lhn: function() {
+      var $area = $(".area")
+        , $lhsHolder = $(".lhs-holder")
+        ;
+
+      this.element.hide();
+      $lhsHolder.css("width", 0);
+      $area.css("margin-left", 0);
+
+      window.resize_areas();
+    }
+
+  , toggle_lhs: function() {
+      var $lhs = $("#lhs")
+        , $lhsHolder = $(".lhs-holder")
+        , $area = $(".area")
+        , $bar = $(".bar-v")
+        ;
+
+      if($lhs.hasClass("lhs-closed")) {
+        $lhs.removeClass("lhs-closed");
+        $bar.removeClass("bar-closed");
+        $lhsHolder.css("width","240px");
+        $area.css("margin-left","248px");
+      } else {
+        $lhs.addClass("lhs-closed");
+        $bar.addClass("bar-closed");
+        $lhsHolder.css("width","40px");
+        $area.css("margin-left","48px");
+      }
+
+      window.resize_areas();
+
+      CMS.Models.DisplayPrefs.findAll().done(function(prefs) {
+        prefs[0].setCollapsed(null, "lhs", $lhs.hasClass("lhs-closed"));
+      })
+    }
+
+  , ".bar-v click": function(el, ev) {
+      this.toggle_lhs();
+    }
+
+  , "#lhs click": function(el, ev) {
+      this.resize_search();
+    }
+
+  , resize_search: function() {
+      // Resize search input as necessary
+      var input = this.element.find('#lhs input.widgetsearch')
+        , width;
+
+      width =
+        input.closest('.form-search').width()
+        - input.parent().outerWidth() + input.parent().width()
+        - input.next().outerWidth() - input.outerWidth() + input.width();
+
+      input.css('width', width + 'px');
+    }
+
+  , "#lhs input.widgetsearch focus": function(el, ev) {
+      this.resize_search();
+    }
+
+  , ".lhs-closed click": function(el, ev) {
+      this.toggle_lhs();
+    }
+});
+
 can.Control("CMS.Controllers.LHN_Search", {
     defaults : {
         list_view : GGRC.mustache_path + "/base_objects/search_result.mustache"
@@ -211,8 +354,18 @@ can.Control("CMS.Controllers.LHN_Search", {
     }
 }, {
     init: function() {
-      var self = this;
+      var self = this
+        , template_path = GGRC.mustache_path + this.element.data('template')
+        ;
 
+      can.view(template_path, {}, function(frag, xhr) {
+        self.element.html(frag);
+        self.post_init();
+      });
+    }
+
+  , post_init: function() {
+      var self = this;
       this.init_object_lists();
       this.init_list_views();
       this.run_search("", this.options.observer.my_work ? { "contact_id": GGRC.current_user.id } : null);

src/ggrc/assets/assets.yaml
@@ -121,6 +121,8 @@ dashboard-js-template-files:
   - dashboard/internav_list.mustache
   - data_assets/modal_content.mustache
   - documents/active_column.mustache
+  - dashboard/lhn.mustache
+  - dashboard/lhn_search.mustache
   - documents/list.mustache
   - documents/modal_content.mustache
   - documents/option_column.mustache

src/ggrc/assets/javascripts/apps/quick_search.js
@@ -466,142 +466,7 @@ GGRC.RELATIONSHIP_TYPES = RELATIONSHIP_TYPES;
 
 
 $(function() {
-
-  $(".recent").ggrc_controllers_recently_viewed();
-  $("#lhs").cms_controllers_lhn_tooltips();
-
-  var obs = new can.Observe();
-  $("#lhs").bind("keypress", "input.widgetsearch", function(ev) {
-    if(ev.which === 13)
-      obs.attr("value", $(ev.target).val());
-  });
-  $("#lhs").bind("click", "input.my-work", function(ev) {
-    var target = $(ev.target);
-    if (target.is('input.my-work')) {
-      var checked = target.prop("checked");
-      obs.attr("my_work", checked);
-      target.closest('.btn')[checked ? 'addClass' : 'removeClass']('btn-success');
-      CMS.Models.DisplayPrefs.findAll().done(function(prefs) {
-        if(prefs.length < 1) {
-          prefs.push(new CMS.Models.DisplayPrefs());
-          prefs[0].save();
-        }
-        prefs[0].setGlobal("lhs", { my_work: checked });
-      });
-    }
-  });
-  CMS.Models.DisplayPrefs.findAll().done(function(prefs) {
-    var settings, checked
-      , target = $('#lhs input.my-work')
-      ;
-      
-    if(prefs.length < 1) {
-      prefs.push(new CMS.Models.DisplayPrefs());
-      prefs[0].save();
-    }
-    settings = prefs[0].getGlobal("lhs");
-    checked = (settings && 'my_work' in settings) ? !!settings.my_work : true;
-    target.prop('checked', checked);
-    obs.attr("my_work", checked);
-    target.closest('.btn')[checked ? 'addClass' : 'removeClass']('btn-success');
-
-    $("#lhs").cms_controllers_lhn_search({ observer: obs });
-  });
-
-  function bindQuickSearch(ev, opts) {
-
-    var $qs = $(this).uniqueId();
-    var obs = new can.Observe();
-    if($qs.find("ul.tree-structure").length) {
-      return;
-    }
-    $qs.bind("keypress", "input.widgetsearch", function(ev) {
-      if(ev.which === 13)
-        obs.attr("value", $(ev.target).val());
-    });
-    can.getObject("Instances", CMS.Controllers.QuickSearch, true)[$qs.attr("id")] = 
-     $qs.find(".lhs-nav:not(.recent), section.content")
-      .cms_controllers_quick_search(can.extend({
-        observer : obs
-        , spin : $qs.is(":not(section)")
-      }, opts)).control(CMS.Controllers.QuickSearch);
-
-  }
-  $("section.widget-tabs").each(function() {
-    bindQuickSearch.call(this, {}, {});
-  });//get anything that exists on the page already.
-
-  //Then listen for new ones
-  $(document.body).on("click", ".quick-search:not(:has(.cms_controllers_quick_search)), section.widget-tabs:not(:has(.cms_controllers_quick_search))", bindQuickSearch);
-
-  $(document.body).on("click", ".bar-v", function(ev) {
-    
-    var $lhs = $("#lhs")
-    ,   $lhsHolder = $(".lhs-holder")
-    ,   $area = $(".area")
-    ,   $bar = $(".bar-v")
-    ;
-    
-    if( $lhs.hasClass("lhs-closed") ) {
-      $lhs.removeClass("lhs-closed");
-      $bar.removeClass("bar-closed");
-      $lhsHolder.css("width","240px");
-      $area.css("margin-left","248px");
-    } else {
-      $lhs.addClass("lhs-closed");
-      $bar.addClass("bar-closed");
-      $lhsHolder.css("width","40px");
-      $area.css("margin-left","48px");
-    }
-    
-    resize_areas();  
-
-    CMS.Models.DisplayPrefs.findAll().done(function(prefs) {
-      prefs[0].setCollapsed(null, "lhs", $lhs.hasClass("lhs-closed"));
-    })
-  });
-
-  // Collapse the LHN if they did it on a previous page
-  CMS.Models.DisplayPrefs.findAll().done(function(prefs) {
-    var collapsed = prefs[0].getCollapsed(null, "lhs");
-    collapsed && $(".bar-v").trigger('click');
-  });
-
-// Resize search input as necessary
-var $lhs = $("#lhs")
-  , last_width = $lhs.length && $lhs[0].offsetWidth;
-if ($lhs.length) {
-  function resize_search() {
-    var input = $('#lhs input.widgetsearch')
-      , width = input.closest('.form-search').width() - input.parent().outerWidth() + input.parent().width()
-                - input.next().outerWidth() - input.outerWidth() + input.width();
-    input.css('width', width + 'px');
-  };
-  $(document.body).on("focus", "#lhs input.widgetsearch", resize_search);
-  $(document.body).on("click", "#lhs", resize_search);
-  setInterval(function() {
-    if (last_width !== $lhs[0].offsetWidth) {
-      last_width = $lhs[0].offsetWidth;
-      resize_search(); 
-    }
-  }, 25);
-}
-
-  $(document.body).on("click", ".lhs-closed", function(ev) {
-    
-    var $lhs = $(this)
-    ,   $lhsHolder = $(".lhs-holder")
-    ,   $area = $(".area")
-    ,   $bar = $(".bar-v")
-    ;
-    
-    $lhs.removeClass("lhs-closed");
-    $bar.removeClass("bar-closed");
-    $lhsHolder.css("width","240px");
-    $area.css("margin-left","248px");
-    
-    resize_areas();  
-  });
+  $("#lhn").cms_controllers_lhn();
 
   $(document.body).on("click", "a[data-toggle=unmap]", function(ev) {
     var $el = $(this)

src/ggrc/assets/javascripts/dashboard.js
@@ -738,8 +738,9 @@ function resize_areas() {
   footerMargin = lhsHeight;
   internavHeight = lhsHeight - 55;
   lhsWidth = $lhsHolder.width();
+  barWidth = $bar.is(":visible") ? $bar.outerWidth() : 0;
   internavWidth = $innerNav.width() || 0; // || 0 for pages without inner-nav
-  objectWidth = winWidth - lhsWidth - internavWidth - 8;
+  objectWidth = winWidth - lhsWidth - internavWidth - barWidth;
   headerWidth = winWidth - lhsWidth;
 
   $lhsHolder.css("height",lhsHeight);

src/ggrc/assets/javascripts/mustache_helper.js
@@ -1912,6 +1912,13 @@ Mustache.registerHelper("if_helpers", function() {
       return options.inverse(options.contexts);
     }
   }
-})
+});
+
+Mustache.registerHelper("with_model_as", function(var_name, model_name, options) {
+  var frame = {};
+  model_name = resolve_computed(Mustache.resolve(model_name));
+  frame[var_name] = CMS.Models[model_name];
+  return options.fn($.extend([], options.contexts, options.contexts.concat([frame])));
+});
 
 })(this, jQuery, can);

src/ggrc/assets/mustache/dashboard/lhn.mustache
@@ -1,0 +1,44 @@
+<form class="form-search" onsubmit="return false">
+  <div class="lhs-search input-append"><input class="widgetsearch" placeholder="Enter text" type="text"><label class="checkbox btn"><input class="my-work" type="checkbox">
+      My Work</label></div>
+</form>
+<div class="lhs-bg"></div>
+
+<div class="lhs-nav emphesized">
+  <ul class="top-level">
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Program" li_class="programs" tooltip_view='/programs/extended_info.mustache'}}}
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Audit" li_class="programs" tooltip_view='/audits/extended_info.mustache' template_view='/static/mustache/audits/search_result.mustache' beta=1}}}
+  </ul>
+</div>
+<div class="lhs-nav">
+  <h2 class="governance">Governance</h2>
+  <ul class="top-level">
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Regulation" li_class="governance"}}}
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Contract" li_class="governance"}}}
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Policy" li_class="governance"}}}
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Standard" li_class="governance"}}}
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Objective" li_class="governance"}}}
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Control" li_class="governance"}}}
+  </ul>
+  <h2 class="entities">People/Groups</h2>
+  <ul class="top-level">
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Person" li_class="entities"}}}
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="OrgGroup" li_class="entities"}}}
+  </ul>
+  <h2 class="business">Assets/Business</h2>
+  <ul class="top-level">
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="System" li_class="business"}}}
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Process" li_class="business"}}}
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="DataAsset" li_class="business"}}}
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Product" li_class="business"}}}
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Project" li_class="business"}}}
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Facility" li_class="business"}}}
+    {{{render '/static/mustache/dashboard/lhn_search.mustache' type="Market" li_class="business"}}}
+  </ul>
+</div>
+<div class="bar-h"></div>
+<div class="lhs-nav recent">
+  <h2>Recently Viewed</h2>
+  <ul class="top-level">
+  </div>
+</div>

src/ggrc/assets/mustache/dashboard/lhn_search.mustache
@@ -1,0 +1,14 @@
+{{#with_model_as "model" type}}
+<li class="{{li_class}} accordion-group" data-tooltip-view="{{tooltip_view}}" data-template="{{template_view}}" data-model-name="{{type}}">
+  <a class="{{li_class}}" href="javascript://" data-object-singular="{{type}}" data-parent=".top-level">
+    <i class="grcicon-{{model.table_singular}}-color"></i>
+    {{model.title_plural}}
+    {{#if beta}}<small>BETA</small>{{/if}}
+    <small>(<span class="item-count"></span>)</small>
+  </a>
+  <div class="content">
+    <ul class="sub-level"></ul>
+    <ul class="sub-actions"></ul>
+  </div>
+</li>
+{{/with_model_as}}

src/ggrc/templates/layouts/dashboard.haml
@@ -6,27 +6,6 @@
 -#
 -extends 'layouts/base.haml'
 
--macro search_result(type, type_plural, li_class='', tooltip_view='', template_view='')
-  %li{ 'class': ['{{ li_class }}', 'accordion-group'], 'data-model-name': '={type}', 'data-tooltip-view' : '={tooltip_view}', 'data-template' : '={template_view}' }
-    %a{ 'href': '#={ type_plural|underscore }_sub_level', 'class': '={li_class}', 'data-parent' : ".top-level", 'data-object-singular' : '={type}' }
-      %i{ 'class': 'grcicon-{{ type|lower|nospace }}-color' }
-      -if type_plural|title == 'Audits'
-        Audits
-        %small BETA
-      -elif type_plural|title == 'People'
-        People
-        %small BETA
-      -else
-        {{ type_plural|title }}
-      %small
-        (
-        %span.item-count> ...
-        )
-    %div.content
-      %ul.sub-level{ 'id' : '={ type_plural|underscore }_sub_level' }
-      %ul.sub-actions
--endmacro
-
 -block extra_javascript
   GGRC.permissions = ={ permissions_json()|safe };
   GGRC.current_user = ={ current_user_json()|safe };
@@ -161,52 +140,8 @@
             Version
             =config.get('VERSION')
 
-    .bar-v
-    .lhs-holder
-      .affix-holder
-        #lhs.lhs.accordion
-          -#NEW LHS
-          %form.form-search{ 'onsubmit': 'return false' }
-            .lhs-search.input-append<
-              %input.widgetsearch{ 'type': 'text', 'placeholder': 'Enter text' }>
-              %label.checkbox.btn<
-                %input.my-work{ 'type': 'checkbox' }
-                My Work
-          .lhs-bg
-          .lhs-nav.emphesized
-            %ul.top-level
-              =search_result('Program', 'programs', 'programs', '/programs/extended_info.mustache')
-              =search_result('Audit', 'audits', 'programs', '/audits/extended_info.mustache', '/static/mustache/audits/search_result.mustache')
-          .lhs-nav
-            %h2.governance Governance
-            %ul.top-level
-              =search_result('Regulation', 'regulations', 'governance')
-              =search_result('Contract', 'contracts', 'governance')
-              =search_result('Policy', 'policies', 'governance')
-              =search_result('Standard', 'standards', 'governance')
-              =search_result('Objective', 'objectives', 'governance')
-              =search_result('Control', 'controls', 'governance')
-            %h2.business Assets/Business
-            %ul.top-level
-              =search_result('System', 'systems', 'business')
-              =search_result('Process', 'processes', 'business')
-              =search_result('DataAsset', 'data assets', 'business')
-              =search_result('Product', 'products', 'business')
-              =search_result('Project', 'projects', 'business')
-              =search_result('Facility', 'facilities', 'business')
-              =search_result('Market', 'markets', 'business')
-            %h2.entities People/Groups
-            %ul.top-level
-              =search_result('Person', 'people', 'entities')
-              =search_result('OrgGroup', 'org groups', 'entities')
-            -#%h2.risk Risk Management
-            -#%ul.top-level
-              =search_result('Risk', 'risks', 'risk')
-              =search_result('RiskyAttribute', 'risky attributes', 'risk')
-          .bar-h
-          -#.lhs-nav
-            -#%h2 My Work
-          -#.bar-h
-          .lhs-nav.recent
-            %h2 Recently Viewed
-            %ul.top-level
+    #lhn
+      .bar-v
+      .lhs-holder
+        .affix-holder
+          #lhs.lhs.accordion{ 'data-template': '/dashboard/lhn.mustache' }

commit d863daee63f78a3f0f8ac5b9987b412db90dedbd

Fix "My Work" checkbox to reflect DisplayPrefs setting
* Broken in a20be8cd49

src/ggrc/assets/javascripts/controllers/quick_search_controller.js
@@ -243,7 +243,6 @@ can.Control("CMS.Controllers.LHN", {
 
       CMS.Models.DisplayPrefs.findAll().done(function(prefs) {
         var settings, checked
-          , target = $('#lhs input.my-work')
           ;
 
         if(prefs.length < 1) {
@@ -253,14 +252,20 @@ can.Control("CMS.Controllers.LHN", {
 
         settings = prefs[0].getGlobal("lhs");
         checked = (settings && 'my_work' in settings) ? !!settings.my_work : true;
-        target.prop('checked', checked);
         self.obs.attr("my_work", checked);
-        target.closest('.btn')[checked ? 'addClass' : 'removeClass']('btn-success');
 
         $("#lhs").cms_controllers_lhn_search({ observer: self.obs });
         $("#lhs").cms_controllers_lhn_tooltips();
-        // Delay Recently Viewed until after LHN is initialized
+
+        // Delay LHN initializations until after LHN is rendered
         setTimeout(function() {
+          var target = self.element.find('#lhs input.my-work')
+            , checked = self.obs.attr('my_work')
+            ;
+
+          target.prop('checked', checked);
+          target.closest('.btn')[checked ? 'addClass' : 'removeClass']('btn-success');
+
           $(".recent").ggrc_controllers_recently_viewed();
         }, 200);
 