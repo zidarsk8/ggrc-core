f73a69175c677b613c8b73f849aa523188b6ffd9
Silas Barta <sbarta@gmail.com>
2013-12-09 11:40:37 -0800

    Make Request status not required, give default.

--- src/ggrc/converters/requests.py
+++ src/ggrc/converters/requests.py
@@ -22,7 +22,7 @@ class RequestRowConverter(BaseRowConverter):
   def reify(self):
     self.handle('objective_id', ObjectiveHandler)
     self.handle('request_type', RequestTypeColumnHandler, is_required=True)
-    self.handle('status', StatusColumnHandler, valid_states=Request.VALID_STATES, is_required=True)
+    self.handle('status', StatusColumnHandler, valid_states=Request.VALID_STATES, default_value='Draft')
     self.handle_date('requested_on')
     self.handle_date('due_on')
     self.handle_text_or_html('description')
