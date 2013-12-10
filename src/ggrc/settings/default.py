bd6c7f22f4d012014a1a725eeed5bff4ea21e0e1
Silas Barta <sbarta@gmail.com>
2013-12-10 11:32:30 -0800

    Version to s18.

--- src/ggrc/settings/default.py
+++ src/ggrc/settings/default.py
@@ -20,7 +20,7 @@ EXTENSIONS = []
 # Deployment-specific variables
 COMPANY = "Company, Inc."
 COMPANY_LOGO_TEXT = "Company GRC"
-VERSION = "s17"
+VERSION = "s18"
 
 # Initialize from environment if present
 import os
