d1be2076fd67f8ad2c6cfb926b05ab53df1fb493
Andy Kant <andy@andykant.com>
2013-12-11 11:38:23 -0600

    Added support for person mapping for person/auth widget

--- src/ggrc/assets/javascripts/apps/programs.js
+++ src/ggrc/assets/javascripts/apps/programs.js
@@ -343,7 +343,7 @@ $(function() {
           widget_id: "person"
         , widget_name: "People"
         , widget_icon: "person"
-        , extra_widget_actions_view: GGRC.mustache_path + "/ggrc_basic_permissions/people_roles/authorizations_modal_actions.mustache"
+        // , extra_widget_actions_view: GGRC.mustache_path + "/ggrc_basic_permissions/people_roles/authorizations_modal_actions.mustache"
         , content_controller: GGRC.Controllers.ListView
         , content_controller_options: {
               list_view: GGRC.mustache_path + "/ggrc_basic_permissions/people_roles/authorizations_by_person_list.mustache"
