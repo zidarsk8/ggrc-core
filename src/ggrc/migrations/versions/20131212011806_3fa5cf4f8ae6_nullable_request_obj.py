ea182d68c88ead24bf0ba9472bb4c7936ab46f4e
Dan Ring <dfring@gmail.com>
2013-12-27 10:32:54 -0800

    Fix migration order

--- src/ggrc/migrations/versions/20131212011806_3fa5cf4f8ae6_nullable_request_obj.py
+++ src/ggrc/migrations/versions/20131212011806_3fa5cf4f8ae6_nullable_request_obj.py
@@ -2,14 +2,14 @@
 """Nullable request objective
 
 Revision ID: 3fa5cf4f8ae6
-Revises: 49c670c7d705
+Revises: 58da8a74882c
 Create Date: 2013-12-12 01:18:06.300862
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '3fa5cf4f8ae6'
-down_revision = '49c670c7d705'
+down_revision = '58da8a74882c'
 
 from alembic import op
 import sqlalchemy as sa
