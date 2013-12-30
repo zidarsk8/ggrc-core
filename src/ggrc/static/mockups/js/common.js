08166cc37d803b18654d0e7880a87de665563367
ArrayStudio <predrag@arraystudio.com>
2013-12-30 21:44:56 +0100

    adding assessments workflow in mockups

--- src/ggrc/static/mockups/js/common.js
+++ src/ggrc/static/mockups/js/common.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
   $('.lhs-holder').css('height', 100 + '%');
   $('.inner-nav').css('height', 100 + '%');
   $('.object-area').css('height', 100 + '%');
+  $('.object-area').css('width', 67.4 + '%'); //FIXME-UI for Predrag: Fix widht of object area
   
   $('body').on('click', '.info-expand a', function() {
     var $this = $(this),
@@ -16,6 +17,85 @@ $(document).ready(function(){
     } else {
       $show_hide.slideDown('fast');
       $this.addClass("active");
+    }
+    
+    return false;
+    
+  });
+  
+  $('body').on('click', '.expand-link a', function() {
+    var $this = $(this),
+        $show_hide = $this.closest('.row-fluid').next('.hidden-fields-area');
+    
+    if($this.hasClass("active")) {
+      $show_hide.slideUp('fast');
+      $this.removeClass("active");
+    } else {
+      $show_hide.slideDown('fast');
+      $this.addClass("active");
+    }
+    
+    return false;
+    
+  });
+  
+  $('body').on('click', 'ul.tree-structure .item-main .grcobject, ul.tree-structure .item-main .openclose', function(e) {
+    openclose.call(this);
+    e.stopPropagation();
+  });
+
+  function openclose(command) {
+    var $that = $(this)
+    , use_slide = $that.length < 100
+
+    $that.each(function(){
+      var $this = $(this)
+        , $main = $this.closest('.item-main')
+        , $li = $main.closest('li')
+        , $content = $li.children('.item-content')
+        , $icon = $main.find('.openclose')
+        , $parentTree = $this.closest('ul.new-tree')
+        , cmd = command;
+
+      if(typeof cmd !== "string" || cmd === "toggle") {
+        cmd = $icon.hasClass("active") ? "close" : "open";
+      }
+
+      if (cmd === "close") {
+        
+        use_slide ? $content.slideUp('fast') : $content.css("display", "none");
+        $icon.removeClass('active');
+        $li.removeClass('item-open');
+        // Only remove tree open if there are no open siblings
+        !$li.siblings('.item-open').length && $parentTree.removeClass('tree-open');
+        $content.removeClass('content-open');
+      } else if(cmd === "open") {
+        use_slide ? $content.slideDown('fast') : $content.css("display", "block");
+        $icon.addClass('active');
+        $li.addClass('item-open');
+        $parentTree.addClass('tree-open');
+        $content.addClass('content-open');
+      }
+    });
+
+    return this;
+    
+  }
+  
+  $.fn.openclose = openclose;
+  
+  $('body').on('click', '.advanced-filter-trigger', function() {
+    var $this = $(this),
+        $filters = $this.closest('.inner-tree').find('.advanced-filters');
+    
+    if($this.hasClass("active")) {
+      $filters.slideUp('fast');
+      $this.removeClass("active");
+      $this.html('<i class="grcicon-search"></i> Show Filters');
+    } else {
+      $filters.slideDown('fast');
+      $this.addClass("active");
+      $this.html('<i class="grcicon-search"></i> Hide Filters');
     }
     
     return false;
