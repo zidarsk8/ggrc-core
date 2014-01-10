2299ccdfd5601df665513a7a0281da0497be8f4d
Silas Barta <sbarta@gmail.com>
2014-01-10 12:15:56 -0800

    Make Objective options on PBC Request import.

--- src/ggrc/converters/requests.py
+++ src/ggrc/converters/requests.py
@@ -21,7 +21,7 @@ class RequestRowConverter(BaseRowConverter):
 
   def reify(self):
     self.handle('slug', SlugColumnHandler)
-    self.handle('objective_id', ObjectiveHandler)
+    self.handle('objective_id', ObjectiveHandler, is_needed_later=True)
     self.handle('request_type', RequestTypeColumnHandler, is_required=True)
     self.handle('status', StatusColumnHandler, valid_states=Request.VALID_STATES, default_value='Draft')
     self.handle_date('requested_on', is_required=True)
