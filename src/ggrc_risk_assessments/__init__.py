3da2fd79574994c30a5e12dcb7acac1b83c60aec
Dan Ring <dfring@gmail.com>
2014-01-12 21:41:23 -0800

    Add `ggrc_risk_assessments` extension
    * Add models, service endpoints, and migration for 5 initial models:
      Template, RiskAssessment, RiskAssessmentMapping, Thread, Vulnerability
    * Add extension to default extensions for Behave tests and vagrant
      environment

---
+++ src/ggrc_risk_assessments/__init__.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2013 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+# Created By: dan@reciprocitylabs.com
+# Maintained By: dan@reciprocitylabs.com
+
+from flask import Blueprint
+from ggrc import settings
+from ggrc.app import app
+from ggrc.services.registry import service
+import ggrc_risk_assessments.models as models
+
+
+# Initialize Flask Blueprint for extension
+blueprint = Blueprint(
+  'risk_assessments',
+  __name__,
+  template_folder='templates',
+  static_folder='static',
+  static_url_path='/static/ggrc_risk_assessments',
+)
+
+
+# Initialize service endpoints
+
+all_collections = [
+  service('templates', models.Template),
+  service('risk_assessments', models.RiskAssessment),
+  service('risk_assessment_mappings', models.RiskAssessmentMapping),
+  service('threats', models.Threat),
+  service('vulnerabilities', models.Vulnerability),
+]
