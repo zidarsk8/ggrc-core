b67fdb652db782ed932110fb20b0d156c5437fef
David Charboneau <david@reciprocitylabs.com>
2014-01-29 14:16:14 -0500

    Fix implications to use the ProgramMappingEditor role.

---
+++ src/ggrc_basic_permissions/migrations/versions/20140129110724_3ee919a0daf_add_publicprogramedi.py
@@ -0,0 +1,23 @@
+
+"""Add PublicProgramEditor role
+
+Revision ID: 3ee919a0daf
+Revises: 496256981937
+Create Date: 2014-01-29 11:07:24.758222
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3ee919a0daf'
+down_revision = '496256981937'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
