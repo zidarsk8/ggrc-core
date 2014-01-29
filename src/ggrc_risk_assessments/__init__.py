e93c50829d9df0bf3f695b0027d6d67395423a6f
Dan Ring <dfring@gmail.com>
2014-01-28 16:56:09 -0800

    Enable configuration of Risk Assessments UI URL
    * `RISK_ASSESSMENT_URL` is now a config variable, and available on
      client-side for Admin users
    * Use this new config variable in templates to link users to the correct
      external server

--- src/ggrc_risk_assessments/__init__.py
+++ src/ggrc_risk_assessments/__init__.py
@@ -6,6 +6,7 @@
 from flask import Blueprint
 from ggrc import settings
 from ggrc.app import app
+from ggrc.rbac import permissions
 from ggrc.services.registry import service
 from ggrc.views.registry import object_view
 import ggrc_risk_assessments.models as models
@@ -19,6 +20,17 @@ blueprint = Blueprint(
   static_folder='static',
   static_url_path='/static/ggrc_risk_assessments',
 )
+
+
+def get_public_config(current_user):
+  """Expose additional permissions-dependent config to client.
+    Specifically here, expose RISK_ASSESSMENT_URL values to ADMIN users.
+  """
+  public_config = {}
+  if permissions.is_admin():
+    if hasattr(settings, 'RISK_ASSESSMENT_URL'):
+      public_config['RISK_ASSESSMENT_URL'] = settings.RISK_ASSESSMENT_URL
+  return public_config
 
 
 # Initialize service endpoints
