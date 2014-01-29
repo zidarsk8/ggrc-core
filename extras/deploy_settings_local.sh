fe881fc88d07074969e90b1c5cfa60b2316668a8
Dan Ring <dfring@gmail.com>
2014-01-28 23:14:46 -0800

    Add secret key to deployment pipeline

--- extras/deploy_settings_local.sh
+++ extras/deploy_settings_local.sh
@@ -3,6 +3,7 @@
 APPENGINE_INSTANCE=local
 SETTINGS_MODULE="development app_engine ggrc_basic_permissions.settings.development"
 DATABASE_URI='mysql+mysqldb://root:root@localhost/ggrcdev?charset=utf8'
+SECRET_KEY='Something-secret'
 GAPI_KEY='<Google Browser Key>'
 GAPI_CLIENT_ID='<Google OAuth Client ID>'
 GAPI_ADMIN_GROUP='<Google Group Email Address>'
