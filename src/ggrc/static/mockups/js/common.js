383d8ecc1e683c5523623de7a6aa5d67760ff234
Vladan Mitevski <vladan@arraystudio.com>
2014-03-05 19:31:21 +0100

    assessment page creating and modal for assessment workflow

--- src/ggrc/static/mockups/js/common.js
+++ src/ggrc/static/mockups/js/common.js
@@ -244,6 +244,14 @@ $(document).ready(function(){
     
     return false;
   });
+  
+  $('.workflow-accordion').on('show', function (e) {
+    $(e.target).prev('.accordion-heading').find('.accordion-toggle').addClass('active');
+  });
+
+  $('.workflow-accordion').on('hide', function (e) {
+    $(this).find('.accordion-toggle').not($(e.target)).removeClass('active');
+  });
 
 });
 
