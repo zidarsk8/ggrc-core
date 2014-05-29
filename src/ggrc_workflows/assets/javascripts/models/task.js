a96f7d323cdbe5dffb45a7773b7e021f31c03802
Dan Ring <dfring@gmail.com>
2014-05-28 18:48:38 -0700

    Add initial javascript models, extensions, and templates

---
+++ src/ggrc_workflows/assets/javascripts/models/task.js
@@ -0,0 +1,34 @@
+/*!
+    Copyright (C) 2013 Google Inc., authors, and contributors <see AUTHORS file>
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+    Created By: dan@reciprocitylabs.com
+    Maintained By: dan@reciprocitylabs.com
+*/
+
+
+(function(can) {
+
+  can.Model.Cacheable("CMS.Models.Task", {
+    root_object: "task",
+    root_collection: "tasks",
+    category: "workflow",
+    findAll: "GET /api/tasks",
+    findOne: "GET /api/tasks/{id}",
+    create: "POST /api/tasks",
+    update: "PUT /api/tasks/{id}",
+    destroy: "DELETE /api/tasks/{id}",
+
+    attributes: {
+    },
+    tree_view_options: {
+      //show_view: GGRC.mustache_path + "/tasks/tree.mustache",
+      footer_view: GGRC.mustache_path + "/tasks/tree_footer.mustache"
+    },
+
+    init: function() {
+      this._super && this._super.apply(this, arguments);
+      this.validatePresenceOf("title");
+    }
+  }, {});
+
+})(window.can);
