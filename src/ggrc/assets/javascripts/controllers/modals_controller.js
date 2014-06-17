zidarsk8/ggrc-core

commit 8a5f600c65b7b45fd34bf8a7631961a6d5a19638

Bugfix: Autocomplete not setting value correctly

src/ggrc/assets/javascripts/controllers/modals_controller.js
@@ -268,7 +268,10 @@ can.Control("GGRC.Controllers.Modals", {
 
   , "input, textarea, select change" : function(el, ev) {
       this.options.instance.removeAttr("_suppress_errors");
-      this.set_value_from_element(el);
+      // Set the value if it isn't a search field
+      if(!el.hasClass("search-icon")){
+        this.set_value_from_element(el);
+      }
   }
 
   , "input:not([data-lookup]), textarea keyup" : function(el, ev) {

commit c7116689af3596b646a3f594b1b4addbb306e97c

Bugfix: Reenable validation for Audit Firm field
8a5f600 broke validations for autocomplete fields. This
commit fixes this issue by moving the code that sets
_tranisent field into the keyup event

src/ggrc/assets/javascripts/controllers/modals_controller.js
@@ -95,6 +95,21 @@ can.Control("GGRC.Controllers.Modals", {
   , "input[data-lookup] focus" : function(el, ev) {
     this.autocomplete(el);
   }
+  , "input[data-lookup] keyup" : function(el, ev) {
+    // Set the transient field for validation
+    var name = el.attr('name').split('.'),
+        instance = this.options.instance,
+        value = el.val();
+    
+    name.pop(); //set the owner to null, not the email
+    instance._transient || instance.attr("_transient", new can.Observe({}));
+    can.reduce(name.slice(0, -1), function(current, next) {
+      current = current + "." + next;
+      instance.attr(current) || instance.attr(current, new can.Observe({}));
+      return current;
+    }, "_transient");
+    instance.attr(["_transient"].concat(name).join("."), value);
+  }
 
   , autocomplete : function(el) {
     $.cms_autocomplete.call(this, el);
@@ -367,14 +382,6 @@ can.Control("GGRC.Controllers.Modals", {
       } else {
 
         if($elem.is("[data-lookup]")) {
-          name.pop(); //set the owner to null, not the email
-          instance._transient || instance.attr("_transient", new can.Observe({}));
-          can.reduce(name.slice(0, -1), function(current, next) {
-            current = current + "." + next;
-            instance.attr(current) || instance.attr(current, new can.Observe({}));
-            return current;
-          }, "_transient");
-          instance.attr(["_transient"].concat(name).join("."), value);
           if(!value) {
             value = null;
           } else {

src/ggrc/assets/javascripts/models/audit_models.js
@@ -75,7 +75,7 @@ can.Model.Cacheable("CMS.Models.Audit", {
       var audit_firm = this.attr("audit_firm");
       var audit_firm_text = this.attr("_transient.audit_firm");
       if(!audit_firm && audit_firm_text
-        || (audit_firm_text != null && audit_firm != null && audit_firm_text !== audit_firm.reify().title)) {
+        || (audit_firm_text !== "" && audit_firm_text != null && audit_firm != null && audit_firm_text !== audit_firm.reify().title)) {
         return "No valid org group selected for firm";
       }
     });