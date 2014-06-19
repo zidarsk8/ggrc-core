c93dfe43ed5890787a0ad0f7b9ca1624b8656cc6
Anže Pečar <ensmotko@gmail.com>
2014-06-19 10:20:45 -0700

    Assigned state for cycle tasks

--- src/ggrc_workflows/models/cycle_task_group_object_task.py
+++ src/ggrc_workflows/models/cycle_task_group_object_task.py
@@ -15,7 +15,7 @@ class CycleTaskGroupObjectTask(
   __tablename__ = 'cycle_task_group_object_tasks'
   _title_uniqueness = False
 
-  VALID_STATES = (None, u'InProgress', u'Finished', u'Verified')
+  VALID_STATES = (None, u'Assigned', u'InProgress', u'Finished', u'Verified')
 
   cycle_task_group_object_id = db.Column(
       db.Integer, db.ForeignKey('cycle_task_group_objects.id'), nullable=False)
