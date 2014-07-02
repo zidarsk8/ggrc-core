140ae1ad8e4962c1f7eff0d1d2d8c304b9e0a832
Dan Ring <dfring@gmail.com>
2014-07-02 16:35:38 -0700

    Formatting fixup

--- src/ggrc_workflows/assets/javascripts/controllers/workflow_page.js
+++ src/ggrc_workflows/assets/javascripts/controllers/workflow_page.js
@@ -22,17 +22,17 @@
     },
 
     "[data-ggrc-action=start-cycle] click": function() {
-      var page_instance = GGRC.page_instance()
-        , that = this
-        , cycle;
+      var page_instance = GGRC.page_instance(),
+          that = this,
+          cycle;
 
       GGRC.Controllers.Modals.confirm({
-        modal_title : "Confirm"
-        , modal_confirm : "Proceed"
-        , skip_refresh : true
-        , button_view : GGRC.mustache_path + "/modals/confirm_buttons.mustache"
-        , content_view : GGRC.mustache_path + "/workflows/confirm_start.mustache"
-        , instance : GGRC.page_instance()
+        modal_title : "Confirm",
+        modal_confirm : "Proceed",
+        skip_refresh : true,
+        button_view : GGRC.mustache_path + "/modals/confirm_buttons.mustache",
+        content_view : GGRC.mustache_path + "/workflows/confirm_start.mustache",
+        instance : GGRC.page_instance()
       }, function() {
         cycle = new CMS.Models.Cycle({
           context: { id: null, type: "Context" },
