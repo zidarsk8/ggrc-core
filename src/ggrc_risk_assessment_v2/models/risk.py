4bfeaf92cd7ec962ac38081101cae9288bd07b7a
Silas Barta <sbarta@gmail.com>
2014-07-24 12:55:18 -0700

    Add backend required fields: Description, Contact, Owner.

--- src/ggrc_risk_assessment_v2/models/risk.py
+++ src/ggrc_risk_assessment_v2/models/risk.py
@@ -3,10 +3,17 @@
 # Created By: silas@reciprocitylabs.com
 # Maintained By: silas@reciprocitylabs.com
 
+from sqlalchemy.ext.declarative import declared_attr
 
 from ggrc import db
-from ggrc.models.mixins import Base, Titled, Slugged
+from ggrc.models.object_owner import Ownable
+from ggrc.models.mixins import Base, Described, Slugged, Titled, WithContact, deferred
 
 
-class Risk(Titled, Slugged, Base, db.Model):
+class Risk(Described, Ownable, WithContact, Titled, Slugged, Base, db.Model):
   __tablename__ = 'risks'
+
+  # Overriding mixin to make mandatory
+  @declared_attr
+  def description(cls):
+    return deferred(db.Column(db.Text, nullable=False), cls.__name__)
