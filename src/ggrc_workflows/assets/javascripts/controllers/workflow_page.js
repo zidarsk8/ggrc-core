fd2f56245ae75f00f75476426750e3011c0ef37e
Anže Pečar <ensmotko@gmail.com>
2014-07-28 12:48:11 +0200

    Bugfix: number of assigned tasks wasn't updating
    When end_cycle button is clicked.

--- src/ggrc_workflows/assets/javascripts/controllers/workflow_page.js
+++ src/ggrc_workflows/assets/javascripts/controllers/workflow_page.js
@@ -61,7 +61,16 @@
           }
         }
         return $.when.apply($, dfds).done(function() {
-          GGRC.page_instance().refresh();
+          return GGRC.page_instance().refresh();
+        }).then(function(){
+          // We need to update person's assigned_tasks mapping manually
+          var person_id = GGRC.current_user.id,
+              person = CMS.Models.Person.cache[person_id];
+              binding = person.get_binding('assigned_tasks');
+
+          // FIXME: Find a better way of removing stagnant items from the list.
+          binding.list.splice(0, binding.list.length);
+          return binding.loader.refresh_list(binding);
         });
       }(), el);
     },
