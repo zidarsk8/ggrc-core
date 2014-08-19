5db417c44f0e793ccc63911c813456b70f783a88
Sasmita Mishra <sasmitamishra@yahoo.com>
2014-08-19 12:48:43 -0700

    Added is_in_selector flag to show/hide unmap link for people in search

--- src/ggrc/assets/javascripts/controllers/modal_selector_controller.js
+++ src/ggrc/assets/javascripts/controllers/modal_selector_controller.js
@@ -837,6 +837,7 @@
               instance: option
             , binding: null
             , mappings: []
+            , is_in_selector: true
             }
           ;
         if (self.results_lists[option_model_name]) {
