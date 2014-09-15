fb7904acc222c4b057b268dddffe00f065ac4c4f
vcmouli90 <mouli@meics.org>
2014-09-15 10:59:48 -0700

    Ignore Assigned status change for generating email.

--- src/ggrc/notification/email.py
+++ src/ggrc/notification/email.py
@@ -68,7 +68,7 @@ class EmailNotification(NotificationBase):
     ignored_notifs={}
     #ToDo(Mouli): The valid states mapping must be set outside the generic email notification module
     ignored_states={
-      'CycleTaskGroupObjectTask': ['Finished', 'InProgress', 'Verified'],
+      'CycleTaskGroupObjectTask': ['Assigned', 'Finished', 'InProgress', 'Verified'],
       'CycleTaskGroup': ['InProgress', 'Verified'],
     }
     for notif in notifs:
