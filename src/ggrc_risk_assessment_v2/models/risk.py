451d79f5bc55d228eb20bb0672480373dfeeb29d
Anže Pečar <ensmotko@gmail.com>
2014-09-21 15:30:44 +0200

    Fix mappings for risk objects

--- src/ggrc_risk_assessment_v2/models/risk.py
+++ src/ggrc_risk_assessment_v2/models/risk.py
@@ -8,12 +8,16 @@ from sqlalchemy.ext.declarative import declared_attr
 from ggrc import db
 from ggrc.models.associationproxy import association_proxy
 from ggrc.models.object_owner import Ownable
+from ggrc.models.object_control import Controllable
+from ggrc.models.object_document import Documentable
+from ggrc.models.object_objective import Objectiveable
 from ggrc.models.mixins import Base, Described, Slugged, Titled, WithContact, deferred
 from ggrc.models.reflection import PublishOnly
+from ggrc.models.relationship import Relatable
 
 
-
-class Risk(Described, Ownable, WithContact, Titled, Slugged, Base, db.Model):
+class Risk(Relatable, Controllable, Objectiveable, Documentable, Described,
+    Ownable, WithContact, Titled, Slugged, Base, db.Model):
   __tablename__ = 'risks'
 
   # Overriding mixin to make mandatory
