a38faa0010a3c7918ed277194810b16db239a0f5
Sasmita Mishra <sasmitamishra@yahoo.com>
2014-10-27 15:04:12 -0700

    removed atttribute uiindex from mustache files CORE-413

--- src/ggrc/assets/javascripts/controllers/modals_controller.js
+++ src/ggrc/assets/javascripts/controllers/modals_controller.js
@@ -270,11 +270,14 @@ can.Control("GGRC.Controllers.Modals", {
     
     //Update UI status array 
     var $form = $(this.element).find('form');
-    //var storable_ui = $form.find('[tabindex]').length;
-    var storable_ui = $form.find('[uiindex]').length;
-    //Some times the tab index and UI-index are not continuous
-    //Add extra for missing entries
-    storable_ui = storable_ui + 10;
+    var tab_list = $form.find('[tabindex]');
+    var hidable_tabs = 0;
+    for (var i = 0; i < tab_list.length; i++) {
+      if ($(tab_list[i]).attr('tabindex') > 0)
+        hidable_tabs++;
+    }
+    //ui_array index is used as the tab_order, Add extra space for skipped numbers
+    var storable_ui = hidable_tabs + 10;
     for(var i = 0; i < storable_ui; i++) {
       //When we start, all the ui elements are visible
       this.options.ui_array.push(0);
@@ -466,10 +469,16 @@ can.Control("GGRC.Controllers.Modals", {
       $hidable.addClass("hidden");
       this.options.reset_visible = true;
       //update ui array
-      var $ui_unit = $hidable.find('[uiindex]');
-      var tab_value = $ui_unit.attr('uiindex');
-      this.options.ui_array[tab_value-1] = 1;
-      $ui_unit.attr('tabindex', '-1');
+      var ui_unit = $hidable.find('[tabindex]');
+      var i, tab_value;
+      for (i = 0; i < ui_unit.length; i++) {
+        tab_value = $(ui_unit[i]).attr('tabindex');
+        if(tab_value > 0) {
+          this.options.ui_array[tab_value-1] = 1;
+          $(ui_unit[i]).attr('tabindex', '-1');
+          $(ui_unit[i]).attr('uiindex', tab_value);
+        }
+      }
 
       for(var i=0; i < $el.closest('.hide-wrap.hidable').find('.inner-hidable').length; i++) {
         if(i == 1) {
@@ -496,12 +505,15 @@ can.Control("GGRC.Controllers.Modals", {
     $(this.element).find('.inner-hide').addClass('inner-hidable');
 
     //Set up the hidden elements index to 1
-    var hidden_elements = $hidables.find('[uiindex]');
+    var hidden_elements = $hidables.find('[tabindex]');
     for(var i = 0; i < hidden_elements.length; i++) {
-      var tab_value = $(hidden_elements[i]).attr('uiindex');
+      var tab_value = $(hidden_elements[i]).attr('tabindex');
       //The UI array index start from 0, and tab-index/io-index is from 1
-      this.options.ui_array[tab_value-1] = 1;
-      $(hidden_elements[i]).attr('tabindex', '-1');
+      if(tab_value > 0){
+        this.options.ui_array[tab_value-1] = 1;
+        $(hidden_elements[i]).attr('tabindex', '-1');
+        $(hidden_elements[i]).attr('uiindex', tab_value);
+      }
     }
 
     el.fadeOut(500);
@@ -551,9 +563,10 @@ can.Control("GGRC.Controllers.Modals", {
       for (var i = 0; i < this.options.ui_array.length; i++){
         if(this.options.ui_array[i] == 1){
           tabindex = i+1;
-          str = '[uiindex=' + tabindex + ']';
+          str = '[tabindex=' + tabindex + ']';
           $selected = $form.find(str);
           $selected.closest('.hidable').addClass('hidden');
+          $selected.attr('uiindex', tabindex);
           $selected.attr('tabindex', '-1');
         }
       }
