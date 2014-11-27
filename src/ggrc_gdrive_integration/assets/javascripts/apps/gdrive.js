cb51ce9f86aba7b36710b38978c517b0cb23f3a3
dandv <ddascalescu+github@gmail.com>
2014-11-26 17:34:42 -0800

    Remove "Population Sample" from the UI
    Backend changes TBD - see PR #2022

--- src/ggrc_gdrive_integration/assets/javascripts/apps/gdrive.js
+++ src/ggrc_gdrive_integration/assets/javascripts/apps/gdrive.js
@@ -53,12 +53,6 @@
     InterviewResponse : {
       _mixins : ["fileable", "folderable"],
     },
-    PopulationSampleResponse : {
-      _mixins : ["fileable", "folderable"],
-      _request : new GGRC.ListLoaders.DirectListLoader("Request", "responses", "request"),
-      folders_via_request: new GGRC.ListLoaders.CrossListLoader("_request", "extended_folders"),
-      extended_folders: new GGRC.ListLoaders.MultiListLoader(["folders", "folders_via_request"])
-    },
     Document : {
       _mixins : ["fileable"]
     },
