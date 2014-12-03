3d9c87c09795b456cd82eeccfba3a379fe63bd33
Laran Evans <laran@reciprocitylabs.com>
2014-12-03 11:54:49 -0800

    Whitespace fix.

--- src/ggrc/models/mixins.py
+++ src/ggrc/models/mixins.py
@@ -484,14 +484,14 @@ class CustomAttributable(object):
 
         from ggrc.fulltext.mysql import MysqlRecordProperty
         from sqlalchemy import and_
-		# 2) Delete all fulltext_record_properties for the list of values
+        # 2) Delete all fulltext_record_properties for the list of values
         db.session.query(MysqlRecordProperty)\
-			.filter(and_(\
-					MysqlRecordProperty.type==cls.__class__.__name__,
+            .filter(and_(\
+                    MysqlRecordProperty.type==cls.__class__.__name__,
                     MysqlRecordProperty.property.in_(ftrp_properties))
-				).delete(synchronize_session='fetch')
+                ).delete(synchronize_session='fetch')
 
-		# 3) Delete the list of custom attribute values
+        # 3) Delete the list of custom attribute values
         db.session.query(CustomAttributeValue)\
             .filter(CustomAttributeValue.id.in_(attr_value_ids))\
             .delete(synchronize_session='fetch')
