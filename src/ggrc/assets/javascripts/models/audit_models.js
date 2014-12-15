6b45014cae1b562dea542560ac590b994823a8f6
Swizec Teller <swizec@swizec.com>
2014-12-15 15:43:46 +0100

    validation+picker work correctly

--- src/ggrc/assets/javascripts/models/audit_models.js
+++ src/ggrc/assets/javascripts/models/audit_models.js
@@ -413,7 +413,7 @@ can.Model.Cacheable("CMS.Models.Response", {
     }
 
     this.validateNonBlank("description");
-    this.validatePresenceOf("assignee");
+    this.validatePresenceOf("contact");
   }
   , create : "POST /api/responses"
   , update : "PUT /api/responses/{id}"
