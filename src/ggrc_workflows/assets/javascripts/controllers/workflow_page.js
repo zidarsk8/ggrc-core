e5a4f8d3885a4a1d18c5355709b33dace2913bb0
Swizec Teller <swizec@swizec.com>
2014-12-16 17:35:38 +0100

    clone task group modal opens

--- src/ggrc_workflows/assets/javascripts/controllers/workflow_page.js
+++ src/ggrc_workflows/assets/javascripts/controllers/workflow_page.js
@@ -223,6 +223,55 @@
     }
   });
 
+  can.Model.Cacheable("CMS.ModelHelpers.CloneTaskGroup", {
+    defaults : {
+      clone_objects: true,
+      clone_tasks: true
+    }
+  }, {
+    // form_preload: function(new_object_form) {
+    //   this.attr("clone_people", true);
+    //   this.attr("clone_tasks", true);
+    // },
+    refresh: function() {
+      return $.when(this);
+    },
+    save: function() {
+      var task_group = new CMS.Models.TaskGroup({
+        clone: this.source_task_group.id,
+        context: null,
+        clone_objects: this.clone_objects,
+        clone_tasks: this.clone_tasks
+      });
+
+      return task_group.save().then(function(task_group) {
+        GGRC.navigate(task_group.viewLink);
+      });
+
+    }
+  });
+
+  can.Component.extend({
+    tag: "task-group-clone",
+    template: "<content/>",
+    events: {
+      click: function(el) {
+        var task_group, $target;
+
+        $target = $('<div class="modal hide"></div>').uniqueId();
+        $target.modal_form({}, el);
+        $target.ggrc_controllers_modals({
+          modal_title: "Clone Task Group",
+          model: CMS.ModelHelpers.CloneTaskGroup,
+          instance: new CMS.ModelHelpers.CloneTaskGroup({ source_task_group: this.scope.taskGroup }),
+          content_view: GGRC.mustache_path + "/task_groups/clone_modal_content.mustache",
+          custom_save_button_text: "Proceed",
+          button_view: GGRC.Controllers.Modals.BUTTON_VIEW_SAVE_CANCEL
+        });
+      }
+    }
+  });
+
   can.Component.extend({
     tag: "cycle-end-cycle",
     template: "<content/>",
