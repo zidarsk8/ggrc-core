76ad0da89a4ee0caa9e893ac2adf34f28517ade3
Anže Pečar <ensmotko@gmail.com>
2015-01-06 16:15:11 +0100

    Prevent having ## in the url link

--- src/ggrc_workflows/assets/javascripts/models/helpers.js
+++ src/ggrc_workflows/assets/javascripts/models/helpers.js
@@ -83,7 +83,7 @@ can.Observe("CMS.ModelHelpers.ApprovalWorkflow", {
               + ") asked you to review newly created "
               + that.original_object.constructor.model_singular + ' "' + that.original_object.title
               + '" before ' + moment(that.end_date).format("MM/DD/YYYY") + ". "
-              + "Click <a href='" + window.location.href.replace(/#.*$/, "#")
+              + "Click <a href='" + window.location.href.replace(/#.*$/, "")
               + "#workflows_widget'>here</a> to perform a review.",
             context: that.original_object.context
           }).save()
