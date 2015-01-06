db06fc72414d0a93ad42ffca531cd2f3592fcd1b
Swizec Teller <swizec@swizec.com>
2015-01-06 14:35:04 +0100

    this should fix the link in email

--- src/ggrc_workflows/assets/javascripts/models/helpers.js
+++ src/ggrc_workflows/assets/javascripts/models/helpers.js
@@ -84,7 +84,7 @@ can.Observe("CMS.ModelHelpers.ApprovalWorkflow", {
               + that.original_object.constructor.model_singular + ' "' + that.original_object.title
               + '" before ' + moment(that.end_date).format("MM/DD/YYYY") + ". "
               + "Click <a href='" + window.location.href.replace(/#.*$/, "#")
-              + "workflows_widget'>here</a> to perform a review.",
+              + "#workflows_widget'>here</a> to perform a review.",
             context: that.original_object.context
           }).save()
         ).then(function(wf) {
