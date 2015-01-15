1f3a903087980e8925cf24d61ed131bc508d1f86
Swizec Teller <swizec@swizec.com>
2015-01-15 14:29:23 +0100

    google headers have years

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 
 // Karma Configuration
-// Copyright (C) 2013 Google Inc., authors, and contributors <see AUTHORS file>
+// Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
 // Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 // Created By: swizec@reciprocitylabs.com
 // Maintained By: swizec@reciprocitylabs.com
