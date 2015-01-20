3852602716d7bddbb9b9b23fa68931daae75d8a7
Swizec Teller <swizec@swizec.com>
2015-01-20 16:55:22 +0100

    tets even faster to run (6s vs 30s, less than 10s refresh wait time)

--- src/tests/ggrc/__init__.py
+++ src/tests/ggrc/__init__.py
@@ -11,22 +11,8 @@ from ggrc.models import create_db, drop_db
 from ggrc import settings
 
 class TestCase(BaseTestCase):
-  done_first_setup = False
-
-  @classmethod
-  def do_first_setup(cls):
-    if cls.done_first_setup:
-      return
-
-    db.engine.execute("DROP DATABASE IF EXISTS ggrcdevtest;")
-    db.engine.execute("CREATE DATABASE ggrcdevtest; USE ggrcdevtest;")
-
-    create_db(use_migrations=True, quiet=True)
-
-    cls.done_first_setup = True
-
   def setUp(self):
-    TestCase.do_first_setup()
+    pass
 
     # if getattr(settings, 'MEMCACHE_MECHANISM', False) is True:
     #   from google.appengine.api import memcache
