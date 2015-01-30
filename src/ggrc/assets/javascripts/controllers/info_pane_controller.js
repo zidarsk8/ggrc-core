67e4579874fe812e60ef7375ade15fca03ecfad8
Anže Pečar <ensmotko@gmail.com>
2015-01-30 18:14:10 +0100

    Info pane is now working with responses

--- src/ggrc/assets/javascripts/controllers/info_pane_controller.js
+++ src/ggrc/assets/javascripts/controllers/info_pane_controller.js
@@ -20,6 +20,8 @@ can.Control("CMS.Controllers.InfoPane", {
 
     if (instance instanceof CMS.Models.Person) {
       view = GGRC.mustache_path + "/ggrc_basic_permissions/people_roles/info.mustache";
+    } else if (instance instanceof CMS.Models.Response) {
+      view = GGRC.mustache_path + "/responses/info.mustache";
     } else if (view in GGRC.Templates) {
       view = GGRC.mustache_path + "/" + view + ".mustache";
     } else {
