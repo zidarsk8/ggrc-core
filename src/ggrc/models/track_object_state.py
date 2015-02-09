3d8d76c4214f711c67f5ee1b42404575ffe5f4ae
Laran Evans <laran@reciprocitylabs.com>
2015-02-09 10:09:26 -0800

    Add os_last_updated_by_user_id field to HasObjectState.

--- src/ggrc/models/track_object_state.py
+++ src/ggrc/models/track_object_state.py
@@ -4,8 +4,17 @@ from datetime import datetime
 from ggrc import db
 from sqlalchemy.ext.declarative import declared_attr
 from .mixins import deferred
+from ggrc.login import get_current_user_id
+from .reflection import PublishOnly
 
 class HasObjectState(object):
+
+  _publish_attrs = [
+    PublishOnly('os_last_updated_by_user_id'),
+    PublishOnly('os_last_updated'),
+    PublishOnly('os_approved_on'),
+    PublishOnly('os_state'),
+  ]
 
   def __init__(self):
     self._skip_os_state_update = False;
@@ -21,6 +30,10 @@ class HasObjectState(object):
   @declared_attr
   def os_approved_on(cls):
     return deferred(db.Column(db.DateTime), cls.__name__)
+
+  @declared_attr
+  def os_last_updated_by_user_id(cls):
+    return deferred(db.Column(db.Integer), cls.__name__)
 
   def skip_os_state_update(self):
     self._skip_os_state_update = True
@@ -43,6 +56,7 @@ def state_before_insert_listener(mapper, connection, target):
   if hasattr(target, 'os_state'):
     target.os_state = ObjectStates.DRAFT
     target.os_last_updated = datetime.now()
+    target.os_last_updated_by_user_id = get_current_user_id()
 
 def state_before_update_listener(mapper, connection, target):
   # import ipdb; ipdb.set_trace()
@@ -52,6 +66,7 @@ def state_before_update_listener(mapper, connection, target):
         return
     target.os_state = ObjectStates.MODIFIED
     target.os_last_updated = datetime.now()
+    target.os_last_updated_by_user_id = get_current_user_id()
 
 def track_state_for_class(object_class):
   event.listen(object_class, 'before_insert', state_before_insert_listener)
