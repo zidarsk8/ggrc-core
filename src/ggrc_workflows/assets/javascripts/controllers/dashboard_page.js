bbf1b71bd90427ffdbf6365ba9aad3f96d180692
Anže Pečar <ensmotko@gmail.com>
2015-02-18 14:23:43 +0100

    Add a render hook for dashboard widgets

---
+++ src/ggrc_workflows/assets/javascripts/controllers/dashboard_page.js
@@ -0,0 +1,23 @@
+/*!
+    Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+    Created By: anze@reciprocitylabs.com
+    Maintained By: anze@reciprocitylabs.com
+*/
+
+;(function(CMS, GGRC, can, $) {
+
+  can.Component.extend({
+    tag: "dashboard-widgets",
+    template: "<content/>",
+    init: function() {
+      console.log("www");
+    },
+    scope: {
+    },
+    events: {
+
+    }
+  });
+
+})(this.CMS, this.GGRC, this.can, this.can.$);
