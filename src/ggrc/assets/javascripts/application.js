903c4a75b453627a33085d80f7bf17eca60aea17
Ivan Tatic <ivan@simplified.me>
2015-03-29 19:02:03 +0200

    CanJS extensions.

--- src/ggrc/assets/javascripts/application.js
+++ src/ggrc/assets/javascripts/application.js
@@ -1150,31 +1150,3 @@ jQuery(function($) {
             || window.location.pathname.substring(1, (window.location.pathname + "/").indexOf("/", 1));
     }
 
-// a few core CanJS extensions below.
-// Core validation for fields not being "blank", i.e.
-// having no content when outside spaces are trimmed away.
-can.Model.validationMessages.non_blank = can.Map.validationMessages.non_blank = "cannot be blank";
-can.Model.validateNonBlank = can.Map.validateNonBlank = function (attrNames, options) {
-  can.Map.validate.call(this, attrNames, options, function (value) {
-      if (value === undefined || value === null || typeof value.trim === "function" && value.trim() === '') {
-          return this.constructor.validationMessages.non_blank;
-      }
-  });
-};
-
-// Adding reduce, a generally useful array comprehension.
-//  Bitovi decided against including it in core CanJS, but
-//  adding it here for easy universal use across can.List
-//  as well as arrays.
-can.reduce ||
-  (can.reduce = function(a, f, i) { if(a==null) return null; return [].reduce.apply(a, arguments.length < 3 ? [f] : [f, i]) });
-})(window.jQuery);
-
-
-// Turn camelSpace strings into Camel Space strings
-can.spaceCamelCase = function (string) {
-    return can.underscore(string)
-        .split("_")
-        .map(can.capitalize)
-        .join(" ");
-};
