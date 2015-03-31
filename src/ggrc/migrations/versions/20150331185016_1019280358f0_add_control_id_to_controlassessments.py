15347da288ef1e03201b198130c4514d9adb3069
Swizec Teller <swizec@swizec.com>
2015-03-31 22:27:23 +0200

    update migration after merge

--- src/ggrc/migrations/versions/20150331185016_1019280358f0_add_control_id_to_controlassessments.py
+++ src/ggrc/migrations/versions/20150331185016_1019280358f0_add_control_id_to_controlassessments.py
@@ -2,14 +2,14 @@
 """add control_id to ControlAssessments
 
 Revision ID: 1019280358f0
-Revises: 56bda17c92ee
-Create Date: 2015-03-30 18:50:16.859278
+Revises: 19471f93d42
+Create Date: 2015-03-31 18:50:16.859278
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '1019280358f0'
-down_revision = '56bda17c92ee'
+down_revision = '19471f93d42'
 
 from alembic import op
 import sqlalchemy as sa
