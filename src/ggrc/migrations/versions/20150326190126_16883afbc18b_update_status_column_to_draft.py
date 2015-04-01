bb936afb42954e4be450ad4b703184749e517328
Sasmita Mishra <sasmitamishra@yahoo.com>
2015-04-01 16:32:08 -0700

    Fixed indentation and license header

--- src/ggrc/migrations/versions/20150326190126_16883afbc18b_update_status_column_to_draft.py
+++ src/ggrc/migrations/versions/20150326190126_16883afbc18b_update_status_column_to_draft.py
@@ -1,3 +1,5 @@
+# Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
 """Update status column to draft
 
@@ -17,16 +19,15 @@ from sqlalchemy.sql import table, column
 from ggrc.models.track_object_state import ObjectStates, ObjectStateTables
 
 def upgrade():
-	for table_name in ObjectStateTables.table_names:
-		# Set the status value to Draft in all existing records where the value is Null
-		object_table = table(table_name,
-							column('status', sa.String(length=250)))
+  for table_name in ObjectStateTables.table_names:
+    # Set the status value to Draft in all existing records where the value is Null
+    object_table = table(table_name, column('status', sa.String(length=250)))
 
-		op.execute(
-			object_table.update().values(status = ObjectStates.DRAFT)\
-				.where(object_table.c.status == None)
-		)
+    op.execute(
+      object_table.update().values(status = ObjectStates.DRAFT)\
+        .where(object_table.c.status == None)
+    )
 
 
 def downgrade():
-    pass
+  pass
