82de320a64f0b219760ccddfae807ba5342b01b6
Anže Pečar <ensmotko@gmail.com>
2015-04-08 13:26:59 +0200

    Removes control assessment after save
    The after save was needed when we were creating the mappings on the
    server side.

--- src/ggrc/assets/javascripts/models/audit_models.js
+++ src/ggrc/assets/javascripts/models/audit_models.js
@@ -747,22 +747,6 @@ can.Model.Cacheable("CMS.Models.ControlAssessment", {
   object_model: can.compute(function() {
     return CMS.Models[this.attr("object_type")];
   }),
-  after_save: function() {
-    // TODO: I will make this a feature in cacheable when I'll be implementing
-    //       Issue objects
-    var audit = this.audit.id && this.audit.reify(),
-        binding;
-
-    if (!audit) {
-      return;
-    }
-
-    binding = audit.get_binding('related_control_assessments');
-
-    binding.list.push(
-      new GGRC.ListLoaders.MappingResult(this, binding)
-    );
-  }
 });
 
 })(this.can);
