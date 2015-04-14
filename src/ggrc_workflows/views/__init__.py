32f726c6f5c7951f99ede76c23d0ddd68473b966
Miha Zidar <zidarsk8@gmail.com>
2015-04-15 01:47:29 +0200

    Add show_pending view for notifications
    This is a helper view that will be available only in test deployments
    and is just meant as help, to list when and which email digest messages
    will be sent.

--- src/ggrc_workflows/views/__init__.py
+++ src/ggrc_workflows/views/__init__.py
@@ -4,6 +4,10 @@
 # Maintained By: dan@reciprocitylabs.com
 
 from ggrc_workflows import start_recurring_cycles
+from ggrc import notification
+from jinja2 import Environment, PackageLoader
+
+env = Environment(loader=PackageLoader('ggrc_workflows', 'templates'))
 
 
 def do_start_recurring_cycles():
@@ -11,7 +15,19 @@ def do_start_recurring_cycles():
   return 'Ok'
 
 
+def show_pending_notifications():
+  pending_notifications = notification.get_pending_notifications()
+
+  pending = env.get_template("notifications/pending_digest_notifitaitons.html")
+
+  return pending.render(data=sorted(pending_notifications.iteritems()))
+
+
 def init_extra_views(app):
   app.add_url_rule(
       "/start_recurring_cycles", "start_recurring_cycles",
       view_func=do_start_recurring_cycles)
+
+  app.add_url_rule(
+      "/_notifications/show_pending", "show_pending_notifications",
+      view_func=show_pending_notifications)
