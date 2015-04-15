90766f474945c7160c7859c21997e233ade16dd3
Miha Zidar <zidarsk8@gmail.com>
2015-04-15 14:57:51 +0200

    Add custom notification views to workflows
    Add extra views to workflows:
    - show todays digest notifications
    - send todays digest notifitations
    - show pending digest notifications
    - send pending digest notifitations

--- src/ggrc_workflows/views/__init__.py
+++ src/ggrc_workflows/views/__init__.py
@@ -5,6 +5,7 @@
 
 from ggrc_workflows import start_recurring_cycles
 from ggrc import notification
+from ggrc.notification import email
 from jinja2 import Environment, PackageLoader
 
 env = Environment(loader=PackageLoader('ggrc_workflows', 'templates'))
@@ -16,11 +17,28 @@ def do_start_recurring_cycles():
 
 
 def show_pending_notifications():
-  pending_notifications = notification.get_pending_notifications()
-
+  notifications = notification.get_pending_notifications()
   pending = env.get_template("notifications/pending_digest_notifitaitons.html")
+  return pending.render(data=sorted(notifications.iteritems()))
 
-  return pending.render(data=sorted(pending_notifications.iteritems()))
+
+def show_todays_digest_notifications():
+  notifications = notification.get_todays_notifications()
+  todays = env.get_template("notifications/todays_digest_notifications.html")
+  return todays.render(data=notifications)
+
+
+def send_pending_notifications():
+  digest_template = env.get_template("notifications/email_digest.html")
+  notifications = notification.get_pending_notifications()
+  for day_notif in notifications.iteritems():
+    for user_email, data in day_notif.iteritems():
+      email_body = digest_template.render(digest=data)
+      email.send_email(user_email, "test", email_body)
+
+
+def send_todays_digest_notifications():
+  pass
 
 
 def init_extra_views(app):
@@ -31,3 +49,15 @@ def init_extra_views(app):
   app.add_url_rule(
       "/_notifications/show_pending", "show_pending_notifications",
       view_func=show_pending_notifications)
+
+  app.add_url_rule(
+      "/_notifications/show_todays_digest", "show_todays_digest_notifications",
+      view_func=show_todays_digest_notifications)
+
+  app.add_url_rule(
+      "/_notifications/send_pending", "send_pending_notifications",
+      view_func=send_pending_notifications)
+
+  app.add_url_rule(
+      "/_notifications/send_todays_digest", "send_todays_digest_notifications",
+      view_func=send_todays_digest_notifications)
