ace27269f1f7c563befeb27426e5703a08f48e36
Miha Zidar <zidarsk8@gmail.com>
2015-04-15 14:51:50 +0200

    Add send_email function

--- src/ggrc/notification/email.py
+++ src/ggrc/notification/email.py
@@ -5,34 +5,33 @@
 
 
 """
- GGRC email notification module hook to prepares email, email digest, notify email to recipients
+ GGRC email notification module hook to prepares email, email digest, notify
+ email to recipients
 """
 
 
-from flask import current_app
 from google.appengine.api import mail
-from ggrc.models import Person, NotificationConfig
-from datetime import datetime, timedelta
-from ggrc import db
+from ggrc.models import NotificationConfig
 from ggrc import settings
 
 
 def getAppEngineEmail():
-  appengine_email = getattr(settings, 'APPENGINE_EMAIL')
-  if appengine_email is not None and appengine_email != '' and appengine_email !=" ":
-    return appengine_email
+  email = getattr(settings, 'APPENGINE_EMAIL')
+  if email is not None and email != '' and email != " ":
+    return email
   else:
     return None
 
+
 def isNotificationEnabled(person_id, notif_type):
   if notif_type == 'Email_Deferred':
-    notif_type='Email_Now'
+    notif_type = 'Email_Now'
   elif notif_type == 'Email_Digest_Deferred':
-    notif_type='Email_Digest'
-    notification_config=NotificationConfig.query.\
-    filter(NotificationConfig.person_id==person_id).\
-    filter(NotificationConfig.notif_type==notif_type).\
-    first()
+    notif_type = 'Email_Digest'
+    notification_config = NotificationConfig.query.\
+        filter(NotificationConfig.person_id == person_id).\
+        filter(NotificationConfig.notif_type == notif_type).\
+        first()
   if notification_config is None:
     if notif_type == 'Email_Digest':
       return True
@@ -42,3 +41,13 @@ def isNotificationEnabled(person_id, notif_type):
     return notification_config.enable_flag
 
 
+def send_email(user_email, subject, body):
+  sender = getAppEngineEmail()
+  if mail.is_email_valid(user_email) and mail.is_email_valid(sender):
+    message = mail.EmailMessage(sender=sender, subject=subject)
+
+    message.to = user_email
+    message.body = "hello"
+    message.html = body
+
+    message.send()
