f41ab008167ce3d25c140ff59d3a5c4aab34fd46
Ivan Kriznar <ivan@simplified.me>
2015-04-16 14:10:55 +0200

    Fixes unique audit title.

--- src/ggrc/assets/javascripts/mustache_helper.js
+++ src/ggrc/assets/javascripts/mustache_helper.js
@@ -1906,23 +1906,25 @@ Mustache.registerHelper("with_review_task", function (options) {
   return options.fn(options.contexts.add({review_task: undefined}));
 });
 
-Mustache.registerHelper("default_audit_title", function (instance, options) {
-  var program, default_title, current_title
-    , index = 1
-    ;
+Mustache.registerHelper('default_audit_title', function (instance, options) {
+  var index,
+      program,
+      default_title,
+      current_title,
+      title;
 
-    instance = Mustache.resolve(instance);
-    program = instance.attr("program");
+  instance = Mustache.resolve(instance);
+  program = instance.attr('program');
 
   if (!instance._transient) {
-    instance.attr("_transient", { default_title : "" });
+    instance.attr('_transient', {default_title: ''});
   }
 
   if (program == null) {
     // Mark the title to be populated when computed_program is defined,
     // returning an empty string here would disable the save button.
-    instance.attr("title", "");
-    instance.attr("_transient.default_title", instance.title);
+    instance.attr('title', '');
+    instance.attr('_transient.default_title', instance.title);
     return;
   }
   if (instance._transient.default_title !== instance.title) {
@@ -1930,19 +1932,17 @@ Mustache.registerHelper("default_audit_title", function (instance, options) {
   }
 
   program = program.reify();
-
   new RefreshQueue().enqueue(program).trigger().then(function () {
+    title = (new Date()).getFullYear() + ': ' + program.title + ' - Audit';
+    default_title = title;
 
-    default_title = new Date().getFullYear() + ": " + program.title + " - Audit";
-
-    // Count the current number of audits with default_title
-    $.map(CMS.Models['Audit'].cache, function (audit) {
-      if (audit.title && audit.title.indexOf(default_title) === 0) {
-        index += 1;
-      }
+    GGRC.Models.Search.counts_for_types(title, ['Audit']).then(function (result) {
+      // Next audit index should be bigger by one than previous, we have unique name policy
+      index = result.getCountFor('Audit') + 1;
+      title = title + ' ' + index;
+      instance.attr('title', title);
+      instance.attr('_transient.default_title', instance.title);
     });
-    instance.attr("title", new Date().getFullYear() + ": " + program.title + " - Audit " + index);
-    instance.attr("_transient.default_title", instance.title);
   });
 });
 
