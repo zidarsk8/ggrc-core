1e7ed98f257669e4cb6161476b27f24daaa0464d
Ivan Kriznar <ivan@simplified.me>
2015-04-29 18:54:24 +0200

    Return last approved task.

--- src/ggrc/assets/javascripts/mustache_helper.js
+++ src/ggrc/assets/javascripts/mustache_helper.js
@@ -1859,8 +1859,8 @@ Mustache.registerHelper("current_user_is_contact", function (instance, options) 
   }
 });
 
-Mustache.registerHelper('last_approved', function (instance, options) {
-  var loader = instance.get_binding('approval_tasks'),
+Mustache.registerHelper("last_approved", function (instance, options) {
+  var loader = instance.get_binding("approval_tasks"),
       frame = new can.Observe();
 
   frame.attr(instance, loader.list);
@@ -1876,16 +1876,13 @@ Mustache.registerHelper('last_approved', function (instance, options) {
       });
     }
     item = item ? item[0] : list[0];
-    if (item) {
-      options.contexts.add(item);
-    }
-    return options.fn(options.contexts);
+    return options.fn(item ? item : options.contexts);
   }
   function fail(error) {
     return options.inverse(options.contexts.add({error: error}));
   }
 
-  return defer_render('span', { done : finish, fail : fail }, loader.refresh_instances());
+  return defer_render("span", {done: finish, fail: fail}, loader.refresh_instances());
 });
 
 Mustache.registerHelper("with_is_reviewer", function (review_task, options) {
