a17026cd3523dd3bdce74c5d68cd84ea1ec4cf0b
Anže Pečar <ensmotko@gmail.com>
2015-05-20 12:48:31 +0200

    Fix indentation in control_controls migrattion

--- src/ggrc/migrations/versions/20150511142405_32e064034091_migrate_control_controls_to_.py
+++ src/ggrc/migrations/versions/20150511142405_32e064034091_migrate_control_controls_to_.py
@@ -1,5 +1,3 @@
-### end Alembic commands ###
-
 # Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 # Created By: anze@reciprocitylabs.com
@@ -27,14 +25,29 @@ def upgrade():
       source_type, destination_id, destination_type, context_id
   )
   SELECT modified_by_id, created_at, updated_at, control_id as source_id,
-         'Control' as source_type, implemented_control_id as destination_id, 'Control' as destination_type, context_id
+         'Control' as source_type, implemented_control_id as destination_id,
+         'Control' as destination_type, context_id
   FROM control_controls;
   """
   op.execute(sql)
-  op.drop_constraint('control_controls_ibfk_1', 'control_controls', type_='foreignkey')
-  op.drop_constraint('control_controls_ibfk_2', 'control_controls', type_='foreignkey')
+  op.drop_constraint(
+      'control_controls_ibfk_1', 'control_controls', type_='foreignkey')
+  op.drop_constraint(
+      'control_controls_ibfk_2', 'control_controls', type_='foreignkey')
 
 
 def downgrade():
-  op.create_foreign_key('control_controls_ibfk_1', 'control_controls', 'controls', ['control_id'], ['id'])
-  op.create_foreign_key('control_controls_ibfk_2', 'control_controls', 'controls', ['implemented_control_id'], ['id'])
+  op.create_foreign_key(
+      'control_controls_ibfk_1',
+      'control_controls',
+      'controls',
+      ['control_id'],
+      ['id']
+  )
+  op.create_foreign_key(
+      'control_controls_ibfk_2',
+      'control_controls',
+      'controls',
+      ['implemented_control_id'],
+      ['id']
+  )
