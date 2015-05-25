cae2133f95d230762271f33c205f6d9a87fa407f
Vladan Mitevski <vladan@mvdevelop.com>
2015-05-25 19:12:59 +0200

    Create import prototype

--- src/ggrc/static/mockups/js/import-export.js
+++ src/ggrc/static/mockups/js/import-export.js
@@ -8,8 +8,16 @@ $(document).ready(function() {
     template: "<content/>",
     helpers: {
     },
-    events: {}
+    events: {
+      ".download-template select change": function(el, ev) {
+        var $value = this.element.find('option:selected').text(),
+            $button_value = this.element.find('.import-button');
+
+        $button_value.find('span').text($value);
+      }
+    }
   });
 
   $(".import-content").html(can.view("/static/mockups/mustache/import-export.mustache",{}));
+
 });
