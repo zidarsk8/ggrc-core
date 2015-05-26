bbc0eabb65eeebea7dbd8afb9efd2adb40c3fbab
Vladan Mitevski <vladan@mvdevelop.com>
2015-05-26 15:57:29 +0200

    Style importing when everything passes

--- src/ggrc/static/mockups/js/import-export.js
+++ src/ggrc/static/mockups/js/import-export.js
@@ -20,4 +20,28 @@ $(document).ready(function() {
 
   $(".import-content").html(can.view("/static/mockups/mustache/import-export.mustache",{}));
 
+  function chooseCSV() {
+    $("#chooseFile").fadeOut(500);
+    $("#analysingFile").delay(500).fadeIn(500);
+    $(".import-analyze").delay(1500).fadeIn(500);
+    $("#analysingFile").delay(2000).fadeOut(500);
+    $("#importFile").delay(3500).fadeIn(500);
+  }
+
+  function importing() {
+    $("#importFile").fadeOut(500);
+    $("#importingFile").delay(500).fadeIn(500);
+    $(".import-progress").delay(500).fadeIn(500);
+    $(".import-analyze h4").delay(1500).fadeOut(500);
+    $("#importingFile").delay(2000).fadeOut(500);
+    $("#importedFile").delay(3500).fadeIn(500);
+    $(".import-progress").delay(2000).fadeOut(500);
+    $(".import-progress-final").delay(3500).fadeIn(500);
+    $(".import-analyze").delay(4000).fadeOut(500);
+    $(".import-final").delay(4500).fadeIn(500);
+  }
+
+  $("#chooseFile").on("click", chooseCSV);
+  $("#importFile").on("click", importing);
+
 });
