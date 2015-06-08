6a9139e065d337dd8759e93f8166323fc25f466a
Miha Zidar <zidarsk8@gmail.com>
2015-06-08 15:34:42 +0200

    Update import helper
    - Add split_array function
    - Add csv reader functions
    - Move relevant functions together

--- src/ggrc/converters/import_helper.py
+++ src/ggrc/converters/import_helper.py
@@ -5,21 +5,13 @@
 
 import csv
 import re
-from flask import current_app
+import chardet
 from StringIO import StringIO
 
 from ggrc.converters import COLUMN_HANDLERS
 from ggrc.converters import COLUMN_ORDER
 from ggrc.converters import handlers
 from ggrc.models.reflection import AttributeInfo
-
-
-def extract_relevant_data(csv_data):
-  """ Split csv data into data and metadata """
-  transpose_data = zip(*csv_data[1:])
-  data = zip(*transpose_data[1:])
-  column_definitions = data.pop(0)
-  return column_definitions, data
 
 
 def get_custom_attributes_definitions(object_class):
@@ -98,27 +90,12 @@ def get_column_order(attr_list):
   return default_attrs + custom_attrs + mapping_attrs
 
 
-def equalize_array(array):
-  """ Expand all rows of 2D array to the same lenght """
-  if len(array) == 0:
-    return array
-  max_length = max(map(len, array))
-  for row in array:
-    diff = max_length - len(row)
-    row.extend([""]*diff)
-  return array
-
-def utf8_encode_array(array):
-  """ encode 2D array to utf8 """
-  return [[val.encode("utf-8") for val in line] for line in array]
-
-
 def generate_csv_string(csv_data):
   output_buffer = StringIO()
   writer = csv.writer(output_buffer)
 
   csv_data = equalize_array(csv_data)
-  csv_data = utf8_encode_array(csv_data)
+  csv_data = utf_8_encode_array(csv_data)
 
   for row in csv_data:
     writer.writerow(row)
@@ -128,11 +105,79 @@ def generate_csv_string(csv_data):
   return body
 
 
+def extract_relevant_data(csv_data):
+  """ Split csv data into data and metadata """
+  transpose_data = zip(*csv_data[1:])
+  data = zip(*transpose_data[1:])
+  column_definitions = data.pop(0)
+  return column_definitions, data
+
+
+def equalize_array(array):
+  """ Expand all rows of 2D array to the same lenght """
+  if len(array) == 0:
+    return array
+  max_length = max(map(len, array))
+  for row in array:
+    diff = max_length - len(row)
+    row.extend([""] * diff)
+  return array
+
+
+def split_array(csv_data):
+  """ Split array by empty lines """
+  data_blocks = []
+  current_block = None
+  for line in csv_data:
+    if sum(map(len, line)) > 0:
+      if current_block is None:
+        data_blocks.append([])
+        current_block = len(data_blocks) - 1
+      data_blocks[current_block].append(line)
+    else:
+      current_block = None
+
+  return data_blocks
+
+
 def generate_2d_array(width, height, value=None):
   """ Generate 2D array of size width x height """
   return [[value for _ in range(width)] for _ in range(height)]
 
 
-
 def pretty_class_name(cls):
   return " ".join(re.findall('[A-Z][^A-Z]*', cls.__name__))
+
+
+def csv_reader(csv_data, dialect=csv.excel, **kwargs):
+  reader = csv.reader(utf_8_encoder(csv_data), dialect=dialect, **kwargs)
+  for row in reader:
+    yield [unicode(cell, 'utf-8') for cell in row]
+
+
+def read_csv_file(csv_file):
+  if isinstance(csv_file, basestring):
+    csv_file = open(csv_file, 'rbU')
+  return [row for row in csv_reader(csv_file)]
+
+
+def utf_8_encode_array(array):
+  """ encode 2D array to utf8 """
+  return [[val.encode("utf-8") for val in line] for line in array]
+
+
+def utf_8_encoder(csv_data):
+  """This function is a generator that attempts to encode the string as utf-8.
+  It is assumed that the data is likely to be encoded in ascii. If encoding
+  fails, however, the function will attempt to guess the encoding and convert
+  it to utf-8.
+  Guessing is only done when a line fails to encode as there may be characters
+  further in the stream that aren't valid ascii and encoding is performed per
+  line yielded; guessing is the fallback on a per-line basis.
+  """
+  for line in csv_data:
+    try:
+      yield line.decode('utf-8').encode('utf-8')
+    except UnicodeDecodeError:
+      encoding_guess = chardet.detect(line)['encoding']
+      yield line.decode(encoding_guess).encode('utf-8')
