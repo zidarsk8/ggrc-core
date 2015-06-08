a5f4248b4287f22e45e4eb953c2e2151707e17ef
Miha Zidar <zidarsk8@gmail.com>
2015-06-08 12:02:09 +0200

    Add Converter and RowConverter classes
    Add basic classes for handling csv data.

---
+++ src/ggrc/converters/base.py
@@ -0,0 +1,174 @@
+# Copyright (C) 2013 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+# Created By: dan@reciprocitylabs.com
+# Maintained By: dan@reciprocitylabs.com
+
+from collections import defaultdict
+from collections import OrderedDict
+
+
+from ggrc import db
+from ggrc.converters import IMPORTABLE
+from ggrc.converters.base_row import RowConverter
+from ggrc.converters.import_helper import get_object_column_definitions
+from ggrc.converters.import_helper import get_column_order
+from ggrc.converters.import_helper import extract_relevant_data
+from ggrc.converters.import_helper import generate_2d_array
+from ggrc.converters.import_helper import pretty_class_name
+
+
+class Converter(object):
+
+  """ Main converter class for dealing with csv files and data
+
+  Attributes:
+    attr_index (dict): reverse index for getting attribute name from
+      display_name
+    errors (list of str): list containing fatal import errors
+    warnings (list of str): list containing import warnings
+    ids (list of int): list containing all ids for the converted objects
+    rows (list of list of str): 2D array containg csv data
+    row_objects (list of RowConverter): list of row convertor objects with
+      data from the coresponding row in rows attribute
+    headers (dict): A dictionary containing csv headers with additional
+      information. Keys are object attributes such as "title", "slug"...
+      Example:
+        {"slug": {
+            "display_name": "Code",
+            "mandatory": True,
+            "default": "value or a callabe function that returns the value",
+            "validator": validator_function,
+            "handler": HandlerClass,
+            "description": "description or function that returns description"
+            "valid_values": "list of valid values"
+
+  """
+
+  @classmethod
+  def from_csv(cls, csv_data):
+    object_class = IMPORTABLE.get(csv_data[0][1])
+    if not object_class:
+      return "ERROR"
+
+    raw_headers, rows = extract_relevant_data(csv_data)
+
+    return Converter(object_class, rows=rows, raw_headers=raw_headers)
+
+  @classmethod
+  def from_ids(cls, object_class, ids=[]):
+    return Converter(object_class)
+
+  def __init__(self, object_class, **options):
+    self.rows = options.get('rows', [])
+    self.ids = options.get('ids', [])
+    self.object_class = object_class
+    self.errors = []
+    self.warnings = []
+    self.row_objects = []
+    self.object_headers = get_object_column_definitions(object_class)
+    raw_headers = options.get('raw_headers', [])
+    self.headers = self.clean_headers(raw_headers)
+
+  def generate_csv_header(self):
+    """ Generate 2D array with csv headre description """
+    csv_header = generate_2d_array(len(self.object_headers) + 1, 2, value="")
+    csv_header[0][0] = "Object type"
+    csv_header[1][0] = pretty_class_name(self.object_class)
+    column_order = get_column_order(self.object_headers.keys())
+    for index, code in enumerate(column_order):
+      display_name = self.object_headers[code]["display_name"]
+      if self.object_headers[code]["mandatory"]:
+        display_name += "*"
+      csv_header[0][index+1] = self.object_headers[code]["description"]
+      csv_header[1][index+1] = display_name
+    return csv_header
+
+  def generate_csv_body(self):
+    """ Generate 2D array populated with object values """
+    return []
+
+  def to_array(self):
+    csv_header = self.generate_csv_header()
+    csv_body = self.generate_csv_body()
+    two_empty_rows = [[],[]]
+    return csv_header + csv_body + two_empty_rows
+
+  def get_header_names(self):
+    """ Get all posible user column names for current object """
+    header_names = {
+        v["display_name"].lower(): k for k, v in self.object_headers.items()}
+    return header_names
+
+  def clean_headers(self, raw_headers):
+    """ Sanitize columns from csv file
+
+    Clear out all the bad column headers and remove coresponding column in the
+    rows data.
+
+    Args:
+      raw_headers (list of str): unmodified header row from csv file
+
+    Returns:
+      Ordered Dictionary containing all valid headers
+    """
+    headers = [val.strip("*") for val in raw_headers]
+    clean_headers = OrderedDict()
+    header_names = self.get_header_names()
+    for index, header in enumerate(headers):
+      if header in header_names:
+        field_name = header_names[header]
+        clean_headers[field_name] = self.object_headers[field_name]
+      elif header.startswith("map:"):
+        clean_headers[field_name] = {"mapping_header": True}
+      else:
+        self.warnings.append("Uknown column " + header)
+        self.remove_culumn(index)
+    return clean_headers
+
+  def remove_culumn(self, index):
+    """ Remove a column from all rows """
+    for row in self.rows:
+      row.pop(index)
+
+  def test_import(self):
+    for row in self.rows:
+      row_converter = RowConverter(self, self.object_class, row=row,
+                                   headers=self.headers)
+      row_converter.setup_import()
+      row_converter
+
+  def import_objects(self):
+    for row in self.rows:
+      row_converter = RowConverter(
+          self, self.object_class, row=row, headers=self.headers)
+      row_converter.setup_import()
+      row_converter.insert_object()
+
+    db.session.commit()
+
+  def import_mappings(self):
+    pass
+
+  def remove_duplicate_codes(self):
+    """ Check for duplacte code entries
+
+    Remove first columns with the same code and return an error """
+    duplicate_codes = defaultdict(list)
+    for i, row in reversed(list(enumerate(self.rows))):
+      code = row[0]
+      duplicate_codes[code].append(i)
+
+    errors = []
+    bad_rows = set()
+    for code, indexes in duplicate_codes.items():
+      if len(indexes) > 1:
+        keep, ignore = indexes[0], indexes[1:]
+        errors.append(
+            "Duplicate records: Lines {ignore} and {keep} contain the same "
+            "code. Lines {ignore} will be ignored.".format(
+                ignore=", ".join(ignore), keep=keep)
+        )
+      bad_rows.update(ignore)
+
+    result = filter(lambda i, row: i not in bad_rows, enumerate(self.rows))
+    return errors, result
---
+++ src/ggrc/converters/base_row.py
@@ -0,0 +1,91 @@
+# Copyright (C) 2013 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+# Created By: dan@reciprocitylabs.com
+# Maintained By: dan@reciprocitylabs.com
+
+
+from ggrc import db
+from ggrc.services.common import Resource
+from ggrc.services.common import get_modified_objects, update_index
+
+
+class RowConverter(object):
+
+  def __init__(self, converter, object_type, **options):
+    self.converter = converter
+    self.options = options.copy()
+    self.object_type = object_type
+    self.obj = None
+    row = options.get("row", [])
+    headers = options.get("headers", [])
+    self.attrs = self.handle_row_data(row, headers)
+
+  def setup_import(self):
+    self.setup_object()
+
+  def setup_export(self):
+    pass
+
+  def handle_row_data(self, row, headers):
+    """ Pack row data with handlers
+
+    Args:
+      row (list of str): row from csv file.
+
+    Returns:
+      Array containing handlers with values for all elements in the row.
+    """
+    if len(headers) != len(row):
+      print headers, row
+      raise Exception("Error: element count does not match header count")
+    items = {}
+    for i, (attr_name, header_dict) in enumerate(headers.items()):
+      Handler = header_dict["handler"]
+      item = Handler(self, attr_name, raw_value=row[i], **header_dict)
+      items[attr_name] = item
+    return items
+
+  def find_by_slug(self, slug):
+    return self.object_type.query.filter_by(slug=slug).first()
+
+  def get_object_by_slug(self):
+    """ Get object if the slug is in the system or return a new object """
+    if "slug" not in self.attrs:
+      return None
+    slug = self.attrs["slug"].value
+    obj = None
+    if slug:
+      obj = self.find_by_slug(slug)
+    if not obj:
+      obj = self.object_type()
+
+    return obj
+
+  def setup_object(self):
+    self.obj = self.get_object_by_slug()
+    self.reify()
+
+  def insert_object(self):
+    obj = self.obj
+    cls = obj.__class__
+    service_class = type(cls.__name__, (Resource,), {
+        '_model': cls,
+    })
+    service_class.model = cls
+    Resource.model_posted.send(
+        obj.__class__, obj=obj, src={}, service=service_class)
+    db.session.add(obj)
+    modified_objects = get_modified_objects(db.session)
+    update_index(db.session, modified_objects)
+
+  def reify(self):
+    """ Set the object values or relate object values
+
+    Set all object attributes to the value specified in attrs. If the value
+    is in some related object such as "UserRole" it should be added there and
+    handled by the handler defined in attrs.
+    """
+    if not self.obj:
+      raise Exception("Reify object is not set")
+    for item_handler in self.attrs.values():
+      item_handler.set_obj_attr()
