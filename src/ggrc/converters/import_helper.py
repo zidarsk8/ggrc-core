d62df80b432fd5505b33b3611ca397ae453396d6
Miha Zidar <zidarsk8@gmail.com>
2015-06-08 11:58:30 +0200

    Update import helper
    - Rename object_type to object_class.
    - Add helper functions for array handling
    - Add function for creating csv from arrays

--- src/ggrc/converters/import_helper.py
+++ src/ggrc/converters/import_helper.py
@@ -3,6 +3,10 @@
 # Created By: dan@reciprocitylabs.com
 # Maintained By: dan@reciprocitylabs.com
 
+import csv
+import re
+from flask import current_app
+from StringIO import StringIO
 
 from ggrc.converters import COLUMN_HANDLERS
 from ggrc.converters import COLUMN_ORDER
@@ -10,9 +14,17 @@ from ggrc.converters import handlers
 from ggrc.models.reflection import AttributeInfo
 
 
-def get_custom_attributes_definitions(target_class):
+def extract_relevant_data(csv_data):
+  """ Split csv data into data and metadata """
+  transpose_data = zip(*csv_data[1:])
+  data = zip(*transpose_data[1:])
+  column_definitions = data.pop(0)
+  return column_definitions, data
+
+
+def get_custom_attributes_definitions(object_class):
   definitions = {}
-  custom_attributes = target_class.get_custom_attribute_definitions()
+  custom_attributes = object_class.get_custom_attribute_definitions()
   for attr in custom_attributes:
     handler = COLUMN_HANDLERS.get(attr.title, handlers.ColumnHandler)
     definitions[attr.title] = {
@@ -32,26 +44,26 @@ def update_definition(definition, values_dict):
       definition[key] = value
 
 
-def get_object_column_definitions(target_class):
+def get_object_column_definitions(object_class):
   definitions = {}
   custom_attr_def = {}
-  aliases = AttributeInfo.gather_aliases(target_class)
+  aliases = AttributeInfo.gather_aliases(object_class)
 
   custom_attributes = aliases.pop("custom_attributes", None)
   if custom_attributes:
-    custom_attr_def = get_custom_attributes_definitions(target_class)
+    custom_attr_def = get_custom_attributes_definitions(object_class)
 
   filtered_aliases = [(k, v) for k, v in aliases.items() if v is not None]
 
   for key, value in filtered_aliases:
 
-    column = target_class.__table__.columns.get(key)
+    column = object_class.__table__.columns.get(key)
 
     definition = {
         "display_name": value,
         "mandatory": False if column is None else not column.nullable,
-        "default": getattr(target_class, "default_{}".format(key), None),
-        "validator": getattr(target_class, "validate_{}".format(key), None),
+        "default": getattr(object_class, "default_{}".format(key), None),
+        "validator": getattr(object_class, "validate_{}".format(key), None),
         "handler": COLUMN_HANDLERS.get(key, handlers.ColumnHandler),
         "description": "",
     }
@@ -84,3 +96,43 @@ def get_column_order(attr_list):
   custom_attrs.sort(key=lambda x: x.lower())
   mapping_attrs.sort(key=lambda x: x.lower())
   return default_attrs + custom_attrs + mapping_attrs
+
+
+def equalize_array(array):
+  """ Expand all rows of 2D array to the same lenght """
+  if len(array) == 0:
+    return array
+  max_length = max(map(len, array))
+  for row in array:
+    diff = max_length - len(row)
+    row.extend([""]*diff)
+  return array
+
+def utf8_encode_array(array):
+  """ encode 2D array to utf8 """
+  return [[val.encode("utf-8") for val in line] for line in array]
+
+
+def generate_csv_string(csv_data):
+  output_buffer = StringIO()
+  writer = csv.writer(output_buffer)
+
+  csv_data = equalize_array(csv_data)
+  csv_data = utf8_encode_array(csv_data)
+
+  for row in csv_data:
+    writer.writerow(row)
+
+  body = output_buffer.getvalue()
+  output_buffer.close()
+  return body
+
+
+def generate_2d_array(width, height, value=None):
+  """ Generate 2D array of size width x height """
+  return [[value for _ in range(width)] for _ in range(height)]
+
+
+
+def pretty_class_name(cls):
+  return " ".join(re.findall('[A-Z][^A-Z]*', cls.__name__))
