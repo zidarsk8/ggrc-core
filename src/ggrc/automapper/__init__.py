35dc6453e1aaca3574a09db0542fec96d47066ec
Andraz Bajt <andraz@bajt.me>
2015-06-09 16:15:05 +0200

    Add automapping hook

---
+++ src/ggrc/automapper/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+# Created By: andraz@reciprocitylabs.com
+# Maintained By: andraz@reciprocitylabs.com
+
+from ggrc.models.relationship import Relationship
+from ggrc.services.common import Resource
+
+def register_automapping_listeners():
+  @Resource.model_posted.connect_via(Relationship)
+  def handle_relationship_post(sender, obj=None, src=None, service=None):
+    if obj is None:
+      return
+    # TODO create actual auto mappings based on rules.py
+    pass
