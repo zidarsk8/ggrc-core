d7a4fa0bf4c69ef2d145c4797af6c8a2bebd23f3
Miha Zidar <zidarsk8@gmail.com>
2015-06-11 22:06:02 +0200

    Add multiple object import test with warnings

--- src/tests/ggrc/converters/test_multi_import_csv.py
+++ src/tests/ggrc/converters/test_multi_import_csv.py
@@ -13,6 +13,7 @@ from flask import json
 from ggrc.models import Policy
 from ggrc.models import OrgGroup
 from ggrc.models import Product
+from ggrc.converters import errors
 from tests.ggrc import TestCase
 from tests.ggrc.generator import GgrcGenerator
 
@@ -66,5 +67,39 @@ class TestCsvImport(TestCase):
     self.assertEqual(info_set, set(response_json["info"]))
     self.assertEqual(set(), set(response_json["warnings"]))
     self.assertEqual(Policy.query.count(), 4)
+    self.assertEqual(OrgGroup.query.count(), 4)
+    self.assertEqual(Product.query.count(), 5)
+
+  def test_multi_basic_policy_orggroup_product_with_warnings(self):
+    self.generate_people(["miha", "predrag", "vladan", "ivan"])
+
+    filename = "multi_basic_policy_orggroup_product_with_warnings.csv"
+    response_json = self.import_file(filename)
+
+    info_set = set([
+        "4 org groups were inserted.",
+        "3 policies were inserted.",
+        "5 products were inserted.",
+        "2 products failed.",
+        "2 policies failed.",
+    ])
+    expected_warnings = set([
+        errors.DUPLICATE_VALUE_IN_CSV.format(
+            line_list="5, 6", column_name="Title", value="dolor",
+            s="", ignore_lines="6"),
+        errors.DUPLICATE_VALUE_IN_CSV.format(
+            line_list="6, 7", column_name="Code", value="p-4",
+            s="", ignore_lines="7"),
+        errors.DUPLICATE_VALUE_IN_CSV.format(
+            line_list="21, 26", column_name="Title", value="meatloaf",
+            s="", ignore_lines="26"),
+        errors.DUPLICATE_VALUE_IN_CSV.format(
+            line_list="21, 26, 27", column_name="Code", value="pro 1",
+            s="s", ignore_lines="26, 27"),
+    ])
+
+    self.assertEqual(info_set, set(response_json["info"]))
+    self.assertEqual(expected_warnings, set(response_json["warnings"]))
+    self.assertEqual(Policy.query.count(), 3)
     self.assertEqual(OrgGroup.query.count(), 4)
     self.assertEqual(Product.query.count(), 5)
