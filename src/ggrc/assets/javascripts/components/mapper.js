13e0c4fe05485b25014dbaa26f010881d70ab63f
Ivan Kriznar <ivan@simplified.me>
2015-06-12 17:45:11 +0200

    Add filter by person functionality

--- src/ggrc/assets/javascripts/components/mapper.js
+++ src/ggrc/assets/javascripts/components/mapper.js
@@ -15,14 +15,25 @@
       object: "@",
       type: "@",
       selected: new can.List(),
+      selected_all: false,
+      owner: {},
+      term: "",
+      person: "",
       "join-object-id": "@"
     },
     events: {
       ".modalSearchButton click": function (el, ev) {
         ev.preventDefault();
+        console.log("SEARCH TERM", this.scope.attr("term"), this.scope.attr("owner", data.item));
+      },
+      "#search-by-owner autocomplete:select": function (el, ev, data) {
+        this.scope.attr("owner", data.item);
       },
       "mapper-selector onTypeChange": function (el, ev, data) {
         this.scope.attr("type", data.value);
+      },
+      "mapper-results onSelect": function (el, ev, count) {
+        this.scope.attr("selected_all", count === this.scope.attr("selected").length);
       },
       "mapper-results onSelectAll": function (el, ev, ids) {
         this.scope.attr("selected").attr(ids);
@@ -63,10 +74,16 @@
       "inserted":  function () {
         this.element.find(".results-wrap").cms_controllers_infinite_scroll();
       },
+      "{document} onSelectChange": "onSelect",
+      "{document} onSelectAll": "onSelect",
+      "onSelect": function () {
+        this.element.trigger("onSelect", this.scope.attr("entries").length);
+      },
       ".results-wrap scrollNext": "drawPage",
       "{document} onTypeChange": function (el, ev, data) {
         this.scope.attr("entries", []);
         this.scope.attr("options", []);
+        this.scope.attr("data", data);
         this.scope.attr("name", data.value);
         this.getResults();
       },
@@ -75,9 +92,16 @@
         el.openclose();
       },
       ".object-check-all change": function (el, ev) {
+        can.each(this.scope.attr("options"), function (model) {
+          model.attr("selected", true);
+        });
         this.element.trigger("onSelectAll", [_.pluck(this.scope.attr("entries"), "id")]);
       },
       ".tree-item .object-check-single change": function (el, ev) {
+        var uid = el.closest(".tree-item").data("id"),
+            isChecked = el.prop("checked", isChecked),
+            item = _.findWhere(this.scope.attr("options"), {id: uid});
+
         this.element.trigger.apply(this.element, ["onSelectChange"].concat(arguments));
       },
       "drawPage": function () {
@@ -95,23 +119,22 @@
           this.scope.attr("isLoading", false);
           this.scope.attr("page", page++);
           options.push.apply(options, can.map(models, function (model) {
-            console.log("MODEL", model);
             return {
               instance: model,
               selected_object: CMS.Models[this.scope.attr("name")],
+              selected: false,
               binding: null,
               mappings: []
             };
           }.bind(this)));
         }.bind(this));
       },
-      "getResults": function () {
-        var join_model = GGRC.Mappings.join_model_name_for(CMS.Models[model_name], model_name),
-            model_name = this.scope.attr("name"),
+      "getResults": function (term, owner) {
+        var model_name = this.scope.attr("name"),
+            join_model = GGRC.Mappings.join_model_name_for(CMS.Models[model_name], model_name),
             permission_parms = {
               __permission_type: "read"
-            },
-            term;
+            };
 
         if (join_model !== "TaskGroupObject" && model_name === "Program") {
           permission_parms = {
@@ -119,10 +142,17 @@
             __permission_model: join_model
           };
         }
+        if (model_name === "AllObject") {
+          model_name = this.scope.attr("data").models;
+        }
+        model_name = _.isString(model_name) ? [model_name] : model_name;
         GGRC.Models.Search
-          .search_for_types(term || "", [model_name], permission_parms)
+          .search_for_types(term || "", model_name, permission_parms)
           .then(function (response) {
-            var entries = response.getResultsForType(model_name);
+            var entries = _.flatten(_.map(model_name, function (name) {
+                            return response.getResultsForType(name);
+                          }));
+
             this.scope.attr("entries", entries);
             this.drawPage();
           }.bind(this));
@@ -147,8 +177,15 @@
         ev.preventDefault();
         this.scope.panels.push({
           value: "",
+          filter: new can.Map(),
           model_name: this.scope.menu[0].model_singular
         });
+      },
+      ".ui-autocomplete-input autocomplete:select": function (el, ev, data) {
+        var index = el.data("index"),
+            panel = this.scope.attr("panels")[index];
+
+        panel.attr("filter", data.item);
       },
       ".remove_filter click": function (el) {
         this.scope.panels.splice(el.data("index"), 1);
@@ -167,7 +204,8 @@
                 name: "All Objects",
                 value: "AllObject",
                 plural: "allobjects",
-                singular: "AllObject"
+                singular: "AllObject",
+                models: []
               },
               "entities": {
                 name: "People/Groups",
@@ -182,6 +220,7 @@
                 items: []
               }
             };
+
         can.each(selector_list, function (model, model_name) {
           if (!model_name || ~["workflow"].indexOf(model_name.toLowerCase())) {
             return;
@@ -190,28 +229,26 @@
               join_model = GGRC.ModalOptionDescriptor.from_join_model(model.model_name, model.option_attr, model_name),
               group = !groups[cms_model.category] ? "governance" : cms_model.category;
 
-
           groups[group]["items"].push({
+            name: cms_model.title_plural,
             value: cms_model.shortName,
-            label: cms_model.title_plural,
             singular: cms_model.shortName,
             plural: cms_model.title_plural.toLowerCase().replace(/\s+/, "_"),
-            isSelected: cms_model.shortName === attrs.type
+            isSelected: cms_model.shortName === this.type
           });
+          groups["all_objects"]["models"].push(cms_model.shortName);
         }, this);
-
         return groups;
       }
     },
     events: {
       "onTypeChange": function () {
         var selected = this.element.find(":selected"),
-            data = {
-              name: selected.attr("label"),
-              value: selected.val(),
-              singular: selected.data("singular"),
-              plural: selected.data("plural")
-            };
+            value = selected.val(),
+            groups = this.scope.groups(),
+            data = _.reduce(_.values(groups), function (memo, val) {
+                    return _.findWhere(val.items || val, {value: value}) || memo;
+                  });
         this.element.trigger("onTypeChange", data);
       },
       "select change": "onTypeChange",
