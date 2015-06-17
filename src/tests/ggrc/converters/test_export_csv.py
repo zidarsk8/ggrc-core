463f59c8a10a0a320908137d06cf11b68d3286a5
Miha Zidar <zidarsk8@gmail.com>
2015-06-17 16:14:14 +0200

    Add csv export test for simple objects

--- src/tests/ggrc/converters/test_export_csv.py
+++ src/tests/ggrc/converters/test_export_csv.py
@@ -5,11 +5,16 @@
 
 from os.path import abspath, dirname, join
 from flask.json import dumps
+from flask.json import loads
 
+from ggrc import db
+from ggrc.models import Policy
 from tests.ggrc import TestCase
+from tests.ggrc.converters.test_import_csv import TestBasicCsvImport
+from tests.ggrc.generator import GgrcGenerator
 
 THIS_ABS_PATH = abspath(dirname(__file__))
-CSV_DIR = join(THIS_ABS_PATH, 'example_csvs/')
+CSV_DIR = join(THIS_ABS_PATH, 'test_csvs/')
 
 
 class TestExportEmptyTemplate(TestCase):
@@ -32,11 +37,11 @@ class TestExportEmptyTemplate(TestCase):
 
   def test_multiple_empty_objects(self):
     data = {
-      "policy": [],
-      "regulation": [],
-      "contract": [],
-      "clause": [],
-      "org group": [],
+        "policy": [],
+        "regulation": [],
+        "contract": [],
+        "clause": [],
+        "org group": [],
     }
 
     response = self.client.post("/_service/export_csv",
@@ -48,3 +53,59 @@ class TestExportEmptyTemplate(TestCase):
     self.assertIn("Contract", response.data)
     self.assertIn("Clause", response.data)
     self.assertIn("Org Group", response.data)
+
+
+class TestExportWithObjects(TestCase):
+
+  def setUp(self):
+    TestCase.setUp(self)
+    self.generator = GgrcGenerator()
+    self.client.get("/login")
+    self.headers = {
+        'Content-Type': 'application/json',
+        "X-Requested-By": "gGRC"
+    }
+
+  def generate_people(self, people):
+    for person in people:
+      self.generator.generate_person({
+          "name": person,
+          "email": "{}@reciprocitylabs.com".format(person),
+      }, "gGRC Admin")
+
+  def import_file(self, filename, dry_run=False):
+    data = {"file": (open(join(CSV_DIR, filename)), filename)}
+    headers = {
+        "X-test-only": "true" if dry_run else "false",
+        "X-requested-by": "gGRC",
+    }
+    response = self.client.post("/_service/import_csv",
+                                data=data, headers=headers)
+    self.assertEqual(response.status_code, 200)
+    return loads(response.data)
+
+  def test_basic_policy_export(self):
+    """
+    importing:
+
+      ,"p1",some weird policy,user@example.com,Draft
+      ,p2,another weird policy,user@example.com,Draft
+      ,,Who let the dogs out,user@example.com,Draft
+
+    """
+    self.import_file("policy_basic_import.csv")
+
+    id_tuples = db.session.query(Policy.id).all()
+    policy_ids = [pid for (pid,) in id_tuples]
+
+    data = {
+        "policy": policy_ids,
+    }
+
+    response = self.client.post("/_service/export_csv",
+                                data=dumps(data), headers=self.headers)
+
+    print response.data
+    self.assertIn("some weird policy", response.data)
+    self.assertIn("p2", response.data)
+    self.assertIn("another weird policy", response.data)
