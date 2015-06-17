cccd5a44160a9d2f98d8e81ecf5a95f4f366de0e
Ivan Kriznar <ivan@simplified.me>
2015-06-17 15:40:27 +0200

    Create CSV template component

--- src/ggrc/assets/javascripts/components/csv_import.js
+++ src/ggrc/assets/javascripts/components/csv_import.js
@@ -8,6 +8,66 @@
 (function(can, $) {
 
 $(function() {
+  can.Component.extend({
+    tag: "csv-template",
+    template: "<content></content>",
+    scope: {
+      url: "/_service/export_csv",
+      selected: []
+    },
+    events: {
+      "#importSelect change": function (el, ev) {
+        var $items = el.find(":selected"),
+            selected = this.scope.attr("selected");
+
+        function isSelected(val) {
+          return ;
+        }
+        $items.each(function () {
+          var $item = $(this);
+          if (_.findWhere(selected, {value: $item.val()})) {
+            return;
+          }
+          return selected.push({
+            name: $item.attr("label"),
+            value: $item.val()
+          });
+        });
+      },
+      ".import-button click": function (el, ev) {
+        ev.preventDefault();
+        var data = _.reduce(this.scope.attr("selected"), function (memo, item) {
+                memo[item.value] = [];
+                return memo;
+              }, {});
+
+        $.ajax({
+          type: "POST",
+          dataType: "json",
+          headers: {
+            "Content-Type": "application/json",
+            "X-test-only": "true",
+            "X-requested-by": "gGRC"
+          },
+          url: this.scope.attr("url"),
+          data: data
+        });
+      },
+      ".import-list a click": function (el, ev) {
+        ev.preventDefault();
+
+        var index = el.data("index"),
+            item = this.scope.attr("selected").splice(index, 1)[0];
+
+        this.element.find("#importSelect option:selected").each(function () {
+          var $item = $(this);
+          if ($item.val() === item.value) {
+            $item.prop("selected", false);
+          }
+        });
+      }
+    }
+  });
 
   can.Component.extend({
     tag: "csv-import",
