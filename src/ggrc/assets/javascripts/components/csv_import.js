74a4ee34c7356495bdd455b11c1a0fed33d81ec5
Ivan Kriznar <ivan@simplified.me>
2015-06-17 16:11:08 +0200

    WIP: CSV importer component

--- src/ggrc/assets/javascripts/components/csv_import.js
+++ src/ggrc/assets/javascripts/components/csv_import.js
@@ -7,7 +7,6 @@
 
 (function(can, $) {
 
-$(function() {
   can.Component.extend({
     tag: "csv-template",
     template: "<content></content>",
@@ -71,27 +70,33 @@ $(function() {
 
   can.Component.extend({
     tag: "csv-import",
+    template: "<content></content>",
     scope: {
       csv_import_url: "/_service/import_csv",
-      import_state: "@",
+      // import_state: "@",
       import_info: [],
       import_errors: [],
       import_warnings: [],
-      button_states: {
-        select_file: {class: "btn-success", text: "Choose CSV file to import"},
-        analysing_file: {class: "btn-draft", text: "Analysing"},
-        import_file: {class: "btn-primary", text: "Import data"},
-        importing_file: {class: "btn-draft", text: "Importing"},
+      state: "select",
+      states: function () {
+        var states = {
+          select: {class: "btn-success", text: "Choose CSV file to import"},
+          analyse: {class: "btn-draft", text: "Analysing"},
+          import: {class: "btn-primary", text: "Import data"},
+          importing: {class: "btn-draft", text: "Importing"}
+        };
+        console.log("States", this, arguments);
+        return states[this.attr("state")] || states["select"];
       },
-      upload_file: function(file){
+      upload_file: function (file) {
         var form_data = new FormData();
 
         this.attr("import_state", "analysing_file");
-        form_data.append('file', file);
+        form_data.append("file", file);
 
         $.ajax({
           type: "POST",
-          url: this.csv_import_url, 
+          url: this.csv_import_url,
           data: form_data,
           cache: false,
           contentType: false,
@@ -100,48 +105,44 @@ $(function() {
             "X-test-only": "true",
             "X-requested-by": "gGRC"
           }
-        }).done(function(data){
+        }).done(function (data) {
           console.log(data);
-          _.forIn(data, function(value, key){
-            this.attr("import_"+key, value);
+          _.forIn(data, function (value, key) {
+            this.attr("import_" + key, value);
           }.bind(this));
           this.attr("import_state", "import_file");
-        }.bind(this)).fail(function(e){
+        }.bind(this)).fail(function (e) {
           this.attr("import_state", "select_file");
           // TODO: write error
         }.bind(this));
       }
     },
-    template: can.view(GGRC.mustache_path + "/import_export/import.mustache"),
     events: {
       "#import_btn click": function (el, ev) {
-        switch (this.scope.import_state) {
-          case "select_file":
-            $("[data-file-upload]").trigger("click");
-            break;
-          case "import_file":
-            break;
-        }
         ev.preventDefault();
+        var states = "select analyse import importing".split(" ");
+
+        // switch (this.scope.import_state) {
+        //   case "select_file":
+        //     $("[data-file-upload]").trigger("click");
+        //     break;
+        //   case "import_file":
+        //     break;
+        // }
       },
-      "[data-file-upload] change": function(el, ev){
+      "[data-file-upload] change": function(el, ev) {
         var file = el[0].files[0];
         this.scope.upload_file(file);
       }
     },
     helpers: {
-      with_button_state: function(state, options){
-        state = Mustache.resolve(state)
+      with_button_state: function (state, options) {
+        state = Mustache.resolve(state);
         return options.fn(options.contexts.add(
             options.context.button_states[state]));
       }
     }
   });
-  
-  var $import = $("#csv_import");
-  if ($import){
-    $import.html(can.view.mustache($import.html()));
-  }
-});
 
+  $("#csv_import").html(can.view(GGRC.mustache_path + "/import_export/import.mustache", {}));
 })(window.can, window.can.$);
