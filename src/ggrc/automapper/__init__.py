21e268d264f6567a639b8554738e8adf8f6a4d94
Andraz Bajt <andraz@bajt.me>
2015-06-23 13:51:12 +0200

    Add related stub caching to automapper

--- src/ggrc/automapper/__init__.py
+++ src/ggrc/automapper/__init__.py
@@ -26,19 +26,23 @@ class AutomapperGenerator(object):
     self.relationship = relationship
     self.processed = set()
     self.queue = set()
+    self.cache = dict()
 
   def related(self, obj):
-    with benchmark("Automapping related"):
-      relationships = Relationship.query.filter(or_(
-          and_(Relationship.source_type == obj.type,
-               Relationship.source_id == obj.id),
-          and_(Relationship.destination_type == obj.type,
-               Relationship.destination_id == obj.id),
-      )).all()
-      return set((stub_from_destination(r)
-                  if r.source_type == obj.type and r.source_id == obj.id
-                  else stub_from_source(r))
-                 for r in relationships)
+    if obj in self.cache:
+      return self.cache[obj]
+    relationships = Relationship.query.filter(or_(
+        and_(Relationship.source_type == obj.type,
+             Relationship.source_id == obj.id),
+        and_(Relationship.destination_type == obj.type,
+             Relationship.destination_id == obj.id),
+    )).all()
+    res = set((stub_from_destination(r)
+               if r.source_type == obj.type and r.source_id == obj.id
+               else stub_from_source(r))
+              for r in relationships)
+    self.cache[obj] = res
+    return res
 
   def relate(self, src, dst):
     if src < dst:
@@ -50,63 +54,69 @@ class AutomapperGenerator(object):
     with benchmark("Automapping generate_automappings"):
       self.queue.add(self.relate(stub_from_source(self.relationship),
                      stub_from_destination(self.relationship)))
+      count = 0
       while len(self.queue) > 0:
+        count += 1
         src, dst = entry = self.queue.pop()
         # TODO check that user can see both objects
         self._ensure_relationship(src, dst)
         self.processed.add(entry)
-        self._step(src, dst)
-        self._step(dst, src)
+        with benchmark("Automapping _step: %d" % count):
+          self._step(src, dst)
+          self._step(dst, src)
 
   def _step(self, src, dst):
-    with benchmark("Automapping _step"):
       explicit, implicit = rules[src.type, dst.type]
       self._step_explicit(src, dst, explicit)
       self._step_implicit(src, dst, implicit)
 
   def _step_explicit(self, src, dst, explicit):
-    with benchmark("Automapping _step_explicit"):
-      if len(explicit) != 0:
-        src_related = (o for o in self.related(src)
-                       if o.type in explicit and o != dst)
-        for r in src_related:
-          entry = self.relate(r, dst)
-          if entry not in self.processed:
-            self.queue.add(entry)
+    if len(explicit) != 0:
+      src_related = (o for o in self.related(src)
+                     if o.type in explicit and o != dst)
+      for r in src_related:
+        entry = self.relate(r, dst)
+        if entry not in self.processed:
+          self.queue.add(entry)
 
   def _step_implicit(self, src, dst, implicit):
-    with benchmark("Automapping _step_implicit"):
-      if not hasattr(models, src.type):
-        logging.warning('Automapping by attr: cannot find model %s' % src.type)
-        return
-      model = getattr(models, src.type)
-      instance = model.query.filter(model.id == src.id).first()
-      if instance is None:
-        logging.warning("Automapping by attr: cannot load model %s: %s" %
-                        (src.type, str(src.id)))
-        return
-      for attr in implicit:
-        if hasattr(instance, attr.name):
-          attr = getattr(instance, attr.name)
-          entry = self.relate(Stub(attr.type, attr.id), dst)
+    if not hasattr(models, src.type):
+      logging.warning('Automapping by attr: cannot find model %s' % src.type)
+      return
+    model = getattr(models, src.type)
+    instance = model.query.filter(model.id == src.id).first()
+    if instance is None:
+      logging.warning("Automapping by attr: cannot load model %s: %s" %
+                      (src.type, str(src.id)))
+      return
+    for attr in implicit:
+      if hasattr(instance, attr.name):
+        value = getattr(instance, attr.name)
+        if value is not None:
+          entry = self.relate(Stub(value.type, value.id), dst)
           if entry not in self.processed:
             self.queue.add(entry)
         else:
-          logging.warning(
-              'Automapping by attr: object %s has no attribute %s' %
-              (str(src), str(attr.name))
-          )
+          logging.warning('Automapping by attr: %s is None' % attr.name)
+      else:
+        logging.warning(
+            'Automapping by attr: object %s has no attribute %s' %
+            (str(src), str(attr.name))
+        )
 
   def _ensure_relationship(self, src, dst):
-    with benchmark("Automapping _ensure_relationship"):
-      if Relationship.find_related(src, dst) is None:
-        db.session.add(Relationship(
-            source_type=src.type,
-            source_id=src.id,
-            destination_type=dst.type,
-            destination_id=dst.id,
-            automapping_id=self.relationship.id
-        ))
+    if Relationship.find_related(src, dst) is None:
+      db.session.add(Relationship(
+          source_type=src.type,
+          source_id=src.id,
+          destination_type=dst.type,
+          destination_id=dst.id,
+          automapping_id=self.relationship.id
+      ))
+      if src in self.cache:
+        self.cache[src].add(dst)
+      if dst in self.cache:
+        self.cache[dst].add(src)
 
 
 def register_automapping_listeners():
