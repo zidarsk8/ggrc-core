b91e55fd2f5f5f17bbb3ad823a55115a97078a46
Miha Zidar <zidarsk8@gmail.com>
2015-06-24 16:11:09 +0200

    Add tests for all custom attribute types

--- src/tests/ggrc/converters/test_import_with_custom_attributes.py
+++ src/tests/ggrc/converters/test_import_with_custom_attributes.py
@@ -9,11 +9,7 @@ from os.path import abspath
 from os.path import dirname
 from os.path import join
 from flask import json
-from nose.plugins.skip import SkipTest
 
-from ggrc.models import Policy
-from ggrc.models import Relationship
-from ggrc.converters import errors
 from tests.ggrc import TestCase
 from tests.ggrc.generator import GgrcGenerator
 
@@ -24,23 +20,18 @@ CSV_DIR = join(THIS_ABS_PATH, 'test_csvs/')
 if os.environ.get("TRAVIS", False):
   random.seed(1)  # so we can reproduce the tests if needed
 
-@SkipTest
+
 class TestImportWithCustomAttributes(TestCase):
 
   def setUp(self):
     TestCase.setUp(self)
     self.generator = GgrcGenerator()
+    self.create_custom_attributes()
+    self.create_people()
     self.client.get("/login")
 
   def tearDown(self):
     pass
-
-  def generate_people(self, people):
-    for person in people:
-      self.generator.generate_person({
-        "name": person,
-        "email": "{}@reciprocitylabs.com".format(person),
-      }, "gGRC Admin")
 
   def import_file(self, filename, dry_run=False):
     data = {"file": (open(join(CSV_DIR, filename)), filename)}
@@ -53,20 +44,76 @@ class TestImportWithCustomAttributes(TestCase):
     self.assertEqual(response.status_code, 200)
     return json.loads(response.data)
 
-  def test_policy_basic_import(self):
+  def create_custom_attributes(self):
+    gen = self.generator.generate_custom_attribute
+    gen("product", attribute_type="Text", title="normal text")
+    gen("product", attribute_type="Text", title="man text", mandatory=True)
+    gen("product", attribute_type="Rich Text", title="normal RT")
+    gen("product", attribute_type="Rich Text", title="man RT", mandatory=True)
+    gen("product", attribute_type="Date", title="normal Date")
+    gen("product", attribute_type="Date", title="man Date", mandatory=True)
+    gen("product", attribute_type="Checkbox", title="normal CH")
+    gen("product", attribute_type="Checkbox", title="man CH", mandatory=True)
+    gen("product", attribute_type="Dropdown", title="normal select",
+        options="a,b,c,d")
+    gen("product", attribute_type="Dropdown", title="man select",
+        options="e,f,g", mandatory=True)
 
-    self.generator.generate_custom_attribute(
-        "policy",
-        title="Custom text")
-    self.generator.generate_custom_attribute(
-        "policy",
-        title="cuStom Mandatory Text",
-        mandatory=True)
+  def create_people(self):
+    emails = [
+        "user1@ggrc.com",
+        "miha@policy.com",
+        "someone.else@ggrc.com",
+        "another@user.com",
+    ]
+    for email in emails:
+      self.generator.generate_person({
+          "name": email.split("@")[0].title(),
+          "email": email,
+      }, "gGRC Admin")
 
-    filename = "policy_basic_custom_attributes.csv"
-
+  def test_product_with_all_custom_attributes(self):
+    filename = "product_with_all_custom_attributes.csv"
     response = self.import_file(filename)
-    from flask import json
-    print json.dumps(response, indent=True, sort_keys=True)
-    self.assertEqual(Policy.query.count(), 3)
+    expected_warnings = set([
+        "Line 6: man CH contains invalid data. The value will be ignored.",
+        "Line 8: normal select contains invalid data. The value will be"
+        " ignored.",
+        "Line 10: man select contains invalid data. The value will be"
+        " ignored.",
+        "Line 11: normal CH contains invalid data. The value will be ignored.",
+        "Line 12: man CH contains invalid data. The value will be ignored.",
+        "Line 14: normal Date contains invalid data. The value will be"
+        "  ignored.",
+        "Line 16: man Date contains invalid data. The value will be ignored.",
+        "Line 21: Owner field does not contain a valid owner. You will be"
+        "  assigned as object owner.",
+        "Line 22: Specified user 'kr@en.com' does not exist. That user will be"
+        "  ignored.",
+        "Line 22: Owner field does not contain a valid owner. You will be"
+        " assigned as object owner.",
+        "Line 26: Owner field does not contain a valid owner. You will be"
+        " assigned as object owner.",
+        "Line 27: Specified user 'user@exameuple.com' does not exist. That"
+        " user will be ignored.",
+        "Line 27: Owner field does not contain a valid owner. You will be"
+        " assigned as object owner."
+    ])
 
+    expected_errors = set([
+        "Line 6: Field man CH is required. The line will be ignored.",
+        "Line 9: Field man select is required. The line will be ignored.",
+        "Line 10: Field man select is required. The line will be ignored.",
+        "Line 12: Field man CH is required. The line will be ignored.",
+        "Line 16: Field man Date is required. The line will be ignored.",
+        "Line 18: Field man RT is required. The line will be ignored.",
+        "Line 20: Field man text is required. The line will be ignored.",
+        "Line 28: Field Title is required. The line will be ignored."
+    ])
+
+    self.assertEqual(expected_warnings, set(response["row_warnings"]))
+    self.assertEqual(expected_errors, set(response["row_errors"]))
+    self.assertEqual(18, set(response["created"]))
+    self.assertEqual(8, set(response["ignored"]))
+
+    self.assertTrue(True)
