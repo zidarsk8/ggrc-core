66fd3fc618485f000bc8f7d8fd241206a46b7b1f
Andraz Bajt <andraz@bajt.me>
2015-06-25 16:01:09 +0200

    Flash a warning when automappings exceed limit

--- src/ggrc/assets/javascripts/controllers/automapper_controller.js
+++ src/ggrc/assets/javascripts/controllers/automapper_controller.js
@@ -9,16 +9,27 @@
   var Controller = can.Control({
     "{CMS.Models.Relationship} created": function(model, ev, instance) {
       if (instance instanceof CMS.Models.Relationship) {
-        CMS.Models.Relationship.findAll({automapping_id: instance.id}).then(function(relationships) {
-          var rq = new RefreshQueue();
-          rq.enqueue(instance.source);
-          rq.enqueue(instance.destination);
-          can.each(relationships, function(relationship) {
-            rq.enqueue(relationship.source);
-            rq.enqueue(relationship.destination);
+        var limit_exceeded = instance.extras && instance.extras.automapping_limit_exceeded;
+        if (limit_exceeded) {
+          // timeout is required because a "mapping created" success flash will show up
+          // and we do not currently support multiple simultaneous flashes
+          setTimeout(function(){
+            $(document.body).trigger("ajax:flash", {
+              "warning": "Automatic mappings were not created because that would result in too many new mappings"
+            });
+          }, 2000); // 2000 is a magic number that feels nice in the UI
+        } else {
+          CMS.Models.Relationship.findAll({automapping_id: instance.id}).then(function(relationships) {
+            var rq = new RefreshQueue();
+            rq.enqueue(instance.source);
+            rq.enqueue(instance.destination);
+            can.each(relationships, function(relationship) {
+              rq.enqueue(relationship.source);
+              rq.enqueue(relationship.destination);
+            });
+            rq.trigger();
           });
-          rq.trigger();
-        });
+        }
       }
     }
   });
