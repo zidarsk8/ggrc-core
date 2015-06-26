95fa35636fc2eae07b068b7c8cb789ab76c46ab4
Andraz Bajt <andraz@bajt.me>
2015-06-26 11:17:55 +0200

    Clean up automapper controller on frontend

--- src/ggrc/assets/javascripts/controllers/automapper_controller.js
+++ src/ggrc/assets/javascripts/controllers/automapper_controller.js
@@ -6,34 +6,43 @@
 */
 
 ;(function(CMS, GGRC, can, $) {
+  function flashWarning() {
+    // timeout is required because a "mapping created" success flash will show up
+    // and we do not currently support multiple simultaneous flashes
+    setTimeout(function() {
+      $(document.body).trigger("ajax:flash", {
+        "warning": "Automatic mappings were not created because that would result in too many new mappings"
+      });
+    }, 2000); // 2000 is a magic number that feels nice in the UI
+  }
+  
+  function refreshAutomappings(instance) {
+    CMS.Models.Relationship.findAll({automapping_id: instance.id}).then(function(relationships) {
+      var rq = new RefreshQueue();
+      rq.enqueue(instance.source);
+      rq.enqueue(instance.destination);
+      can.each(relationships, function(relationship) {
+        rq.enqueue(relationship.source);
+        rq.enqueue(relationship.destination);
+      });
+      rq.trigger();
+    });
+  }
+  
   var Controller = can.Control({
     "{CMS.Models.Relationship} created": function(model, ev, instance) {
       if (instance instanceof CMS.Models.Relationship) {
         var limit_exceeded = instance.extras && instance.extras.automapping_limit_exceeded;
         if (limit_exceeded) {
-          // timeout is required because a "mapping created" success flash will show up
-          // and we do not currently support multiple simultaneous flashes
-          setTimeout(function(){
-            $(document.body).trigger("ajax:flash", {
-              "warning": "Automatic mappings were not created because that would result in too many new mappings"
-            });
-          }, 2000); // 2000 is a magic number that feels nice in the UI
+          flashWarning()
         } else {
-          CMS.Models.Relationship.findAll({automapping_id: instance.id}).then(function(relationships) {
-            var rq = new RefreshQueue();
-            rq.enqueue(instance.source);
-            rq.enqueue(instance.destination);
-            can.each(relationships, function(relationship) {
-              rq.enqueue(relationship.source);
-              rq.enqueue(relationship.destination);
-            });
-            rq.trigger();
-          });
+          refreshAutomappings(instance);
         }
       }
     }
   });
-  $(function(){
+  
+  $(function() {
     new Controller();
   });
 })(this.CMS, this.GGRC, this.can, this.can.$);
