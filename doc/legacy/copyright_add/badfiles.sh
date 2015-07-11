e6cffa049083f293d67c60d3675932bc8b618867
Urban Skudnik <urban.skudnik@gmail.com>
2015-07-11 16:45:14 +0200

    Added copyright notices for python files

--- doc/legacy/copyright_add/badfiles.sh
+++ doc/legacy/copyright_add/badfiles.sh
@@ -1,9 +1,9 @@
 # note: initially found 628 non-compliant files
 # Did the above search for file endings .scss, .feature, .html. js, .py, .haml, .mako and applied the appropriate header.
-for x in `grep -rLI --exclude-dir=tmp "Copyright (C)" . | grep -v --file=doc/copyright_add/excludes.grep | grep "\.haml$"`
+for x in `grep -rLI --exclude-dir=tmp "Copyright (C)" . | grep -v --file=doc/legacy/copyright_add/excludes.grep | grep "\.py$"`
 do
 				#echo $x
 				cp $x temporary_file
-				cat doc/copyright_add/haml_header.haml temporary_file > $x
+				cat doc/legacy/copyright_add/py_header.py temporary_file > $x
 				rm temporary_file
 done
--- src/ggrc_basic_permissions/migrations/versions/20131122001039_24c56d737c18_add_roles_and_role_i.py
+++ src/ggrc_basic_permissions/migrations/versions/20131122001039_24c56d737c18_add_roles_and_role_i.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+# Created By: anze@reciprocitylabs.com
+# Maintained By: anze@reciprocitylabs.com
+
 """Add roles and role implications for gdrive connectors
 
 Revision ID: 24c56d737c18
