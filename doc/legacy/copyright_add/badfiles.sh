d5048451a0f024c16599ba24254e6e67e925d71d
Urban Skudnik <urban.skudnik@gmail.com>
2015-07-11 16:59:09 +0200

    Part mustache

--- doc/legacy/copyright_add/badfiles.sh
+++ doc/legacy/copyright_add/badfiles.sh
@@ -1,9 +1,9 @@
 # note: initially found 628 non-compliant files
 # Did the above search for file endings .scss, .feature, .html. js, .py, .haml, .mako and applied the appropriate header.
-for x in `grep -rLI --exclude-dir=tmp "Copyright (C)" . | grep -v --file=doc/legacy/copyright_add/excludes.grep | grep "\.py$"`
+for x in `grep -rLI --exclude-dir=tmp "Copyright (C)" . | grep -v --file=doc/legacy/copyright_add/excludes.grep | grep "\.html$"`
 do
 				#echo $x
 				cp $x temporary_file
-				cat doc/legacy/copyright_add/py_header.py temporary_file > $x
+				cat doc/legacy/copyright_add/html_header.html temporary_file > $x
 				rm temporary_file
 done
