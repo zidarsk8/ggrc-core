7bba217bb1545f0ded472fdf47536b6a373b783c
Urban Skudnik <urban.skudnik@gmail.com>
2015-07-13 17:13:07 +0200

    Remaining mustache files

--- doc/legacy/copyright_add/badfiles.sh
+++ doc/legacy/copyright_add/badfiles.sh
@@ -1,9 +1,9 @@
 # note: initially found 628 non-compliant files
 # Did the above search for file endings .scss, .feature, .html. js, .py, .haml, .mako and applied the appropriate header.
-for x in `grep -rLI --exclude-dir=tmp "Copyright (C)" . | grep -v --file=doc/legacy/copyright_add/excludes.grep | grep "\.html$"`
+for x in `grep -rLI --exclude-dir=tmp "Copyright (C)" . | grep -v --file=doc/legacy/copyright_add/excludes.grep | grep "\.mustache$"`
 do
 				#echo $x
 				cp $x temporary_file
-				cat doc/legacy/copyright_add/html_header.html temporary_file > $x
+				cat doc/legacy/copyright_add/mustache_header.mustache temporary_file > $x
 				rm temporary_file
 done
