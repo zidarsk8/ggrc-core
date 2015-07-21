0ce470183ec76364814baa9bd0d2c34ab96aa038
Anže Pečar <ensmotko@gmail.com>
2015-07-21 16:12:39 +0200

    Delete legacy mockup javascript files

--- src/ggrc/views/mockups.py
+++ src/ggrc/views/mockups.py
@@ -9,42 +9,6 @@ from flask import render_template
 
 
 def init_mockup_views():
-
-  @app.route("/mockups/audit-3.0/")
-  @login_required
-  def audit_3_0():
-    """Audit 3.0 mockup
-    """
-    return render_template("/mockups/audit-3.0/info.html")
-
-  @app.route("/mockups/audit-3.0/control-assessment.html")
-  @login_required
-  def audit_3_0_ca():
-    """Audit 3.0 CA mockup
-    """
-    return render_template("/mockups/audit-3.0/control-assessment.html")
-
-  @app.route("/mockups/import/")
-  @login_required
-  def import_redesign():
-    """Import prototype
-    """
-    return render_template("/mockups/import/index.html")
-
-  @app.route("/mockups/export/")
-  @login_required
-  def export_redesign():
-    """Export prototype
-    """
-    return render_template("/mockups/export/index.html")
-
-  @app.route("/mockups/export-object/")
-  @login_required
-  def export_object_redesign():
-    """Export object prototype
-    """
-    return render_template("/mockups/export/object.html")
-
   @app.route("/mockups/risk-assessment")
   @login_required
   def risk_assessment_redesign():
