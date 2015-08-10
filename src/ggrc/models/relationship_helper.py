ee450dca143e10b48f6e5cd1e535b9d8cceae5e0
Miha Zidar <zidarsk8@gmail.com>
2015-08-10 15:52:24 +0200

    Update relationship helper
    Prepare relationship helper class to allow special mappings and mappings
    from other modules.

--- src/ggrc/models/relationship_helper.py
+++ src/ggrc/models/relationship_helper.py
@@ -3,7 +3,6 @@
 # Created By: miha@reciprocitylabs.com
 # Maintained By: miha@reciprocitylabs.com
 
-
 from sqlalchemy import and_
 
 from ggrc import db
@@ -13,12 +12,33 @@ from ggrc.models.relationship import Relationship
 class RelationshipHelper(object):
 
   @classmethod
+  def get_special_mappings(cls, object_type, related_type, related_ids=[]):
+    return []
+
+  @classmethod
+  def get_extension_mappings(cls, object_type, related_type, related_ids=[]):
+    return []
+
+  @classmethod
+  def _array_union(cls, queries):
+    """ Union of all valid queries in array """
+    clean_queries = [q for q in queries if q is not None]
+    if len(clean_queries) == 0:
+      return None
+
+    query = clean_queries.pop()
+    for q in clean_queries:
+      query = query.union(q)
+    return query
+
+  @classmethod
   def get_ids_related_to(cls, object_type, related_type, related_ids=[]):
     """ get ids of objects
 
     Get a list of all ids for object with object_type, that are related to any
     of the objects with type related_type and id in related_ids
     """
+
     if isinstance(related_ids, (int, long)):
       related_ids = [related_ids]
 
@@ -36,4 +56,11 @@ class RelationshipHelper(object):
             Relationship.destination_id.in_(related_ids),
         )
     )
-    return source_ids.union(destination_ids)
+
+    queries = [destination_ids, source_ids]
+    queries.extend(cls.get_extension_mappings(
+        object_type, related_type, related_ids))
+    queries.extend(cls.get_special_mappings(
+        object_type, related_type, related_ids))
+
+    return cls._array_union(queries)
