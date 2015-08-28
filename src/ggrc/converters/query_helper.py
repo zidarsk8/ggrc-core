10d8bc578b656febc22902b0464368af340011e4
Andraz Bajt <andraz@bajt.me>
2015-08-28 11:53:31 +0200

    Fix free text search in export
    Handle fields with special filters and ignore fields that are not in the
    attr_name_map.

--- src/ggrc/converters/query_helper.py
+++ src/ggrc/converters/query_helper.py
@@ -120,6 +120,7 @@ class QueryHelper(object):
     def build_expression(exp):
       if "op" not in exp:
         return None
+
       def relevant():
         query = (self.query[exp["ids"][0]]
                  if exp["object_name"] == "__previous__" else exp)
@@ -131,14 +132,8 @@ class QueryHelper(object):
             )
         )
 
-      text_search = lambda: or_(
-          *(getattr(object_class, field).ilike("%{}%".format(exp["text"]))
-            for field in object_query.get("fields", [])
-            if hasattr(object_class, field))
-      )
-
-      def with_left(p):
-        key = exp["left"].lower()
+      def with_key(key, p):
+        key = key.lower()
         key, filter_by = self.attr_name_map[object_class].get(key, (key, None))
         if hasattr(filter_by, "__call__"):
           return filter_by(p)
@@ -150,8 +145,20 @@ class QueryHelper(object):
                                     .format(object_class.__name__, key))
           return p(attr)
 
+      with_left = lambda p: with_key(exp["left"], p)
+
       lift_bin = lambda f: f(build_expression(exp["left"]),
                              build_expression(exp["right"]))
+
+      def text_search():
+        existing_fields = self.attr_name_map[object_class]
+        text = "%{}%".format(exp["text"])
+        p = lambda f: f.ilike(text)
+        return or_(*(
+            with_key(field, p)
+            for field in object_query.get("fields", [])
+            if field in existing_fields
+        ))
 
       ops = {
         "AND": lambda: lift_bin(and_),
