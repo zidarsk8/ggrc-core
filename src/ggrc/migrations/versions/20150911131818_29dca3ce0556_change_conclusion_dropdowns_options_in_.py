a022dd383c2c6169febb152a3ed5008fe93520a5
JostN <jost.novljan@gmail.com>
2015-09-11 15:24:36 +0200

    Change conclusion dropdown values

---
+++ src/ggrc/migrations/versions/20150911131818_29dca3ce0556_change_conclusion_dropdowns_options_in_.py
@@ -0,0 +1,51 @@
+
+"""Change conclusion dropdowns options in control assessment
+
+Revision ID: 29dca3ce0556
+Revises: 2d8a46b1e4a4
+Create Date: 2015-09-11 13:18:18.269109
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '29dca3ce0556'
+down_revision = '2d8a46b1e4a4'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+def upgrade():
+    sql = """
+    UPDATE control_assessments
+    SET design = 'Ineffective'
+    WHERE
+        design = 'Material weakness' OR
+        design = 'Significant deficiency'
+    """
+    op.execute(sql)
+
+    sql = """
+    UPDATE control_assessments
+    SET operationally = 'Ineffective'
+    WHERE
+        operationally = 'Material weakness' OR
+        operationally = 'Significant deficiency'
+    """
+    op.execute(sql)
+
+
+def downgrade():
+    sql = """
+    UPDATE control_assessments
+    SET design = 'Significant deficiency'
+    WHERE design = 'Ineffective'
+    """
+    op.execute(sql)
+
+    sql = """
+    UPDATE control_assessments
+    SET operationally = 'Significant deficiency'
+    WHERE operationally = 'Ineffective'
+    """
+    op.execute(sql)
--- src/ggrc/models/control_assessment.py
+++ src/ggrc/models/control_assessment.py
@@ -36,8 +36,8 @@ class ControlAssessment(HasObjectState, TestPlanned, CustomAttributable,
   audit = {}  # we add this for the sake of client side error checking
 
   VALID_CONCLUSIONS = frozenset([
-      "Effective", "Material weakness", "Needs improvement",
-      "Significant deficiency", "Not Applicable"
+      "Effective", "Ineffective", "Needs improvement",
+      "Not Applicable"
   ])
 
   # REST properties
