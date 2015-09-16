ee659cdd8a8d882efc0ba235352b54fb20372ea4
Andraz Bajt <andraz@bajt.me>
2015-09-16 15:02:40 +0200

    Integrate workflows and risks
    Sorry, this is ungly - workflows extension needs to be aware of the risks
    extension because imports mutate state of the rest of the application. This can
    go away when we have proper stateless extension imports.

--- src/ggrc_workflows/assets/javascripts/apps/workflows.js
+++ src/ggrc_workflows/assets/javascripts/apps/workflows.js
@@ -11,7 +11,8 @@
         'Program Regulation Policy Standard Contract Clause Section'.split(' '),
         'Control Objective'.split(' '),
         'OrgGroup Vendor'.split(' '),
-        'System Process DataAsset Product Project Facility Market Issue'.split(' ')
+        'System Process DataAsset Product Project Facility Market Issue'.split(' '),
+        'Risk ThreatActor'.split(' ')
       ),
       _task_sort_function = function(a, b) {
         var date_a = +new Date(a.end_date),
@@ -248,7 +249,11 @@
 
     // Insert `workflows` mappings to all business object types
     can.each(_workflow_object_types, function (type) {
-      CMS.Models[type].attributes.cycle_objects = 'CMS.Models.CycleTaskGroupObject.stubs';
+      var model = CMS.Models[type];
+      if (model === undefined || model === null) {
+        return;
+      }
+      model.attributes.cycle_objects = 'CMS.Models.CycleTaskGroupObject.stubs';
       mappings[type] = {
         task_groups: new GGRC.ListLoaders.ProxyListLoader('TaskGroupObject', 'object', 'task_group', 'task_group_objects', null),
         cycle_objects: Direct('CycleTaskGroupObject', 'object', 'cycle_task_group_objects'),
@@ -606,7 +611,11 @@
     }
   });
   can.each(_workflow_object_types, function(model_name) {
-    draft_on_update_mixin.add_to(CMS.Models[model_name]);
+    var model = CMS.Models[model_name];
+    if (model === undefined || model === null) {
+      return;
+    }
+    draft_on_update_mixin.add_to(model);
   });
 
 })(this.can.$, this.CMS, this.GGRC);
