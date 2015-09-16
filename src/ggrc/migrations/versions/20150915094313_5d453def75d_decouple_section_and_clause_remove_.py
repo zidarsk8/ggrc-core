ce8b0609ffef46eb1c24be7d5ea33d080d0d5030
JostN <jost.novljan@gmail.com>
2015-09-16 10:41:59 +0200

    Migrations for new Clause table

--- src/ggrc/migrations/versions/20150915094313_5d453def75d_decouple_section_and_clause_remove_.py
+++ src/ggrc/migrations/versions/20150915094313_5d453def75d_decouple_section_and_clause_remove_.py
@@ -44,11 +44,83 @@ def upgrade():
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('slug', name='uq_clauses')
     )
+    
+
+    # move clauses to a stand alone table
+    sql = """
+    INSERT INTO clauses (
+      type, na, notes, os_state, parent_id, description,
+      url, reference_url, secondary_contact_id, contact_id,
+      title, slug, created_at, modified_by_id, updated_at, context_id,
+      status, end_date, start_date
+    )
+    SELECT type, na, notes, os_state, parent_id, description,
+         url, reference_url, secondary_contact_id, contact_id,
+         title, slug, created_at, modified_by_id, updated_at, context_id,
+         status, end_date, start_date
+    FROM sections
+    WHERE type = 'Clause'
+    """
+    op.execute(sql)
+
+    # remove clauses from the sections table
+    sql = """
+    DELETE
+    FROM sections
+    WHERE type = 'Clause'
+    """
+    op.execute(sql)
+
+    sql = """
+    REPLACE INTO relationships (
+      modified_by_id, created_at, updated_at, source_id,
+      source_type, destination_id, destination_type, context_id
+    )
+    SELECT dc.modified_by_id, dc.created_at, dc.updated_at,
+         dc.id as source_id, 'Section' as source_type,
+         dc.directive_id as destination_id,
+         d.meta_kind as destination_type,
+         dc.context_id
+    FROM sections as dc JOIN directives as d ON dc.directive_id = d.id;
+    """
+    op.execute(sql)
+
+    # remove foreign key constraint
+    op.drop_constraint(
+        'sections_ibfk_1',
+        'sections',
+        type_='foreignkey')
 
     op.drop_column('sections', 'directive_id')
 
 
 def downgrade():
-    op.add_column('sections', sa.Column('directive_id', mysql.INTEGER(display_width=11), autoincrement=False, nullable=True))
+    # add the foreign key back. 
+    op.add_column(
+        'sections',
+        sa.Column(
+            'directive_id',
+            mysql.INTEGER(display_width=11),
+            sa.ForeignKey('directives.id', name='sections_ibfk_1'),
+            autoincrement=False,
+            nullable=True
+        )
+    )
+
+    # TODO: WILL THIS BREAK ANY MAPPINGS? ID-s ARE DIFFERENT ON DOWNGRADE!
+    sql = """
+    INSERT INTO sections (
+      type, na, notes, os_state, parent_id, description,
+      url, reference_url, secondary_contact_id, contact_id,
+      title, slug, created_at, modified_by_id, updated_at, context_id,
+      status, end_date, start_date
+    )
+    SELECT type, na, notes, os_state, parent_id, description,
+         url, reference_url, secondary_contact_id, contact_id,
+         title, slug, created_at, modified_by_id, updated_at, context_id,
+         status, end_date, start_date
+    FROM clauses
+    """
+    op.execute(sql)
 
     op.drop_table('clauses')
