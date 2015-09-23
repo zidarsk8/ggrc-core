9be34531f680b53933b25fdda5e1955ff4c04aa3
Ivan Kriznar <ivan@simplified.me>
2015-09-23 11:32:39 +0200

    Restore request data lost in rebase

--- src/ggrc/assets/mockups/request/main.js
+++ src/ggrc/assets/mockups/request/main.js
@@ -14,19 +14,34 @@
   }
 
   // Setup the object page:
-  var mockup = new CMS.Controllers.MockupHelper($("body"), {
+  var mockup = new CMS.Controllers.MockupHelper($('body'), {
     // Object:
     object: {
       icon: "grciconlarge-audit",
-      title: "Request",
+      title: "My new request",
     },
     // Views:
     views: [{
         // Example on how to use an existing template:
         title: "Info",
         icon: "grcicon-info",
-        template: "/request/info.mustache"
+        template: "/request/info.mustache",
+        info_title: "My new request",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae ante dapibus lacus dictum vestibulum. Nullam finibus semper convallis. Ut libero mauris, viverra nec augue eget, congue viverra felis. Aenean ut arcu vel tortor rhoncus dictum id vel urna. Sed a enim laoreet diam lacinia euismod.",
+        state: "In Progress",
+        people_assignee: "Martin L.K.",
+        people_requester: "Josh Smith",
+        people_verifier: "Prasanna V.",
+        created_on: "12/03/14",
+        due_on: "12/31/15",
+        type_a: "assignee",
+        type_r: "requester",
+        type_v: "verifier"
+      }, {
+        title: "Audits",
+        icon: "grcicon-audit-color",
+        template: "/base_objects/tree.mustache"
       }
-    ],
+    ]
   });
 })(this.can, this.can.$);
