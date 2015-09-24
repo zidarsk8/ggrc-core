e177fff47503940d0c819942bd888343514960d9
Ivan Kriznar <ivan@simplified.me>
2015-09-24 18:04:07 +0200

    Use routing for mockups

--- src/ggrc/assets/mockups/mockuphelper_controller.js
+++ src/ggrc/assets/mockups/mockuphelper_controller.js
@@ -7,86 +7,60 @@
 
 
 (function (can, $) {
+  can.route(":tab", {tab: "Info"});
+  can.route(":tab/:id");
+
+  // Activate router
+  $(document).ready(can.route.ready);
 
   can.Control("CMS.Controllers.MockupHelper", {
     defaults: {
       title_view: GGRC.mustache_path + "/title.mustache",
-      object_views: {},
+      object_views: {}
     }
   }, {
     init: function (el, opts) {
-      if (opts.object) {
-        this.initObject(opts.object, el);
-      }
-      if (opts.views) {
-        this.initViews(opts.views, el);
-        this.options.object_views[opts.views[0].title].show();
-      }
+      var options = {
+        views: this.options.views
+      };
+      new CMS.Controllers.MockupNav(this.element.find(".internav"), options);
+      new CMS.Controllers.MockupView(this.element.find(".inner-content"), options);
+      this.element.find(".title-content").html(can.view(this.options.title_view, opts.object));
     },
-    initObject: function (obj, el) {
-      var content = el.find(".title-content");
-      content.html(can.view(this.options.title_view, obj));
-    },
-    initViews: function (views, el) {
-      var inner_content = $(".inner-content");
-      _.forEach(views, function (view) {
-        var placeholder = $("<div />"),
-            viewInst = new CMS.Controllers.MockupView(placeholder, view);
-        inner_content.append(placeholder);
-        this.options.object_views[view.title] = viewInst;
-      }.bind(this));
-    },
-    '.internav > li click': function (el, ev) {
-      _.forEach(this.options.object_views, function (view) {
-        view.element.hide();
+    "{can.route} tab": function (router, ev, tab) {
+      _.each(this.options.views, function (view) {
+        view.active = view.title === tab;
       });
-      var href = el.find("a").attr("href").replace("#", "");
-      el.siblings().removeClass("active");
-      el.addClass("active");
-      this.options.object_views[href].element.show();
+    }
+  });
+
+  can.Control("CMS.Controllers.MockupNav", {
+    defaults: {
+      view: "/static/mockups/base_templates/nav_item.mustache"
+    }
+  }, {
+    "{can.route} tab": function () {
+      this.element.html(can.view(this.options.view, this.options));
     }
   });
 
   can.Control("CMS.Controllers.MockupView", {
     defaults: {
-      title_view: GGRC.mustache_path + "/title.mustache",
+      title_view: GGRC.mustache_path + "/title.mustache"
     }
   }, {
-    init: function (el, opts) {
-      this.initTab(opts);
-      this.initContent(el, opts);
-    },
-    initTab: function (view) {
-      var internav = $(".internav"),
-          element = $("<li><a href='#" + view.title + "'></a></li>");
-      element.find("a").html(can.view(this.options.title_view, view));
-      internav.append(element);
-      this.tab = element;
-    },
-    initContent: function (el, view) {
-      this.element.html(can.view(GGRC.mustache_path + view.template, {instance: new can.Model.Cacheable(view), scope: view.scope}));
-      _.forOwn(view.events, function(event_handler, event_name) {
-        event_list = event_name.split(" ");
-        event_name = event_list.pop();
-        this.element.find(event_list.join(" ")).on(event_name, event_handler.bind(view));
-      }.bind(this));
-    },
-    show: function () {
-      this.element.siblings().hide();
-      this.element.show();
-      this.tab.siblings().removeClass("active");
-      this.tab.addClass("active");
+    "{can.route} tab": function () {
+      var view = _.findWhere(this.options.views, {active: true});
+      this.element.html(can.view(GGRC.mustache_path + view.template, {
+        instance: new can.Model.Cacheable(view),
+        scope: view.scope
+      }));
     }
   });
 
   can.Control("CMS.Controllers.MockupTreeView", {
-    defaults: {
-      title_view: GGRC.mustache_path + "/title.mustache",
-    }
   }, {
     init: function (el, opts) {
-      this.initTab(opts);
-      this.initContent(el, opts);
     }
   });
 })(this.can, this.can.$);
