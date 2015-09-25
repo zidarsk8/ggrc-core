a9d4f87e00ff657d094ef400b6dbce5cfa7f043b
Ivan Kriznar <ivan@simplified.me>
2015-09-25 02:02:25 +0200

    Fix selected tree item and info pin item

--- src/ggrc/assets/mockups/mockuphelper_controller.js
+++ src/ggrc/assets/mockups/mockuphelper_controller.js
@@ -55,7 +55,6 @@
   }, {
     "{can.route} tab": function () {
       var view = _.findWhere(this.options.views, {active: true});
-      console.log("VIEW", view, this.options.views);
       this.element.html(can.view(GGRC.mustache_path + view.template));
       if (view.children) {
         new CMS.Controllers.MockupTreeView(this.element.find(".base-tree-view"), view);
@@ -75,6 +74,9 @@
       }, this);
     },
     "{can.route} item": function (router, ev, item) {
+      if (!item || !item.length) {
+        return;
+      }
       item = item.split("-");
       var view = _.findWhere(this.options.children, {
             type: item[0],
@@ -120,12 +122,54 @@
 
   can.Control("CMS.Controllers.MockupInfoPanel", {
     defaults: {
-      view: "/static/mockups/base_templates/info_panel.mustache"
+      view: "/static/mockups/base_templates/info_panel.mustache",
+      slide: 240
     }
   }, {
-    "{can.route} active": function () {
-      // TODO: We need to render via item-id, not title ^_^
-      this.element.html(can.view(this.options.view, this.options));
+    ".pin-action a click": function (el, ev) {
+      var size = el.data("size"),
+          height = Math.round($(window).height() / 3),
+          heights = {
+            deselect: 0,
+            min: height,
+            normal: height*2,
+            max: height*3
+          };
+
+      el.find("i").css("opacity", 1).closest("li").siblings().find("i").css("opacity", 0.25);
+      this.element
+        .show()
+        .animate({
+          height: heights[size]
+        }, {
+          duration: this.options.slide,
+          complete: function () {
+            if (size === "deselect") {
+              this.element.hide();
+              this.active.attr("active", false);
+              can.route.removeAttr("item");
+            }
+          }.bind(this)
+        });
+    },
+    "{can.route} item": function (router, ev, item) {
+      // TODO: Simplify this
+      if (!item || !item.length) {
+        return;
+      }
+      function recursiveFind(needle) {
+        if (needle.type === item[0] && needle.id === item[1]) {
+          return needle;
+        }
+        if (needle.children && needle.children.length) {
+          return _.map(needle.children, recursiveFind);
+        }
+      }
+      item = item.split("-");
+      var view = _.compact(_.flattenDeep(_.map(this.options.views, recursiveFind)))[0];
+      this.active = view;
+      this.element.html(can.view(this.options.view, view));
+      this.element.show();
     }
   });
 })(this.can, this.can.$);
