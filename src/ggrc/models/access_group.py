1b7ed84231f58f3e1c48bced37282eeb996fd5cc
Urban Skudnik <urban.skudnik@gmail.com>
2015-10-02 14:07:24 +0200

    Use abolute paths and fix indent

--- src/ggrc/models/access_group.py
+++ src/ggrc/models/access_group.py
@@ -4,17 +4,17 @@
 # Maintained By: urban@reciprocitylabs.com
 
 from ggrc import db
-from .mixins import BusinessObject, Timeboxed, CustomAttributable
-from .object_document import Documentable
-from .object_owner import Ownable
-from .object_person import Personable
-from .relationship import Relatable
-from .track_object_state import HasObjectState, track_state_for_class
+from ggrc.models.mixins import BusinessObject, Timeboxed, CustomAttributable
+from ggrc.models.object_document import Documentable
+from ggrc.models.object_owner import Ownable
+from ggrc.models.object_person import Personable
+from ggrc.models.relationship import Relatable
+from ggrc.models.track_object_state import HasObjectState, track_state_for_class
 
 
 class AccessGroup(HasObjectState,
-                CustomAttributable, Personable, Documentable, Relatable,
-                Timeboxed, Ownable, BusinessObject, db.Model):
+                  CustomAttributable, Personable, Documentable, Relatable,
+                  Timeboxed, Ownable, BusinessObject, db.Model):
     __tablename__ = 'access_groups'
 
     _aliases = {"url": "Access Group URL"}
