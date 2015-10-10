01f27d1f6ab7ea54563c804391f23df2a825f671
Vladan Mitevski <vladan@mvdevelop.com>
2015-10-10 16:50:40 +0200

    Populate assessment info panel with data

--- src/ggrc/assets/mockups/request/pages/assessments.js
+++ src/ggrc/assets/mockups/request/pages/assessments.js
@@ -1,4 +1,4 @@
-(function (GGRC) {
+(function (GGRC, Generator) {
   GGRC.Bootstrap.Mockups = GGRC.Bootstrap.Mockups || {};
   GGRC.Bootstrap.Mockups.Request = GGRC.Bootstrap.Mockups.Request || {};
 
@@ -6,77 +6,26 @@
     title: "Assessments",
     icon: "grcicon-assessment-color",
     template: "/request/audit.mustache",
+    infopane_template: "/static/mockups/base_templates/info_panel_assessments.mustache",
     children: [{
-      title: "Very first assessment for this Request",
-      info_title: "Very first assessment for this Request",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae ante dapibus lacus dictum vestibulum. Nullam finibus semper convallis. Ut libero mauris, viverra nec augue eget, congue viverra felis. Aenean ut arcu vel tortor rhoncus dictum id vel urna. Sed a enim laoreet diam lacinia euismod.",
-      state: "Draft",
-      state_color: "draft",
+      title: "Very first assessment",
+      info_title: "Very first assessment",
+      description: Generator.paragraph(7),
+      state: "In Progress",
+      state_color: "inprogress",
       type: "assessment",
-      status: "Draft",
+      status: "In Progress",
       id: "2",
-      files: [{
-        icon: "zip",
-        date: "09/24/2015",
-        name: "Compressed_files.zip",
-        url: "https://github.com/Compressed_files.zip"
-      }, {
-        icon: "url",
-        date: "09/23/2015",
-        name: "https://github.com/",
-        url: "https://github.com/"
-      }, {
-        icon: "",
-        date: "09/22/2015",
-        name: "simple_file.reg",
-        url: "http://google.com/"
-      }, {
-        icon: "text",
-        date: "09/21/2015",
-        name: "canjs-observe-bug.txt",
-        url: "http://google.com/"
-      }, {
-        icon: "image",
-        date: "09/19/2015",
-        name: "Image_of_nature.png",
-        url: "http://google.com/"
-      }, {
-        icon: "xls",
-        date: "04/18/2015",
-        name: "Simple_Excel_document.xls",
-        url: "http://google.com/"
-      }, {
-        icon: "doc",
-        date: "04/15/2014",
-        name: "Simple_Word_document.doc",
-        url: "http://google.com/"
-      }, {
-        icon: "pdf",
-        date: "04/05/2010",
-        name: "MTV_001_SIGNED_LoginAccess_List.pdf",
-        url: "http://google.com/"
-      }],
-      people_assignee: [{
-        name: "Gisele Bundchen"
-      }, {
-        name: "Cara Delevingne"
-      }, {
-        name: "Adriana Lima"
-      }],
-      people_requester: [{
-        name: "Doutzen Kroes"
-      }, {
-        name: "Natalia Vodianova"
-      }, {
-        name: "Miranda Kerr"
-      }],
-      people_verifier: [{
-        name: "Prasanna V."
-      }],
+      files: Generator.get("file", 8, {sort: "date"}),
+      comments: Generator.get("comment", 3, {sort: "date"}),
+      urls: Generator.get("url", 3),
+      people_assignee: Generator.get("user", 5),
+      people_assessor: Generator.get("user"),
+      people_verifier: Generator.get("user", 3),
       created_on: "12/03/14",
       due_on: "12/31/15",
       type_a: "assignee",
-      type_r: "requester",
+      type_s: "assessor",
       type_v: "verifier",
       mapped_objects: [{
         icon: "objective",
@@ -94,30 +43,37 @@
         state: "In Progress",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
       }],
-      comments: [{
-        type: "assignee",
-        author: "Albert Chan",
-        date: "09/20/2015 07:31:02am PDT",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non mauris euismod, suscipit velit eu, imperdiet ex. Curabitur nisl diam, blandit in luctus ac, eleifend quis libero. Morbi in lobortis risus. Vestibulum congue dictum finibus.",
-        attachments: [{
-          url: "http://google.com/",
-          title: "canjs-observe-bug.txt"
-        }]
+      mapped_objects_requests: [{
+        icon: "request",
+        title: "My new request",
+        state: "In Progress",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
       }, {
-        type: "verifier",
-        author: "Prasanna V.",
-        date: "08/30/2015 05:31:02am PDT",
-        text: "Curabitur nisl diam, blandit in luctus ac, eleifend quis libero. Morbi in lobortis risus. Vestibulum congue dictum finibus."
+        icon: "request",
+        title: "Simple Request for Programs",
+        state: "Draft",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
       }, {
-        type: "requester",
-        author: "Jost Novljan",
-        date: "07/21/2015 01:31:02pm PDT",
-        text: "See usecase here: https://docs.google.com/document/d/1kU6DgyJBOxbPX5eDhphq97dcMhg-b-LpzTJT27XlHYk/edit#heading=h.9wrhlxa3ye2d."
+        icon: "request",
+        title: "Request made for Sections inspection",
+        state: "Draft",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
+      }],
+      mapped_objects_issues: [{
+        icon: "issue",
+        title: "090.7068 issue 1",
+        state: "Draft",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
       }, {
-        type: "assignee",
-        author: "Albert Chan",
-        date: "07/18/2015 03:16:02pm PDT",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non mauris euismod, suscipit velit eu, imperdiet ex. Curabitur nisl diam, blandit in luctus ac, eleifend quis libero. Morbi in lobortis risus. Vestibulum congue dictum finibus."
+        icon: "issue",
+        title: "Access to the Private Network with expired Key v0906984",
+        state: "In Progress",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
+      }, {
+        icon: "issue",
+        title: "a object under issue",
+        state: "In Progress",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
       }],
       logs: [{
         type: "requester",
@@ -187,35 +143,11 @@
         field: "Evidence",
         original_value: [{
           text: "",
-          file_list: [{
-            icon: "pdf",
-            file_name: "MTV_001_SIGNED_LoginAccess_List.pdf"
-          }, {
-            icon: "doc",
-            file_name: "Simple_Word_document.doc"
-          }]
+          file_list: Generator.get("file", "random")
         }],
         new_value: [{
           text: "",
-          file_list: [{
-            icon: "pdf",
-            file_name: "MTV_001_SIGNED_LoginAccess_List.pdf"
-          }, {
-            icon: "doc",
-            file_name: "Simple_Word_document.doc"
-          }, {
-            icon: "xls",
-            file_name: "Simple_Excel_document.xls"
-          }, {
-            icon: "text",
-            file_name: "Some_file_from_Google_Drive.txt"
-          }, {
-            icon: "image",
-            file_name: "Image_of_nature.png"
-          }, {
-            icon: "",
-            file_name: "canjs-observe-bug.reg"
-          }]
+          file_list: Generator.get("file", "random")
         }]
       }, {
         type: "assignee",
@@ -278,112 +210,7 @@
           text: ""
         }]
       }],
-      past_requests: [{
-        date: "26/09/2015",
-        title: "My new request lorem ipsum dolor sit amet consectetur adipiscing elit morbi et turpis et arcu viverra posuere in et sapien",
-        past_requests_files: [{
-          icon: "pdf",
-          name: "MTV_001_SIGNED_LoginAccess_List.pdf",
-          date: "26/09/2015",
-          url: "http://www.this-is-great.com"
-        }, {
-          icon: "pdf",
-          name: "MTV_aug_SIGNED_LoginAccess_List.pdf",
-          date: "26/09/2015",
-          url: "http://www.this-is-great.com"
-        }, {
-          icon: "pdf",
-          name: "MTV_sep_SIGNED_LoginAccess_List.pdf",
-          date: "26/09/2015",
-          url: "http://www.this-is-great.com"
-        }],
-        mapped_objects: [{
-          icon: "objective",
-          title: "090.7068 objective 1",
-          state: "Draft",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
-        }, {
-          icon: "control",
-          title: "Access to the Private Network with expired Key v0906984",
-          state: "In Progress",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
-        }, {
-          icon: "regulation",
-          title: "a regulation object",
-          state: "In Progress",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
-        }]
-      }, {
-        date: "20/09/2015",
-        title: "My new request",
-        past_requests_files: [{
-          icon: "pdf",
-          name: "MTV_001_SIGNED_LoginAccess_List.pdf",
-          date: "26/09/2015",
-          url: "http://www.this-is-great.com"
-        }, {
-          icon: "pdf",
-          name: "MTV_aug_SIGNED_LoginAccess_List.pdf",
-          date: "26/09/2015",
-          url: "http://www.this-is-great.com"
-        }, {
-          icon: "pdf",
-          name: "MTV_sep_SIGNED_LoginAccess_List.pdf",
-          date: "26/09/2015",
-          url: "http://www.this-is-great.com"
-        }],
-        mapped_objects: [{
-          icon: "objective",
-          title: "090.7068 objective 1",
-          state: "Draft",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
-        }, {
-          icon: "control",
-          title: "Access to the Private Network with expired Key v0906984",
-          state: "In Progress",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
-        }, {
-          icon: "regulation",
-          title: "a regulation object",
-          state: "In Progress",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
-        }]
-      }, {
-        date: "10/09/2015",
-        title: "My new request",
-        past_requests_files: [{
-          icon: "pdf",
-          name: "MTV_001_SIGNED_LoginAccess_List.pdf",
-          date: "26/09/2015",
-          url: "http://www.this-is-great.com"
-        }, {
-          icon: "pdf",
-          name: "MTV_aug_SIGNED_LoginAccess_List.pdf",
-          date: "26/09/2015",
-          url: "http://www.this-is-great.com"
-        }, {
-          icon: "pdf",
-          name: "MTV_sep_SIGNED_LoginAccess_List.pdf",
-          date: "26/09/2015",
-          url: "http://www.this-is-great.com"
-        }],
-        mapped_objects: [{
-          icon: "objective",
-          title: "090.7068 objective 1",
-          state: "Draft",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
-        }, {
-          icon: "control",
-          title: "Access to the Private Network with expired Key v0906984",
-          state: "In Progress",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
-        }, {
-          icon: "regulation",
-          title: "a regulation object",
-          state: "In Progress",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sem id lectus porta, eu rutrum nunc commodo."
-        }]
-      }],
+      past_requests: Generator.get("request", 5),
       children: [{
         title: "Other title",
         type: "process",
@@ -398,14 +225,14 @@
         id: "12"
       }]
     }, {
-      title: "Brutal Pure Firecracker",
-      type: "assessment",
+      title: "Simple Request for Programs",
+      type: "issue",
       id: "3",
       status: "Draft",
       children: []
     }, {
-      title: "Sleepy Vital Notorious",
-      type: "assessment",
+      title: "Request made for Sections inspection",
+      type: "audit",
       id: "5",
       status: "Draft",
       children: [{
@@ -435,4 +262,4 @@
       }]
     }]
   };
-})(GGRC || {});
+})(GGRC || {}, GGRC.Mockup.Generator);
