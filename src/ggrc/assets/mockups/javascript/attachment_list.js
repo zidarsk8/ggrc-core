eecb6d4d6bfd5326888fa2715686d4ec566819fd
Ivan Kriznar <ivan@simplified.me>
2015-10-21 15:47:34 +0200

    Separate JS info files and optimize info pin behaviour

---
+++ src/ggrc/assets/mockups/javascript/attachment_list.js
@@ -0,0 +1,61 @@
+/*!
+    Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+    Created By: ivan@reciprocitylabs.com
+    Maintained By: ivan@reciprocitylabs.com
+*/
+
+(function (can, $, Generator) {
+  can.Component.extend({
+    tag: "attachment-list",
+    template: can.view("/static/mockups/base_templates/attachment_list.mustache"),
+    scope: {
+      title: "@",
+      types: "@",
+      files: new can.List()
+    },
+    events: {
+      "inserted": "updateFiles",
+      "{scope.data} add": "updateFiles",
+      "{scope.data} remove": "updateFiles",
+      "updateFiles": function () {
+        var types = this.scope.attr("types"),
+            isNegation = types.charAt(0) === "!",
+            result;
+        if (isNegation) {
+          types = types.slice(1);
+        }
+        result = _.reduce(this.scope.attr("data"), function (memo, comment) {
+          var attachments = _.filter(comment.attachments, function (attachment) {
+            if (isNegation) {
+              return attachment.extension !== types;
+            }
+            return attachment.extension === types;
+          });
+          if (attachments.length) {
+            return memo.concat(_.filter(attachments, function (attachment) {
+              return !attachment.attr("deleted");
+            }));
+          }
+          return memo;
+        }, []);
+        this.scope.attr("files", result);
+      },
+      ".js-trigger--delete click": function (el, ev) {
+        var file = el.data("file");
+        file.attr("deleted", true);
+        this.updateFiles();
+      },
+      ".btn-draft click": function (el, ev) {
+        if (this.scope.attr("types") !== "url") {
+          return this.scope.attr("data").unshift({
+            author: Generator.current.u,
+            timestamp: Generator.current.d,
+            attachments: Generator.get("file"),
+            comment: ""
+          });
+        }
+      }
+    }
+  });
+})(this.can, this.can.$, GGRC.Mockup.Generator);
