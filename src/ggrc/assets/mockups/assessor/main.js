3d4541ae84050efa8cbf88fc49d31c51b2c0d96c
Ivan Kriznar <ivan@simplified.me>
2015-10-29 14:20:45 +0100

    Add Assessment page

---
+++ src/ggrc/assets/mockups/assessor/main.js
@@ -0,0 +1,27 @@
+/*!
+    Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+    Created By: anze@reciprocitylabs.com
+    Maintained By: anze@reciprocitylabs.com
+*/
+
+
+(function (can, $) {
+
+  // Only load this file when the URL is mockups/sample:
+  if (window.location.pathname !== "/mockups/assessor") {
+    return;
+  }
+
+  // Setup the object page:
+  var mockup = new CMS.Controllers.MockupHelper($("body"), {
+    // Object:
+    object: {
+      icon: "grciconlarge-assessment",
+      title: "My Assessments",
+    },
+    infopin: "normal",
+    // Views:
+    views: _.values(GGRC.Bootstrap.Mockups.Assessor)
+  });
+})(this.can, this.can.$);
