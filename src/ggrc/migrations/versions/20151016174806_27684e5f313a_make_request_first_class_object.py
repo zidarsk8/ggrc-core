ca4d64b4933f7bdbb40d99e417222a42ecb3afbd
Andraz Bajt <andraz@bajt.me>
2015-11-02 11:07:48 +0100

    Make request assignee db field nullable

--- src/ggrc/migrations/versions/20151016174806_27684e5f313a_make_request_first_class_object.py
+++ src/ggrc/migrations/versions/20151016174806_27684e5f313a_make_request_first_class_object.py
@@ -113,6 +113,7 @@ def upgrade():
       WHERE relationships.source_type = "Request";
 
     ALTER TABLE requests DROP FOREIGN KEY requests_ibfk_1;
+    ALTER TABLE requests MODIFY COLUMN assignee_id INT(11) NULL;
     """
 
     sql += "COMMIT;"
