facadf8f9c388b6306fa268ede5b0a2822b09c68
Jure Vrscaj <jure.vrscaj@gmail.com>
2015-11-04 16:49:29 +0100

    Add licence headers to migrations in ggrc_risk_assessments

--- src/ggrc_risk_assessments/migrations/versions/20150716192442_5b29b4becf8_add_slug_to_risk_assessment.py
+++ src/ggrc_risk_assessments/migrations/versions/20150716192442_5b29b4becf8_add_slug_to_risk_assessment.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+# Created By: miha@reciprocitylabs.com
+# Maintained By: miha@reciprocitylabs.com
 
 """add slug to risk_assessment
 
