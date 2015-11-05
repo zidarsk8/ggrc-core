abfc1ecda732258fb85d94f43116a4fd7935f236
Anže Pečar <ensmotko@gmail.com>
2015-11-05 15:21:47 +0100

    Change comment_controller from 4 space to 2 space indentation

--- src/ggrc/assets/javascripts/controllers/comment_controller.js
+++ src/ggrc/assets/javascripts/controllers/comment_controller.js
@@ -6,53 +6,53 @@
  */
 
 can.Component.extend({
-    tag: "add-comment",
-    template: can.view("/static/mustache/base_templates/add_comment.mustache"),
-    scope: {
-        attachments: new can.List(),
-        parent_instance: null,
-        instance: null,
-        instance_attr: "@",
-        source_mapping: "@",
-        source_mapping_source: "@",
-        mapping: "@",
-        deferred: "@",
-        attributes: {},
-        list: [],
-        // the following are just for the case when we have no object to start with,
-        changes: []
+  tag: "add-comment",
+  template: can.view("/static/mustache/base_templates/add_comment.mustache"),
+  scope: {
+    attachments: new can.List(),
+    parent_instance: null,
+    instance: null,
+    instance_attr: "@",
+    source_mapping: "@",
+    source_mapping_source: "@",
+    mapping: "@",
+    deferred: "@",
+    attributes: {},
+    list: [],
+    // the following are just for the case when we have no object to start with,
+    changes: []
 
+  },
+  events: {
+    init: function() {
+      this.scope.attr("source_mapping", GGRC.page_instance());
     },
-    events: {
-        init: function() {
-            this.scope.attr("source_mapping", GGRC.page_instance());
-        },
-        "cleanPanel": function () {
-            this.scope.attachments.replace([]);
-            this.element.find("textarea").val("");
-        },
-        ".js-trigger-attachdata click": function (el, ev) {
-            //this.scope.attachments.push();
-            console.log("WIP")
-        },
-        ".btn-success click": function (el, ev) {
-            var $textarea = this.element.find(".add-comment textarea"),
-                description = $.trim($textarea.val()),
-                attachments = this.scope.attachments,
-                source = this.scope.source_mapping,
-                comment = CMS.Models["Comment"];
+    "cleanPanel": function () {
+      this.scope.attachments.replace([]);
+      this.element.find("textarea").val("");
+    },
+    ".js-trigger-attachdata click": function (el, ev) {
+      //this.scope.attachments.push();
+      console.log("WIP")
+    },
+    ".btn-success click": function (el, ev) {
+      var $textarea = this.element.find(".add-comment textarea"),
+        description = $.trim($textarea.val()),
+        attachments = this.scope.attachments,
+        source = this.scope.source_mapping,
+        comment = CMS.Models.Comment;
 
-            if (!description.length && !attachments.length) {
-                return;
-            }
-            var data = {
-                description: description,
-                context: source.context
-            };
-            new comment(data).save();
-            this.cleanPanel();
-        }
+      if (!description.length && !attachments.length) {
+        return;
+      }
+      var data = {
+        description: description,
+        context: source.context
+      };
+      new comment(data).save();
+      this.cleanPanel();
     }
+  }
 });
 
 (function(can, $) {
@@ -60,29 +60,29 @@ can.Component.extend({
 can.Control("GGRC.Controllers.Comments", {
 
 }, {
-    _create_relationship: function(source, destination) {
+  _create_relationship: function(source, destination) {
 
-        if (!destination) {
-            return $.Deferred().resolve();
-        }
-
-        return new CMS.Models.Relationship({
-            source: source.stub(),
-            destination: destination,
-            context: source.context,
-        }).save();
-    },
-    "{CMS.Models.Comment} created": function(model, ev, instance) {
-        if (!(instance instanceof  CMS.Models.Comment)) {
-            return;
-        }
-        var parent_dfd = this._create_relationship(GGRC.page_instance(), instance);
-        instance.delay_resolving_save_until($.when(parent_dfd));
+    if (!destination) {
+      return $.Deferred().resolve();
     }
+
+    return new CMS.Models.Relationship({
+      source: source.stub(),
+      destination: destination,
+      context: source.context,
+    }).save();
+  },
+  "{CMS.Models.Comment} created": function(model, ev, instance) {
+    if (!(instance instanceof  CMS.Models.Comment)) {
+      return;
+    }
+    var parent_dfd = this._create_relationship(GGRC.page_instance(), instance);
+    instance.delay_resolving_save_until($.when(parent_dfd));
+  }
 });
 
-    $(function() {
-        $(document.body).ggrc_controllers_comments();
-    });
+  $(function() {
+    $(document.body).ggrc_controllers_comments();
+  });
 
 })(this.can, this.can.$);
