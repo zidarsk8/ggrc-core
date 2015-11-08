fe3b27fa1d2108926c2f54b1afc2bbb7c5e20483
Anže Pečar <ensmotko@gmail.com>
2015-11-08 22:05:15 +0100

    Remove dead code

--- src/ggrc/assets/javascripts/components/people_list.js
+++ src/ggrc/assets/javascripts/components/people_list.js
@@ -27,96 +27,6 @@
       }
     },
     events: {
-      "{instance} updated": "update",
-      "{instance} created": "update",
-      "{groups} change": function () {
-        if (!this.viewModel.attr("editable")) {
-          return;
-        }
-        var instance = this.viewModel.attr("instance"),
-            isAllowed = [];
-
-        this.viewModel.attr("groups").each(function (group, type) {
-          group = group.filter(function (person) {
-            return person.attr("person_state") !== "deleted";
-          });
-          isAllowed.push(group.length);
-        });
-        instance.attr("people", _.every(isAllowed));
-      },
-      inserted: function () {
-        // this.scope.instance.get_assignees().then(function (assignees) {
-        //   assignees = _.mapKeys(assignees, function (val, key) {
-        //     return key.toLowerCase();
-        //   });
-        //   _.each(["verifier", "assignee", "requester"], function(type) {
-        //     if (!assignees[type]) {
-        //       assignees[type] = [];
-        //     }
-        //   });
-        //   assignees = _.mapValues(assignees, function (users) {
-        //     return _.map(users, function (user) {
-        //       user.person.relationship_id = user.relationship.id;
-        //       return user.person;
-        //     });
-        //   });
-        //   this.viewModel.attr("groups", assignees);
-        // }.bind(this));
-      },
-      "getRelationship": function (person, destination, type, action) {
-        if (action === "deleted") {
-          return CMS.Models.Relationship.findInCacheById(person.relationship_id);
-        }
-        return new CMS.Models.Relationship({
-          attrs: {
-            "AssigneeType": can.capitalize(type)
-          },
-          source: {
-            href: person.href,
-            type: person.type,
-            id: person.id
-          },
-          context: null,
-          destination: destination
-        });
-      },
-      "update": function () {
-        var instance = this.viewModel.attr("instance"),
-            destination, relationships = [];
-
-        destination = {
-          context_id: instance.context_id,
-          href: instance.href,
-          type: instance.type,
-          id: instance.id
-        };
-        this.viewModel.attr("groups").each(function (group, type) {
-          group.each(function (person) {
-            var action = person.person_state,
-                states = {
-                  "added": function (model) {
-                    return model.save();
-                  },
-                  "deleted": function (model) {
-
-                    return model.refresh().then(function (response) {
-                      return response.destroy();
-                    });
-                  },
-                },
-                model;
-
-            if (!_.contains(_.keys(states), action)) {
-              return;
-            }
-            person.attr("person_state", null);
-            model = this.getRelationship(person, destination, type, action);
-            relationships.push(states[action](model));
-          }, this);
-        }, this);
-
-        this.viewModel.attr("instance").delay_resolving_save_until($.when.apply($, relationships));
-      }
     }
   });
 
@@ -139,14 +49,6 @@
       toggle_add: false,
     },
     events: {
-      ".js-trigger--person-delete click": function (el, ev) {
-        var person = el.data("person");
-        if (person.attr("person_state")) {
-          person.attr("person_state", null);
-          return this.viewModel.attr("people").splice(el.data("index"), 1);
-        }
-        person.attr("person_state", "deleted");
-      },
       ".person-selector input autocomplete:select": function (el, ev, ui) {
         var person = ui.item,
             destination = this.viewModel.instance,
