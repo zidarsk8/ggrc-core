99cbec8ff026cb8f1c8b63bc09ba016becd79dbd
Urban Skudnik <urban.skudnik@gmail.com>
2015-11-09 12:04:58 +0100

    Add license to migration

--- src/ggrc/migrations/versions/20151016174806_27684e5f313a_make_request_first_class_object.py
+++ src/ggrc/migrations/versions/20151016174806_27684e5f313a_make_request_first_class_object.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+# Created By: urban@reciprocitylabs.com
+# Maintained By: urban@reciprocitylabs.com
+
 """Make Request First-class object
 
 Revision ID: 27684e5f313a
