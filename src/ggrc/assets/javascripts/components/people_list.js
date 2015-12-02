d7415b351e3910ff8b7814594a819a2cf8c12e7f
Urban Skudnik <urban.skudnik@gmail.com>
2015-12-02 17:54:32 +0100

    Refactor triggoring change to not initialise control

--- src/ggrc/assets/javascripts/components/people_list.js
+++ src/ggrc/assets/javascripts/components/people_list.js
@@ -112,9 +112,7 @@
         }
         if (destination.id === undefined && destination._pending_joins) {
           // Force re-validation of all fields on person select
-          var modal = $(".ggrc_controllers_modals");
-          var control = $(modal).control();
-          control.options.instance._triggerChange("title");
+          this.scope.instance._triggerChange("title");
         }
       },
     },
