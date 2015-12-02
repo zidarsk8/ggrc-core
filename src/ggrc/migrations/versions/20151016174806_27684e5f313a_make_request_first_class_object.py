2e3e52a4a4c9e45009d08e7006b853b7ca335761
Urban Skudnik <urban.skudnik@gmail.com>
2015-12-02 16:11:00 +0100

    Remove migration of audit - request relationships

--- src/ggrc/migrations/versions/20151016174806_27684e5f313a_make_request_first_class_object.py
+++ src/ggrc/migrations/versions/20151016174806_27684e5f313a_make_request_first_class_object.py
@@ -16,8 +16,6 @@ revision = '27684e5f313a'
 down_revision = '1bad7fe16295'
 
 from alembic import op
-import sqlalchemy as sa
-from sqlalchemy.dialects import mysql
 
 from HTMLParser import HTMLParser
 import bleach
@@ -42,7 +40,7 @@ def cleaner(value, bleach_tags=[], bleach_attrs={}):
     value = parser.unescape(value)
 
   ret = parser.unescape(
-    bleach.clean(value, bleach_tags, bleach_attrs, strip=True)
+      bleach.clean(value, bleach_tags, bleach_attrs, strip=True)
   )
   return ret
 
@@ -73,33 +71,8 @@ def upgrade():
         AO.context_id
       FROM requests AS R, audit_objects AS AO WHERE AO.id = R.audit_object_id;
     """)
-  # 2. Create relations to Audits in Relationship table
-  #   source_type = Audit
-  #   source_id   = audit.id
-  #   destionation_type = Request
-  #   destination_id = request.id
-  op.execute("""
-    INSERT INTO relationships (
-      modified_by_id,
-      created_at,
-      updated_at,
-      source_id,
-      source_type,
-      destination_id,
-      destination_type,
-      context_id) SELECT
-      AO.modified_by_id,
-      NOW(),
-      NOW(),
-      AO.audit_id,
-      "Audit",
-      R.id,
-      "Request",
-      AO.context_id
-      FROM requests AS R, audit_objects AS AO WHERE AO.id = R.audit_object_id;
-    """)
 
-  # 3. Change status values
+  # 2. Change status values
   op.execute("""ALTER TABLE requests CHANGE status status ENUM("Draft","Requested","Responded","Amended Request","Updated Response","Accepted","Unstarted","In Progress","Finished","Verified") NOT NULL;""")
   op.execute("""UPDATE requests SET status="Unstarted" WHERE status="Draft";""")
   op.execute("""UPDATE requests SET status="In Progress" WHERE status="Requested";""")
@@ -113,7 +86,7 @@ def upgrade():
   op.execute("""ALTER TABLE requests DROP FOREIGN KEY requests_ibfk_1;""")
   op.execute("""ALTER TABLE requests MODIFY COLUMN assignee_id INT(11) NULL;""")
 
-  # Make pretty title
+  # 4. Make pretty title
   requests = db.session.query(Request)
   for request in requests:
     cleaned_desc = cleaner(request.description)
@@ -137,27 +110,26 @@ def upgrade():
   # sql += "COMMIT;"
   # op.execute(sql)
 
-
   # TODO: drop requests table from audits
 
   # TODO: 5. Link all objects that are mapped to Audit to requests
 
   # TODO: Drop relationship audit_objects from Audits????
 
-  # Migrate responses to comments
+  # 5. Migrate responses to comments
   documentation_responses = db.session.query(DocumentationResponse)
   for dr in documentation_responses:
     related = dr.related_sources + dr.related_destinations
     comment = Comment(
-      description=dr.description,
-      created_at=dr.created_at,
-      modified_by=dr.modified_by,
-      updated_at=dr.updated_at,
-      context=dr.context)
+        description=dr.description,
+        created_at=dr.created_at,
+        modified_by=dr.modified_by,
+        updated_at=dr.updated_at,
+        context=dr.context)
 
     request_comment_rel = Relationship(
-      source=dr.request,
-      destination=comment)
+        source=dr.request,
+        destination=comment)
 
     for rel in related:
       if not rel.source or not rel.destination:
@@ -169,8 +141,8 @@ def upgrade():
       else:
         continue
       related_objects_to_request = Relationship(
-        source=dr.request,
-        destination=destination
+          source=dr.request,
+          destination=destination
       )
       db.session.add(related_objects_to_request)
     db.session.add(comment)
@@ -187,8 +159,8 @@ def upgrade():
 
       for m in ir.meetings:
         desc += "<a href=\"{url}\">Meeting</a> requested on {date}<br />". \
-          format(url=m.title,
-                 date=m.created_at.strftime("%m/%d/%Y at %H:%M"))
+            format(url=m.title,
+                   date=m.created_at.strftime("%m/%d/%Y at %H:%M"))
 
     if ir.people:
       desc += "<br /><br /><b>Attendees</b><hr />"
@@ -196,15 +168,15 @@ def upgrade():
         desc += "- {} ({})<br />".format(p.name, p.email)
 
     comment = Comment(
-      description=desc,
-      created_at=ir.created_at,
-      modified_by=ir.modified_by,
-      updated_at=ir.updated_at,
-      context=ir.context)
+        description=desc,
+        created_at=ir.created_at,
+        modified_by=ir.modified_by,
+        updated_at=ir.updated_at,
+        context=ir.context)
 
     request_comment_rel = Relationship(
-      source=ir.request,
-      destination=comment)
+        source=ir.request,
+        destination=comment)
 
     for rel in related:
       if not rel.source or not rel.destination:
@@ -216,8 +188,8 @@ def upgrade():
       else:
         continue
       related_objects_to_request = Relationship(
-        source=ir.request,
-        destination=destination)
+          source=ir.request,
+          destination=destination)
       db.session.add(related_objects_to_request)
     db.session.add(comment)
     db.session.add(request_comment_rel)
