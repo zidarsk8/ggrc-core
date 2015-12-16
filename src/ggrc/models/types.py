5198bd7db80db5d3808b8d4a61c1a274fe75f0d7
Miha Zidar <zidarsk8@gmail.com>
2015-12-16 15:07:33 +0100

    Put magic numbers into constants
    Put magic numbers into descriptive constants and remove the comments
    that aren't neede anymore.

--- src/ggrc/models/types.py
+++ src/ggrc/models/types.py
@@ -17,13 +17,13 @@ from ggrc.models import exceptions
 
 class JsonType(types.TypeDecorator):
   # pylint: disable=W0223
-
   """ Custom Json data type
 
   Custom type for storing Json objects in our database as serialized text.
   The Limit for the serialized Json is the same as the database text column
   limit (65534).
   """
+  MAX_TEXT_LENGTH = 65534
   impl = types.Text
 
   def process_result_value(self, value, dialect):
@@ -36,21 +36,19 @@ class JsonType(types.TypeDecorator):
       pass
     else:
       value = utils.as_json(value)
-      # Detect if the byte-length of the encoded JSON is larger than the
-      # database "TEXT" column type can handle
-      if len(value.encode('utf-8')) > 65534:
+      if len(value.encode('utf-8')) > self.MAX_TEXT_LENGTH:
         raise exceptions.ValidationError("Log record content too long")
     return value
 
 
 class CompressedType(types.TypeDecorator):
   # pylint: disable=W0223
-
   """ Custom Compresed data type
 
   Custom type for storing any python object in our database as serialized text.
   """
-  impl = types.LargeBinary(length=16777215)
+  MAX_BINARY_LENGTH = 16777215
+  impl = types.LargeBinary(length=MAX_BINARY_LENGTH)
 
   def process_result_value(self, value, dialect):
     if value is not None:
@@ -59,8 +57,6 @@ class CompressedType(types.TypeDecorator):
 
   def process_bind_param(self, value, dialect):
     value = pickle.dumps(value)
-    # Detect if the byte-length of the compressed pickle is larger than the
-    # database "LargeBinary" column type can handle
-    if len(value) > 16777215:
+    if len(value) > self.MAX_BINARY_LENGTH:
       raise exceptions.ValidationError("Log record content too long")
     return value
