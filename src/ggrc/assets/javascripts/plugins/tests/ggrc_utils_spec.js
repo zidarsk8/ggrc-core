5f51cdf16c98094bfb946153fd6d990c31b7800a
Ivan Kriznar <ivan@simplified.me>
2015-12-24 19:05:34 +0100

    Add tests for is allowed to map

--- src/ggrc/assets/javascripts/plugins/tests/ggrc_utils_spec.js
+++ src/ggrc/assets/javascripts/plugins/tests/ggrc_utils_spec.js
@@ -11,7 +11,9 @@ describe("GGRC utils allowed_to_map() method", function () {
   var allowed_to_map,  // the method under test
       fakeOptions,
       fakeProgram,
-      fakeAudit;
+      fakeAudit,
+      fakeCA,
+      fakePersonCreator;
 
   beforeAll(function () {
     allowed_to_map = GGRC.Utils.allowed_to_map;
@@ -19,17 +21,18 @@ describe("GGRC utils allowed_to_map() method", function () {
 
   beforeEach(function () {
     fakeOptions = {};
-
-    fakeProgram = new CMS.Models.Program({type: "Program"});
-    fakeAudit = new CMS.Models.Audit({type: "Audit"});
-
-    spyOn(GGRC.Mappings, "get_canonical_mapping_name")
-      .and.returnValue("audits");
-
-    spyOn(Permission,"is_allowed_for").and.returnValue(true);
   });
 
   describe("given an Audit and Program pair", function () {
+    beforeEach(function () {
+      fakeProgram = new CMS.Models.Program({type: "Program"});
+      fakeAudit = new CMS.Models.Audit({type: "Audit"});
+
+      spyOn(GGRC.Mappings, "get_canonical_mapping_name")
+        .and.returnValue("audits");
+
+      spyOn(Permission, "is_allowed_for").and.returnValue(true);
+    });
     it("returns false for Audit as source and Program as target", function () {
       var result = allowed_to_map(fakeAudit, fakeProgram, fakeOptions);
       expect(result).toBe(false);
@@ -41,4 +44,27 @@ describe("GGRC utils allowed_to_map() method", function () {
     });
   });
 
+  describe("given an Person and Control Assessment pair", function () {
+    beforeEach(function () {
+      fakeCA = new CMS.Models.ControlAssessment({type: "ControlAssessment"});
+      fakePersonCreator = new CMS.Models.Person({type: "Person"});
+
+      spyOn(Permission, "is_allowed_for").and.returnValue(true);
+    });
+    it("returns true for Control Assessment as source and Person as target", function () {
+      var result = allowed_to_map(fakeCA, fakePersonCreator, fakeOptions);
+
+      spyOn(GGRC.Mappings, "get_canonical_mapping_name")
+        .and.returnValue("people");
+      expect(result).toBe(true);
+    });
+
+    it("returns false for Person as source and as Control Assessment target", function () {
+      var result = allowed_to_map(fakePersonCreator, fakeCA, fakeOptions);
+
+      spyOn(GGRC.Mappings, "get_canonical_mapping_name")
+        .and.returnValue("related_objects");
+      expect(result).toBe(false);
+    });
+  });
 });
