d30238380bd2a93268b3cbe4673878595abed33b
Miha Zidar <zidarsk8@gmail.com>
2016-01-23 21:08:51 +0100

    Add build steps to test docker provision
    - Add build_compass and build_assets to the provisioning of the testing
      docker containers.
    - change tabs to spaces.

--- bin/jenkins/functions.sh
+++ bin/jenkins/functions.sh
@@ -14,23 +14,26 @@ setup () {
     PROJECT="${1}"
   fi
 
-	docker-compose --file docker-compose-testing.yml \
-		--project-name ${PROJECT} \
-		up --force-recreate -d
-
-	if [[ $UID -eq 0 ]]; then
-		# This fixes permissions with bindfs only on jenkins.
-		chown 1000:1000 -R .
-	elif [[ $UID -eq 1000 ]]; then
-		docker exec -i ${PROJECT}_dev_1 sh -c "
-		  bindfs /vagrant_bind /vagrant --map=$UID/1000 -o nonempty
-		"
-	fi
-
-	echo "Provisioning ${PROJECT}_dev_1"
-	docker exec -i ${PROJECT}_dev_1 su vagrant -c "
-		ansible-playbook -i provision/docker/inventory provision/site.yml
-	"
+  docker-compose --file docker-compose-testing.yml \
+    --project-name ${PROJECT} \
+    up --force-recreate -d
+  
+  if [[ $UID -eq 0 ]]; then
+    # This fixes permissions with bindfs only on jenkins.
+    chown 1000:1000 -R .
+  elif [[ $UID -eq 1000 ]]; then
+    docker exec -i ${PROJECT}_dev_1 sh -c "
+    bindfs /vagrant_bind /vagrant --map=$UID/1000 -o nonempty
+    "
+  fi
+  
+  echo "Provisioning ${PROJECT}_dev_1"
+  docker exec -i ${PROJECT}_dev_1 su vagrant -c "
+    source /vagrant/bin/init_vagrant_env
+    ansible-playbook -i provision/docker/inventory provision/site.yml
+    build_compass
+    build_assets
+  "
 }
 
 teardown () {
@@ -41,8 +44,8 @@ teardown () {
     PROJECT="${1}"
   fi
 
-	docker exec -i ${PROJECT}_dev_1 sh -c "chown $UID -R /vagrant"
-	docker exec -i ${PROJECT}_selenium_1 sh -c "chown $UID -R /selenium"
+  docker exec -i ${PROJECT}_dev_1 sh -c "chown $UID -R /vagrant"
+  docker exec -i ${PROJECT}_selenium_1 sh -c "chown $UID -R /selenium"
 
-	docker-compose -p ${PROJECT} stop
+  docker-compose -p ${PROJECT} stop
 }
