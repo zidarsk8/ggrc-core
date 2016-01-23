ccae1c7152f202788d5661fe6e2c7e7d905b9339
Miha Zidar <zidarsk8@gmail.com>
2016-01-23 02:43:07 +0100

    Add license header to functions bash script

--- bin/jenkins/functions.sh
+++ bin/jenkins/functions.sh
@@ -1,3 +1,7 @@
+# Copyright (C) 2016 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+# Created By: miha@reciprocitylabs.com
+# Maintained By: miha@reciprocitylabs.com
 
 set -o nounset
 set -o errexit
@@ -42,4 +46,3 @@ teardown () {
 
 	docker-compose -p ${PROJECT} stop
 }
-
