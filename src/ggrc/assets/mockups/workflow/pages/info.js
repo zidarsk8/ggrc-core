781d8d270a069b968276ab91e3d2694b3257bd9d
Vladan Mitevski <vladan@mvdevelop.com>
2016-02-01 11:42:28 +0100

    Make workflow mockups dynamic

--- src/ggrc/assets/mockups/workflow/pages/info.js
+++ src/ggrc/assets/mockups/workflow/pages/info.js
@@ -42,5 +42,66 @@
         randomize: "state"
       })
     },
+    t_title: Generator.create({
+      hidable: false
+    }),
+    start_date: Generator.create({
+      hidable: true
+    }),
+    end_date: Generator.create({
+      hidable: true
+    }),
+    t_description: Generator.create({
+      hidable: true
+    }),
+    t_type: Generator.create({
+      hidable: false,
+      sidebar: false,
+      size: 5
+    }),
+    select: {
+      "type": Generator.create({
+        option_text: "%title",
+      }, {
+        count: 3
+      }),
+      "frequency": Generator.create({
+        option_text: "%title",
+      }, {
+        count: 5
+      })
+    },
+    // t_type_data: [{
+    //     option_text: "Rich text"
+    //   }, {
+    //     option_text: "Dropdown"
+    //   }, {
+    //     option_text: "Checkboxes"
+    // }],
+    t_group: Generator.create({
+      hidable: true
+    }),
+    w_title: Generator.create({
+      hidable: true
+    }),
+    w_desc: Generator.create({
+      hidable: true
+    }),
+    frequency: Generator.create({
+      hidable: false,
+      sidebar: true,
+      size: 6
+    }),
+    // frequency_data: [{
+    //     option_text: "One time"
+    //   }, {
+    //     option_text: "Weekly"
+    //   }, {
+    //     option_text: "Monthly"
+    //   }, {
+    //     option_text: "Quarterly"
+    //   }, {
+    //     option_text: "Annually"
+    // }]
   };
 })(GGRC || {}, GGRC.Mockup.Generator);
