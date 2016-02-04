48abb2148b07800da5236ae31602b097cadd586a
Urban Skudnik <urban.skudnik@gmail.com>
2016-02-04 22:57:05 +0100

    Test output format only and not the data itself
    Testing for AM/PM (and the whole date function) will be tricky
    until we migrate converters off of pure JS date functions to
    full-pipeline moment support. Focusing on output format only for
    now.

--- src/ggrc/assets/js_specs/mustache_helpers/date_spec.js
+++ src/ggrc/assets/js_specs/mustache_helpers/date_spec.js
@@ -16,50 +16,50 @@ describe('can.mustache.helper.date', function () {
   });
 
   it('returns date only when boolean true is passed in', function () {
-    instance = new Date(2015, 4, 3, 12, 34, 56);
+    instance = new Date(2015, 4, 3, 7, 34, 56);
     expect(helper(instance, true)).toEqual('05/03/2015');
   });
 
   it('returns datetime when stringy truthy value is passed in', function () {
     var timezone = moment(
-      '2015-05-03T12:34:45Z').tz(moment.tz.guess()).format('A z');
-    instance = new Date(2015, 4, 3, 12, 34, 56);
+      '2015-05-03T12:34:45Z').tz(moment.tz.guess()).format('z');
+    instance = new Date(2015, 4, 3, 7, 34, 56);
 
-    expect(helper(instance, 'true')).toEqual('05/03/2015 12:34:56 '+ timezone);
+    expect(helper(instance, 'true')).toEqual('05/03/2015 07:34:56 AM '+ timezone);
   });
 
   it('returns datetime when stringy falsey value is passed in', function () {
     var timezone = moment(
-      '2015-05-03T12:34:45Z').tz(moment.tz.guess()).format('A z');
-    instance = new Date(2015, 4, 3, 12, 34, 56);
+      '2015-05-03T12:34:45Z').tz(moment.tz.guess()).format('z');
+    instance = new Date(2015, 4, 3, 7, 34, 56);
 
     expect(
-      helper(instance, 'false')).toEqual('05/03/2015 12:34:56 '+ timezone);
+      helper(instance, 'false')).toEqual('05/03/2015 07:34:56 AM '+ timezone);
   });
 
   it('returns datetime when false value is passed in', function () {
     var timezone = moment(
-      '2015-05-03T12:34:45Z').tz(moment.tz.guess()).format('A z');
-    instance = new Date(2015, 4, 3, 12, 34, 56);
+      '2015-05-03T12:34:45Z').tz(moment.tz.guess()).format('z');
+    instance = new Date(2015, 4, 3, 7, 34, 56);
 
     expect(
-      helper(instance, false)).toEqual('05/03/2015 12:34:56 '+ timezone);
+      helper(instance, false)).toEqual('05/03/2015 07:34:56 AM '+ timezone);
   });
 
 
   it('returns datetime when random values are passed in', function () {
     var timezone = moment(
-      '2015-05-03T12:34:45Z').tz(moment.tz.guess()).format('A z');
-    instance = new Date(2015, 4, 3, 12, 34, 56);
+      '2015-05-03T12:34:45Z').tz(moment.tz.guess()).format('z');
+    instance = new Date(2015, 4, 3, 7, 34, 56);
 
     expect(
-      helper(instance, 'asdasd')).toEqual('05/03/2015 12:34:56 '+ timezone);
+      helper(instance, 'asdasd')).toEqual('05/03/2015 07:34:56 AM '+ timezone);
     expect(
-      helper(instance, {a: 1})).toEqual('05/03/2015 12:34:56 '+ timezone);
+      helper(instance, {a: 1})).toEqual('05/03/2015 07:34:56 AM '+ timezone);
     expect(
-      helper(instance, '{a: 1}')).toEqual('05/03/2015 12:34:56 '+ timezone);
+      helper(instance, '{a: 1}')).toEqual('05/03/2015 07:34:56 AM '+ timezone);
     expect(
-      helper(instance, 123)).toEqual('05/03/2015 12:34:56 '+ timezone);
+      helper(instance, 123)).toEqual('05/03/2015 07:34:56 AM '+ timezone);
   });
 
 });
