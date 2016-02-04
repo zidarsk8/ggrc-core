d16cec0a2506d75901ab22e9d42ab51176279125
Jernej Makovšek <jernej.makovsek@gmail.com>
2016-02-04 20:17:54 +0100

    Handle missing slash in app url

--- test/selenium/src/lib/environment/__init__.py
+++ test/selenium/src/lib/environment/__init__.py
@@ -18,8 +18,11 @@ _yaml = file_ops.load_yaml_contents(_config_file_path)
 LOGGING_FORMAT = _yaml[constants.yaml.LOGGING][constants.yaml.FORMAT]
 LOGGING_LEVEL = _yaml[constants.yaml.LOGGING][constants.yaml.LEVEL]
 CHROME_DRIVER_PATH = _yaml[constants.yaml.WEBDRIVER][constants.yaml.PATH]
-APP_URL = _yaml[constants.yaml.APP][constants.yaml.URL]
-SERVER_WAIT_TIME = _yaml[constants.yaml.APP][constants.yaml.WAIT_TIME]
+
+_yaml_app_url = _yaml[constants.yaml.APP][constants.yaml.URL]
+APP_URL = _yaml_app_url \
+    if _yaml_app_url.endswith("/") \
+    else _yaml_app_url.join("/")
 DISPLAY_WINDOWS = _yaml[constants.yaml.BROWSER][constants.yaml.DISPLAY]
 WINDOW_RESOLUTION = literal_eval(_yaml[constants.yaml.BROWSER][
     constants.yaml.RESOLUTION])
