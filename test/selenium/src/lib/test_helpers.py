72cc7237da1c7d384f4ddb1a4251c17af1bee74e
Jernej Makovšek <jernej.makovsek@gmail.com>
2016-02-12 15:08:28 +0100

    Update helpers

--- test/selenium/src/lib/test_helpers.py
+++ test/selenium/src/lib/test_helpers.py
@@ -36,24 +36,37 @@ class ModalNewProgramPage(base.Test):
     """Fills out all fields in the lhn_modal
 
     Args:
-        modal (lib.page.lhn_modal.new_program.NewProgramModal)
+        modal (lib.page.lhn_modal.new_program.EditProgramModal)
     """
     unique_id = str(uuid.uuid4())
 
     modal.enter_title(modal_create_new.Program.TITLE + unique_id)
     modal.enter_description(
-        modal_create_new.Program.DESCRIPTION_SHORT)
+        modal_create_new.Program.DESCRIPTION_SHORT + unique_id)
     modal.enter_notes(
-        modal_create_new.Program.NOTES_SHORT)
+        modal_create_new.Program.NOTES_SHORT + unique_id)
     modal.enter_code(modal_create_new.Program.CODE + unique_id)
     modal.filter_and_select_primary_contact("example")
     modal.filter_and_select_secondary_contact("example")
     modal.enter_program_url(
-        modal_create_new.Program.PROGRAM_URL)
+        unique_id + modal_create_new.Program.PROGRAM_URL)
     modal.enter_reference_url(
-        modal_create_new.Program.REFERENCE_URL)
-    modal.enter_effective_date_start_month()
-    modal.enter_stop_date_end_month()
+        unique_id + modal_create_new.Program.REFERENCE_URL)
+
+  @staticmethod
+  def set_start_end_dates(modal, day_start, day_end):
+    """
+    Sets the dates from the datepicker in the new program/edit modal.
+
+    Args:
+      modal (lib.page.lhn_modal.new_program.EditProgramModal)
+      day_start (int): for more info see
+        base.DatePicker.select_day_in_current_month
+      day_end (int): for more info see
+        base.DatePicker.select_day_in_current_month
+    """
+    modal.enter_effective_date_start_month(day_start)
+    modal.enter_stop_date_end_month(day_end)
 
 
 class ModalNewProgramCustomAttribute(base.Test):
