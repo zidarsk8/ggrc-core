ffda584bca5373e37e7acf94e943e91cba93c4fb
Ivan Kriznar <ivan@simplified.me>
2016-02-23 17:58:40 +0100

    Fix migration line length

--- src/ggrc/migrations/versions/20160223152916_204540106539_assessment_titles.py
+++ src/ggrc/migrations/versions/20160223152916_204540106539_assessment_titles.py
@@ -10,20 +10,24 @@ Revises: 4e989ef86619
 Create Date: 2016-02-23 15:29:16.361412
 
 """
+# Disable Invalid constant name pylint warning for mandatory Alembic variables.
+# pylint: disable=C0103
+
+from alembic import op
+from ggrc.models import assessment
+from ggrc.migrations import utils
+
 
 # revision identifiers, used by Alembic.
 revision = '204540106539'
 down_revision = '4e989ef86619'
 
-from alembic import op
-
-from ggrc.models.assessment import Assessment
-from ggrc.migrations.utils import resolve_duplicates
-
 
 def upgrade():
   op.drop_constraint('uq_t_control_assessments', 'assessments', 'unique')
 
+
 def downgrade():
-  resolve_duplicates(Assessment, 'title', ' ')
-  op.create_unique_constraint('uq_t_control_assessments', 'assessments', ['title'])
+  utils.resolve_duplicates(assessment.Assessment, 'title', ' ')
+  op.create_unique_constraint('uq_t_control_assessments',
+                              'assessments', ['title'])
