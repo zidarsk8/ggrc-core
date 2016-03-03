ab24b69e30c05bb83eedf432804a06445c1985ec
Peter Lamut <peter.lamut@niteoweb.com>
2016-03-03 10:09:51 +0100

    Add a (CanJS) component registry to GGRC.Utils

--- src/ggrc/assets/javascripts/plugins/ggrc_utils.js
+++ src/ggrc/assets/javascripts/plugins/ggrc_utils.js
@@ -1,10 +1,14 @@
 /*!
-    Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
-    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
-    Created By: ivan@reciprocitylabs.com
-    Maintained By: ivan@reciprocitylabs.com
+  Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
+  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+  Created By: ivan@reciprocitylabs.com
+  Maintained By: ivan@reciprocitylabs.com
 */
-(function($, GGRC, moment, Permission) {
+
+(function ($, GGRC, moment, Permission) {
+  /**
+   * A module containing various utility functions.
+   */
   GGRC.Utils = {
     firstWorkingDay: function (date) {
       date = moment(date);
