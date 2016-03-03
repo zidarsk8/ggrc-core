9d7aab08c4cf899ac69bb6d999c73d80b34f4544
Vladan Mitevski <vladan@mvdevelop.com>
2016-03-03 10:26:14 +0100

    Lint task file

--- src/ggrc/assets/mockups/javascript/generators/task.js
+++ src/ggrc/assets/mockups/javascript/generators/task.js
@@ -5,122 +5,124 @@
     Maintained By: ivan@reciprocitylabs.com
 */
 
-
 (function (GGRC, _, Generator) {
   Generator.task = function () {
     return {
-      title: "Simple task",
-      info_title: "Simple task",
+      title: 'Simple task',
+      info_title: 'Simple task',
       description: Generator.paragraph(7),
-      type: "task",
+      type: 'task',
       state: {
-        title: "Draft",
-        class_name: "draft"
+        title: 'Draft',
+        class_name: 'draft'
       },
-      state_color: "draft",
-      status: "Draft",
-      id: "33",
-      icon: "calendar-check-o",
-      comments: Generator.get("comment", 5, {sort: "date", types: ["assignee", "verifier"]}),
+      state_color: 'draft',
+      status: 'Draft',
+      id: '33',
+      icon: 'calendar-check-o',
+      comments: Generator.get('comment', 5, {
+        sort: 'date',
+        types: ['assignee', 'verifier']
+      }),
       people: {
-        "assignee": Generator.get("user", 2),
-        "verifiers": Generator.get("user", 2)
+        assignee: Generator.get('user', 2),
+        verifiers: Generator.get('user', 2)
       },
       created_on: Generator.get_date({year: 2015}),
       due_on: Generator.get_date({year: 2015}),
       mapped: {
-        "objects": Generator.create({
-            icon: ["objective", "control", "regulation"],
-            title: "%title",
-            description: "%text",
-            state: ["In Progress", "Draft"]
-          }, {
-            count: 5,
-            randomize: ["state", "icon"]
-          }),
-        "requests": Generator.create({
-            icon: "requests",
-            title: "%title",
-            description: "%text",
-            state: ["In Progress", "Draft"]
-          }, {
-            count: 5,
-            randomize: "state"
-          }),
-        "issues": Generator.create({
-            icon: "issue",
-            title: "%title",
-            description: "%text",
-            state: ["In Progress", "Draft"]
-          }, {
-            count: 5,
-            randomize: "state"
-          })
+        objects: Generator.create({
+          icon: ['objective', 'control', 'regulation'],
+          title: '%title',
+          description: '%text',
+          state: ['In Progress', 'Draft']
+        }, {
+          count: 5,
+          randomize: ['state', 'icon']
+        }),
+        requests: Generator.create({
+          icon: 'requests',
+          title: '%title',
+          description: '%text',
+          state: ['In Progress', 'Draft']
+        }, {
+          count: 5,
+          randomize: 'state'
+        }),
+        issues: Generator.create({
+          icon: 'issue',
+          title: '%title',
+          description: '%text',
+          state: ['In Progress', 'Draft']
+        }, {
+          count: 5,
+          randomize: 'state'
+        })
       },
       logs: Generator.create({
-        author: "%user",
-        timestamp: "%date",
+        author: '%user',
+        timestamp: '%date',
         data: [{
-          status: "made changes",
-          field: "Comment",
+          status: 'made changes',
+          field: 'Comment',
           original: {
-            text: "%text"
+            text: '%text'
           },
           changed: {
-            text: "%text"
+            text: '%text'
           }
         }, {
-          status: "made changes",
-          field: "Evidence",
+          status: 'made changes',
+          field: 'Evidence',
           original: {
             files: []
           },
           changed: {
-            files: "%files"
+            files: '%files'
           }
         }, {
-          status: "made changes",
-          field: "People - Requester",
+          status: 'made changes',
+          field: 'People - Requester',
           original: {
-            author: "%user"
+            author: '%user'
           },
           changed: {
-            author: "%user"
+            author: '%user'
           }
         }, {
-          status: "created request",
-          field: ""
+          status: 'created request',
+          field: ''
         }, {
-          status: "made changes",
-          field: "Dates - Due on",
+          status: 'made changes',
+          field: 'Dates - Due on',
           original: {
-            text: "%date"
+            text: '%date'
           },
           changed: {
-            text: "%date"
+            text: '%date'
           }
         }, {
-          status: "made changes",
-          field: "Dates - Created on",
+          status: 'made changes',
+          field: 'Dates - Created on',
           original: {
-            text: "%date"
+            text: '%date'
           },
           changed: {
-            text: "%date"
+            text: '%date'
           }
         }, {
-          status: "made changes",
-          field: "Description",
+          status: 'made changes',
+          field: 'Description',
           original: {
-            text: "%text"
+            text: '%text'
           },
           changed: {
-            text: "%text"
+            text: '%text'
           }
         }]
       }, {
         count: 5,
-        randomize: "data"
+        randomize: 'data'
       })
     };
   };
