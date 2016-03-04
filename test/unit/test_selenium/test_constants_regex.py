95a8d36c32bd0d141b6697d3207383b015bd0f53
Jernej Makovšek <jernej.makovsek@gmail.com>
2016-03-04 17:36:15 +0100

    Updated regex and unit test

--- test/unit/test_selenium/test_constants_regex.py
+++ test/unit/test_selenium/test_constants_regex.py
@@ -10,37 +10,41 @@ from lib.constants import regex
 
 
 def test_url_to_widget_info_regex():
-  """Test regex for getting id and widget name from url."""
+  """Test regex for parsing the object id, object name and widget name from
+  url."""
   urls = [
       ("https://grc-test.appspot.com/dashboard",
-          "", ""),
+          "dashboard", "", ""),
       ("https://grc-test.appspot.com/dashboard#clause_widget",
-          "", "clause_widget"),
+          "dashboard", "", "clause_widget"),
       ("https://grc-test.appspot.com/clauses/90#/clause/90",
-          90, "info_widget"),
+          "clauses", 90, "info_widget"),
       ("https://grc-test.appspot.com/clauses/90#",
-          90, "info_widget"),
+          "clauses", 90, "info_widget"),
       ("https://grc-test.appspot.com/clauses/90",
-          90, "info_widget"),
+          "clauses", 90, "info_widget"),
       ("https://grc-test.appspot.com/clauses/90#control_widget",
-          90, "control_widget"),
+          "clauses", 90, "control_widget"),
       ("https://grc-test.appspot.com/clauses/90#info_widget",
-          90, "info_widget"),
+          "clauses", 90, "info_widget"),
       ("https://grc-test.appspot.com/workflows/107",
-          107, "info_widget"),
+          "workflows", 107, "info_widget"),
       ("https://grc-test.appspot.com/workflows/107#task_group_widget/"
-          "task_group/122", 107, "task_group_widget"),
+          "task_group/122", "workflows", 107, "task_group_widget"),
       ("https://grc-test.appspot.com/workflows/107#info_widget/workflow/107",
-          107, "info_widget"),
+          "workflows", 107, "info_widget"),
       ("https://grc-test.appspot.com/workflows/107#/workflow/107",
-          107, "info_widget"),
+          "workflows", 107, "info_widget"),
   ]
 
-  for url, expected_id, expected_name in urls:
-    object_id, name = re.match(regex.URL_WIDGET_INFO, url).groups()
+  for url, expected_object_name, expected_id, expected_widget_name in urls:
+    object_name, object_id, widget_name = re.search(
+        regex.URL_WIDGET_INFO, url).groups()
+
     if object_id:
       object_id = int(object_id)
-      name = name or "info_widget"
+      widget_name = widget_name or "info_widget"
 
     assert object_id == expected_id
-    assert name == expected_name
+    assert widget_name == expected_widget_name
+    assert object_name == expected_object_name
