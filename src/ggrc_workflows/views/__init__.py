a67631ca41e2246521e41798e0ceab1b91004d0a
Miha Zidar <zidarsk8@gmail.com>
2016-03-06 15:46:28 +0100

    Lint workflow views init

--- src/ggrc_workflows/views/__init__.py
+++ src/ggrc_workflows/views/__init__.py
@@ -3,9 +3,12 @@
 # Created By: dan@reciprocitylabs.com
 # Maintained By: urban@reciprocitylabs.com
 
+"""Ggrc workflow module views."""
+
 from datetime import date
-from flask import render_template, redirect, url_for
-from jinja2 import Environment, PackageLoader
+from flask import redirect
+from flask import render_template
+from flask import url_for
 
 from ggrc import db
 from ggrc.app import app
@@ -14,26 +17,39 @@ from ggrc.views.cron import run_job
 from ggrc_workflows import start_recurring_cycles
 from ggrc_workflows.models import Workflow
 
-env = Environment(loader=PackageLoader('ggrc', 'templates'))
-
-# TODO: move these views to ggrc_views and all the functions to notifications
-# module.
-
 
 def _get_unstarted_workflows():
+  """Get a list of all workflows that should have a cycle started.
+
+  This function is used for fixing failed cycle starts.
+
+  Returns:
+    list of workflows that are missing any started cycles.
+  """
   return db.session.query(Workflow).filter(
       Workflow.next_cycle_start_date < date.today(),
-      Workflow.recurrences == True,  # noqa
+      Workflow.recurrences.is_(True),
       Workflow.status == 'Active',
   ).all()
 
 
 def unstarted_cycles():
+  """View for showing a list of all cycles that are missing.
+
+  Returns:
+    html string that displays all missing cycles.
+  """
   workflows = _get_unstarted_workflows()
   return render_template("unstarted_cycles.haml", workflows=workflows)
 
 
 def start_unstarted_cycles():
+  """Start missing cycles.
+
+  This function is used for fixing workflow cycles if that  have not been
+  automatically started in the nightly cronjob. It should be manually triggered
+  by a system administrator.
+  """
   workflows = _get_unstarted_workflows()
   for workflow in workflows:
     tasks_start_days = [task.relative_start_day
@@ -62,11 +78,16 @@ def start_unstarted_cycles():
   return redirect(url_for('unstarted_cycles'))
 
 
-def init_extra_views(app):
-  app.add_url_rule(
+def init_extra_views(app_):
+  """Init all views neede for ggrc_workflows module.
+
+  Args:
+    app: current flask application.
+  """
+  app_.add_url_rule(
       "/admin/unstarted_cycles",
       view_func=login_required(unstarted_cycles))
 
-  app.add_url_rule(
+  app_.add_url_rule(
       "/admin/start_unstarted_cycles",
       view_func=login_required(start_unstarted_cycles))
