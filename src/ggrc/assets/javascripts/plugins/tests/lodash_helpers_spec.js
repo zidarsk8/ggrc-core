e72538b6a77f614244aa602fe1f6022b2edd9b2f
Peter Lamut <peter.lamut@niteoweb.com>
2016-03-08 13:22:53 +0100

    Fix tests for the lodash's splitTrim() function

--- src/ggrc/assets/javascripts/plugins/tests/lodash_helpers_spec.js
+++ src/ggrc/assets/javascripts/plugins/tests/lodash_helpers_spec.js
@@ -5,7 +5,7 @@
   Maintained By: ivan@reciprocitylabs.com
 */
 
-describe('_.splitTrim method', function () {
+describe('_.splitTrim() method', function () {
   'use strict';
 
   describe('Given an string with dots and dot splitter', function () {
@@ -14,21 +14,21 @@ describe('_.splitTrim method', function () {
 
     it('return split without spaces', function () {
       var result = _.splitTrim(input, splitter);
-      expect(result).toBe(['a', 'b', 'c', 'd', 'a', '', 'b']);
+      expect(result).toEqual(['a', 'b', 'c', 'd', 'a', 'b', '', 'b']);
     });
 
     it('return unique values without spaces', function () {
       var result = _.splitTrim(input, splitter, {
         unique: true
       });
-      expect(result).toBe(['a', 'b', 'c', 'd', '']);
+      expect(result).toEqual(['a', 'b', 'c', 'd', '']);
     });
 
     it('return compact values without spaces', function () {
       var result = _.splitTrim(input, splitter, {
         compact: true
       });
-      expect(result).toBe(['a', 'b', 'c', 'd', 'a', 'b', 'b']);
+      expect(result).toEqual(['a', 'b', 'c', 'd', 'a', 'b', 'b']);
     });
 
     it('return compact and uniquee values without spaces', function () {
@@ -36,29 +36,30 @@ describe('_.splitTrim method', function () {
         unique: true,
         compact: true
       });
-      expect(result).toBe(['a', 'b', 'c', 'd']);
+      expect(result).toEqual(['a', 'b', 'c', 'd']);
     });
   });
+
   describe('Given an string with commas given default splitter', function () {
     var input = 'a,b,c , d ,c,a  b, , , f';
 
     it('return values without spaces', function () {
       var result = _.splitTrim(input);
-      expect(result).toBe(['a', 'b', 'c', 'd', 'c', 'ab', '', '', 'f']);
+      expect(result).toEqual(['a', 'b', 'c', 'd', 'c', 'a  b', '', '', 'f']);
     });
 
     it('return unique split values without spaces', function () {
       var result = _.splitTrim(input, {
         unique: true
       });
-      expect(result).toBe(['a', 'b', 'c', 'd', 'ab', '', '', 'f']);
+      expect(result).toEqual(['a', 'b', 'c', 'd', 'a  b', '', 'f']);
     });
 
     it('return compact split values without spaces', function () {
       var result = _.splitTrim(input, {
         compact: true
       });
-      expect(result).toBe(['a', 'b', 'c', 'd', 'c', 'ab', 'f']);
+      expect(result).toEqual(['a', 'b', 'c', 'd', 'c', 'a  b', 'f']);
     });
 
     it('return unique and compact split values without spaces', function () {
@@ -66,7 +67,7 @@ describe('_.splitTrim method', function () {
         compact: true,
         unique: true
       });
-      expect(result).toBe(['a', 'b', 'c', 'd', 'ab', 'f']);
+      expect(result).toEqual(['a', 'b', 'c', 'd', 'a  b', 'f']);
     });
   });
 });
