bd162c18a6301d37126b3d6096e69c14488fbf17
Ivan Kriznar <ivan@simplified.me>
2016-03-08 10:54:10 +0100

    Fix migration down version

--- src/ggrc/migrations/versions/20160223152916_204540106539_assessment_titles.py
+++ src/ggrc/migrations/versions/20160223152916_204540106539_assessment_titles.py
@@ -6,7 +6,7 @@
 """Assessment titles
 
 Revision ID: 204540106539
-Revises: 1839dabd2357
+Revises: 1e2abee7566c
 Create Date: 2016-02-23 15:29:16.361412
 
 """
@@ -20,7 +20,7 @@ from ggrc.migrations import utils
 
 # revision identifiers, used by Alembic.
 revision = '204540106539'
-down_revision = '1839dabd2357'
+down_revision = '1e2abee7566c'
 
 
 def upgrade():
