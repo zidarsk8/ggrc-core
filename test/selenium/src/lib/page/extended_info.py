cda2db0e0127e64e8b88bc5435ddcde0e5ff5042
Jernej Makovšek <jernej.makovsek@gmail.com>
2016-03-11 17:47:41 +0100

    Extend extended info model

--- test/selenium/src/lib/page/extended_info.py
+++ test/selenium/src/lib/page/extended_info.py
@@ -6,6 +6,8 @@
 """A module for extended info page models (visible in LHN on hover over
 object members)"""
 
+from selenium.common import exceptions
+
 from lib import base
 from lib.constants import locator
 
@@ -13,9 +15,27 @@ from lib.constants import locator
 class ExtendedInfoMappable(base.Component):
   """Model representing an extended info box that allows the object to be
   mapped"""
+  _locator = locator.ExtendedInfo
   def __init__(self, driver):
     super(ExtendedInfoMappable, self).__init__(driver)
-    self.button_map = base.Button(driver, locator.ExtendedInfo.BUTTON_MAP_TO)
+    self.button_map = None
+    self._reload_contents()
+
+  def _reload_contents(self):
+    self.button_map = base.Button(
+        self._driver, self._locator.BUTTON_MAP_TO)
 
   def map_to_object(self):
-    self.button_map.click()
+    try:
+      self.button_map.click()
+    except exceptions.StaleElementReferenceException:
+      self._reload_contents()
+      return self.map_to_object()
+
+  def is_already_mapped(self):
+    """Checks if the object is already mapped"""
+    try:
+      self._driver.find_element(self._locator.ALREADY_MAPPED)
+      return True
+    except exceptions.NoSuchElementException:
+      return False
