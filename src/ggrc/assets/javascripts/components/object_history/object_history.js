3e0fdc14e30c001611892add6dd21b1690fb4b6c
Peter Lamut <peter.lamut@niteoweb.com>
2016-03-16 20:49:06 +0100

    Fetch and calculate the object's mapping history

--- src/ggrc/assets/javascripts/components/object_history/object_history.js
+++ src/ggrc/assets/javascripts/components/object_history/object_history.js
@@ -133,12 +133,163 @@
       '/components/object_history/object_history.mustache'
     ),
 
+    // TODO: tests for the default values
     scope: {
-      instance: null
+      instance: null,
+      objectChanges: new can.List(),
+      mappingsChanges: new can.List()
     },
 
     helpers: {
       revisions_diff: calculateRevisionsDiff
+    },
+
+    // the type of the object the component is operating on
+    _INSTANCE_TYPE: null,
+
+    /**
+     * The component's entry point. Invoked when a new component instance has
+     * been created.
+     */
+    init: function () {
+      // TODO: check and raise error if instance not set! must be passed to it!
+
+      this._INSTANCE_TYPE = this.scope.instance.type;
+
+      this._fetchRevisionsData(
+        this.scope.instance
+      )
+      .then(function (revisions) {
+        // var objChanges = computeObjectChanges(revisions.object);
+        var mappingsChanges = this._computeMappingChanges(revisions.mappings);
+
+        // this.scope.attr('objectChanges', objChanges);
+        this.scope.attr('mappingChanges', mappingsChanges);
+      }.bind(this));
+    },
+
+    /**
+     * Fetch the instance's Revisions data from the server, including the
+     * Revisions of the instance's mappings.
+     *
+     * The `instance` here refers to the instance of an object currently being
+     * handled by the component.
+     *
+     * @return {can.Deferred} - an object representing the async operation of
+     *   fetching the data from the server. On success it is resolved with an
+     *   object containing the following:
+     *   - {can.List} object - the list of Revisions of the instance itself,
+     *   - {can.List} mappings - the list of Revisions of all the instance's
+     *      mappings
+     */
+    _fetchRevisionsData: function () {
+      // TODO: tests
+      //
+      // TODO: add fail handler
+      // TODO: run run when there is a change of the instance?
+      // TODO: this seems to always run when the UI tab is switched - is there
+      //       an easy way to cache the results? And for how long?
+      var Revision = CMS.Models.Revision;
+
+      var instance = this.scope.instance;
+
+      // TODO: make sure to order by! oldest to newest probably...
+      // __sort: "field1, field2" ... kako pa sicer? - spredaj da je obratno?
+      // AND document in the docstring!
+      var dfd = Revision.findAll({
+        resource_type: this._INSTANCE_TYPE,
+        resource_id: instance.id
+      });
+
+      var dfd2 = Revision.findAll({
+        source_type: this._INSTANCE_TYPE,
+        source_id: instance.id
+      });
+
+      var dfd3 = Revision.findAll({
+        destination_type: this._INSTANCE_TYPE,
+        destination_id: instance.id
+      });
+
+      var dfdResults = can.when(
+        dfd, dfd2, dfd3
+      ).then(function (objRevisions, mappingsSrc, mappingsDest) {
+        var revisions = {
+          object: objRevisions,
+          mappings: mappingsSrc.concat(mappingsDest)
+        };
+        return revisions;
+      });
+
+      return dfdResults;
+    },
+
+    /**
+     * Compute the instance's object mapping-related changes from the list of
+     * mapping revisions.
+     *
+     * @param {can.List} revisions - the list of instance mappings' revision
+     *   history, sorted from oldest to newest.
+     *
+     * @return {can.List} - the history of instance mappings' changes. Each
+     *   element follows the format returned by the `_mappingChange` helper
+     *   method.
+     */
+    _computeMappingChanges: function (revisions) {
+      // TODO: tests
+      var result = _.map(revisions, this._mappingChange.bind(this));
+      return new can.List(result);
+    },
+
+    /**
+     * A helper function for extracting the mapping change information from a
+     * Revision object.
+     *
+     * @param {CMS.Models.Revision} rev - a Revision object describing a
+     *   mapping between the object instance the component is handling, and an
+     *   external object
+     *
+     * @return {can.Map} - A "change" object describing a single modification
+     *   of a mapping. The object has the following attributes:
+     *   - madeBy: the user who made the changes
+     *   - updatedAt: the time when the changes have been made
+     *   - mapping:
+     *       Information about the changed mapping containing the following:
+     *         - action: the mapping action that was performed by a user,
+     *             can be either "Create" or "Delete"
+     *         - relatedObjId: the ID of the object at the other end of the
+     *             mapping (*)
+     *         - relatedObjType: the type of the object at the other end of the
+     *             mapping (*)
+     *
+     *         (*) The object on "this" side of the mapping is of course the
+     *             instance the component is handling.
+     */
+    _mappingChange: function (rev) {
+      // TODO: tests
+      var change = {
+        madeBy: null,
+        updatedAt: null,
+        mapping: {}
+      };
+
+      change.madeBy = 'User ' + rev.modified_by.id;
+      change.updatedAt = rev.updated_at;
+
+      change.mapping.action = _.capitalize(rev.action);
+
+      // The instance the component is handling can be on either side of the
+      // mapping, i.e. source or destination, but we are interested in the
+      // object on the "other" end of the mapping.
+      if (rev.destination_type === this._INSTANCE_TYPE) {
+        change.mapping.relatedObjId = rev.source_id;
+        change.mapping.relatedObjType = rev.source_type;
+      } else {
+        change.mapping.relatedObjId = rev.destination_id;
+        change.mapping.relatedObjType = rev.destination_type;
+      }
+
+      return new can.Map(change);
     }
   });
 })(window.GGRC, window.can);
