6727f4aebcd8a9e187310c51fca07b3ac008ac0c
Miha Zidar <zidarsk8@gmail.com>
2016-03-22 02:55:59 +0100

    Add additional checks to comment notifications

--- test/integration/ggrc/notifications/test_comment_notifications.py
+++ test/integration/ggrc/notifications/test_comment_notifications.py
@@ -90,3 +90,9 @@ class TestCommentNotification(converters.TestCase):
         1,
         "Missing comment notification entry."
     )
+    self.client.get("/_notifications/send_todays_digest")
+    self.assertEqual(
+        self._get_notifications(notif_type="comment_created").count(),
+        0,
+        "Found comment notification that was not sent."
+    )
