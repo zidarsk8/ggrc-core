5ecef933e400fafa8234d778cb92a86590eccc7c
Ivan Kriznar <ivan@simplified.me>
2016-04-05 02:13:36 +0200

    Improve code style

--- src/ggrc/assets/javascripts/models/join_models.js
+++ src/ggrc/assets/javascripts/models/join_models.js
@@ -240,21 +240,22 @@ can.Model.Join("CMS.Models.ObjectPerson", {
 }, {});
 
 can.Model.Join("CMS.Models.ObjectOwner", {
-  root_object : "object_owner"
-  , root_collection : "object_owners"
-  , findAll: "GET /api/object_owners"
-  , create : "POST /api/object_owners"
-  , update : "PUT /api/object_owners/{id}"
-  , destroy : "DELETE /api/object_owners/{id}"
-  , join_keys : {
-    ownable : can.Model.Cacheable
-    , person : CMS.Models.Person
-  }
-  , attributes : {
-      context : "CMS.Models.Context.stub"
-    , modified_by : "CMS.Models.Person.stub"
-    , person : "CMS.Models.Person.stub"
-    , ownable : "CMS.Models.get_stub"
+  root_object: 'object_owner',
+  root_collection: 'object_owners',
+  findOne: 'GET /api/object_owners/{id}',
+  findAll: 'GET /api/object_owners',
+  create: 'POST /api/object_owners',
+  update: 'PUT /api/object_owners/{id}',
+  destroy: 'DELETE /api/object_owners/{id}',
+  join_keys: {
+    ownable: 'can.Model.Cacheable',
+    person: 'CMS.Models.Person'
+  },
+  attributes: {
+    context: 'CMS.Models.Context.stub',
+    modified_by: 'CMS.Models.Person.stub',
+    person: 'CMS.Models.Person.stub',
+    ownable: 'CMS.Models.get_stub',
   }
 
 }, {});
