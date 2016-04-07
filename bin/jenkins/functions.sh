8b9e79801b830a6b83da96b1679bdeb8a420caf3
Jernej Makovšek <jernej.makovsek@gmail.com>
2016-04-08 01:39:14 +0200

    Refactor selenium_test function

--- bin/jenkins/functions.sh
+++ bin/jenkins/functions.sh
@@ -115,8 +115,7 @@ selenium_tests () {
 
   echo "Running Selenium tests"
   docker exec -i ${PROJECT}_selenium_1 sh -c "
-    python /selenium/src/run_selenium.py --junitxml=/selenium/logs/selenium.xml
-  " && rc=$? || rc=$?
+    python /selenium/bin/run_selenium.py" && rc=$? || rc=$?
 
   mv ./test/selenium/logs/selenium.xml ./test/selenium.xml || true
 
