158869d8b4ca644158419ddbc41916f6ba97b4f8
Urban Skudnik <urban.skudnik@gmail.com>
2016-04-27 21:22:22 +0200

    Remove old (2015) migration code style checks

--- src/ggrc/migrations/versions/20151203153139_297131e22e28_add_final_state_to_request_status_and_.py
+++ src/ggrc/migrations/versions/20151203153139_297131e22e28_add_final_state_to_request_status_and_.py
@@ -11,21 +11,23 @@ Create Date: 2015-12-03 15:31:39.979333
 
 """
 
+# pylint: disable=invalid-name,missing-docstring,wrong-import-position
+
+from alembic import op
+
 # revision identifiers, used by Alembic.
 revision = '297131e22e28'
 down_revision = '504f541411a5'
 
-from alembic import op
-
 
 def upgrade():
-  op.execute("""ALTER TABLE requests CHANGE status status ENUM("Unstarted","In Progress","Finished","Verified","Open","Final") NOT NULL;""")
+  op.execute("""ALTER TABLE requests CHANGE status status ENUM("Unstarted","In Progress","Finished","Verified","Open","Final") NOT NULL;""")  # noqa
   op.execute("""UPDATE requests SET status="Open" WHERE status="Unstarted";""")
-  op.execute("""ALTER TABLE requests CHANGE status status ENUM("Open","In Progress","Finished","Verified","Final") NOT NULL;""")
+  op.execute("""ALTER TABLE requests CHANGE status status ENUM("Open","In Progress","Finished","Verified","Final") NOT NULL;""")  # noqa
 
 
 def downgrade():
-  op.execute("""ALTER TABLE requests CHANGE status status ENUM("Open","In Progress","Finished","Verified","Final","Unstarted") NOT NULL;""")
+  op.execute("""ALTER TABLE requests CHANGE status status ENUM("Open","In Progress","Finished","Verified","Final","Unstarted") NOT NULL;""")  # noqa
   op.execute("""UPDATE requests SET status="Unstarted" WHERE status="Open";""")
   op.execute("""UPDATE requests SET status="Finished" WHERE status="Final";""")
-  op.execute("""ALTER TABLE requests CHANGE status status ENUM("Unstarted","In Progress","Finished","Verified") NOT NULL;""")
+  op.execute("""ALTER TABLE requests CHANGE status status ENUM("Unstarted","In Progress","Finished","Verified") NOT NULL;""")  # noqa
