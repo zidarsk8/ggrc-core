a7a99b732d1faec3ee82484d0f33c7c1809200e5
Anže Pečar <ensmotko@gmail.com>
2016-04-28 01:20:08 +0200

    Replace OR with UNION in _join_filter

--- src/ggrc_basic_permissions/__init__.py
+++ src/ggrc_basic_permissions/__init__.py
@@ -173,16 +173,22 @@ def objects_via_relationships_query(model, roles, user_id, context_not_role):
   _user_role = aliased(all_models.UserRole, name="ur")
 
   def _join_filter(query, cond):
-    return query.join(_model, cond).\
-        join(_implications,
-             or_(_model.context_id == _implications.source_context_id,
-                 _model.context_id == _implications.context_id)).\
+    return query.join(_model, cond).join(
+        _implications, _model.context_id == _implications.source_context_id).\
         join(_user_role,
              and_(_user_role.person_id == user_id,
                   _user_role.context_id == _implications.context_id)).\
         join(_role, and_(_user_role.role_id == _role.id,
                          _role.name.in_(roles))).\
-        distinct()
+        distinct().\
+        union(query.join(_model, cond).join(_implications,
+              _model.context_id == _implications.context_id).
+              join(_user_role,
+                   and_(_user_role.person_id == user_id,
+                        _user_role.context_id == _implications.context_id)).
+              join(_role, and_(_user_role.role_id == _role.id,
+                               _role.name.in_(roles))).
+              distinct())
 
   def _add_relationship_join(query):
     # We do a UNION here because using an OR to JOIN both destination
