888b40f135bbd9a2396803a63283a56c582bf28b
Anže Pečar <ensmotko@gmail.com>
2016-04-28 09:58:14 +0200

    Add a docstring to the join filter function

--- src/ggrc_basic_permissions/__init__.py
+++ src/ggrc_basic_permissions/__init__.py
@@ -173,6 +173,15 @@ def objects_via_relationships_query(model, roles, user_id, context_not_role):
   _user_role = aliased(all_models.UserRole, name="ur")
 
   def _join_filter(query, cond):
+    """Filters a query based on user roles
+
+    Args:
+        query: query to be filtered
+        cond: condition used for the initial model query
+
+    Returns:
+        query object with applied conditions
+    """
     user_role_cond = and_(_user_role.person_id == user_id,
                           _user_role.context_id == _implications.context_id)
     role_cond = and_(_user_role.role_id == _role.id,
