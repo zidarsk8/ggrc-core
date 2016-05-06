2c02a47b3187fd0b537a6ff50c9568aced185408
Urban Skudnik <urban.skudnik@gmail.com>
2016-05-06 15:51:33 +0200

    Add Clonable mixin and `clone` method to Audit

--- src/ggrc/models/audit.py
+++ src/ggrc/models/audit.py
@@ -1,13 +1,17 @@
-# Copyright (C) 2013 Google Inc., authors, and contributors <see AUTHORS file>
+# Copyright (C) 2016 Google Inc., authors, and contributors <see AUTHORS file>
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
-# Created By: vraj@reciprocitylabs.com
-# Maintained By: vraj@reciprocitylabs.com
+# Created By: urban@reciprocitylabs.com
+# Maintained By: urban@reciprocitylabs.com
+
+"""Audit model."""
 
 from ggrc import db
 from .mixins import (
     deferred, Timeboxed, Noted, Described, Hyperlinked, WithContact,
     Titled, Slugged, CustomAttributable
 )
+
+from ggrc.models import mixins_clonable
 from ggrc.models.relationship import Relatable
 from ggrc.models.object_person import Personable
 from ggrc.models.context import HasOwnContext
@@ -17,10 +21,12 @@ from ggrc.models.program import Program
 from ggrc.models.person import Person
 
 
-class Audit(
-        CustomAttributable, Personable, HasOwnContext, Relatable,
-        Timeboxed, Noted, Described, Hyperlinked, WithContact, Titled, Slugged,
-        db.Model):
+class Audit(mixins_clonable.Clonable,
+            CustomAttributable, Personable, HasOwnContext, Relatable,
+            Timeboxed, Noted, Described, Hyperlinked, WithContact, Titled,
+            Slugged, db.Model):
+  """Audit model."""
+
   __tablename__ = 'audits'
   _slug_uniqueness = False
 
@@ -28,6 +34,8 @@ class Audit(
       u'Planned', u'In Progress', u'Manager Review',
       u'Ready for External Review', u'Completed'
   )
+
+  CLONEABLE_CHILDREN = {"AssessmentTemplate"}
 
   report_start_date = deferred(db.Column(db.Date), 'Audit')
   report_end_date = deferred(db.Column(db.Date), 'Audit')
@@ -57,7 +65,8 @@ class Audit(
       'program',
       'requests',
       'object_type',
-      PublishOnly('audit_objects')
+      PublishOnly('audit_objects'),
+      PublishOnly("_operation_data")
   ]
 
   _sanitize_html = [
@@ -93,6 +102,83 @@ class Audit(
       "url": None,
       "reference_url": None,
   }
+
+  def _clone(self):
+    """Internal audit cloning.
+
+    Keeps the internals of actual audit cloning and everything that is related
+    to audit itself (auditors, audit firm, context setting,
+    custom attribute values, etc.)
+    """
+    from ggrc_basic_permissions import create_audit_context
+
+    data = {
+        "title": self.generate_attribute("title"),
+        "description": self.description,
+        "audit_firm": self.audit_firm,
+        "start_date": self.start_date,
+        "end_date": self.end_date,
+        "program": self.program,
+        "status": self.VALID_STATES[0],
+        "report_start_date": self.report_start_date,
+        "report_end_date": self.report_end_date,
+        "contact": self.contact
+    }
+
+    audit_copy = Audit(**data)
+    db.session.add(audit_copy)
+    db.session.flush()
+
+    create_audit_context(audit_copy)
+
+    self._clone_auditors(audit_copy)
+    self.clone_custom_attribute_values(audit_copy)
+
+    return audit_copy
+
+  def _clone_auditors(self, audit):
+    """Auditors cloning"""
+    from ggrc_basic_permissions.models import Role, UserRole
+
+    role = Role.query.filter_by(name="Auditor").first()
+    auditors = [ur.person for ur in UserRole.query.filter_by(
+        role=role, context=self.context).all()]
+
+    for auditor in auditors:
+      user_role = UserRole(
+          context=audit.context,
+          person=auditor,
+          role=role
+      )
+      db.session.add(user_role)
+    db.session.flush()
+
+  def clone(self, children=None):
+    """Audit cloning.
+
+    Args:
+      children: A list of related objects that should also be copied and linked
+        to a new audit.
+    """
+    if not children:
+      children = []
+
+    audit_copy = self._clone()
+
+    related_children = self.related_objects(
+        set([c for c in children if c in self.CLONEABLE_CHILDREN]))
+
+    for obj in related_children:
+      obj.clone(audit_copy)
+
+    self._operation_data = {
+        "clone": {
+            "audit": {
+                "id": audit_copy.id
+            }
+        }
+    }
+    db.session.commit()
 
   @classmethod
   def _filter_by_program(cls, predicate):
