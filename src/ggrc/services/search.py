ed868bfd3078fcdc15f0da6a9be2266270cfd700
Andraz Bajt <andraz@bajt.me>
2016-05-20 15:41:55 +0200

    Fix lint warnings in search.py

--- src/ggrc/services/search.py
+++ src/ggrc/services/search.py
@@ -4,11 +4,15 @@
 # Maintained By: david@reciprocitylabs.com
 
 import json
-import ggrc.models.relationship
+
 from flask import request, current_app
+
+import ggrc.models.relationship
+
 from ggrc.fulltext import get_indexer
 from ggrc.utils import GrcEncoder, url_for, benchmark
 from ggrc import db
+
 
 def search():
   terms = request.args.get('q')
@@ -16,17 +20,17 @@ def search():
   permission_model = request.args.get('__permission_model', None)
   if terms is None:
     return current_app.make_response((
-      'Query parameter "q" specifying search terms must be provided.',
-      400,
-      [('Content-Type', 'text/plain')],
-      ))
+        'Query parameter "q" specifying search terms must be provided.',
+        400,
+        [('Content-Type', 'text/plain')],
+    ))
 
   should_group_by_type = request.args.get('group_by_type', '')
   should_group_by_type = should_group_by_type.lower() == 'true'
   should_just_count = request.args.get('counts_only', '')
   should_just_count = should_just_count.lower() == 'true'
 
-  types = request.args.get('types', '');
+  types = request.args.get('types', '')
   types = [t.strip() for t in types.split(',') if len(t.strip()) > 0]
   if len(types) == 0:
     types = None
@@ -40,15 +44,19 @@ def search():
 
   if extra_params:
     # Parse t1:a=b,c=d;t2:e=f into dict {t1:{a:b,c:d},t2:{e:f}}
-    extra_params = {k: {kk: vv for kk, vv in
-                    map(lambda x: x.split('='), v.split(','))} for k, v in
-                    map(lambda x: x.split(':'), extra_params.split(';'))}
+    extra_params = {
+        k: {
+            kk: vv for kk, vv in (x.split('=') for x in v.split(','))
+        } for k, v in (x.split(':') for x in extra_params.split(';'))
+    }
   else:
     extra_params = {}
 
   if extra_columns:
     # Parse a=b,c=d into dict {a:b,c:d}
-    extra_columns = {k: v for k, v in map(lambda x: x.split('='), extra_columns.split(','))}
+    extra_columns = {k: v for k, v in
+                     (x.split('=') for x in extra_columns.split(','))}
+
   else:
     extra_columns = {}
 
@@ -74,52 +82,60 @@ def search():
       relevant_type, relevant_ids
   )
 
-def do_counts(terms, types=None, contact_id=None, extra_params={}, extra_columns={}):
-  from ggrc.rbac import permissions
 
+def do_counts(terms, types=None, contact_id=None,
+              extra_params={}, extra_columns={}):
   # FIXME: ? This would make the query more efficient, but will also prune
   #   objects the user is allowed to read in other contexts.
   # Remove types that the user can't read
-  #types = [type for type in types if permissions.is_allowed_read(type, None)]
+  # types = [type for type in types if permissions.is_allowed_read(type, None)]
 
   indexer = get_indexer()
   with benchmark("Counts"):
     results = indexer.counts(terms, types=types, contact_id=contact_id,
-                             extra_params=extra_params, extra_columns=extra_columns)
+                             extra_params=extra_params,
+                             extra_columns=extra_columns)
 
   results = [(r[2] if r[2] != "" else r[0], r[1]) for r in results]
   return current_app.make_response((
-    json.dumps({ 'results': {
-        'selfLink': request.url,
-        'counts': dict(results)
-        }
+      json.dumps({
+          'results': {
+              'selfLink': request.url,
+              'counts': dict(results)
+          }
       }, cls=GrcEncoder),
-    200,
-    [('Content-Type', 'application/json')],
-    ))
+      200,
+      [('Content-Type', 'application/json')],
+  ))
 
-def do_search(
-    terms, list_for_type, types=None, permission_type='read',
-    permission_model=None, contact_id=None, extra_params=None,
-    relevant_type=None, relevant_ids=None):
+
+def do_search(terms, list_for_type, types=None, permission_type='read',
+              permission_model=None, contact_id=None, extra_params=None,
+              relevant_type=None, relevant_ids=None):
   indexer = get_indexer()
   with benchmark("Search"):
     results = indexer.search(
         terms, types=types, permission_type=permission_type,
-        permission_model=permission_model, contact_id=contact_id, extra_params=extra_params)
+        permission_model=permission_model, contact_id=contact_id,
+        extra_params=extra_params
+    )
 
   related_filter = None
   if relevant_type and relevant_ids:
-    R = ggrc.models.relationship.Relationship
-    src_query = db.session.query(R.source_type, R.source_id).filter(
-        R.source_type.in_(types) | (types is None),
-        R.destination_type == relevant_type,
-        R.destination_id.in_(relevant_ids)
+    relationship = ggrc.models.relationship.Relationship
+    src_query = db.session.query(
+        relationship.source_type, relationship.source_id
+    ).filter(
+        relationship.source_type.in_(types) | (types is None),
+        relationship.destination_type == relevant_type,
+        relationship.destination_id.in_(relevant_ids)
     )
-    dst_query = db.session.query(R.destination_type, R.destination_id).filter(
-        R.destination_type.in_(types) | (types is None),
-        R.source_type == relevant_type,
-        R.source_id.in_(relevant_ids)
+    dst_query = db.session.query(
+        relationship.destination_type, relationship.destination_id
+    ).filter(
+        relationship.destination_type.in_(types) | (types is None),
+        relationship.source_type == relevant_type,
+        relationship.source_id.in_(relevant_ids)
     )
     related_filter = set(src_query.union(dst_query))
 
@@ -130,40 +146,50 @@ def do_search(
     model_type = result.type
     result_pair = (model_type, id)
     if (result_pair not in seen_results and
-        (related_filter is None or result_pair in related_filter)):
+       (related_filter is None or result_pair in related_filter)):
       seen_results[result_pair] = True
       entries_list = list_for_type(model_type)
       entries_list.append({
-        'id': id,
-        'type': model_type,
-        'href': url_for(model_type, id=id),
-        })
+          'id': id,
+          'type': model_type,
+          'href': url_for(model_type, id=id),
+      })
+
 
 def make_search_result(entries):
   return current_app.make_response((
-    json.dumps({ 'results': {
-        'selfLink': request.url,
-        'entries': entries,
-        }
+      json.dumps({
+          'results': {
+              'selfLink': request.url,
+              'entries': entries,
+          }
       }, cls=GrcEncoder),
-    200,
-    [('Content-Type', 'application/json')],
-    ))
+      200,
+      [('Content-Type', 'application/json')],
+  ))
 
-def basic_search(
-    terms, types=None, permission_type='read', permission_model=None,
-    contact_id=None, extra_params=None, relevant_type=None, relevant_ids=None):
+
+def basic_search(terms, types=None,
+                 permission_type='read', permission_model=None,
+                 contact_id=None, extra_params=None,
+                 relevant_type=None, relevant_ids=None):
   entries = []
-  list_for_type = lambda t: entries
+
+  def list_for_type(_):
+    return entries
+
   do_search(terms, list_for_type, types, permission_type, permission_model,
             contact_id, extra_params, relevant_type, relevant_ids)
   return make_search_result(entries)
 
+
 def group_by_type_search(terms, types=None, contact_id=None, extra_params={},
                          relevant_type=None, relevant_ids=None):
   entries = {}
-  list_for_type = \
-      lambda t: entries[t] if t in entries else entries.setdefault(t, [])
+
+  def list_for_type(t):
+    return entries[t] if t in entries else entries.setdefault(t, [])
+
   do_search(terms, list_for_type, types, contact_id=contact_id,
             extra_params=extra_params,
             relevant_type=relevant_type, relevant_ids=relevant_ids)
