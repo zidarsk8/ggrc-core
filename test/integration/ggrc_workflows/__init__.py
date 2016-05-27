e9c32014093af49edc6a12b6db37b44a04d12892
Andraz Brodnik <brodul@brodul.org>
2016-05-27 15:31:15 +0200

    Add WorkflowTestCase to integration tests

--- test/integration/ggrc_workflows/__init__.py
+++ test/integration/ggrc_workflows/__init__.py
@@ -2,3 +2,18 @@
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 # Created By: miha@reciprocitylabs.com
 # Maintained By: miha@reciprocitylabs.com
+from integration.ggrc import TestCase
+from integration.ggrc_workflows.generator import WorkflowsGenerator
+from integration.ggrc.api_helper import Api
+from integration.ggrc.generator import ObjectGenerator
+
+
+class WorkflowTestCase(TestCase):
+
+  def setUp(self):
+    super(WorkflowTestCase, self).setUp()
+    self.api = Api()
+    self.generator = WorkflowsGenerator()
+    self.object_generator = ObjectGenerator()
+
+    self.random_objects = self.object_generator.generate_random_objects()
