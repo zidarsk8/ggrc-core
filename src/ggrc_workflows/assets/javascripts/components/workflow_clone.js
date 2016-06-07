073ebcd7f1d3db43d169132a8ba4efe7f3a8fb23
Ivan Kriznar <ivan@simplified.me>
2016-06-07 17:34:18 +0200

    Move workflow clone component to separate file

---
+++ src/ggrc_workflows/assets/javascripts/components/workflow_clone.js
@@ -0,0 +1,31 @@
+  /*!
+  Copyright (C) 2016 Google Inc., authors, and contributors <see AUTHORS file>
+  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+  Created By: ivan@reciprocitylabs.com
+  Maintained By: ivan@reciprocitylabs.com
+*/
+
+(function (GGRC, can) {
+  'use strict';
+
+  can.Component.extend({
+    tag: "workflow-clone",
+    template: "<content/>",
+    events: {
+      click: function(el) {
+        var workflow, $target;
+
+        $target = $('<div class="modal hide"></div>').uniqueId();
+        $target.modal_form({}, el);
+        $target.ggrc_controllers_modals({
+          modal_title: "Clone Workflow",
+          model: CMS.ModelHelpers.CloneWorkflow,
+          instance: new CMS.ModelHelpers.CloneWorkflow({ source_workflow: this.scope.workflow }),
+          content_view: GGRC.mustache_path + "/workflows/clone_modal_content.mustache",
+          custom_save_button_text: "Proceed",
+          button_view: GGRC.Controllers.Modals.BUTTON_VIEW_SAVE_CANCEL
+        });
+      }
+    }
+  });
+})(window.GGRC, window.can);
--- src/ggrc_workflows/assets/javascripts/controllers/workflow_page.js
+++ src/ggrc_workflows/assets/javascripts/controllers/workflow_page.js
@@ -196,27 +196,6 @@
     }
   });
 
-  can.Component.extend({
-    tag: "workflow-clone",
-    template: "<content/>",
-    events: {
-      click: function(el) {
-        var workflow, $target;
-
-        $target = $('<div class="modal hide"></div>').uniqueId();
-        $target.modal_form({}, el);
-        $target.ggrc_controllers_modals({
-          modal_title: "Clone Workflow",
-          model: CMS.ModelHelpers.CloneWorkflow,
-          instance: new CMS.ModelHelpers.CloneWorkflow({ source_workflow: this.scope.workflow }),
-          content_view: GGRC.mustache_path + "/workflows/clone_modal_content.mustache",
-          custom_save_button_text: "Proceed",
-          button_view: GGRC.Controllers.Modals.BUTTON_VIEW_SAVE_CANCEL
-        });
-      }
-    }
-  });
-
   can.Model.Cacheable("CMS.ModelHelpers.CloneTaskGroup", {
     defaults : {
       clone_objects: true,
