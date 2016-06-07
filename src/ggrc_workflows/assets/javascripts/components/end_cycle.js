0b28c919d852d3b22707dd61268e9c6fb759c812
Ivan Kriznar <ivan@simplified.me>
2016-06-07 17:36:09 +0200

    Move end cycle component to separate file

---
+++ src/ggrc_workflows/assets/javascripts/components/end_cycle.js
@@ -0,0 +1,33 @@
+  /*!
+  Copyright (C) 2016 Google Inc., authors, and contributors <see AUTHORS file>
+  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+  Created By: ivan@reciprocitylabs.com
+  Maintained By: ivan@reciprocitylabs.com
+*/
+
+(function (GGRC, can) {
+  'use strict';
+
+  can.Component.extend({
+    tag: "cycle-end-cycle",
+    template: "<content/>",
+    events: {
+      click: function() {
+        this.scope.cycle.refresh().then(function(cycle) {
+          cycle.attr('is_current', false).save().then(function() {
+            return GGRC.page_instance().refresh();
+          }).then(function(){
+            // We need to update person's assigned_tasks mapping manually
+            var person_id = GGRC.current_user.id,
+                person = CMS.Models.Person.cache[person_id];
+                binding = person.get_binding('assigned_tasks');
+
+            // FIXME: Find a better way of removing stagnant items from the list.
+            binding.list.splice(0, binding.list.length);
+            return binding.loader.refresh_list(binding);
+          });
+        });
+      }
+    }
+  });
+})(window.GGRC, window.can);
--- src/ggrc_workflows/assets/javascripts/controllers/workflow_page.js
+++ src/ggrc_workflows/assets/javascripts/controllers/workflow_page.js
@@ -219,27 +219,4 @@
     }
   });
 
-  can.Component.extend({
-    tag: "cycle-end-cycle",
-    template: "<content/>",
-    events: {
-      click: function() {
-        this.scope.cycle.refresh().then(function(cycle) {
-          cycle.attr('is_current', false).save().then(function() {
-            return GGRC.page_instance().refresh();
-          }).then(function(){
-            // We need to update person's assigned_tasks mapping manually
-            var person_id = GGRC.current_user.id,
-                person = CMS.Models.Person.cache[person_id];
-                binding = person.get_binding('assigned_tasks');
-
-            // FIXME: Find a better way of removing stagnant items from the list.
-            binding.list.splice(0, binding.list.length);
-            return binding.loader.refresh_list(binding);
-          });
-        });
-      }
-    }
-  });
-
 })(this.CMS, this.GGRC, this.can, this.can.$);
