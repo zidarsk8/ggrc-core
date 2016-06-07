9c8fe8eea77cdae5fcc784a402f63354b97f1587
Ivan Kriznar <ivan@simplified.me>
2016-06-07 17:35:09 +0200

    Move taskgroup clone component to separate file

--- src/ggrc_workflows/assets/javascripts/controllers/workflow_page.js
+++ src/ggrc_workflows/assets/javascripts/controllers/workflow_page.js
@@ -220,27 +220,6 @@
   });
 
   can.Component.extend({
-    tag: "task-group-clone",
-    template: "<content/>",
-    events: {
-      click: function(el) {
-        var $target;
-
-        $target = $('<div class="modal hide"></div>').uniqueId();
-        $target.modal_form({}, el);
-        $target.ggrc_controllers_modals({
-          modal_title: "Clone Task Group",
-          model: CMS.ModelHelpers.CloneTaskGroup,
-          instance: new CMS.ModelHelpers.CloneTaskGroup({ source_task_group: this.scope.taskGroup }),
-          content_view: GGRC.mustache_path + "/task_groups/clone_modal_content.mustache",
-          custom_save_button_text: "Proceed",
-          button_view: GGRC.Controllers.Modals.BUTTON_VIEW_SAVE_CANCEL
-        });
-      }
-    }
-  });
-
-  can.Component.extend({
     tag: "cycle-end-cycle",
     template: "<content/>",
     events: {
