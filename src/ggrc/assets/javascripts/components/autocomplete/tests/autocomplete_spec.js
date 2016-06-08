20238ad46ced0635a4b30f4dbcbc9c3857c50d49
Ivan Kriznar <ivan@simplified.me>
2016-06-08 20:38:35 +0200

    Cast disabled property using define plugin

--- src/ggrc/assets/javascripts/components/autocomplete/tests/autocomplete_spec.js
+++ src/ggrc/assets/javascripts/components/autocomplete/tests/autocomplete_spec.js
@@ -24,65 +24,6 @@ describe('GGRC.Components.autocomplete', function () {
     it('sets the automappingOff flag to true', function () {
       expect(scope.automappingOff).toBe(true);
     });
-
-    it('sets the disable flag to false', function () {
-      expect(scope.disable).toBe(false);
-    });
-  });
-
-  describe('init() method', function () {
-    var componentInst;  // fake component instance
-    var element;  // the DOM element passed to the component instance
-    var init;  // the method under test
-    var options;
-
-    beforeEach(function () {
-      element = $('<div></div>')[0];
-      options = {};
-      componentInst = {};
-      componentInst.scope = new can.Map();
-      init = Component.prototype.init.bind(componentInst);
-    });
-
-    it('sets the disable flag to false if the element\'s disable attribute ' +
-      'is empty',
-      function () {
-        $(element).attr('disable', '');
-        componentInst.scope.attr('disable', true);
-        init(element, options);
-        expect(componentInst.scope.disable).toBe(false);
-      }
-    );
-
-    it('sets the disable flag to false if the element\'s disable attribute ' +
-      'has a value false',
-      function () {
-        $(element).attr('disable', 'false');
-        componentInst.scope.attr('disable', true);
-        init(element, options);
-        expect(componentInst.scope.attr('disable')).toBe(false);
-      }
-    );
-
-    it('sets the disable flag to true if the element\'s disable attribute ' +
-      'has a value true',
-      function () {
-        $(element).attr('disable', 'true');
-        componentInst.scope.attr('disable', false);
-        init(element, options);
-        expect(componentInst.scope.attr('disable')).toBe(true);
-      }
-    );
-
-    it('leaves the disable flag unchanged if the element\'s disable ' +
-      'attribute is neither empty nor true/false',
-      function () {
-        $(element).attr('disable', 'whatever');
-        componentInst.scope.attr('disable', true);
-        init(element, options);
-        expect(componentInst.scope.attr('disable')).toBe(true);
-      }
-    );
   });
 
   describe('item selected event handler', function () {
