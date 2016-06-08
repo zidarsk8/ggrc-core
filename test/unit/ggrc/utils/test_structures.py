418efbf1959558d016023def40708f471aff0870
Miha Zidar <zidarsk8@gmail.com>
2016-06-08 13:53:40 +0200

    Add case insensitive dict implementations
    This adds case insensitive dict and default dict implementations that
    should be used when caching objects based on code or title or any
    other case insensitive string id.

---
+++ test/unit/ggrc/utils/test_structures.py
@@ -0,0 +1,96 @@
+# Copyright (C) 2016 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+# Created By: miha@reciprocitylabs.com
+# Maintained By: miha@reciprocitylabs.com
+
+import unittest
+
+from ggrc.utils import structures
+
+
+class TestCaseInsensitiveDict(unittest.TestCase):
+
+  def setUp(self):
+    self.ci_dict = structures.CaseInsensitiveDict()
+
+  def test_basic_dict_functions(self):
+    self.ci_dict["Hello"] = "World"
+    self.assertEqual(self.ci_dict["Hello"], "World")
+    self.ci_dict["empty"] = []
+    self.assertEqual(self.ci_dict["empty"], [])
+    self.ci_dict["EMpTY"].append(56)
+    self.assertEqual(self.ci_dict["EmpTy"], [56])
+
+    def get_(dict_, key):
+      return dict_[key]
+
+    self.assertRaises(KeyError, get_, self.ci_dict, "non existent key")
+    self.assertRaises(KeyError, get_, self.ci_dict, None)
+
+  def test_in_function(self):
+    self.ci_dict["Hello"] = "World"
+    self.assertTrue("Hello" in self.ci_dict)
+    self.assertTrue("hello" in self.ci_dict)
+    self.assertTrue("he" not in self.ci_dict)
+
+  def test_items(self):
+    """Test that items return sames cases as they were set."""
+    self.ci_dict["Hello"] = "World"
+    self.ci_dict["HELLO"] = "World"
+    self.ci_dict["fOO"] = "bar"
+
+    self.assertEqual(
+      sorted(self.ci_dict.items()),
+      sorted([("HELLO", "World"),("fOO", "bar")])
+    )
+
+  def test_lower_items(self):
+    """Test that lower_items does not change values."""
+    self.ci_dict["Hello"] = "World"
+    self.ci_dict["FOO"] = "BAR"
+
+    self.assertEqual(
+      sorted(self.ci_dict.lower_items()),
+      sorted([("hello", "World"),("foo", "BAR")])
+    )
+
+class TestCaseInsensitiveDefDict(unittest.TestCase):
+
+  def setUp(self):
+    self.ci_dict = structures.CaseInsensitiveDefaultDict(list)
+
+  def test_basic_dict_functions(self):
+    self.ci_dict["Hello"] = "World"
+    self.assertEqual(self.ci_dict["Hello"], "World")
+    self.assertEqual(self.ci_dict["empty"], [])
+    self.ci_dict["empty"].append(55)
+    self.assertEqual(self.ci_dict["empty"], [55])
+    self.ci_dict["EMpTY"].append(56)
+    self.assertEqual(self.ci_dict["EmpTy"], [55, 56])
+
+  def test_in_function(self):
+    self.ci_dict["Hello"] = "World"
+    self.assertTrue("Hello" in self.ci_dict)
+    self.assertTrue("hello" in self.ci_dict)
+    self.assertTrue("he" not in self.ci_dict)
+
+  def test_items(self):
+    """Test that items return sames cases as they were set."""
+    self.ci_dict["Hello"] = "World"
+    self.ci_dict["HELLO"] = "World"
+    self.ci_dict["fOO"] = "bar"
+
+    self.assertEqual(
+      sorted(self.ci_dict.items()),
+      sorted([("HELLO", "World"),("fOO", "bar")])
+    )
+
+  def test_lower_items(self):
+    """Test that lower_items does not change values."""
+    self.ci_dict["Hello"] = "World"
+    self.ci_dict["FOO"] = "BAR"
+
+    self.assertEqual(
+      sorted(self.ci_dict.lower_items()),
+      sorted([("hello", "World"),("foo", "BAR")])
+    )
