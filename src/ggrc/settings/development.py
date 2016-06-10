8a3132cc4605e202dc39a18067f2af65876cdeef
Anže Pečar <ensmotko@gmail.com>
2016-06-10 14:38:43 -0700

    Fix DB URI in db_reset and development.py

--- src/ggrc/settings/development.py
+++ src/ggrc/settings/development.py
@@ -10,14 +10,16 @@ TESTING = True
 PRODUCTION = False
 FLASK_DEBUGTOOLBAR = False
 HOST = '0.0.0.0'
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:root@127.0.0.1/ggrcdev?charset=utf8'
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    'GGRC_DATABASE_URI',
+    'mysql+mysqldb://root:root@127.0.0.1/ggrcdev?charset=utf8')
 FULLTEXT_INDEXER = 'ggrc.fulltext.mysql.MysqlIndexer'
 LOGIN_MANAGER = 'ggrc.login.noop'
-#SQLALCHEMY_ECHO = True
-#SQLALCHEMY_RECORD_QUERIES = True
+# SQLALCHEMY_ECHO = True
+# SQLALCHEMY_RECORD_QUERIES = True
 AUTOBUILD_ASSETS = True
 ENABLE_JASMINE = True
-#DEBUG_ASSETS = True
+# DEBUG_ASSETS = True
 USE_APP_ENGINE_ASSETS_SUBDOMAIN = False
 MEMCACHE_MECHANISM = False
 APPENGINE_EMAIL = "user@example.com"
