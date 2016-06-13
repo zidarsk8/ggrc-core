bdcaa5455000b0fc9852fd930abb6064b8a795c0
Miha Zidar <zidarsk8@gmail.com>
2016-06-14 01:43:30 +0200

    Fix getting object state for invalid objects
    The database can contain some cycle tasks without an end_date. That
    causes api calls for any object on that task to fail. The main
    difference in the code here is the check if task.end_date is None right
    before the date < today check. The rest is just a trivial performance
    improvement since we only need to check if any such task exists.
    
    The underlying problem for this will need to be found and solved in a
    different PR. The point if this commit is to make sure invalid database
    state does not break the app.

--- src/ggrc_workflows/models/workflow.py
+++ src/ggrc_workflows/models/workflow.py
@@ -375,9 +375,10 @@ class WorkflowState(object):
     if not current_tasks:
       return None
 
-    overdue_tasks = [task for task in current_tasks
-                     if task.status != "Verified" and
-                     task.end_date <= date.today()]
+    today = date.today()
+    overdue_tasks = any(task.end_date and task.end_date <= today
+                        for task in current_tasks)
+
     if overdue_tasks:
       return "Overdue"
 
