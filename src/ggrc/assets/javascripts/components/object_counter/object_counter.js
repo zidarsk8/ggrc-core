b68b0c078892f33d449864c91229fc6b4e55fdcd
Urban Skudnik <urban.skudnik@gmail.com>
2016-06-16 16:02:08 +0200

    Move object counter processing to backend
    Initial implementation used performed a GET query to backend to
    retrieve the number of user tasks. This moves count operations
    to backend leaving user only to specify which counter to use
    for a certian number.

--- src/ggrc/assets/javascripts/components/object_counter/object_counter.js
+++ src/ggrc/assets/javascripts/components/object_counter/object_counter.js
@@ -9,68 +9,25 @@
   'use strict';
 
   /**
-   *  Component for object counting. You can specify a model, a list of search
-   *  keys and a list of search values. Lengths should be equal and
-   *  key-value pair should be on the same place in the semicolon-separated
-   *  list.
+   *  Component for object counting. This component relies on backend to
+   *  provide specified counter in GGRC.page_object.counts.
    *
-   *  Search values are strings so there's a translation table getValue that
-   *  translates string to value (e.g., 'current_user' gets resolved to
-   *  GGRC.current_user.id) before making a GET request.
-   *
-   *  @param {CMS.Models} Model - Model to perform search on
-   *  @param {string} searchKeys - a list semi-colon separated keys to use for
-   *    searching
-   *  @param {string} searchValues - a list of somi-colon separated values for
-   *  corresponding keys that will be used for searching.
+   *  @param {string} counter - Counter returned by backend in
+   *    GGRC.page_object.counts.
    */
   GGRC.Components('ObjectCounter', {
     tag: 'object-counter',
     template: can.view(GGRC.mustache_path +
       '/components/object_counter/object_counter.mustache'),
     scope: {
-      model: '@',
-      searchKeys: '@',
-      searchValues: '@',
+      counter: '@',
       count: '@',
-      getValue: {
-        current_user: GGRC.current_user.id,
-        'true': true,
-        'false': false
-      },
       updateCount: function () {
-        var searchData = {};
-        var i;
-        var key;
-        var value;
-        var data;
-
-        var model = this.attr('model');
-        var keys = this.attr('searchKeys').split(';');
-        var values = this.attr('searchValues').split(';');
-
-        if (!GGRC.current_user || !GGRC.current_user.id) {
-          return;
+        var counter = this.attr('counter');
+        if (_.isUndefined(GGRC.page_object.counts[counter])) {
+          throw new Error('Specified counter doesn\'t exist.');
         }
-
-        if (keys.length !== values.length) {
-          throw new Error('Search keys and values must be of equal length.');
-        }
-
-        if (_.isUndefined(CMS.Models[model])) {
-          throw new Error('Non-existing model');
-        }
-
-        for (i = 0; i < keys.length; i++) {
-          key = keys[i].trim();
-          value = this.getValue[values[i].trim()] || values[i];
-          searchData[key] = value;
-        }
-
-        data = CMS.Models[model].findAll(searchData);
-        data.done(function (objectList) {
-          this.attr('count', objectList.length);
-        }.bind(this));
+        this.attr('count', GGRC.page_object.counts[counter]);
       }
     },
     events: {
