936ab2dee0e10d1af406b5013a3546e736bb6b0b
Miha Zidar <zidarsk8@gmail.com>
2016-06-19 18:21:22 +0200

    Fix bad license headers

--- src/ggrc/assets/js_specs/models/mappers_spec.js
+++ src/ggrc/assets/js_specs/models/mappers_spec.js
@@ -1,6 +1,6 @@
 /*!
-    Copyright (C) 2016-2015 Google Inc., authors, and contributors
-    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+  Copyright (C) 2016 Google Inc., authors, and contributors
+  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
 describe("mappers", function() {
