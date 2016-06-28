a337390c58fe6000724508953f6631e0b2b4deb2
Miha Zidar <zidarsk8@gmail.com>
2016-06-28 22:13:20 +0200

    Update License headers on new files

--- src/ggrc/migrations/versions/20160626111744_e258ae7cdb5_add_default_value_to_test_plan_procedure.py
+++ src/ggrc/migrations/versions/20160626111744_e258ae7cdb5_add_default_value_to_test_plan_procedure.py
@@ -1,7 +1,5 @@
-# Copyright (C) 2016 Google Inc., authors, and contributors <see AUTHORS file>
+# Copyright (C) 2016 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
-# Created By: dan@reciprocitylabs.com
-# Maintained By: peter@reciprocitylabs.com
 
 """
 Add default value to test_plan_procedure
