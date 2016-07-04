764df1bf0a2dfcfb9c752cda22da6e10e8567138
Urban Skudnik <urban.skudnik@gmail.com>
2016-07-04 15:10:33 +0200

    Guarantee role order by using Map
    Guarantee role order by using Map instead of Object. While previous
    implementation worked in Chrome, Javascript Objects have no guarantee to
    return in the order specified, therefore switch to Map to minimise the
    amount of code changes.
    
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map

--- src/ggrc/assets/javascripts/components/add_comment/add_comment.js
+++ src/ggrc/assets/javascripts/components/add_comment/add_comment.js
@@ -41,14 +41,14 @@
         });
       },
       get_assignee_type: can.compute(function () {
-        // TODO: We prioritize order V > A > R
-        var types = {
-          related_verifiers: 'verifier',
-          related_assessors: 'assessor',
-          related_assignees: 'assignee',
-          related_requesters: 'requester',
-          related_creators: 'creator'
-        };
+        var types = new Map([
+          ['related_verifiers', 'verifier'],
+          ['related_assessors', 'assessor'],
+          ['related_assignees', 'assignee'],
+          ['related_requesters', 'requester'],
+          ['related_creators', 'creator']
+        ]);
+
         var instance = this.attr('parent_instance');
         var user = GGRC.current_user;
         var user_type;
@@ -56,7 +56,7 @@
         if (!instance || !user) {
           return;
         }
-        _.each(types, function (type, mapping) {
+        types.forEach(function (type, mapping) {
           var mappings = instance.get_mapping(mapping);
           if (!mappings.length) {
             return;
