9c7c8c2ebe6450e3e9ec878ece4f7b0435e597b0
Alexander Novik <al.novik88@gmail.com>
2016-07-11 12:07:02 +0300

    Fix linting

--- src/ggrc/assets/javascripts/controllers/automapper_controller.js
+++ src/ggrc/assets/javascripts/controllers/automapper_controller.js
@@ -5,38 +5,39 @@
     Maintained By: andraz@reciprocitylabs.com
 */
 
-;(function(CMS, GGRC, can, $) {
-  var MIN_WAIT = 2000,
-      MAX_WAIT = 5000;
+;(function (CMS, GGRC, can, $) {
+  var MIN_WAIT = 200;
+  var MAX_WAIT = 5000;
 
   var refresher = {
     instanceQueue: [],
     lastRefresh: Date.now(),
-    refreshInstance: function(instance) {
+    refreshInstance: function (instance) {
       this.instanceQueue.push(instance);
       setTimeout(this.tick(instance), MIN_WAIT);
     },
-    tick: function(instance) {
-      return (function(){
-        var isCurrent = this.instanceQueue[this.instanceQueue.length - 1] === instance;
+    tick: function (instance) {
+      return (function () {
+        var isCurrent =
+          this.instanceQueue[this.instanceQueue.length - 1] === instance;
         var isOverdue = (this.lastRefresh + MAX_WAIT) < Date.now();
         if (isCurrent || isOverdue) {
           this.go();
         }
       }).bind(this);
     },
-    go: function() {
-      this.lastRefresh = Date.now();
+    go: function () {
       var instances = this.instanceQueue;
-      this.instanceQueue = [];
-
-      var ids = $.map(instances, function(instance) {
+      var ids = $.map(instances, function (instance) {
         return instance.id;
       });
       var filter = {automapping_id__in: ids.join()};
-      CMS.Models.Relationship.findAll(filter).then(function(relationships) {
+      this.lastRefresh = Date.now();
+      this.instanceQueue = [];
+
+      CMS.Models.Relationship.findAll(filter).then(function (relationships) {
         var rq = new RefreshQueue();
-        can.each(relationships.concat(instances), function(relationship) {
+        can.each(relationships.concat(instances), function (relationship) {
           rq.enqueue(relationship.source);
           rq.enqueue(relationship.destination);
         });
@@ -45,21 +46,24 @@
     }
   };
 
-  var flashWarning = function() {
-    // timeout is required because a "mapping created" success flash will show up
+  var flashWarning = function () {
+    // timeout is required because a 'mapping created' success flash will show up
     // and we do not currently support multiple simultaneous flashes
-    setTimeout(function() {
-      $(document.body).trigger("ajax:flash", {
-        "warning": "Automatic mappings were not created because that would result in too many new mappings"
+    setTimeout(function () {
+      $(document.body).trigger('ajax:flash', {
+        warning: 'Automatic mappings were not created because that would ' +
+        'result in too many new mappings'
       });
     }, 2000); // 2000 is a magic number that feels nice in the UI
   };
 
   var Controller = can.Control({
-    "{CMS.Models.Relationship} created": function(model, ev, instance) {
+    '{CMS.Models.Relationship} created': function (model, ev, instance) {
+      var limitExceeded;
       if (instance instanceof CMS.Models.Relationship) {
-        var limit_exceeded = instance.extras && instance.extras.automapping_limit_exceeded;
-        if (limit_exceeded) {
+        limitExceeded = instance.extras &&
+          instance.extras.automapping_limit_exceeded;
+        if (limitExceeded) {
           flashWarning();
         } else {
           refresher.refreshInstance(instance);
@@ -68,7 +72,7 @@
     }
   });
 
-  $(function() {
+  $(function () {
     new Controller();
   });
 })(this.CMS, this.GGRC, this.can, this.can.$);
