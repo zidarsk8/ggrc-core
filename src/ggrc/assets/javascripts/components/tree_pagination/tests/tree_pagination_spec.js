cd9241b2daef82e16ff33874aac1bc7738bffcdb
Egor Miasnikov <e.miasnikov@gmail.com>
2016-07-22 12:15:17 +0300

    Add pagination component

---
+++ src/ggrc/assets/javascripts/components/tree_pagination/tests/tree_pagination_spec.js
@@ -0,0 +1,138 @@
+/*!
+  Copyright (C) 2016 Google Inc.
+  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+describe('GGRC.Components.treePagination', function () {
+  'use strict';
+  var helpers;
+  var scope;
+
+  beforeAll(function () {
+    var Component = GGRC.Components.get('treePagination');
+    helpers = Component.prototype.helpers;
+    scope = Component.prototype.scope;
+  });
+
+  beforeEach(function () {
+    scope.paging = new can.Map({
+      current: 1,
+      pageSize: 10,
+      count: 3
+    });
+  });
+
+  describe('paginationInfo() method in helpers', function () {
+    it('returns info about visible items on page', function () {
+      var result;
+      result = helpers.paginationInfo(15, 10, 3000);
+      expect(result).toEqual('141-150 of 3000 items');
+    });
+
+    it('returns "No records" if we don\'t have elements', function () {
+      var result;
+      result = helpers.paginationInfo(1, 0, 0);
+      expect(result).toEqual('No records');
+    });
+  });
+  describe('paginationPlaceholder() method in helpers', function () {
+    it('returns placeholder into page input', function () {
+      var result;
+      result = helpers.paginationPlaceholder(2, 10);
+      expect(result).toEqual('Page 2 of 10');
+    });
+    it('returns empty string if we don\'t have amount of pages', function () {
+      var result;
+      result = helpers.paginationPlaceholder(2, 0);
+      expect(result).toEqual('');
+    });
+    it('returns "Wrong value" if current page bigger than amount of pages',
+      function () {
+        var result;
+        result = helpers.paginationPlaceholder(10, 2);
+        expect(result).toEqual('Wrong value');
+      });
+  });
+  describe('setNextPage() method ', function () {
+    it('changes current and increase it by 1', function () {
+      scope.setNextPage();
+      expect(scope.paging.current).toEqual(2);
+    });
+    it('doesn\'t change current value if current equal amount of pages',
+      function () {
+        scope.paging.attr('current', 3);
+        scope.setNextPage();
+        expect(scope.paging.current).toEqual(3);
+      });
+  });
+  describe('setPrevPage() method ', function () {
+    it('changes current and decrease it by 1', function () {
+      scope.paging.attr('current', 2);
+      scope.setPrevPage();
+      expect(scope.paging.current).toEqual(1);
+    });
+    it('doesn\'t change current value if current equal 1',
+      function () {
+        scope.paging.attr('current', 1);
+        scope.setPrevPage();
+        expect(scope.paging.current).toEqual(1);
+      });
+  });
+  describe('setFirstPage() method ', function () {
+    it('changes current if current more than 1', function () {
+      scope.paging.attr('current', 3);
+      scope.setFirstPage();
+      expect(scope.paging.current).toEqual(1);
+    });
+  });
+  describe('setLastPage() method ', function () {
+    it('changes current if current less than amount of pages', function () {
+      scope.paging.attr('current', 2);
+      scope.setLastPage();
+      expect(scope.paging.current).toEqual(3);
+    });
+  });
+  describe('changePageSize() method ', function () {
+    it('changes current to first page and page size', function () {
+      scope.paging.attr('current', 2);
+      expect(scope.paging.pageSize).toEqual(10);
+      scope.changePageSize(25);
+      expect(scope.paging.pageSize).toEqual(25);
+      expect(scope.paging.current).toEqual(1);
+    });
+  });
+  describe('setCurrentPage() method ', function () {
+    var input;
+    beforeEach(function () {
+      input = {
+        value: null,
+        val: function () {
+          return this.value;
+        },
+        blur: jasmine.createSpy()
+      };
+    });
+    it('changes current if value more than 1 and less than amount of pages',
+      function () {
+        input.value = 2;
+        scope.setCurrentPage({}, input);
+        expect(scope.paging.current).toEqual(2);
+      });
+    it('changes current to 1 if value less than 1', function () {
+      input.value = -1;
+      scope.setCurrentPage({}, input);
+      expect(scope.paging.current).toEqual(1);
+    });
+    it('changes current to last if value more than amount of pages',
+      function () {
+        input.value = 5;
+        scope.setCurrentPage({}, input);
+        expect(scope.paging.current).toEqual(3);
+      });
+    it('changes current to 1 if value is NaN', function () {
+      input.value = 'test word';
+      scope.setCurrentPage({}, input);
+      expect(scope.paging.current).toEqual(1);
+    });
+  });
+});
---
+++ src/ggrc/assets/javascripts/components/tree_pagination/tree_pagination.js
@@ -0,0 +1,113 @@
+/*!
+ Copyright (C) 2016 Google Inc.
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
+/**
+ * A component that renders a tree pagination widget
+ * Usage: <tree-pagination paging="paging"></tree-pagination>
+ */
+(function (GGRC, can) {
+  GGRC.Components('treePagination', {
+    tag: 'tree-pagination',
+    template: can.view(
+      GGRC.mustache_path +
+      '/components/tree_pagination/tree_pagination.mustache'
+    ),
+    init: function () {
+      /**
+       * Entrance object validation
+       *
+       * paging = {
+       *  current: {Number}, - current page number
+       *  pageSize: {Number}, - amount elements on the page
+       *  total: {Number}, - total amount of elements
+       *  count: {Number}, - total amount of pages
+       *  pageSizeSelect: {Array} - array of numbers that used for pageSize popover
+       * }
+       */
+      if (!this.scope.attr('paging')) {
+        throw new Error('Paging object didn\'t init');
+      }
+    },
+    scope: {
+      /**
+       * Gets value from input and after validation set it to paging.current
+       * @param {Object} object - paging object
+       * @param {Object} input - DOM element
+       */
+      setCurrentPage: function (object, input) {
+        var _value = parseInt(input.val(), 10);
+        var _page = Math.min(Math.max(_value, 1) || 1, this.paging.count);
+
+        this.paging.attr('current', _page);
+        input.val('');
+        input.blur();
+      },
+      changePageSize: function (size) {
+        this.paging.attr('pageSize', size);
+        this.paging.attr('current', 1);
+      },
+      setLastPage: function () {
+        if (this.paging.current < this.paging.count) {
+          this.paging.attr('current', this.paging.count);
+        }
+      },
+      setFirstPage: function () {
+        if (this.paging.current > 1) {
+          this.paging.attr('current', 1);
+        }
+      },
+      setPrevPage: function () {
+        if (this.paging.current > 1) {
+          this.paging.attr('current', this.paging.current - 1);
+        }
+      },
+      setNextPage: function () {
+        if (this.paging.current < this.paging.count) {
+          this.paging.attr('current', this.paging.current + 1);
+        }
+      }
+    },
+    helpers: {
+      /**
+       * @param {Number|String} current - current page number
+       * @param {Number|String} count - total amount of pages
+       * @return {string} pagination placeholder
+       */
+      paginationPlaceholder: function (current, count) {
+        var _radix = 10;
+        var _current = parseInt(Mustache.resolve(current), _radix);
+        var _count = parseInt(Mustache.resolve(count), _radix);
+
+        if (_count && _count >= _current) {
+          return 'Page ' + _current + ' of ' + _count;
+        } else if (!_count) {
+          return '';
+        }
+
+        return 'Wrong value';
+      },
+      /**
+       * @param {Number|String} current - current page number
+       * @param {Number|String} size - amount elements on the page
+       * @param {Number|String} total - total amount of elements
+       * @return {string} - pagination info
+       */
+      paginationInfo: function (current, size, total) {
+        var _radix = 10;
+        var _first;
+        var _last;
+        var _current = parseInt(Mustache.resolve(current), _radix);
+        var _size = parseInt(Mustache.resolve(size), _radix);
+        var _total = parseInt(Mustache.resolve(total), _radix);
+
+        _first = (_current - 1) * _size + 1;
+        _last = _current * _size < _total ? _current * _size : _total;
+
+        return _last ? _first + '-' + _last + ' of ' + _total + ' items' :
+          'No records';
+      }
+    }
+  });
+})(window.GGRC, window.can);
