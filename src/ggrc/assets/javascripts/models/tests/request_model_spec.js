35abfb13c8f8f0739957a086d288efc8d1d61d24
Peter Lamut <peter.lamut@niteoweb.com>
2016-07-27 14:06:16 +0200

    Add a test and docstring for Request.display_name

---
+++ src/ggrc/assets/javascripts/models/tests/request_model_spec.js
@@ -0,0 +1,34 @@
+/*!
+  Copyright (C) 2016 Google Inc.
+  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+describe('CMS.Models.Request', function () {
+  'use strict';
+
+  var Model;
+
+  beforeAll(function () {
+    Model = CMS.Models.Request;
+  });
+
+  describe('display_name() method', function () {
+    var method;  // the method under test
+    var instance;
+
+    beforeEach(function () {
+      instance = new can.Map({
+        title: 'Request 18',
+        id: 18
+      });
+      method = Model.prototype.display_name.bind(instance);
+    });
+
+    it('returns a generic name for deleted objects', function () {
+      var result;
+      instance.attr('title', undefined);
+      result = method();
+      expect(result).toEqual('"Request 18" (DELETED)');
+    });
+  });
+});
