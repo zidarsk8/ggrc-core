c2576275ef34cd810ae74833f46432e07db81fe1
Ivan Kriznar <ivan@simplified.me>
2016-08-04 20:53:07 +0200

    Update tests for mappable types

--- src/ggrc/assets/javascripts/plugins/tests/ggrc_utils_spec.js
+++ src/ggrc/assets/javascripts/plugins/tests/ggrc_utils_spec.js
@@ -3,9 +3,9 @@
   Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
-describe('GGRC utils allowed_to_map() method', function () {
-  'use strict';
+'use strict';
 
+describe('GGRC utils allowed_to_map() method', function () {
   var allowedToMap;
   var fakeOptions;
   var fakeProgram;
@@ -91,8 +91,6 @@ describe('GGRC utils allowed_to_map() method', function () {
 });
 
 describe('GGRC utils isEmptyCA() method', function () {
-  'use strict';
-
   var isEmptyCA;
 
   beforeAll(function () {
@@ -220,3 +218,79 @@ describe('GGRC utils getRelatedObjects() method', function () {
   });
 });
 
+describe('GGRC utils getMappableTypes() method', function () {
+  var mapper;
+
+  beforeAll(function () {
+    var canonicalMappings = {};
+    var OBJECT_TYPES = [
+      'DataAsset', 'Facility', 'Market', 'OrgGroup', 'Vendor', 'Process',
+      'Product', 'Project', 'System', 'Regulation', 'Policy', 'Contract',
+      'Standard', 'Program', 'Issue', 'Control', 'Section', 'Clause',
+      'Objective', 'Audit', 'Assessment', 'AccessGroup', 'Request',
+      'Document', 'Risk', 'Threat'
+    ];
+    mapper = GGRC.Utils.getMappableTypes;
+    OBJECT_TYPES.forEach(function (item) {
+      canonicalMappings[item] = {};
+    });
+    spyOn(GGRC.Mappings, 'get_canonical_mappings_for')
+      .and.returnValue(canonicalMappings);
+  });
+
+  it('excludes the References type from the result', function () {
+    var result = mapper('Reference');
+    expect(_.contains(result, 'Reference')).toBe(false);
+  });
+  it('returns no results for Person', function () {
+    var result = mapper('Person');
+    expect(result.length).toBe(0);
+  });
+  it('returns no results for AssessmentTemplate', function () {
+    var result = mapper('AssessmentTemplate');
+    expect(result.length).toBe(0);
+  });
+  it('always returns whitelisted items', function () {
+    var whitelisted = ['Hello', 'World'];
+    var result = mapper('AssessmentTemplate', {
+      whitelist: whitelisted
+    });
+    expect(_.intersection(result, whitelisted)).toEqual(whitelisted);
+  });
+  it('always remove forbidden items', function () {
+    var forbidden = ['Policy', 'Process', 'Product', 'Program'];
+    var list = mapper('DataAsset');
+    var result = mapper('DataAsset', {
+      forbidden: forbidden
+    });
+    expect(_.difference(list, result).sort()).toEqual(forbidden.sort());
+  });
+  it('always leave whitelisted and remove forbidden items', function () {
+    var forbidden = ['Policy', 'Process', 'Product', 'Program'];
+    var whitelisted = ['Hello', 'World'];
+    var list = mapper('DataAsset');
+    var result = mapper('DataAsset', {
+      forbidden: forbidden,
+      whitelist: whitelisted
+    });
+    var input = _.difference(list, result).concat(_.difference(result, list));
+    var output = forbidden.concat(whitelisted);
+
+    expect(input.sort()).toEqual(output.sort());
+  });
+});
+
+describe('GGRC utils isMappableType() method', function () {
+  it('returns false for Person and any type', function () {
+    var result = GGRC.Utils.isMappableType('Person', 'Program');
+    expect(result).toBe(false);
+  });
+  it('returns false for AssessmentTemplate and  any type', function () {
+    var result = GGRC.Utils.isMappableType('AssessmentTemplate', 'Program');
+    expect(result).toBe(false);
+  });
+  it('returns true for Program and Control', function () {
+    var result = GGRC.Utils.isMappableType('Program', 'Control');
+    expect(result).toBe(true);
+  });
+});
