4d3af5f1cd6e7af7f34a49e94ca8ecce7cd79ce7
Alieh Rymasheuski <alieh.rymasheuski@gmail.com>
2016-08-08 13:01:32 +0300

    Add a TODO for setting `attribute_object` to None

--- src/ggrc/models/custom_attribute_value.py
+++ src/ggrc/models/custom_attribute_value.py
@@ -70,8 +70,9 @@ class CustomAttributeValue(Base, db.Model):
         value: model instance
     """
     if value is None:
-      # We get here if a CustomAttributeValue is POSTed via the REST API.
-      # As no attribute_object manipulations make sense if it is None, return.
+      # We get here if "attribute_object" does not get resolved.
+      # TODO: make sure None value can be set for removing CA attribute object
+      # value
       return
     self.attribute_object_id = value.id
     return setattr(self, self._attribute_object_attr, value)
