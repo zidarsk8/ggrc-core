ba525da9143a20522fa9caf7f300af95efbe9bd8
akaralionak <akaralionak@google.com>
2016-08-09 17:07:54 +0300

    Create Collapsible-Panel Component

---
+++ src/ggrc/assets/javascripts/components/collapsible-panel/collapsible-panel-header.js
@@ -0,0 +1,27 @@
+/*!
+ Copyright (C) 2016 Google Inc., authors, and contributors
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
+(function (can, GGRC) {
+  'use strict';
+
+  var tag = 'collapsible-panel-header';
+  var tpl = can.view(GGRC.mustache_path +
+    '/components/collapsible-panel/collapsible-panel-header.mustache');
+  /**
+   * Collapsible Panel component to add collapsing behavior
+   */
+  GGRC.Components('collapsiblePanelHeader', {
+    tag: tag,
+    template: tpl,
+    scope: {
+      content: '<content></content>',
+      titleIcon: null,
+      expanded: null,
+      toggle: function () {
+        this.attr('expanded', !this.attr('expanded'));
+      }
+    }
+  });
+})(window.can, window.GGRC);
---
+++ src/ggrc/assets/javascripts/components/collapsible-panel/collapsible-panel.js
@@ -0,0 +1,29 @@
+/*!
+ Copyright (C) 2016 Google Inc., authors, and contributors
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
+(function (can, GGRC) {
+  'use strict';
+
+  var tag = 'collapsible-panel';
+  var tpl = can.view(GGRC.mustache_path +
+    '/components/collapsible-panel/collapsible-panel.mustache');
+  /**
+   * Collapsible Panel component to add collapsing behavior
+   */
+  GGRC.Components('collapsiblePanel', {
+    tag: tag,
+    template: tpl,
+    scope: {
+      titleText: '@',
+      titleIcon: '@',
+      content: '<content></content>',
+      expanded: true,
+      collapsed: '@'
+    },
+    init: function () {
+      this.scope.attr('expanded', !(this.scope.attr('collapsed')));
+    }
+  });
+})(window.can, window.GGRC);
