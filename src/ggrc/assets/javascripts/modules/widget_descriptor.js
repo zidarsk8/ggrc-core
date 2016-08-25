6818a77a8942254f1c5a8964fc259cb8bfdae5d3
Peter Lamut <peter.lamut@niteoweb.com>
2016-08-25 14:20:45 +0200

    ESLint widget list and widget descriptor files

--- src/ggrc/assets/javascripts/modules/widget_descriptor.js
+++ src/ggrc/assets/javascripts/modules/widget_descriptor.js
@@ -4,7 +4,7 @@
 */
 
 (function ($, CMS, GGRC) {
-  //A widget descriptor has the minimum five properties:
+  // A widget descriptor has the minimum five properties:
   // widget_id:  the unique ID string for the widget
   // widget_name: the display title for the widget in the UI
   // widget_icon: the CSS class for the widget in the UI
@@ -12,30 +12,31 @@
   // content_controller_options: options passed directly to the content controller; the
   //   precise options depend on the controller itself.  They usually require instance
   //   and/or model and some view.
-  can.Construct("GGRC.WidgetDescriptor", {
+  can.Construct('GGRC.WidgetDescriptor', {
     /*
       make an info widget descriptor for a GGRC object
       You must provide:
       instance - an instance that is a subclass of can.Model.Cacheable
-      widget_view [optional] - a template for rendering the info.
+      widgetView [optional] - a template for rendering the info.
     */
-    make_info_widget: function (instance, widget_view) {
-      var default_info_widget_view = GGRC.mustache_path + "/base_objects/info.mustache";
+    make_info_widget: function (instance, widgetView) {
+      var defaultInfoWidgetView = GGRC.mustache_path +
+                                     '/base_objects/info.mustache';
       return new this(
-        instance.constructor.shortName + ":info", {
-          widget_id: "info",
+        instance.constructor.shortName + ':info', {
+          widget_id: 'info',
           widget_name: function () {
-            if (instance.constructor.title_singular === 'Person')
+            if (instance.constructor.title_singular === 'Person') {
               return 'Info';
-            else
-              return instance.constructor.title_singular + " Info";
+            }
+            return instance.constructor.title_singular + ' Info';
           },
-          widget_icon: "info-circle",
+          widget_icon: 'info-circle',
           content_controller: GGRC.Controllers.InfoWidget,
           content_controller_options: {
             instance: instance,
             model: instance.constructor,
-            widget_view: widget_view || default_info_widget_view
+            widget_view: widgetView || defaultInfoWidgetView
           },
           order: 0
         });
@@ -44,44 +45,51 @@
       make a tree view widget descriptor with mostly default-for-GGRC settings.
       You must provide:
       instance - an instance that is a subclass of can.Model.Cacheable
-      far_model - a can.Model.Cacheable class
+      farModel - a can.Model.Cacheable class
       mapping - a mapping object taken from the instance
       extenders [optional] - an array of objects that will extend the default widget config.
     */
-    make_tree_view: function (instance, far_model, mapping, extenders) {
+    make_tree_view: function (instance, farModel, mapping, extenders) {
       var descriptor = {
         content_controller: CMS.Controllers.TreeView,
-        content_controller_selector: "ul",
+        content_controller_selector: 'ul',
         widget_initial_content: '<ul class="tree-structure new-tree"></ul>',
-        widget_id: far_model.table_singular,
+        widget_id: farModel.table_singular,
         widget_guard: function () {
-          if (far_model.title_plural === "Audits" && instance instanceof CMS.Models.Program) {
-            return "context" in instance && !!(instance.context.id);
+          if (
+            farModel.title_plural === 'Audits' &&
+            instance instanceof CMS.Models.Program
+          ) {
+            return 'context' in instance && !!(instance.context.id);
           }
           return true;
         },
         widget_name: function () {
-          var $objectArea = $(".object-area");
-          if ($objectArea.hasClass("dashboard-area") || instance.constructor.title_singular === "Person") {
+          var $objectArea = $('.object-area');
+          if (
+            $objectArea.hasClass('dashboard-area') ||
+            instance.constructor.title_singular === 'Person'
+          ) {
             if (/dashboard/.test(window.location)) {
-              return "My " + far_model.title_plural;
-            } else {
-              return far_model.title_plural;
+              return 'My ' + farModel.title_plural;
             }
-          } else if (far_model.title_plural === "Audits") {
-            return "Mapped Audits";
-          } else {
-            return (far_model.title_plural === "References" ? "Linked " : "Mapped ") + far_model.title_plural;
+            return farModel.title_plural;
+          } else if (farModel.title_plural === 'Audits') {
+            return 'Mapped Audits';
           }
+          return (
+            farModel.title_plural === 'References' ?
+                                         'Linked ' : 'Mapped '
+          ) + farModel.title_plural;
         },
-        widget_icon: far_model.table_singular,
-        object_category: far_model.category || 'default',
-        model: far_model,
+        widget_icon: farModel.table_singular,
+        object_category: farModel.category || 'default',
+        model: farModel,
         content_controller_options: {
           child_options: [],
           draw_children: true,
           parent_instance: instance,
-          model: far_model,
+          model: farModel,
           list_loader: function () {
             return mapping.refresh_list();
           }
@@ -90,11 +98,14 @@
 
       $.extend.apply($, [true, descriptor].concat(extenders || []));
 
-      return new this(instance.constructor.shortName + ":" + far_model.table_singular, descriptor);
+      return new this(
+        instance.constructor.shortName + ':' + farModel.table_singular,
+        descriptor
+      );
     },
     newInstance: function (id, opts) {
       var ret;
-      if (!opts && typeof id === "object") {
+      if (!opts && typeof id === 'object') {
         opts = id;
         id = opts.widget_id;
       }
@@ -108,12 +119,12 @@
           GGRC.widget_descriptors[id] = ret;
         }
         return GGRC.widget_descriptors[id];
-      } else {
-        ret = this._super.apply(this, arguments);
-        $.extend(ret, opts);
-        GGRC.widget_descriptors[id] = ret;
-        return ret;
       }
+
+      ret = this._super.apply(this, arguments);
+      $.extend(ret, opts);
+      GGRC.widget_descriptors[id] = ret;
+      return ret;
     }
   }, {});
 })(this.can.$, this.CMS, this.GGRC);
--- src/ggrc/assets/javascripts/modules/widget_list.js
+++ src/ggrc/assets/javascripts/modules/widget_list.js
@@ -17,20 +17,22 @@
     See the comments for GGRC.WidgetDescriptor for details in what is necessary to define
     a widget descriptor.
   */
-  can.Construct("GGRC.WidgetList", {
+  can.Construct('GGRC.WidgetList', {
     modules: {},
     /*
       get_widget_list_for: return a keyed object of widget descriptors for the specified page type.
 
-      page_type - one of a GGRC object model's shortName, or "admin"
+      pageType - one of a GGRC object model's shortName, or "admin"
 
       The widget descriptors are built on the first call of this function; subsequently they are retrieved from the
        widget descriptor cache.
     */
-    get_widget_list_for: function (page_type) {
+    get_widget_list_for: function (pageType) {
       var widgets = {};
+      var descriptors = {};
+
       can.each(this.modules, function (module) {
-        can.each(module[page_type], function (descriptor, id) {
+        can.each(module[pageType], function (descriptor, id) {
           if (!widgets[id]) {
             widgets[id] = descriptor;
           } else {
@@ -38,43 +40,47 @@
           }
         });
       });
-      var descriptors = {};
-      can.each(widgets, function (widget, widget_id) {
+
+      can.each(widgets, function (widget, widgetId) {
         switch (widget.content_controller) {
-        case GGRC.Controllers.InfoWidget:
-          descriptors[widget_id] = GGRC.WidgetDescriptor.make_info_widget(
-            widget.content_controller_options && widget.content_controller_options.instance || widget.instance,
-            widget.content_controller_options && widget.content_controller_options.widget_view || widget.widget_view
-          );
-          break;
-        case GGRC.Controllers.TreeView:
-          descriptors[widget_id] = GGRC.WidgetDescriptor.make_tree_view(
-            widget.content_controller_options && (widget.content_controller_options.instance || widget.content_controller_options.parent_instance) || widget.instance,
-            widget.content_controller_options && widget.content_controller_options.model || widget.far_model || widget.model,
-            widget.content_controller_options && widget.content_controller_options.mapping || widget.mapping,
-            widget
-          );
-          break;
-        default:
-          descriptors[widget_id] = new GGRC.WidgetDescriptor(page_type + ":" + widget_id, widget);
+          case GGRC.Controllers.InfoWidget:
+            descriptors[widgetId] = GGRC.WidgetDescriptor.make_info_widget(
+              widget.content_controller_options && widget.content_controller_options.instance || widget.instance,
+              widget.content_controller_options && widget.content_controller_options.widget_view || widget.widget_view
+            );
+            break;
+          case GGRC.Controllers.TreeView:
+            descriptors[widgetId] = GGRC.WidgetDescriptor.make_tree_view(
+              widget.content_controller_options && (widget.content_controller_options.instance || widget.content_controller_options.parent_instance) || widget.instance,
+              widget.content_controller_options && widget.content_controller_options.model || widget.far_model || widget.model,
+              widget.content_controller_options && widget.content_controller_options.mapping || widget.mapping,
+              widget
+            );
+            break;
+          default:
+            descriptors[widgetId] = new GGRC.WidgetDescriptor(
+                pageType + ':' + widgetId, widget);
         }
       });
+
       can.each(descriptors, function (descriptor, id) {
         if (descriptor.suppressed) {
           delete descriptors[id];
         }
       });
+
       return descriptors;
     },
     /*
       returns a keyed object of widget descriptors that represents the current page.
     */
     get_current_page_widgets: function () {
-      return this.get_widget_list_for(GGRC.page_instance().constructor.shortName);
+      return this.get_widget_list_for(
+        GGRC.page_instance().constructor.shortName);
     },
     get_default_widget_sort: function () {
       return this.sort;
-    },
+    }
   }, {
     init: function (name, opts, sort) {
       this.constructor.modules[name] = this;
@@ -87,27 +93,27 @@
       Here instead of using the object format described in the class comments, you may instead
       add widgets to the WidgetList by using add_widget.
 
-      page_type - the shortName of a GGRC object class, or "admin"
+      pageType - the shortName of a GGRC object class, or "admin"
       id - the desired widget's id.
       descriptor - a widget descriptor appropriate for the widget type. FIXME - the descriptor's
         widget_id value must match the value passed as "id"
     */
-    add_widget: function (page_type, id, descriptor) {
-      this[page_type] = this[page_type] || {};
-      if (this[page_type][id]) {
-        can.extend(true, this[page_type][id], descriptor);
+    add_widget: function (pageType, id, descriptor) {
+      this[pageType] = this[pageType] || {};
+      if (this[pageType][id]) {
+        can.extend(true, this[pageType][id], descriptor);
       } else {
-        this[page_type][id] = descriptor;
+        this[pageType][id] = descriptor;
       }
     },
-    suppress_widget: function (page_type, id) {
-      this[page_type] = this[page_type] || {};
-      if (this[page_type][id]) {
-        can.extend(true, this[page_type][id], {
+    suppress_widget: function (pageType, id) {
+      this[pageType] = this[pageType] || {};
+      if (this[pageType][id]) {
+        can.extend(true, this[pageType][id], {
           suppressed: true
         });
       } else {
-        this[page_type][id] = {
+        this[pageType][id] = {
           suppressed: true
         };
       }
