ab2c0d4160f3d250654a50ffd607ab41425b45b4
Alexander Novik <al.novik88@gmail.com>
2016-08-31 15:51:39 +0300

    Add helper for work with Query API

--- src/ggrc/assets/javascripts/plugins/tests/ggrc_utils_spec.js
+++ src/ggrc/assets/javascripts/plugins/tests/ggrc_utils_spec.js
@@ -294,3 +294,79 @@ describe('GGRC utils isMappableType() method', function () {
     expect(result).toBe(true);
   });
 });
+
+describe('GGRC Utils Query API', function () {
+  describe('buildParams() method', function () {
+    var relevant;
+    var objectName;
+    var paging;
+    var method;
+
+    beforeEach(function () {
+      paging = {
+        current: 1,
+        total: null,
+        pageSize: 10,
+        filter: '',
+        count: 6
+      };
+
+      method = GGRC.Utils.QueryAPI.buildParams;
+    });
+
+    describe('Assessment related to Audit', function () {
+      beforeEach(function () {
+        relevant = {type: 'Audit', id: 1};
+        objectName = 'Assessment';
+      });
+
+      it('return default params for paging request', function () {
+        var result = method(objectName, paging, relevant)[0];
+
+        expect(result.object_name).toEqual('Assessment');
+        expect(result.limit).toEqual([0, 10]);
+        expect(result.filters.expression.object_name).toEqual('Audit');
+      });
+
+      it('return limit for 3rd page', function () {
+        var result;
+        paging.current = 3;
+        paging.pageSize = 50;
+
+        result = method(objectName, paging, relevant)[0];
+
+        expect(result.limit).toEqual([100, 150]);
+      });
+    });
+
+    describe('Request related to Assessment', function () {
+      beforeEach(function () {
+        relevant = {
+          id: 1,
+          type: 'Assessment'
+        };
+        objectName = 'Request';
+      });
+
+      it('return default params for paging request', function () {
+        var result = method(objectName, paging, relevant)[0];
+
+        expect(result.object_name).toEqual('Request');
+        expect(result.limit).toEqual([0, 10]);
+        expect(result.filters.expression.object_name).toEqual('Assessment');
+      });
+
+      it('return expression for filter', function () {
+        var filterResult;
+        paging.filter = 'status="in progress"';
+
+        filterResult =
+          method(objectName, paging, relevant)[0].filters.expression.right;
+
+        expect(filterResult.left).toEqual('status');
+        expect(filterResult.right).toEqual('in progress');
+        expect(filterResult.op.name).toEqual('=');
+      });
+    });
+  });
+});
