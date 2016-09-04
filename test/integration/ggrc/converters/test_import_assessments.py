182bf7d7462272bcee864270b88bc0bd0647a40b
Peter Lamut <peter.lamut@niteoweb.com>
2016-09-04 11:36:34 +0200

    Add test for Assessment export with clashing names

--- test/integration/ggrc/converters/test_import_assessments.py
+++ test/integration/ggrc/converters/test_import_assessments.py
@@ -1,20 +1,21 @@
 # Copyright (C) 2016 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
-# pylint: disable=maybe-no-member
+# pylint: disable=maybe-no-member, invalid-name
 
 """Test request import and updates."""
 
+from ggrc import db
 from ggrc import models
 from integration.ggrc import converters
 
 
-class TestRequestImport(converters.TestCase):
+class TestAssessmentImportExport(converters.TestCase):
+  """Basic Assessment import tests with.
 
-  """Basic Request import tests with.
-
-  This test suite should test new Request imports and updates. The main focus
-  of these tests is checking error messages for invalid state transitions.
+  This test suite should test new Assessment imports, exports, and updates.
+  The main focus of these tests is checking error messages for invalid state
+  transitions.
   """
 
   def setUp(self):
@@ -22,7 +23,7 @@ class TestRequestImport(converters.TestCase):
     converters.TestCase.setUp(self)
     self.client.get("/login")
 
-  def test_assessments_with_templates(self):
+  def test_import_assessments_with_templates(self):
     """Test importing of assessments with templates."""
 
     self.import_file("assessment_template_no_warnings.csv")
@@ -35,3 +36,50 @@ class TestRequestImport(converters.TestCase):
     values = set(v.attribute_value for v in assessment.custom_attribute_values)
     self.assertIn("abc", values)
     self.assertIn("2015-07-15 00:00:00", values)
+
+  def test_export_assessments_with_filters_and_conflicting_ca_names(self):
+    """Test exporting assessments with conflicting custom attribute names."""
+    self.import_file(u"assessment_template_no_warnings.csv")
+    self.import_file(u"assessment_with_templates.csv")
+
+    # also create an object level custom attribute with a name that clashes
+    # with a name of a "regular" attribute
+    assessment = models.Assessment.query.filter(
+        models.Assessment.slug == u"A 2").first()
+    cad = models.CustomAttributeDefinition(
+        attribute_type=u"Text",
+        title=u"title",
+        definition_type=u"assessment",
+        definition_id=assessment.id
+    )
+    db.session.add(cad)
+    db.session.commit()
+
+    data = [{
+        "object_name": "Assessment",
+        "fields": ["slug", "title", "description", "status"],
+        "filters": {
+            "expression": {
+                "left": {
+                    "left": "code",
+                    "op": {"name": "~"},
+                    "right": "A 2"
+                },
+                "op": {"name": "AND"},
+                "right": {
+                    "left": "title",
+                    "op": {"name": "~"},
+                    "right": "no template Assessment"
+                }
+            },
+            "keys": ["code", "title", "status"],
+            "order_by": {
+                "keys": [],
+                "order": "",
+                "compare": None
+            }
+        }
+    }]
+
+    response = self.export_csv(data)
+    self.assertIn(u"No template Assessment 2", response.data)
