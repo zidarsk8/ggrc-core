1a555cbac82beff37b81ce3d3b0af86f50fa2858
Anže Pečar <apecar@google.com>
2016-09-08 18:37:00 +0200

    Lint log_event.py

--- src/ggrc/services/log_event.py
+++ src/ggrc/services/log_event.py
@@ -3,13 +3,15 @@
 
 from flask import request, current_app
 
+
 def log_event():
   '''Log javascript client errors to syslog via application logger.'''
   method = request.method.lower()
   if method == 'post':
     severity = request.json['log_event']['severity']
     description = request.json['log_event']['description']
-    current_app.logger.error('Javascript Client: %s %s',
+    current_app.logger.error(
+        'Javascript Client: %s %s',
         severity, description)
     return current_app.make_response(('', 200, []))
   raise NotImplementedError()
