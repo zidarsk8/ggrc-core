d29a150bc979b78d8113f3e9cd12334338fbba87
Alexander Novik <al.novik88@gmail.com>
2016-09-15 16:06:48 +0300

    Tests for getRelationshipBetweenInstances method

--- src/ggrc/assets/javascripts/plugins/tests/ggrc_query_api_spec.js
+++ src/ggrc/assets/javascripts/plugins/tests/ggrc_query_api_spec.js
@@ -1,3 +1,8 @@
+/*!
+ Copyright (C) 2016 Google Inc.
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
 describe('GGRC Utils Query API', function () {
   describe('buildParams() method', function () {
     var relevant;
