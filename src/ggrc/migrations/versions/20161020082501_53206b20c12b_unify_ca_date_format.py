f3ed207a89137354e9328a6f4f16e6f626176b75
Alieh Rymasheuski <alieh.rymasheuski@gmail.com>
2016-10-28 14:17:13 +0300

    Add logic to strip whitespace around dates

--- src/ggrc/migrations/versions/20161020082501_53206b20c12b_unify_ca_date_format.py
+++ src/ggrc/migrations/versions/20161020082501_53206b20c12b_unify_ca_date_format.py
@@ -10,6 +10,7 @@ Create Date: 2016-10-20 08:25:01.140510
 # pylint: disable=invalid-name
 
 from alembic import op
+from sqlalchemy.sql import text
 
 
 # revision identifiers, used by Alembic.
@@ -17,9 +18,35 @@ revision = '53206b20c12b'
 down_revision = 'bb6fe8e14bb'
 
 
+def _strip_whitespace_around_dates(connection):
+  """Fix possibly existing dates prefixed with spaces in the DB."""
+
+  dates = connection.execute("""
+      SELECT cavs.id as id, attribute_value as value
+      FROM custom_attribute_values AS cavs JOIN
+           custom_attribute_definitions as cads ON
+               cads.id = cavs.custom_attribute_id
+      WHERE attribute_type = 'Date'
+  """)
+
+  for date in dates:
+    if date.value is None:
+      new_value = None
+    else:
+      new_value = date.value.strip()
+    if date.value != new_value:
+      connection.execute(text("""
+          UPDATE custom_attribute_values SET
+              attribute_value = :val
+          WHERE id = :id
+      """), val=new_value, id=date.id)
+
+
 def upgrade():
   """Upgrade database schema and/or data, creating a new revision."""
   connection = op.get_bind()
+
+  _strip_whitespace_around_dates(connection)
 
   # change M/DD/YYYY or M/D/YYYY into MM/DD/YYYY or MM/D/YYYY correspondigly
   connection.execute("""
