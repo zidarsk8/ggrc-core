30fe139b8cfb1221f5a53c4d9a3a8a2cfa925bf3
Alieh Rymasheuski <alieh.rymasheuski@gmail.com>
2016-12-07 18:50:12 +0300

    Remove pointless DontPropagate class

--- src/ggrc/models/reflection.py
+++ src/ggrc/models/reflection.py
@@ -87,9 +87,8 @@ EXCLUDE_MAPPINGS = set([
 ])
 
 
-class DontPropagate(object):
-
-  """Attributes wrapped by ``DontPropagate`` instances should not be considered
+class PublishOnly(object):
+  """Attributes wrapped by ``PublishOnly`` instances should not be considered
   to be a part of an inherited list. For example, ``_update_attrs`` can be
   inherited from ``_publish_attrs`` if left unspecified. This class provides
   a mechanism to use that inheritance while excluding some elements from the
@@ -99,7 +98,7 @@ class DontPropagate(object):
 
     _publish_attrs = [
       'inherited_attr',
-      DontPropagate('not_inherited_attr'),
+      PublishOnly('not_inherited_attr'),
       ]
 
   is equivalent to this:
@@ -118,13 +117,6 @@ class DontPropagate(object):
 
   def __init__(self, attr_name):
     self.attr_name = attr_name
-
-
-class PublishOnly(DontPropagate):
-  """Alias of ``DontPropagate`` for use in a ``_publish_attrs`` specification.
-  """
-  # pylint: disable=too-few-public-methods
-  pass
 
 
 class AttributeInfo(object):
@@ -181,7 +173,7 @@ class AttributeInfo(object):
       main_class = tgt_class
     src_attrs = src_attrs if isinstance(src_attrs, list) else [src_attrs]
     accumulator = accumulator if accumulator is not None else set()
-    ignore_dontpropagate = True
+    ignore_publishonly = True
     for attr in src_attrs:
       attrs = None
       # Only get the attribute if it is defined on the target class, but
@@ -191,15 +183,15 @@ class AttributeInfo(object):
         if callable(attrs):
           attrs = attrs(main_class)
       if attrs is not None:
-        if not ignore_dontpropagate:
-          attrs = [a for a in attrs if not isinstance(a, DontPropagate)]
+        if not ignore_publishonly:
+          attrs = [a for a in attrs if not isinstance(a, PublishOnly)]
         else:
-          attrs = [a if not isinstance(a, DontPropagate) else a.attr_name for
+          attrs = [a if not isinstance(a, PublishOnly) else a.attr_name for
                    a in attrs]
         accumulator.update(attrs)
         break
       else:
-        ignore_dontpropagate = False
+        ignore_publishonly = False
     for base in tgt_class.__bases__:
       cls.gather_attrs(base, src_attrs, accumulator, main_class=main_class)
     return accumulator
