d7155a8d9edc246d828547a6cea8821c11c53027
Peter Lamut <inbox@peterlamut.com>
2016-12-20 10:57:34 +0100

    Disable date serialization in timeboxed mixin

--- src/ggrc/assets/javascripts/models/mixins.js
+++ src/ggrc/assets/javascripts/models/mixins.js
@@ -349,10 +349,31 @@
     }
   });
 
+  /**
+   * A mixin to use for objects that can have a time limit imposed on them.
+   *
+   * @class CMS.Models.Mixins.timeboxed
+   */
   can.Model.Mixin('timeboxed', {
     'extend:attributes': {
       start_date: 'date',
       end_date: 'date'
+    },
+
+    // Override default CanJS's conversion/serialization of dates, because
+    // that takes the browser's local timezone into account, which we do not
+    // want with our UTC dates. Having plain UTC-formatted date strings is
+    // more suitable for the current structure of the app.
+    serialize: {
+      date: function (val, type) {
+        return val;
+      }
+    },
+
+    convert: {
+      date: function (val, oldVal, fn, type) {
+        return val;
+      }
     }
   }, {
   });
