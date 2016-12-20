50026fb6b772f76bf82056789e5f8fc7b804753c
Alieh Rymasheuski <alieh.rymasheuski@gmail.com>
2016-12-20 12:18:29 +0300

    Remove Request-specific tests

--- test/integration/ggrc_basic_permissions/test_creator_program.py
+++ test/integration/ggrc_basic_permissions/test_creator_program.py
@@ -5,8 +5,6 @@
 Test Creator role with Program scoped roles
 """
 
-import unittest
-
 from ggrc import db
 from ggrc.models import all_models
 from integration.ggrc import TestCase
@@ -294,80 +292,3 @@ class TestCreatorProgram(TestCase):
                     test_case, action, obj, res, actions[action]))
       # Try mapping
     self.assertEqual(errors, [])
-
-  @unittest.skip("Test for request object, not important anymore")
-  def test_creator_audit_request_creation(self):
-    self.init_objects("ProgramOwner")
-    program = self.objects.get("program")
-    creator = self.people.get("creator")
-    # Create an audit
-    response = self.api.post(all_models.Audit, {
-        "audit": {
-            "title": "Audit for program",
-            "status": "Planned",
-            "context": {
-                "id": program.context_id,
-                "type": "Context"
-            },
-            "program": {
-                "context_id": program.context_id,
-                "id": program.id,
-                "type": "Program",
-            },
-            "contact": {
-                "id": creator.id,
-                "type": "Person"
-            }
-        },
-    })
-    self.assertEqual(response.status_code, 201)
-    audit_id = response.json.get("audit").get("id")
-    audit_context_id = response.json.get("audit").get("context").get("id")
-    # Create a request
-    response = self.api.post(all_models.Request, {
-        "request": {
-            "title": "Request for audit",
-            "status": "In Progress",
-            "context": {
-                "id": audit_context_id,
-                "type": "Context"
-            },
-            "audit": {
-                "id": audit_id,
-                "type": "Audit",
-            },
-            "end_date": "2015-12-08",
-            "start_date": "2015-12-01",
-            "request_type": "documentation"
-        },
-    })
-    self.assertEqual(response.status_code, 201)
-    request_id = response.json.get("request").get("id")
-
-    # Create assignee/requester relationships
-    assignee = self.people.get("notmapped")
-    response = self.api.post(all_models.Relationship, {
-        "relationship": {
-            "attrs": {
-                "AssigneeType": "Assignee"
-            },
-            "context": {
-                "id": audit_context_id,
-                "type": "Context",
-            },
-            "destination": {
-                "id": request_id,
-                "type": "Request",
-            },
-            "source": {
-                "id": assignee.id,
-                "type": "Person"
-            }
-        },
-    })
-    self.assertEqual(response.status_code, 201)
-    relationship_id = response.json.get("relationship").get("id")
-    response = self.api.get_collection(all_models.Relationship,
-                                       relationship_id)
-    num = len(response.json["relationships_collection"]["relationships"])
-    self.assertEqual(num, 2)
