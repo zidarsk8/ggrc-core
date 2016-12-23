1b12b952cc026bf627d8d2adaa3c477e1542cfce
akaralionak <akaralionak@google.com>
2016-12-23 16:37:35 +0300

    Modify base behavior of 'Select All' button functionality for Snapshot Loader

--- src/ggrc/assets/javascripts/components/snapshot-loader/snapshot-loader.js
+++ src/ggrc/assets/javascripts/components/snapshot-loader/snapshot-loader.js
@@ -16,8 +16,10 @@
       paging: {
         current: 1,
         pageSize: 5,
+        filter: '',
         pageSizeSelect: [5, 10, 15]
       },
+      allSelected: false,
       mapper: null,
       isLoading: false,
       items: [],
@@ -32,11 +34,10 @@
         this.attr('paging.count', count);
       },
       setItems: function () {
-        // Clean up selection before any items update
-        this.unselectAll();
         this.attr('items').replace(this.load());
       },
       onSearch: function () {
+        this.clearSelection();
         this.setItems();
       },
       prepareRelevantFilters: function () {
@@ -65,13 +66,9 @@
         }
         return filters;
       },
-      prepareBaseQuery: function (modelName, filters, ownedFilter) {
+      prepareBaseQuery: function (modelName, paging, filters, ownedFilter) {
         return GGRC.Utils.QueryAPI
-          .buildParam(modelName, {
-            filter: this.attr('term'),
-            current: this.attr('paging.current'),
-            pageSize: this.attr('paging.pageSize')
-          }, filters, [], ownedFilter);
+          .buildParam(modelName, paging, filters, [], ownedFilter);
       },
       prepareRelatedQuery: function (modelName, ownedFilter) {
         return GGRC.Utils.QueryAPI
@@ -101,33 +98,74 @@
           }
         };
       },
-      getQuery: function () {
+      loadAllItemsIds: function () {
+        var dfd = can.Deferred();
+        var queryType = 'ids';
+        var query = this.getQuery(queryType, false);
+
+        GGRC.Utils.QueryAPI
+          .makeRequest(query)
+          .done(function (responseArr) {
+            var data = responseArr[0];
+            var filters = responseArr[1].Snapshot.ids;
+            var values = data.Snapshot[queryType];
+            var result = values.map(function (item) {
+              return {
+                id: item,
+                type: 'Snapshot'
+              };
+            });
+            // Do not perform extra mapping validation in case Assessment generation
+            if (!this.attr('mapper.assessmentGenerator')) {
+              result = result.filter(function (item) {
+                return filters.indexOf(item.id) < 0;
+              });
+            }
+            dfd.resolve(result);
+          }.bind(this))
+          .fail(function () {
+            dfd.resolve([]);
+          });
+        return dfd;
+      },
+      getQuery: function (queryType, addPaging) {
         var modelName = this.attr('type');
+        var paging = addPaging ? {
+          filter: this.attr('term'),
+          current: this.attr('paging.current'),
+          pageSize: this.attr('paging.pageSize')
+        } : {
+          filter: this.attr('term')
+        };
         var filters = this.prepareRelevantFilters();
         var ownedFilter = this.prepareOwnedFilter();
-        var query = this.prepareBaseQuery(modelName, filters, ownedFilter);
+        var query =
+          this.prepareBaseQuery(modelName, paging, filters, ownedFilter);
         var relatedQuery = this.prepareRelatedQuery(modelName, ownedFilter);
         // Transform Base Query to Snapshot
         query = GGRC.Utils.Snapshots.transformQuery(query);
         // Add Permission check
         query.permissions = 'update';
+        query.type = queryType || 'values';
         // Transform Related Query to Snapshot
         relatedQuery = GGRC.Utils.Snapshots.transformQuery(relatedQuery);
         return {data: [query, relatedQuery]};
       },
       load: function () {
         var dfd = can.Deferred();
-        var query = this.getQuery();
+        var query = this.getQuery('values', true);
         this.attr('isLoading', true);
         GGRC.Utils.QueryAPI
           .makeRequest(query)
           .done(function (responseArr) {
             var data = responseArr[0];
             var filters = responseArr[1].Snapshot.ids;
+            var allSelected = this.attr('allSelected');
             var values = data.Snapshot.values;
             var result = values.map(function (item) {
               item = GGRC.Utils.Snapshots.toObject(item);
               item.attr('instance', item);
+              item.attr('isSelected', allSelected);
               return item;
             });
             // Do not perform extra mapping validation in case Assessment generation
@@ -149,6 +187,11 @@
           }.bind(this));
         return dfd;
       },
+      clearSelection: function () {
+        this.attr('allSelected', false);
+        // Remove all selected items
+        this.attr('selected').replace([]);
+      },
       unselectAll: function () {
         this.attr('items')
           .forEach(function (item) {
@@ -156,13 +199,18 @@
               item.attr('isSelected', false);
             }
           });
+        this.clearSelection();
       },
       selectAll: function () {
+        this.attr('allSelected', true);
+        // Add visual Selection to all currently visible items
         this.attr('items').forEach(function (item) {
           if (!item.attr('isMapped')) {
             item.attr('isSelected', true);
           }
         });
+        // Replace with actual items loaded from the back-end
+        this.attr('selected').replace(this.loadAllItemsIds());
       }
     },
     events: {
