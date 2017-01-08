0270ce7930055e6534807d80e289392335c4d527
Miha Zidar <zidarsk8@gmail.com>
2017-01-08 15:00:56 +0100

    Add CSV generator for invalid objects
    This will generate a csv file that can be used for removing all objects
    that violate the migration state of the database. After import the file
    delete_invalid_data.csv the database validation should pass and allow
    database migration.

--- bin/validate_database.py
+++ bin/validate_database.py
@@ -12,6 +12,8 @@ Example:
 # pylint: disable=invalid-name
 
 import sys
+import csv
+from collections import defaultdict
 
 from sqlalchemy.sql import and_
 from sqlalchemy.sql import func
@@ -20,6 +22,7 @@ from sqlalchemy.sql import select
 # We have to import app before we can use db and other parts of the app.
 from ggrc import app  # noqa  pylint: disable=unused-import
 from ggrc import db
+from ggrc.models import all_models
 
 from ggrc.models.relationship import Relationship
 from ggrc.models.assessment import Assessment
@@ -99,6 +102,30 @@ def validate_database2():
   return audits_more, ghost_objects
 
 
+def _generate_delete_csv(all_bad_ids):
+  """Generate a CSV file for deleting bad objects."""
+  data = []
+  for model_name, ids in all_bad_ids.items():
+    model = getattr(all_models, model_name, None)
+    if not model:
+      print "Incorrect model found:", model_name
+      sys.exit(1)
+
+    slugs = db.session.query(model.slug).filter(model.id.in_(ids))
+    data += [
+        ["Object type", "", ""],
+        [model.__name__, "Code", "Delete"],
+    ]
+    for row in slugs:
+      data.append(["", row.slug, "force"])
+    data.append(["", "", ""])
+
+  with open("/vagrant/delete_invalid_data.csv", "wb") as csv_file:
+    writer = csv.writer(csv_file)
+    for row in data:
+      writer.writerow(row)
+
+
 def validate():
   """Migrate audit-related data and concepts to audit snapshots"""
   print "Checking database: {}".format(db.engine)
@@ -112,18 +139,23 @@ def validate():
 
   audits_more, ghost_objects = validate_database2()
 
+  all_bad_ids = defaultdict(list)
   if audits_more or ghost_objects:
     for klass_name, result in audits_more:
       ids = [id_ for _, id_ in result]
+      all_bad_ids[klass_name] += ids
       print "Too many Audits mapped to {klass}: {ids}".format(
           klass=klass_name,
           ids=",".join(str(id_) for id_ in sorted(ids))
       )
-    for klass_name, result in ghost_objects:
+    for klass_name, ids in ghost_objects:
+      all_bad_ids[klass_name] += ids
       print "No Audits mapped to {klass}: {ids}".format(
           klass=klass_name,
-          ids=",".join(str(id_) for id_ in sorted(result))
+          ids=",".join(str(id_) for id_ in sorted(ids))
       )
+    _generate_delete_csv(all_bad_ids)
+    print "To remove all violating objects import delete_invalid_data.csv."
     print "FAIL"
     sys.exit(1)
   else:
