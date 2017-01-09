9dfa08722444cbbdc7f10e1966f6bcae1267c4ee
Miha Zidar <mihaz@google.com>
2017-01-09 14:27:42 +0100

    Move validation code to migration utils package
    This same code will be used for Audit snapshot and should be in a
    universal location.

--- bin/validate_database.py
+++ bin/validate_database.py
@@ -24,82 +24,7 @@ from ggrc import app  # noqa  pylint: disable=unused-import
 from ggrc import db
 from ggrc.models import all_models
 
-from ggrc.models.relationship import Relationship
-from ggrc.models.assessment import Assessment
-from ggrc.models.issue import Issue
-
-
-def validate_database():
-  """Check if the database is in a valid state for Audit migrations.
-
-  The Audit snapshot migration does not produce correct results if any
-  Assessment or Issue are not mapped to exactly one Audit. This function
-  checks and outputs the ids of the offending objects and returns a non zero
-  code if any such object is found.
-  """
-
-  # pylint: disable=too-many-locals
-  multiple_mappings = []
-  zero_mappings = []
-  relationships_table = Relationship.__table__
-  assessments_table = Assessment.__table__
-  issues_table = Issue.__table__
-
-  tables = [
-      ("Assessment", assessments_table),
-      ("Issue", issues_table),
-      # "Request",  ignoring requests since they have an audit foreign key and
-      # can not be in an invalid state.
-  ]
-
-  for (klass_name, table_) in tables:
-    sql_base_left = select([
-        func.count(relationships_table.c.id).label("relcount"),
-        relationships_table.c.source_id.label("object_id"),
-    ]).where(
-        and_(
-            relationships_table.c.source_type == klass_name,
-            relationships_table.c.destination_type == "Audit"
-        )
-    ).group_by(relationships_table.c.source_id)
-
-    sql_base_right = select([
-        func.count(relationships_table.c.id).label("relcount"),
-        relationships_table.c.destination_id.label("object_id"),
-    ]).where(
-        and_(
-            relationships_table.c.destination_type == klass_name,
-            relationships_table.c.source_type == "Audit"
-        )
-    ).group_by(relationships_table.c.destination_id)
-
-    sql_left_more = sql_base_left.having(sql_base_left.c.relcount > 1)
-    sql_right_more = sql_base_right.having(sql_base_right.c.relcount > 1)
-    sql_left_one = sql_base_left.having(sql_base_left.c.relcount == 1)
-    sql_right_one = sql_base_right.having(sql_base_right.c.relcount == 1)
-
-    result_left_more = db.session.execute(sql_left_more).fetchall()
-    result_right_more = db.session.execute(sql_right_more).fetchall()
-    result_more = result_left_more + result_right_more
-
-    result_left_one = db.session.execute(sql_left_one).fetchall()
-    result_right_one = db.session.execute(sql_right_one).fetchall()
-    result_one = result_left_one + result_right_one
-
-    all_object_ids = {
-        x.id for x in db.session.execute(select([table_.c.id])).fetchall()
-    }
-    to_audit_mapped_ids = {
-        x.object_id for x in result_more + result_one
-    }
-
-    result_zero = all_object_ids - to_audit_mapped_ids
-
-    if result_more:
-      multiple_mappings += [(klass_name, result_more)]
-    if result_zero:
-      zero_mappings += [(klass_name, result_zero)]
-  return multiple_mappings, zero_mappings
+from ggrc.migrations.utils import validation
 
 
 def _generate_delete_csv(all_bad_ids):
@@ -127,7 +52,7 @@ def _generate_delete_csv(all_bad_ids):
 
 
 def validate():
-  """Migrate audit-related data and concepts to audit snapshots"""
+  """Migrate audit-related data and concepts to audit snapshots."""
   print "Checking database: {}".format(db.engine)
 
   tables = set(row[0] for row in db.session.execute("show tables"))
@@ -137,7 +62,7 @@ def validate():
     # run on an empty database (usually with db_reset)
     return
 
-  multiple_mappings, zero_mappings = validate_database()
+  multiple_mappings, zero_mappings = validation.validate_database(db.session)
 
   all_bad_ids = defaultdict(list)
   if multiple_mappings or zero_mappings:
