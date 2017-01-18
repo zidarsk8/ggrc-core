1dc1ce0e5032eaaa883d141f84b6bc947dc6696d
SergeyRabtsau <ts.astesr@gmail.com>
2017-01-18 23:40:02 +0300

    Update rest service by adding new object types

--- test/selenium/src/lib/service/rest/template_provider.py
+++ test/selenium/src/lib/service/rest/template_provider.py
@@ -1,47 +1,49 @@
 # Copyright (C) 2017 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
-
-# pylint: disable=too-few-public-methods
-
 """The module contains functionality for working with json templates"""
+# pylint: disable=too-few-public-methods
+# pylint: disable=redefined-builtin
+# pylint: disable=invalid-name
 
 import copy
 import json
+
 import os
 
-from lib.constants import url
-from lib.constants import objects
+from lib.constants import url, objects
 
 
 class TemplateProvider(object):
-  """Processes json templates"""
+  """Processes JSON templates."""
   RELATIVE_PATH_TEMPLATE = "template/{0}.json"
   parsed_data = dict()
 
   @staticmethod
-  def get_template_as_dict(obj_type, **kwargs):
-    """Return object representation based on json template"""
+  def get_template_as_dict(type, **kwargs):
+    """Return object representation based on JSON template."""
     try:
-      obj = copy.deepcopy(TemplateProvider.parsed_data[obj_type])
+      obj = copy.deepcopy(TemplateProvider.parsed_data[type])
     except KeyError:
       path = os.path.join(
           os.path.dirname(__file__),
-          TemplateProvider.RELATIVE_PATH_TEMPLATE.format(obj_type))
+          TemplateProvider.RELATIVE_PATH_TEMPLATE.format(type))
       with open(path) as json_file:
         json_data = json_file.read()
       data = json.loads(json_data)
-      TemplateProvider.parsed_data[obj_type] = data
+      TemplateProvider.parsed_data[type] = data
       obj = copy.deepcopy(data)
     obj.update(kwargs)
-    contact = {"contact": TemplateProvider.generate_object(1, objects.PEOPLE)}
-    obj.update(contact)
-    return {obj_type: obj}
+    if type != "relationship" and "object_owner":
+      contact = (
+          {"contact": TemplateProvider.generate_object(1, objects.PEOPLE)})
+      obj.update(contact)
+    return {type: obj}
 
   @staticmethod
-  def generate_object(obj_id, obj_type):
-    """Return minimal object representation by id and type"""
+  def generate_object(id, type):
+    """Return minimal object representation by id and type."""
     result = {}
-    result["id"] = obj_id
-    result["href"] = "/".join([url.API, obj_type, str(obj_id)])
-    result["type"] = objects.get_singular(obj_type)
+    result["id"] = id
+    result["href"] = "/".join([url.API, type, str(id)])
+    result["type"] = objects.get_singular(type)
     return result
--- test/selenium/src/lib/service/rest_service.py
+++ test/selenium/src/lib/service/rest_service.py
@@ -1,64 +1,146 @@
 # Copyright (C) 2017 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
-
-# pylint: disable=too-few-public-methods
-
 """The module provides API for creating and manipulating GGRC's business
-objects via REST"""
+objects via REST."""
+# pylint: disable=too-few-public-methods
 
 import json
 
-from lib.service.rest.client import RestClient
+import re
+
+from lib import environment
 from lib.constants import url
+from lib.entities.entities_factory import (
+    ProgramFactory, AuditFactory, AsmtTmplFactory, AsmtFactory, ControlFactory,
+    PersonFactory)
+from lib.service.rest.client import RestClient
 
 
 class BaseService(object):
-  """Base class for business layer's services objects"""
+  """Base class for business layer's services objects."""
   def __init__(self):
     self.client = RestClient(self.ENDPOINT)
 
+  def create_objs(self, count, factory, **kwargs):
+    """Create business objects used entities factories and REST API
+    (responses from web-server parsed to list of attributes (list of dicts))
+    Return list of objects.
+    """
+    objs = [factory.create() for _ in xrange(count)]
+    list_of_attrs = [
+        self.get_list_of_obj_attributes(self.client.create_object(
+            type=obj.type, title=obj.title, **kwargs)) for
+        obj in objs]
+    return [self.set_obj_attrs(attrs, obj, **kwargs) for
+            attrs, obj in zip(list_of_attrs, objs)]
+
+
   @staticmethod
-  def get_list_of_created_objects(response):
-    """Forms the list of business entities from response"""
+  def get_list_of_obj_attributes(response):
+    """Form the list of dicts with business object's attributes (dict's items)
+    from server response.
+    """
     def minimize(object_element):
-      """Minimize response json data to request ready format"""
+      """Minimize response json data to request ready format."""
       obj = object_element[1].values()[0]
-      return {"id": obj["id"], "href": obj["selfLink"], "type": obj["type"]}
-    return [minimize(object_element=x) for x in json.loads(response.text)]
+      return {"id": obj["id"], "href": obj["selfLink"], "type": obj["type"],
+              "title": obj["title"],
+              "url": environment.APP_URL + obj["viewLink"][1:],
+              "name": re.search(r"\/([a-z_]*)\/", obj["viewLink"]).group(1)}
+    return [minimize(object_element=x) for x in json.loads(response.text)][0]
+
+
+  @staticmethod
+  def set_obj_attrs(attrs, obj, **kwargs):
+    """Update business object's attributes according type of object and
+    list of dicts with business object's attributes (dict's items).
+    """
+    if attrs.get("id"):
+      obj.id = attrs["id"]
+    if attrs.get("href"):
+      obj.href = attrs["href"]
+    if kwargs:
+      if kwargs.get("program") and kwargs.get("program").get("title"):
+        obj.program = kwargs["program"]["title"]
+      if kwargs.get("audit") and kwargs.get("audit").get("title"):
+        obj.audit = kwargs["audit"]["title"]
+      if kwargs.get("object") and kwargs.get("object").get("title"):
+        obj.object = kwargs["object"]["title"]
+    return obj
 
 
 class ControlsService(BaseService):
-  """Incapsulates logic for working with business entity Control"""
+  """Encapsulates logic for working with business entity Control."""
   ENDPOINT = url.CONTROLS
 
-  def create_controls(self, count):
-    return self.get_list_of_created_objects(
-        self.client.create_objects("control", count=count))
+  def create(self, count):
+    return self.create_objs(count=count, factory=ControlFactory())
+
+  def update(self, objs):
+    return [objs.__dict__]
 
 
 class ProgramsService(BaseService):
-  """Incapsulates logic for working with business entity Program"""
+  """Encapsulates logic for working with business entity Program."""
   ENDPOINT = url.PROGRAMS
 
-  def create_programs(self, count):
-    return self.get_list_of_created_objects(
-        self.client.create_objects("program", count=count))
+  def create(self, count):
+    return self.create_objs(count=count, factory=ProgramFactory())
 
 
 class AuditsService(BaseService):
-  """Incapsulates logic for working with business entity Audit"""
+  """Encapsulates logic for working with business entity Audit."""
   ENDPOINT = url.AUDITS
 
-  def create_audits(self, count, program):
-    return self.get_list_of_created_objects(
-        self.client.create_objects("audit", count=count, program=program))
+  def create(self, count, program):
+    return self.create_objs(count=count, factory=AuditFactory(),
+                            program=program.__dict__)
+
+
+class AsmtTmplsService(BaseService):
+  """Encapsulates logic for working with business entity
+  Assessment Template.
+  """
+  ENDPOINT = url.ASSESSMENT_TEMPLATES
+
+  def create(self, count, audit):
+    return self.create_objs(count=count, factory=AsmtTmplFactory(),
+                            audit=audit.__dict__)
 
 
 class AssessmentsService(BaseService):
-  """Incapsulates logic for working with business entity Assessment"""
+  """Encapsulates logic for working with business entity Assessment."""
   ENDPOINT = url.ASSESSMENTS
 
-  def create_assessments(self, count, obj, audit):
-    return self.get_list_of_created_objects(
-        self.client.create_objects("assessment", count=count, object=obj,
-                                   audit=audit))
+  def create(self, count, object, audit):
+    return self.create_objs(count=count, factory=AsmtFactory(),
+                            object=object.__dict__, audit=audit.__dict__)
+
+
+class RelationshipsService(BaseService):
+  """Encapsulates logic for working with business entity Relationship."""
+  ENDPOINT = url.RELATIONSHIPS
+
+  def create(self, src_obj, dest_objs):
+    if isinstance(dest_objs, list):
+      return [self.client.create_object("relationship",
+                                        source=src_obj.__dict__,
+                                        destination=dest_obj.__dict__) for
+              dest_obj in dest_objs]
+    else:
+      return self.client.create_object("relationship", source=src_obj.__dict__,
+                                       destination=dest_objs.__dict__)
+
+
+class ObjectsOwnersService(BaseService):
+  """Encapsulates logic for working with business entity Object Owners."""
+  ENDPOINT = url.OBJECT_OWNERS
+
+  def create(self, objs, owner=PersonFactory().default()):
+    if isinstance(objs, list):
+      return [self.client.create_object("object_owner", ownable=obj.__dict__,
+                                        person=owner.__dict__) for
+              obj in objs]
+    else:
+      return self.client.create_object("object_owner", ownable=objs.__dict__,
+                                       person=owner.__dict__)
