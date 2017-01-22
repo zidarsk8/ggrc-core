6b14c9e5683d41ca9d8b9138c25af7526c83d1e4
Miha Zidar <zidarsk8@gmail.com>
2017-01-22 17:58:12 +0100

    Optimize basic delete import tests
    The dry-run check is now automatically performed on each import and we
    do not need to duplicate the work in the delete test.

--- test/integration/ggrc/converters/test_import_delete.py
+++ test/integration/ggrc/converters/test_import_delete.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2017 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
-from ggrc.converters import errors
 from integration.ggrc import TestCase
 
 
@@ -16,9 +15,7 @@ class TestBasicCsvImport(TestCase):
     self.import_file(filename)
 
     filename = "ca_deletion.csv"
-    response_data_dry = self.import_file(filename, dry_run=True)
     response_data = self.import_file(filename)
-    self.assertEqual(response_data_dry, response_data)
 
     self.assertEqual(response_data[0]["deleted"], 2)
     self.assertEqual(response_data[0]["ignored"], 0)
