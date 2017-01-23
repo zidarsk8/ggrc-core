946c0734d5574a931a6709586cc17c8b11faf8c1
Miha Zidar <zidarsk8@gmail.com>
2017-01-23 06:51:55 +0100

    Add tests for Assessment relationship query

--- test/integration/ggrc/services/test_query_snapshots.py
+++ test/integration/ggrc/services/test_query_snapshots.py
@@ -54,8 +54,9 @@ class TestAuditSnapshotQueries(TestCase):
     audit = factories.AuditFactory()
 
     for i in range(5):
+      factories.ControlFactory(title="Control {}".format(i + 1))
       factories.OrgGroupFactory()
-      market = factories.MarketFactory()
+      market = factories.MarketFactory(title="Market {}".format(i + 1))
       factories.CustomAttributeValueFactory(
           custom_attribute=date_cad,
           attributable=market,
@@ -68,7 +69,7 @@ class TestAuditSnapshotQueries(TestCase):
       )
 
     revisions = models.Revision.query.filter(
-        models.Revision.resource_type.in_(["OrgGroup", "Market"]),
+        models.Revision.resource_type.in_(["OrgGroup", "Market", "Control"]),
         models.Revision.id.in_(
             db.session.query(func.max(models.Revision.id)).group_by(
                 models.Revision.resource_type,
@@ -77,13 +78,58 @@ class TestAuditSnapshotQueries(TestCase):
         ),
     )
 
-    for revision in revisions:
-      factories.SnapshotFactory(
-          child_id=revision.resource_id,
-          child_type=revision.resource_type,
-          revision=revision,
-          parent=audit,
-      )
+    snapshots = [
+        factories.SnapshotFactory(
+            child_id=revision.resource_id,
+            child_type=revision.resource_type,
+            revision=revision,
+            parent=audit,
+        )
+        for revision in revisions
+    ]
+
+    snapshot_map = {snapshot.revision.content["title"]: snapshot
+                    for snapshot in snapshots}
+
+    # create Assessments and issues and map some snapshots to them
+    # Markets and Controls represent snapshots of those objects.
+
+    # Assessment 1 -- Market 1,
+    # Assessment 2 -- Market 2, Control 1,
+    # Assessment 3 -- Market 3, Control 1, Control 2
+    # Assessment 4 -- Market 4, Control 1, Control 2, Control 3
+
+    # Issue 1 -- Market 1,
+    # Issue 2 -- Market 2, Control 1,
+    # Issue 3 -- Market 3, Control 1, Control 2
+    # Issue 4 -- Market 4, Control 1, Control 2, Control 3
+
+    assessment_issues = (
+        factories.AssessmentFactory,
+        factories.IssueFactory,
+    )
+    for i in range(4):
+      for factory in assessment_issues:
+        obj = factory(
+            title="{} {}".format(factory._meta.model.__name__, i + 1),
+        )
+        factories.RelationshipFactory(
+            source=audit if i % 2 == 0 else obj,
+            destination=audit if i % 2 == 1 else obj,
+        )
+
+        market = snapshot_map["Market {}".format(i + 1)]
+        factories.RelationshipFactory(
+            source=market if i % 2 == 0 else obj,
+            destination=market if i % 2 == 1 else obj,
+        )
+        for j in range(i):
+          control = snapshot_map["Control {}".format(j + 1)]
+          factories.RelationshipFactory(
+              source=control if i % 2 == 0 else obj,
+              destination=control if i % 2 == 1 else obj,
+          )
+
     views.do_reindex()
 
   def test_basic_snapshot_query(self):
@@ -189,6 +235,91 @@ class TestAuditSnapshotQueries(TestCase):
         }
     ])
     self.assertEqual(len(result.json[0]["Snapshot"]["values"]), 3)
+
+  def test_assesessment_relationships(self):
+    """Test relationships between Assessments and original objects."""
+    control_id_map = {control.title: control.id
+                      for control in models.Control.query}
+
+    # assessment 1 --
+    # assessment 2 -- control 1,
+    # assessment 3 -- control 1, control 2
+    # assessment 4 -- control 1, control 2, control 3
+    expected_counts = {
+        "Control 1": 3,
+        "Control 2": 2,
+        "Control 3": 1,
+        "Control 4": 0,
+        "Control 5": 0,
+    }
+
+    for title, count in expected_counts.items():
+      result = self._post([
+          {
+              "object_name": "Assessment",
+              "filters": {
+                  "expression": {
+                      "object_name": "Control",
+                      "op": {"name": "relevant"},
+                      "ids": [control_id_map[title]]
+                  },
+                  "keys": [],
+                  "order_by": {"keys": [], "order": "", "compare": None}
+              }
+          }
+      ])
+      self.assertEqual(
+          len(result.json[0]["Assessment"]["values"]),
+          count,
+          "Invalid related Assessment count for '{}'."
+          "Expected {}, got {}".format(
+              title,
+              count,
+              len(result.json[0]["Assessment"]["values"]),
+          )
+      )
+
+  def test_original_objects(self):
+    """Test relationships between original objects and Assessments."""
+    assessment_id_map = {assessment.title: assessment.id
+                      for assessment in models.Assessment.query}
+
+    # assessment 1 --
+    # assessment 2 -- control 1,
+    # assessment 3 -- control 1, control 2
+    # assessment 4 -- control 1, control 2, control 3
+    expected_counts = {
+        "Assessment 1": 0,
+        "Assessment 2": 1,
+        "Assessment 3": 2,
+        "Assessment 4": 3,
+    }
+
+    for title, count in expected_counts.items():
+      result = self._post([
+          {
+              "object_name": "Control",
+              "filters": {
+                  "expression": {
+                      "object_name": "Assessment",
+                      "op": {"name": "relevant"},
+                      "ids": [assessment_id_map[title]]
+                  },
+                  "keys": [],
+                  "order_by": {"keys": [], "order": "", "compare": None}
+              }
+          }
+      ])
+      self.assertEqual(
+          len(result.json[0]["Control"]["values"]),
+          count,
+          "Invalid related Control count for '{}'."
+          "Expected {}, got {}".format(
+              title,
+              count,
+              len(result.json[0]["Control"]["values"]),
+          )
+      )
 
   @staticmethod
   def _get_model_expression(model_name="Market"):
