b6ac2c77fb2f6acae96029a7d57d0314820ad5bb
akaralionak <akaralionak@google.com>
2017-01-30 12:27:20 +0300

    Simplify Simple Modal removing all redundant code

--- src/ggrc/assets/javascripts/components/simple-modal/simple-modal.js
+++ src/ggrc/assets/javascripts/components/simple-modal/simple-modal.js
@@ -10,62 +10,21 @@
     '/components/simple-modal/simple-modal.mustache');
   var baseCls = 'simple-modal';
 
-  function recalculatePosition(el) {
-    var pos = el[0].getBoundingClientRect();
-    var top = Math
-      .floor(window.document.body.scrollHeight / 2 - pos.height / 2);
-    var left = Math
-      .floor(window.document.body.scrollWidth / 2 - pos.width / 2);
-
-    return {top: top, left: left};
-  }
-
   can.Component.extend({
     tag: 'simple-modal',
     template: tpl,
-    scope: {
+    viewModel: {
       extraCssClass: '@',
       instance: null,
-      modalEl: null,
-      modalTitle: '@',
+      modalTitle: '',
       state: {
         open: false
       },
-      modalCls: function () {
-        return this.attr('state.open') ? baseCls + '-open' : '';
-      },
-      modalOverlayCls: function () {
-        return this.attr('state.open') ? baseCls + '__overlay-open' : '';
-      },
-      hide: function hide() {
+      hide: function () {
         this.attr('state.open', false);
       },
       show: function () {
         this.attr('state.open', true);
-      },
-      toggle: function (isOpen) {
-        this.setPosition(isOpen);
-      },
-      setPosition: function (isOpen) {
-        var modal = this.attr('modalEl');
-        if (isOpen && modal) {
-          modal.offset(recalculatePosition(modal));
-        }
-      }
-    },
-    events: {
-      inserted: function (el) {
-        var modal = el.find('.' + baseCls);
-        modal.appendTo('body');
-        this.scope.attr('modalEl', modal);
-        el.find('.' + baseCls + '__overlay').appendTo('body');
-      },
-      '{scope.state} open': function (scope, ev, val) {
-        this.scope.toggle(val);
-      },
-      '{window} resize': function () {
-        var isOpen = this.scope.attr('state.open');
-        this.scope.setPosition(isOpen);
       }
     }
   });
