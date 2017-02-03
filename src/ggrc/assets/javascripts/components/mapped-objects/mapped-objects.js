c7c265bb31eba8c1930367dcea1895bebf8d3660
akaralionak <akaralionak@google.com>
2017-02-03 11:16:46 +0300

    Update Mapped Objects Component to support loading of Normal Object via Query API

--- src/ggrc/assets/javascripts/components/mapped-objects/mapped-objects.js
+++ src/ggrc/assets/javascripts/components/mapped-objects/mapped-objects.js
@@ -12,11 +12,50 @@
   /**
    * Mapped objects view component
    */
-  GGRC.Components('mappedObjects', {
+  can.Component.extend({
     tag: tag,
     template: tpl,
-    scope: {
-      mappedSnapshots: false,
+    viewModel: {
+      define: {
+        relatedTypes: {
+          type: '*',
+          value: function () {
+            return [];
+          }
+        },
+        mappedSnapshots: {
+          type: Boolean,
+          value: false
+        },
+        requireLimit: {
+          get: function () {
+            return this.attr('mappedItems.length') > this.attr('visibleItems');
+          }
+        },
+        showItems: {
+          type: can.List,
+          get: function () {
+            return this.attr('showAll') ?
+              this.attr('mappedItems') :
+              this.attr('mappedItems').slice(0, this.attr('visibleItems'));
+          }
+        },
+        showAll: {
+          value: false,
+          type: Boolean
+        },
+        showAllButtonText: {
+          get: function () {
+            return !this.attr('showAll') ?
+            'Show All (' + this.attr('mappedItems.length') + ')' :
+              'Show Less';
+          }
+        },
+        visibleItems: {
+          type: Number,
+          value: 3
+        }
+      },
       isLoading: false,
       mapping: '@',
       parentInstance: null,
@@ -26,10 +65,15 @@
         only: [],
         exclude: []
       },
+      toggleShowAll: function () {
+        var isShown = this.attr('showAll');
+        this.attr('showAll', !isShown);
+      },
       filterMappedObjects: function (items) {
         function getTypeFromInstance(item) {
           return item.instance.type;
         }
+
         return GGRC.Utils.filters
           .applyTypeFilter(items,
             this.attr('filter').attr(), getTypeFromInstance);
@@ -40,25 +84,32 @@
       getSnapshotQueryFilters: function () {
         var includeTypes = this.attr('filter.only').attr();
         var excludeTypes = this.attr('filter.exclude').attr();
-        var includeFilters = includeTypes.map(function (type) {
-          return {
-            expression: {
-              op: {name: '='},
-              left: 'child_type',
-              right: type
-            }
-          };
-        });
+        var includeFilters = {
+          keys: [],
+          expression: {}
+        };
         var excludeFilters = excludeTypes.map(function (type) {
           return {
             expression: {
               op: {name: '!='},
               left: 'child_type',
               right: type
-            }
+            },
+            keys: []
           };
         });
-        return [].concat(includeFilters, excludeFilters);
+        includeTypes.forEach(function (type) {
+          includeFilters = GGRC.query_parser.join_queries({
+            expression: {
+              op: {name: '='},
+              left: 'child_type',
+              right: type
+            },
+            keys: []
+          }, includeFilters, 'OR');
+        });
+        excludeFilters.push(includeFilters);
+        return excludeFilters;
       },
       getSnapshotQuery: function () {
         var relevantFilters = [{
@@ -70,6 +121,17 @@
           .buildParam('Snapshot', {}, relevantFilters, [], filters);
         return {data: [query]};
       },
+      getObjectQuery: function () {
+        var relevantFilters = [{
+          type: this.attr('parentInstance.type'),
+          id: this.attr('parentInstance.id')
+        }];
+        var query = this.attr('relatedTypes').map(function (type) {
+          return GGRC.Utils.QueryAPI
+            .buildParam(type, {}, relevantFilters, [], []);
+        });
+        return {data: query};
+      },
       loadSnapshots: function () {
         var dfd = can.Deferred();
         var query = this.getSnapshotQuery();
@@ -80,10 +142,31 @@
             var data = responseArr[0];
             var values = data.Snapshot.values;
             var result = values.map(function (item) {
-              item = GGRC.Utils.Snapshots.toObject(item);
-              item.attr('instance', item);
-              return item;
+              return {instance: item, isSelected: false};
             });
+            dfd.resolve(result);
+          })
+          .always(function () {
+            this.attr('isLoading', false);
+          }.bind(this));
+        return dfd;
+      },
+      loadObjects: function () {
+        var dfd = can.Deferred();
+        var query = this.getObjectQuery();
+        this.attr('isLoading', true);
+        GGRC.Utils.QueryAPI
+          .makeRequest(query)
+          .done(function (responseArr) {
+            var result = [];
+            responseArr.forEach(function (responseObject) {
+              var type = Object.keys(responseObject)[0];
+              var values = responseObject[type].values;
+              values.forEach(function (item) {
+                result.push({instance: item, isSelected: false});
+              });
+            });
+
             dfd.resolve(result);
           })
           .always(function () {
@@ -116,18 +199,22 @@
       },
       setMappedObjects: function () {
         this.attr('mappedItems').replace(
-          this.attr('mappedSnapshots') ? this.loadSnapshots() : this.load());
+          this.attr('mappedSnapshots') ?
+            this.loadSnapshots() :
+            this.attr('mapping') ?
+              this.load() :
+              this.loadObjects());
       }
     },
     init: function () {
-      this.scope.setMappedObjects();
+      this.viewModel.setMappedObjects();
     },
     events: {
-      '{scope.parentInstance} related_sources': function () {
-        this.scope.setMappedObjects();
+      '{viewModel.parentInstance} related_sources': function () {
+        this.viewModel.setMappedObjects();
       },
-      '{scope.parentInstance} related_destinations': function () {
-        this.scope.setMappedObjects();
+      '{viewModel.parentInstance} related_destinations': function () {
+        this.viewModel.setMappedObjects();
       }
     }
   });
