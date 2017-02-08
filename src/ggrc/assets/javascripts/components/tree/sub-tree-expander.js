3e607f6d38ee82052eeb532cd9b69cb6b5f131a7
Alexander Novik <al.novik88@gmail.com>
2017-02-08 20:12:52 +0300

    Change logic of subtree expander

--- src/ggrc/assets/javascripts/components/tree/sub-tree-expander.js
+++ src/ggrc/assets/javascripts/components/tree/sub-tree-expander.js
@@ -9,21 +9,13 @@
   GGRC.Components('subTreeExpander', {
     tag: 'sub-tree-expander',
     template: '<content/>',
-    scope: {
-      expanded: null
-    },
-    events: {
-      'a click': function () {
-        var treeViewEl = this.element.closest('.cms_controllers_tree_view_node');
-        var ctrl = treeViewEl.control();
-
-        if (this.scope.expanded) {
-          treeViewEl.find('.parent-related').hide();
-        } else {
-          ctrl.display_subtrees(true);
-        }
-
-        this.scope.attr('expanded', !this.scope.attr('expanded'));
+    viewModel: {
+      expanded: null,
+      disabled: false,
+      onChangeState: null,
+      onExpand: function () {
+        this.attr('expanded', !this.attr('expanded'));
+        this.onChangeState(this.attr('expanded'));
       }
     }
   });
