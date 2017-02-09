12d376a66edc122f6fe71fb669914a66d471a05e
akaralionak <akaralionak@google.com>
2017-02-09 18:45:35 +0300

    Add Detailed Business Object Component to present additional information

---
+++ src/ggrc/assets/javascripts/components/object-list-item/detailed-business-object-list-item.js
@@ -0,0 +1,49 @@
+/*!
+ Copyright (C) 2017 Google Inc., authors, and contributors
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
+(function (can, GGRC) {
+  'use strict';
+
+  var tpl = can.view(GGRC.mustache_path +
+    '/components/object-list-item/detailed-business-object-list-item.mustache');
+  var tag = 'detailed-business-object-list-item';
+  /**
+   * Assessment specific mapped objects popover view component
+   */
+  can.Component.extend({
+    tag: tag,
+    template: tpl,
+    viewModel: {
+      instance: {},
+      define: {
+        isSnapshot: {
+          get: function () {
+            return this.attr('instance.type') === 'Snapshot';
+          }
+        },
+        itemData: {
+          get: function () {
+            return this.attr('isSnapshot') ?
+              this.attr('instance.revision.content') :
+              this.attr('instance');
+          }
+        },
+        objectLink: {
+          get: function () {
+            return this.attr('itemData.viewLink');
+          }
+        },
+        objectTitle: {
+          get: function () {
+            return this.attr('itemData.title') ||
+              this.attr('itemData.description_inline') ||
+              this.attr('itemData.name') ||
+              this.attr('itemData.email') || false;
+          }
+        }
+      }
+    }
+  });
+})(window.can, window.GGRC);
