c1fc416e3c59e5bff78c02fe121183c2aaf370ef
akaralionak <akaralionak@google.com>
2017-02-13 15:25:14 +0300

    Create Show Related Assessments Button Component to encapsulate inner logic of showing Related Assessments

---
+++ src/ggrc/assets/javascripts/components/show-related-assessments-button/show-related-assessments-button.js
@@ -0,0 +1,27 @@
+/*!
+ Copyright (C) 2017 Google Inc.
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
+(function (can, GGRC) {
+  'use strict';
+
+  var template = can.view(GGRC.mustache_path +
+    '/components/show-related-assessments-button' +
+    '/show-related-assessments-button.mustache');
+
+  can.Component.extend({
+    tag: 'show-related-assessments-button',
+    template: template,
+    viewModel: {
+      instance: null,
+      state: {
+        open: false
+      },
+      modalTitle: 'Related Assessments',
+      showRelatedAssessments: function () {
+        this.attr('state.open', true);
+      }
+    }
+  });
+})(window.can, window.GGRC);
