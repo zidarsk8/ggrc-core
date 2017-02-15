868f02cb409b633a0f8e8953b9d6b59c56b0b066
Peter Lamut <inbox@peterlamut.com>
2017-02-15 16:27:20 +0100

    Add default Assessment notification settings
    Comment notifications on the Assessment model are now enabled by
    default, and all Assessment user roles are notified.

---
+++ src/ggrc/migrations/versions/20170215144723_2f1cee67a8f3_add_default_assessment_notifications_.py
@@ -0,0 +1,68 @@
+# Copyright (C) 2017 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""
+Add default Assessment notifications settings.
+
+Create Date: 2017-02-15 14:47:23.772467
+"""
+# disable Invalid constant name pylint warning for mandatory Alembic variables.
+# pylint: disable=invalid-name
+
+from alembic import op
+
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+
+# revision identifiers, used by Alembic.
+revision = "2f1cee67a8f3"
+down_revision = "1e65abd56ccb"
+
+
+def upgrade():
+  """Enable sending Assessment comment notifications by default.
+
+  The notifications should (by default) be sent to all people roles assigned
+  to a particular Assessment.
+  """
+  sql = """
+    UPDATE assessments
+    SET send_by_default = 1
+    WHERE send_by_default IS NULL
+  """
+  op.execute(sql)
+
+  sql = """
+    UPDATE assessments
+    SET recipients = "Assessor,Creator,Verifier"
+    WHERE recipients IS NULL
+  """
+  op.execute(sql)
+
+  op.alter_column(
+      "assessments", "send_by_default",
+      server_default=u"1",
+      existing_type=sa.Integer(),
+      existing_nullable=True)
+
+  op.alter_column(
+      "assessments", "recipients",
+      server_default=u"Assessor,Creator,Verifier",
+      existing_type=mysql.VARCHAR(length=250),
+      existing_nullable=True)
+
+
+def downgrade():
+  """Remove default settings for Assessment comment notifications."""
+  op.alter_column(
+      "assessments", "send_by_default",
+      server_default=None,
+      existing_type=sa.Integer(),
+      existing_nullable=True)
+
+  op.alter_column(
+      "assessments", "recipients",
+      server_default=None,
+      existing_type=mysql.VARCHAR(length=250),
+      existing_nullable=True)
