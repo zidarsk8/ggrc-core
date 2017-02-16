7058746e5c4e062f743a838eb8771d810151cf31
Alexander Novik <al.novik88@gmail.com>
2017-02-16 12:31:53 +0300

    Linting ggrc_risks module

--- src/ggrc_risks/assets/javascripts/models/threat.js
+++ src/ggrc_risks/assets/javascripts/models/threat.js
@@ -3,37 +3,37 @@
  * Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
  */
 
-
 (function (can) {
-  can.Model.Cacheable("CMS.Models.Threat", {
-    root_object: "threat",
-    root_collection: "threats",
-    category: "risk",
-    findAll: "GET /api/threats",
-    findOne: "GET /api/threats/{id}",
-    create: "POST /api/threats",
-    update: "PUT /api/threats/{id}",
-    destroy: "DELETE /api/threats/{id}",
+  can.Model.Cacheable('CMS.Models.Threat', {
+    root_object: 'threat',
+    root_collection: 'threats',
+    category: 'risk',
+    findAll: 'GET /api/threats',
+    findOne: 'GET /api/threats/{id}',
+    create: 'POST /api/threats',
+    update: 'PUT /api/threats/{id}',
+    destroy: 'DELETE /api/threats/{id}',
     mixins: ['ownable', 'contactable', 'unique_title', 'ca_update'],
     is_custom_attributable: true,
     attributes: {
-      context: "CMS.Models.Context.stub",
-      contact: "CMS.Models.Person.stub",
-      owners: "CMS.Models.Person.stubs",
-      modified_by: "CMS.Models.Person.stub",
-      object_people: "CMS.Models.ObjectPerson.stubs",
-      people: "CMS.Models.Person.stubs",
-      related_sources: "CMS.Models.Relationship.stubs",
-      related_destinations: "CMS.Models.Relationship.stubs",
-      object_objectives: "CMS.Models.ObjectObjective.stubs",
-      objectives: "CMS.Models.Objective.stubs",
-      object_controls: "CMS.Models.ObjectControl.stubs",
-      controls: "CMS.Models.Control.stubs",
-      object_sections: "CMS.Models.ObjectSection.stubs",
-      sections: "CMS.Models.get_stubs"
+      context: 'CMS.Models.Context.stub',
+      contact: 'CMS.Models.Person.stub',
+      owners: 'CMS.Models.Person.stubs',
+      modified_by: 'CMS.Models.Person.stub',
+      object_people: 'CMS.Models.ObjectPerson.stubs',
+      people: 'CMS.Models.Person.stubs',
+      related_sources: 'CMS.Models.Relationship.stubs',
+      related_destinations: 'CMS.Models.Relationship.stubs',
+      object_objectives: 'CMS.Models.ObjectObjective.stubs',
+      objectives: 'CMS.Models.Objective.stubs',
+      object_controls: 'CMS.Models.ObjectControl.stubs',
+      controls: 'CMS.Models.Control.stubs',
+      object_sections: 'CMS.Models.ObjectSection.stubs',
+      sections: 'CMS.Models.get_stubs'
     },
     tree_view_options: {
-      add_item_view : GGRC.mustache_path + "/base_objects/tree_add_item.mustache",
+      add_item_view: GGRC.mustache_path +
+      '/base_objects/tree_add_item.mustache',
       attr_view: GGRC.mustache_path + '/base_objects/tree-item-attr.mustache',
       attr_list: can.Model.Cacheable.attr_list.concat([
         {attr_title: 'URL', attr_name: 'url'},
@@ -45,8 +45,10 @@
     },
     statuses: ['Draft', 'Deprecated', 'Active'],
     init: function () {
-      this._super && this._super.apply(this, arguments);
-      this.validatePresenceOf("title");
+      if (this._super) {
+        this._super.apply(this, arguments);
+      }
+      this.validatePresenceOf('title');
     }
   }, {});
 })(window.can);
