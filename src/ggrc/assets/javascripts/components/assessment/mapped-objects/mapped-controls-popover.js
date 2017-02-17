51feab81a447d95c4513019fc31e077adc242ef6
akaralionak <akaralionak@google.com>
2017-02-17 16:34:02 +0300

    Revert back Tests for Assessment Mapped Controls Components

--- src/ggrc/assets/javascripts/components/assessment/mapped-objects/mapped-controls-popover.js
+++ src/ggrc/assets/javascripts/components/assessment/mapped-objects/mapped-controls-popover.js
@@ -9,8 +9,8 @@
   var tpl = can.view(GGRC.mustache_path +
     '/components/assessment/mapped-objects/mapped-controls-popover.mustache');
   var tag = 'assessment-mapped-controls-popover';
-  /**
-   * Assessment specific mapped controls popover view component
+  /*
+   Default Response Object
    */
   var defaultResponseArr = [{
     Snapshot: {
@@ -21,56 +21,94 @@
       values: []
     }
   }];
-
-  can.Component.extend({
-    tag: tag,
-    template: tpl,
-    viewModel: {
+  /**
+   * ViewModel for Assessment Mapped Controls Popover.
+   * @type {can.Map}
+   */
+  var viewModel = GGRC.Components.ViewModels.AssessmentMappedControlsPopover =
+    can.Map.extend({
       define: {
+        /**
+         * Public Attribute defining ID of Control's Snapshot
+         * @type {Number}
+         * @public
+         */
         instanceId: {
-          type: Number,
+          type: 'number',
           value: 0
         },
+        /**
+         * Private Attribute defining array of requested Objects, Types and Fields of Objects
+         * @type {Array}
+         * @private
+         */
         queries: {
           type: '*',
           value: [
-            {type: 'objectives'},
-            {type: 'regulations'}
+            {type: 'objectives', objName: 'Objective', fields: ['revision']},
+            {type: 'regulations', objName: 'Regulation', fields: ['revision']}
           ]
         },
+        /**
+         * Public Attribute defining necessity of data loading
+         * Should start loading only in case false is passed
+         * @type {Boolean}
+         * @public
+         */
         performLoading: {
-          type: Boolean,
+          type: 'boolean',
           value: false,
           set: function (newValue) {
             if (newValue) {
+              // Start loading only if newValue is true
               this.loadItems();
             }
             return newValue;
           }
+        },
+        /**
+         * Private attribute to indicate loading state
+         * @private
+         */
+        isLoading: {
+          type: 'boolean',
+          value: false
+        },
+        objectives: {
+          value: []
+        },
+        regulations: {
+          value: []
         }
       },
-      objectives: [],
-      regulations: [],
-      isLoading: false,
+      /**
+       * Generate params required for Query API
+       * @param {Number} id - Id of Control's Snapshot
+       * @return {Object} request query object
+       */
       getParams: function (id) {
         var params = {};
+        // Right now we load only Snapshots of related Objectives and Regulations
         var relevant = {
+          type: 'Snapshot',
           id: id,
-          type: 'Snapshot'
+          operation: 'relevant'
         };
-        var fields = ['revision'];
-        params.data = [
-          GGRC.Utils.Snapshots.transformQuery(
-            GGRC.Utils.QueryAPI.buildParam('Objective', {}, relevant, fields)
-          ),
-          GGRC.Utils.Snapshots.transformQuery(
-            GGRC.Utils.QueryAPI.buildParam('Regulation', {}, relevant, fields)
-          )
-        ];
+        params.data = this.attr('queries')
+          .map(function (query) {
+            var resultingQuery = GGRC.Utils.QueryAPI
+                .buildParam(query.objName, {}, relevant, query.fields);
+            return GGRC.Utils.Snapshots.transformQuery(resultingQuery);
+          });
         return params;
       },
+      /**
+       * Parse raw response data from Query API and set appropriate properties
+       * @param {Array} responseArr - Raw Array of Data requested from Query API
+       */
       setItems: function (responseArr) {
         responseArr.forEach(function (item, i) {
+          // Order of items in Queries Object is the same as order of Requested Queries
           var type = this.attr('queries')[i].type;
           this.attr(type).replace(item.Snapshot.values
             .map(function (item) {
@@ -101,6 +139,14 @@
             this.attr('isLoading', false);
           }.bind(this));
       }
-    }
+    });
+  /**
+   * Specific Wrapper Component to present Controls only inner popover data.
+   * Should Load on expand Related Objectives and Regulations
+   */
+  can.Component.extend({
+    tag: tag,
+    template: tpl,
+    viewModel: viewModel
   });
 })(window.can, window.GGRC);
--- src/ggrc/assets/javascripts/components/assessment/mapped-objects/tests/mapped-controls-popover_spec.js
+++ src/ggrc/assets/javascripts/components/assessment/mapped-objects/tests/mapped-controls-popover_spec.js
@@ -2,76 +2,157 @@
  Copyright (C) 2017 Google Inc.
  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
  */
-/*
-describe('GGRC.Components.mappedControlsPopover', function () {
+
+describe('GGRC.Components.AssessmentMappedControlsPopover', function () {
   'use strict';
 
-  var scope;
+  var viewModel;
+  var defaultResponseArr = [{
+    Snapshot: {
+      values: [
+        {revision: {content: {}}},
+        {revision: {content: {}}}
+      ]
+    }
+  }, {
+    Snapshot: {
+      values: []
+    }
+  }];
+  var noDataResponseArr = [{
+    Snapshot: {
+      values: []
+    }
+  }, {
+    Snapshot: {
+      values: []
+    }
+  }];
+  var params = {
+    data: [
+      {
+        object_name: 'Snapshot',
+        filters: {
+          expression: {
+            left: {
+              left: 'child_type',
+              op: {name: '='},
+              right: 'Objective'},
+            op: {name: 'AND'},
+            right: {
+              object_name: 'Snapshot',
+              op: {name: 'relevant'},
+              ids: ['1']
+            }
+          },
+          keys: [],
+          order_by: {
+            keys: [],
+            order: '',
+            compare: null
+          }
+        },
+        fields: ['revision']
+      }, {
+        object_name: 'Snapshot',
+        filters: {
+          expression: {
+            left: {
+              left: 'child_type',
+              op: {name: '='},
+              right: 'Regulation'
+            },
+            op: {name: 'AND'},
+            right: {
+              object_name: 'Snapshot',
+              op: {name: 'relevant'},
+              ids: ['1']
+            }
+          },
+          keys: [],
+          order_by: {
+            keys: [],
+            order: '',
+            compare: null
+          }
+        },
+        fields: ['revision']
+      }]
+  };
 
-  beforeAll(function () {
-    var Component = GGRC.Components.get('mappedControlsPopover');
-    scope = Component.prototype.scope;
-    scope = new can.Map(scope);
+  beforeEach(function () {
+    viewModel = new GGRC.Components.ViewModels
+      .AssessmentMappedControlsPopover();
   });
 
-  describe('creates params for request', function () {
-    beforeEach(function () {
-      scope.attr('item', {});
-      scope.attr('item.data', {
-        id: 1050,
-        type: 'Control'
-      });
+  describe('By default shouldn\'t load any data', function () {
+    it('"objectives" and ' +
+      ' "regulations" array should zero length' +
+      '"performLoading" should be false', function () {
+      expect(viewModel.attr('performLoading')).toEqual(false);
+      expect(viewModel.attr('objectives').length).toEqual(0);
+      expect(viewModel.attr('regulations').length).toEqual(0);
+      expect(viewModel.attr('instanceId')).toEqual(0);
     });
-    it('when itemData.id is 1050', function () {
-      var id = scope.attr('item.data.id');
-      var type = scope.attr('item.data.type');
-      var params = scope.getParams(id, type);
-      var resultParams = [{
-        object_name: 'Objective',
-        filters: {
-          expression: {
-            object_name: 'Control',
-            op: {
-              name: 'relevant'
-            },
-            ids: ['1050']
-          },
-          keys: [],
-          order_by: {
-            keys: [],
-            order: '',
-            compare: null
-          }
-        },
-        fields: ['id', 'title', 'notes', 'description']
-      }, {
-        object_name: 'Regulation',
-        filters: {
-          expression: {
-            object_name: 'Control',
-            op: {
-              name: 'relevant'
-            },
-            ids: ['1050']
-          },
-          keys: [],
-          order_by: {
-            keys: [],
-            order: '',
-            compare: null
-          }
-        },
-        fields: ['id', 'title', 'notes', 'description']
-      }];
-      params.data.forEach(function (item, index) {
-        expect(item.object_name).toEqual(resultParams[index].object_name);
-        expect(item.filters.expression.object_name)
-          .toEqual(resultParams[index].filters.expression.object_name);
-        expect(item.filters.expression.ids)
-          .toEqual(resultParams[index].filters.expression.ids);
-        expect(item.fields).toEqual(resultParams[index].fields);
-      });
+  });
+
+  describe('On "performLoading" attribute set to true', function () {
+    beforeEach(function () {
+      spyOn(viewModel, 'loadItems');
+
+      viewModel.attr('performLoading', true);
+    });
+
+    it('should start loading of required data', function () {
+      expect(viewModel.loadItems).toHaveBeenCalled();
+    });
+
+    it('should start loading of required data', function () {
+      expect(viewModel.loadItems.calls.count()).toEqual(1);
+    });
+  });
+
+  describe('On "performLoading" attribute set to false', function () {
+    beforeEach(function () {
+      spyOn(viewModel, 'loadItems');
+
+      viewModel.attr('performLoading', false);
+    });
+
+    it('should start loading of required data', function () {
+      expect(viewModel.loadItems.calls.count()).toEqual(0);
+    });
+  });
+
+  describe('On "instanceId" attribute set to 0 (no modification)' +
+    ' and "performLoading" attribute set to true', function () {
+    beforeEach(function () {
+      spyOn(viewModel, 'setItems');
+
+      viewModel.attr('instanceId', 0);
+      viewModel.attr('performLoading', true);
+    });
+
+    it('should start loading of required data', function () {
+      expect(viewModel.setItems).toHaveBeenCalled();
+    });
+
+    it('should start loading of required data', function () {
+      expect(viewModel.setItems).toHaveBeenCalledWith(noDataResponseArr);
+    });
+  });
+
+  describe('#setItems', function () {
+    it('should set correct objectives and regulations arrays', function () {
+      viewModel.setItems(defaultResponseArr);
+      expect(viewModel.attr('objectives').length).toEqual(2);
+      expect(viewModel.attr('regulations').length).toEqual(0);
+    });
+  });
+  describe('#getParams', function () {
+    it('should set correct params object', function () {
+      var result = JSON.stringify(viewModel.getParams(1));
+      expect(result).toBe(JSON.stringify(params));
     });
   });
 });
-*/
