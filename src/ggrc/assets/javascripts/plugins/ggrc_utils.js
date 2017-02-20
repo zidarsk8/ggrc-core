efe53902b44c43ca9fd40ba3d57637203e7ebfd4
selahssea <anton.snytchenko@gmail.com>
2017-02-20 11:26:40 +0300

    Display snapshot in second tier of tree view

--- src/ggrc/assets/javascripts/plugins/ggrc_utils.js
+++ src/ggrc/assets/javascripts/plugins/ggrc_utils.js
@@ -1062,6 +1062,7 @@
      * @return {Object} The object
      */
     function toObject(instance) {
+      var object;
       var model = CMS.Models[instance.child_type];
       var content = instance.revision.content;
       var type = model.root_collection;
@@ -1074,7 +1075,9 @@
       content.selfLink = content.snapshot.selfLink;
       content.type = instance.child_type;
       content.id = instance.id;
-      return new model(content);
+      object = new model(content);
+      model.removeFromCacheById(content.id);  /* removes snapshot object from cache */
+      return object;
     }
 
     /**
