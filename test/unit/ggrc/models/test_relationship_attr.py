43ee0e468eb3761fc297ca9944ffe17275479c46
Alieh Rymasheuski <alieh.rymasheuski@gmail.com>
2017-02-21 18:18:21 +0300

    Minimize the tests for RelationshipAttr

--- test/unit/ggrc/models/test_relationship_attr.py
+++ test/unit/ggrc/models/test_relationship_attr.py
@@ -1,25 +1,20 @@
 # Copyright (C) 2017 Google Inc.
+
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
-from ggrc import db
+import mock
+import unittest
+
+import werkzeug.exceptions
+
+import ggrc
 from ggrc.models.mixins import Base
 from ggrc.models.relationship import Relationship
 from ggrc.models.relationship import RelationshipAttr
-from integration.ggrc import TestCase
-import ggrc
-import ggrc.builder
-import ggrc.services
-import random
-import werkzeug.exceptions
 
 
-class RelationshipTestMockModel(Base, ggrc.db.Model):
-  __tablename__ = 'relationship_test_mock_model'
-  foo = db.Column(db.String)
-
-  # REST properties
-  _publish_attrs = ['modified_by_id', 'foo']
-  _update_attrs = ['foo']
+class RelationshipAttrTestMockModel(Base, ggrc.db.Model):
+  __tablename__ = 'relationship_attr_test_mock_model'
 
   @staticmethod
   def _validate_relationship_attr(cls, source, destination, old, name, value):
@@ -34,50 +29,35 @@ class RelationshipTestMockModel(Base, ggrc.db.Model):
     return value
 
 
-class TestRelationship(TestCase):
+class TestRelationshipAttr(unittest.TestCase):
 
   def setUp(self):
-    super(TestRelationship, self).setUp()
-    if RelationshipTestMockModel.__table__.exists(db.engine):
-      RelationshipTestMockModel.__table__.drop(db.engine)
-    RelationshipTestMockModel.__table__.create(db.engine)
-    with self.client.session_transaction() as session:
-      session['permissions'] = {
-          "__GGRC_ADMIN__": {"__GGRC_ALL__": {"contexts": [0]}}
-      }
+    super(TestRelationshipAttr, self).setUp()
 
-    self.m1 = self.mock_model()
-    self.m2 = self.mock_model()
-    self.rel = Relationship(source=self.m1, destination=self.m2)
-    db.session.add(self.rel)
+    m1 = RelationshipAttrTestMockModel()
+    m2 = RelationshipAttrTestMockModel()
+    self.rel = Relationship(source=m1, destination=m2)
 
-  def mock_model(self, id=None, modified_by_id=1, **kwarg):
-    if 'id' not in kwarg:
-      kwarg['id'] = random.randint(0, 999999999)
-    if 'modified_by_id' not in kwarg:
-      kwarg['modified_by_id'] = 1
-    mock = RelationshipTestMockModel(**kwarg)
-    return mock
+    self.mapper_mock = mock.Mock(name='mapper')
+    self.connection_mock = mock.Mock(name='connection')
 
   def test_attrs_validation_ok(self):
     self.rel.attrs["validated_attr"] = "123"
 
-    db.session.commit()
+    # not raises ValueError
+    self.rel.validate_attrs(self.mapper_mock, self.connection_mock, self.rel)
 
   def test_attrs_validation_invalid_attr(self):
     self.rel.attrs["foo"] = "bar"
 
     with self.assertRaises(werkzeug.exceptions.BadRequest):
-      db.session.commit()
+      self.rel.validate_attrs(self.mapper_mock, self.connection_mock, self.rel)
 
   def test_attrs_validation_invalid_value(self):
     self.rel.attrs["validated_attr"] = "wrong value"
 
     with self.assertRaises(werkzeug.exceptions.BadRequest):
-      db.session.commit()
-
-
-class TestRelationshipAttr(TestCase):
+      self.rel.validate_attrs(self.mapper_mock, self.connection_mock, self.rel)
 
   def test_gather_validators(self):
     class ValidatorParent(object):
