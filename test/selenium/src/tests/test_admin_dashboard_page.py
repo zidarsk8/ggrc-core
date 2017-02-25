11bfffc31a36ea140c85aac4e54de7b94f73e19c
AleksNeStu <A.V.Nesterovich@gmail.com>
2017-02-26 02:57:47 +0300

    Improve code style of tests modules

--- test/selenium/src/tests/test_admin_dashboard_page.py
+++ test/selenium/src/tests/test_admin_dashboard_page.py
@@ -1,17 +1,18 @@
 # Copyright (C) 2017 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
-"""Admin dashboard page smoke tests"""
+"""Admin dashboard page smoke tests."""
 # pylint: disable=no-self-use
 # pylint: disable=invalid-name
 # pylint: disable=too-few-public-methods
 # pylint: disable=protected-access
-import random
 
-import pytest
+import random
 import re
 
+import pytest
+
 from lib import base, constants
-from lib.constants import objects
+from lib.constants import objects, messages
 from lib.constants.element import AdminWidgetCustomAttrs
 from lib.entities.entities_factory import CAFactory
 from lib.page import dashboard
@@ -21,7 +22,6 @@ class TestAdminDashboardPage(base.Test):
   """Tests for the admin dashboard page."""
   _role_el = constants.element.AdminWidgetRoles
   _event_el = constants.element.AdminWidgetEvents
-  err_msg_format = constants.messages.ERR_MSG_FORMAT
 
   @pytest.fixture(scope="function")
   def admin_dashboard(self, selenium):
@@ -30,27 +30,26 @@ class TestAdminDashboardPage(base.Test):
 
   @pytest.mark.smoke_tests
   def test_roles_widget(self, admin_dashboard):
-    """Check count and content of role scopes"""
+    """Check count and content of role scopes."""
     admin_roles_widget = admin_dashboard.select_roles()
     expected_dict = self._role_el.ROLE_SCOPES_DICT
     actual_dict = admin_roles_widget.get_role_scopes_text_as_dict()
     assert admin_dashboard.tab_roles.member_count == len(expected_dict)
-    assert expected_dict == actual_dict, self.err_msg_format.format(
+    assert expected_dict == actual_dict, messages.ERR_MSG_FORMAT.format(
         expected_dict, actual_dict)
 
   @pytest.mark.smoke_tests
   def test_events_widget_tree_view_has_data(self, admin_dashboard):
-    """Confirms tree view has at least one data row in valid format"""
+    """Confirms tree view has at least one data row in valid format."""
     admin_events_tab = admin_dashboard.select_events()
-    list_of_items = admin_events_tab.get_events()
-    assert len(list_of_items) > 0
-    items_with_incorrect_format = \
-        [getattr(item, 'text') for item in list_of_items if
-            not(re.compile(self._event_el.TREE_VIEW_ROW_REGEXP).match(
-                getattr(item, 'text')))]
+    list_items = admin_events_tab.get_events()
+    assert len(list_items) > 0
+    items_with_incorrect_format = [
+        getattr(item, 'text') for item in list_items if not
+        re.compile(self._event_el.TREE_VIEW_ROW_REGEXP).
+        match(getattr(item, 'text'))]
     assert items_with_incorrect_format == []
-    assert admin_events_tab.widget_header.text == \
-        self._event_el.WIDGET_HEADER
+    assert admin_events_tab.widget_header.text == self._event_el.WIDGET_HEADER
 
   @pytest.mark.smoke_tests
   def test_check_ca_groups(self, admin_dashboard):
@@ -61,22 +60,21 @@ class TestAdminDashboardPage(base.Test):
     act_ca_groups_set = set([item.text for item in ca_widget.get_items_list()])
     exp_ca_groups_set = set([objects.get_normal_form(item) for item in
                              objects.ALL_CA_OBJECTS])
-    assert exp_ca_groups_set == act_ca_groups_set, self.err_msg_format.format(
-        exp_ca_groups_set, act_ca_groups_set)
+    assert exp_ca_groups_set == act_ca_groups_set, (
+        messages.ERR_MSG_FORMAT.format(
+            exp_ca_groups_set, act_ca_groups_set))
 
   @pytest.mark.smoke_tests
-  @pytest.mark.parametrize("ca_type, def_type",
-                           [(type_item, random.choice(objects.ALL_CA_OBJECTS))
-                            for type_item in
-                            AdminWidgetCustomAttrs.ALL_ATTRS_TYPES])
+  @pytest.mark.parametrize(
+      "ca_type, def_type",
+      [(type_item, random.choice(objects.ALL_CA_OBJECTS)) for type_item in
+       AdminWidgetCustomAttrs.ALL_ATTRS_TYPES])
   def test_add_global_ca(self, admin_dashboard, ca_type, def_type):
     """Create different types of Custom Attribute on Admin Dashboard."""
     exp_custom_attribute = CAFactory().create(
-        ca_type=ca_type,
-        definition_type=def_type)
+        ca_type=ca_type, definition_type=def_type)
     ca_widget = admin_dashboard.select_custom_attributes()
     ca_widget.add_custom_attribute(exp_custom_attribute)
     act_ca_list = ca_widget.get_custom_attributes_list(
-        exp_custom_attribute.definition_type
-    )
+        exp_custom_attribute.definition_type)
     assert exp_custom_attribute in act_ca_list
--- test/selenium/src/tests/test_my_work_page.py
+++ test/selenium/src/tests/test_my_work_page.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2017 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
-"""All smoke tests relevant to my work page."""
+"""My Work page smoke tests."""
 # pylint: disable=no-self-use
 # pylint: disable=invalid-name
 # pylint: disable=too-few-public-methods
@@ -15,82 +15,63 @@ from lib.utils import conftest_utils, selenium_utils
 
 
 class TestMyWorkPage(base.Test):
-  """Tests the my work page, a part of smoke tests, section 2"""
+  """Tests the My Work page, a part of smoke tests, section 2."""
 
   @pytest.mark.smoke_tests
   def test_horizontal_nav_bar_tabs(self, selenium, new_controls_rest):
     """Tests that several objects in a widget can be deleted sequentially."""
     selenium.get(dashboard.Dashboard.URL)
-    controls_widget = dashboard\
-        .Dashboard(selenium)\
-        .select_controls()
-
+    controls_widget = dashboard.Dashboard(selenium).select_controls()
     for _ in xrange(controls_widget.member_count):
       counter = controls_widget.get_items_count()
-      controls_widget\
-          .select_nth_member(0)\
-          .press_object_settings()\
-          .select_delete()\
-          .confirm_delete()
+      (controls_widget.select_nth_member(0).
+       press_object_settings().select_delete().confirm_delete())
       controls_widget.wait_member_deleted(counter)
     assert generic_widget.Controls(selenium).members_listed == []
 
   @pytest.mark.smoke_tests
   def test_redirect(self, selenium):
     """Tests if the user is redirected to the My Work page after clicking on
-    the my work button in user dropdown"""
+    the my work button in user dropdown."""
     conftest_utils.navigate_to_page_with_lhn(selenium)
-
-    dashboard.Header(selenium)\
-        .open_user_list()\
-        .select_my_work()
-
+    dashboard.Header(selenium).open_user_list().select_my_work()
     assert selenium.current_url == dashboard.Dashboard.URL
 
   @pytest.mark.smoke_tests
   def test_lhn_stays_expanded(self, selenium):
     """Tests if, after opening the LHN, it slides out and stays expanded."""
     conftest_utils.navigate_to_page_with_lhn(selenium)
-
     lhn_menu = dashboard.Header(selenium).open_lhn_menu()
     initial_position = lhn_menu.my_objects.element.location
-
     selenium_utils.wait_until_stops_moving(lhn_menu.my_objects.element)
     selenium_utils.hover_over_element(
-        selenium,
-        dashboard.Header(selenium).button_my_tasks.element)
-
-    assert initial_position == \
-        lhn.Menu(selenium).my_objects.element.location
+        selenium, dashboard.Header(selenium).button_my_tasks.element)
+    assert initial_position == lhn.Menu(selenium).my_objects.element.location
 
   @pytest.mark.smoke_tests
   def test_lhn_remembers_tab_state(self, selenium):
     """Tests if LHN remembers which tab is selected (my or all objects) after
-    closing it"""
+    closing it."""
     conftest_utils.navigate_to_page_with_lhn(selenium)
     header = dashboard.Header(selenium)
-
     # check if my objects tab saves state
     lhn_menu = header.open_lhn_menu()
     lhn_menu.select_my_objects()
     header.close_lhn_menu()
     header.open_user_list()
     new_lhn = header.open_lhn_menu()
-    assert selenium_utils.is_value_in_attr(
-        new_lhn.my_objects.element) is True
-
+    assert selenium_utils.is_value_in_attr(new_lhn.my_objects.element) is True
     # check if all objects tab saves state
     lhn_menu = header.open_lhn_menu()
     lhn_menu.select_all_objects()
     header.close_lhn_menu()
     header.open_user_list()
     new_lhn = header.open_lhn_menu()
-    assert selenium_utils.is_value_in_attr(
-        new_lhn.all_objects.element) is True
+    assert selenium_utils.is_value_in_attr(new_lhn.all_objects.element) is True
 
   @pytest.mark.smoke_tests
   def test_lhn_pin(self, selenium):
-    """Tests if the pin is present and if it's default state is off"""
+    """Tests if the pin is present and if it's default state is off."""
     conftest_utils.navigate_to_page_with_lhn(selenium)
     lhn_menu = dashboard.Header(selenium).open_lhn_menu()
     assert lhn_menu.pin.is_activated is False
@@ -102,9 +83,7 @@ class TestMyWorkPage(base.Test):
     exception if they're not present."""
     conftest_utils.navigate_to_page_with_lhn(selenium)
     user_list = dashboard.Header(selenium).open_user_list()
-
     user_list.checkbox_daily_digest.click()
     user_list.checkbox_daily_digest.element.get_attribute("disabled")
-
     # restore previous state
     user_list.checkbox_daily_digest.click()
