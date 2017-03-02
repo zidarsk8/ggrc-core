8fc720393de344fa8109e3faa822bcbcf0a5d63f
AleksNeStu <A.V.Nesterovich@gmail.com>
2017-03-02 16:21:48 +0300

    Extend factories to work with audit cloning

--- test/selenium/src/lib/entities/entities_factory.py
+++ test/selenium/src/lib/entities/entities_factory.py
@@ -1,12 +1,13 @@
 # Copyright (C) 2017 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
-"""Module for factories that create entities."""
+"""Factories that create entities."""
 # pylint: disable=too-many-arguments
 # pylint: disable=invalid-name
 # pylint: disable=redefined-builtin
 
 
 import random
+import re
 
 from lib.constants import element, objects, roles, url as const_url
 from lib.constants.element import AdminWidgetCustomAttrs
@@ -25,19 +26,26 @@ class EntitiesFactory(object):
   _obj_audit = objects.get_singular(objects.AUDITS)
   _obj_asmt_tmpl = objects.get_singular(objects.ASSESSMENT_TEMPLATES)
   _obj_asmt = objects.get_singular(objects.ASSESSMENTS)
+  _obj_issue = objects.get_singular(objects.ISSUES)
 
-  all_objs_attrs_names = tuple(entity.Entity().attrs_names_all_entities())
+  all_objs_attrs_names = tuple(entity.Entities().attrs_names_all_entities())
 
   @classmethod
   def update_obj_attrs_values(cls, obj, attrs_names=all_objs_attrs_names,
                               **arguments):
-    """Update the object's (obj) attributes values according to the list of
+    """Update object's (obj) attributes values according to list of
     unique possible objects' names and dictionary of arguments (key = value).
     """
     # pylint: disable=expression-not-assigned
     [setattr(obj, attr_name, arguments[attr_name]) for
         attr_name in attrs_names if arguments.get(attr_name)]
     return obj
+
+  @classmethod
+  def get_obj_copy(cls, obj):
+    """Get copy of object with all attributes."""
+    blank_obj = getattr(entity, obj.__class__.__name__)()
+    return cls.update_obj_attrs_values(obj=blank_obj, **obj.__dict__)
 
   @classmethod
   def _generate_title(cls, obj_type):
@@ -59,8 +67,8 @@ class EntitiesFactory(object):
         mail_name=random_uuid(), domain=domain)
 
 
-class PersonFactory(EntitiesFactory):
-  """Factory class for Person entity."""
+class PersonsFactory(EntitiesFactory):
+  """Factory class for Persons entities."""
 
   @classmethod
   def default(cls):
@@ -73,7 +81,6 @@ class PersonFactory(EntitiesFactory):
   def create(cls, title=None, id=None, href=None, url=None, type=None,
              email=None, authorizations=None):
     """Create Person object.
-
     Random values will be used for title aka name.
     Predictable values will be used for mail and system_wide_role.
     """
@@ -86,7 +93,7 @@ class PersonFactory(EntitiesFactory):
   @classmethod
   def _create_random_person(cls):
     """Create Person entity with randomly filled fields."""
-    random_person = entity.Person()
+    random_person = entity.PersonsEntity()
     random_person.title = cls._generate_title(cls._obj_person)
     random_person.type = cls._obj_person
     random_person.email = cls._generate_email()
@@ -95,14 +102,13 @@ class PersonFactory(EntitiesFactory):
 
 
 class CAFactory(EntitiesFactory):
-  """Factory class for Custom Attribute entity."""
+  """Factory class for Custom Attributes entities."""
 
   @classmethod
   def create(cls, title=None, ca_type=None, definition_type=None, helptext="",
              placeholder=None, multi_choice_options=None, is_mandatory=False,
              ca_global=True):
     """Create CustomAttribute object.
-
     Random values will be used for title, ca_type, definition_type and
     multi_choice_options if they are None.
     """
@@ -118,7 +124,7 @@ class CAFactory(EntitiesFactory):
   @classmethod
   def _create_random_ca(cls):
     """Create CustomAttribute entity with randomly filled fields."""
-    random_ca = entity.CustomAttribute()
+    random_ca = entity.CustomAttributesEntity()
     random_ca.ca_type = random.choice(AdminWidgetCustomAttrs.ALL_ATTRS_TYPES)
     random_ca.title = cls._generate_title(random_ca.ca_type)
     random_ca.definition_type = random.choice(objects.ALL_CA_OBJECTS)
@@ -126,7 +132,7 @@ class CAFactory(EntitiesFactory):
 
   @classmethod
   def _fill_ca_entity_fields(cls, ca_object, **ca_object_fields):
-    """Set the CustomAttributes object's attributes."""
+    """Set CustomAttributes object's attributes."""
     if ca_object_fields.get("ca_type"):
       ca_object.ca_type = ca_object_fields["ca_type"]
       ca_object.title = cls._generate_title(ca_object.ca_type)
@@ -134,20 +140,17 @@ class CAFactory(EntitiesFactory):
       ca_object.definition_type = ca_object_fields["definition_type"]
     if ca_object_fields.get("title"):
       ca_object.title = ca_object_fields["definition_type"]
-
     # "Placeholder" field exists only for Text and Rich Text.
     if (ca_object_fields.get("placeholder") and
         ca_object.ca_type in (AdminWidgetCustomAttrs.TEXT,
                               AdminWidgetCustomAttrs.RICH_TEXT)):
       ca_object.placeholder = ca_object_fields["placeholder"]
-
     if ca_object_fields.get("helptext"):
       ca_object.helptext = ca_object_fields["helptext"]
     if ca_object_fields.get("is_mandatory"):
       ca_object.is_mandatory = ca_object_fields["is_mandatory"]
     if ca_object_fields.get("ca_global"):
       ca_object.ca_global = ca_object_fields["ca_global"]
-
     # "Possible Values" - is a mandatory field for dropdown CustomAttribute.
     if ca_object.ca_type == AdminWidgetCustomAttrs.DROPDOWN:
       if (ca_object_fields.get("multi_choice_options") and
@@ -161,7 +164,6 @@ class CAFactory(EntitiesFactory):
   @classmethod
   def _normalize_ca_definition_type(cls, ca_object):
     """Transform definition type to title form.
-
     Title from used for UI operations.
     For REST operations definition type should be interpreted as
     objects.get_singular().get_object_name_form().
@@ -172,55 +174,53 @@ class CAFactory(EntitiesFactory):
     return ca_object
 
 
-class ProgramFactory(EntitiesFactory):
-  """Factory class for Program entity."""
+class ProgramsFactory(EntitiesFactory):
+  """Factory class for Programs entities."""
 
   @classmethod
   def create(cls, title=None, id=None, href=None, url=None, type=None,
-             manager=None, pr_contact=None, code=None, state=None,
+             manager=None, primary_contact=None, code=None, state=None,
              last_update=None):
     """Create Program object.
-
     Random values will be used for title and code.
-    Predictable values will be used for type, manager, pr_contact, state.
+    Predictable values will be used for type, manager, primary_contact, state.
     """
     program_entity = cls._create_random_program()
     program_entity = cls.update_obj_attrs_values(
         obj=program_entity, title=title, id=id, href=href, url=url, type=type,
-        manager=manager, pr_contact=pr_contact, code=code, state=state,
-        last_update=last_update)
+        manager=manager, primary_contact=primary_contact, code=code,
+        state=state, last_update=last_update)
     return program_entity
 
   @classmethod
   def _create_random_program(cls):
     """Create Program entity with randomly and predictably filled fields."""
     # pylint: disable=protected-access
-    random_program = entity.Program()
+    random_program = entity.ProgramsEntity()
     random_program.title = cls._generate_title(cls._obj_program)
     random_program.type = cls._obj_program
     random_program.code = cls._generate_code()
     random_program.manager = roles.DEFAULT_USER
-    random_program.pr_contact = roles.DEFAULT_USER
-    random_program.state = element.ObjectStates.DRAFT
+    random_program.primary_contact = roles.DEFAULT_USER
+    random_program.state = element.ObjectsStates.DRAFT
     return random_program
 
 
-class ControlFactory(EntitiesFactory):
-  """Factory class for Control entity."""
+class ControlsFactory(EntitiesFactory):
+  """Factory class for Controls entities."""
 
   @classmethod
   def create(cls, title=None, id=None, href=None, url=None, type=None,
-             owner=None, pr_contact=None, code=None, state=None,
+             owner=None, primary_contact=None, code=None, state=None,
              last_update=None):
     """Create Control object.
-
     Random values will be used for title and code.
-    Predictable values will be used for type, owner, pr_contact, state.
+    Predictable values will be used for type, owner, primary_contact, state.
     """
     control_entity = cls._create_random_control()
     control_entity = cls.update_obj_attrs_values(
         obj=control_entity, title=title, id=id, href=href, url=url, type=type,
-        owner=owner, pr_contact=pr_contact, code=code, state=state,
+        owner=owner, primary_contact=primary_contact, code=code, state=state,
         last_update=last_update)
     return control_entity
 
@@ -228,25 +228,43 @@ class ControlFactory(EntitiesFactory):
   def _create_random_control(cls):
     """Create Control entity with randomly and predictably filled fields."""
     # pylint: disable=protected-access
-    random_control = entity.Control()
+    random_control = entity.ControlsEntity()
     random_control.title = cls._generate_title(cls._obj_control)
     random_control.type = cls._obj_control
     random_control.code = cls._generate_code()
     random_control.owner = roles.DEFAULT_USER
-    random_control.pr_contact = roles.DEFAULT_USER
-    random_control.state = element.ObjectStates.DRAFT
+    random_control.primary_contact = roles.DEFAULT_USER
+    random_control.state = element.ObjectsStates.DRAFT
     return random_control
 
 
-class AuditFactory(EntitiesFactory):
+class AuditsFactory(EntitiesFactory):
   """Factory class for Audit entity."""
+
+  @classmethod
+  def clone(cls, audit, count_to_clone=1):
+    """Clone Audit object.
+    Predictable values will be used for title, id, href, url, code.
+    """
+    # pylint: disable=anomalous-backslash-in-string
+    from lib.service.rest_service import ObjectsInfoService
+    actual_count_all_audits = ObjectsInfoService().get_total_count(
+        obj_name=objects.get_normal_form(cls._obj_audit, with_space=False))
+    if actual_count_all_audits == audit.id:
+      return [
+          cls.update_obj_attrs_values(
+              obj=cls.get_obj_copy(audit),
+              title=audit.title + " - copy " + str(num), id=audit.id + num,
+              href=re.sub("\d+$", str(audit.id + num), audit.href),
+              url=re.sub("\d+$", str(audit.id + num), audit.url),
+              code=(cls._obj_audit.upper() + "-" + str(audit.id + num))) for
+          num in xrange(1, count_to_clone + 1)]
 
   @classmethod
   def create(cls, title=None, id=None, href=None, url=None, type=None,
              program=None, audit_lead=None, code=None, status=None,
              last_update=None):
     """Create Audit object.
-
     Random values will be used for title and code.
     Predictable values will be used for type, audit_lead, status.
     """
@@ -261,24 +279,41 @@ class AuditFactory(EntitiesFactory):
   def _create_random_audit(cls):
     """Create Audit entity with randomly and predictably filled fields."""
     # pylint: disable=protected-access
-    random_audit = entity.Audit()
+    random_audit = entity.AuditsEntity()
     random_audit.title = cls._generate_title(cls._obj_audit)
     random_audit.type = cls._obj_audit
     random_audit.code = cls._generate_code()
     random_audit.audit_lead = roles.DEFAULT_USER
-    random_audit.status = element.AuditStates.PLANNED
+    random_audit.status = element.AuditsStates().PLANNED
     return random_audit
 
 
-class AsmtTmplFactory(EntitiesFactory):
-  """Factory class for Assessment Template entity."""
+class AssessmentTemplatesFactory(EntitiesFactory):
+  """Factory class for Assessment Templates entities."""
+
+  @classmethod
+  def clone(cls, asmt_tmpl, count_to_clone=1):
+    """Clone Assessment Template object.
+    Predictable values will be used for title, id, href, url, code.
+    """
+    # pylint: disable=anomalous-backslash-in-string
+    from lib.service.rest_service import ObjectsInfoService
+    actual_count_all_asmt_tmpls = ObjectsInfoService().get_total_count(
+        obj_name=objects.get_normal_form(cls._obj_asmt_tmpl, with_space=False))
+    if actual_count_all_asmt_tmpls == asmt_tmpl.id:
+      return [
+          cls.update_obj_attrs_values(
+              obj=cls.get_obj_copy(asmt_tmpl), id=asmt_tmpl.id + num,
+              href=re.sub("\d+$", str(asmt_tmpl.id + num), asmt_tmpl.href),
+              url=re.sub("\d+$", str(asmt_tmpl.id + num), asmt_tmpl.url),
+              code=("TEMPLATE-" + str(asmt_tmpl.id + num))) for
+          num in xrange(1, count_to_clone + 1)]
 
   @classmethod
   def create(cls, title=None, id=None, href=None, url=None, type=None,
              audit=None, asmt_objects=None, def_assessors=None,
              def_verifiers=None, code=None, last_update=None):
     """Create Assessment Template object.
-
     Random values will be used for title and code.
     Predictable values will be used for type, asmt_objects, def_assessors,
     def_verifiers.
@@ -296,7 +331,7 @@ class AsmtTmplFactory(EntitiesFactory):
     """Create Assessment Template entity with randomly and predictably
     filled fields.
     """
-    random_asmt_tmpl = entity.AsmtTmpl()
+    random_asmt_tmpl = entity.AssessmentTemplatesEntity()
     random_asmt_tmpl.title = cls._generate_title(cls._obj_asmt_tmpl)
     random_asmt_tmpl.type = cls._obj_asmt_tmpl
     random_asmt_tmpl.code = cls._generate_code()
@@ -306,21 +341,18 @@ class AsmtTmplFactory(EntitiesFactory):
     return random_asmt_tmpl
 
 
-class AsmtFactory(EntitiesFactory):
-  """Factory class for Assessment entity."""
+class AssessmentsFactory(EntitiesFactory):
+  """Factory class for Assessments entities."""
 
   @classmethod
   def generate(cls, objs_under_asmt_tmpl, audit):
-    """Generate Assessment object according to:
-    objects' under Assessment Template titles from list objects
-    "objs_under_asmt_tmpl", audit's title from "audit", the total number of
-    objects under Assessment Template.
+    """Generate Assessment object.
+    Predictable values will be used for title, code, audit.
     """
     # pylint: disable=too-many-locals
     from lib.service.rest_service import ObjectsInfoService
     actual_count_all_asmts = ObjectsInfoService().get_total_count(
-        obj_name=objects.get_normal_form(cls._obj_asmt)
-    )
+        obj_name=objects.get_normal_form(cls._obj_asmt, with_space=False))
     return [
         cls.create(
             title=obj_under_asmt_tmpl.title + " assessment for " + audit.title,
@@ -331,32 +363,63 @@ class AsmtFactory(EntitiesFactory):
   @classmethod
   def create(cls, title=None, id=None, href=None, url=None, type=None,
              object=None, audit=None, creators=None, assignees=None,
-             pr_contact=None, is_verified=None, code=None, state=None,
+             primary_contact=None, is_verified=None, code=None, state=None,
              last_update=None):
     """Create Assessment object.
-
     Random values will be used for title and code.
     Predictable values will be used for type, object, creators, assignees,
     state, is_verified.
     """
+    # pylint: disable=too-many-locals
     asmt_entity = cls._create_random_asmt()
     asmt_entity = cls.update_obj_attrs_values(
         obj=asmt_entity, title=title, id=id, href=href, url=url, type=type,
         object=object, audit=audit, creators=creators, assignees=assignees,
-        pr_contact=pr_contact, is_verified=is_verified, code=code, state=state,
-        last_update=last_update)
+        primary_contact=primary_contact, is_verified=is_verified, code=code,
+        state=state, last_update=last_update)
     return asmt_entity
 
   @classmethod
   def _create_random_asmt(cls):
     """Create Assessment entity with randomly and predictably filled fields."""
-    random_asmt = entity.Asmt()
+    random_asmt = entity.AssessmentsEntity()
     random_asmt.title = cls._generate_title(cls._obj_asmt)
     random_asmt.type = cls._obj_asmt
     random_asmt.code = cls._generate_code()
     random_asmt.object = roles.DEFAULT_USER
     random_asmt.creators = roles.DEFAULT_USER
     random_asmt.assignees = roles.DEFAULT_USER
-    random_asmt.state = element.AsmtStates.NOT_STARTED
+    random_asmt.state = element.AssessmentsStates.NOT_STARTED
     random_asmt.is_verified = element.Common.FALSE
     return random_asmt
+
+
+class IssuesFactory(EntitiesFactory):
+  """Factory class for Issues entities."""
+
+  @classmethod
+  def create(cls, title=None, id=None, href=None, url=None, type=None,
+             audit=None, owner=None, primary_contact=None, code=None,
+             state=None, last_update=None):
+    """Create Issue object.
+    Random values will be used for title and code.
+    Predictable values will be used for type, owner, primary_contact, state.
+    """
+    issue_entity = cls._create_random_issue()
+    issue_entity = cls.update_obj_attrs_values(
+        obj=issue_entity, title=title, id=id, href=href, url=url, type=type,
+        audit=audit, owner=owner, primary_contact=primary_contact, code=code,
+        state=state, last_update=last_update)
+    return issue_entity
+
+  @classmethod
+  def _create_random_issue(cls):
+    """Create Issue entity with randomly and predictably filled fields."""
+    random_issue = entity.IssuesEntity()
+    random_issue.title = cls._generate_title(cls._obj_issue)
+    random_issue.type = cls._obj_issue
+    random_issue.code = cls._generate_code()
+    random_issue.owner = roles.DEFAULT_USER
+    random_issue.primary_contact = roles.DEFAULT_USER
+    random_issue.state = element.IssuesStates.DRAFT
+    return random_issue
