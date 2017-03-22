9a3aa7389543e9b29c549694ee46edeb109dfdc6
Miha Zidar <zidarsk8@gmail.com>
2017-03-22 09:32:19 +0000

    Add migration check for audit FK constraint

--- src/ggrc/migrations/versions/20170302155757_2127ea770285_add_audit_fk_to_assessments_and_issues.py
+++ src/ggrc/migrations/versions/20170302155757_2127ea770285_add_audit_fk_to_assessments_and_issues.py
@@ -19,8 +19,40 @@ revision = '2127ea770285'
 down_revision = '1a5ec1ed04af'
 
 
+def _check_invalid_state(table_name):
+  """Check if database is in invalid state for migrating a given table."""
+  connection = op.get_bind()
+  results = connection.execute("""
+      SELECT t.id
+      FROM {table_name} AS t
+      LEFT JOIN contexts AS c ON
+          c.id = t.context_id AND
+          c.related_object_type = "Audit"
+      LEFT JOIN audits AS au ON
+          c.related_object_id = au.id
+      WHERE
+        au.id IS NULL
+  """.format(
+      table_name=table_name,
+  )).fetchall()
+
+  if results:
+    ids = [str(line[0]) for line in results]
+
+    print("There are invalid {table_name} in the database."
+          "\nYou can remove them by running:\n\n"
+          "DELETE FROM {table_name} WHERE id IN ({ids})\n\n".format(
+              table_name=table_name,
+              ids=", ".join(ids),
+          ))
+
+    return True
+  return False
+
+
 def upgrade_table(table_name):
   """Add audit foreign key to a table."""
+
   op.add_column(
       table_name,
       sa.Column("audit_id", sa.Integer(), nullable=True)
@@ -34,14 +66,6 @@ def upgrade_table(table_name):
           c.related_object_id = au.id
       SET
         t.audit_id = au.id
-  """.format(
-      table_name=table_name,
-  ))
-
-  # Simple fix for testing with invalid objects
-  op.execute("""
-      DELETE FROM {table_name}
-      WHERE audit_id IS NULL
   """.format(
       table_name=table_name,
   ))
@@ -65,6 +89,12 @@ def upgrade_table(table_name):
 
 def upgrade():
   """Upgrade database schema and/or data, creating a new revision."""
+
+  bad_assessments = _check_invalid_state("assessments")
+  bad_issues = _check_invalid_state("issues")
+  if bad_assessments or bad_issues:
+    raise Exception("Invalid database state for a given migration")
+
   upgrade_table("assessments")
   upgrade_table("issues")
 
