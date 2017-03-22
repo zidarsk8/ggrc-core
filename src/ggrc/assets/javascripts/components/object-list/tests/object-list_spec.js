1c31f1a0d35bf93bc5e81eaa108b0c948682d19a
akaralionak <akaralionak@google.com>
2017-03-22 15:28:41 +0300

    Simplify Object List Related Components
     - Remove Object List Item Component to avoid useless abstraction layer
     - Simplify {window} click handlers to avoid extra HTML DOM manipulations
     - Simplify Selection Logic
     - Start using {{each}} section renderer to avoid extra repaint on single list item modification

---
+++ src/ggrc/assets/javascripts/components/object-list/tests/object-list_spec.js
@@ -0,0 +1,115 @@
+/*!
+ Copyright (C) 2017 Google Inc.
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
+describe('GGRC.Components.objectList', function () {
+  'use strict';
+
+  var viewModel;
+
+  describe('should have some default values', function () {
+    beforeEach(function () {
+      viewModel = GGRC.Components.getViewModel('objectList');
+    });
+
+    it('and they should be correct', function () {
+      expect(viewModel.attr('items').length).toBe(0);
+      expect(viewModel.attr('itemSelector')).toBe('');
+      expect(viewModel.attr('isLoading')).toBe(false);
+      expect(viewModel.attr('emptyMessage')).toBe('None');
+      expect(viewModel.attr('isInnerClick')).toBe(false);
+      expect(viewModel.attr('selectedItem.el')).toBeNull();
+      expect(viewModel.attr('selectedItem.data')).toBeNull();
+    });
+  });
+
+  describe('modifySelection() method ', function () {
+    var scope = new can.Map(
+      {
+        instance: {
+          type: 'a',
+          id: 1
+        },
+        isSelected: false
+      }
+    );
+
+    var items = new can.List([
+      scope,
+      {
+        instance: {
+          type: 'b',
+          id: 1
+        },
+        isSelected: false
+      }
+    ]);
+    beforeEach(function () {
+      viewModel = GGRC.Components.getViewModel('objectList');
+      viewModel.attr('items', items);
+    });
+
+    it('should select item with correct', function () {
+      viewModel.modifySelection(scope, {}, $('body'));
+      expect(viewModel.attr('items')[0].attr('isSelected')).toBe(true);
+    });
+  });
+
+  describe('clearSelection() method ', function () {
+    var scope;
+    var items;
+
+    beforeEach(function () {
+      scope = new can.Map({
+        instance: {
+          type: 'a',
+          id: 1
+        },
+        isSelected: false
+      });
+      items = new can.List([scope, {
+        instance: {
+          type: 'b',
+          id: 1
+        },
+        isSelected: false
+      }]);
+      viewModel = GGRC.Components.getViewModel('objectList');
+      viewModel.attr('items', items);
+      viewModel.modifySelection(scope, {}, $('body'));
+      viewModel.clearSelection();
+    });
+
+    it('should deselect selected item and empty selection', function () {
+      expect(viewModel.attr('items').attr()
+        .every(function (item) {
+          return !item.isSelected;
+        })).toBe(true);
+      expect(viewModel.attr('selectedItem.el')).toBeNull();
+      expect(viewModel.attr('selectedItem.data')).toBeNull();
+    });
+  });
+
+  describe('onOuterClick() method ', function () {
+
+    beforeEach(function () {
+      viewModel = GGRC.Components.getViewModel('objectList');
+      spyOn(viewModel, 'clearSelection');
+    });
+
+    it('should clear selection ' +
+      'if attribute "isInnerClick" is false', function () {
+      viewModel.onOuterClick();
+      expect(viewModel.clearSelection).toHaveBeenCalled();
+    });
+
+    it('should not clear selection and set attribute "isInnerClick" to false' +
+      'if attribute "isInnerClick" is true', function () {
+      viewModel.attr('isInnerClick', true);
+      viewModel.onOuterClick();
+      expect(viewModel.clearSelection).not.toHaveBeenCalled();
+      expect(viewModel.attr('isInnerClick')).toBe(false);
+    });
+  });
+});
