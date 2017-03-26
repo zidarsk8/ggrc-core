1200a16356a272f1634d121222b4af5313e4608d
Alexander Novik <al.novik88@gmail.com>
2017-03-26 04:26:32 +0300

    Modify show-related-assessments component

--- src/ggrc/assets/javascripts/components/show-related-assessments-button/show-related-assessments-button.js
+++ src/ggrc/assets/javascripts/components/show-related-assessments-button/show-related-assessments-button.js
@@ -14,11 +14,39 @@
     tag: 'show-related-assessments-button',
     template: template,
     viewModel: {
+      define: {
+        cssClasses: {
+          type: String,
+          get: function () {
+            return !this.attr('resetStyles') ?
+              'btn btn-draft btn-extra-gray ' + this.attr('extraBtnCSS') : '';
+          }
+        },
+        resetStyles: {
+          type: Boolean,
+          value: false
+        },
+        showTitle: {
+          type: Boolean,
+          value: true
+        },
+        showIcon: {
+          type: Boolean,
+          value: false
+        },
+        title: {
+          type: String,
+          get: function () {
+            return this.attr('text') || 'Assessments';
+          }
+        }
+      },
       instance: null,
       state: {
         open: false
       },
       extraBtnCSS: '@',
+      text: '@',
       modalTitle: 'Related Assessments',
       showRelatedAssessments: function () {
         this.attr('state.open', true);
