1903ec867bbf6026d74f4a971b470df5950e3ab0
Alexander Novik <al.novik88@gmail.com>
2017-03-29 11:58:47 +0300

    Add tree-view component

---
+++ src/ggrc/assets/javascripts/components/tree/tree-view.js
@@ -0,0 +1,27 @@
+/*!
+ Copyright (C) 2017 Google Inc.
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
+(function (can, GGRC) {
+  'use strict';
+
+  var template = can.view(GGRC.mustache_path +
+    '/components/tree/tree-view.mustache');
+  var viewModel = can.Map.extend({
+    define: {
+    },
+    items: null,
+    displayAttrs: [],
+    loading: false,
+    limitDepthTree: 0
+  });
+
+  GGRC.Components('treeView', {
+    tag: 'tree-view',
+    template: template,
+    viewModel: viewModel,
+    events: {
+    }
+  });
+})(window.can, window.GGRC);
