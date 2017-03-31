4012357932dc0d9acb17076cd321affa6d94cb81
akaralionak <akaralionak@google.com>
2017-03-31 17:25:16 +0300

    Add Extra Loading Logic to Assessment Info Pane Component

--- src/ggrc/assets/javascripts/components/assessment/info-pane/info-pane.js
+++ src/ggrc/assets/javascripts/components/assessment/info-pane/info-pane.js
@@ -16,12 +16,69 @@
     template: tpl,
     viewModel: {
       define: {
-        isLocked: {
-          type: 'htmlbool',
-          value: false
+        mappedSnapshots: {
+          value: function () {
+            return [];
+          }
+        },
+        controls: {
+          get: function () {
+            return this.attr('mappedSnapshots')
+              .filter(function (item) {
+                return item.child_type === 'Control';
+              });
+          }
+        },
+        relatedInformation: {
+          get: function () {
+            return this.attr('mappedSnapshots')
+              .filter(function (item) {
+                return item.child_type !== 'Control';
+              });
+          }
         }
       },
-      instance: {}
+      instance: null,
+      getSnapshotQuery: function () {
+        var relevantFilters = [{
+          type: this.attr('instance.type'),
+          id: this.attr('instance.id'),
+          operation: 'relevant'
+        }];
+        return GGRC.Utils.QueryAPI
+          .buildParam('Snapshot', {}, relevantFilters, [], []);
+      },
+      requestQuery: function (query) {
+        var dfd = can.Deferred();
+        this.attr('isLoading', true);
+        GGRC.Utils.QueryAPI
+          .batchRequests(query)
+          .done(function (response) {
+            var type = Object.keys(response)[0];
+            var values = response[type].values;
+            dfd.resolve(values);
+          })
+          .fail(function () {
+            dfd.resolve([]);
+          })
+          .always(function () {
+            this.attr('isLoading', false);
+          }.bind(this));
+        return dfd;
+      },
+      loadSnapshots: function () {
+        var query = this.getSnapshotQuery();
+        return this.requestQuery(query);
+      }
+    },
+    init: function () {
+      this.viewModel.attr('mappedSnapshots')
+        .replace(this.viewModel.loadSnapshots());
+    },
+    events: {
+      '{viewModel.instance} related_destinations': function () {
+        console.info('Was related_destinations called!!!!', arguments);
+      }
     }
   });
 })(window.can, window.GGRC);
