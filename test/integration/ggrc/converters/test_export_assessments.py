64e855f380717def038df9b1f41d5bfbf359c550
pod2metra <pod2metra@gmail.com>
2017-03-31 15:47:30 +0300

    Add test to filter created_at and updated_at

--- test/integration/ggrc/converters/test_export_assessments.py
+++ test/integration/ggrc/converters/test_export_assessments.py
@@ -3,11 +3,14 @@
 
 
 """Tests for task group task specific export."""
+from ddt import data, ddt
+
 from ggrc import db
 from integration.ggrc.models import factories
 from integration.ggrc import TestCase
 
 
+@ddt
 class TestExport(TestCase):
   """Test imports for assessment objects."""
 
@@ -19,14 +22,19 @@ class TestExport(TestCase):
         "X-Requested-By": "GGRC",
         "X-export-view": "blocks",
     }
+    extr_comment = factories.CommentFactory(description="bad_desc")
+    extr_assessment = factories.AssessmentFactory()
+    db.engine.execute(
+        'update assessments set '
+        'updated_at = "2010-10-10", '
+        'created_at = "2010-10-10";'
+    )
+    factories.RelationshipFactory(source=extr_assessment,
+                                  destination=extr_comment)
     self.comment = factories.CommentFactory(description="123")
     self.assessment = factories.AssessmentFactory()
     self.rel = factories.RelationshipFactory(source=self.comment,
                                              destination=self.assessment)
-    extr_comment = factories.CommentFactory(description="bad_desc")
-    extr_assessment = factories.AssessmentFactory()
-    factories.RelationshipFactory(source=extr_assessment,
-                                  destination=extr_comment)
 
   # pylint: disable=invalid-name
   def assertSlugs(self, field, value, slugs):
@@ -73,3 +81,32 @@ class TestExport(TestCase):
     db.session.delete(self.comment)
     db.session.commit()
     self.assertSlugs("comment", self.comment.description, [])
+
+  def filter_by_datetime(self, alias, value, slugs):
+    """Util function, check date for all valid formats"""
+    parts = ["year", "month", "day", "hour", "minute", "second"]
+    kwargs = {i: getattr(value, i) for i in parts}
+    formats = [
+        "{year}",
+        "{year}-{month}",
+        "{year}-{month}-{day}",
+        "{year}-{month}-{day}:{hour}",
+        "{year}-{month}-{day}:{hour}:{minute}",
+        "{year}-{month}-{day}:{hour}:{minute}:{second}",
+    ]
+    for f_str in formats:
+      self.assertSlugs(alias, f_str.format(**kwargs), slugs)
+
+  @data("created_at", "Created On", "created on")
+  def test_filter_by_created_at(self, alias):
+    """Test filter by created at"""
+    self.filter_by_datetime(alias,
+                            self.assessment.created_at,
+                            [self.assessment.slug])
+
+  @data("updated_at", "Last Updated", "Last Updated")
+  def test_filter_by_updated_at(self, alias):
+    """Test filter by updated at"""
+    self.filter_by_datetime(alias,
+                            self.assessment.updated_at,
+                            [self.assessment.slug])
