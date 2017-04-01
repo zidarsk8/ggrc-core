394100c5c43e0b61391d8c1524ed97189cc3b1a7
Anže Pečar <apecar@google.com>
2017-04-01 16:37:30 +0100

    Add admin permission test

--- test/integration/ggrc/access_control/test_access_control_role.py
+++ test/integration/ggrc/access_control/test_access_control_role.py
@@ -7,6 +7,7 @@ from ggrc.access_control.access_control_role import AccessControlRole
 from integration.ggrc import TestCase
 from integration.ggrc.api_helper import Api
 from integration.ggrc.models.factories import random_str
+from integration.ggrc.generator import ObjectGenerator
 
 
 class TestAccessControlRole(TestCase):
@@ -15,11 +16,17 @@ class TestAccessControlRole(TestCase):
   def setUp(self):
     super(TestAccessControlRole, self).setUp()
     self.api = Api()
+    self.object_generator = ObjectGenerator()
+    self.people = {}
+    for name in ["Creator", "Reader", "Editor"]:
+      _, user = self.object_generator.generate_person(
+          data={"name": name}, user_role=name)
+      self.people[name] = user
 
-  def test_create_access_control_role(self):
-    """Test Access Control Role creation"""
+  def _post_role(self):
+    """Helper function for POSTing roles"""
     name = random_str(prefix="Access Control Role - ")
-    response = self.api.post(AccessControlRole, {
+    return self.api.post(AccessControlRole, {
         "access_control_role": {
             "name": name,
             "objec_type": "Control",
@@ -27,6 +34,10 @@ class TestAccessControlRole(TestCase):
             "read": True
         },
     })
+
+  def test_create(self):
+    """Test Access Control Role creation"""
+    response = self._post_role()
     assert response.status_code == 201, \
         "Failed to create a new access control role, response was {}".format(
             response.status)
@@ -39,3 +50,13 @@ class TestAccessControlRole(TestCase):
         "Update permission not correctly saved {}".format(role.update)
     assert role.delete == 0, \
         "Update permission not correctly saved {}".format(role.delete)
+
+  def test_only_admin_can_post(self):
+    """Only admin users should be able to POST access control roles"""
+    for name in ("Creator", "Reader", "Editor"):
+      person = self.people.get(name)
+      self.api.set_user(person)
+      response = self._post_role()
+      assert response.status_code == 403, \
+          "Non admins should get forbidden error when POSTing role. {}".format(
+              response.status)
