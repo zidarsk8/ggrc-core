f42640baa03637873e12ec27f8554f77fcd83318
selahssea <anton.snytchenko@gmail.com>
2017-04-03 11:40:59 +0300

    Change status color in Related Assessments

--- src/ggrc/assets/javascripts/components/state-colors-map/tests/state-colors-map_spec.js
+++ src/ggrc/assets/javascripts/components/state-colors-map/tests/state-colors-map_spec.js
@@ -6,20 +6,20 @@
 describe('GGRC.Components.stateColorsMap', function () {
   'use strict';
 
-  var completedState = 'Completed';
-  var completedColor = '#8bc34a';
-  var defaultColor = '#bdbdbd';
+  var completedState = 'In Progress';
+  var completedSuffix = 'inprogress';
+  var defaultSuffix = 'notstarted';
 
-  describe('in case state is set as "Completed"', function () {
+  describe('in case state is set as "In Progress"', function () {
     var viewModel;
 
     beforeEach(function () {
       viewModel = GGRC.Components.getViewModel('stateColorsMap');
     });
 
-    it('color property should be "#8bc34a"', function () {
+    it('suffix property should be "inprogress"', function () {
       viewModel.attr('state', completedState);
-      expect(viewModel.attr('color')).toBe(completedColor);
+      expect(viewModel.attr('suffix')).toBe(completedSuffix);
     });
   });
 
@@ -30,38 +30,9 @@ describe('GGRC.Components.stateColorsMap', function () {
       viewModel = GGRC.Components.getViewModel('stateColorsMap');
     });
 
-    it('color property should be "#bdbdbd"', function () {
+    it('suffix property should be "notstarted"', function () {
       viewModel.attr('state', null);
-      expect(viewModel.attr('color')).toBe(defaultColor);
-    });
-  });
-
-  describe('in case colorsMap is mistakenly defined as null' +
-    ' default colorMap should be used ', function () {
-    var viewModel;
-
-    beforeEach(function () {
-      viewModel = GGRC.Components.getViewModel('stateColorsMap');
-    });
-
-    it('and color property should be "#bdbdbd"', function () {
-      viewModel.attr('colorsMap', null);
-      viewModel.attr('state', null);
-      expect(viewModel.attr('color')).toBe(defaultColor);
-    });
-  });
-
-  describe('in case colorsMap is redefined', function () {
-    var viewModel;
-
-    beforeEach(function () {
-      viewModel = GGRC.Components.getViewModel('stateColorsMap');
-    });
-
-    it('color property should be "#aaa"', function () {
-      viewModel.attr('colorsMap', {some: '#aaa'});
-      viewModel.attr('state', 'some');
-      expect(viewModel.attr('color')).toBe('#aaa');
+      expect(viewModel.attr('suffix')).toBe(defaultSuffix);
     });
   });
 });
