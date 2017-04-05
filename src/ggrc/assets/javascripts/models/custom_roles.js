e29b11d209c642d388acd5b0eb6212b5e8490651
plamut <plamut@google.com>
2017-04-05 15:34:08 +0200

    Add Roleable "mixin" on frontend

---
+++ src/ggrc/assets/javascripts/models/custom_roles.js
@@ -0,0 +1,47 @@
+/*!
+    Copyright (C) 2017 Google Inc.
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+(function (can, GGRC, CMS) {
+  'use strict';
+
+   // using this function for sorting will group model types by categories
+  function sortByCategory(a, b) {
+    if (a.category < b.category) {
+      return -1;
+    } else if (a.category > b.category) {
+      return 1;
+    }
+    return 0;
+  }
+
+  /**
+   * A "mixin" denoting a model type that can be assigned custom roles.
+   *
+   * It inherits from Cacheable model, because it needs get_binding() to
+   * correctly display AccessControlRole instances as children in tree views.
+   *
+   * This "mixin" is thus applied to models in a slightly different way - a
+   * model needs to have a static property named `isRoleable` set to true.
+   *
+   * @class
+   */
+  can.Model.Cacheable('CMS.Models.Roleable', {
+    findAll: function () {
+      // We do not query the backend, this implementation is used to diplay
+      // a list of objects in the Custom Roles widget.
+      var types = GGRC.roleableTypes.sort(sortByCategory);
+
+      var instances = can.map(types, function (type, i) {
+        var withId = can.extend(type, {id: i});
+        return new CMS.Models.Roleable(withId);
+      });
+
+      return can.when(instances);
+    }
+  }, {
+    // Cacheable checks if selfLink is set when the findAll deferred is done
+    selfLink: '/custom_roles_list'  // TODO: what path here?
+  });
+})(window.can, window.GGRC, window.CMS);
