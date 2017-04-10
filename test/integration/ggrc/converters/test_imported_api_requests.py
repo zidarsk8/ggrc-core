7a018c29bca0b2d0dd10cdfe61415e93abba6a17
Anže Pečar <apecar@google.com>
2017-04-10 13:05:03 +0100

    Increase query limit in api request tests
    Adding additional queries for access controls rbac has pushed the limit
    a bit.

--- test/integration/ggrc/converters/test_imported_api_requests.py
+++ test/integration/ggrc/converters/test_imported_api_requests.py
@@ -33,7 +33,7 @@ class TestComprehensiveSheets(TestCase):
             if model_name not in WHITELIST]
 
   # limit found by trial and error, may need tweaking if models change
-  LIMIT = 33
+  LIMIT = 36
 
   @classmethod
   def setUpClass(cls):
