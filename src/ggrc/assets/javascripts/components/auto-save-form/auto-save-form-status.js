e3cb3a15dd3ba86331b701198080b3295eece0e2
akaralionak <akaralionak@google.com>
2017-04-13 11:40:17 +0300

    Modify Form State Indicator Component
    - Show by default 'All changes saved'
    - Remove redundant code

--- src/ggrc/assets/javascripts/components/auto-save-form/auto-save-form-status.js
+++ src/ggrc/assets/javascripts/components/auto-save-form/auto-save-form-status.js
@@ -2,18 +2,28 @@
  Copyright (C) 2017 Google Inc.
  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
  */
-(function (can) {
+(function (can, GGRC) {
   'use strict';
+
+  var ALL_SAVED_TEXT = 'All changes saved';
+  var IS_SAVING_TEXT = 'Saving...';
 
   GGRC.Components('autoSaveFormStatus', {
     tag: 'auto-save-form-status',
-    template: can.view(
-      GGRC.mustache_path +
-      '/components/auto-save-form/auto-save-form-status.mustache'
-    ),
+    template: '{{formStatusText}}<content></content>',
     viewModel: {
-      formSaving: false,
-      formAllSaved: false
+      define: {
+        formSaving: {
+          type: 'boolean',
+          value: false
+        },
+        formStatusText: {
+          type: 'string',
+          get: function () {
+            return this.attr('formSaving') ? IS_SAVING_TEXT : ALL_SAVED_TEXT;
+          }
+        }
+      }
     }
   });
-})(window.can);
+})(window.can, window.GGRC);
