c468ab72b8863f986655678496498b1e098eb7bc
akaralionak <akaralionak@google.com>
2017-04-14 11:07:53 +0300

    Update Comment Related Components

--- src/ggrc/assets/javascripts/components/comment/comment-input.js
+++ src/ggrc/assets/javascripts/components/comment/comment-input.js
@@ -13,52 +13,40 @@
   GGRC.Components('commentInput', {
     tag: tag,
     template: template,
-    scope: {
-      placeholder: '@',
-      disabled: false,
-      clean: false,
-      isEmpty: true,
-      value: '',
-      input: {
-        placeholder: '',
-        value: ''
-      },
-      setDisabled: function (isDisabled) {
-        this.attr('disabled', isDisabled);
-      },
-      /**
-       * Updates values of the component on modifications of the input of the Rich Text Editor
-       * @param {String|null} value - Value received from the Rich Text Editor
-       */
-      setValues: function (value) {
-        this.attr('value', value);
-        this.attr('isEmpty', !(value && value.length));
-      },
-      initValues: function () {
-        // Comment Input by default is empty
-        this.attr('isEmpty', true);
-        this.attr('input.placeholder', this.attr('placeholder') || '');
-        this.attr('input.value', this.attr('value'));
-      },
-      performCleanup: function () {
-        this.attr('input.value', null);
-      }
-    },
-    events: {
-      init: function () {
-        this.scope.performCleanup();
-        // Set default values
-        this.scope.initValues();
-      },
-      '{scope} disabled': function (scope, ev, isDisabled) {
-        this.scope.setDisabled(isDisabled);
-      },
-      '{scope.input} value': function (scope, ev, value) {
-        this.scope.setValues(value);
-      },
-      '{scope} clean': function (scope, ev, doCleaning) {
-        if (doCleaning) {
-          this.scope.performCleanup();
+    viewModel: {
+      define: {
+        disabled: {
+          type: 'boolean',
+          value: false
+        },
+        placeholder: {
+          type: 'string',
+          value: ''
+        },
+        isEmpty: {
+          type: 'boolean',
+          value: true,
+          get: function () {
+            var value = this.attr('value') || '';
+            return !value.length;
+          }
+        },
+        clean: {
+          type: 'boolean',
+          value: true,
+          set: function (newValue) {
+            if (newValue) {
+              this.attr('value', '');
+            }
+            return newValue;
+          }
+        },
+        value: {
+          type: 'string',
+          value: '',
+          set: function (newValue) {
+            return newValue || '';
+          }
         }
       }
     }
