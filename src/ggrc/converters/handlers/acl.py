6e96eede926518cf231b341d711d0cbd0be58bc6
Miha Zidar <zidarsk8@gmail.com>
2017-04-16 01:03:10 +0200

    Add basic acl column handler

---
+++ src/ggrc/converters/handlers/acl.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2017 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""Handlers for access control roles."""
+
+from ggrc.converters.handlers import handlers
+
+
+class AccessControlRoleColumnHandler(handlers.UsersColumnHandler):
+  """Handler for access control roles."""
+  pass
