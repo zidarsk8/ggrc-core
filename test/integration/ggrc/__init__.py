a909ec81419ff289b3d3df454368ad4e2853895d
Miha Zidar <zidarsk8@gmail.com>
2017-04-16 02:02:39 +0200

    Fix import_data test helper function
    This fix adds support for multi line fields in import tests. The proper
    fix would be to generate the data with csv module, but that is outside
    the scope at the moment.

--- test/integration/ggrc/__init__.py
+++ test/integration/ggrc/__init__.py
@@ -185,7 +185,8 @@ class TestCase(BaseTestCase, object):
         object_type = data.pop("object_type")
         keys = data.keys()
         tmp.write('{0},{1}\n'.format(object_type, ','.join(keys)))
-        tmp.write(',{0}\n'.format(','.join(str(data[k]) for k in keys)))
+        line = ','.join(u"\"{}\"".format(data[k]) for k in keys)
+        tmp.write(',{0}\n'.format(line))
         tmp.seek(0)
       return cls._import_file(os.path.basename(tmp.name), dry_run, person)
 
