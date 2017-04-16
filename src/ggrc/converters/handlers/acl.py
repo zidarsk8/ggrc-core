75dddf1294c6d3a01ba29a4a455f9c3d143004cd
Miha Zidar <zidarsk8@gmail.com>
2017-04-16 02:26:15 +0200

    Fix update AC roles with import

--- src/ggrc/converters/handlers/acl.py
+++ src/ggrc/converters/handlers/acl.py
@@ -10,12 +10,33 @@ from ggrc.converters.handlers import handlers
 class AccessControlRoleColumnHandler(handlers.UsersColumnHandler):
   """Handler for access control roles."""
 
-  def set_obj_attr(self):
-    role = models.AccessControlRole.query.filter_by(
-        name=self.display_name).one()
-    for person in self.value:
+  def __init__(self, row_converter, key, **options):
+    super(AccessControlRoleColumnHandler, self).__init__(
+        row_converter, key, **options)
+    self.role = models.AccessControlRole.query.filter_by(
+        name=self.display_name
+    ).first()
+
+  def _add_people(self, people_list):
+    for person in people_list:
       models.AccessControlList(
           object=self.row_converter.obj,
           person=person,
-          ac_role_id=role.id
+          ac_role_id=self.role.id
       )
+
+  def _remove_people(self, people_list):
+    acl_email_map = {
+        acl.person.email: acl
+        for acl in self.row_converter.obj.access_control_list
+    }
+    for person in people_list:
+      acl = acl_email_map[person.email]
+      self.row_converter.obj.access_control_list.remove(acl)
+
+  def set_obj_attr(self):
+    list_old = {i.person for i in self.row_converter.obj.access_control_list}
+    list_new = set(self.value)
+
+    self._add_people(list_new - list_old)
+    self._remove_people(list_old - list_new)
