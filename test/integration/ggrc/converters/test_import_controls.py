f2ec6db8c2bb70c8061fd80ba300a3b2b7d67758
Dzmitry Yazepchyk <dyezepchik@gmail.com>
2017-04-18 19:46:00 +0300

    Add test for evidence import in Control

---
+++ test/integration/ggrc/converters/test_import_controls.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2017 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+# pylint: disable=maybe-no-member, invalid-name
+
+"""Test request import and updates."""
+
+from ggrc import models
+
+from integration.ggrc import TestCase
+
+
+class TestControlsImport(TestCase):
+  """Basic Assessment import tests with.
+
+  This test suite should test new Assessment imports, exports, and updates.
+  The main focus of these tests is checking error messages for invalid state
+  transitions.
+  """
+
+  def setUp(self):
+    """Set up for Assessment test cases."""
+    super(TestControlsImport, self).setUp()
+    self.client.get("/login")
+
+  def test_import_controls_with_evidence(self):
+    """Test importing of assessments with templates."""
+
+    response = self.import_file("controls_no_warnings.csv")
+    self._check_csv_response(response, {})
+
+    evidence = models.Document.query.filter_by(title="Some title 3").all()
+    self.assertEqual(len(evidence), 1)
+    control = models.Control.query.filter_by(slug="control-3").first()
+    self.assertEqual(
+        control.object_documents[0].document.title,
+        "Some title 3"
+    )
