5c336b79a1f0a0b2a624770f0aaba3cd82f631b1
Alexander Novik <al.novik88@gmail.com>
2017-05-29 12:17:32 +0300

    Linting DashboardWidgetController

--- src/ggrc/assets/javascripts/controllers/dashboard_widgets_controller.js
+++ src/ggrc/assets/javascripts/controllers/dashboard_widgets_controller.js
@@ -3,67 +3,60 @@
     Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
-//= require can.jquery-all
-//= require controllers/filterable_controller
-
-CMS.Controllers.Filterable("CMS.Controllers.DashboardWidgets", {
-  defaults : {
-    model : null
-    , widget_id : ""
-    , widget_name : ""
-    , widget_icon : ""
-    , widget_view : "/static/mustache/dashboard/object_widget.mustache"
-    , widget_guard : null
-    , widget_initial_content : ''
-    , show_filter : false
-    , object_category : null //e.g. "governance"
-    , content_selector : ".content"
-    //, minimum_widget_height : 100
-    , content_controller : null
-    , content_controller_options : {}
-    , content_controller_selector : null
+CMS.Controllers.Filterable('CMS.Controllers.DashboardWidgets', {
+  defaults: {
+    model: null,
+    widget_id: '',
+    widget_name: '',
+    widget_icon: '',
+    widget_view: '/static/mustache/dashboard/object_widget.mustache',
+    widget_guard: null,
+    widget_initial_content: '',
+    show_filter: false,
+    object_category: null,
+    content_selector: '.content',
+    content_controller: null,
+    content_controller_options: {},
+    content_controller_selector: null
   }
 }, {
-
-  init : function() {
-    if(!this.options.model && GGRC.page_model) {
+  init: function () {
+    if (!this.options.model && GGRC.page_model) {
       this.options.model = GGRC.infer_object_type(GGRC.page_object);
     }
 
-    if(!this.options.widget_icon && this.options.model) {
+    if (!this.options.widget_icon && this.options.model) {
       this.options.widget_icon = this.options.model.table_singular;
     }
-    if(this.options.widget_icon && !/^fa/.test(this.options.widget_icon)) {
+    if (this.options.widget_icon && !/^fa/.test(this.options.widget_icon)) {
       this.options.widget_icon = this.options.widget_icon;
     }
 
-    if(!this.options.object_category && this.options.model) {
+    if (!this.options.object_category && this.options.model) {
       this.options.object_category = this.options.model.category;
     }
 
     this.options.widget_count = new can.Observe();
 
     this.element
-          .addClass("widget")
-          .addClass(this.options.object_category)
-          .attr("id", this.options.widget_id + "_widget")
+      .addClass('widget')
+      .addClass(this.options.object_category)
+      .attr('id', this.options.widget_id + '_widget')
       //  This is used only by ResizeWidgets controller
-          .trigger("section_created");
-  }
-
-  , prepare: function() {
-      if (this._prepare_deferred)
-        return this._prepare_deferred;
-
-      this._prepare_deferred = $.when(
-        can.view(this.options.widget_view, $.when(this.options))
-        , CMS.Models.DisplayPrefs.getSingleton()
-      ).then(this.proxy("draw_widget"));
-
+      .trigger('section_created');
+  },
+  prepare: function () {
+    if (this._prepare_deferred)
       return this._prepare_deferred;
-    }
 
-  , draw_widget : function(frag, prefs) {
+    this._prepare_deferred = $.when(
+      can.view(this.options.widget_view, $.when(this.options))
+      , CMS.Models.DisplayPrefs.getSingleton()
+    ).then(this.proxy('draw_widget'));
+
+    return this._prepare_deferred;
+  },
+  draw_widget: function (frag, prefs) {
 
     this.element.html(frag[0]);
 
@@ -73,18 +66,18 @@ CMS.Controllers.Filterable("CMS.Controllers.DashboardWidgets", {
     if (prefs.getCollapsed(window.getPageToken(), this.element.attr("id"))) {
 
       this.element
-      .find(".widget-showhide > a")
-      .showhide("hide");
+        .find('.widget-showhide > a')
+        .showhide('hide');
 
-      content.add(this.element).css("height", "");
-      if(content.is(".ui-resizable")) {
-        content.resizable("destroy");
+      content.add(this.element).css('height', '');
+      if (content.is('.ui-resizable')) {
+        content.resizable('destroy');
       }
     } else {
-      content.trigger("min_size");
+      content.trigger('min_size');
     }
 
-    if(this.options.content_controller) {
+    if (this.options.content_controller) {
       controller_content = this.element.find(this.options.content_selector);
       if (this.options.content_controller_selector)
         controller_content =
@@ -96,7 +89,7 @@ CMS.Controllers.Filterable("CMS.Controllers.DashboardWidgets", {
 
       this.options.content_controller_options.show_header = true;
       this.content_controller = new this.options.content_controller(
-          controller_content
+        controller_content
         , this.options.content_controller_options
       );
 
@@ -107,61 +100,44 @@ CMS.Controllers.Filterable("CMS.Controllers.DashboardWidgets", {
         return new $.Deferred().resolve();
       }
     }
-  }
+  },
+  display: function () {
+    var that = this
+      , tracker_stop = GGRC.Tracker.start(
+      'DashboardWidget', 'display', this.options.model.shortName)
+      ;
 
-  , display: function() {
-      var that = this
-       , tracker_stop = GGRC.Tracker.start(
-          "DashboardWidget", "display", this.options.model.shortName)
-       ;
-
-      if (this._display_deferred) {
-        return this._display_deferred;
-      }
-
-      if (that.content_controller) {
-        this._display_deferred = this.prepare().then(function() {
-          if (that.content_controller && that.content_controller.display) {
-            return that.content_controller.display();
-          }
-          else {
-            return new $.Deferred().resolve();
-          }
-        }).done(tracker_stop);
-      } else {
-        this._display_deferred =
-          this.element.find('tree-widget-container').viewModel().display();
-      }
-
+    if (this._display_deferred) {
       return this._display_deferred;
-    },
-
-  /*, ".remove-widget click" : function() {
-    var parent = this.element.parent();
-    this.element.remove();
-    parent.trigger("sortremove");
-  }
-
-  , ".widgetsearch keydown" : function(el, ev) {
-    if(ev.which === 13) {
-      this.filter(el.val());
-      this.element.trigger('kill-all-popovers');
     }
-    ev.stopPropagation();
-    ev.originalEvent && ev.originalEvent.stopPropagation();
-  }*/
-  "updateCount": function (el, ev, count, updateCount) {
-    this.options.widget_count.attr("count", "" + count);
-  }
 
-  , display_path: function(path, refetch) {
-      var that = this;
-      if (!that.content_controller) {
-        return this.display();
-      }
-      return this.display().then(function() {
-        if (that.content_controller && that.content_controller.display_path)
-          return that.content_controller.display_path(path, refetch);
-      });
+    if (that.content_controller) {
+      this._display_deferred = this.prepare().then(function () {
+        if (that.content_controller && that.content_controller.display) {
+          return that.content_controller.display();
+        }
+        else {
+          return new $.Deferred().resolve();
+        }
+      }).done(tracker_stop);
+    } else {
+      this._display_deferred =
+        this.element.find('tree-widget-container').viewModel().display();
     }
+
+    return this._display_deferred;
+  },
+  'updateCount': function (el, ev, count, updateCount) {
+    this.options.widget_count.attr('count', '' + count);
+  },
+  display_path: function (path, refetch) {
+    var that = this;
+    if (!that.content_controller) {
+      return this.display();
+    }
+    return this.display().then(function () {
+      if (that.content_controller && that.content_controller.display_path)
+        return that.content_controller.display_path(path, refetch);
+    });
+  }
 });
