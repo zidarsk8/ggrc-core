5c97284985575c3494a546462cf0bc0480c5d89e
Alieh Rymasheuski <alieh.rymasheuski@gmail.com>
2017-06-20 15:12:27 +0300

    Fix styling

--- src/ggrc/login/appengine.py
+++ src/ggrc/login/appengine.py
@@ -15,29 +15,35 @@ E.g., ``login: required`` must be specified *at least* for the '/login' route.
 """
 
 from google.appengine.api import users
-from ggrc.login.common import find_or_create_user_by_email, get_next_url
+import flask
 import flask_login
-from flask import url_for, redirect, request, session, flash
+
+from ggrc.login import common
 
 
 def get_user():
+  """Get current user using appengine authentication."""
   ae_user = users.get_current_user()
   email = ae_user.email()
   nickname = ae_user.nickname()
-  user = find_or_create_user_by_email(email, name=nickname)
+  user = common.find_or_create_user_by_email(email, name=nickname)
   return user
 
+
 def login():
+  """Log in current user."""
   user = get_user()
   if user.system_wide_role != 'No Access':
     flask_login.login_user(user)
-    return redirect(get_next_url(request, default_url=url_for('dashboard')))
-  else:
-    flash(u'You do not have access. Please contact your administrator.', 'alert alert-info')
-    return redirect('/')
+    return flask.redirect(common.get_next_url(
+        flask.request, default_url=flask.url_for('dashboard')))
+
+  flask.flash(u'You do not have access. Please contact your administrator.',
+              'alert alert-info')
+  return flask.redirect('/')
+
 
 def logout():
   flask_login.logout_user()
-  return redirect(
-    users.create_logout_url(
-      get_next_url(request, default_url=url_for('index'))))
+  return flask.redirect(users.create_logout_url(common.get_next_url(
+      flask.request, default_url=flask.url_for('index'))))
