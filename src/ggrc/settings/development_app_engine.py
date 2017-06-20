3dc6e51151d3b7c8c620174ec49e9ace426d303d
Alieh Rymasheuski <alieh.rymasheuski@gmail.com>
2017-06-20 17:00:43 +0300

    Unset TESTING for non-testing environments
    TESTING disables login manager

--- src/ggrc/settings/development_app_engine.py
+++ src/ggrc/settings/development_app_engine.py
@@ -2,7 +2,7 @@
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
 DEBUG = True
-TESTING = True
+TESTING = False
 PRODUCTION = False
 HOST = '0.0.0.0'
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:root@localhost/ggrcdev'
