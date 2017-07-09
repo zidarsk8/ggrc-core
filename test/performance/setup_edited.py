4e303b0dcda9b916d0ff455a865f15a01814bff7
Miha Zidar <zidarsk8@gmail.com>
2017-07-09 19:19:23 +0800

    Add snapshot mappings to generated assessments

--- test/performance/setup_edited.py
+++ test/performance/setup_edited.py
@@ -22,13 +22,26 @@ class SetUpAssessments(base.BaseTaskSet):
 
   def set_up(self):
     super(SetUpAssessments, self).set_up()
+    self.get_objects("Snapshot")
 
     count = len(self.objects["Assessment"]) / 2
     assessments = generator.random_objects("Assessment", count, self.objects)
     self._edit_assessments(assessments)
     self._edit_assessment_states(assessments[:count / 2])
+    self._map_snapshots(assessments)
 
     sys.exit(0)
+
+  def _map_snapshots(self, assessments):
+    for slug in assessments:
+      assessment = self._get_object(slug)
+      all_snapshots = self.snapshots[assessment["audit"]["id"]]
+      destinations = (
+          generator.random_objects("Control", 5, all_snapshots) +
+          generator.random_objects("Objective", 5, all_snapshots) +
+          generator.random_objects("Regulation", 5, all_snapshots)
+      )
+      self.create_relationships([slug], destinations)
 
   def _edit_assessments(self, assessments):
     for assessment in assessments:
