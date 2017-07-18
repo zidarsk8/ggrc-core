7d0203eeeb9e68454ef5e499662d3d4aad5d0b43
Alexander Novik <al.novik88@gmail.com>
2017-07-18 16:31:13 +0300

    Apply GGRC modules

---
+++ src/ggrc/assets/javascripts/modules.js
@@ -0,0 +1,25 @@
+/*!
+    Copyright (C) 2017 Google Inc.
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+import '../../../ggrc_risks/assets/javascripts';
+import '../../../ggrc_workflows/assets/javascripts';
+import '../../../ggrc_risk_assessments/assets/javascripts';
+import '../../../ggrc_basic_permissions/assets/javascripts';
+import '../../../ggrc_gdrive_integration/assets/javascripts';
+
+// _.compact(_.map(GGRC_SETTINGS_MODULE.split(' '), function (module) {
+//   var name;
+//   if (/^ggrc/.test(module)) {
+//     name = module.split('.')[0];
+//   }
+//   if (module === 'development') {
+//     name = 'ggrc';
+//   }
+//   if (!name) {
+//     return '';
+//   }
+//   // require(name);
+//   require('../../../' + name + '/assets/javascripts');
+// }));
---
+++ src/ggrc_workflows/assets/javascripts/index.js
@@ -0,0 +1,20 @@
+/*!
+    Copyright (C) 2017 Google Inc.
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+import './apps/workflows';
+import './controllers/dashboard_page';
+import './controllers/workflow_page';
+import './models/cycle_models';
+import './models/task_group';
+import './models/workflow';
+import './models/workflow_join_models';
+import './models/helpers';
+import './workflow_mustache_helpers';
+import './controllers/approval_workflow_controller';
+import './components/workflow_clone';
+import './components/taskgroup_clone';
+import './components/end_cycle';
+import './components/workflow_active';
+import './components/workflow_deactivate';
