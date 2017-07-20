5b88e44b0a5245f46392bc987206e903f39005df
Alexander Novik <al.novik88@gmail.com>
2017-07-20 14:55:56 +0300

    Clean up quick_search from unused code

--- src/ggrc/assets/javascripts/controllers/quick_search_controller.js
+++ src/ggrc/assets/javascripts/controllers/quick_search_controller.js
@@ -4,182 +4,6 @@
 */
 
 (function(can, $) {
-  function with_params(href, params) {
-    if (href.charAt(href.length - 1) === '?')
-      return href + params;
-    else if (href.indexOf('?') > 0)
-      return href + '&' + params;
-    else
-      return href + '?' + params;
-  }
-
-  function get_attr(el, attrnames) {
-    var attrval = null
-    , $el = $(el);
-    can.each(can.makeArray(attrnames), function(attrname) {
-      var a = $el.attr(attrname);
-      if(a) {
-        attrval = a;
-        return false;
-      }
-    });
-    return attrval;
-  }
-
-CMS.Controllers.Filterable("CMS.Controllers.QuickSearch", {
-  defaults : {
-    list_view : GGRC.mustache_path + "/dashboard/object_list.mustache"
-    , spin : true
-    , tab_selector : 'ul.nav-tabs:first > li > a'
-    , tab_href_attr : [ "href", "data-tab-href" ]
-    , tab_target_attr : [ "data-tab-target", "href" ]
-    , tab_model_attr : [ "data-model", "data-object-singular" ]
-    , limit : null
-  }
-}, {
-
-  setup : function(el, opts) {
-    this._super && this._super.apply(this, arguments);
-    if(!opts.observer) {
-      opts.observer = new can.Observe();
-    }
-  }
-
-  , init : function(opts) {
-    var that = this;
-    var $tabs = this.element.find(this.options.tab_selector);
-    $tabs.each(function(i, tab) {
-      var $tab = $(tab)
-      , href = get_attr($tab, that.options.tab_href_attr)
-      , loaded = $tab.data('tab-loaded')
-      , pane = get_attr($tab, that.options.tab_target_attr)
-      , $pane = $(pane)
-      , template = $tab.data("template") || that.options.list_view
-      , model_name = get_attr($tab, that.options.tab_model_attr)
-      , model = can.getObject("CMS.Models." + model_name) || can.getObject("GGRC.Models." + model_name)
-      , view_data = null
-      , xhrs = {};
-
-      if(!template && typeof console !== "undefined") {
-        console.warn("No template defined for quick_search in ", $pane.attr("id"));
-      }
-
-      if(model && template) {
-        view_data = new can.Observe({
-          list: new model.List()
-          , all_items: new model.List()
-          , filtered_items: new model.List()
-          , observer: that.options.observer
-          , model : model
-        });
-
-        $tab.data("view_data", view_data);
-        $tab.data("model", model);
-        $pane.trigger("loading");
-        model.findAll().done(function(data) {
-          view_data.attr('all_items', data);
-          view_data.attr('filtered_items', data.slice(0));
-          if($tab.is("li.active a")) {
-            can.Observe.startBatch();
-            if(that.options.limit != null) {
-              view_data.attr('list').replace(data.slice(0, that.options.limit));
-            } else {
-              view_data.attr('list', data);
-            }
-            can.Observe.stopBatch();
-            $pane.trigger("loaded", xhrs[$pane.attr("id")], $tab.data("list"));
-          } else {
-            GGRC.queue_event(function() {
-              if(that.options.limit != null) {
-                view_data.attr('list').replace(data.slice(0, that.options.limit));
-              } else {
-                view_data.attr('list', data);
-              }
-              $pane.trigger("loaded", xhrs[$pane.attr("id")], $tab.data("list"));
-            });
-          }
-          $tab.find(".item-count").html(data ? data.length : 0);
-        });
-
-        model.bind("created", function(ev, instance) {
-          if(instance.constructor === model) {
-            view_data.list.unshift(instance.serialize());
-          }
-        });
-      }
-
-      if(that.options.spin) {
-        // Scroll up so spinner doesn't get pushed out of visibility
-        $pane.scrollTop(0);
-      }
-
-      if (view_data) {
-        can.view(template, view_data, function(frag, xhr) {
-          $tab.data('tab-loaded', true);
-          $pane.html(frag);
-          xhrs[$pane.attr("id")] = xhr;
-        });
-      }
-    });
-  }
-
-  , "{observer} value" : function(el, ev, newval) {
-    this.filter(newval);
-    this.element.trigger('kill-all-popovers');
-  }
-
-  , "{observer} my_work" : function(el, ev, newval) {
-    this.filter(null, newval ? { "contact_id": GGRC.current_user.id } : null);
-    this.element.trigger('kill-all-popovers');
-  }
-
-  // @override
-  , redo_last_filter : function(id_to_add) {
-    var that = this;
-    var $tabs = $(this.element).find(this.options.tab_selector);
-    var old_sel = this.options.filterable_items_selector;
-    var old_ids = this.last_filter_ids;
-
-    $tabs.each(function(i, tab) {
-      var $tab = $(tab)
-      , model = $tab.data("model")
-      , res = old_ids ? that.last_filter.getResultsFor(model) : null
-      , view_data = $tab.data("view_data");
-
-      //that.options.filterable_items_selector = $(get_attr($tab, that.options.tab_href_attr)).find("li:not(.view-more, .add-new)");
-      that.last_filter_ids = res = res ? can.unique(can.map(res, function(v) { return v.id; })) : null; //null is the show-all case
-      if(res) {
-        view_data.filtered_items.replace(can.map(view_data.all_items, function(item) { return ~can.inArray(item.id, res) ? item : undefined; }));
-      } else {
-        view_data.filtered_items.replace(view_data.all_items.slice(0));
-      }
-      view_data.list.replace(that.options.limit ? view_data.filtered_items.slice(0, that.options.limit) : view_data.filtered_items);
-
-      //that._super();
-      // res = can.map(res, function(obj, i) {
-      //   var m = new model(obj);
-      //   if(!m.selfLink) {
-      //     m.refresh();
-      //   }
-      //   return m;
-      // });
-      $tab.find(".item-count").html(res ? res.length : $tab.data("view_data").filtered_items.length);
-    });
-  }
-
-  , ".tabbable loaded" : function(el, ev) {
-    $(el).scrollTop(0);
-  }
-
-  , ".nav-tabs li click" : function(el, ev) {
-    var plural = el.children("a").attr("data-object-plural");
-    var singular = can.map(window.cms_singularize(plural).split("_"), can.capitalize).join(" ");
-    el.closest(".widget").find(".object-type").text(singular)
-      .closest("a").attr("data-object-plural", plural.split(" ").join("_").toLowerCase())
-      .attr("data-object-singular", singular.replace(" ", ""));
-  }
-});
-
 can.Control("CMS.Controllers.LHN", {
     defaults: {
     }
@@ -563,7 +387,6 @@ can.Control("CMS.Controllers.LHN", {
   }
 });
 
-
 can.Control("CMS.Controllers.InfiniteScroll", {
     defaults: {
     }
@@ -624,7 +447,6 @@ can.Control("CMS.Controllers.InfiniteScroll", {
       this.element.trigger("scrollNext");
     }
 });
-
 
 can.Control("CMS.Controllers.LHN_Search", {
     defaults : {
@@ -1285,7 +1107,6 @@ can.Control("CMS.Controllers.LHN_Search", {
     this.run_search(term, param);
   }
 });
-
 
 can.Control("CMS.Controllers.LHN_Tooltips", {
     defaults : {
