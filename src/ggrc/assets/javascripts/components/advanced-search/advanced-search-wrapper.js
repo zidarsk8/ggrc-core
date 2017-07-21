5bc10bea50f13fcc93d631f75debb54acb02cb94
Artsiom Valchanin <volchanin.dev@gmail.com>
2017-07-21 16:15:56 +0300

    Add special filter string builder

--- src/ggrc/assets/javascripts/components/advanced-search/advanced-search-wrapper.js
+++ src/ggrc/assets/javascripts/components/advanced-search/advanced-search-wrapper.js
@@ -6,45 +6,139 @@
 (function (can, GGRC) {
   'use strict';
 
-  var viewModel = can.Map.extend({
-    define: {
-      filterItems: {
-        value: [],
-        get: function (items) {
-          if (items && !items.length) {
-            items.push(GGRC.Utils.AdvancedSearch.create.state({
-              items: GGRC.Utils.State
-                .getDefaultStatesForModel(this.attr('modelName')),
-              operator: 'ANY',
-              modelName: this.attr('modelName')
-            }));
-          }
-          return items;
+  var builders = {
+    attribute: function (attribute) {
+      var operators = {
+        '~': 'contains',
+        '=': 'equals',
+        '!~': 'does not contain',
+        '!=': 'is not equal',
+        '<': 'less than',
+        '>': 'greater than'
+      };
+      return [
+        attribute.field,
+        operators[attribute.operator],
+        '"' + attribute.value.trim() + '"'
+      ].join(' ');
+    },
+    operator: function (operator) {
+      return ' ' + operator + ' ';
+    },
+    state: function stateToFilter(state) {
+      var result = '';
+      var operators = {
+        ANY: '=',
+        NONE: '!='
+      };
+      function itemsToString(states) {
+        var statesString = _.map(states, function (item) {
+          return '"' + item + '"';
+        }).join(', ');
+        if (states.length > 1) {
+          statesString = '(' + statesString + ')';
         }
+        return statesString;
       }
+      if (state.items.length) {
+        result = [
+          'AND state',
+          operators[state.operator],
+          itemsToString(state.items)
+        ].join(' ');
+      }
+      return result;
     },
-    modelName: null,
-    mappingItems: [],
-    availableAttributes: function () {
-      var available = GGRC.Utils.TreeView.getColumnsForModel(
-        this.attr('modelName'),
-        null,
-        true
-      ).available;
-      return available;
+    group: function groupToFilter(items, request) {
+      return '(' + itemsToString(items, request) + ')';
     },
-    resetFilters: function () {
-      this.attr('filterItems', []);
-      this.attr('mappingItems', []);
-    },
-    filterString: function () {
-      return GGRC.Utils.AdvancedSearch.buildFilter(this.attr('filterItems'));
+    mappingCriteria: function mappingCriteriaToFilter(criteria) {
+      var title = CMS.Models[criteria.objectName].title_singular;
+      var result = [
+        'mapped to',
+        title,
+        'with',
+        builders.attribute(criteria.filter.value)
+      ].join(' ');
+      if (criteria.mappedTo) {
+        result += [
+          ' where',
+          title,
+          'is',
+          builders[criteria.mappedTo.type](criteria.mappedTo.value)
+        ].join(' ');
+      }
+      return result;
     }
-  });
+  };
+
+  function itemsToString(items, initialState) {
+    var result = '';
+    if (items.length) {
+      result = initialState || result;
+      _.each(items, function (item) {
+        result += builders[item.type](item.value);
+      });
+    }
+    return result;
+  }
+
+  function relevantToString(items) {
+    var result = _.map(items, function (item) {
+      return [
+        'AND mapped to',
+        CMS.Models[item.type].title_singular,
+        item.title
+      ].join(' ');
+    }).join(' ');
+    return result;
+  }
 
   GGRC.Components('advancedSearchWrapper', {
     tag: 'advanced-search-wrapper',
-    viewModel: viewModel,
+    viewModel: can.Map.extend({
+      define: {
+        filterItems: {
+          value: [],
+          get: function (items) {
+            if (items && !items.length) {
+              items.push(GGRC.Utils.AdvancedSearch.create.state({
+                items: GGRC.Utils.State
+                  .getDefaultStatesForModel(this.attr('modelName')),
+                operator: 'ANY',
+                modelName: this.attr('modelName')
+              }));
+            }
+            return items;
+          }
+        }
+      },
+      modelName: null,
+      modelDisplayName: null,
+      mappingItems: [],
+      relevantTo: [],
+      availableAttributes: function () {
+        var available = GGRC.Utils.TreeView.getColumnsForModel(
+          this.attr('modelName'),
+          null,
+          true
+        ).available;
+        return available;
+      },
+      resetFilters: function () {
+        this.attr('filterItems', []);
+        this.attr('mappingItems', []);
+      },
+      filterString: function () {
+        var filterString = [
+          this.attr('modelDisplayName'),
+          itemsToString(this.attr('filterItems')),
+          relevantToString(this.attr('relevantTo')),
+          itemsToString(this.attr('mappingItems'), 'AND ')
+        ].join(' ');
+        return filterString;
+      }
+    }),
     events: {
       '{viewModel} modelName': function () {
         this.viewModel.resetFilters();
--- src/ggrc/assets/javascripts/components/advanced-search/tests/advanced-search-wrapper_spec.js
+++ src/ggrc/assets/javascripts/components/advanced-search/tests/advanced-search-wrapper_spec.js
@@ -29,4 +29,96 @@ describe('GGRC.Components.advancedSearchWrapper', function () {
       expect(viewModel.resetFilters).toHaveBeenCalled();
     });
   });
+
+  describe('filterString() method', function () {
+    it('builds correct filter string', function () {
+      spyOn(GGRC.Utils.State, 'getDefaultStatesForModel');
+      viewModel.attr('filterItems', [
+        GGRC.Utils.AdvancedSearch.create.state({
+          items: ['Active', 'Draft'],
+          operator: 'ANY'
+        }),
+        GGRC.Utils.AdvancedSearch.create.operator('AND'),
+        GGRC.Utils.AdvancedSearch.create.attribute({
+          field: 'Title',
+          operator: '~',
+          value: 'test'
+        }),
+        GGRC.Utils.AdvancedSearch.create.operator('OR'),
+        GGRC.Utils.AdvancedSearch.create.group([
+          GGRC.Utils.AdvancedSearch.create.attribute({
+            field: 'Para',
+            operator: '=',
+            value: 'meter'
+          }),
+          GGRC.Utils.AdvancedSearch.create.operator('AND'),
+          GGRC.Utils.AdvancedSearch.create.attribute({
+            field: 'Other',
+            operator: '!=',
+            value: 'value'
+          })
+        ])
+      ]);
+
+      viewModel.attr('mappingItems', [
+        GGRC.Utils.AdvancedSearch.create.mappingCriteria({
+          objectName: 'Product',
+          filter: GGRC.Utils.AdvancedSearch.create.attribute({
+            field: 'title',
+            operator: '~',
+            value: 'A'
+          }),
+          mappedTo: GGRC.Utils.AdvancedSearch.create.mappingCriteria({
+            objectName: 'System',
+            filter: GGRC.Utils.AdvancedSearch.create.attribute({
+              field: 'title',
+              operator: '~',
+              value: 'B'
+            })
+          })
+        }),
+        GGRC.Utils.AdvancedSearch.create.operator('AND'),
+        GGRC.Utils.AdvancedSearch.create.mappingCriteria({
+          objectName: 'Control',
+          filter: GGRC.Utils.AdvancedSearch.create.attribute({
+            field: 'title',
+            operator: '~',
+            value: 'C'
+          }),
+          mappedTo: GGRC.Utils.AdvancedSearch.create.group([
+            GGRC.Utils.AdvancedSearch.create.mappingCriteria({
+              objectName: 'System',
+              filter: GGRC.Utils.AdvancedSearch.create.attribute({
+                field: 'title',
+                operator: '~',
+                value: 'D'
+              })
+            }),
+            GGRC.Utils.AdvancedSearch.create.operator('OR'),
+            GGRC.Utils.AdvancedSearch.create.mappingCriteria({
+              objectName: 'System',
+              filter: GGRC.Utils.AdvancedSearch.create.attribute({
+                field: 'title',
+                operator: '~',
+                value: 'E'
+              })
+            })
+          ])
+        })
+      ]);
+      viewModel.attr('modelDisplayName', 'TestType');
+      viewModel.attr('relevantTo', [{type: 'Audit', title: 'test'}]);
+
+      expect(viewModel.filterString()).toBe(
+        'TestType AND state = ("Active", "Draft") ' +
+        'AND Title contains "test" OR ' +
+        '(Para equals "meter" AND Other is not equal "value") ' +
+        'AND mapped to Audit test ' +
+        'AND mapped to Product with title contains "A" ' +
+        'where Product is mapped to System with title contains "B" ' +
+        'AND mapped to Control with title contains "C" ' +
+        'where Control is (mapped to System with title contains "D" ' +
+        'OR mapped to System with title contains "E")');
+    });
+  });
 });
