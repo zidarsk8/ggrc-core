77e8b23e6dcaa971d4f1df9df1a921c8847c6a09
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-07-25 13:49:34 +0300

    Fix Workflow model's repeat_every type validator

--- src/ggrc_workflows/models/workflow.py
+++ src/ggrc_workflows/models/workflow.py
@@ -132,6 +132,8 @@ class Workflow(mixins.CustomAttributable, HasOwnContext, mixins.Timeboxed,
 
     repeat_every shouldn't have 0 value.
     """
+    if value is not None and not isinstance(value, int):
+      raise ValueError("'repeat_every' should be integer")
     if value == 0:
       raise ValueError("'repeat_every' cannot be set to 0")
     return value
