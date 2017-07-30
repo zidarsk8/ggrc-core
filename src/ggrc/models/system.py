62adf3f32a88fa854928acac1143d5cc64fcb3a3
plamut <plamut@google.com>
2017-07-30 21:55:34 +0200

    Fix Reference URLs revisions for polymorphic types
    Polymorphic types (i.e. those sharing a single DB table with other
    object types) did not get their reference URLs properly stored in
    their revision history, thus their reference URLs did not show up on
    their snapshotted versions.
    
    This commit assures that the query used to fetch object data correctly
    fetches reference URLs, resulting in correct object revisions.

--- src/ggrc/models/system.py
+++ src/ggrc/models/system.py
@@ -17,8 +17,16 @@ from ggrc.models import track_object_state
 from ggrc.models import reflection
 
 
+# NOTE: The PublicDocumentable mixin is not applied directly to the
+# SystemOrProcess base class, but instead to all of its specialized subclasses.
+# The reason for this is the PublicDocumentable's declared attribute
+# `documents` that builds a dynamic DB relationship based on the class name,
+# and thus the attribute needs to be run in the context of each particular
+# subclass.
+# (of course, if there is a nice way of overriding/customizing declared
+# attributes in subclasses, we might want to use that approach)
 class SystemOrProcess(track_object_state.HasObjectState,
-                      LastDeprecatedTimeboxed, PublicDocumentable,
+                      LastDeprecatedTimeboxed,
                       BusinessObject, db.Model):
   # Override model_inflector
   _table_plural = 'systems_or_processes'
@@ -95,11 +103,16 @@ class SystemOrProcess(track_object_state.HasObjectState,
 
 
 class System(CustomAttributable, Personable, Roleable,
-             Relatable, SystemOrProcess, Indexed):
+             Relatable, PublicDocumentable, SystemOrProcess, Indexed):
   __mapper_args__ = {
       'polymorphic_identity': False
   }
   _table_plural = 'systems'
+
+  _aliases = {
+      "document_url": None,
+      "document_evidence": None,
+  }
 
   @validates('is_biz_process')
   def validates_is_biz_process(self, key, value):
@@ -107,11 +120,16 @@ class System(CustomAttributable, Personable, Roleable,
 
 
 class Process(CustomAttributable, Personable, Roleable,
-              Relatable, SystemOrProcess, Indexed):
+              Relatable, PublicDocumentable, SystemOrProcess, Indexed):
   __mapper_args__ = {
       'polymorphic_identity': True
   }
   _table_plural = 'processes'
+
+  _aliases = {
+      "document_url": None,
+      "document_evidence": None,
+  }
 
   @validates('is_biz_process')
   def validates_is_biz_process(self, key, value):
