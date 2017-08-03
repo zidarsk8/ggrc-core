47db23645ca483818f6e70e38e4d3db9f53255ef
AleksNeStu <a.v.nesterovich@gmail.com>
2017-08-03 13:04:36 +0300

    Change PO according to new unify mapper w/ advanced search

--- test/selenium/src/lib/page/modal/unified_mapper.py
+++ test/selenium/src/lib/page/modal/unified_mapper.py
@@ -3,27 +3,28 @@
 """Modals for map objects."""
 
 from lib import base, decorator
-from lib.constants import locator
-from lib.utils import filter_utils, selenium_utils
+from lib.constants import locator, element
+from lib.utils import selenium_utils
 
 
 class CommonUnifiedMapperModal(base.Modal):
   """Common unified mapper modal."""
   _locators = locator.ModalMapObjects
+  _elements = element.UnifiedMapperModal
 
   def __init__(self, driver, obj_name):
     super(CommonUnifiedMapperModal, self).__init__(driver)
     # labels
+    self.filter_toggle = base.Toggle(driver, self._locators.FILTER_TOGGLE_CSS)
+    self.filter_toggle.is_activated = True
     self.title_modal = base.Label(driver, self._locators.MODAL_TITLE)
     self.obj_type = base.Label(driver, self._locators.OBJ_TYPE)
-    self.filter_by_expression = base.Label(
-        driver, self._locators.FILTER_BY_EXPRESSION)
     # user input elements
-    self.filter_via_expression_text_box = base.TextInputField(
-        driver, self._locators.FILTER_VIA_EXPRESSION_TEXT_BOX)
-    self.filter_by_state_text_box = base.DropdownStatic(
-        driver, self._locators.FILTER_BY_STATE_DROPDOWN)
     self.tree_view = base.UnifiedMapperTreeView(driver, obj_name=obj_name)
+    self._add_attr_btn = None
+    self.search_result_toggle = base.Toggle(
+        driver, self._locators.RESULT_TOGGLE_CSS)
+    self.search_result_toggle.toggle()
 
   def get_available_to_map_obj_aliases(self):
     """Return texts of all objects available to map via UnifiedMapper."""
@@ -35,25 +36,55 @@ class CommonUnifiedMapperModal(base.Modal):
   def obj_type_dropdown(self):
     return base.DropdownStatic(self._driver, self._locators.OBJ_TYPE_DROPDOWN)
 
-  def _select_dest_obj_type(self, obj_name):
+  def _select_dest_obj_type(self, obj_name, is_asmts_generation=False):
     """Open dropdown and select element according to destination object name.
     If is_asmts_generation then TextFilterDropdown, else DropdownStatic.
     """
     if obj_name:
-      self.obj_type_dropdown.select(obj_name)
+      obj_type_dropdown = base.DropdownStatic(
+          self._driver, self._locators.OBJ_TYPE_DROPDOWN)
+      obj_type_dropdown.select(obj_name)
+      if is_asmts_generation:
+        asmt_tmpl_dropdown = base.DropdownStatic(
+            self._driver, self._locators.OBJ_TYPE_DROPDOWN,)
+        asmt_tmpl_dropdown.select_by_label(obj_name)
 
-  def _filter_dest_objs_via_expression_by_titles(self, objs_titles):
-    """Enter expression is like 'title=obj1_title or title=obj2_title' into
-    filter via expression text box according to destination objects titles.
-    """
-    # pylint: disable=invalid-name
-    expression = filter_utils.FilterUtils.get_filter_exp_by_title(
-        titles=objs_titles)
-    self.filter_via_expression_text_box.enter_text(expression)
+  def add_filter_attr(
+      self, attr_name, value, operator=_elements.OPERATOR_AND,
+      compare=_elements.COMPARE_OPERATOR_EQUALS
+  ):
+    """Add filter attribute according to passed parameters. """
+    if not self._add_attr_btn:
+      self._add_attr_btn = selenium_utils.get_when_visible(
+          self._driver, self._locators.FILTER_ADD_ATTRIBUTE)
+    self._add_attr_btn.click()
+    last_filter_param = self._get_latest_filter_elements()
+    last_filter_param['name'].select(attr_name)
+    last_filter_param['value'].enter_text(value)
+    last_filter_param['compare'].select(compare)
+    last_filter_param['operator'].select(operator)
+
+  def _get_latest_filter_elements(self):
+    """Return elements of last filter parameter"""
+    return {
+        "name": base.DropdownStatic(
+            self._driver, selenium_utils.get_when_all_visible(
+                self._driver, self._locators.FILTER_ATTRIBUTE_NAME)[-1]),
+        "operator": base.DropdownStatic(
+            self._driver, selenium_utils.get_when_all_visible(
+                self._driver, self._locators.FILTER_OPERATOR)[-1]),
+        "compare": base.DropdownStatic(
+            self._driver, selenium_utils.get_when_all_visible(
+                self._driver, self._locators.FILTER_ATTRIBUTE_COMPARE)[-1]),
+        "value": base.TextInputField(
+            self._driver, selenium_utils.get_when_all_visible(
+                self._driver, self._locators.FILTER_ATTRIBUTE_VALUE)[-1])}
 
   def _select_search_dest_objs(self):
     """Click Search button to search objects according set filters."""
     base.Button(self._driver, self._locators.BUTTON_SEARCH).click()
+    selenium_utils.wait_for_js_to_load(self._driver)
+    self.filter_toggle.is_activated = False
 
   def _select_dest_objs_to_map(self, objs_titles):
     """Select checkboxes regarding to titles from list of checkboxes
@@ -85,34 +116,44 @@ class CommonUnifiedMapperModal(base.Modal):
     selenium_utils.get_when_invisible(
         self._driver, locator.TreeView.NO_RESULTS_MESSAGE)
 
-  def search_dest_objs(self, dest_objs_type, dest_objs_titles):
+  def search_dest_objs(self, dest_objs_type, dest_objs_titles,
+                       is_asmts_generation=False):
     """Filter and search destination objects according to them type and titles.
     If is_asmts_generation then TextFilterDropdown is using.
     """
-    self._select_dest_obj_type(obj_name=dest_objs_type)
-    self._filter_dest_objs_via_expression_by_titles(
-        objs_titles=dest_objs_titles)
+    if not self.filter_toggle.is_activated:
+      self.filter_toggle.toggle()
+    self._select_dest_obj_type(obj_name=dest_objs_type,
+                               is_asmts_generation=is_asmts_generation)
+    for enum, title in enumerate(dest_objs_titles):
+      if enum == 0:
+        operator = self._elements.OPERATOR_AND
+      else:
+        operator = self._elements.OPERATOR_OR
+      self.add_filter_attr(self._elements.ATTRIBUTE_TITLE, title,
+                           operator=operator)
     self._select_search_dest_objs()
     return self.tree_view.get_list_members_as_list_scopes()
 
-  def map_dest_objs(self, dest_objs_type, dest_objs_titles):
+  def map_dest_objs(self, dest_objs_type, dest_objs_titles,
+                    is_asmts_generation=False):
     """Filter, search destination objects according to them type and titles.
     Map found destination objects to source object.
     If is_asmts_generation then TextFilterDropdown is using.
     """
-    self.search_dest_objs(dest_objs_type, dest_objs_titles)
+    selenium_utils.wait_for_js_to_load(self._driver)
+    self.search_dest_objs(dest_objs_type, dest_objs_titles,
+                          is_asmts_generation=is_asmts_generation)
     self._select_dest_objs_to_map(objs_titles=dest_objs_titles)
     self._confirm_map_selected()
 
 
 class MapObjectsModal(CommonUnifiedMapperModal):
   """Modal for map objects."""
-  _locators = locator.ModalMapObjects
 
 
 class SearchObjectsModal(CommonUnifiedMapperModal):
   """Modal for search objects."""
-  _locators = locator.ModalMapObjects
 
 
 class GenerateAssessmentsModal(CommonUnifiedMapperModal):
@@ -128,4 +169,4 @@ class GenerateAssessmentsModal(CommonUnifiedMapperModal):
     # pylint: disable=invalid-name
     self.map_dest_objs(
         dest_objs_type=asmt_tmpl_title,
-        dest_objs_titles=objs_under_asmt_titles)
+        dest_objs_titles=objs_under_asmt_titles, is_asmts_generation=True)
