6d878c7236b6439c0c8b45782d7b29495c4e88be
AleksNeStu <a.v.nesterovich@gmail.com>
2017-08-09 15:31:16 +0300

    Improve assert for test of objectives via REST

--- test/selenium/src/tests/test_objective.py
+++ test/selenium/src/tests/test_objective.py
@@ -38,10 +38,10 @@ class TestObjectivePage(base.Test):
   ):
     """Check if Objectives can be mapped to Control via REST."""
     # pylint: disable=too-many-arguments
-
     expected_status_code = RestClient.STATUS_CODES["OK"]
-    actual_status_code = dynamic_relationships[0].status_code
-    assert expected_status_code == actual_status_code
+    dynamic_relationships = string_utils.convert_to_list(dynamic_relationships)
+    assert all(expected_status_code == relationship.status_code
+               for relationship in dynamic_relationships)
     assert (
         (rest_service.RelationshipsService().map_objs(
             src_obj=new_control_rest,
