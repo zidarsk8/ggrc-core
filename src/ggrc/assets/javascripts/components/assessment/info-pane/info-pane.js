ea689ab857214206dbcf96e37f414d950e5ff978
Artsiom Valchanin <volchanin.dev@gmail.com>
2017-08-24 11:14:50 +0300

    Add ability to save changes in a batch

--- src/ggrc/assets/javascripts/components/assessment/info-pane/info-pane.js
+++ src/ggrc/assets/javascripts/components/assessment/info-pane/info-pane.js
@@ -248,54 +248,67 @@
           } :
           [];
       },
+      addAction: function (actionType, related) {
+        var assessment = this.attr('instance');
+        var path = 'actions.' + actionType;
+
+        if (!assessment.attr('actions')) {
+          assessment.attr('actions', {});
+        }
+        if (assessment.attr(path)) {
+          assessment.attr(path).push(related);
+        } else {
+          assessment.attr(path, [related]);
+        }
+      },
       addRelatedItem: function (event, type) {
         var self = this;
-        var assessment = this.attr('instance');
+        var related = {
+          id: event.item.attr('id'),
+          type: event.item.attr('type')
+        };
 
-        assessment.attr('actions', {
-          add_related: [{
-            id: event.item.attr('id'),
-            type: event.item.attr('type')
-          }]
-        }).save()
-          .done(function () {
-            self.afterCreate({
-              items: [event.item],
-              success: true
-            }, type);
-          })
-          .fail(function () {
-            self.afterCreate({
-              items: [event.item],
-              success: false
-            }, type);
-          })
-          .always(function (assessment) {
-            assessment.removeAttr('actions');
-          });
+        this.attr('deferredSave').push(function () {
+          self.addAction('add_related', related);
+        })
+        .done(function () {
+          self.afterCreate({
+            items: [event.item],
+            success: true
+          }, type);
+        })
+        .fail(function () {
+          self.afterCreate({
+            items: [event.item],
+            success: false
+          }, type);
+        })
+        .always(function (assessment) {
+          assessment.removeAttr('actions');
+        });
       },
       removeRelatedItem: function (item, type) {
         var self = this;
-        var assessment = this.attr('instance');
+        var related = {
+          id: item.attr('id'),
+          type: item.attr('type')
+        };
         var items = self.attr(type);
         var index = items.indexOf(item);
         this.attr('isUpdating' + can.capitalize(type), true);
         items.splice(index, 1);
 
-        assessment.attr('actions', {
-          remove_related: [{
-            id: item.attr('id'),
-            type: item.attr('type')
-          }]
-        }).save()
-          .fail(function () {
-            GGRC.Errors.notifier('error', 'Unable to remove URL.');
-            items.splice(index, 0, item);
-          })
-          .always(function (assessment) {
-            assessment.removeAttr('actions');
-            self.attr('isUpdating' + can.capitalize(type), false);
-          });
+        this.attr('deferredSave').push(function () {
+          self.addAction('remove_related', related);
+        })
+        .fail(function () {
+          GGRC.Errors.notifier('error', 'Unable to remove URL.');
+          items.splice(index, 0, item);
+        })
+        .always(function (assessment) {
+          assessment.removeAttr('actions');
+          self.attr('isUpdating' + can.capitalize(type), false);
+        });
       },
       updateRelatedItems: function () {
         this.attr('isUpdatingRelatedItems', true);
@@ -328,6 +341,12 @@
             return CAUtils.convertToFormViewField(cav);
           })
         );
+      },
+      initializeDeferredSave: function () {
+        this.attr('deferredSave', new GGRC.Utils.DeferredTransaction(
+          function (resolve, reject) {
+            this.attr('instance').save().done(resolve).fail(reject);
+          }.bind(this), 1000));
       },
       onFormSave: function () {
         this.attr('triggerFormSaveCbs').fire();
@@ -417,6 +436,7 @@
       this.viewModel.initializeFormFields();
       this.viewModel.initGlobalAttributes();
       this.viewModel.updateRelatedItems();
+      this.viewModel.initializeDeferredSave();
     },
     events: {
       '{viewModel.instance} refreshMapping': function () {
