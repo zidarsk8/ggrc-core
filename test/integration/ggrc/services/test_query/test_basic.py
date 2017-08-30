a4764a5b33ad91d5682c34b5d914a6d2c13930a8
pod2metra <pod2metra@gmail.com>
2017-08-30 16:31:16 +0300

    Change error msg about wrong datetime

--- test/integration/ggrc/services/test_query/test_basic.py
+++ test/integration/ggrc/services/test_query/test_basic.py
@@ -161,10 +161,9 @@ class TestAdvancedQueryAPI(TestCase, WithQueryApi):
     response = self._post(data)
     self.assert400(response)
 
-    self.assertEqual(
-        ("Invalid date was typed into `{}` field, "
-         "please change it and try again!").format(field),
-        response.json['message'])
+    self.assertEqual(u"Specified date format is invalid for search, "
+                     u"please update it and try again.",
+                     response.json['message'])
 
   def test_basic_query_text_search(self):
     """Filter by fulltext search."""
@@ -1285,10 +1284,9 @@ class TestQueryWithCA(TestCase, WithQueryApi):
     )
     response = self._post(data)
     self.assert400(response)
-    self.assertEqual(
-        ("Invalid date was typed into `ca date` field, "
-         "please change it and try again!"),
-        response.json['message'])
+    self.assertEqual(u"Specified date format is invalid for search, "
+                     u"please update it and try again.",
+                     response.json['message'])
 
 
 @ddt.ddt
