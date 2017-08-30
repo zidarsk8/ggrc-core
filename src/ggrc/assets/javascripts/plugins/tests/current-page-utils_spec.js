13e44020ea01418a2be9b3d2f12e858f758b147f
m-vitaly <vitaly851001@gmail.com>
2017-08-30 13:00:09 +0300

    Cover GGRC.Utils.CurrentPage methods with Unit Tests

--- src/ggrc/assets/javascripts/plugins/tests/current-page-utils_spec.js
+++ src/ggrc/assets/javascripts/plugins/tests/current-page-utils_spec.js
@@ -123,4 +123,348 @@ describe('GGRC Utils CurrentPage', function () {
       expect(method()).toBeFalsy();
     });
   });
+
+  describe('getWidgetList() method', function () {
+    var method;
+
+    beforeEach(function () {
+      spyOn(GGRC.WidgetList, 'get_widget_list_for')
+        .and.returnValue({
+          control: {},
+          assessment: {},
+          objective: {}
+        });
+      method = GGRC.Utils.CurrentPage.getWidgetList;
+    });
+
+    it('returns an empty object when model is not provided', function () {
+      var result = method('', 'assessments_view');
+
+      expect(result.assessment).toBeUndefined();
+    });
+
+    it('returns assessments widget for assessment view', function () {
+      var result = method('assessment', '/assessments_view');
+      var keys = Object.keys(result);
+
+      expect(keys.length).toEqual(1);
+      expect(keys).toContain('assessment');
+    });
+
+    it('returns widgets for non-assessment view', function () {
+      var result = method('assessment', '/controls_view');
+      var keys = Object.keys(result);
+
+      expect(keys.length).toEqual(3);
+    });
+  });
+
+  describe('getDefaultWidgets() method', function () {
+    var method;
+
+    beforeEach(function () {
+      method = GGRC.Utils.CurrentPage.getDefaultWidgets;
+    });
+
+    it('should return "Info" widget for non-object browser path', function () {
+      var result = method({
+        control: {},
+        assessment: {},
+        objective: {},
+        info: {}
+      }, '/assessments_view');
+
+      expect(result).toContain('info');
+    });
+
+    it('should return "Info" widget for non-object browser path', function () {
+      var result = method({
+        control: {},
+        assessment: {},
+        objective: {},
+        info: {}
+      }, '/objectBrowser/');
+
+      expect(result).not.toContain('info');
+    });
+  });
+
+  describe('getWidgetModels() method', function () {
+    var method;
+
+    beforeEach(function () {
+      spyOn(GGRC.WidgetList, 'get_widget_list_for')
+        .and.returnValue({
+          control: {
+            content_controller_options: {
+              model: {
+                shortName: 'Control'
+              }
+            }
+          },
+          assessment: {
+            content_controller_options: {
+              model: {
+                shortName: 'Assessment'
+              }
+            }
+          },
+          objective: {
+            content_controller_options: {
+              model: {
+                shortName: 'Objective'
+              }
+            }
+          },
+          info: {
+            content_controller_options: {
+              model: {
+                shortName: 'Info'
+              }
+            }
+          }
+        });
+      method = GGRC.Utils.CurrentPage.getWidgetModels;
+    });
+
+    it('returns an empty array when model is not provided', function () {
+      var result = method('', 'assessments_view');
+
+      expect(result.length).toEqual(0);
+    });
+
+    it('returns assessment model name only for assessment view', function () {
+      var result = method('assessment', '/assessments_view');
+
+      expect(result).toContain('Assessment');
+    });
+
+    it('returns appropriate models for non-assessment view',
+      function () {
+        var result = method('assessment', '/controls_view');
+
+        expect(result).toContain('Assessment');
+        expect(result).toContain('Control');
+        expect(result).toContain('Objective');
+        expect(result).toContain('Info');
+      });
+
+    it('returns non-info models for object browser view',
+      function () {
+        var result = method('assessment', '/objectBrowser/');
+
+        expect(result).toContain('Assessment');
+        expect(result).toContain('Control');
+        expect(result).toContain('Objective');
+        expect(result).not.toContain('Info');
+      });
+  });
+
+  describe('initCounts() method', function () {
+    var method;
+    var queryDfd;
+    var getCounts;
+    var id = 1;
+
+    beforeEach(function () {
+      queryDfd = can.Deferred();
+      method = GGRC.Utils.CurrentPage.initCounts;
+      getCounts = GGRC.Utils.CurrentPage.getCounts;
+
+      spyOn(GGRC.Utils.TreeView, 'makeRelevantExpression')
+        .and.returnValue({
+          type: 'Assessment',
+          id: id,
+          operation: 'owned'
+        });
+      spyOn(GGRC.Utils.Snapshots, 'isSnapshotRelated')
+        .and.callFake(function (type, widgetType) {
+          return widgetType === 'Control';
+        });
+      spyOn(GGRC.Utils.QueryAPI, 'buildParam')
+        .and.callFake(function (objName) {
+          return {
+            objectName: objName
+          };
+        });
+
+      spyOn(GGRC.query_parser, 'parse')
+        .and.returnValue({});
+
+      spyOn(GGRC.Utils.QueryAPI, 'makeRequest')
+        .and.returnValue(queryDfd);
+    });
+
+    it('should not make request when no widget was provided', function () {
+      method([], 'Control', 1);
+
+      expect(GGRC.Utils.QueryAPI.makeRequest)
+        .not.toHaveBeenCalled();
+    });
+
+    it('should init counts for snapshotable objects', function () {
+      var result;
+
+      method(['Control'], 'Assessment', 1);
+
+      expect(GGRC.Utils.QueryAPI.makeRequest)
+        .toHaveBeenCalledWith({
+          data: [
+            {
+              type: 'count',
+              objectName: 'Snapshot'
+            }
+          ]
+        });
+
+      queryDfd.resolve([{
+        Snapshot: {
+          total: 10
+        }
+      }]);
+
+      result = getCounts();
+
+      expect(result.Control).toEqual(10);
+    });
+
+    it('should init counts for non-snapshotable objects', function () {
+      var result;
+
+      method(['Assessment'], 'Control', 1);
+
+      expect(GGRC.Utils.QueryAPI.makeRequest)
+        .toHaveBeenCalledWith({
+          data: [
+            {
+              type: 'count',
+              objectName: 'Assessment'
+            }
+          ]
+        });
+
+      queryDfd.resolve([{
+        Assessment: {
+          total: 10
+        }
+      }]);
+
+      result = getCounts();
+
+      expect(result.Assessment).toEqual(10);
+    });
+
+    it('should init counts for virtual objects', function () {
+      var result;
+
+      method([{
+        name: 'Cycle',
+        countsName: 'ActiveCycle'
+      }], 'Control', 1);
+
+      expect(GGRC.Utils.QueryAPI.makeRequest)
+        .toHaveBeenCalledWith({
+          data: [
+            {
+              type: 'count',
+              objectName: 'Cycle'
+            }
+          ]
+        });
+
+      queryDfd.resolve([{
+        Cycle: {
+          total: 10
+        }
+      }]);
+
+      result = getCounts();
+
+      expect(result.ActiveCycle).toEqual(10);
+    });
+  });
+
+  describe('initMappedInstances() method', function () {
+    var requestDfds = [];
+    var method;
+
+    beforeEach(function () {
+      var requestDfd;
+
+      spyOn(GGRC.Utils.QueryAPI, 'buildRelevantIdsQuery')
+        .and.callFake(function (objName, page, relevant, additionalFilter) {
+          return {
+            model: objName,
+            operation: 'relevant'
+          };
+        });
+
+      spyOn(GGRC.Utils.Snapshots, 'isSnapshotRelated')
+        .and.callFake(function (parent, child) {
+          return child === 'Control';
+        });
+
+      spyOn(GGRC.Utils.Snapshots, 'transformQuery')
+        .and.callFake(function (query) {
+          return query;
+        });
+
+      spyOn(GGRC.Utils.QueryAPI, 'batchRequests')
+        .and.callFake(function () {
+          requestDfd = can.Deferred();
+          requestDfds.push(requestDfd);
+          return requestDfd;
+        });
+      method = GGRC.Utils.CurrentPage.initMappedInstances;
+    });
+
+    it('should init mappings for snapshotable objects',
+      function (done) {
+        var snapshotIds = [1, 2, 3];
+
+        function validateResult(result) {
+          snapshotIds.forEach(function (id) {
+            expect(result.Control[id]).toBeTruthy();
+          });
+          done();
+        }
+
+        spyOn(GGRC.Mappings, 'getMappingList')
+          .and.returnValue(['Control']);
+
+        method().then(validateResult);
+        requestDfds.forEach(function (dfd) {
+          dfd.resolve({
+            Snapshot: {
+              ids: snapshotIds
+            }
+          });
+        });
+      });
+
+    it('should init mappings for non-snapshotable objects',
+      function (done) {
+        var nonSnapshotIds = [4, 5, 6];
+
+        function validateResult(result) {
+          nonSnapshotIds.forEach(function (id) {
+            expect(result.Assessment[id]).toBeTruthy();
+          });
+
+          done();
+        }
+
+        spyOn(GGRC.Mappings, 'getMappingList')
+          .and.returnValue(['Assessment']);
+
+        method().then(validateResult);
+        requestDfds.forEach(function (dfd) {
+          dfd.resolve({
+            Assessment: {
+              ids: nonSnapshotIds
+            }
+          });
+        });
+      });
+  });
 });
