73ffd676855dbc5a34c69016fd47769801d3d9ad
Artsiom Valchanin <volchanin.dev@gmail.com>
2017-08-30 16:04:51 +0300

    Cleanup GlobalSearch modal

--- src/ggrc/assets/javascripts/components/advanced-search/advanced-search-wrapper.js
+++ src/ggrc/assets/javascripts/components/advanced-search/advanced-search-wrapper.js
@@ -6,116 +6,12 @@
 (function (can, GGRC) {
   'use strict';
 
-  var builders = {
-    attribute: function (attribute) {
-      var operators = {
-        '~': 'contains',
-        '=': 'equals',
-        '!~': 'does not contain',
-        '!=': 'is not equal',
-        '<': 'less than',
-        '>': 'greater than'
-      };
-      return [
-        attribute.field,
-        operators[attribute.operator],
-        '"' + attribute.value.trim() + '"'
-      ].join(' ');
-    },
-    operator: function (operator) {
-      return ' ' + operator + ' ';
-    },
-    state: function stateToFilter(state) {
-      var result = '';
-      var operators = {
-        ANY: '=',
-        NONE: '!='
-      };
-      function itemsToString(states) {
-        var statesString = _.map(states, function (item) {
-          return '"' + item + '"';
-        }).join(', ');
-        if (states.length > 1) {
-          statesString = '(' + statesString + ')';
-        }
-        return statesString;
-      }
-      if (state.items.length) {
-        result = [
-          'AND state',
-          operators[state.operator],
-          itemsToString(state.items)
-        ].join(' ');
-      }
-      return result;
-    },
-    group: function groupToFilter(items, request) {
-      return '(' + itemsToString(items, request) + ')';
-    },
-    mappingCriteria: function mappingCriteriaToFilter(criteria) {
-      var title = CMS.Models[criteria.objectName].title_singular;
-      var result = [
-        'mapped to',
-        title,
-        'with',
-        builders.attribute(criteria.filter.value)
-      ].join(' ');
-      if (criteria.mappedTo) {
-        result += [
-          ' where',
-          title,
-          'is',
-          builders[criteria.mappedTo.type](criteria.mappedTo.value)
-        ].join(' ');
-      }
-      return result;
-    }
-  };
-
-  function itemsToString(items, initialState) {
-    var result = '';
-    if (items.length) {
-      result = initialState || result;
-      _.each(items, function (item) {
-        result += builders[item.type](item.value);
-      });
-    }
-    return result;
-  }
-
-  function relevantToString(items) {
-    var result = _.map(items, function (item) {
-      return [
-        'AND mapped to',
-        CMS.Models[item.type].title_singular,
-        item.title
-      ].join(' ');
-    }).join(' ');
-    return result;
-  }
-
   GGRC.Components('advancedSearchWrapper', {
     tag: 'advanced-search-wrapper',
     viewModel: can.Map.extend({
-      define: {
-        filterItems: {
-          value: [],
-          get: function (items) {
-            if (items && !items.length &&
-              GGRC.Utils.State.hasFilter(this.attr('modelName'))) {
-              items.push(GGRC.Utils.AdvancedSearch.create.state({
-                items: GGRC.Utils.State
-                  .getDefaultStatesForModel(this.attr('modelName')),
-                operator: 'ANY',
-                modelName: this.attr('modelName')
-              }));
-            }
-            return items;
-          }
-        }
-      },
       modelName: null,
       modelDisplayName: null,
+      filterItems: [],
       mappingItems: [],
       relevantTo: [],
       availableAttributes: function () {
@@ -129,15 +25,6 @@
       resetFilters: function () {
         this.attr('filterItems', []);
         this.attr('mappingItems', []);
-      },
-      filterString: function () {
-        var filterString = [
-          this.attr('modelDisplayName'),
-          itemsToString(this.attr('filterItems')),
-          relevantToString(this.attr('relevantTo')),
-          itemsToString(this.attr('mappingItems'), 'AND ')
-        ].join(' ');
-        return filterString;
       }
     }),
     events: {
