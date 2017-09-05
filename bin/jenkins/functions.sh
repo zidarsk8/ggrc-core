05883f121eea163c71df964fa99f14eb9b9bfed9
Anže Pečar <apecar@google.com>
2017-09-05 17:09:42 +0100

    Remove 1000 user hack

--- bin/jenkins/functions.sh
+++ bin/jenkins/functions.sh
@@ -56,14 +56,6 @@ setup () {
     --project-name ${PROJECT} \
     up --force-recreate -d ${MACHINE_ID}
 
-  if [[ $UID -eq 0 ]]; then
-    # Allow users inside the containers to access files.
-    chown 1000 -R .
-  elif [[ $UID -ne 1000 ]]; then
-    echo "These tests must be run with UID 1000 or 0. Your UID is $UID"
-    exit 1
-  fi
-
   echo "Provisioning ${PROJECT}_dev_1"
   docker exec -i ${PROJECT}_dev_1 su -c "
     source /vagrant/bin/init_vagrant_env
