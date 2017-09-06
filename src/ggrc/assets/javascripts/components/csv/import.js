68316a3e6b8813c1e44fc47a1ebf6b57b79beb30
Klim Shuplenkou <kshuplenkou@google.com>
2017-09-06 11:46:10 +0300

    Fix code style of import.js

--- src/ggrc/assets/javascripts/components/csv/import.js
+++ src/ggrc/assets/javascripts/components/csv/import.js
@@ -1,4 +1,4 @@
-/*!
+/*
     Copyright (C) 2017 Google Inc.
     Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
@@ -10,35 +10,36 @@ import '../show-more/show-more';
 import template from './templates/csv-import.mustache';
 
 export default GGRC.Components('csvImportWidget', {
-  tag: "csv-import",
+  tag: 'csv-import',
   template: template,
   requestData: null,
   scope: {
-    importUrl: "/_service/import_csv",
-    import: null,
+    importUrl: '/_service/import_csv',
+    'import': null,
     fileId: '',
     fileName: '',
     isLoading: false,
-    state: "select",
+    state: 'select',
     helpUrl: GGRC.config.external_import_help_url,
     states: function () {
-      var state = this.attr("state") || "select",
-          states = {
+      var state = this.attr('state') || 'select';
+      var states = {
             select: {
               'class': 'btn-green',
               text: 'Choose file to import',
             },
             analyzing: {
-              'class': "btn-white",
+              'class': 'btn-white',
               showSpinner: true,
               isDisabled: true,
-              text: "Analyzing"
+              text: 'Analyzing',
             },
-            import: {
-              'class': "btn-green",
-              text: "Import",
+            'import': {
+              'class': 'btn-green',
+              text: 'Import',
               isDisabled: function () {
-                var toImport = this.import;  // info on blocks to import
+                // info on blocks to import
+                var toImport = this.import;
                 var nonEmptyBlockExists;
                 var hasErrors;
 
@@ -57,20 +58,20 @@ export default GGRC.Components('csvImportWidget', {
                 });
 
                 return hasErrors || !nonEmptyBlockExists;
-              }.bind(this)  // bind the scope object as context
+              }.bind(this),
             },
             importing: {
-              'class': "btn-white",
+              'class': 'btn-white',
               showSpinner: true,
               isDisabled: true,
-              text: "Importing"
+              text: 'Importing',
             },
             success: {
-              'class': "btn-green",
+              'class': 'btn-green',
               isDisabled: true,
-              text: "<i class=\"fa fa-check-square-o white\">"+
-                "</i> Import successful"
-            }
+              text: '<i class="fa fa-check-square-o white">'+
+                '</i> Import successful',
+            },
           };
 
       return _.extend(states[state], {state: state});
@@ -78,7 +79,7 @@ export default GGRC.Components('csvImportWidget', {
     prepareDataForCheck: function (requestData) {
       var checkResult = {
         hasDeprecations: false,
-        hasDeletions: false
+        hasDeletions: false,
       };
 
       // check if imported data has deprecated or deleted objects
@@ -101,27 +102,27 @@ export default GGRC.Components('csvImportWidget', {
 
       return {
         data: requestData,
-        check: checkResult
+        check: checkResult,
       };
     },
     processLoadedInfo: function (data) {
-      this.attr("import", _.map(data, function (element) {
+      this.attr('import', _.map(data, function (element) {
         element.data = [];
         if (element.block_errors.concat(element.row_errors).length) {
           element.data.push({
-            status: "errors",
-            messages: element.block_errors.concat(element.row_errors)
+            status: 'errors',
+            messages: element.block_errors.concat(element.row_errors),
           });
         }
         if (element.block_warnings.concat(element.row_warnings).length) {
           element.data.push({
-            status: "warnings",
-            messages: element.block_warnings.concat(element.row_warnings)
+            status: 'warnings',
+            messages: element.block_warnings.concat(element.row_warnings),
           });
         }
         return element;
       }));
-      this.attr("state", "import");
+      this.attr('state', 'import');
     },
     needWarning: function (checkObj, data) {
       var hasWarningTypes = _.every(data, function (item) {
@@ -146,7 +147,7 @@ export default GGRC.Components('csvImportWidget', {
             modal_description:
               'In the result of import some Products, Systems or ' +
               'Processes will be ' + operation.past + '.',
-            operation: operation.action
+            operation: operation.action,
           },
           function () {
             this.processLoadedInfo(data);
@@ -164,26 +165,26 @@ export default GGRC.Components('csvImportWidget', {
     getOperationNameFromCheckObj: function (checkObj) {
       var action = _.compact([
         checkObj.hasDeletions ? 'deletion' : '',
-        checkObj.hasDeprecations ? 'deprecation' : ''
+        checkObj.hasDeprecations ? 'deprecation' : '',
       ]).join(' and ');
       var pastForm = _.compact([
         checkObj.hasDeletions ? 'deleted' : '',
-        checkObj.hasDeprecations ? 'deprecated' : ''
+        checkObj.hasDeprecations ? 'deprecated' : '',
       ]).join(' and ');
 
       return {
         action: action,
-        past: pastForm
+        past: pastForm,
       };
     },
     resetFile: function (element) {
       this.attr({
-        state: "select",
+        state: 'select',
         fileId: '',
         fileName: '',
-        import: null
+        'import': null
       });
-      element.find(".csv-upload").val("");
+      element.find('.csv-upload').val('');
     },
     requestImport: function (file) {
       this.attr('state', 'analyzing');
@@ -207,37 +208,37 @@ export default GGRC.Components('csvImportWidget', {
         .always(function () {
           this.attr('isLoading', false);
         }.bind(this));
-    }
+    },
   },
   events: {
-    ".state-reset click": function (el, ev) {
+    '.state-reset click': function (el, ev) {
       ev.preventDefault();
       this.scope.resetFile(this.element);
     },
-    ".state-import click": function (el, ev) {
+    '.state-import click': function (el, ev) {
       ev.preventDefault();
-      this.scope.attr("state", "importing");
+      this.scope.attr('state', 'importing');
 
       GGRC.Utils.import_request({
         data: {id: this.scope.attr('fileId')},
       }, false)
       .done(function (data) {
         var result_count = data.reduce(function (prev, curr) {
-              _.each(Object.keys(prev), function(key) {
+              _.each(Object.keys(prev), function (key) {
                 prev[key] += curr[key] || 0;
               });
               return prev;
             }, {created: 0, updated: 0, deleted: 0, ignored: 0});
 
-        this.scope.attr("state", "success");
-        this.scope.attr("data", [result_count]);
+        this.scope.attr('state', 'success');
+        this.scope.attr('data', [result_count]);
       }.bind(this))
       .fail(function (data) {
-        this.scope.attr("state", "select");
+        this.scope.attr('state', 'select');
         GGRC.Errors.notifier('error', data.responseJSON.message);
       }.bind(this))
       .always(function () {
-        this.scope.attr("isLoading", false);
+        this.scope.attr('isLoading', false);
       }.bind(this));
     },
     '#import_btn.state-select click': function (el, ev) {
@@ -251,7 +252,6 @@ export default GGRC.Components('csvImportWidget', {
           gapi.load('picker', {callback: createPicker});
         });
 
-      // Create and render a Picker object for searching images.
       function createPicker() {
         GGRC.Controllers.GAPI.oauth_dfd.done(function (token, oauth_user) {
           var dialog;
@@ -271,14 +271,12 @@ export default GGRC.Components('csvImportWidget', {
 
           picker = picker.build();
           picker.setVisible(true);
-          // use undocumented fu to make the Picker be "modal"
-          // this is the "mask" displayed behind the dialog box div
-          $('div.picker-dialog-bg').css('zIndex', 4000);  // there are multiple divs of that sort
-          // and this is the dialog box modal div, which we must display on top of our modal, if any
+
+          $('div.picker-dialog-bg').css('zIndex', 4000);
 
           dialog = GGRC.Utils.getPickerElement(picker);
           if (dialog) {
-            dialog.style.zIndex = 4001; // our modals start with 2050
+            dialog.style.zIndex = 4001;
           }
         });
       }
@@ -305,6 +303,6 @@ export default GGRC.Components('csvImportWidget', {
           }
         }
       }
-    }
-  }
+    },
+  },
 });
