730882bf4b82cc7bddf0fa5052c610a0203d881f
Anže Pečar <apecar@google.com>
2017-09-18 15:47:46 +0100

    Improve conflict resolution

--- src/ggrc/assets/javascripts/models/cacheable_conflict_resolution.js
+++ src/ggrc/assets/javascripts/models/cacheable_conflict_resolution.js
@@ -1,14 +1,43 @@
-/*!
+/*
     Copyright (C) 2017 Google Inc.
     Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
+function checkValues(baseAttrs, attrs, remoteAttrs, obj) {
+  var conflict = false;
+  can.each(baseAttrs, function (val, key) {
+    // We skip the updated_at key because we know it has changed
+    if (key === 'updated_at') {
+      return;
+    }
+
+        // The object attribute was changed on the server
+    if (!can.Object.same(val, remoteAttrs[key]) &&
+        // The object attribute was also changed on the client
+        !can.Object.same(val, attrs[key]) &&
+        // The change on the server was not the same as the change on the client
+        !can.Object.same(attrs[key], remoteAttrs[key])) {
+      conflict = true;
+      // Restore the version that user wrote changes
+      obj.attr(key, attrs[key]);
+      console.warn('Conflict', key, 'User wrote:', attrs[key],
+        'Server has:', remoteAttrs[key], 'User saw', baseAttrs[key]);
+               // The attribute hasn't changed on the server
+    } else if (can.Object.same(val, remoteAttrs[key]) &&
+              // The attribute was changed on the client
+               !can.Object.same(val, attrs[key])) {
+      obj.attr(key, attrs[key]);
+    }
+  });
+  return conflict;
+}
+
 export default function resolveConflict(xhr, obj) {
-  var attrs = obj.attr();
-  var baseAttrs = obj._backupStore() || {};
+  var attrs = can.extend(true, {}, obj.attr());
+  var baseAttrs = can.extend(true, {}, obj._backupStore()) || {};
   return obj.refresh().then(function (obj) {
     var conflict = false;
-    var remoteAttrs = obj.attr();
+    var remoteAttrs = can.extend(true, {}, obj.attr());
 
     if (can.Object.same(remoteAttrs, attrs)) {
       // current state is same as server state -- do nothing.
@@ -18,18 +47,10 @@ export default function resolveConflict(xhr, obj) {
       return obj.attr(attrs).save();
     }
     // check what properties changed -- we can merge if the same prop wasn't changed on both
-    can.each(baseAttrs, function (val, key) {
-      if (!can.Object.same(attrs[key], remoteAttrs[key])) {
-        if (can.Object.same(val, remoteAttrs[key])) {
-          obj.attr(key, attrs[key]);
-        } else if (!can.Object.same(val, attrs[key])) {
-          conflict = true;
-        }
-      }
-    });
+    conflict = checkValues(baseAttrs, attrs, remoteAttrs, obj);
     if (conflict) {
       $(document.body).trigger('ajax:flash', {
-        warning: GGRC.Errors.messages[409]
+        warning: GGRC.Errors.messages[409],
       });
       return new $.Deferred().reject(xhr, 409, 'CONFLICT');
     }
