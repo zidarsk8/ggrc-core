f4659fc6185127b1b1ce3beca99064b15bd49124
Ilya Tsupryk <ilya.tsupryk@gmail.com>
2017-09-20 08:07:34 -0400

    Add issue cascade unmap

--- src/ggrc/models/hooks/relationship.py
+++ src/ggrc/models/hooks/relationship.py
@@ -9,6 +9,7 @@ import itertools
 
 import sqlalchemy as sa
 
+from ggrc import db
 from ggrc.services import signals
 from ggrc.models import all_models
 from ggrc.models.comment import Commentable
@@ -72,8 +73,127 @@ def handle_audit_issue_mapping(session, flush_context, instances):
         _handle_del_audit_issue_mapping(audit=src, issue=dst)
 
 
+def related_condition(obj, type_):
+  """Get condition to select all relationships between objects"""
+  return sa.or_(
+      sa.and_(
+          all_models.Relationship.source_id == obj.id,
+          all_models.Relationship.source_type == obj.type,
+          all_models.Relationship.destination_type == type_,
+      ),
+      sa.and_(
+          all_models.Relationship.destination_id == obj.id,
+          all_models.Relationship.destination_type == obj.type,
+          all_models.Relationship.source_type == type_,
+      )
+  )
+
+
+def related_to_other_condition(obj, other):
+  """Get condition to select all relationships between
+   objects except passed one"""
+  return sa.or_(
+      sa.and_(
+          all_models.Relationship.source_id == obj.id,
+          all_models.Relationship.source_type == obj.type,
+          all_models.Relationship.destination_type == other.type,
+          all_models.Relationship.destination_id != other.id,
+      ),
+      sa.and_(
+          all_models.Relationship.destination_id == obj.id,
+          all_models.Relationship.destination_type == obj.type,
+          all_models.Relationship.source_type == other.type,
+          all_models.Relationship.source_id != other.id,
+      )
+  )
+
+
+def related_id(related_to_type, label):
+  """Get source_id/destination_id related to provided type"""
+  return sa.case(
+      [(
+          all_models.Relationship.source_type == related_to_type,
+          all_models.Relationship.destination_id
+      )],
+      else_=all_models.Relationship.source_id
+  ).label(label)
+
+
+def unmap_issue_cascade(asmnt, issue):
+  """Unmap issue cascade: unmap all Audits/Snapshots mapped both to Issue and
+  Assessment and not mapped to any other Assessment
+  """
+  # All relationships of current Issue and Assessments except current one
+  other_asmnts = db.session.query(related_id("Issue", "asmnt_id")).filter(
+      related_to_other_condition(issue, asmnt)
+  )
+
+  other_asmnt_snap_ids = db.session.query(
+      related_id("Assessment", "snapshot_id"),
+  ).filter(
+      sa.or_(
+          sa.and_(
+              all_models.Relationship.source_id.in_(other_asmnts),
+              all_models.Relationship.source_type == "Assessment",
+              all_models.Relationship.destination_type == "Snapshot",
+          ),
+          sa.and_(
+              all_models.Relationship.destination_id.in_(other_asmnts),
+              all_models.Relationship.destination_type == "Assessment",
+              all_models.Relationship.source_type == "Snapshot",
+          )
+      )
+  ).subquery()
+
+  asmnt_snap_ids = db.session.query(
+      related_id("Assessment", "snapshot_id"),
+  ).filter(
+      related_condition(asmnt, "Snapshot")
+  ).subquery()
+
+  # Find relationships between Snapshot and Issue to unmap
+  unmap_rels = db.session.query(all_models.Relationship.id).filter(
+      sa.and_(
+          related_condition(issue, "Snapshot"),
+          related_id("Issue", "snapshot_id").in_(asmnt_snap_ids),
+          ~related_id("Issue", "snapshot_id").in_(other_asmnt_snap_ids),
+          all_models.Relationship.automapping_id.isnot(None),
+      )
+  )
+
+  # If there are not any relationships between Issue and Assessment
+  # except one we received in request, we can unmap Issue from Audit
+  if not db.session.query(other_asmnts.exists()).scalar():
+    unmap_audit_rels = db.session.query(all_models.Relationship.id).filter(
+        sa.or_(
+            sa.and_(
+                all_models.Relationship.source_id == issue.id,
+                all_models.Relationship.source_type == issue.type,
+                all_models.Relationship.destination_type == issue.audit.type,
+                all_models.Relationship.destination_id == issue.audit.id,
+                all_models.Relationship.automapping_id.isnot(None),
+            ),
+            sa.and_(
+                all_models.Relationship.source_id == issue.audit.id,
+                all_models.Relationship.source_type == issue.audit.type,
+                all_models.Relationship.destination_id == issue.id,
+                all_models.Relationship.destination_type == issue.type,
+                all_models.Relationship.automapping_id.isnot(None),
+            )
+        )
+    )
+    unmap_rels = unmap_rels.union_all(unmap_audit_rels)
+  else:
+    unmap_rels = db.session.query(unmap_rels.subquery())
+
+  all_models.Relationship.query.filter(
+      all_models.Relationship.id.in_(unmap_rels)
+  ).delete(synchronize_session="fetch")
+
+
 def init_hook():
   """Initialize Relationship-related hooks."""
+  # pylint: disable=unused-variable
   sa.event.listen(all_models.Relationship, "before_insert",
                   all_models.Relationship.validate_attrs)
   sa.event.listen(all_models.Relationship, "before_update",
@@ -82,6 +202,7 @@ def init_hook():
   @signals.Restful.collection_posted.connect_via(all_models.Relationship)
   def handle_comment_mapping(sender, objects=None, **kwargs):
     """Update Commentable.updated_at when Comment mapped."""
+    # pylint: disable=unused-argument
     for obj in objects:
       if obj.source_type != u"Comment" and obj.destination_type != u"Comment":
         continue
@@ -95,3 +216,22 @@ def init_hook():
 
   sa.event.listen(sa.orm.session.Session, "before_flush",
                   handle_audit_issue_mapping)
+
+  @signals.Restful.model_deleted.connect_via(all_models.Relationship)
+  def handle_cascade_delete(sender, obj, service):
+    """Process cascade removing of relationship."""
+    # pylint: disable=unused-argument
+    is_asmnt_issue_rel = (
+        obj.source_type == "Assessment" and
+        obj.destination_type == "Issue"
+    ) or (
+        obj.source_type == "Issue" and
+        obj.destination_type == "Assessment"
+    )
+    cascade = service.request.args.get("cascade")
+    if cascade and cascade.lower() == "true" and is_asmnt_issue_rel:
+      asmnt, issue = obj.source, obj.destination
+      if asmnt.type != "Assessment":
+        asmnt, issue = issue, asmnt
+
+      unmap_issue_cascade(asmnt, issue)
