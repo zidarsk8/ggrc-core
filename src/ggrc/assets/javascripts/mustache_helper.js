7f9badb6c176355206da87de5b479965883122b3
m-vitaly <vitaly851001@gmail.com>
2017-10-13 12:40:46 +0300

    Update object review logic with Task Assignees

--- src/ggrc/assets/javascripts/mustache_helper.js
+++ src/ggrc/assets/javascripts/mustache_helper.js
@@ -1440,13 +1440,24 @@ Mustache.registerHelper("last_approved", function (instance, options) {
   return defer_render("span", {done: finish, fail: fail}, loader.refresh_instances());
 });
 
-Mustache.registerHelper("with_is_reviewer", function (review_task, options) {
-  review_task = Mustache.resolve(review_task);
-  var current_user_id = GGRC.current_user.id;
-  var is_reviewer = review_task &&
-      (current_user_id == review_task.contact.id ||
-      Permission.is_allowed("__GGRC_ADMIN__"));
-  return options.fn(options.contexts.add({is_reviewer: is_reviewer}));
+Mustache.registerHelper('with_is_reviewer', function (reviewTask, options) {
+  var assigneeRole = _.find(GGRC.access_control_roles, {
+    object_type: 'TaskGroupTask',
+    name: 'Task Assignees',
+  });
+  var currentUserId = GGRC.current_user.id;
+  var isReviewer;
+
+  reviewTask = Mustache.resolve(reviewTask);
+
+  isReviewer = reviewTask &&
+      (_.some(reviewTask.access_control_list, function (acl) {
+        return acl.ac_role_id === assigneeRole.id &&
+          acl.person &&
+          acl.person.id === currentUserId;
+      }) ||
+      Permission.is_allowed('__GGRC_ADMIN__'));
+  return options.fn(options.contexts.add({is_reviewer: isReviewer}));
 });
 
 Mustache.registerHelper("with_review_task", function (options) {
