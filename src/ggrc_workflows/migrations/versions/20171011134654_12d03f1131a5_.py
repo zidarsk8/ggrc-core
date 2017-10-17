faab5e9971e3a44aea0889804d83db05318ebe24
Miha Zidar <zidarsk8@gmail.com>
2017-10-17 12:21:31 +0200

    Join acl migrations into a single file
    The acl migration is small and there is no reason to have it split into
    multiple files.
    
    The joined migration will also make it easier ta properly change the
    downgrade part so that the database state will be correct (same) before
    an upgrade and after the downgrade.

--- src/ggrc_workflows/migrations/versions/20171011134654_12d03f1131a5_.py
+++ src/ggrc_workflows/migrations/versions/20171011134654_12d03f1131a5_.py
@@ -16,32 +16,8 @@ from alembic import op
 revision = '12d03f1131a5'
 down_revision = '251191c050d0'
 
-INSERT_SQL = """
-INSERT INTO access_control_list
-    (person_id, object_id, ac_role_id, object_type, created_at, updated_at)
-SELECT t.contact_id, t.id, r.id, r.object_type, now(), now()
-FROM task_group_tasks as t
-JOIN access_control_roles as r on r.object_type = "TaskGroupTask"
-WHERE t.contact_id IS NOT NULL and r.name = "Task Assignees"
-UNION ALL
-SELECT t.contact_id, t.id, r.id, r.object_type, now(), now()
-FROM cycle_task_group_object_tasks as t
-JOIN access_control_roles as r on r.object_type = "CycleTaskGroupObjectTask"
-where t.contact_id IS NOT NULL AND r.name = "Task Assignees";
-"""
-
-DELETE_SQL = """
-DELETE a FROM access_control_list AS a
-JOIN access_control_roles AS r on r.id = a.ac_role_id
-WHERE r.name = "Task Assignees"   AND (
-      r.object_type = "TaskGroupTask" OR
-      r.object_type = "CycleTaskGroupObjectTask");
-"""
-
 
 def upgrade():
-  op.execute(INSERT_SQL)
 
 
 def downgrade():
-  op.execute(DELETE_SQL)
