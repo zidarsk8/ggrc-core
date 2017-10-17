1ce5736032d540d494f78b25539ffda500080678
ipin4 <pin4vital@gmail.com>
2017-10-17 17:35:55 +0300

    Change display of people list

---
+++ src/ggrc/assets/javascripts/components/tree/get-owner-people-list.js
@@ -0,0 +1,26 @@
+/*
+  Copyright (C) 2017 Google Inc., authors, and contributors <see AUTHORS file>
+  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+const viewModel = can.Map.extend({
+  role: '',
+  define: {
+    allOwners: {
+      get: function () {
+        const roleId = GGRC.roles
+          .find((item) => item.name == this.role).id;
+        return this.data
+          .filter((item) =>
+            item.instance.role && item.instance.role.id == roleId)
+          .map((item) => item.instance.person);
+      },
+    },
+  },
+});
+
+GGRC.Components('getOwnerPeopleList', {
+  tag: 'get-owner-people-list',
+  template: '<content/>',
+  viewModel: viewModel,
+});
--- src/ggrc/assets/javascripts/components/tree/tree-widget-container.js
+++ src/ggrc/assets/javascripts/components/tree/tree-widget-container.js
@@ -21,6 +21,7 @@ import './tree-item-status-for-workflow';
 import './tree-no-results';
 import './tree-assignee-field';
 import './tree-people-list-field';
+import './get-owner-people-list';
 import './tree-people-with-role-list-field';
 import '../advanced-search/advanced-search-filter-container';
 import '../advanced-search/advanced-search-mapping-container';
