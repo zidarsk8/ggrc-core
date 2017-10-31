d73a4f141224a2f16949a07f2bc839712f9c3c88
Alexander Novik <al.novik88@gmail.com>
2017-10-31 11:04:17 +0300

    Fix linting issues

--- src/ggrc/assets/javascripts/components/related-objects/related-objects.js
+++ src/ggrc/assets/javascripts/components/related-objects/related-objects.js
@@ -1,4 +1,4 @@
-/*!
+/*
  Copyright (C) 2017 Google Inc.
  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
  */
@@ -20,23 +20,23 @@ import {REFRESH_RELATED} from '../../events/eventTypes';
           get: function () {
             return 'No Related ' + this.attr('relatedItemsType') + 's ' +
               'were found';
-          }
+          },
         },
         isLoading: {
           type: 'boolean',
-          value: false
+          value: false,
         },
         paging: {
           value: function () {
             return new GGRC.VM.Pagination({pageSizeSelect: [5, 10, 15]});
-          }
+          },
         },
         relatedObjects: {
-          Value: can.List
+          Value: can.List,
         },
         predefinedFilter: {
-          type: '*'
-        }
+          type: '*',
+        },
       },
       baseInstance: null,
       relatedItemsType: '@',
@@ -55,8 +55,8 @@ import {REFRESH_RELATED} from '../../events/eventTypes';
             expression: {
               object_name: type,
               op: isAssessment ? {name: 'similar'} : {name: 'relevant'},
-              ids: [id]
-            }
+              ids: [id],
+            },
           };
         }
         return filters;
@@ -82,7 +82,7 @@ import {REFRESH_RELATED} from '../../events/eventTypes';
           limit: this.attr('paging.limits'),
           object_name: relatedType,
           order_by: this.getSortingInfo(),
-          filters: filters
+          filters: filters,
         }];
         return params;
       },
@@ -98,7 +98,7 @@ import {REFRESH_RELATED} from '../../events/eventTypes';
             var values = data[relatedType].values;
             var result = values.map(function (item) {
               return {
-                instance: CMS.Models[relatedType].model(item)
+                instance: CMS.Models[relatedType].model(item),
               };
             });
             // Update paging object
@@ -130,7 +130,7 @@ import {REFRESH_RELATED} from '../../events/eventTypes';
       },
       setRelatedItems: function () {
         this.attr('relatedObjects').replace(this.loadRelatedItems());
-      }
+      },
     },
     init: function () {
       this.viewModel.setRelatedItems();
@@ -155,7 +155,7 @@ import {REFRESH_RELATED} from '../../events/eventTypes';
       },
       '{viewModel.orderBy} changed': function () {
         this.viewModel.setRelatedItems();
-      }
-    }
+      },
+    },
   });
 })(window.can, window.GGRC, window.CMS);
