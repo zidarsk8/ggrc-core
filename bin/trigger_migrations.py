75eda39427c29826e1c181ef282f7bc6c2bc7891
Saroj Silwal Khatri <skhatri@skhatri.svl.corp.google.com>
2017-11-01 08:54:44 -0700

    Add logging

--- bin/trigger_migrations.py
+++ bin/trigger_migrations.py
@@ -3,12 +3,16 @@
 
 """Triggers db migration."""
 
+from logging import getLogger
+
 import sys
 import json
 import requests
 import time
 
 CUTOFF_TIME = 600
+
+logger = getLogger(__name__)
 
 
 def trigger(version, appengine_instance, access_token):
@@ -21,7 +25,7 @@ def trigger(version, appengine_instance, access_token):
   post_data = {'access_token': access_token}
   response = requests.post(url, data=post_data)
   response_body = json.loads(response.text)
-  print response_body.get("message")
+  logger.info("Response message : %s", response_body.get("message"))
   task_id = response_body.get("migration_task_id")
 
   if response.status_code == 202 and task_id:
@@ -31,9 +35,9 @@ def trigger(version, appengine_instance, access_token):
       response = requests.get(url)
       response_body = json.loads(response.text)
       if response.status_code != 202:
-        print "Unrecognized http response : {}, {}".format(
-            response.text,
-            response.status_code)
+        logger.error("Unrecognized http response : %s, %s",
+                     response.text,
+                     response.status_code)
         sys.exit(1)
       if response_body.get("status") == "Fail":
         print response_body.get("message")
