1975fc5b8e0bbbb7e6b81e8e9188dd8f9cf5469e
Saroj Silwal Khatri <skhatri@skhatri.svl.corp.google.com>
2017-11-02 16:52:24 -0700

    Add accidentally deleted files

---
+++ extras/deploy_settings_local.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+APPENGINE_INSTANCE=local
+SETTINGS_MODULE="development app_engine ggrc_basic_permissions.settings.development ggrc_risks.settings.development ggrc_risk_assessments.settings.development ggrc_workflows.settings.development ggrc_gdrive_integration.settings.development"
+DATABASE_URI="mysql+mysqldb://root:root@${GGRC_DATABASE_HOST:-localhost}/ggrcdev?charset=utf8"
+SECRET_KEY='Something-secret'
+GOOGLE_ANALYTICS_ID=""
+GOOGLE_ANALYTICS_DOMAIN=""
+GAPI_KEY="$GGRC_GAPI_KEY"
+GAPI_CLIENT_ID="$GGRC_GAPI_CLIENT_ID"
+GAPI_CLIENT_SECRET="$GGRC_GAPI_CLIENT_SECRET"
+GAPI_ADMIN_GROUP='<Google Group Email Address>'
+BOOTSTRAP_ADMIN_USERS='user@example.com'
+MIGRATOR='Default Migrator <migrator@example.com>'
+RISK_ASSESSMENT_URL='http://localhost:8080'
+CUSTOM_URL_ROOT=''
+ABOUT_URL='#'
+ABOUT_TEXT='About GGRC'
+EXTERNAL_HELP_URL='#set_GGRC_EXTERNAL_HELP_URL_env_var'
+EXTERNAL_IMPORT_HELP_URL='#set_GGRC_EXTERNAL_IMPORT_HELP_URL_env_var'
+INSTANCE_CLASS='B4'
+MAX_INSTANCES='4'
+SCALING=$(printf "manual_scaling:\n  instances: ${MAX_INSTANCES}\n")
+STATIC_SERVING=""
+GGRC_Q_INTEGRATION_URL=""
+INTEGRATION_SERVICE_URL="$GGRC_INTEGRATION_SERVICE_URL"
+URLFETCH_SERVICE_ID="$URLFETCH_SERVICE_ID"
+DASHBOARD_INTEGRATION="On"
+ALLOWED_QUERYAPI_APP_IDS=""
+APPENGINE_EMAIL=""
+AUTHORIZED_DOMAIN=""
