bb8dbd9063f4a5aab5e890744b03d9a5a07fce12
Alexander Novik <al.novik88@gmail.com>
2017-11-02 23:13:40 +0300

    Define template for related-assessments component

--- src/ggrc/assets/javascripts/components/related-objects/related-assessments.js
+++ src/ggrc/assets/javascripts/components/related-objects/related-assessments.js
@@ -3,6 +3,8 @@
  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
  */
 
+import template from './templates/related-assessments.mustache';
+
 (function (can, GGRC, CMS) {
   'use strict';
 
@@ -18,6 +20,7 @@
 
   GGRC.Components('relatedAssessments', {
     tag: 'related-assessments',
+    template,
     viewModel: {
       define: {
         baseInstanceDocuments: {
