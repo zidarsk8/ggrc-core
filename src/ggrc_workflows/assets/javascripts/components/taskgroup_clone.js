7c4f6adea3cb15ca6623af8fd8716a3c4123e54d
Alexander Novik <al.novik88@gmail.com>
2017-11-10 15:06:22 +0300

    Move modals related object into lazy loads chunk

--- src/ggrc_workflows/assets/javascripts/components/taskgroup_clone.js
+++ src/ggrc_workflows/assets/javascripts/components/taskgroup_clone.js
@@ -1,34 +1,53 @@
-  /*!
+/*
   Copyright (C) 2017 Google Inc.
   Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
-*/
+ */
 
 import {BUTTON_VIEW_SAVE_CANCEL} from '../../../../ggrc/assets/javascripts/plugins/utils/modals';
 
-(function (GGRC, can) {
-  'use strict';
+let CloneTaskGroup = can.Model.Cacheable({
+  defaults: {
+    clone_objects: true,
+    clone_tasks: true,
+    clone_people: true
+  }
+}, {
+  refresh: function () {
+    return $.when(this);
+  },
+  save: function () {
+    var task_group = new CMS.Models.TaskGroup({
+      clone: this.source_task_group.id,
+      context: null,
+      clone_objects: this.clone_objects,
+      clone_tasks: this.clone_tasks,
+      clone_people: this.clone_people
+    });
 
-  can.Component.extend({
-    tag: "task-group-clone",
-    template: "<content/>",
-    events: {
-      click: function(el) {
-        var $target;
+    return task_group.save();
+  }
+});
 
-        $target = $('<div class="modal hide"></div>').uniqueId();
-        $target.modal_form({}, el);
-        import(/* webpackChunkName: "modalsCtrls" */'../../../../ggrc/assets/javascripts/controllers/modals')
-          .then(() => {
-            $target.ggrc_controllers_modals({
-              modal_title: "Clone Task Group",
-              model: CMS.ModelHelpers.CloneTaskGroup,
-              instance: new CMS.ModelHelpers.CloneTaskGroup({source_task_group: this.scope.taskGroup}),
-              content_view: GGRC.mustache_path + "/task_groups/clone_modal_content.mustache",
-              custom_save_button_text: "Proceed",
-              button_view: BUTTON_VIEW_SAVE_CANCEL
-            });
+can.Component.extend({
+  tag: "task-group-clone",
+  template: "<content/>",
+  events: {
+    click: function (el) {
+      var $target;
+
+      $target = $('<div class="modal hide"></div>').uniqueId();
+      $target.modal_form({}, el);
+      import(/* webpackChunkName: "modalsCtrls" */'../../../../ggrc/assets/javascripts/controllers/modals')
+        .then(() => {
+          $target.ggrc_controllers_modals({
+            modal_title: "Clone Task Group",
+            model: CloneTaskGroup,
+            instance: new CloneTaskGroup({source_task_group: this.scope.taskGroup}),
+            content_view: GGRC.mustache_path + "/task_groups/clone_modal_content.mustache",
+            custom_save_button_text: "Proceed",
+            button_view: BUTTON_VIEW_SAVE_CANCEL
           });
-      }
+        });
     }
-  });
-})(window.GGRC, window.can);
+  }
+});
--- src/ggrc_workflows/assets/javascripts/components/workflow_clone.js
+++ src/ggrc_workflows/assets/javascripts/components/workflow_clone.js
@@ -5,30 +5,53 @@
 
 import {BUTTON_VIEW_SAVE_CANCEL} from '../../../../ggrc/assets/javascripts/plugins/utils/modals';
 
-(function (GGRC, can) {
-  'use strict';
+let CloneWorkflow = can.Model.Cacheable({
+  defaults : {
+    clone_people: true,
+    clone_tasks: true,
+    clone_objects: true
+  }
+}, {
+  refresh: function() {
+    return $.when(this);
+  },
+  save: function() {
+    var workflow = new CMS.Models.Workflow({
+      clone: this.source_workflow.id,
+      context: null,
+      clone_people: this.clone_people,
+      clone_tasks: this.clone_tasks,
+      clone_objects: this.clone_objects
+    });
 
-  can.Component.extend({
-    tag: "workflow-clone",
-    template: "<content/>",
-    events: {
-      click: function(el) {
-        var workflow, $target;
+    return workflow.save().then(function(workflow) {
+      GGRC.navigate(workflow.viewLink);
+      return this;
+    });
 
-        $target = $('<div class="modal hide"></div>').uniqueId();
-        $target.modal_form({}, el);
-        import(/* webpackChunkName: "modalsCtrls" */'../../../../ggrc/assets/javascripts/controllers/modals')
-          .then(() => {
-            $target.ggrc_controllers_modals({
-              modal_title: "Clone Workflow",
-              model: CMS.ModelHelpers.CloneWorkflow,
-              instance: new CMS.ModelHelpers.CloneWorkflow({source_workflow: this.scope.workflow}),
-              content_view: GGRC.mustache_path + "/workflows/clone_modal_content.mustache",
-              custom_save_button_text: "Proceed",
-              button_view: BUTTON_VIEW_SAVE_CANCEL
-            });
+  }
+});
+
+can.Component.extend({
+  tag: "workflow-clone",
+  template: "<content/>",
+  events: {
+    click: function(el) {
+      var $target;
+
+      $target = $('<div class="modal hide"></div>').uniqueId();
+      $target.modal_form({}, el);
+      import(/* webpackChunkName: "modalsCtrls" */'../../../../ggrc/assets/javascripts/controllers/modals')
+        .then(() => {
+          $target.ggrc_controllers_modals({
+            modal_title: "Clone Workflow",
+            model: CloneWorkflow,
+            instance: new CloneWorkflow({source_workflow: this.scope.workflow}),
+            content_view: GGRC.mustache_path + "/workflows/clone_modal_content.mustache",
+            custom_save_button_text: "Proceed",
+            button_view: BUTTON_VIEW_SAVE_CANCEL
           });
-      }
+        });
     }
-  });
-})(window.GGRC, window.can);
+  }
+});
