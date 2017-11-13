d5b5f723c53c1abcc6e19208b202baae2dd196f9
Alexander Novik <al.novik88@gmail.com>
2017-11-13 12:17:51 +0300

    Move common modules between pages into separate file

--- src/ggrc/assets/javascripts/entrypoints/dashboard/index.js
+++ src/ggrc/assets/javascripts/entrypoints/dashboard/index.js
@@ -3,29 +3,7 @@
  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
  */
 
-import '../../permission';
-import '../../bootstrap/modal-form';
-import '../../plugins/lodash_helpers';
-import '../../plugins/persistent_notifier';
-import '../../ggrc_base';
-import '../../apps/base_widgets';
-import '../../application';
-import '../../plugins/ggrc_utils';
-import '../../plugins/datepicker';
-import '../../plugins/can_control';
-import '../../plugins/autocomplete';
-import '../../plugins/ajax_extensions';
-import '../../plugins/canjs_extensions';
-import '../../plugins/component_registry';
-import '../../plugins/openclose';
-import '../../plugins/tooltip';
-import '../../plugins/popover';
-import '../../plugins/popover_template';
-import '../../mustache_helper';
-import '../../generated/ggrc_filter_query_parser';
-
-// Models
-import '../../models';
+import '../commons';
 
 // Controllers
 import '../../controllers/tree/tree-loader';
