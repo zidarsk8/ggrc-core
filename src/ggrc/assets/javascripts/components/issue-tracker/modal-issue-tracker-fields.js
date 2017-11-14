416f3a71798ba6ac2a5c029e21fd93db14989aa0
YuriChupyrkin <yu4.1990@gmail.com>
2017-11-14 15:31:14 +0300

    Use 'modal-issue-tracker-fields' as component

---
+++ src/ggrc/assets/javascripts/components/issue-tracker/modal-issue-tracker-fields.js
@@ -0,0 +1,16 @@
+/*
+ Copyright (C) 2017 Google Inc., authors, and contributors
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
+import template from './templates/modal-issue-tracker-fields.mustache';
+
+const tag = 'modal-issue-tracker-fields';
+
+export default GGRC.Components('modalIssueTrackerFields', {
+  tag: tag,
+  template: template,
+  viewModle: {
+    instance: {},
+  },
+});
