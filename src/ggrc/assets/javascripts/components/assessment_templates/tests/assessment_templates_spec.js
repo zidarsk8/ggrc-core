740292722480070469551afe33c10f996d953025
m-vitaly <vitaly851001@gmail.com>
2017-11-16 10:56:02 +0300

    Migrate assessment-templates on ES6

--- src/ggrc/assets/javascripts/components/assessment_templates/tests/assessment_templates_spec.js
+++ src/ggrc/assets/javascripts/components/assessment_templates/tests/assessment_templates_spec.js
@@ -1,75 +1,74 @@
-/*!
+/* !
   Copyright (C) 2017 Google Inc.
   Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
-describe('GGRC.Components.assessmentTemplates', function () {
-  'use strict';
+import Component from '../assessment_templates';
 
-  var viewModel;
+describe('assessmentTemplates', ()=> {
+  let viewModel;
 
-  beforeAll(function () {
-    viewModel = GGRC.Components.getViewModel('assessmentTemplates');
+  beforeAll(()=> {
+    viewModel = new (can.Map.extend(Component.prototype.viewModel));
   });
 
   describe('_selectInitialTemplate() method', function () {
-    var templates;
+    let templates;
 
-    beforeEach(function () {
+    beforeEach(()=> {
       templates = [
         {
           title: 'No Template',
-          value: ''
+          value: '',
         },
         {
           group: 'FooBarBaz objects',
           subitems: [
             {title: 'object Foo', value: 'foo'},
             {title: 'object Bar', value: 'bar'},
-            {title: 'object Baz', value: 'baz'}
-          ]
+            {title: 'object Baz', value: 'baz'},
+          ],
         },
         {
           group: 'Animal objects',
           subitems: [
             {title: 'Elephant Dumbo', value: 'elephant'},
             {title: 'Flying Pig', value: 'pig'},
-            {title: 'Tiny Mouse', value: 'mouse'}
-          ]
-        }
+            {title: 'Tiny Mouse', value: 'mouse'},
+          ],
+        },
       ];
     });
 
     it('selects the first item from the first option group if it was empty',
-    function () {
-      viewModel.attr('assessmentTemplate', null);
-      viewModel._selectInitialTemplate(templates);
-      expect(viewModel.attr('assessmentTemplate')).toEqual('foo');
-    });
+      ()=> {
+        viewModel.attr('assessmentTemplate', null);
+        viewModel._selectInitialTemplate(templates);
+        expect(viewModel.attr('assessmentTemplate')).toEqual('foo');
+      });
 
-    it('leaves item if the option was not empty', function () {
+    it('leaves item if the option was not empty', ()=> {
       viewModel.attr('assessmentTemplate', 'template-123');
       viewModel._selectInitialTemplate(templates);
       expect(viewModel.attr('assessmentTemplate')).toEqual('template-123');
     });
 
-    it('leaves the current template unchanged if only a dummy value in ' +
-      'the templates list',
-      function () {
+    it(`leaves the current template unchanged if only a dummy value in
+      the templates list`,
+      ()=> {
         viewModel.attr('assessmentTemplate', 'template-123');
-        templates.splice(1);  // keep only the 1st (dummy) option
+        templates.splice(1); // keep only the 1st (dummy) option
 
         viewModel._selectInitialTemplate(templates);
 
         expect(viewModel.attr('assessmentTemplate')).toEqual('template-123');
-      }
-    );
+      });
 
     it('leaves the current template unchanged if first object group empty',
-      function () {
+      ()=> {
         viewModel.attr('assessmentTemplate', 'template-123');
         templates[1].subitems.length = 0;
-        spyOn(console, 'warn');  // just to silence it
+        spyOn(console, 'warn'); // just to silence it
 
         viewModel._selectInitialTemplate(templates);
 
@@ -77,10 +76,10 @@ describe('GGRC.Components.assessmentTemplates', function () {
       }
     );
 
-    it('issues a warning if an empty group is encountered', function () {
-      var expectedMsg = [
+    it('issues a warning if an empty group is encountered', ()=> {
+      const expectedMsg = [
         'GGRC.Components.assessmentTemplates: ',
-        'An empty template group encountered, possible API error'
+        'An empty template group encountered, possible API error',
       ].join('');
 
       spyOn(console, 'warn');
@@ -92,7 +91,7 @@ describe('GGRC.Components.assessmentTemplates', function () {
     });
 
     it('selects the first non-dummy value if it precedes all object groups',
-      function () {
+      ()=> {
         viewModel.attr('assessmentTemplate', null);
         templates.splice(1, 0, {title: 'No Group Template', value: 'single'});
 
