e5bc01e163e617af3f9f69a84ada6d5e05f3328c
ivan_matsiash <ivan.matyash97@gmail.com>
2017-11-16 14:59:13 +0300

    Fix problem of creating new revision after deleting CAD

--- test/integration/ggrc/models/mixins/test_customattributable.py
+++ test/integration/ggrc/models/mixins/test_customattributable.py
@@ -3,6 +3,8 @@
 
 """Integration test for custom attributable mixin"""
 
+import ddt
+
 from ggrc import db
 from ggrc import models
 
@@ -10,6 +12,8 @@ from integration.ggrc import TestCase
 from integration.ggrc.models.factories import ProgramFactory
 from integration.ggrc.models.factories import \
     CustomAttributeDefinitionFactory as CAD
+from integration.ggrc.models import factories
+from integration.ggrc import api_helper
 
 
 class TestCustomAttributableMixin(TestCase):
@@ -62,7 +66,6 @@ class TestCustomAttributableMixin(TestCase):
   def test_updating_ca_values(self):
     """Test updating custom attribute values."""
     cad1 = CAD(definition_type="program", title="CA 1",)
-
     val1 = models.CustomAttributeValue(
         attribute_value="55",
         custom_attribute=cad1,
@@ -198,3 +201,62 @@ class TestCustomAttributableMixin(TestCase):
         set(v.attribute_value for v in prog.custom_attribute_values),
     )
     self.assertEqual(len(prog.custom_attribute_values), 2)
+
+
+@ddt.ddt
+class TestCreateRevisionAfterDeleteCAD(TestCase):
+  """Test cases for creating new revision after delete CAD"""
+  def setUp(self):
+    self.api_helper = api_helper.Api()
+
+  @ddt.data(True, False)
+  def test_latest_revision_delete_cad(self, is_add_cav):
+    """Test creating new revision after deleting CAD.
+
+    In case of deleting CAD, snapshot attribute is_latest_revision
+    must be False
+    """
+    with factories.single_commit():
+      control = factories.ControlFactory()
+      program = factories.ProgramFactory()
+      factories.RelationshipFactory(
+          source=program,
+          destination=control,
+      )
+
+      audit = factories.AuditFactory()
+
+      factories.RelationshipFactory(
+          source=audit,
+          destination=control
+      )
+      cad = factories.CustomAttributeDefinitionFactory(
+          title="test_name",
+          definition_type="control",
+          attribute_type="Text",
+      )
+
+      if is_add_cav:
+        factories.CustomAttributeValueFactory(
+            custom_attribute=cad,
+            attributable=control,
+            attribute_value="test")
+
+      last_revision = models.Revision.query.filter(
+          models.Revision.resource_id == control.id,
+          models.Revision.resource_type == control.type,
+      ).order_by(models.Revision.id.desc()).first()
+
+      snapshot = factories.SnapshotFactory(
+          parent=audit,
+          child_id=control.id,
+          child_type=control.type,
+          revision=last_revision)
+
+    self.assertTrue(snapshot.is_latest_revision)
+
+    self.api_helper.delete(cad)
+
+    snapshot = models.Snapshot.query.filter().first()
+
+    self.assertEqual(snapshot.is_latest_revision, False)
