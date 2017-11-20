4ae499975e02fa66c3973ca979bcc29ed69050e1
Alexander Novik <al.novik88@gmail.com>
2017-11-20 12:36:53 +0300

    Polishing dashboard bootstrap entry

--- src/ggrc/assets/javascripts/entrypoints/dashboard/bootstrap.js
+++ src/ggrc/assets/javascripts/entrypoints/dashboard/bootstrap.js
@@ -10,18 +10,19 @@ import {
   getPageType,
   initCounts,
   initWidgets,
+  isMyAssessments,
+  isMyWork,
 } from '../../plugins/utils/current-page-utils';
 
-var $area = $('.area').first();
-var defaults;
-var extraPageOptions;
-var instance;
-var location = window.location.pathname;
-var isAssessmentsView;
-var isObjectBrowser;
-var modelName;
-var widgetList;
-var widgetModels;
+const $area = $('.area').first();
+const instance = GGRC.page_instance();
+const location = window.location.pathname;
+const isAssessmentsView = isMyAssessments();
+const modelName = instance.constructor.shortName;
+let defaults;
+let extraPageOptions;
+let widgetList;
+let widgetModels;
 
 extraPageOptions = {
   Program: {
@@ -31,8 +32,8 @@ extraPageOptions = {
   },
   Person: {
     page_title: function (controller) {
-      var instance = controller.options.instance;
-      return /dashboard/.test(window.location) ?
+      const instance = controller.options.instance;
+      return isMyWork() ?
         'GRC: My Work' :
         'GRC Profile: ' +
         (instance.name && instance.name.trim()) ||
@@ -41,41 +42,27 @@ extraPageOptions = {
   }
 };
 
-isAssessmentsView = /^\/assessments_view/.test(location);
-isObjectBrowser = /^\/objectBrowser\/?$/.test(location);
+initWidgets();
 
-if (/^\/\w+\/\d+($|\?|\#)/.test(location) || /^\/dashboard/.test(location) ||
-  isAssessmentsView || isObjectBrowser) {
-  instance = GGRC.page_instance();
-  modelName = instance.constructor.shortName;
+widgetList = getWidgetList(modelName, location);
+defaults = getDefaultWidgets(widgetList, location);
+widgetModels = getWidgetModels(modelName, location);
 
-  initWidgets();
-
-  widgetList = getWidgetList(modelName, location);
-  defaults = getDefaultWidgets(widgetList, location);
-  widgetModels = getWidgetModels(modelName, location);
-
-  if (!isAssessmentsView && getPageType() !== 'Workflow') {
-    initCounts(widgetModels, instance.type, instance.id);
-  }
-
-  $area.cms_controllers_page_object(can.extend({
-    widget_descriptors: widgetList,
-    default_widgets: defaults || GGRC.default_widgets || [],
-    instance: GGRC.page_instance(),
-    header_view: GGRC.mustache_path + '/base_objects/page_header.mustache',
-    GGRC: GGRC,  // make the global object available in Mustache templates
-    page_title: function (controller) {
-      return controller.options.instance.title;
-    },
-    page_help: function (controller) {
-      return controller.options.instance.constructor.table_singular;
-    },
-    current_user: GGRC.current_user
-  }, extraPageOptions[modelName]));
-} else {
-  $area.cms_controllers_dashboard({
-    widget_descriptors: GGRC.widget_descriptors,
-    default_widgets: GGRC.default_widgets
-  });
+if (!isAssessmentsView && getPageType() !== 'Workflow') {
+  initCounts(widgetModels, instance.type, instance.id);
 }
+
+$area.cms_controllers_page_object(can.extend({
+  widget_descriptors: widgetList,
+  default_widgets: defaults || GGRC.default_widgets || [],
+  instance: GGRC.page_instance(),
+  header_view: GGRC.mustache_path + '/base_objects/page_header.mustache',
+  GGRC: GGRC,  // make the global object available in Mustache templates
+  page_title: function (controller) {
+    return controller.options.instance.title;
+  },
+  page_help: function (controller) {
+    return controller.options.instance.constructor.table_singular;
+  },
+  current_user: GGRC.current_user
+}, extraPageOptions[modelName]));
