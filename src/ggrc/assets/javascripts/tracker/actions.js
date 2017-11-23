74ce268779e5a7ba108addfcd7a022f0ab85a78d
Alexander Novik <al.novik88@gmail.com>
2017-11-23 18:11:48 +0300

    Define tracker logic

---
+++ src/ggrc/assets/javascripts/tracker/actions.js
@@ -0,0 +1,30 @@
+/*
+    Copyright (C) 2017 Google Inc.
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+export const FOCUS_AREAS = {
+  ASSESSMENT: 'Assessment',
+  AUDIT: 'Audit',
+  RELATED_ASSESSMENTS: 'Related Assessments',
+  SNAPSHOTS: 'Snapshots',
+  IMPORT_AND_EXPORT: 'Import/Export',
+};
+
+export const USER_JOURNEY_KEYS = {
+  LOADING: 'Loading',
+  NAVIGATION: 'Navigation',
+  ADD_ATTACHMENT: 'Add attachment',
+  MAPPING_TO_OBJECT: 'Mapping to object',
+};
+
+export const USER_ACTIONS = {
+  MULTISELECT_FILTER: 'Multi-select filter',
+  OPEN_INFO_PANE: 'Open Info pane',
+  ADVANCED_SEARCH_FILTER: 'Advanced search filter',
+  ASSESSMENT: {
+    OPEN_ASMT_GEN_MODAL: 'Open Assessment Generation modal',
+    CHANGE_STATUS: 'Change status',
+    EDIT_LCA: 'Editing Local CA',
+  },
+};
---
+++ src/ggrc/assets/javascripts/tracker/base.js
@@ -0,0 +1,10 @@
+/*
+    Copyright (C) 2017 Google Inc.
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+export default class BaseStrategy {
+  constructor() {
+  }
+  timing(category, variable, label, value) {}
+}
