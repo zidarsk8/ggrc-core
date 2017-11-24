a5c7cf4d3e54d6f748b874745b90c835b7deec15
Ilya Tsupryk <ilya.tsupryk@gmail.com>
2017-11-24 09:01:06 -0500

    Fix wrong recipients in db

---
+++ src/ggrc/migrations/versions/20171124111204_3911f39325b4_fix_wrong_recipients.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2017 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""
+Fix wrong recipients
+
+Create Date: 2017-11-24 11:12:04.455535
+"""
+# disable Invalid constant name pylint warning for mandatory Alembic variables.
+# pylint: disable=invalid-name
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '3911f39325b4'
+down_revision = '3c69a1e45812'
+
+
+def upgrade():
+  """Upgrade database schema and/or data, creating a new revision."""
+  op.execute("""
+      UPDATE assessments
+      SET recipients = ''
+      WHERE recipients RLIKE '^,+$';
+  """)
+
+
+def downgrade():
+  """Downgrade database schema and/or data back to the previous revision."""
+  # We can't change recients back as we don't know what item was empty
+  # at first and what not (also there is no sense to do it).
