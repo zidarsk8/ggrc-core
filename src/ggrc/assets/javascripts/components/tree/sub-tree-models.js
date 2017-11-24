835998a47fc958fdebf71bdd32b0a7d0960a96a8
Klim Shuplenkou <kshuplenkou@google.com>
2017-11-24 13:12:57 +0300

    Add childModelsMap to sub-tree-models component

--- src/ggrc/assets/javascripts/components/tree/sub-tree-models.js
+++ src/ggrc/assets/javascripts/components/tree/sub-tree-models.js
@@ -4,6 +4,13 @@
  */
 
 import template from './templates/sub-tree-models.mustache';
+import childModelsMap from '../tree/child-models-map';
+import {
+  getModelsForSubTier,
+} from '../../plugins/utils/tree-view-utils';
+import {
+  getWidgetConfig,
+} from '../../plugins/utils/object-versions-utils';
 
 var viewModel = can.Map.extend({
   define: {
@@ -20,22 +27,61 @@ var viewModel = can.Map.extend({
         });
       },
     },
+    selectedModels: {
+      set: function (newModels) {
+        var modelsList = this.attr('modelsList') || [];
+
+        modelsList.forEach(function (item) {
+          item.attr('display', newModels.indexOf(item.name) !== -1);
+        });
+        return newModels;
+      },
+    },
   },
+  init: function () {
+    var modelName = this.attr('type');
+    var defaultModels = getModelsForSubTier(modelName).selected;
+    this.attr('modelsList', this.getDisplayModels(modelName));
+
+    // list of models can be changed in others tree-items
+    childModelsMap.attr('container').bind(modelName, function (ev) {
+      this.attr('selectedModels',
+        childModelsMap.getModels(modelName) || defaultModels);
+    }.bind(this));
+  },
+  type: null,
   modelsList: null,
   title: null,
-  $el: null,
   activate: function () {
     this.attr('isActive', true);
   },
-  setDisplayModels: function (ev) {
-    var selectedModels = this.attr('modelsList').filter(function (item) {
-      return item.display;
-    }).map(function (item) {
-      return item.name;
-    });
-    can.trigger(this.attr('$el'), 'childModelsChange', [selectedModels]);
+  // is called when "Set Visibility" button is clicked
+  setVisibility: function (ev) {
+    var selectedModels = this.getSelectedModels();
+
+    childModelsMap.setModels(this.attr('type'), selectedModels);
+
     this.attr('isActive', false);
     ev.stopPropagation();
+  },
+  getDisplayModels: function (parentType) {
+    var savedModels = childModelsMap.getModels(parentType);
+    var defaultModels = getModelsForSubTier(parentType);
+    var selectedModels = savedModels || defaultModels.selected;
+    var displayList;
+
+    displayList = defaultModels.available.map(function (model) {
+      return {
+        widgetName: getWidgetConfig(model).widgetName,
+        name: model,
+        display: selectedModels.indexOf(model) !== -1,
+      };
+    });
+    return displayList;
+  },
+  getSelectedModels: function () {
+    return this.attr('modelsList').filter((model) => model.display)
+      .map((model) => model.name);
   },
   selectAll: function (ev) {
     ev.stopPropagation();
@@ -56,9 +102,6 @@ GGRC.Components('subTreeModels', {
   template: template,
   viewModel: viewModel,
   events: {
-    inserted: function () {
-      this.viewModel.attr('$el', this.element);
-    },
     '.sub-tree-models mouseleave': function () {
       this.viewModel.attr('isActive', false);
     },
