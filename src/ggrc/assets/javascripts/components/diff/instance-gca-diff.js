a161d97dc297bec86d98d71a1f1328034d35c0d0
YuriChupyrkin <yu4.1990@gmail.com>
2017-12-07 18:33:11 +0300

    Add instance-gca-diff component

---
+++ src/ggrc/assets/javascripts/components/diff/instance-gca-diff.js
@@ -0,0 +1,141 @@
+/*
+ Copyright (C) 2017 Google Inc., authors, and contributors
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
+import DiffBaseVM from './diff-base-vm';
+const tag = 'instance-gca-diff';
+
+const viewModel = DiffBaseVM.extend({
+  modifiedAttributes: {},
+  _customAttributesValues: [],
+
+  prepareAttributes() {
+    const values = this.attr('currentInstance.custom_attribute_values')
+      .map((value) => value.reify());
+
+    this.attr('_customAttributesValues', values);
+  },
+  buildDiffObject() {
+    const modifiedAttributes = this.attr('modifiedAttributes');
+    const caKeys = can.Map.keys(modifiedAttributes);
+
+    this.prepareAttributes();
+    this.attr('diff', []);
+
+    caKeys.forEach((attrId) => {
+      let attr;
+      let modifiedAttr;
+      attrId = Number(attrId);
+
+      attr = this.getValueAndDefinition(attrId);
+      modifiedAttr = modifiedAttributes[attrId];
+
+      // attr was deleted
+      if (!attr.def) {
+        return;
+      }
+
+      if (attr.def.attribute_type !== 'Map:Person') {
+        const diffObject = this.buildAttributeDiff(modifiedAttr, attr);
+        this.attr('diff').push(diffObject);
+        return;
+      }
+
+      this.buildPersonDiff(modifiedAttr, attr).then((diffObject) => {
+        this.attr('diff').push(diffObject);
+      });
+    });
+  },
+  buildAttributeDiff(modifiedAttr, currentAttr) {
+    const value = currentAttr.value;
+    const def = currentAttr.def;
+    let modifiedVal = this.attr('emptyValue');
+    let currentVal = this.attr('emptyValue');
+
+    if (value) {
+      currentVal = this
+        .convertValue(value.attribute_value, def.attribute_type);
+    }
+
+    if (modifiedAttr && modifiedAttr.attribute_value) {
+      modifiedVal = this
+        .convertValue(modifiedAttr.attribute_value, def.attribute_type);
+    }
+
+    return {
+      attrName: def.title,
+      currentVal: currentVal,
+      modifiedVal: modifiedVal,
+    };
+  },
+  buildPersonDiff(modifiedAttr, currentAttr) {
+    const val = currentAttr.value;
+    const def = currentAttr.def;
+    const dfd = can.Deferred();
+    const diffObject = {
+      attrName: def.title,
+      currentVal: this.attr('emptyValue'),
+      modifiedVal: this.attr('emptyValue'),
+    };
+
+    if (modifiedAttr.attribute_object) {
+      diffObject.modifiedVal = modifiedAttr.attribute_object.email;
+    }
+
+    // value is empty. Attr filled first time
+    if (val && val.attribute_object) {
+      GGRC.Utils.getPersonInfo(val.attribute_object).then((person) => {
+        diffObject.currentVal = person.email;
+        dfd.resolve(diffObject);
+      });
+    } else {
+      dfd.resolve(diffObject);
+    }
+
+    return dfd;
+  },
+  convertValue(value, type) {
+    if (!value) {
+      return this.attr('emptyValue');
+    }
+
+    switch (type) {
+      case 'Date':
+        return GGRC.Utils.formatDate(value, true);
+      case 'Checkbox':
+        return value === true || value === '1' ?
+          '✓' :
+          this.attr('emptyValue');
+      default:
+        return value;
+    }
+  },
+  getValueAndDefinition(attrId) {
+    const instance = this.attr('currentInstance');
+    const value = this.attr('_customAttributesValues').attr()
+      .find((val) => val.custom_attribute_id === attrId);
+    const definition = instance.attr('custom_attribute_definitions').attr()
+      .find((def) => def.id === attrId);
+
+    return {
+      value: value,
+      def: definition,
+    };
+  },
+});
+
+export default can.Component.extend({
+  tag,
+  viewModel: viewModel,
+  events: {
+    inserted() {
+      const instance = this.viewModel.attr('currentInstance');
+      const modifiedCA = this.viewModel.attr('modifiedAttributes');
+      if (!modifiedCA || !instance) {
+        return;
+      }
+      this.viewModel.buildDiffObject();
+    },
+  },
+});
---
+++ src/ggrc/assets/javascripts/components/diff/tests/instance-gca-diff_spec.js
@@ -0,0 +1,113 @@
+/*
+ Copyright (C) 2017 Google Inc., authors, and contributors
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
+import Component from '../instance-gca-diff';
+
+describe('instance-gca-diff component', () => {
+  let viewModel;
+  let emptyValue;
+  const EXPECTED_CA_TITLE = 'Attr title...';
+
+  beforeAll(() => {
+    viewModel = getComponentVM(Component);
+    emptyValue = viewModel.attr('emptyValue');
+  });
+
+  describe('"buildAttributeDiff" method', () => {
+    let buildAttributeDiff;
+
+    beforeAll(() => {
+      buildAttributeDiff = viewModel.buildAttributeDiff.bind(viewModel);
+    });
+
+    function buildCurrentAttrObject(value) {
+      const currentAttr = {
+        def: {
+          title: EXPECTED_CA_TITLE,
+        },
+        value: {
+          attribute_value: value,
+        },
+      };
+
+      return currentAttr;
+    }
+
+    it('should return diff with empty currentVal', () => {
+      const expectedValue = 'simple text';
+      const modifiedAttr = {
+        attribute_value: expectedValue,
+      };
+      const currentAttr = buildCurrentAttrObject();
+      const result = buildAttributeDiff(modifiedAttr, currentAttr);
+
+      expect(result.attrName).toEqual(EXPECTED_CA_TITLE);
+      expect(result.currentVal).toEqual(emptyValue);
+      expect(result.modifiedVal).toEqual(expectedValue);
+    });
+
+    it('should return diff with empty modifiedVal', () => {
+      const expectedValue = 'simple text';
+      const modifiedAttr = {
+        attribute_value: '',
+      };
+      const currentAttr = buildCurrentAttrObject(expectedValue);
+      const result = buildAttributeDiff(modifiedAttr, currentAttr);
+
+      expect(result.attrName).toEqual(EXPECTED_CA_TITLE);
+      expect(result.modifiedVal).toEqual(emptyValue);
+      expect(result.currentVal).toEqual(expectedValue);
+    });
+
+    it('should return diff with filled fields', () => {
+      const expectedValue = 'simple text';
+      const expectedCurrentValue = 'simple';
+      const modifiedAttr = {
+        attribute_value: expectedValue,
+      };
+      const currentAttr = buildCurrentAttrObject(expectedCurrentValue);
+      const result = buildAttributeDiff(modifiedAttr, currentAttr);
+
+      expect(result.attrName).toEqual(EXPECTED_CA_TITLE);
+      expect(result.modifiedVal).toEqual(expectedValue);
+      expect(result.currentVal).toEqual(expectedCurrentValue);
+    });
+  });
+
+  describe('"convertValue" function', () => {
+    const CHECKBOX_TRUE_VALUE = '✓';
+    const CHECKOBX_TYPE = 'Checkbox';
+    let convertValue;
+
+    beforeAll(() => {
+      convertValue = viewModel.convertValue.bind(viewModel);
+    });
+
+    it('should return emptyValue. Empty string', () => {
+      const result = convertValue('', 'Text');
+      expect(result).toEqual(emptyValue);
+    });
+
+    it('should return true. Checkbox type. Value is string', () => {
+      const result = convertValue('1', CHECKOBX_TYPE);
+      expect(result).toEqual(CHECKBOX_TRUE_VALUE);
+    });
+
+    it('should return true. Checkbox type. Value is boolean (true)', () => {
+      const result = convertValue(true, CHECKOBX_TYPE);
+      expect(result).toEqual(CHECKBOX_TRUE_VALUE);
+    });
+
+    it('should return true. Checkbox type. Value is boolean (false)', () => {
+      const result = convertValue(false, CHECKOBX_TYPE);
+      expect(result).toEqual(emptyValue);
+    });
+
+    it('should return emptyValue. Checkbox type. Wrong string', () => {
+      const result = convertValue('11', CHECKOBX_TYPE);
+      expect(result).toEqual(emptyValue);
+    });
+  });
+});
