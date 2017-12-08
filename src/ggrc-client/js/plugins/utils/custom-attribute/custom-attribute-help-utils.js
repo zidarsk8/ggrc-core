cb15982e3c4617d0c1fe1fb4f89d5f5bb18b5fb7
Yury Ostroukh <0str0uchura@gmail.com>
2017-12-08 14:42:05 +0300

    Add help utils for validation actions

---
+++ src/ggrc-client/js/plugins/utils/custom-attribute/custom-attribute-help-utils.js
@@ -0,0 +1,24 @@
+/*
+ Copyright (C) 2018 Google Inc.
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+/**
+ * Checks if the [custom attribute object]{@link CustomAttributeObject} has
+ * an empty value.
+ * @param {CustomAttributeObject} caObject - The custom attribute object.
+ * @return {boolean} - true if caObject has an empty value else false.
+ */
+function hasEmptyValue(caObject) {
+  const value = caObject.value;
+
+  if (typeof value === 'string') {
+    return _.flow(_.trim, _.isEmpty)(value);
+  }
+
+  return Boolean(value) === false;
+}
+
+export {
+  hasEmptyValue,
+};
