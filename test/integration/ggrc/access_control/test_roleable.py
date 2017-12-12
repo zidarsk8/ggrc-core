ec7677d6f5ed1f91859a99691a26554f4d7b50d6
Anže Pečar <apecar@google.com>
2017-12-12 10:23:34 +0000

    Improve roleable tests
    1. Replace assert statement with self.assert statements
    2. Add test for removing a person from access_control_list

--- test/integration/ggrc/access_control/test_roleable.py
+++ test/integration/ggrc/access_control/test_roleable.py
@@ -34,18 +34,19 @@ class TestAccessControlRoleable(TestCase):
     obj = all_models.Control(
         title="New Control",
         access_control_list=acl_list(self))
-    assert obj.access_control_list is not None
+    self.assertIsNotNone(obj.access_control_list)
     acl = obj.access_control_list[0]
-    assert acl is not None
-    assert isinstance(acl, all_models.AccessControlList)
-    assert acl.person.id == self.person.id
-    assert acl.ac_role.id == self.role.id
-    assert acl.object == obj
+    self.assertIsNotNone(acl)
+    self.assertIsInstance(acl, all_models.AccessControlList)
+    self.assertEqual(acl.person.id, self.person.id)
+    self.assertEqual(acl.ac_role.id, self.role.id)
+    self.assertEqual(acl.object, obj)
 
   def test_with_dict_objs_multiple(self):
     """Test access_control_list setter without ids"""
     person_1 = all_models.Person(name="Frodo", email="frodo@baggins.com")
     person_2 = all_models.Person(name="Bilbo", email="bilbo@baggins.com")
+    person_3 = factories.PersonFactory(name="Merry", email="merry@buck.com")
     role = all_models.AccessControlRole(name="Hobbit")
     obj = all_models.Control(title="Test Control", access_control_list=[{
         "person": person_1,
@@ -55,12 +56,31 @@ class TestAccessControlRoleable(TestCase):
         "ac_role": role,
     }])
     db.session.commit()
-    assert obj.access_control_list is not None
-    assert len(obj.access_control_list) == 2
-    assert obj.access_control_list[0].person == person_1
-    assert obj.access_control_list[1].person == person_2
+    self.assertIsNotNone(obj.access_control_list)
+    self.assertEqual(len(obj.access_control_list), 2)
+    self.assertEqual(obj.access_control_list[0].person, person_1)
+    self.assertEqual(obj.access_control_list[1].person, person_2)
     num_acls = all_models.AccessControlList.query.filter(
         all_models.AccessControlList.object_id == obj.id,
         all_models.AccessControlList.object_type == "Control"
     ).count()
-    assert num_acls == 2
+    self.assertEqual(num_acls, 2)
+
+    obj.access_control_list = [{
+        "person": {
+            "id": person_2.id,
+        },
+        "ac_role_id": role.id,
+    }, {
+        "person": {
+            "id": person_3.id,
+        },
+        "ac_role_id": role.id,
+    }]
+    db.session.commit()
+
+    num_acls = all_models.AccessControlList.query.filter(
+        all_models.AccessControlList.object_id == obj.id,
+        all_models.AccessControlList.object_type == "Control"
+    ).count()
+    self.assertEqual(num_acls, 2)
