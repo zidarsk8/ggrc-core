27f932443de2c979f3b61602471e7f0fbd10e6e6
ipin4 <pin4vital@gmail.com>
2017-12-13 11:35:39 +0300

    Read-more linting and refactoring changes

--- src/ggrc/assets/javascripts/components/read-more/read-more.js
+++ src/ggrc/assets/javascripts/components/read-more/read-more.js
@@ -5,77 +5,71 @@
 
 import template from './read-more.mustache';
 
-(function (can, GGRC) {
-  'use strict';
-
-  var tag = 'read-more';
-  var readMore = 'Read More';
-  var readLess = 'Read Less';
-  var classPrefix = 'ellipsis-truncation-';
-  /**
-   * Assessment specific read more view component
-   */
-  GGRC.Components('readMore', {
-    tag: tag,
-    template: template,
-    viewModel: {
-      define: {
-        text: {
-          type: 'string',
-          value: ''
-        },
-        maxLinesNumber: {
-          type: 'number',
-          value: 5
-        },
-        cssClass: {
-          type: 'string',
-          value: '',
-          get: function () {
-            return this.attr('expanded') ? '' :
-              classPrefix + this.attr('maxLinesNumber');
-          }
-        }
-      },
-      expanded: false,
-      overflowing: false,
-      lineHeight: null,
-      btnText: function () {
-        return this.attr('expanded') ? readLess : readMore;
-      },
-      toggle: function (ev) {
-        ev.stopPropagation();
-        this.attr('expanded', !this.attr('expanded'));
-      },
-      isOverflowing: function (element) {
-        var result;
-        var clientHeight = element.clientHeight;
-        var scrollHeight = element.scrollHeight;
-
-        if (!this.attr('expanded')) {
-          result = scrollHeight > clientHeight;
-        } else {
-          result = clientHeight >=
-            (this.attr('lineHeight') * this.attr('maxLinesNumber'));
-        }
-        this.attr('overflowing', result);
-      },
-      checkOverflowing: function (el) {
-        var $element = $(el).find('.read-more__body');
-        var element = $element[0];
-
-        this.attr('lineHeight',
-          parseInt($element.css('line-height'), 10));
-
-        if (element) {
-          this.isOverflowing(element);
-        }
-      }
+const readMore = 'Read More';
+const readLess = 'Read Less';
+const classPrefix = 'ellipsis-truncation-';
+const viewModel = {
+  define: {
+    text: {
+      type: 'string',
+      value: '',
     },
-    events: {
-      '{element} mouseover': function () {
-        this.viewModel.checkOverflowing(this.element);
-      }
+    maxLinesNumber: {
+      type: 'number',
+      value: 5,
+    },
+    cssClass: {
+      type: 'string',
+      value: '',
+      get() {
+        return this.attr('expanded') ? '' :
+          classPrefix + this.attr('maxLinesNumber');
+      },
+    },
+  },
+  expanded: false,
+  overflowing: false,
+  lineHeight: null,
+  btnText() {
+    return this.attr('expanded') ? readLess : readMore;
+  },
+  toggle(ev) {
+    ev.stopPropagation();
+    this.attr('expanded', !this.attr('expanded'));
+  },
+  checkOverflowing(el) {
+    const $element = $(el).find('.read-more__body');
+    const element = $element[0];
+
+    this.attr('lineHeight',
+      parseInt($element.css('line-height'), 10));
+
+    if (element) {
+      this.isOverflowing(element);
     }
-  });
-})(window.can, window.GGRC);
+  },
+  isOverflowing(element) {
+    let result;
+    const clientHeight = element.clientHeight;
+    const scrollHeight = element.scrollHeight;
+
+    if (!this.attr('expanded')) {
+      result = scrollHeight > clientHeight;
+    } else {
+      result = clientHeight >=
+        (this.attr('lineHeight') * this.attr('maxLinesNumber'));
+    }
+    this.attr('overflowing', result);
+  },
+};
+
+export default can.Component.extend({
+  tag: 'read-more',
+  template,
+  viewModel,
+  events: {
+    '{element} mouseover'() {
+      this.viewModel.checkOverflowing(this.element);
+    },
+  },
+});
--- src/ggrc/assets/javascripts/components/read-more/tests/read-more_spec.js
+++ src/ggrc/assets/javascripts/components/read-more/tests/read-more_spec.js
@@ -3,30 +3,30 @@
   Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
-describe('GGRC.Component.ReadMore', function () {
-  'use strict';
-  var vm;
+import Component from '../read-more';
 
-  beforeEach(function () {
-    vm = GGRC.Components.getViewModel('readMore');
+describe('GGRC.Component.ReadMore', () => {
+  let vm;
+  beforeEach(() => {
+    vm = new (can.Map.extend(Component.prototype.viewModel));
   });
 
-  describe('toggle() method', function () {
-    var eventMock;
+  describe('toggle() method', () => {
+    let eventMock;
 
-    beforeEach(function () {
+    beforeEach(() => {
       eventMock = {
-        stopPropagation: jasmine.createSpy()
+        stopPropagation: jasmine.createSpy(),
       };
     });
 
-    it('calls stopPropagation()', function () {
+    it('calls stopPropagation()', () => {
       vm.toggle(eventMock);
 
       expect(eventMock.stopPropagation).toHaveBeenCalled();
     });
 
-    it('switchs expanded attribute', function () {
+    it('switchs expanded attribute', () => {
       vm.attr('expanded', true);
       vm.toggle(eventMock);
 
@@ -36,65 +36,64 @@ describe('GGRC.Component.ReadMore', function () {
       expect(vm.attr('expanded')).toBe(true);
     });
   });
-  describe('set() of cssClass attribute', function () {
-    it('returns empty string if viewModel.expanded is true', function () {
+  describe('set() of cssClass attribute', () => {
+    it('returns empty string if viewModel.expanded is true', () => {
       vm.attr('expanded', true);
       expect(vm.attr('cssClass')).toEqual('');
     });
     it('returns specific css string ' +
-    'if viewModel.expanded is false', function () {
-      var i = 1;
-      for (; i <= 10; i++) {
+    'if viewModel.expanded is false', () => {
+      for (let i; i <= 10; i++) {
         vm.attr('maxLinesNumber', i);
         expect(vm.attr('cssClass')).toEqual('ellipsis-truncation-' + i);
       }
     });
   });
-  describe('isOverflowing(element) method', function () {
-    var element;
+  describe('isOverflowing(element) method', () => {
+    let element;
 
-    describe('if component is not expanded', function () {
+    describe('if component is not expanded', () => {
       it('sets true to overflowing' +
-      ' if scrollHeight of element greater then clientHeight', function () {
+      ' if scrollHeight of element greater then clientHeight', () => {
         vm.attr('expanded', false);
         element = {
           scrollHeight: 100,
-          clientHeight: 50
+          clientHeight: 50,
         };
         vm.isOverflowing(element);
         expect(vm.attr('overflowing')).toBe(true);
       });
       it('sets false to overflowing if scrollHeight' +
-      ' of element less or equal then clientHeight', function () {
+      ' of element less or equal then clientHeight', () => {
         vm.attr('expanded', false);
         element = {
           scrollHeight: 100,
-          clientHeight: 101
+          clientHeight: 101,
         };
         vm.isOverflowing(element);
         expect(vm.attr('overflowing')).toBe(false);
       });
     });
 
-    describe('if component is expanded', function () {
+    describe('if component is expanded', () => {
       it('sets true to overflowing if clientHeight of element' +
-      ' greater or equal then minimal allowed height', function () {
+      ' greater or equal then minimal allowed height', () => {
         vm.attr('expanded', true);
         vm.attr('lineHeight', 20);
         vm.attr('maxLinesNumber', 5);
         element = {
-          clientHeight: 100
+          clientHeight: 100,
         };
         vm.isOverflowing(element);
         expect(vm.attr('overflowing')).toBe(true);
       });
       it('sets false to overflowing if clientHeight of element' +
-      ' less then minimal allowed height', function () {
+      ' less then minimal allowed height', () => {
         vm.attr('expanded', true);
         vm.attr('lineHeight', 20);
         vm.attr('maxLinesNumber', 5);
         element = {
-          clientHeight: 80
+          clientHeight: 80,
         };
         vm.isOverflowing(element);
         expect(vm.attr('overflowing')).toBe(false);
