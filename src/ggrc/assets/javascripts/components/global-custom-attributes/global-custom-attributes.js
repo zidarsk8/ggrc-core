8c42757360fe5ef0b2bd9879b87213b495de60ad
YuriChupyrkin <yu4.1990@gmail.com>
2017-12-15 16:16:23 +0300

    Set readonly GCA on Control and Risk info pane

--- src/ggrc/assets/javascripts/components/global-custom-attributes/global-custom-attributes.js
+++ src/ggrc/assets/javascripts/components/global-custom-attributes/global-custom-attributes.js
@@ -30,6 +30,7 @@ import Permission from '../../permission';
             return this.attr('instance.snapshot') ||
               this.attr('instance.isRevision') ||
               this.attr('instance.archived') ||
+              this.isReadOnlyForInstance(this.attr('instance')) ||
               !Permission.is_allowed_for('update', this.attr('instance'));
           },
         },
@@ -40,6 +41,13 @@ import Permission from '../../permission';
        * @type {can.List}
        */
       items: [],
+      isReadOnlyForInstance(instance) {
+        if (!instance) {
+          return false;
+        }
+
+        return instance.class.isProposable;
+      },
       initCustomAttributes: function () {
         var result = [];
 
