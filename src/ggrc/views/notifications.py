ac2b15ee6b80805964c7cb3ef44018ce8dd1d7c1
pod2metra <pod2metra@gmail.com>
2017-12-19 11:12:53 +0300

    Add show proposal email

--- src/ggrc/views/notifications.py
+++ src/ggrc/views/notifications.py
@@ -4,6 +4,8 @@
 """Views for email notifications."""
 
 from ggrc.notifications import common
+from ggrc.utils import proposal
+
 from ggrc.login import login_required
 
 
@@ -24,3 +26,8 @@ def init_notification_views(app):
   app.add_url_rule(
       "/_notifications/show_daily_digest", "show_daily_digest_notifications",
       view_func=login_required(common.show_daily_digest_notifications))
+
+  app.add_url_rule(
+      "/_notifications/show_proposal_digest",
+      "show_proposal_digest",
+      view_func=login_required(proposal.present_notifications))
