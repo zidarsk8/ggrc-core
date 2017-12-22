2af06b0a206c4b79adadcc1bc7cb66b9b6fda010
IlyaTsupryk <ilya.tsupryk@gmail.com>
2017-12-22 19:11:50 +0300

    Skip status validation on import

--- src/ggrc/models/mixins/validate_on_complete.py
+++ src/ggrc/models/mixins/validate_on_complete.py
@@ -17,19 +17,32 @@ class ValidateOnComplete(object):
 
   Requires Stateful and Statusable to be mixed in as well."""
 
-  # pylint: disable=too-few-public-methods
+  skip_validation = False
 
+  # pylint: disable=too-few-public-methods
   @validates("status")
   def validate_status(self, key, value):
     """Check that no CA-introduced completion preconditions are missing."""
     # support for multiple validators for status
     if hasattr(super(ValidateOnComplete, self), "validate_status"):
       value = super(ValidateOnComplete, self).validate_status(key, value)
+    self.validate_done_state(self.status, value)
+    return value
 
-    if self.status in self.NOT_DONE_STATES and value in self.DONE_STATES:
+  def validate_done_state(self, old_value, new_value):
+    """Check if it's allowed to set done state from not done.
+
+    Args:
+        old_value: Old state (Assessment.VALID_STATES).
+        new_value: New state (Assessment.VALID_STATES).
+
+    Raise:
+        ValidationError if state can't be changed.
+    """
+    if not self.skip_validation and \
+       old_value in self.NOT_DONE_STATES and \
+       new_value in self.DONE_STATES:
       if hasattr(self, "preconditions_failed") and self.preconditions_failed:
         raise ValidationError("CA-introduced completion preconditions "
                               "are not satisfied. Check preconditions_failed "
                               "of items of self.custom_attribute_values")
-
-    return value
