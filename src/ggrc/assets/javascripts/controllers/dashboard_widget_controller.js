7c2126513195f9a20a2060934391efaf095ff767
Alexander Novik <al.novik88@gmail.com>
2017-12-26 13:16:34 +0300

    Migrate DashboardWidget controller on module syntax

--- src/ggrc/assets/javascripts/controllers/dashboard_widget_controller.js
+++ src/ggrc/assets/javascripts/controllers/dashboard_widget_controller.js
@@ -5,45 +5,43 @@
 
 import {getDashboards} from '../plugins/utils/dashboards-utils';
 
-(function (can, $) {
-  can.Control('GGRC.Controllers.DashboardWidget', {
-    defaults: {
-      model: null,
-      instance: null,
-      isLoading: true
-    },
-    init: function () {
-      var that = this;
-      $(function () {
-        if (GGRC.page_object) {
-          $.extend(that.defaults, {
-            model: GGRC.infer_object_type(GGRC.page_object),
-            instance: GGRC.page_instance()
-          });
-        }
-      });
-    }
-  }, {
-    init: function () {
-      var frag;
-      var dashboards =
-        getDashboards(this.options.instance);
+export default can.Control('GGRC.Controllers.DashboardWidget', {
+  defaults: {
+    model: null,
+    instance: null,
+    isLoading: true
+  },
+  init: function () {
+    var that = this;
+    $(function () {
+      if (GGRC.page_object) {
+        $.extend(that.defaults, {
+          model: GGRC.infer_object_type(GGRC.page_object),
+          instance: GGRC.page_instance()
+        });
+      }
+    });
+  }
+}, {
+  init: function () {
+    var frag;
+    var dashboards =
+      getDashboards(this.options.instance);
 
-      this.options.context = new can.Map({
-        model: this.options.model,
-        instance: this.options.instance,
-        dashboards: dashboards,
-        activeDashboard: dashboards[0],
-        showDashboardList: dashboards.length > 1,
-        selectDashboard: function (dashboard) {
-          this.attr('activeDashboard', dashboard);
-        }
-      });
+    this.options.context = new can.Map({
+      model: this.options.model,
+      instance: this.options.instance,
+      dashboards: dashboards,
+      activeDashboard: dashboards[0],
+      showDashboardList: dashboards.length > 1,
+      selectDashboard: function (dashboard) {
+        this.attr('activeDashboard', dashboard);
+      }
+    });
 
-      frag = can.view(this.options.widget_view,
-                      this.options.context);
-      this.element.html(frag);
-      return 0;
-    }
-  });
-})(window.can, window.can.$);
+    frag = can.view(this.options.widget_view,
+                    this.options.context);
+    this.element.html(frag);
+    return 0;
+  }
+});
