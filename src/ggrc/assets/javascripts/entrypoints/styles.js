bf8a5f70a726a8120bb3aa4f35f51b357768ecbf
Alexander Novik <al.novik88@gmail.com>
2018-01-05 13:02:19 +0300

    Move images into ggrc-client directory

--- src/ggrc/assets/javascripts/entrypoints/styles.js
+++ src/ggrc/assets/javascripts/entrypoints/styles.js
@@ -3,5 +3,5 @@
     Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
-import '../../images/favicon.ico';
+import '../../../../ggrc-client/images/favicon.ico';
 import '../../stylesheets/dashboard.scss';
