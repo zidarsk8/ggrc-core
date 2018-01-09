2260151eac8802a0337e01c305fedb01b587fa27
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2018-01-09 16:52:47 +0300

    Fix failed tests cause Workflow ACL import was implemented

--- test/integration/ggrc_workflows/converters/test_import_workflow_people.py
+++ test/integration/ggrc_workflows/converters/test_import_workflow_people.py
@@ -13,22 +13,6 @@ from integration.ggrc import TestCase
 from integration.ggrc.models import factories as ggrc_factories
 
 
-def _get_people_emails_by_role(workflow, role_name):
-  """Get people emails by user_role.
-
-  Args:
-      workflow: Workflow instance
-      role_name: Workflow user role name
-  Returns:
-      People emails list.
-  """
-  people_emails = []
-  for wp in workflow.workflow_people:
-    if wp.person.user_roles[0].role.name == role_name:
-      people_emails.append(wp.person.email)
-  return people_emails
-
-
 @ddt.ddt
 class TestWorkflowPeopleImport(TestCase):
   """Tests for workflow people imports."""
@@ -50,38 +34,38 @@ class TestWorkflowPeopleImport(TestCase):
     """Import people list to workflow and compare with expected results.
 
     Args:
-        owners_idx: Indexes of the test people data who should get Owner role.
-        members_idx: Indexes of the test people data who should get Member
-            role.
-        expected_owners_idx: Expected indexes for the people with Owner role.
-        expected_members_idx: Expected indexes for the people with Member role.
+        import_data: Dictionary contains 2 lists.
+            'admins': Test people data indexes who should get Admin role.
+            'members': Test people data indexes who should get Member role.
+        expected_data: Dictionary contains 2 lists.
+            'admins': Test people data indexes who should get Admin role.
+            'members': Test people data indexes who should get Member role.
         is_success: Shows is import was successful or not.
         success_resp_action: Action which was performed on imported item.
     """
     if import_data['members']:
       import_members = '\n'.join(
-          (self.user_emails[idx] for idx in import_data['members']))
-      self.wf_import_params['member'] = import_members
-    if import_data['owners']:
+          self.user_emails[idx] for idx in import_data['members'])
+      self.wf_import_params['workflow member'] = import_members
+    if import_data['admins']:
       import_owners = '\n'.join(
-          (self.user_emails[idx] for idx in import_data['owners']))
-      self.wf_import_params['manager'] = import_owners
+          self.user_emails[idx] for idx in import_data['admins'])
+      self.wf_import_params['admin'] = import_owners
     resp = self.import_data(self.wf_import_params)
     if is_success:
       self.assertEqual(resp[0][success_resp_action], 1)
       self._check_csv_response(resp, {})
-      workflow = Workflow.query.filter(Workflow.slug == self.wf_slug).first()
+      workflow = Workflow.eager_query().filter(
+          Workflow.slug == self.wf_slug).first()
 
-      # Check that every WorkflowPerson has only one role in its scope
-      for wp in workflow.workflow_people:
-        self.assertEqual(len(wp.person.user_roles), 1)
+      exst_admins = [acl.person.email for acl in workflow.access_control_list
+                     if acl.ac_role.name == 'Admin']
+      expected_admins = [self.user_emails[idx]
+                         for idx in expected_data['admins']]
+      self.assertItemsEqual(exst_admins, expected_admins)
 
-      exst_owners = _get_people_emails_by_role(workflow, 'WorkflowOwner')
-      expected_owners = [self.user_emails[idx]
-                         for idx in expected_data['owners']]
-      self.assertItemsEqual(exst_owners, expected_owners)
-
-      exst_members = _get_people_emails_by_role(workflow, 'WorkflowMember')
+      exst_members = [acl.person.email for acl in workflow.access_control_list
+                      if acl.ac_role.name == 'Workflow Member']
       expected_members = [self.user_emails[idx]
                           for idx in expected_data['members']]
       self.assertItemsEqual(exst_members, expected_members)
@@ -91,69 +75,61 @@ class TestWorkflowPeopleImport(TestCase):
   @ddt.data(
       {
           'import_data': {
-              'owners': [0, 1],
+              'admins': [0, 1],
               'members': [2, 3]
           },
           'expected_data': {
-              'owners': [0, 1],
+              'admins': [0, 1],
               'members': [2, 3]
           }
       },
       {
           'import_data': {
-              'owners': [0, 1],
+              'admins': [0, 1],
               'members': [1, 2]
           },
           'expected_data': {
-              'owners': [0, 1],
-              'members': [2]
+              'admins': [0, 1],
+              'members': [1, 2]
           }
       },
       {
           'import_data': {
-              'owners': [0, 1],
+              'admins': [0, 1],
               'members': [0, 1]
           },
           'expected_data': {
-              'owners': [0, 1],
-              'members': []
-          }
-      },
-      {
-          'import_data': {
-              'owners': [0, 1],
-              'members': []
-          },
-          'expected_data': {
-              'owners': [0, 1],
-              'members': []
-          }
-      },
-      {
-          'import_data': {
-              'owners': [],
+              'admins': [0, 1],
               'members': [0, 1]
+          }
+      },
+      {
+          'import_data': {
+              'admins': [0, 1],
+              'members': []
           },
           'expected_data': {
-              'owners': [],
+              'admins': [0, 1],
               'members': []
+          }
+      },
+      {
+          'import_data': {
+              'admins': [],
+              'members': [0, 1]
           },
           'is_success': False
       },
       {
           'import_data': {
-              'owners': [],
-              'members': []
-          },
-          'expected_data': {
-              'owners': [],
+              'admins': [],
               'members': []
           },
           'is_success': False
       }
   )
   @ddt.unpack
-  def test_create_workflow_with_people(self, import_data, expected_data,
+  def test_create_workflow_with_people(self, import_data, expected_data=None,
                                        is_success=True):
     """Tests importing new workflow with data: {import_data}."""
     self._check_workflow_people(import_data, expected_data, is_success,
@@ -162,81 +138,81 @@ class TestWorkflowPeopleImport(TestCase):
   @ddt.data(
       {
           'import_data': {
-              'owners': [0, 1],
+              'admins': [0, 1],
               'members': [2, 3]
           },
           'expected_data': {
-              'owners': [0, 1],
+              'admins': [0, 1],
               'members': [2, 3]
           }
       },
       {
           'import_data': {
-              'owners': [0, 1],
+              'admins': [0, 1],
               'members': [1, 2]
           },
           'expected_data': {
-              'owners': [0, 1],
-              'members': [2]
+              'admins': [0, 1],
+              'members': [1, 2]
           }
       },
       {
           'import_data': {
-              'owners': [0, 1],
+              'admins': [0, 1],
               'members': [0, 1]
           },
           'expected_data': {
-              'owners': [0, 1],
-              'members': [4, 5]
-          }
-      },
-      {
-          'import_data': {
-              'owners': [0, 1],
-              'members': []
-          },
-          'expected_data': {
-              'owners': [0, 1],
-              'members': [4, 5]
-          }
-      },
-      {
-          'import_data': {
-              'owners': [4, 5],
-              'members': []
-          },
-          'expected_data': {
-              'owners': [4, 5],
-              'members': []
-          }
-      },
-      {
-          'import_data': {
-              'owners': [],
-              'members': [0, 1]
-          },
-          'expected_data': {
-              'owners': [6, 7],
+              'admins': [0, 1],
               'members': [0, 1]
           }
       },
       {
           'import_data': {
-              'owners': [],
+              'admins': [0, 1],
               'members': []
           },
           'expected_data': {
-              'owners': [6, 7],
-              'members': [4, 5]
-          }
-      },
-      {
-          'import_data': {
-              'owners': [],
+              'admins': [0, 1],
               'members': [6, 7]
+          }
+      },
+      {
+          'import_data': {
+              'admins': [6, 7],
+              'members': []
           },
           'expected_data': {
-              'owners': [6, 7],
+              'admins': [6, 7],
+              'members': [6, 7]
+          }
+      },
+      {
+          'import_data': {
+              'admins': [],
+              'members': [0, 1]
+          },
+          'expected_data': {
+              'admins': [4, 5],
+              'members': [0, 1]
+          }
+      },
+      {
+          'import_data': {
+              'admins': [],
+              'members': []
+          },
+          'expected_data': {
+              'admins': [4, 5],
+              'members': [6, 7]
+          }
+      },
+      {
+          'import_data': {
+              'admins': [],
+              'members': [4, 5]
+          },
+          'expected_data': {
+              'admins': [4, 5],
               'members': [4, 5]
           }
       }
@@ -244,9 +220,9 @@ class TestWorkflowPeopleImport(TestCase):
   @ddt.unpack
   def test_update_workflow_with_people(self, import_data, expected_data):
     """Tests importing existing workflow with data: {import_data}."""
-    self.wf_import_params['member'] = '\n'.join((self.user_emails[4],
-                                                 self.user_emails[5]))
-    self.wf_import_params['manager'] = '\n'.join((self.user_emails[6],
-                                                  self.user_emails[7]))
+    self.wf_import_params['admin'] = '\n'.join((self.user_emails[4],
+                                                self.user_emails[5]))
+    self.wf_import_params['workflow member'] = '\n'.join((self.user_emails[6],
+                                                          self.user_emails[7]))
     self.import_data(self.wf_import_params)
     self._check_workflow_people(import_data, expected_data, True, 'updated')
