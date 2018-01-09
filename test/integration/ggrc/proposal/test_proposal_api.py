66d8643a785e27fadfdf0bc00f7a1166cf0d9b12
pod2metra <pod2metra@gmail.com>
2018-01-09 13:02:12 +0300

    Add test proposal perms over proposed instance

--- test/integration/ggrc/proposal/test_proposal_api.py
+++ test/integration/ggrc/proposal/test_proposal_api.py
@@ -296,7 +296,6 @@ class TestProposalApi(TestCase):
                                 {"id": person_3.id, "email": person_3.email}],
                       "deleted": [],
                   },
-
               }
           },
           agenda="agenda content")
@@ -885,3 +884,45 @@ class TestProposalApi(TestCase):
     self.assertEqual(1, len(control.comments))
     comment = tmpl.format(user=test_email, text=comment_agenda)
     self.assertEqual(comment, control.comments[0].description)
+
+  @ddt.data(
+      {"read": 1, "update": 0},
+      {"read": 0, "update": 1},
+      {"read": 1, "update": 1},
+      {"read": 0, "update": 0},
+  )
+  @ddt.unpack
+  def test_proposal_acl_poulation(self, read, update):
+    """Test proposal perms with read {read} and update {update}."""
+    with factories.single_commit():
+      control = factories.ControlFactory(title="1")
+      role = factories.AccessControlRoleFactory(object_type=control.type,
+                                                name="role",
+                                                read=read,
+                                                update=update)
+      proposal = factories.ProposalFactory(instance=control,
+                                           content={"field": "a"},
+                                           agenda="agenda content")
+      person = factories.PersonFactory()
+    self.assertFalse(proposal.access_control_list)
+    role_id = role.id
+    control_id = control.id
+    proposal_id = proposal.id
+    control_content = control.log_json()
+    control_content["access_control_list"] = [
+        {"ac_role_id": role_id,
+            "person": {"type": "Person", "id": person.id}}
+    ]
+    resp = self.api.put(control, {"control": control_content})
+    self.assertEqual(200, resp.status_code)
+    control = all_models.Control.query.get(control_id)
+    proposal = all_models.Proposal.query.get(proposal_id)
+    self.assertEqual(int(read or update), len(proposal.access_control_list))
+    expected_roles = []
+    if update:
+      expected_roles.append(all_models.Proposal.ACRoles.EDITOR)
+    elif read:
+      expected_roles.append(all_models.Proposal.ACRoles.READER)
+    self.assertEqual(
+        sorted(expected_roles),
+        sorted([a.ac_role.name for a in proposal.access_control_list]))
