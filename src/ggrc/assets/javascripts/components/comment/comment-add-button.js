396cd0e3bd50b09412dcd07f219e4d8969b80a53
Natalia Zarubina <nataly.zarubina@gmail.com>
2018-01-15 15:33:56 +0300

    Remove unneeded IIFE

--- src/ggrc/assets/javascripts/components/comment/comment-add-button.js
+++ src/ggrc/assets/javascripts/components/comment/comment-add-button.js
@@ -3,58 +3,54 @@
  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
  */
 
-(function (can, GGRC) {
-  'use strict';
+const tag = 'comment-add-button';
+const template = `<button class="btn btn-small btn-gray"
+      ($click)="createComment()">
+      <content></content>
+    </button>`;
 
-  var tag = 'comment-add-button';
-  var template = '<button class="btn btn-small btn-gray"' +
-    ' ($click)="createComment()">' +
-    '<content></content>' +
-    '</button>';
-
-  GGRC.Components('commentAddButton', {
-    tag: tag,
-    template: template,
-    viewModel: {
-      define: {
-        disabled: {
-          get: function () {
-            return this.attr('isSaving') ||
-              !this.attr('value').length ||
-              this.attr('isDisabled');
-          }
+export default can.Component.extend({
+  tag: tag,
+  template: template,
+  viewModel: {
+    define: {
+      disabled: {
+        get: function () {
+          return this.attr('isSaving') ||
+            !this.attr('value').length ||
+            this.attr('isDisabled');
         },
-        value: {
-          type: 'string',
-          value: '',
-          set: function (newValue) {
-            return newValue || '';
-          }
-        }
       },
-      isDisabled: false,
-      isSaving: false,
-      createComment: function () {
-        var comment;
-        var description = this.attr('value');
+      value: {
+        type: 'string',
+        value: '',
+        set: function (newValue) {
+          return newValue || '';
+        },
+      },
+    },
+    isDisabled: false,
+    isSaving: false,
+    createComment: function () {
+      var comment;
+      var description = this.attr('value');
 
-        if (this.attr('disabled')) {
-          return;
-        }
-
-        comment = new CMS.Models.Comment({
-          description: description,
-          created_at: new Date(),
-          modified_by: {type: 'Person', id: GGRC.current_user.id}
-        });
-        // Erase RichText Field after Comment Creation
-        this.attr('value', '');
-
-        this.dispatch({
-          type: 'commentCreated',
-          comment: comment
-        });
+      if (this.attr('disabled')) {
+        return;
       }
-    }
-  });
-})(window.can, window.GGRC);
+
+      comment = new CMS.Models.Comment({
+        description: description,
+        created_at: new Date(),
+        modified_by: {type: 'Person', id: GGRC.current_user.id},
+      });
+      // Erase RichText Field after Comment Creation
+      this.attr('value', '');
+
+      this.dispatch({
+        type: 'commentCreated',
+        comment: comment,
+      });
+    },
+  },
+});
