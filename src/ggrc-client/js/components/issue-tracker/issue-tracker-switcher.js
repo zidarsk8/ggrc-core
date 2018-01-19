d1ddb7c40d26c8f427c8aebd6f76480152c11bd6
Alex Ladyga <neocoder@gmail.com>
2018-01-19 16:53:23 +0300

    Refactor issueTrackerIntegratable mixin to fix logic issues

--- src/ggrc-client/js/components/issue-tracker/issue-tracker-switcher.js
+++ src/ggrc-client/js/components/issue-tracker/issue-tracker-switcher.js
@@ -13,18 +13,18 @@ export default GGRC.Components('issueTrackerSwitcher', {
       isIntegrationEnabled: {
         set: function (newValue, setValue) {
           // convert to bool type. dropdown returns "true" or "false" as string
-          let boolValue = this.convertToBool(newValue);
-
-          if (boolValue) {
-            this.setDefaults();
+          const enabled = this.convertToBool(newValue);
+          if ( enabled ) {
+            this.attr('instance').initIssueTrackerForAssessment();
           }
-
-          setValue(boolValue);
+          setValue(enabled);
         },
       },
       defaultTitle: {
         set: function (newValue, setValue) {
-          if (newValue && this.attr('canUpdateDefaultTitle')) {
+          if (newValue &&
+              this.attr('setIssueTitle') &&
+              this.attr('instance').isNew()) {
             this.setDefaultIssueTitle(newValue);
           }
 
@@ -39,7 +39,6 @@ export default GGRC.Components('issueTrackerSwitcher', {
     },
     instance: {},
     parent: null,
-    canUpdateDefaultTitle: false,
     setIssueTitle: false,
     convertToBool: function (value) {
       if (typeof value === 'boolean') {
@@ -54,46 +53,17 @@ export default GGRC.Components('issueTrackerSwitcher', {
       args.type = 'issueTrackerSwitcherChanged';
 
       if (dropdownValue) {
-        this.setDefaults();
+        this.attr('instance').initIssueTrackerForAssessment();
       }
 
       this.dispatch(args);
     },
-    setDefaults: function () {
-      if (!this.attr('isParent') && this.attr('instance').wasEnabled()) {
-        this.setDeaultsFromParent('issue_priority');
-        this.setDeaultsFromParent('issue_severity');
-        this.setDeaultsFromParent('component_id');
-        this.setDeaultsFromParent('hotlist_id');
-        this.setDeaultsFromParent('issue_type');
-
-        if (this.attr('setIssueTitle')) {
-          // can override issue title in case when user creates new instance
-          // instance turns on issue tracker integration first time
-          this.attr('canUpdateDefaultTitle', true);
-          this.setDefaultIssueTitle();
-        }
-      }
-    },
-    setDeaultsFromParent: function (propName) {
-      let issueTracker = this.attr('instance.issue_tracker');
-      let parentIssueTracker = this.attr('parent.issue_tracker');
-
-      issueTracker.attr(propName,
-        issueTracker.attr(propName) || parentIssueTracker.attr(propName));
-    },
     setDefaultIssueTitle: function (value) {
-      let title = this.attr('instance.title');
       let issueTracker = this.attr('instance.issue_tracker');
-
-      if (value) {
-        issueTracker.attr('title', value);
-        return;
-      }
 
       // set from instance title
-      if (title && !issueTracker.attr('title')) {
-        issueTracker.attr('title', title);
+      if (issueTracker) {
+        issueTracker.attr('title', value);
       }
     },
   },
