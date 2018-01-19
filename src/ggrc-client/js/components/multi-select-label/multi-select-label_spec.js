2351167e4073973cff066e2bfd85762fc28a8f1f
Klim Shuplenkou <kshuplenkou@google.com>
2018-01-19 12:37:02 +0300

    Remake spec_helpers as module

--- src/ggrc-client/js/components/multi-select-label/multi-select-label_spec.js
+++ src/ggrc-client/js/components/multi-select-label/multi-select-label_spec.js
@@ -3,6 +3,7 @@
     Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
+import {getComponentVM} from '../../../js_specs/spec_helpers';
 import Component from './multi-select-label';
 
 describe('multi-select-label component', () => {
--- src/ggrc-client/js/components/unified-mapper/tests/mapper-results-columns-configuration_spec.js
+++ src/ggrc-client/js/components/unified-mapper/tests/mapper-results-columns-configuration_spec.js
@@ -3,6 +3,7 @@
   Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
+import {getComponentVM} from '../../../../js_specs/spec_helpers';
 import * as TreeViewUtils from '../../../plugins/utils/tree-view-utils';
 import Component from '../mapper-results-columns-configuration';
 
