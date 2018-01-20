d618c7f656cfa036f9cfa2878fd5ecfd137290ff
Miha Zidar <zidarsk8@gmail.com>
2018-01-20 19:20:56 +0100

    Move functions out of get_myobjects_query
    This is just a small refactor to clean up my objects query, to match
    pylint requirements of max 15 local variables.

--- src/ggrc/query/my_objects.py
+++ src/ggrc/query/my_objects.py
@@ -25,6 +25,114 @@ def _types_to_type_models(types):
   return [m for m in all_models.all_models if m.__name__ in types]
 
 
+def _get_people():
+  """Get all the people w/o any restrictions."""
+  all_people = db.session.query(
+      all_models.Person.id.label('id'),
+      literal(all_models.Person.__name__).label('type'),
+      literal(None).label('context_id')
+  )
+  return all_people
+
+
+def _get_object_people(contact_id, model_names):
+  """Objects to which the user is 'mapped'."""
+  object_people_query = db.session.query(
+      ObjectPerson.personable_id.label('id'),
+      ObjectPerson.personable_type.label('type'),
+      literal(None).label('context_id')
+  ).filter(
+      and_(
+          ObjectPerson.person_id == contact_id,
+          ObjectPerson.personable_type.in_(model_names)
+      )
+  )
+  return object_people_query
+
+
+def _get_object_mapped_ca(contact_id, model_names):
+  """Objects to which the user is mapped via a custom attribute."""
+  ca_mapped_objects_query = db.session.query(
+      CustomAttributeValue.attributable_id.label('id'),
+      CustomAttributeValue.attributable_type.label('type'),
+      literal(None).label('context_id')
+  ).filter(
+      and_(
+          CustomAttributeValue.attribute_value == "Person",
+          CustomAttributeValue.attribute_object_id == contact_id,
+          CustomAttributeValue.attributable_type.in_(model_names)
+      )
+  )
+  return ca_mapped_objects_query
+
+
+def _get_objects_user_assigned(contact_id, model_names):
+  """Objects for which the user is assigned."""
+  dst_assignee_query = db.session.query(
+      Relationship.destination_id.label('id'),
+      Relationship.destination_type.label('type'),
+      literal(None).label('context_id'),
+  ).filter(
+      and_(
+          Relationship.source_type == "Person",
+          Relationship.source_id == contact_id,
+          Relationship.destination_type.in_(model_names)
+      ),
+  )
+  src_assignee_query = db.session.query(
+      Relationship.source_id.label('id'),
+      Relationship.source_type.label('type'),
+      literal(None).label('context_id'),
+  ).filter(
+      and_(
+          Relationship.destination_type == "Person",
+          Relationship.destination_id == contact_id,
+          Relationship.source_type.in_(model_names)
+      ),
+  )
+  return dst_assignee_query.union(src_assignee_query)
+
+
+def _get_results_by_context(contact_id, model):
+  """Objects based on the context of the current model.
+
+  Return the objects that are in private contexts via UserRole.
+  """
+  context_query = db.session.query(
+      model.id.label('id'),
+      literal(model.__name__).label('type'),
+      literal(None).label('context_id'),
+  ).join(
+      UserRole,
+      and_(
+          UserRole.context_id == model.context_id,
+          UserRole.person_id == contact_id,
+      )
+  )
+  return context_query
+
+
+def _get_custom_roles(contact_id, model_names):
+  """Objects for which the user is an 'owner'."""
+  custom_roles_query = db.session.query(
+      all_models.AccessControlList.object_id.label('id'),
+      all_models.AccessControlList.object_type.label('type'),
+      literal(None).label('context_id')
+  ).join(
+      all_models.AccessControlRole,
+      all_models.AccessControlList.ac_role_id ==
+      all_models.AccessControlRole.id
+  ).filter(
+      and_(
+          all_models.AccessControlList.person_id == contact_id,
+          all_models.AccessControlList.object_type.in_(model_names),
+          all_models.AccessControlRole.my_work == true(),
+          all_models.AccessControlRole.read == true()
+      )
+  )
+  return custom_roles_query
+
+
 def get_myobjects_query(types=None, contact_id=None, is_creator=False):  # noqa
   """Filters by "myview" for a given person.
 
@@ -37,88 +145,6 @@ def get_myobjects_query(types=None, contact_id=None, is_creator=False):  # noqa
   type_models = _types_to_type_models(types)
   model_names = [model.__name__ for model in type_models]
   type_union_queries = []
-
-  def _get_people():
-    """Get all the people w/o any restrictions."""
-    all_people = db.session.query(
-        all_models.Person.id.label('id'),
-        literal(all_models.Person.__name__).label('type'),
-        literal(None).label('context_id')
-    )
-    return all_people
-
-  def _get_object_people(contact_id, model_names):
-    """Objects to which the user is 'mapped'."""
-    object_people_query = db.session.query(
-        ObjectPerson.personable_id.label('id'),
-        ObjectPerson.personable_type.label('type'),
-        literal(None).label('context_id')
-    ).filter(
-        and_(
-            ObjectPerson.person_id == contact_id,
-            ObjectPerson.personable_type.in_(model_names)
-        )
-    )
-    return object_people_query
-
-  def _get_object_mapped_ca(contact_id, model_names):
-    """Objects to which the user is mapped via a custom attribute."""
-    ca_mapped_objects_query = db.session.query(
-        CustomAttributeValue.attributable_id.label('id'),
-        CustomAttributeValue.attributable_type.label('type'),
-        literal(None).label('context_id')
-    ).filter(
-        and_(
-            CustomAttributeValue.attribute_value == "Person",
-            CustomAttributeValue.attribute_object_id == contact_id,
-            CustomAttributeValue.attributable_type.in_(model_names)
-        )
-    )
-    return ca_mapped_objects_query
-
-  def _get_objects_user_assigned(contact_id, model_names):
-    """Objects for which the user is assigned."""
-    dst_assignee_query = db.session.query(
-        Relationship.destination_id.label('id'),
-        Relationship.destination_type.label('type'),
-        literal(None).label('context_id'),
-    ).filter(
-        and_(
-            Relationship.source_type == "Person",
-            Relationship.source_id == contact_id,
-            Relationship.destination_type.in_(model_names)
-        ),
-    )
-    src_assignee_query = db.session.query(
-        Relationship.source_id.label('id'),
-        Relationship.source_type.label('type'),
-        literal(None).label('context_id'),
-    ).filter(
-        and_(
-            Relationship.destination_type == "Person",
-            Relationship.destination_id == contact_id,
-            Relationship.source_type.in_(model_names)
-        ),
-    )
-    return dst_assignee_query.union(src_assignee_query)
-
-  def _get_results_by_context(contact_id, model):
-    """Objects based on the context of the current model.
-
-    Return the objects that are in private contexts via UserRole.
-    """
-    context_query = db.session.query(
-        model.id.label('id'),
-        literal(model.__name__).label('type'),
-        literal(None).label('context_id'),
-    ).join(
-        UserRole,
-        and_(
-            UserRole.context_id == model.context_id,
-            UserRole.person_id == contact_id,
-        )
-    )
-    return context_query
 
   def _get_tasks_in_cycle(model):
     """Filter tasks with particular statuses and cycle.
@@ -180,26 +206,6 @@ def get_myobjects_query(types=None, contact_id=None, is_creator=False):  # noqa
     if model is all_models.CycleTaskGroupObjectTask:
       model_type_query = _get_tasks_in_cycle(model)
     return model_type_query
-
-  def _get_custom_roles(contact_id, model_names):
-    """Objects for which the user is an 'owner'."""
-    custom_roles_query = db.session.query(
-        all_models.AccessControlList.object_id.label('id'),
-        all_models.AccessControlList.object_type.label('type'),
-        literal(None).label('context_id')
-    ).join(
-        all_models.AccessControlRole,
-        all_models.AccessControlList.ac_role_id ==
-        all_models.AccessControlRole.id
-    ).filter(
-        and_(
-            all_models.AccessControlList.person_id == contact_id,
-            all_models.AccessControlList.object_type.in_(model_names),
-            all_models.AccessControlRole.my_work == true(),
-            all_models.AccessControlRole.read == true()
-        )
-    )
-    return custom_roles_query
 
   # Note: We don't return mapped objects for the Creator because being mapped
   # does not give the Creator necessary permissions to view the object.
