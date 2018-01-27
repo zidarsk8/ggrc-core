a56a3162b4f19e0b3f947a723d67f003efa257d0
Anže Pečar <apecar@google.com>
2018-01-27 02:39:14 +0100

    Fix WorkflowEditor role migration

--- src/ggrc_workflows/migrations/versions/20170626123211_55501ef0f634_add_workfloweditor_role.py
+++ src/ggrc_workflows/migrations/versions/20170626123211_55501ef0f634_add_workfloweditor_role.py
@@ -8,34 +8,46 @@ Create Date: 2017-06-26 12:32:11.421317
 """
 # disable Invalid constant name pylint warning for mandatory Alembic variables.
 # pylint: disable=invalid-name
-import sqlalchemy
-
-from ggrc import db
-from ggrc_basic_permissions import Role
+import sqlalchemy as sa
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '55501ef0f634'
 down_revision = '50788b66dcd4'
 
-_NOW = sqlalchemy.func.now()
+_NOW = sa.func.now()
+
+roles = sa.sql.table(
+    'roles',
+    sa.sql.column('name', sa.String),
+    sa.sql.column('permissions_json', sa.String),
+    sa.sql.column('description', sa.String),
+    sa.sql.column('created_at', sa.DateTime),
+    sa.sql.column('updated_at', sa.DateTime),
+    sa.sql.column('scope', sa.String),
+)
 
 
 def upgrade():
   """Upgrade database schema and/or data, creating a new revision."""
-  db.session.add(Role(
-      name="WorkflowEditor",
-      permissions_json="CODE DECLARED ROLE",
-      description="This role grants a user permission to edit workflow "
-                  "mappings and details",
-      created_at=_NOW,
-      updated_at=_NOW,
-      scope="Workflow Implied"
-  ))
-  db.session.commit()
+  op.execute(
+      roles.insert().
+      values({
+          "name": "WorkflowEditor",
+          "permissions_json": "CODE DECLARED ROLE",
+          "description": "This role grants a user permission to edit workflow "
+                         "mappings and details",
+          "created_at": _NOW,
+          "updated_at": _NOW,
+          "scope": "Workflow Implied"
+      })
+  )
 
 
 def downgrade():
   """Downgrade database schema and/or data back to the previous revision."""
-  wf_editor_role = Role.query.filter_by(name="WorkflowEditor").first()
-  db.session.delete(wf_editor_role)
-  db.session.commit()
+  op.execute(
+      roles.delete().where(
+          roles.c.name == "WorkflowEditor"
+      )
+  )
