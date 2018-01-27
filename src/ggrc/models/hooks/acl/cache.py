93f2358e9b3e314d7d6770c3851c9fc1d699fc50
Anže Pečar <apecar@google.com>
2018-01-27 21:13:02 +0100

    Make acl cache return the added acl instance
    This is very useful when the instance was already cached

--- src/ggrc/models/hooks/acl/cache.py
+++ src/ggrc/models/hooks/acl/cache.py
@@ -9,19 +9,20 @@ from ggrc.models import all_models
 class AccessControlListCache(object):  # pylint: disable=R0903
   """Access Control List helper"""
   def __init__(self):
-    self.cache = set()
+    self.cache = {}
 
   def add(self, obj, parent, person, role_id):
     """Add new item if it wasn't already added"""
     key = (obj.id, obj.type, person.id, role_id, parent.id)
     if key in self.cache:
-      return
-    self.cache.add(key)
+      return self.cache[key]
     acl = all_models.AccessControlList(
         object_id=obj.id,
         object_type=obj.type,
         parent=parent,
         person=person,
         ac_role_id=role_id)
+    self.cache[key] = acl
     if hasattr(obj, "access_control_list"):
       obj.access_control_list.append(acl)
+    return acl
