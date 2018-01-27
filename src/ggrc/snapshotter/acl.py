9638c158291ab7e76dfeac0349b7149eb980ac6c
Anže Pečar <apecar@google.com>
2018-01-27 21:14:10 +0100

    Propagate program acl roles in snapshotter

--- src/ggrc/snapshotter/acl.py
+++ src/ggrc/snapshotter/acl.py
@@ -19,7 +19,10 @@ def get_acl_payload(snapshots):
       all_models.AccessControlRole.name).filter(
       all_models.AccessControlRole.name.in_((
           "Auditors", "Audit Captains", "Auditors Snapshot Mapped",
-          "Audit Captains Mapped"))
+          "Audit Captains Mapped",
+          "Program Managers Mapped",
+          "Program Editors Mapped",
+          "Program Readers Mapped"))
   )
   ac_roles = {name: id_ for id_, name in ac_roles}
   parent_roles = db.session.query(
@@ -28,13 +31,19 @@ def get_acl_payload(snapshots):
       all_models.AccessControlList.ac_role_id
   ).filter(
       all_models.AccessControlList.ac_role_id.in_(
-          (ac_roles["Auditors"], ac_roles["Audit Captains"])),
+          (ac_roles["Auditors"], ac_roles["Audit Captains"],
+           ac_roles["Program Managers Mapped"],
+           ac_roles["Program Editors Mapped"],
+           ac_roles["Program Readers Mapped"])),
       tuple_(all_models.AccessControlList.object_id,
              all_models.AccessControlList.object_type).in_(parents)
   )
   child_roles = {
       ac_roles["Auditors"]: ac_roles["Auditors Snapshot Mapped"],
-      ac_roles["Audit Captains"]: ac_roles["Audit Captains Mapped"]
+      ac_roles["Audit Captains"]: ac_roles["Audit Captains Mapped"],
+      ac_roles["Program Managers Mapped"]: ac_roles["Program Managers Mapped"],
+      ac_roles["Program Editors Mapped"]: ac_roles["Program Editors Mapped"],
+      ac_roles["Program Readers Mapped"]: ac_roles["Program Readers Mapped"],
   }
   for parent_id, person_id, ac_role_id in parent_roles:
     for snapshot in snapshots:
