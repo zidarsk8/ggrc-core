6b32c55916dfcfcf290481cdcf16d8f83e2f4e21
Miha Zidar <zidarsk8@gmail.com>
2018-01-30 00:29:57 +0100

    Add wrapper for possible exceptions
    The related_assessments resource should always return a normal bad
    request response if the request parameters are not as they should be.
    This commit ensures all possible exceptions are caught and handled
    correctly.

--- src/ggrc/services/resources/related_assessments.py
+++ src/ggrc/services/resources/related_assessments.py
@@ -25,6 +25,7 @@ from ggrc.utils import benchmark
 from ggrc.rbac import permissions
 from ggrc.services import common
 from ggrc.query import pagination
+from ggrc.query.exceptions import BadQueryException
 
 
 class RelatedAssessmentsResource(common.Resource):
@@ -123,8 +124,6 @@ class RelatedAssessmentsResource(common.Resource):
         models.Document.document_type.in_(related_document_types)
     )
 
-
-
     assessment_document_map = defaultdict(set)
     all_document_ids = set()
     for assessment_id, document_id in source_query.union(destination_query):
@@ -133,7 +132,7 @@ class RelatedAssessmentsResource(common.Resource):
 
     if all_document_ids:
       documents = models.Document.query.options(
-        orm.Load(models.Document).undefer_group("Document_complete")
+          orm.Load(models.Document).undefer_group("Document_complete")
       ).filter(
           models.Document.id.in_(all_document_ids)
       ).all()
@@ -230,43 +229,52 @@ class RelatedAssessmentsResource(common.Resource):
   def dispatch_request(self, *args, **kwargs):  # noqa
     """Dispatch request for related_assessments."""
     with benchmark("dispatch related_assessments request"):
+      try:
 
-      object_type = request.args.get("object_type")
-      object_id = int(request.args.get("object_id"))
-      order_by = request.args.get("order_by", "")
-      limit_string = request.args.get("limit", "")
-      limit = [int(i) for i in limit_string.split(",") if i]
+        object_type = request.args.get("object_type")
+        object_id = int(request.args.get("object_id"))
+        order_by = request.args.get("order_by", "")
+        limit_string = request.args.get("limit", "")
+        limit = [int(i) for i in limit_string.split(",") if i]
 
-      if request.method != 'GET':
+        if request.method != 'GET':
+          raise BadRequest()
+
+        model = models.inflector.get_model(object_type)
+        obj = model.query.get(object_id)
+
+        with benchmark("get related assessments"):
+          assessments, total = self._get_assessments(
+              model, object_type, object_id, order_by, limit)
+
+        with benchmark("get documents of related assessments"):
+          document_json_map = self._get_documents(assessments)
+        with benchmark("get snapshots of related assessments"):
+          snapshot_json_map = self._get_snapshots(obj, assessments)
+
+        with benchmark("generate related_assessment json"):
+          assessments_json = []
+          for assessment in assessments:
+            single_json = assessment.log_json_base()
+            single_json["audit"] = assessment.audit.log_json_base()
+            single_json["custom_attribute_values"] = [
+                cav.log_json_base()
+                for cav in assessment.custom_attribute_values
+            ]
+            single_json["custom_attribute_definitions"] = [
+                cad.log_json_base()
+                for cad in assessment.custom_attribute_definitions
+            ]
+            single_json["snapshots"] = snapshot_json_map[assessment.id]
+            single_json["documents"] = document_json_map[assessment.id]
+            assessments_json.append(single_json)
+
+          return self.json_success_response(assessments_json, )
+
+      except (ValueError, TypeError, AttributeError, BadQueryException):
+        # Type Error and Value Error are for invalid integer values,
+        # Attribute error is for invalid models passed, which return None type
+        # that does not have query attribute.
+        # Bad query exception is for invalid parameters for limit such as
+        # negative numbers.
         raise BadRequest()
-
-      model = models.inflector.get_model(object_type)
-      obj = model.query.get(object_id)
-
-      with benchmark("get related assessments"):
-        assessments, total = self._get_assessments(
-            model, object_type, object_id, order_by, limit)
-
-      with benchmark("get documents of related assessments"):
-        document_json_map = self._get_documents(assessments)
-      with benchmark("get snapshots of related assessments"):
-        snapshot_json_map = self._get_snapshots(obj, assessments)
-
-      with benchmark("generate related_assessment json"):
-        assessments_json = []
-        for assessment in assessments:
-          single_json = assessment.log_json_base()
-          single_json["audit"] = assessment.audit.log_json_base()
-          single_json["custom_attribute_values"] = [
-              cav.log_json_base()
-              for cav in assessment.custom_attribute_values
-          ]
-          single_json["custom_attribute_definitions"] = [
-              cad.log_json_base()
-              for cad in assessment.custom_attribute_definitions
-          ]
-          single_json["snapshots"] = snapshot_json_map[assessment.id]
-          single_json["documents"] = document_json_map[assessment.id]
-          assessments_json.append(single_json)
-
-        return self.json_success_response(assessments_json, )
