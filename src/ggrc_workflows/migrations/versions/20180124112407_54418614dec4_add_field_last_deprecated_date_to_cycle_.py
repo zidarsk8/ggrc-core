97f61a44865b1a99afa8539b3bf5bc7a7d3303ee
Aliaksei Buziuma <buzuma.leha@gmail.com>
2018-01-29 13:35:22 +0300

    Add Last Deprecated Date for CycleTaskGroupObjectTask

---
+++ src/ggrc_workflows/migrations/versions/20180124112407_54418614dec4_add_field_last_deprecated_date_to_cycle_.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2018 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""
+add field last_deprecated_date to cycle_task_group_object_tasks
+
+Create Date: 2018-01-24 11:24:07.291724
+"""
+# disable Invalid constant name pylint warning for mandatory Alembic variables.
+# pylint: disable=invalid-name
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '54418614dec4'
+down_revision = '58e0f07e174b'
+
+
+def upgrade():
+  """Upgrade database schema and/or data, creating a new revision."""
+  op.add_column('cycle_task_group_object_tasks',
+                sa.Column('last_deprecated_date', sa.Date))
+
+
+def downgrade():
+  """Downgrade database schema and/or data back to the previous revision."""
+  op.drop_column('cycle_task_group_object_tasks', 'last_deprecated_date')
