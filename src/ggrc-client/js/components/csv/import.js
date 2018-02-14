021184ca36d867ec139c87808fcf0386bcfcd1e7
Alexander Novik <al.novik88@gmail.com>
2018-02-15 01:12:41 +0300

    Update quick tips

--- src/ggrc-client/js/components/csv/import.js
+++ src/ggrc-client/js/components/csv/import.js
@@ -8,6 +8,7 @@ import {warning} from '../../plugins/utils/modals';
 import {hasWarningType} from '../../plugins/utils/controllers';
 import './csv-template';
 import '../show-more/show-more';
+import quickTips from './templates/quick-tips.mustache';
 import template from './templates/csv-import.mustache';
 
 export default GGRC.Components('csvImportWidget', {
@@ -16,6 +17,7 @@ export default GGRC.Components('csvImportWidget', {
   requestData: null,
   scope: {
     importUrl: '/_service/import_csv',
+    quickTips,
     'import': null,
     fileId: '',
     fileName: '',
