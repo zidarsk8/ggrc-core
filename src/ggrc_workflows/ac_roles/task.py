30994dc2b2a0f88f28616c3c909fbf6494d991df
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2018-02-14 18:08:25 +0300

    Add ACR names constants for Workflow related models

---
+++ src/ggrc_workflows/ac_roles/task.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2018 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+"""Module contains Access Control Role names related to TaskGroupTask model."""
+
+ASSIGNEE_NAME = "Task Assignees"
+SECONDARY_ASSIGNEE_NAME = "Task Secondary Assignees"
---
+++ src/ggrc_workflows/ac_roles/workflow.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2018 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+"""Module contains Access Control Role names related to Workflow model."""
+
+ADMIN_NAME = "Admin"
+ADMIN_MAPPED_NAME = "{} Mapped".format(ADMIN_NAME)
+MEMBER_NAME = "Workflow Member"
+MEMBER_MAPPED_NAME = "{} Mapped".format(MEMBER_NAME)
