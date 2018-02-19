e4a082aff84068f80a817a15c5c6167446c54942
Yury Ostroukh <0str0uchura@gmail.com>
2018-02-19 12:49:07 +0300

    Split workflow-activate to separate vars

--- src/ggrc-client/js/components/workflow/workflow-activate.js
+++ src/ggrc-client/js/components/workflow/workflow-activate.js
@@ -9,111 +9,116 @@ import {
 } from '../../plugins/utils/current-page-utils';
 import Permission from '../../permission';
 
-export default can.Component.extend({
-  tag: 'workflow-activate',
-  template: '<content/>',
-  init: function () {
+const viewModel = can.Map.extend({
+  taskGroupAmount: 0,
+  instance: {},
+  waiting: true,
+  can_activate: false,
+  _can_activate_def: function () {
+    let self = this;
+    const workflow = this.attr('instance');
+
+    self.attr('waiting', true);
+    $.when(
+      workflow.refresh_all('task_groups', 'task_group_objects'),
+      workflow.refresh_all('task_groups', 'task_group_tasks')
+    )
+    .always(function () {
+      self.attr('waiting', false);
+    })
+    .done(function () {
+      let taskGroups = workflow.task_groups.reify();
+      let canActivate = taskGroups.length;
+
+      taskGroups.each(function (taskGroup) {
+        if (!taskGroup.task_group_tasks.length) {
+          canActivate = false;
+        }
+      });
+      self.attr('can_activate', canActivate);
+    })
+    .fail(function (error) {
+      console.warn('Workflow activate error', error.message);
+    });
+  },
+  _handle_refresh: function (model) {
+    let models = ['TaskGroupTask', 'TaskGroupObject'];
+    if (models.indexOf(model.shortName) > -1) {
+      this._can_activate_def();
+    }
+  },
+  _restore_button: function () {
+    this.attr('waiting', false);
+  },
+  _activate: function () {
+    const workflow = this.attr('instance');
+    let viewModel = this;
+    let restoreButton = viewModel._restore_button.bind(viewModel);
+
+    viewModel.attr('waiting', true);
+    if (workflow.unit !== null) {
+      workflow.refresh()
+        .then(function () {
+          workflow.attr('recurrences', true);
+          workflow.attr('status', 'Active');
+          return workflow.save();
+        })
+        .then(() => Permission.refresh())
+        .then(function () {
+          let WorkflowExtension =
+            _.find(GGRC.extensions, function (extension) {
+              return extension.name === 'workflows';
+            });
+
+          return initCounts([
+              WorkflowExtension.countsMap.activeCycles,
+            ],
+              workflow.type,
+              workflow.id);
+        })
+        .then(function () {
+          return workflow.refresh_all('task_groups', 'task_group_tasks');
+        })
+        .then(() => {
+          const cycleStub = workflow.attr('cycles')[0];
+          workflowHelpers.redirectToCycle(cycleStub);
+        })
+        .always(restoreButton);
+    } else {
+      workflowHelpers.generateCycle(workflow)
+        .then(function () {
+          return workflow.refresh();
+        })
+        .then(function (workflow) {
+          return workflow.attr('status', 'Active').save();
+        })
+        .always(restoreButton);
+    }
+  },
+});
+
+const events = {
+  '{viewModel} taskGroupAmount'() {
     this.viewModel._can_activate_def();
   },
-  viewModel: {
-    taskGroupAmount: 0,
-    instance: {},
-    waiting: true,
-    can_activate: false,
-    _can_activate_def: function () {
-      let self = this;
-      const workflow = this.attr('instance');
-
-      self.attr('waiting', true);
-      $.when(
-        workflow.refresh_all('task_groups', 'task_group_objects'),
-        workflow.refresh_all('task_groups', 'task_group_tasks')
-      )
-      .always(function () {
-        self.attr('waiting', false);
-      })
-      .done(function () {
-        let taskGroups = workflow.task_groups.reify();
-        let canActivate = taskGroups.length;
-
-        taskGroups.each(function (taskGroup) {
-          if (!taskGroup.task_group_tasks.length) {
-            canActivate = false;
-          }
-        });
-        self.attr('can_activate', canActivate);
-      })
-      .fail(function (error) {
-        console.warn('Workflow activate error', error.message);
-      });
-    },
-    _handle_refresh: function (model) {
-      let models = ['TaskGroupTask', 'TaskGroupObject'];
-      if (models.indexOf(model.shortName) > -1) {
-        this._can_activate_def();
-      }
-    },
-    _restore_button: function () {
-      this.attr('waiting', false);
-    },
-    _activate: function () {
-      const workflow = this.attr('instance');
-      let viewModel = this;
-      let restoreButton = viewModel._restore_button.bind(viewModel);
-
-      viewModel.attr('waiting', true);
-      if (workflow.unit !== null) {
-        workflow.refresh()
-          .then(function () {
-            workflow.attr('recurrences', true);
-            workflow.attr('status', 'Active');
-            return workflow.save();
-          })
-          .then(() => Permission.refresh())
-          .then(function () {
-            let WorkflowExtension =
-              _.find(GGRC.extensions, function (extension) {
-                return extension.name === 'workflows';
-              });
-
-            return initCounts([
-                WorkflowExtension.countsMap.activeCycles,
-              ],
-                workflow.type,
-                workflow.id);
-          })
-          .then(function () {
-            return workflow.refresh_all('task_groups', 'task_group_tasks');
-          })
-          .then(() => {
-            const cycleStub = workflow.attr('cycles')[0];
-            workflowHelpers.redirectToCycle(cycleStub);
-          })
-          .always(restoreButton);
-      } else {
-        workflowHelpers.generateCycle(workflow)
-          .then(function () {
-            return workflow.refresh();
-          })
-          .then(function (workflow) {
-            return workflow.attr('status', 'Active').save();
-          })
-          .always(restoreButton);
-      }
-    },
+  '{can.Model.Cacheable} created': function (model) {
+    this.viewModel._handle_refresh(model);
   },
-  events: {
-    '{viewModel} taskGroupAmount'() {
-      this.viewModel._can_activate_def();
-    },
-    '{can.Model.Cacheable} created': function (model) {
-      this.viewModel._handle_refresh(model);
-    },
-    '{can.Model.Cacheable} destroyed': function (model) {
-      this.viewModel._handle_refresh(model);
-    },
-    'button click': function () {
-      this.viewModel._activate();
-    },
+  '{can.Model.Cacheable} destroyed': function (model) {
+    this.viewModel._handle_refresh(model);
   },
+  'button click': function () {
+    this.viewModel._activate();
+  },
+};
+
+const init = function () {
+  this.viewModel._can_activate_def();
+};
+
+export default can.Component.extend({
+  tag: 'workflow-activate',
+  init,
+  viewModel,
+  events,
 });
