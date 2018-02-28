d2284806351ad6d6534167534170046c4d31c2c2
Anže Pečar <apecar@google.com>
2018-02-28 17:53:12 +0000

    Fix unmap button
    Since related_sources/related_destinations are no longer present on the
    FE we need to make 2 or 3 requests to perform an unmapping.

--- src/ggrc-client/js/components/unmap-button/unmap-button.js
+++ src/ggrc-client/js/components/unmap-button/unmap-button.js
@@ -23,7 +23,6 @@ import {DESTINATION_UNMAPPED} from '../../events/eventTypes';
         this.attr('isUnmapping', true);
         this.dispatch({type: 'beforeUnmap', item: this.attr('source')});
         this.getMapping()
-          .refresh()
           .done((item) => {
             item.destroy()
               .then(() => {
@@ -40,21 +39,47 @@ import {DESTINATION_UNMAPPED} from '../../events/eventTypes';
             this.attr('isUnmapping', false);
           });
       },
+      _findRelationship: function (source, destination) {
+        return CMS.Models.Relationship.findAll({
+          source_id: source.attr('id'),
+          source_type: source.attr('type'),
+          destination_id: destination.attr('id'),
+          destination_type: destination.attr('type')});
+      },
       getMapping: function () {
         let type = this.attr('mappingType') || defaultType;
         let destinations;
         let sources;
         let mapping;
         if (type === defaultType) {
-          destinations = this.attr('destination.related_destinations')
-          .concat(this.attr('destination.related_sources'));
-          sources = this.attr('source.related_destinations')
-          .concat(this.attr('source.related_sources'));
+          // defaultType is a Relationship mapping and since we no longer have
+          // related_source/related_destinations on the FE we need to make
+          // 2 or 3 requests to find the correct relationship object to delete.
+          // 1. Try fetching a relationship where this.source is the source
+          //    and this.destination is the destination.
+          // 2. If 1. did not give a result, we try the opposite, where
+          //    this.source is the destination and this.destination is
+          //    the source.
+          // 3. We then have to do a refresh to get the ETAG before we can do
+          //    the DELETE.
+          // This code can be simplified once we add a /relationships/unmap API
+          return this._findRelationship(this.source, this.destination)
+            .then((res) => {
+              if (!res.length) {
+                return this._findRelationship(this.destination, this.source);
+              }
+              return res;
+            }).then((res) => {
+              if (!res.length) {
+                console.error('Could not find the corresponding relationship');
+              }
+              return res[0].refresh();
+            });
         } else {
           destinations = this.attr('destination')
-              .attr(this.attr('objectProp')) || [];
+            .attr(this.attr('objectProp')) || [];
           sources = this.attr('source')
-              .attr(this.attr('objectProp')) || [];
+            .attr(this.attr('objectProp')) || [];
         }
         sources = sources
           .map(function (item) {
@@ -64,7 +89,7 @@ import {DESTINATION_UNMAPPED} from '../../events/eventTypes';
           .filter(function (dest) {
             return sources.indexOf(dest.id) > -1;
           })[0];
-        return new CMS.Models[type](mapping || {});
+        return new CMS.Models[type](mapping || {}).refresh();
       },
     },
     events: {
