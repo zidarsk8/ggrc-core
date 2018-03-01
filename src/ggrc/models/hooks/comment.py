28c1caa7d88ce8ea39d9b4132fd2b3595308861f
Miha Zidar <zidarsk8@gmail.com>
2018-03-01 02:12:35 +0100

    Update comment acl hook
    The comment ACL entry should be done before by our post signals before
    any sqlalchemy events are triggered. This way we can ensure that all
    triggers will contain the needed data.

--- src/ggrc/models/hooks/comment.py
+++ src/ggrc/models/hooks/comment.py
@@ -2,25 +2,31 @@
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
 """A module with Comment object creation hooks"""
-from sqlalchemy import event
 
-from ggrc.login import get_current_user
+from ggrc import login
 from ggrc.models import all_models
 from ggrc.access_control import role
+from ggrc.services import signals
 
 
 def init_hook():
   """Initialize all hooks"""
   # pylint: disable=unused-variable
-  @event.listens_for(all_models.Comment, "after_insert")
-  def handle_comment_post(mapper, connection, target):
-    """Save information on which user created the Comment object."""
+  @signals.Restful.collection_posted.connect_via(all_models.Comment)
+  def handle_comment_post(mapper, objects, **kwargs):
+    """Save information on which user created the Comment object.
+
+    Comments have their users set in a hook, because we must ensure that it is
+    always set to the current user, and that is why the info is not sent from
+    the front-end through access_control_list property.
+    """
     # pylint: disable=unused-argument
     comment_roles = role.get_ac_roles_for(all_models.Comment.__name__)
     comment_admin = comment_roles["Admin"]
-    user = get_current_user()
-    all_models.AccessControlList(
-        ac_role=comment_admin,
-        person=user,
-        object=target,
-    )
+    user = login.get_current_user()
+    for comment in objects:
+      all_models.AccessControlList(
+          ac_role=comment_admin,
+          person=user,
+          object=comment,
+      )
