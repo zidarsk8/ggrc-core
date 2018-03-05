a1de5f14a2da0aed925aa9a10226e4c94a898380
Alexander Novik <al.novik88@gmail.com>
2018-03-05 19:21:36 +0300

    Move relevant-filter into import-export folder

--- src/ggrc-client/js/components/import-export/export.js
+++ src/ggrc-client/js/components/import-export/export.js
@@ -3,7 +3,7 @@
   Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
-import '../relevant_filters';
+import './relevant-filter';
 import './export-group';
 import csvExportTemplate from './templates/csv-export.mustache';
 import {confirm} from '../../plugins/utils/modals';
