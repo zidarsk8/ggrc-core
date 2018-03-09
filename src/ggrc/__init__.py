eba16a2a867a236bd13af64ddbfb81728466b714
Anže Pečar <apecar@google.com>
2018-03-09 03:52:40 +0000

    Highlight slow request in local development
    This adds a simple log that highlights slow reqests in yellow (if
    it takes more than 1s).

--- src/ggrc/__init__.py
+++ src/ggrc/__init__.py
@@ -2,8 +2,9 @@
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
 """Main GGRC module"""
-
+import time
 from ggrc import bootstrap
 
+INIT_TIME = time.time(), time.clock()
 # pylint: disable=invalid-name
 db = bootstrap.get_db()
