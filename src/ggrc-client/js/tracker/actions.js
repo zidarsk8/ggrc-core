de30e8ec8683bd403af5e572a039266175342335
Alexander Novik <al.novik88@gmail.com>
2018-03-13 15:15:31 +0300

    Change labels for LHN

--- src/ggrc-client/js/tracker/actions.js
+++ src/ggrc-client/js/tracker/actions.js
@@ -3,14 +3,17 @@
     Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
+// Timing Category in Google Analytics Timing API
 export const FOCUS_AREAS = {
   ASSESSMENT: 'Assessment',
   AUDIT: 'Audit',
   RELATED_ASSESSMENTS: 'Related Assessments',
   SNAPSHOTS: 'Snapshots',
   IMPORT_AND_EXPORT: 'Import/Export',
+  LHN: 'LHN',
 };
 
+// Timing Variable in Google Analytics Timing API
 export const USER_JOURNEY_KEYS = {
   LOADING: 'Loading',
   NAVIGATION: 'Navigation',
@@ -18,13 +21,18 @@ export const USER_JOURNEY_KEYS = {
   MAPPING_TO_OBJECT: 'Mapping to object',
 };
 
+// Timing Label in Google Analytics Timing API
 export const USER_ACTIONS = {
   MULTISELECT_FILTER: 'Multi-select filter',
   OPEN_INFO_PANE: 'Open Info pane',
   ADVANCED_SEARCH_FILTER: 'Advanced search filter',
+  LHN: {
+    SHOW_LIST: 'LHN: Show list of items',
+    SHOW_MORE: 'LHN: Show more items for the list',
+  },
   ASSESSMENT: {
     OPEN_ASMT_GEN_MODAL: 'Open Assessment Generation modal',
-    CHANGE_STATUS: 'Change status',
+    CHANGE_STATUS: 'Changing status of Assessment',
     EDIT_LCA: 'Editing Local CA',
   },
 };
