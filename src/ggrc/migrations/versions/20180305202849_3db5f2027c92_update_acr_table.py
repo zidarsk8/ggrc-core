2cecdd4ea6e271de923619f7cced588cac3edaae
Miha Zidar <zidarsk8@gmail.com>
2018-03-13 01:58:56 +0100

    Add assessment ACR propagation entries

--- src/ggrc/migrations/versions/20180305202849_3db5f2027c92_update_acr_table.py
+++ src/ggrc/migrations/versions/20180305202849_3db5f2027c92_update_acr_table.py
@@ -13,13 +13,44 @@ import sqlalchemy as sa
 
 from alembic import op
 
+from ggrc.migrations.utils import acr_propagation
+
+
 # revision identifiers, used by Alembic.
 revision = '3db5f2027c92'
 down_revision = '19a260ec358e'
 
+_PROPAGATION = {
+    "Relationship R": {
+        "Audit R": {
+            "Relationship R": {
+                "Document R": {},
+            },
+        },
+        "Document RU": {},
+        "Comment R": {},
+        "Issue R": {
+            "Relationship R": {
+                "Comment R": {},
+                "Document R": {},
+            },
+        },
+        "Snapshot R": {
+            "Relationship R": {
+                "Snapshot R": {},
+            },
+        },
+    },
+}
 
-def upgrade():
-  """Upgrade database schema and/or data, creating a new revision."""
+ASSESSMENT_PROPAGATION = {
+    "Creators": _PROPAGATION,
+    "Assignees": _PROPAGATION,
+    "Verifiers": _PROPAGATION,
+}
+
+
+def _add_parent_id_column():
   op.add_column('access_control_roles', sa.Column(
       'parent_id', sa.Integer(), nullable=True))
   op.create_foreign_key(
@@ -30,11 +61,32 @@ def upgrade():
   )
 
 
-def downgrade():
-  """Downgrade database schema and/or data back to the previous revision."""
+def _remove_parent_id_column():
   op.drop_constraint(
       "fk_access_control_roles_parent_id",
       "access_control_roles",
       "foreignkey",
   )
   op.drop_column('access_control_roles', 'parent_id')
+
+
+def _add_assessment_roles_tree():
+  acr_propagation.propagate_roles(
+      "Assessment",
+      ASSESSMENT_PROPAGATION,
+  )
+
+
+def upgrade():
+  """Upgrade database schema and/or data, creating a new revision."""
+  _add_parent_id_column()
+  _add_assessment_roles_tree()
+
+
+def downgrade():
+  """Downgrade database schema and/or data back to the previous revision."""
+  acr_propagation.remove_propagated_roles(
+      "Assessment",
+      ASSESSMENT_PROPAGATION.keys()
+  )
+  _remove_parent_id_column()
