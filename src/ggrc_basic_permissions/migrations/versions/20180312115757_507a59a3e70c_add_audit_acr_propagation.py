983a64694659c1a8db2402e017aef60e6b814e2d
Miha Zidar <zidarsk8@gmail.com>
2018-03-19 04:15:39 +0100

    Update role propagation migration
    This update changes the propagation format to match the new style for
    the propagation utils module.
    
    The commit also contains new elements containing all propagated roles.

--- src/ggrc_basic_permissions/migrations/versions/20180312115757_507a59a3e70c_add_audit_acr_propagation.py
+++ src/ggrc_basic_permissions/migrations/versions/20180312115757_507a59a3e70c_add_audit_acr_propagation.py
@@ -17,56 +17,131 @@ from ggrc.migrations.utils import acr_propagation
 revision = '3db5f2027c92'
 down_revision = '242b8dc8493b'
 
-
-AUDIT_PROPAGATION = {
-    "Audit Captains": {
-        "Relationship R": {
-            "Assessment RUD": {
-                "Relationship R": {
-                    "Comment R": {},
-                    "Document RU": {},
-                },
-            },
-            "AssessmentTemplate RUD": {},
-            "Comment R": {},
-            "Document RU": {},
-            "Issue RUD": {
-                "Relationship R": {
-                    "Comment R": {},
-                    "Document RU": {},
-                },
+_AUDIT_FULL_ACCESS = {
+    "Relationship R": {
+        "Assessment RUD": {
+            "Relationship R": {
+                "Comment R": {},
+                "Document RU": {},
             },
         },
-        "Snapshot RU": {},
+        "AssessmentTemplate RUD": {},
+        "Document RU": {},
+        "Issue RUD": {
+            "Relationship R": {
+                "Comment R": {},
+                "Document RU": {},
+            },
+        },
     },
-    "Auditors": {
-        "Relationship R": {
-            "Assessment RU": {
-                "Relationship R": {
-                    "Comment R": {},
-                    "Document RU": {},
-                },
-            },
-            "AssessmentTemplate R": {},
-            "Comment R": {},
-            "Document R": {},
-            "Issue RU": {
-                "Relationship R": {
-                    "Comment R": {},
-                    "Document RU": {},
-                },
+    "Snapshot RU": {},
+}
+
+_AUDIT_READ_ACCESS = {
+    "Relationship R": {
+        "Assessment RU": {
+            "Relationship R": {
+                "Comment R": {},
+                "Document RU": {},
             },
         },
-        "Snapshot RU": {},
-    }
+        "AssessmentTemplate R": {},
+        "Document R": {},
+        "Issue RU": {
+            "Relationship R": {
+                "Comment R": {},
+                "Document RU": {},
+            },
+        },
+    },
+    "Snapshot RU": {},
+}
+
+
+_PROGRAM_OBJECTS = (
+    "AccessGroup",
+    "Clause",
+    "Contract",
+    # "Control",  # control is separate due to proposals
+    "DataAsset",
+    "Facility",
+    "Issue",
+    "Market",
+    "Objective",
+    "OrgGroup",
+    "Policy",
+    "Process",
+    "Product",
+    "Project",
+    "Regulation",
+    # "Risk",  # excluded due to proposals
+    "RiskAssessment",
+    "Section",
+    "Standard",
+    "System",
+    "Threat",
+    "Vendor",
+)
+
+PROPAGATION = {
+    "Program": {
+        ("Program Managers", "Program Editors"): {
+            "Audit RUD": _AUDIT_FULL_ACCESS,
+            "Relationship R": {
+                "Comment R": {},
+                "Document RU": {},
+                _PROGRAM_OBJECTS: {
+                    "Relationship R": {
+                        "Comment R": {},
+                        "Document RU": {},
+                    }
+                },
+                ("Control", "Risk"): {
+                    "Relationship R": {
+                        "Comment R": {},
+                        "Document RU": {},
+                        "Proposal RU": {},
+                    }
+                },
+            }
+        },
+        "Program Readers": {
+            "Audit R": _AUDIT_READ_ACCESS,
+            "Relationship R": {
+                "Comment R": {},
+                "Document R": {},
+                _PROGRAM_OBJECTS: {
+                    "Relationship R": {
+                        "Comment R": {},
+                        "Document R": {},
+                    }
+                },
+                ("Control", "Risk"): {
+                    "Relationship R": {
+                        "Comment R": {},
+                        "Document R": {},
+                        "Proposal R": {},
+                    }
+                },
+            }
+        },
+    },
+    "Audit": {
+        # Audit captains might also need to get propagated access to program
+        # and all program related objects, so that they could clone audits and
+        # update all audit snapshots.
+        "Audit Captains": _AUDIT_FULL_ACCESS,
+        "Auditors": _AUDIT_READ_ACCESS,
+    },
 }
 
 
 def upgrade():
   """Upgrade database schema and/or data, creating a new revision."""
-  acr_propagation.propagate_roles("Audit", AUDIT_PROPAGATION)
+  acr_propagation.propagate_roles(PROPAGATION)
 
 
 def downgrade():
   """Downgrade database schema and/or data back to the previous revision."""
-  acr_propagation.remove_propagated_roles("Audit", AUDIT_PROPAGATION.keys())
+  for object_type, roles_tree in PROPAGATION.items():
+    acr_propagation.remove_propagated_roles(object_type, roles_tree.keys())
