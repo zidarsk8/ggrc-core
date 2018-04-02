5e8b07340e08658b103409537ff10f57a97b1ead
Artsiom Valchanin <volchanin.dev@gmail.com>
2018-04-02 15:39:16 +0300

    Refactor related-urls component

--- src/ggrc-client/js/components/related-objects/related-urls.js
+++ src/ggrc-client/js/components/related-objects/related-urls.js
@@ -3,137 +3,119 @@
     Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
-(function (can, $) {
-  'use strict';
+import template from './templates/related-urls.mustache';
 
-  GGRC.Components('relatedUrls', {
-    tag: 'related-urls',
-    viewModel: {
-      element: null,
-      define: {
-        urls: {
-          value: function () {
-            return [];
-          },
-        },
-        value: {
-          type: 'string',
-          value: '',
-        },
-        isFormVisible: {
-          type: 'boolean',
-          value: false,
-        },
-        isDisabled: {
-          type: 'boolean',
-          value: false,
-        },
-        isNotEditable: {
-          type: 'boolean',
-          value: false,
-        },
-      },
-      /**
-       * @description Moves focus to the create url input element
-       */
-      moveFocusToInput: function () {
-        let inputElements = this.element.find('input');
-        if (inputElements.length) {
-          inputElements[0].focus();
-        }
-      },
-      /**
-       * @description Validates user input
-       *
-       * @param  {String} data Data for validation
-       * @return {Boolean} - If incoming string is empty it returns false,
-       * otherwise true
-       */
-      validateUserInput: function (data) {
-        return data.length > 0;
-      },
-      /**
-       * Handle changes during toggling form visibility.
-       *
-       * @param  {Boolean} isVisible - New value for form visibility
-       * @param  {Boolean} [keepValue=false] - Whether to preserve the existing
-       *   value of the form input field or not.
-       */
+GGRC.Components('relatedUrls', {
+  tag: 'related-urls',
+  template: template,
+  viewModel: {
+    element: null,
+    urls: [],
+    value: '',
+    isFormVisible: false,
+    isDisabled: false,
+    isNotEditable: false,
+    allowToRemove: true,
+    /**
+     * @description Moves focus to the create url input element
+     */
+    moveFocusToInput: function () {
+      let inputElements = this.element.find('input');
+      if (inputElements.length) {
+        inputElements[0].focus();
+      }
+    },
+    /**
+     * @description Validates user input
+     *
+     * @param  {String} data Data for validation
+     * @return {Boolean} - If incoming string is empty it returns false,
+     * otherwise true
+     */
+    validateUserInput: function (data) {
+      return data.length > 0;
+    },
+    /**
+     * Handle changes during toggling form visibility.
+     *
+     * @param  {Boolean} isVisible - New value for form visibility
+     * @param  {Boolean} [keepValue=false] - Whether to preserve the existing
+     *   value of the form input field or not.
+     */
 
-      toggleFormVisibility: function (isVisible, keepValue) {
-        this.attr('isFormVisible', isVisible);
-        if (!keepValue) {
-          this.attr('value', '');
-        }
-        if (isVisible) {
-          this.moveFocusToInput();
-        }
-      },
-      /**
-       * @description Handles create url form submitting
-       *
-       * @param  {String} url - url to create
-       * @return {Boolean} - it returns false to prevent page refresh
-       */
-      submitCreateUrlForm: function (url) {
-        let existingUrls;
-        let trimmedUrl = url.trim();
-        let isValid = this.validateUserInput(trimmedUrl);
+    toggleFormVisibility: function (isVisible, keepValue) {
+      this.attr('isFormVisible', isVisible);
+      if (!keepValue) {
+        this.attr('value', '');
+      }
+      if (isVisible) {
+        this.moveFocusToInput();
+      }
+    },
+    /**
+     * @description Handles create url form submitting
+     *
+     * @param  {String} url - url to create
+     * @return {Boolean} - it returns false to prevent page refresh
+     */
+    submitCreateUrlForm: function (url) {
+      let existingUrls;
+      let trimmedUrl = url.trim();
+      let isValid = this.validateUserInput(trimmedUrl);
 
-        // non-valid user input case - empty string
-        if (!isValid) {
-          GGRC.Errors.notifier('error', 'Please enter a URL.');
-          this.toggleFormVisibility(true);
-          return false;
-        }
-
-        // duplicate URLs check
-        existingUrls = _.map(this.attr('urls'), 'link');
-
-        if (_.contains(existingUrls, trimmedUrl)) {
-          GGRC.Errors.notifier('error', 'URL already exists.');
-          this.toggleFormVisibility(true, true);
-          return false;
-        }
-
-        this.createUrl(trimmedUrl);
-
-        this.toggleFormVisibility(false);
+      // non-valid user input case - empty string
+      if (!isValid) {
+        GGRC.Errors.notifier('error', 'Please enter a URL.');
+        this.toggleFormVisibility(true);
         return false;
-      },
-      /**
-       * @description Dispatches 'createUrl' event with appropriate
-       * data payload
-       *
-       * @param  {String} url - url to create
-       */
-      createUrl: function (url) {
-        this.dispatch({
-          type: 'createUrl',
-          payload: url,
-        });
-      },
-      /**
-       * @description Dispatches 'removeUrl' event with appropriate
-       * data payload
-       *
-       * @param  {string} url - url to delete
-       */
-      removeUrl: function (url) {
-        this.dispatch({
-          type: 'removeUrl',
-          payload: url,
-        });
-      },
+      }
+
+      // duplicate URLs check
+      existingUrls = _.map(this.attr('urls'), 'link');
+
+      if (_.contains(existingUrls, trimmedUrl)) {
+        GGRC.Errors.notifier('error', 'URL already exists.');
+        this.toggleFormVisibility(true, true);
+        return false;
+      }
+
+      this.createUrl(trimmedUrl);
+
+      this.toggleFormVisibility(false);
+      return false;
     },
-    events: {
-      /**
-       * @description Handler for 'inserted' event to save reference
-       * to component element
-       */
-      inserted: function () {
-        this.viewModel.attr('element', this.element);
-      },
+    /**
+     * @description Dispatches 'createUrl' event with appropriate
+     * data payload
+     *
+     * @param  {String} url - url to create
+     */
+    createUrl: function (url) {
+      this.dispatch({
+        type: 'createUrl',
+        payload: url,
+      });
     },
-  });
-})(window.can, window.can.$);
+    /**
+     * @description Dispatches 'removeUrl' event with appropriate
+     * data payload
+     *
+     * @param  {string} url - url to delete
+     */
+    removeUrl: function (url) {
+      this.dispatch({
+        type: 'removeUrl',
+        payload: url,
+      });
+    },
+  },
+  events: {
+    /**
+     * @description Handler for 'inserted' event to save reference
+     * to component element
+     */
+    inserted: function () {
+      this.viewModel.attr('element', this.element);
+    },
+  },
+});
