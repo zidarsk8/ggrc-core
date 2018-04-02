57eb24bc6c28d769e5e1f9f546b1ea421d8b5865
Miha Zidar <zidarsk8@gmail.com>
2018-04-02 03:00:12 +0200

    Add retries for workflow ACL propagation
    This commit ensures if there is something wrong with mysql query for ACL
    propagation that we retry it at least PROPAGATION_RETRIES times. There
    is sufficient logging in place to warn us when a retry has occurred and
    if the operation did not finis even after the given retry limit.
    
    The code here that ensures a request raises an error if the operation
    does not succeed, is not tested due to instability of the tests. If a
    developer wishes to test the response they can set the propagation retry
    counter to 1 and then run the tests:
    
    nosetests integration.ggrc_workflows.test_acl_concurrency
    
    The current propagation retry count is set to 10 since that seems
    sufficient for 20 concurrent requests, which is more than the rest of
    the code supports.

--- src/ggrc_workflows/models/hooks/workflow.py
+++ src/ggrc_workflows/models/hooks/workflow.py
@@ -3,6 +3,8 @@
 
 """SQLAlchemy hooks for Workflow model."""
 
+import logging
+
 import flask
 import sqlalchemy as sa
 
@@ -11,6 +13,13 @@ from ggrc import login
 from ggrc import utils
 from ggrc.models import all_models
 from ggrc.access_control import role
+
+
+logger = logging.getLogger(__name__)
+
+
+# retry count for possible deadlock issues.
+PROPAGATION_RETRIES = 10
 
 
 RELATED_MODELS = (
@@ -146,23 +155,38 @@ def _insert_select_acls(select_statement):
 
   inserter = acl_table.insert().prefix_with("IGNORE")
 
-  db.session.execute(
-      inserter.from_select(
-          [
-              acl_table.c.person_id,
-              acl_table.c.ac_role_id,
-              acl_table.c.object_id,
-              acl_table.c.object_type,
-              acl_table.c.created_at,
-              acl_table.c.modified_by_id,
-              acl_table.c.updated_at,
-              acl_table.c.parent_id,
-          ],
-          select_statement
+  last_error = None
+  for _ in range(PROPAGATION_RETRIES):
+    try:
+      last_error = None
+      db.session.execute(
+          inserter.from_select(
+              [
+                  acl_table.c.person_id,
+                  acl_table.c.ac_role_id,
+                  acl_table.c.object_id,
+                  acl_table.c.object_type,
+                  acl_table.c.created_at,
+                  acl_table.c.modified_by_id,
+                  acl_table.c.updated_at,
+                  acl_table.c.parent_id,
+              ],
+              select_statement
+          )
       )
-  )
+      db.session.plain_commit()
+      break
+    except sa.exc.OperationalError as error:
+      logger.exception(error)
+      last_error = error
 
-  db.session.plain_commit()
+  if last_error:
+    logger.critical(
+        "Workflow ACL propagation failed with %d retries on statement: \n %s",
+        PROPAGATION_RETRIES,
+        select_statement,
+    )
+    raise last_error
 
 
 def _propagate_to_wf_children(new_wf_acls, child_class):
