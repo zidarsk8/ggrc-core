7315d29405846828fbcedc61bbd614410cb8eded
Miha Zidar <zidarsk8@gmail.com>
2018-04-03 21:33:20 +0200

    Move propagation constants into a separate file
    This change is just an extra step to ensure future devs won't change the
    propagation constants.

--- src/ggrc/migrations/utils/acr_propagation.py
+++ src/ggrc/migrations/utils/acr_propagation.py
@@ -9,46 +9,6 @@ import sqlalchemy as sa
 
 from alembic import op
 
-BASIC_ROLES = (
-    "Admin",
-    "Primary Contacts",
-    "Secondary Contacts",
-)
-
-COMMENT_DOCUMENT_R = {
-    "Relationship R": {
-        "Comment R": {},
-        "Document R": {},
-    },
-}
-
-COMMENT_DOCUMENT_RU = {
-    "Relationship R": {
-        "Comment R": {},
-        "Document RU": {},
-    },
-}
-
-PROPOSAL_RU = {
-    "Relationship R": {
-        "Comment R": {},
-        "Document RU": {},
-        "Proposal RU": {},
-    },
-}
-
-PROPOSAL_R = {
-    "Relationship R": {
-        "Comment R": {},
-        "Document RU": {},
-        "Proposal RU": {},
-    },
-}
-
-BASIC_PROPAGATION = {
-    "Admin": COMMENT_DOCUMENT_RU,
-}
-
 
 ACR_TABLE = sa.sql.table(
     "access_control_roles",
