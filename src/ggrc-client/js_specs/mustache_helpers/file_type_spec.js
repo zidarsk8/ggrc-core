08c1608a0396d45edebf1d2ba9382c4a60bc7881
Artsiom Valchanin <volchanin.dev@gmail.com>
2018-04-06 19:22:56 +0300

    Fix rest eslint issues in mustache_helper.js

--- src/ggrc-client/js_specs/mustache_helpers/file_type_spec.js
+++ src/ggrc-client/js_specs/mustache_helpers/file_type_spec.js
@@ -28,7 +28,7 @@ describe("can.mustache.helper.file_type", function () {
       instance.type = "FooBar";
       callWithInvalid = helper.bind(helper, instance);
 
-      expect(callWithInvalid).toThrow(errMsg);
+      expect(callWithInvalid).toThrowError(errMsg);
     }
   );
 
@@ -39,7 +39,7 @@ describe("can.mustache.helper.file_type", function () {
     instance.title = undefined;
     callWithInvalid = helper.bind(helper, instance);
 
-    expect(callWithInvalid).toThrow(errMsg);
+    expect(callWithInvalid).toThrowError(errMsg);
   });
 
   it("returns a default value if there is no file extension", function () {
