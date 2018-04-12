92a2a05de85a77daef3b5f2f0cf29c2da8084fd1
Miha Zidar <zidarsk8@gmail.com>
2018-04-12 13:09:48 +0200

    Store data for ACL propagation to flask g

--- src/ggrc/models/hooks/acl/__init__.py
+++ src/ggrc/models/hooks/acl/__init__.py
@@ -12,19 +12,45 @@ import flask
 import sqlalchemy as sa
 from sqlalchemy.orm.session import Session
 
-
 from ggrc.models.hooks.acl import audit_roles
 from ggrc.models.hooks.acl import program_roles
 from ggrc.models.hooks.acl import relationship_deletion
 from ggrc.models.hooks import access_control_list
 from ggrc.models.hooks import relationship
+
+from ggrc.models import all_models
+from ggrc.models.hooks.acl import propagation
 from ggrc_workflows.models.hooks import workflow
+
+
+def _get_propagation_entries(session):
+  acl_ids = set()
+  relationship_ids = set()
+  for obj in session.new:
+    if isinstance(obj, all_models.AccessControlList):
+      acl_ids.add(obj.id)
+    if isinstance(obj, all_models.Relationship):
+      relationship_ids.add(obj.id)
+
+  return acl_ids, relationship_ids
 
 
 def after_flush(session, _):
   """Handle all ACL hooks after after flush."""
   if not flask.has_app_context():
     return
+
+  acl_ids, relationship_ids = _get_propagation_entries(session)
+
+  if hasattr(flask.g, "new_acl_ids"):
+    flask.g.new_acl_ids.update(acl_ids)
+  else:
+    flask.g.new_acl_ids = acl_ids
+
+  if hasattr(flask.g, "new_relationship_ids"):
+    flask.g.new_relationship_ids.update(relationship_ids)
+  else:
+    flask.g.new_relationship_ids = relationship_ids
 
   relationship.handle_relationship_creation(session)
   access_control_list.handle_acl_creation(session)
@@ -33,6 +59,9 @@ def after_flush(session, _):
   audit_role_handler = audit_roles.AuditRolesHandler()
   audit_role_handler.after_flush(session)
   relationship_deletion.after_flush(session)
+
+  # Legacy propagation for workflows that will have to be refactored to use
+  # relationships and the code above
 
   if hasattr(flask.g, "new_wf_acls"):
     flask.g.new_wf_acls.update(workflow.get_new_wf_acls(session))
@@ -46,6 +75,7 @@ def after_flush(session, _):
 
 
 def after_commit():
+  propagation.propagate()
   workflow.handle_acl_changes()
 
 
