f849bc874879a705e0ee13786931a99b4336e685
YuriChupyrkin <yu4.1990@gmail.com>
2018-04-14 12:42:08 +0300

    Cleanup unused code managing backgroud deletion

--- src/ggrc-client/js/controllers/modals/index.js
+++ src/ggrc-client/js/controllers/modals/index.js
@@ -6,7 +6,6 @@
 import './modals_controller';
 import './archive_modal_controller';
 import './delete_modal_controller';
-import './unmap_modal_controller';
 import './quick_form_controller';
 import './gapi-modal';
 import './approval-workflow-modal';
