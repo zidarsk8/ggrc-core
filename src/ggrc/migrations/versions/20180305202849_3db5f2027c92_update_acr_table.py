8a6a3ca832ac330276a9e5b6a66370272042313c
Miha Zidar <zidarsk8@gmail.com>
2018-04-25 01:06:29 +0200

    Add ACL downgrade option with info
    This change allows us to have a semi-proper downgrade with info on all
    introduced changes by the given downgrade.
    
    Note: this commits adds two warnings on the original upgrade procedure
    that can be ignored. Sqlalchemy warns about unknown table due to "delete
    if not exists" clauses. But the warning is invalid and the migration
    still works and subsequent migrations also work without the warnings
    once the table has been created.

--- src/ggrc/migrations/versions/20180305202849_3db5f2027c92_update_acr_table.py
+++ src/ggrc/migrations/versions/20180305202849_3db5f2027c92_update_acr_table.py
@@ -48,8 +48,21 @@ def _add_assessment_roles_tree():
   acr_propagation.propagate_roles(const.GGRC_PROPAGATION)
 
 
+def _copy_current_acl_table():
+  """Create semi temporary backup tables for old ACLs.
+
+  These backups should only exist for one release in case we would need to
+  downgrade the release to previous version.
+  """
+  op.execute("DROP TABLE IF EXISTS acl_copy")
+  op.execute("CREATE TABLE acl_copy AS SELECT * FROM access_control_list")
+  op.execute("DROP TABLE IF EXISTS acr_copy")
+  op.execute("CREATE TABLE acr_copy AS SELECT * FROM access_control_roles")
+
+
 def upgrade():
   """Upgrade database schema and/or data, creating a new revision."""
+  _copy_current_acl_table()
   _add_parent_id_column()
   _add_assessment_roles_tree()
   acr_propagation.remove_deprecated_roles([
@@ -64,6 +77,52 @@ def upgrade():
 
 def downgrade():
   """Downgrade database schema and/or data back to the previous revision."""
+
+  connection = op.get_bind()
+  ids = connection.execute("""
+      select
+        acl.id,
+        acl.object_type,
+        acl.object_id,
+        p.email,
+        acr.name
+      from acl_copy as acl
+      left join people as p on
+        acl.person_id = p.id
+      left join access_control_roles as acr on
+        acl.ac_role_id = acr.id
+      where
+        acl.parent_id is null and
+        acl.id not in (select id from access_control_list)
+  """).fetchall()
+  print "ACL entries that will be reintroduced by this downgrade:"
+  for row in ids:
+    print row
+
+  ids = connection.execute("""
+      select
+        acl.id,
+        acl.object_type,
+        acl.object_id,
+        p.email,
+        acr.name
+      from access_control_list as acl
+      left join people as p on
+        acl.person_id = p.id
+      left join access_control_roles as acr on
+        acl.ac_role_id = acr.id
+      where
+        acl.parent_id is null and
+        acl.id not in (select id from acl_copy)
+  """).fetchall()
+  print "ACL entries that will not be propagated properly by this downgrade:"
+  print "To fix these roles please manually remove and re-add them."
+  for row in ids:
+    print row
+
   for object_type, roles_tree in const.GGRC_PROPAGATION.items():
     acr_propagation.remove_propagated_roles(object_type, roles_tree.keys())
   _remove_parent_id_column()
+
+  op.execute("INSERT IGNORE INTO access_control_roles SELECT * from acr_copy")
+  op.execute("INSERT IGNORE INTO access_control_list SELECT * from acl_copy")
