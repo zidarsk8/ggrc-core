3695b99eca1f8dbd70d87911ce589de053280c27
Miha Zidar <zidarsk8@gmail.com>
2018-05-14 11:34:28 +0800

    Reformat load_access_control_list function
    These changes do nothing, but will be useful for cleaner git diff in the
    future commits.

--- src/ggrc_basic_permissions/__init__.py
+++ src/ggrc_basic_permissions/__init__.py
@@ -338,14 +338,15 @@ def load_access_control_list(user, permissions):
       acl.object_id,
       func.max(acr.read),
       func.max(acr.update),
-      func.max(acr.delete)
-  ).filter(and_(
-      all_models.AccessControlList.object_type != "Relationship",
-      all_models.AccessControlList.person_id == user.id,
-      all_models.AccessControlList.ac_role_id == acr.id)
+      func.max(acr.delete),
+  ).filter(
+      and_(
+          acl.person_id == user.id,
+          acl.ac_role_id == acr.id,
+      )
   ).group_by(
-      all_models.AccessControlList.object_id,
-      all_models.AccessControlList.object_type
+      acl.object_id,
+      acl.object_type,
   )
 
   for object_type, object_id, read, update, delete in access_control_list:
