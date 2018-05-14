f0dc22cb18d31f317e24be882bdee838d7cc8df5
Klim Shuplenkou <kshuplenkou@google.com>
2018-05-14 13:49:54 +0300

    Add "Back to top" button

--- src/ggrc-client/js/components/release-notes-list/release-notes-list.js
+++ src/ggrc-client/js/components/release-notes-list/release-notes-list.js
@@ -3,7 +3,14 @@
     Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
-import template from './release-notes.md';
+import template from './release-notes-list.mustache';
+import notesTemplate from './release-notes.md';
+
+const viewModel = can.Map.extend({
+  notesTemplate: notesTemplate,
+  onTopButtonCssClass: 'instantly-hidden',
+  prevScrollValue: null,
+});
 
 const events = {
   ['a click'](el, event) {
@@ -20,10 +27,29 @@ const events = {
       });
     }
   },
+  ['{$content} scroll'](el, ev) {
+    let prevScrollValue = this.viewModel.attr('prevScrollValue');
+    let currentTop = ev.currentTarget.scrollTop;
+    let clientHeight = ev.currentTarget.clientHeight;
+
+
+    if ((currentTop > clientHeight) && (prevScrollValue > currentTop)) {
+      this.viewModel.attr('onTopButtonCssClass', '');
+    } else {
+      this.viewModel.attr('onTopButtonCssClass', 'delayed-hidden');
+    }
+
+    this.viewModel.attr('prevScrollValue', currentTop);
+  },
+  ['.on-top-button click'](el, event) {
+    $(this.element).scrollTop(0);
+    this.viewModel.attr('onTopButtonCssClass', 'instantly-hidden');
+  },
 };
 
 export default can.Component.extend({
   tag: 'release-notes-list',
   template,
+  viewModel,
   events,
 });
