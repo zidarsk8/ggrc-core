e424124394735a7eb54ca674f0be71776a0290b1
Kate Zabelova <kzabelova@gmail.com>
2018-05-16 12:52:33 +0300

    Fix typo in the type name

--- src/ggrc/utils/__init__.py
+++ src/ggrc/utils/__init__.py
@@ -64,7 +64,7 @@ def as_json(obj, **kwargs):
 
 def service_for(obj):
   module = sys.modules['ggrc.services']
-  if isinstance(obj, (str, uncode)):  # noqa
+  if isinstance(obj, (str, unicode)):  # noqa
     model_type = obj
   else:
     model_type = obj.__class__.__name__
@@ -84,7 +84,7 @@ def url_for(obj, id=None):
 
 def view_service_for(obj):
   module = sys.modules['ggrc.views']
-  if isinstance(obj, (str, uncode)):  # noqa
+  if isinstance(obj, (str, unicode)):  # noqa
     model_type = obj
   else:
     model_type = obj.__class__.__name__
