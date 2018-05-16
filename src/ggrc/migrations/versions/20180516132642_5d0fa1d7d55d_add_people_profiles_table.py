e9ab30f6db01e6f4cd1b40f94bb830ec55966205
belovmd <belovmd@gmail.com>
2018-05-16 16:35:41 +0300

    Create people_profiles table

---
+++ src/ggrc/migrations/versions/20180516132642_5d0fa1d7d55d_add_people_profiles_table.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2018 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""
+Add people_profiles table
+
+Create Date: 2018-05-16 13:26:42.639428
+"""
+# disable Invalid constant name pylint warning for mandatory Alembic variables.
+# pylint: disable=invalid-name
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '5d0fa1d7d55d'
+down_revision = 'e6127a4956b1'
+
+
+def upgrade():
+  """Upgrade database schema and/or data, creating a new revision."""
+  op.create_table(
+      'people_profiles',
+      sa.Column('id', sa.Integer(), nullable=False),
+      sa.Column('person_id', sa.Integer(), nullable=False),
+      sa.Column('last_seen_whats_new', sa.DateTime(), nullable=False),
+      sa.ForeignKeyConstraint(['person_id'],
+                              ['people.id'],
+                              ondelete="CASCADE"),
+      sa.PrimaryKeyConstraint('id'))
+
+  op.execute("""
+      INSERT INTO `people_profiles` (`person_id`, `last_seen_whats_new`)
+      SELECT id, NOW() - INTERVAL 14 DAY
+      FROM `people`
+  """)
+
+
+def downgrade():
+  """Downgrade database schema and/or data back to the previous revision."""
+  op.execute(" TRUNCATE TABLE `people_profiles`")
+  op.drop_table('people_profiles')
