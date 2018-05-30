e80b382624d59cdf5ec462b2487a8d95b5ce1d35
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2018-05-30 19:36:27 +0300

    Fix unittests

--- test/unit/ggrc_workflows/notification/test_data_handler.py
+++ test/unit/ggrc_workflows/notification/test_data_handler.py
@@ -21,46 +21,9 @@ class GetCycleTaskUrlTestCase(unittest.TestCase):
       return_value=u"http://www.foo.com/")
   def test_generates_correct_url_for_task(self, *mocks):
     """The method should return a correct URL for the given Task."""
-    task = MagicMock()
-    task.id = 15
-    task.cycle_task_group.id = 6
-    task.cycle_task_group.cycle.id = 77
-    task.cycle_task_group.cycle.workflow.id = 9
+    result = data_handler.get_cycle_task_url()
 
-    result = data_handler.get_cycle_task_url(task)
-
-    expected_url = (
-        u"http://www.foo.com/"
-        u"workflows/9#current_widget"
-        u"/cycle/77"
-        u"/cycle_task_group/6"
-        u"/cycle_task_group_object_task/15"
-    )
-    self.assertEqual(result, expected_url)
-
-  # pylint: disable=invalid-name, unused-argument
-  @patch(
-      u"ggrc_workflows.notification.data_handler.get_url_root",
-      return_value=u"http://www.foo.com/")
-  def test_generates_correct_url_with_filter_if_given(self, *mocks):
-    """The method should return a correct URL for the given Task."""
-    task = MagicMock()
-    task.id = 15
-    task.cycle_task_group.id = 6
-    task.cycle_task_group.cycle.id = 77
-    task.cycle_task_group.cycle.workflow.id = 9
-
-    cycle_filter = u"id=77"
-
-    result = data_handler.get_cycle_task_url(task, filter_exp=cycle_filter)
-
-    expected_url = (
-        u"http://www.foo.com/"
-        u"workflows/9?filter=id%3D77#current_widget"
-        u"/cycle/77"
-        u"/cycle_task_group/6"
-        u"/cycle_task_group_object_task/15"
-    )
+    expected_url = u"http://www.foo.com/dashboard#!task_widget"
     self.assertEqual(result, expected_url)
 
 
