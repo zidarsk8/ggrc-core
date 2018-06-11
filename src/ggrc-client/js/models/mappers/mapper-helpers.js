152246a0fc11d2a9581ec746a48b1613470b355d
Yury Ostroukh <0str0uchura@gmail.com>
2018-06-11 09:37:03 +0300

    Use "import/export" syntax for GGRC.MapperHelpers utils

--- src/ggrc-client/js/models/mappers/mapper-helpers.js
+++ src/ggrc-client/js/models/mappers/mapper-helpers.js
@@ -3,54 +3,63 @@
     Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
-(function (GGRC, can) {
-  GGRC.MapperHelpers = {};
+function Proxy(optionModelName, joinOptionAttr, joinModelName,
+  joinObjectAttr, instanceJoinAttr) {
+  return new GGRC.ListLoaders.ProxyListLoader(
+    joinModelName, joinObjectAttr, joinOptionAttr,
+    instanceJoinAttr, optionModelName);
+}
 
-  GGRC.MapperHelpers.Proxy = function Proxy(
-    optionModelName, joinOptionAttr, joinModelName, joinObjectAttr,
-    instanceJoinAttr) {
-    return new GGRC.ListLoaders.ProxyListLoader(
-      joinModelName, joinObjectAttr, joinOptionAttr,
-      instanceJoinAttr, optionModelName);
-  };
+function Direct(
+  optionModelName, instanceJoinAttr, remoteJoinAttr) {
+  return new GGRC.ListLoaders.DirectListLoader(
+    optionModelName, instanceJoinAttr, remoteJoinAttr);
+}
 
-  GGRC.MapperHelpers.Direct = function Direct(
-    optionModelName, instanceJoinAttr, remoteJoinAttr) {
-    return new GGRC.ListLoaders.DirectListLoader(
-      optionModelName, instanceJoinAttr, remoteJoinAttr);
-  };
+function Indirect(instModelName, joinAttr) {
+  return new GGRC.ListLoaders.IndirectListLoader(instModelName, joinAttr);
+}
 
-  GGRC.MapperHelpers.Indirect = function Indirect(instModelName, joinAttr) {
-    return new GGRC.ListLoaders.IndirectListLoader(instModelName, joinAttr);
-  };
+function Search(queryFunction, observeTypes) {
+  return new GGRC.ListLoaders.SearchListLoader(queryFunction, observeTypes);
+}
 
-  GGRC.MapperHelpers.Search = function Search(queryFunction, observeTypes) {
-    return new GGRC.ListLoaders.SearchListLoader(queryFunction, observeTypes);
-  };
+function Multi(sources) {
+  return new GGRC.ListLoaders.MultiListLoader(sources);
+}
 
-  GGRC.MapperHelpers.Multi = function Multi(sources) {
-    return new GGRC.ListLoaders.MultiListLoader(sources);
-  };
+function TypeFilter(source, modelName) {
+  return new GGRC.ListLoaders.TypeFilteredListLoader(source, [modelName]);
+}
 
-  GGRC.MapperHelpers.TypeFilter = function TypeFilter(source, modelName) {
-    return new GGRC.ListLoaders.TypeFilteredListLoader(source, [modelName]);
-  };
+function AttrFilter(source, filterName,
+  keyword, type) {
+  return new GGRC.ListLoaders.AttrFilteredListLoader(source, filterName,
+    keyword, type);
+}
 
-  GGRC.MapperHelpers.AttrFilter = function AttrFilter(source, filterName,
-    keyword, type) {
-    return new GGRC.ListLoaders.AttrFilteredListLoader(source, filterName,
-      keyword, type);
-  };
+function CustomFilter(source, filterFn) {
+  return new GGRC.ListLoaders.CustomFilteredListLoader(source, filterFn);
+}
 
-  GGRC.MapperHelpers.CustomFilter = function CustomFilter(source, filterFn) {
-    return new GGRC.ListLoaders.CustomFilteredListLoader(source, filterFn);
-  };
+function Reify(source) {
+  return new GGRC.ListLoaders.ReifyingListLoader(source);
+}
 
-  GGRC.MapperHelpers.Reify = function Reify(source) {
-    return new GGRC.ListLoaders.ReifyingListLoader(source);
-  };
+function Cross(localMapping, remoteMapping) {
+  return new GGRC.ListLoaders.CrossListLoader(localMapping, remoteMapping);
+}
 
-  GGRC.MapperHelpers.Cross = function Cross(localMapping, remoteMapping) {
-    return new GGRC.ListLoaders.CrossListLoader(localMapping, remoteMapping);
-  };
-})(window.GGRC, window.can);
+export {
+  Proxy,
+  Direct,
+  Indirect,
+  Search,
+  Multi,
+  TypeFilter,
+  AttrFilter,
+  CustomFilter,
+  Reify,
+  Cross,
+};
+
