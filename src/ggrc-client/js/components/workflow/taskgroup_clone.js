f2d9bcd58acd66afc24fd5129eadaf338085d6a0
Yury Ostroukh <0str0uchura@gmail.com>
2018-06-11 13:12:48 +0300

    Fix eslint errors for "taskgroup_clone.js"

--- src/ggrc-client/js/components/workflow/taskgroup_clone.js
+++ src/ggrc-client/js/components/workflow/taskgroup_clone.js
@@ -9,45 +9,47 @@ let CloneTaskGroup = can.Model.Cacheable({
   defaults: {
     clone_objects: true,
     clone_tasks: true,
-    clone_people: true
-  }
+    clone_people: true,
+  },
 }, {
-  refresh: function () {
+  refresh() {
     return $.when(this);
   },
-  save: function () {
-    var task_group = new CMS.Models.TaskGroup({
+  save() {
+    const task_group = new CMS.Models.TaskGroup({
       clone: this.source_task_group.id,
       context: null,
       clone_objects: this.clone_objects,
       clone_tasks: this.clone_tasks,
-      clone_people: this.clone_people
+      clone_people: this.clone_people,
     });
 
     return task_group.save();
-  }
+  },
 });
 
 can.Component.extend({
-  tag: "task-group-clone",
-  template: "<content/>",
+  tag: 'task-group-clone',
   events: {
-    click: function (el) {
-      var $target;
-
-      $target = $('<div class="modal hide"></div>').uniqueId();
+    click(el) {
+      const $target = $('<div class="modal hide"></div>').uniqueId();
       $target.modal_form({}, el);
       import(/* webpackChunkName: "modalsCtrls" */'../../controllers/modals')
         .then(() => {
+          const contentView =
+            `${GGRC.mustache_path}/task_groups/clone_modal_content.mustache`;
           $target.ggrc_controllers_modals({
-            modal_title: "Clone Task Group",
+            modal_title: 'Clone Task Group',
             model: CloneTaskGroup,
-            instance: new CloneTaskGroup({source_task_group: this.scope.taskGroup}),
-            content_view: GGRC.mustache_path + "/task_groups/clone_modal_content.mustache",
-            custom_save_button_text: "Proceed",
-            button_view: BUTTON_VIEW_SAVE_CANCEL
+            instance: new CloneTaskGroup({
+              source_task_group:
+              this.viewModel.taskGroup
+            }),
+            content_view: contentView,
+            custom_save_button_text: 'Proceed',
+            button_view: BUTTON_VIEW_SAVE_CANCEL,
           });
         });
-    }
-  }
+    },
+  },
 });
