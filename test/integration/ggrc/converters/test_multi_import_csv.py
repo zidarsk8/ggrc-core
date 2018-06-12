d1f703ce6d71d278d8bc9cd3c4bf89c5fab4baee
Alieh Rymasheuski <alieh.rymasheuski@gmail.com>
2018-06-12 19:45:21 +0300

    Remove an excessive test
    Workflow imports are well-tested in workflows module, and this CSV is
    used as a set up data for QueryAPI tests (for now at least), therefore
    there is no sense in this just-import-file test.

--- test/integration/ggrc/converters/test_multi_import_csv.py
+++ test/integration/ggrc/converters/test_multi_import_csv.py
@@ -1,8 +1,6 @@
 # Copyright (C) 2018 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
-import unittest
-
 from sqlalchemy import and_
 from sqlalchemy import or_
 
@@ -130,9 +128,3 @@ class TestCsvImport(TestCase):
 
     self.assertEqual(get_relationships_for(p1).count(), 3)
     self.assertEqual(get_relationships_for(org1).count(), 5)
-
-  @unittest.skip("unskip when import/export fixed for workflows")
-  def test_big_import_with_mappings(self):
-    """Test big import with mappings"""
-    response = self.import_file("data_for_export_testing.csv")
-    self._check_csv_response(response, {})
