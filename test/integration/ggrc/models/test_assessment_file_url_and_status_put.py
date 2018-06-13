ef618266de26093354b04c1e776f668e3c5cb17e
Maryia Barashka <mashabarashko@gmail.com>
2018-06-13 16:27:00 +0300

    Add tests for mandatory evidence

--- test/integration/ggrc/models/test_assessment_file_url_and_status_put.py
+++ test/integration/ggrc/models/test_assessment_file_url_and_status_put.py
@@ -23,6 +23,21 @@ class TestAssessmentCompleteWithAction(ggrc.TestCase):
                                               status="In Progress")
       self.evidence = factories.EvidenceFactory()
 
+  def _prepare_mandatory_evidence_cad(self):
+    """Prepare mandatory evidence"""
+    cad = factories.CustomAttributeDefinitionFactory(
+        attribute_type="Dropdown",
+        definition_type="assessment",
+        definition_id=self.asmt.id,
+        multi_choice_options="value_1,value_2",
+        multi_choice_mandatory="0,2",
+    )
+    factories.CustomAttributeValueFactory(
+        custom_attribute=cad,
+        attributable=self.asmt,
+        attribute_value="value_2",
+    )
+
   @ddt.data(("In Review", "In Review"),
             ("Verified", "Completed"),
             ("Completed", "Completed"),
@@ -80,26 +95,16 @@ class TestAssessmentCompleteWithAction(ggrc.TestCase):
 
   @ddt.data("In Review", "Verified", "Completed")
   def test_remove_required_evidence(self, status):
-    """Test remove mandatory evidence and update {0}
-    status to completed in single PUT. Result status
+    """Test remove mandatory evidence and update
+    status to {0} in single PUT. Result status
     should be `In Progress`"""
     asmt_id = self.asmt.id
-    cad = factories.CustomAttributeDefinitionFactory(
-        attribute_type="Dropdown",
-        definition_type="assessment",
-        definition_id=self.asmt.id,
-        multi_choice_options="value_1,value_2",
-        multi_choice_mandatory="0,2",
-    )
-    factories.CustomAttributeValueFactory(
-        custom_attribute=cad,
-        attributable=self.asmt,
-        attribute_value="value_2",
-    )
-    factories.RelationshipFactory(
+    self._prepare_mandatory_evidence_cad()
+    rel = factories.RelationshipFactory(
         source=self.asmt,
         destination=self.evidence,
     )
+    rel_id = rel.id
     response = self.api.put(self.asmt, {
         "status": status,
         "actions": {"remove_related": [{"id": self.evidence.id,
@@ -112,3 +117,29 @@ class TestAssessmentCompleteWithAction(ggrc.TestCase):
                               " of items of self.custom_attribute_values")
     asmnt = all_models.Assessment.query.filter_by(id=asmt_id).first()
     self.assertEqual(asmnt.status, "In Progress")
+
+    relationship = all_models.Relationship.query.get(rel_id)
+    self.assertIsNotNone(relationship)
+
+  def test_remove_req_evid_deprecated(self):
+    """Test remove mandatory evidence and update
+    status to deprecated in single PUT. Result status
+    should be `In Progress`"""
+    asmt_id = self.asmt.id
+    self._prepare_mandatory_evidence_cad()
+    rel = factories.RelationshipFactory(
+        source=self.asmt,
+        destination=self.evidence,
+    )
+    rel_id = rel.id
+    response = self.api.put(self.asmt, {
+        "status": "Deprecated",
+        "actions": {"remove_related": [{"id": self.evidence.id,
+                                        "type": "Evidence"}]},
+    })
+    self.assertEqual(response.status_code, 200)
+    asmnt = all_models.Assessment.query.filter_by(id=asmt_id).first()
+    self.assertEqual(asmnt.status, "Deprecated")
+
+    relationship = all_models.Relationship.query.get(rel_id)
+    self.assertIsNone(relationship)
