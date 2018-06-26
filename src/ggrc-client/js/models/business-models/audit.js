f94ff056fc2f89fb5203f16b1bddfed20229fdfb
Artsiom Valchanin <volchanin.dev@gmail.com>
2018-06-26 14:13:32 +0300

    Remove unused attach-folder-button component

--- src/ggrc-client/js/models/business-models/audit.js
+++ src/ggrc-client/js/models/business-models/audit.js
@@ -3,7 +3,6 @@
  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
  */
 
-import '../../components/audit/attach-folder-button';
 import {getRole} from '../../plugins/utils/acl-utils';
 
 export default can.Model.Cacheable('CMS.Models.Audit', {
