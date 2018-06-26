07aefb14a3da0798e6c6c19b73d33519d7fd648a
Artsiom Valchanin <volchanin.dev@gmail.com>
2018-06-26 13:34:37 +0300

    Update imports

---
+++ src/ggrc-client/js/models/mappers/index.js
@@ -0,0 +1,22 @@
+/*
+  Copyright (C) 2018 Google Inc.
+  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+import './mapping-result';
+import './list-binding';
+import './base-list-loader';
+import './stub-filtered-list-loader';
+import './cross-list-loader';
+import './type-filtered-list-loader';
+import './attr-filtered-list-loader';
+import './custom-filtered-list-loader';
+import './multi-list-loader';
+import './proxy-list-loader';
+import './direct-list-loader';
+import './indirect-list-loader';
+import './search-list-loader';
+import './reifying-list-loader';
+import './mapper-helpers';
+import './mappings';
+import './mappings-ggrc';
