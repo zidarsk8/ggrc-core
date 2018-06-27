d8db5e2e50041dfacadfe734cc81d605bc9c3a16
Miha Zidar <zidarsk8@gmail.com>
2018-06-27 14:53:53 +0200

    Add background task admin role

---
+++ src/ggrc/migrations/versions/20180627104424_385ee48cd6b9_add_acr_for_background_tasks.py
@@ -0,0 +1,57 @@
+# Copyright (C) 2018 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""
+Add ACR for background tasks
+
+Create Date: 2018-06-27 10:44:24.975681
+"""
+# disable Invalid constant name pylint warning for mandatory Alembic variables.
+# pylint: disable=invalid-name
+
+import datetime
+import sqlalchemy as sa
+
+from alembic import op
+from ggrc.migrations.utils import acr_propagation
+
+
+# revision identifiers, used by Alembic.
+revision = '385ee48cd6b9'
+down_revision = '7a8eaf2c9b75'
+
+ROLE_NAME = "Admin"
+OBJECT_TYPE = "BackgroundTask"
+
+
+def upgrade():
+  op.execute(
+      acr_propagation.ACR_TABLE.insert().values(
+          name=ROLE_NAME,
+          object_type=OBJECT_TYPE,
+          parent_id=None,
+          created_at=datetime.datetime.now(),
+          updated_at=datetime.datetime.now(),
+          internal=False,
+          non_editable=True,
+          read=True,
+          update=True,
+          delete=True,
+          my_work=False,
+      )
+  )
+
+
+def downgrade():
+  condition = sa.and_(
+      acr_propagation.ACR_TABLE.c.name == ROLE_NAME,
+      acr_propagation.ACR_TABLE.c.object_type == OBJECT_TYPE,
+  )
+  op.execute(
+      acr_propagation.ACL_TABLE.delete().where(
+          acr_propagation.ACL_TABLE.c.ac_role_id.in_(
+              sa.select([acr_propagation.ACR_TABLE.c.id]).where(condition)
+          )
+      )
+  )
+  op.execute(acr_propagation.ACR_TABLE.delete().where(condition))
