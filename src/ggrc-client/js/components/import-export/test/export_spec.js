34273d9f36c4f437c64aaa7672a69ca347b6637a
Svetlana Drozdova <drozdovase@gmail.com>
2018-07-07 16:13:54 +0300

    Add tests for export component

--- src/ggrc-client/js/components/import-export/test/export_spec.js
+++ src/ggrc-client/js/components/import-export/test/export_spec.js
@@ -5,6 +5,7 @@
 
 import exportPanel from '../export-panel';
 import Component from '../export-group';
+import ExportComponent from '../export';
 import {getComponentVM} from '../../../../js_specs/spec_helpers';
 
 describe('export-group', function () {
@@ -189,5 +190,96 @@ describe('GGRC.Components.exportPanel', function () {
           .toBe(false);
       }
     );
+  });
+});
+
+describe('export component', () => {
+  describe('getObjectsForExport() method', () => {
+    let viewModel;
+
+    beforeEach(function () {
+      viewModel = getComponentVM(ExportComponent);
+    });
+
+    it('returns object with empty expression if filters are empty', () => {
+      const panelModel = new can.Map({
+        type: 'Program',
+        attributes: new can.List(),
+        localAttributes: new can.List(),
+        mappings: new can.List(),
+      });
+      const expectedObjects = [{
+        object_name: 'Program',
+        fields: [],
+        filters: {expression: {}},
+      }];
+
+      viewModel.panels.push(panelModel);
+
+      expect(viewModel.getObjectsForExport()).toEqual(expectedObjects);
+    });
+    it('returns object with simple expression if there is one filter', () => {
+      const panelModel = new can.Map({
+        type: 'Program',
+        attributes: new can.List(),
+        localAttributes: new can.List(),
+        mappings: new can.List(),
+        relevant: [{
+          filter: {id: 2},
+          model_name: 'Program',
+        }],
+      });
+      const expectedObjects = [{
+        object_name: 'Program',
+        fields: [],
+        filters: {expression: {
+          object_name: 'Program',
+          op: {name: 'relevant'},
+          ids: ['2'],
+        }},
+      }];
+
+      viewModel.panels.push(panelModel);
+
+      expect(viewModel.getObjectsForExport()).toEqual(expectedObjects);
+    });
+    it('returns object with OR in expression ' +
+      'if the second filter has OR operator', () => {
+      const panelModel = new can.Map({
+        type: 'Program',
+        attributes: new can.List(),
+        localAttributes: new can.List(),
+        mappings: new can.List(),
+        relevant: [{
+          filter: {id: 2},
+          model_name: 'Program',
+        }, {
+          filter: {id: 3},
+          model_name: 'Audit',
+          operator: 'OR',
+        }],
+      });
+      const expectedObjects = [{
+        object_name: 'Program',
+        fields: [],
+        filters: {expression: {
+          left: {
+            object_name: 'Program',
+            op: {name: 'relevant'},
+            ids: ['2'],
+          },
+          op: {name: 'OR'},
+          right: {
+            object_name: 'Audit',
+            op: {name: 'relevant'},
+            ids: ['3'],
+          },
+        }},
+      }];
+
+      viewModel.panels.push(panelModel);
+
+      expect(viewModel.getObjectsForExport()).toEqual(expectedObjects);
+    });
   });
 });
