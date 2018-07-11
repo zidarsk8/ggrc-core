7210a9c00266ecc0e75631f255e88cc7e01f8c9b
Artsiom Valchanin <volchanin.dev@gmail.com>
2018-07-11 16:12:02 +0300

    Add additional unit-tests

--- src/ggrc-client/js/components/approval-link/tests/approval-link_spec.js
+++ src/ggrc-client/js/components/approval-link/tests/approval-link_spec.js
@@ -7,6 +7,8 @@ import Component from '../approval-link';
 import {getComponentVM} from '../../../../js_specs/spec_helpers';
 import Permission from '../../../permission';
 import * as aclUtils from '../../../plugins/utils/acl-utils';
+import * as queryApiUtils from '../../../plugins/utils/query-api-utils';
+import {REFRESH_APPROVAL} from '../../../events/eventTypes';
 
 describe('approval-link component', ()=> {
   let viewModel;
@@ -15,80 +17,194 @@ describe('approval-link component', ()=> {
     viewModel = getComponentVM(Component);
   });
 
-  describe('isReviewer get()', () => {
-    let assigneeRole;
+  describe('viewModel', () => {
+    describe('isReviewer get()', () => {
+      let assigneeRole;
 
-    beforeAll(() => {
-      assigneeRole = {
-        id: -1,
-      };
-      spyOn(aclUtils, 'getRole').and.returnValue(assigneeRole);
-    });
-
-    it('returns false when reviewTask is not set', () => {
-      viewModel.attr('reviewTask', null);
-      spyOn(Permission, 'is_allowed').and.returnValue(false);
-
-      let result = viewModel.attr('isReviewer');
-
-      expect(result).toBe(false);
-    });
-
-    it('returns true when user is Admin', () => {
-      viewModel.attr('reviewTask', {});
-      spyOn(Permission, 'is_allowed').and.returnValue(true);
-
-      let result = viewModel.attr('isReviewer');
-
-      expect(result).toBe(true);
-    });
-
-    it('returns false when ACL person does not match current user', () => {
-      viewModel.attr('reviewTask', {
-        access_control_list: {
-          ac_role_id: assigneeRole.id,
-          person: {
-            id: 12345,
-          },
-        },
+      beforeAll(() => {
+        assigneeRole = {
+          id: -1,
+        };
+        spyOn(aclUtils, 'getRole').and.returnValue(assigneeRole);
       });
-      spyOn(Permission, 'is_allowed').and.returnValue(false);
 
-      let result = viewModel.attr('isReviewer');
+      it('returns false when reviewTask is not set', () => {
+        viewModel.attr('reviewTask', null);
+        spyOn(Permission, 'is_allowed').and.returnValue(false);
 
-      expect(result).toBe(false);
+        let result = viewModel.attr('isReviewer');
+
+        expect(result).toBe(false);
+      });
+
+      it('returns true when user is Admin', () => {
+        viewModel.attr('reviewTask', {});
+        spyOn(Permission, 'is_allowed').and.returnValue(true);
+
+        let result = viewModel.attr('isReviewer');
+
+        expect(result).toBe(true);
+      });
+
+      it('returns false when ACL person does not match current user', () => {
+        viewModel.attr('reviewTask', {
+          access_control_list: {
+            ac_role_id: assigneeRole.id,
+            person: {
+              id: 12345,
+            },
+          },
+        });
+        spyOn(Permission, 'is_allowed').and.returnValue(false);
+
+        let result = viewModel.attr('isReviewer');
+
+        expect(result).toBe(false);
+      });
+
+      it('returns false when ACL role non-Task Assignees for user', () => {
+        viewModel.attr('reviewTask', {
+          access_control_list: {
+            ac_role_id: 12,
+            person: {
+              id: 12345,
+            },
+          },
+        });
+        spyOn(Permission, 'is_allowed').and.returnValue(false);
+
+        let result = viewModel.attr('isReviewer');
+
+        expect(result).toBe(false);
+      });
+
+      it('returns true when ACL person role matches current user', () => {
+        viewModel.attr('reviewTask', {
+          access_control_list: {
+            ac_role_id: assigneeRole.id,
+            person: {
+              id: 1,
+            },
+          },
+        });
+        spyOn(Permission, 'is_allowed').and.returnValue(false);
+
+        let result = viewModel.attr('isReviewer');
+
+        expect(result).toBe(true);
+      });
     });
 
-    it('returns false when ACL role non-Task Assignees for user', () => {
-      viewModel.attr('reviewTask', {
-        access_control_list: {
-          ac_role_id: 12,
-          person: {
-            id: 12345,
+    describe('loadReviewTask() method', () => {
+      let requestDfd;
+      let response;
+      beforeEach(() => {
+        requestDfd = can.Deferred();
+        response = {
+          CycleTaskGroupObjectTask: {
+            values: [],
           },
-        },
+        };
+        spyOn(queryApiUtils, 'batchRequests').and.returnValue(requestDfd);
+        spyOn(queryApiUtils, 'buildParam');
+        viewModel.attr('instance', {});
       });
-      spyOn(Permission, 'is_allowed').and.returnValue(false);
 
-      let result = viewModel.attr('isReviewer');
+      it('builds correct queryApi request', () => {
+        viewModel.attr('instance', {
+          type: 'testType',
+          id: 'testId',
+        });
+        viewModel.loadReviewTask();
 
-      expect(result).toBe(false);
+        expect(queryApiUtils.buildParam).toHaveBeenCalledWith(
+          'CycleTaskGroupObjectTask', {
+            current: 1,
+            pageSize: 1,
+          }, {
+            type: 'testType',
+            id: 'testId',
+          },
+          null, {
+            expression: {
+              left: 'object_approval',
+              op: {name: '='},
+              right: 'true',
+            },
+          });
+      });
+
+      it('sets "isInitializing" flag to true', () => {
+        viewModel.attr('isInitializing', false);
+
+        viewModel.loadReviewTask();
+
+        let result = viewModel.attr('isInitializing');
+        expect(result).toBe(true);
+      });
+
+      it('sets "isInitializing" flag to false in the end', (done) => {
+        viewModel.attr('isInitializing', true);
+
+        viewModel.loadReviewTask();
+
+        requestDfd.resolve(response).then(() => {
+          let result = viewModel.attr('isInitializing');
+          expect(result).toBe(false);
+          done();
+        });
+      });
+
+      it('initializes "reviewTask" field', (done) => {
+        let task = {
+          id: 1,
+          type: 'CycleTaskGroupObjectTask',
+          isOverdue: false,
+          access_control_list: [],
+        };
+        response.CycleTaskGroupObjectTask.values = [task];
+        viewModel.loadReviewTask();
+
+        requestDfd.resolve(response).then(() => {
+          let result = viewModel.attr('reviewTask').serialize();
+          expect(result).toEqual(task);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('events', () => {
+    describe('"inserted" event', () => {
+      let event;
+      beforeEach(() => {
+        event = Component.prototype.events['inserted'].bind({viewModel});
+      });
+
+      it('loads review task', () => {
+        spyOn(viewModel, 'loadReviewTask');
+
+        event();
+
+        expect(viewModel.loadReviewTask).toHaveBeenCalled();
+      });
     });
 
-    it('returns true when ACL person role matches current user', () => {
-      viewModel.attr('reviewTask', {
-        access_control_list: {
-          ac_role_id: assigneeRole.id,
-          person: {
-            id: 1,
-          },
-        },
+    describe('"REFRESH_APPROVAL" instance event', () => {
+      let event;
+      beforeEach(() => {
+        event = Component.prototype
+          .events[`{viewModel.instance} ${REFRESH_APPROVAL.type}`]
+          .bind({viewModel});
       });
-      spyOn(Permission, 'is_allowed').and.returnValue(false);
 
-      let result = viewModel.attr('isReviewer');
+      it('loads review task', () => {
+        spyOn(viewModel, 'loadReviewTask');
 
-      expect(result).toBe(true);
+        event();
+
+        expect(viewModel.loadReviewTask).toHaveBeenCalled();
+      });
     });
   });
 });
