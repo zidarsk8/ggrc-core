6256de0a8e277d0ace23227d0456acb945d92d0b
Artsiom Valchanin <volchanin.dev@gmail.com>
2018-07-13 17:26:36 +0300

    Use common error message in GCA inline edit

--- src/ggrc-client/js/components/global-custom-attributes/global-custom-attributes.js
+++ src/ggrc-client/js/components/global-custom-attributes/global-custom-attributes.js
@@ -70,10 +70,8 @@ import Permission from '../../permission';
               success: 'Saved',
             });
           })
-          .fail(function () {
-            $(document.body).trigger('ajax:flash', {
-              error: 'There was a problem saving',
-            });
+          .fail(function (instance, xhr) {
+            GGRC.Errors.notifierXHR('error')(xhr);
           })
           .always(function () {
             this.attr('isSaving', false);
