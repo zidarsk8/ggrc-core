0c1820e96306d90bbc224ccad27d5612503fd681
Yury Ostroukh <0str0uchura@gmail.com>
2018-07-17 08:57:57 +0300

    Cleanup quick_form_controller

--- src/ggrc-client/js/controllers/modals/quick_form_controller.js
+++ src/ggrc-client/js/controllers/modals/quick_form_controller.js
@@ -18,93 +18,12 @@ export default ModalsController({
       this.options.model = this.options.instance.constructor;
     }
     this.options.$content = this.element;
-    if (this.element.data('force-refresh')) {
-      this.options.instance.refresh();
-    }
-  },
-  'input, textarea, select change': function (el) {
-    let self = this;
-    let val;
-    let prop;
-    let oldValue;
-    if (el.data('toggle') === 'datepicker') {
-      val = el.datepicker('getDate');
-      prop = el.attr('name');
-      oldValue = this.options.instance.attr(prop);
-
-      if (moment(val).isSame(oldValue)) {
-        return;
-      }
-      can.when(this.options.instance.refresh()).then(function () {
-        self.options.instance.attr(prop, val);
-        self.options.instance.save();
-      });
-      return;
-    }
-    if (!el.is('[data-lookup]')) {
-      this.set_value_from_element(el);
-      setTimeout(function () {
-        this.options.instance.save();
-      }.bind(this), 100);
-    }
-  },
-  autocomplete_select: function (el, event, ui) {
-    let self = this;
-    let prop = el.attr('name').split('.').slice(0, -1).join('.');
-    if (this._super(...arguments) !== false) {
-      setTimeout(function () {
-        self.options.instance.save().then(function () {
-          let obj = self.options.instance.attr(prop);
-          if (obj.attr) {
-            obj.attr('saved', true);
-          }
-        });
-      }, 100);
-    } else {
-      return false;
-    }
-  },
-  'input, select, textarea click': function (el, ev) {
-    if (el.data('toggle') === 'datepicker') {
-      return;
-    }
-    if (this._super) {
-      this._super(...arguments);
-    }
-    ev.stopPropagation();
-  },
-  '.dropdown-menu > li click': function (el, ev) {
-    let self = this;
-    ev.stopPropagation();
-    this.set_value({name: el.data('name'), value: el.data('value')});
-    setTimeout(function () {
-      self.options.instance.save();
-      $(el).closest('.open').removeClass('open');
-    }, 100);
   },
   'button[data-name][data-value]:not(.disabled), a.undoable[data-toggle*=modal] click': function (el, ev) {
     let self = this;
     let name = el.data('name');
     let oldValue = {};
-    let action;
-    let openclose;
-    let isOpened;
     const instance = self.options.instance;
-
-    if (el.data('openclose')) {
-      action = el.data('openclose');
-      openclose = el.closest('.item-main').find('.openclose');
-      isOpened = openclose.hasClass('active');
-
-      // We can't use main.openclose(action) here because content may not be loaded yet
-      if (action === 'trigger') {
-        openclose.trigger('click');
-      } else if (action === 'close' && isOpened) {
-        openclose.trigger('click');
-      } else if (action === 'open' && !isOpened) {
-        openclose.trigger('click');
-      }
-    }
 
     oldValue[name] = instance.attr(name);
     if (el.data('also-undo')) {
@@ -145,22 +64,5 @@ export default ModalsController({
           instance.attr('_disabled', '');
         });
     }
-  },
-  'a.undo click': function (el, ev) {
-    let self = this;
-    let newValue = this.options.instance.attr('_undo').shift();
-    ev.stopPropagation();
-
-    this.options.instance.attr('_disabled', 'disabled');
-    this.options.instance
-      .refresh()
-      .then(function (instance) {
-        can.each(newValue, function (value, name) {
-          self.set_value({name: name, value: value});
-        });
-        return instance.save();
-      }).then(function () {
-        self.options.instance.attr('_disabled', '');
-      });
   },
 });
