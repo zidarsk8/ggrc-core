0ebdbf87bd02d896e712f1c3f3f761d095611e3a
Yury Ostroukh <0str0uchura@gmail.com>
2018-07-17 12:41:04 +0300

    Remove deprecated "undo" logic

--- src/ggrc-client/js/controllers/modals/quick_form_controller.js
+++ src/ggrc-client/js/controllers/modals/quick_form_controller.js
@@ -19,50 +19,28 @@ export default ModalsController({
     }
     this.options.$content = this.element;
   },
-  'button[data-name][data-value]:not(.disabled), a.undoable[data-toggle*=modal] click': function (el, ev) {
+  'button[data-name][data-value]:not(.disabled)': function (el, ev) {
     let self = this;
-    let name = el.data('name');
-    let oldValue = {};
     const instance = self.options.instance;
 
-    oldValue[name] = instance.attr(name);
-    if (el.data('also-undo')) {
-      can.each(el.data('also-undo').split(','), function (attrname) {
-        attrname = attrname.trim();
-        oldValue[attrname] = instance.attr(attrname);
+    ev.stopPropagation();
+
+    instance.attr('_disabled', 'disabled');
+    instance
+      .refresh()
+      .then(function (instance) {
+        self.set_value({name: el.data('name'), value: el.data('value')});
+        return instance.save();
+      })
+      .then(function () {
+        const modelType = el.data('objectType');
+        const id = el.data('objectId');
+        if (modelType && !isNull(id) && !isUndefined(id)) {
+          return CMS.Models[modelType].findOne({id});
+        }
+      })
+      .then(function () {
+        instance.attr('_disabled', '');
       });
-    }
-
-    // Check if the undo button was clicked:
-    instance.attr('_undo') || instance.attr('_undo', []);
-
-    if (el.is('[data-toggle*=modal')) {
-      setTimeout(function () {
-        $('.modal:visible').one('modal:success', function () {
-          instance.attr('_undo').unshift(oldValue);
-        });
-      }, 100);
-    } else {
-      ev.stopPropagation();
-      instance.attr('_undo').unshift(oldValue);
-
-      instance.attr('_disabled', 'disabled');
-      instance
-        .refresh()
-        .then(function (instance) {
-          self.set_value({name: el.data('name'), value: el.data('value')});
-          return instance.save();
-        })
-        .then(function () {
-          const modelType = el.data('objectType');
-          const id = el.data('objectId');
-          if (modelType && !isNull(id) && !isUndefined(id)) {
-            return CMS.Models[modelType].findOne({id});
-          }
-        })
-        .then(function () {
-          instance.attr('_disabled', '');
-        });
-    }
   },
 });
