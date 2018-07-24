ab1e6f5ba70c02ae00a16624de1d6777f6796e1e
pavelglebov <pg.incloud@gmail.com>
2018-07-24 18:35:09 +0300

    Add user-utils

---
+++ src/ggrc-client/js/plugins/utils/user-utils.js
@@ -0,0 +1,42 @@
+/*
+ Copyright (C) 2018 Google Inc.
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
+let profilePromise;
+
+function loadUserProfile() {
+  if (typeof profilePromise === 'undefined') {
+    profilePromise = can.ajax({
+      type: 'GET',
+      headers: $.extend({
+        'Content-Type': 'application/json',
+      }, {}),
+      url: '/api/people/' + GGRC.current_user.id + '/profile',
+    });
+  }
+
+  return profilePromise;
+}
+
+function updateUserProfile(profile) {
+  let result = can.ajax({
+    type: 'PUT',
+    headers: $.extend({
+      'Content-Type': 'application/json',
+    }, {}),
+    url: '/api/people/' + GGRC.current_user.id + '/profile',
+    data: profile,
+  });
+
+  result.then(() => {
+    profilePromise = undefined;
+  });
+
+  return result;
+}
+
+export {
+  loadUserProfile,
+  updateUserProfile,
+};
