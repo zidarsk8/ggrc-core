89ca84b0316e366cfa0337be02dd4bd0a67297cb
Aleh Natseuski <natsevsky@gmail.com>
2018-08-01 11:34:44 +0300

    Add tests

---
+++ test/integration/ggrc/converters/test_audit_csv_template.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2018 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""Tests generation csv template for audits."""
+
+from integration.ggrc import TestCase
+
+
+class TestAuditCSVTemplate(TestCase):
+  """Tests generation csv template for audits."""
+
+  def setUp(self):
+    """Set up for test cases."""
+    super(TestAuditCSVTemplate, self).setUp()
+    self.client.get("/login")
+
+  def test_exclude_evidence_file(self):
+    """Test exclude evidence file field from csv template"""
+    objects = [{"object_name": "Audit"}]
+    response = self.export_csv_template(objects)
+    self.assertNotIn("Evidence File", response.data)
