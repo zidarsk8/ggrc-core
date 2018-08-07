f984aba6173e93b4e0a58b4e15cdd2cf39c38b20
Klim Shuplenkou <kshuplenkou@google.com>
2018-08-07 16:58:59 +0300

    Add connection-lost-notifier

---
+++ src/ggrc-client/js/components/import-export/connection-lost-notifier.js
@@ -0,0 +1,14 @@
+/*
+    Copyright (C) 2018 Google Inc.
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+import {notifier} from '../../plugins/utils/notifiers-utils';
+
+export const connectionLostNotifier = () => {
+  const message = `Internet connection was lost. You may close this page or
+  continue your work. We will send you an email notification when it completes
+  or if there are errors or warnings.`;
+
+  notifier('error', message);
+};
