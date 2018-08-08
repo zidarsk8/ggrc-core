bf51d705268b6fd0e89b7846179a5106b787eca4
Artsiom Valchanin <volchanin.dev@gmail.com>
2018-08-08 11:54:28 +0300

    Remove 'children' attribute

--- src/ggrc-client/js/models/business-models/clause.js
+++ src/ggrc-client/js/models/business-models/clause.js
@@ -37,7 +37,6 @@ export default Cacheable('CMS.Models.Clause', {
     context: 'CMS.Models.Context.stub',
     modified_by: 'CMS.Models.Person.stub',
     object_people: 'CMS.Models.ObjectPerson.stubs',
-    children: 'CMS.Models.get_stubs',
     directive_sections: 'CMS.Models.DirectiveSection.stubs',
     directives: 'CMS.Models.get_stubs',
   },
--- src/ggrc-client/js/models/business-models/requirement.js
+++ src/ggrc-client/js/models/business-models/requirement.js
@@ -37,7 +37,6 @@ export default Cacheable('CMS.Models.Requirement', {
     context: 'CMS.Models.Context.stub',
     modified_by: 'CMS.Models.Person.stub',
     object_people: 'CMS.Models.ObjectPerson.stubs',
-    children: 'CMS.Models.get_stubs',
     directive_requirements: 'CMS.Models.DirectiveRequirement.stubs',
     directives: 'CMS.Models.get_stubs',
   },
