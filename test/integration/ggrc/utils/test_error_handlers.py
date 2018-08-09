f71bb68656ae924134ab5b70bb01fc52dd41468b
Kate Zabelova <kzabelova@gmail.com>
2018-08-09 19:20:58 +0300

    Update invalid type error message to more informative

--- test/integration/ggrc/utils/test_error_handlers.py
+++ test/integration/ggrc/utils/test_error_handlers.py
@@ -10,4 +10,6 @@ class TestErrorHandlers(TestCase):
 
   def test_non_flask_error_handling(self):
     response = self.import_file("not_a_csv.txt", safe=False)
-    self.assertEqual(response["message"], "Invalid file type.")
+    self.assertEqual(response["message"],
+                     "Line 0: Wrong file type. Only .csv files are supported."
+                     " Please upload a .csv file.")
