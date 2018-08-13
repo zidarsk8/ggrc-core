c2a189837300437300051b882ada3b2c0356ec89
Klim Shuplenkou <kshuplenkou@google.com>
2018-08-13 16:06:33 +0300

    Do not use regular expressions to match header

--- loaders/parse-inner-links.js
+++ loaders/parse-inner-links.js
@@ -16,16 +16,16 @@ module.exports = function(html) {
   linkMatches.forEach((match, index) => {
     let hash = (Date.now() * Math.random()).toFixed();
     let innerText = match[2];
-    let headerRegexpStr = `<h3>${innerText}<\/h3>`;
-    let linkRegexpStr = `<a href="${match[1]}">${innerText}<\/a>`;
+    let headerStr = `<h3>${innerText}<\/h3>`;
+    let linkStr = `<a href="${match[1]}">${innerText}<\/a>`;
 
-    if (html.match(headerRegexpStr)) {
+    if (html.indexOf(headerStr) > -1) {
       // set hash as id for header
-      html = html.replace(headerRegexpStr,
+      html = html.replace(headerStr,
         `<h3 id="${hash}">${innerText}<\/h3>`);
 
       // set hash as href for link
-      html = html.replace(linkRegexpStr,
+      html = html.replace(linkStr,
         `<a href="#${hash}">${innerText}<\/a>`);
     }
   });
