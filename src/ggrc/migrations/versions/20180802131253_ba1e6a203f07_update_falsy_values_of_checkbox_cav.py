b5f08d4bfd77a2dfb9592d3c65f726eca8d264cf
Kate Zabelova <kzabelova@gmail.com>
2018-08-13 13:49:09 +0300

    Add integration test for old revisions filtering

--- src/ggrc/migrations/versions/20180802131253_ba1e6a203f07_update_falsy_values_of_checkbox_cav.py
+++ src/ggrc/migrations/versions/20180802131253_ba1e6a203f07_update_falsy_values_of_checkbox_cav.py
@@ -9,6 +9,7 @@ Create Date: 2018-08-02 13:12:53.218878
 # disable Invalid constant name pylint warning for mandatory Alembic variables.
 # pylint: disable=invalid-name
 
+from datetime import datetime
 from alembic import op
 from sqlalchemy.sql import text
 from ggrc.migrations.utils.migrator import get_migration_user_id
@@ -22,6 +23,7 @@ def upgrade():
   """Upgrade database schema and/or data, creating a new revision."""
   connection = op.get_bind()
   migration_user_id = get_migration_user_id(connection)
+  current_datetime = datetime.now()
 
   op.execute("SET SESSION SQL_SAFE_UPDATES = 0")
 
@@ -29,11 +31,14 @@ def upgrade():
       UPDATE custom_attribute_values AS cav
         JOIN custom_attribute_definitions AS cad
           ON cad.id = cav.custom_attribute_id
-      SET cav.attribute_value = 0
+      SET cav.attribute_value = 0,
+          cav.modified_by_id = :modified_by_id,
+          cav.updated_at = :current_datetime
       WHERE cad.attribute_type = "Checkbox" and cav.attribute_value = "";
   """
   connection.execute(text(update_sql),
-                     modified_by_id=migration_user_id)
+                     modified_by_id=migration_user_id,
+                     current_datetime=current_datetime)
 
 
 def downgrade():
