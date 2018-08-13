fc5409e4f86834f5dbfaef4f996d17aa0be6a7b8
Artsiom Valchanin <volchanin.dev@gmail.com>
2018-08-13 12:50:41 +0300

    Cover Stub model by unit-tests

---
+++ src/ggrc-client/js/models/tests/stub_spec.js
@@ -0,0 +1,126 @@
+/*
+  Copyright (C) 2018 Google Inc.
+  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+import Stub from '../stub';
+
+describe('Stub model', () => {
+  let TestModel;
+  beforeAll(() => {
+    TestModel = can.Model.extend({shortName: 'testModel'}, {});
+  });
+
+  it('Creates correct stub for single model', () => {
+    let model = new TestModel({
+      prop1: 'val1',
+      id: '123',
+      prop2: 'val2',
+      href: 'href',
+    });
+
+    let stub = new Stub(model);
+
+    expect(stub.serialize()).toEqual({
+      type: 'testModel',
+      id: '123',
+      href: 'href',
+    });
+  });
+
+  it('Creates correct stub for single object', () => {
+    let obj = {
+      type: 'test',
+      prop1: 'val1',
+      id: '123',
+      prop2: 'val2',
+      selfLink: 'selfLink',
+    };
+
+    let stub = new Stub(obj);
+
+    expect(stub.serialize()).toEqual({
+      type: 'test',
+      id: '123',
+      href: 'selfLink',
+    });
+  });
+
+  it('Creates instance of Stub', () => {
+    let obj = {
+      id: '123',
+      type: 'test',
+    };
+
+    let stub = new Stub(obj);
+
+    expect(stub instanceof Stub).toBe(true);
+  });
+
+  describe('List model', () => {
+    it('Creates correct stub for list of models', () => {
+      let models = [
+        new TestModel({
+          prop1: 'val1',
+          id: '123',
+          prop2: 'val2',
+          href: 'href',
+        }),
+        new TestModel({
+          prop1: 'val1-2',
+          id: '123-2',
+          prop2: 'val2',
+          href: 'href-2',
+        }),
+      ];
+
+      let stub = new Stub.List(models);
+
+      expect(stub.serialize()).toEqual([{
+        type: 'testModel',
+        id: '123',
+        href: 'href',
+      }, {
+        type: 'testModel',
+        id: '123-2',
+        href: 'href-2',
+      }]);
+    });
+
+    it('Creates correct stub for list of objects', () => {
+      let objs = [{
+        type: 'test',
+        prop1: 'val1',
+        id: '123',
+        prop2: 'val2',
+        selfLink: 'selfLink',
+      }, {
+        type: 'test',
+        prop1: 'val1-2',
+        id: '123-2',
+        prop2: 'val2-2',
+        selfLink: 'selfLink-2',
+      }];
+
+      let stub = new Stub.List(objs);
+
+      expect(stub.serialize()).toEqual([{
+        type: 'test',
+        id: '123',
+        href: 'selfLink',
+      }, {
+        type: 'test',
+        id: '123-2',
+        href: 'selfLink-2',
+      }]);
+    });
+
+    it('Creates instance of Stub.List', () => {
+      let objs = [];
+
+      let stub = new Stub.List(objs);
+
+      expect(stub instanceof Stub.List).toBe(true);
+    });
+  });
+});
