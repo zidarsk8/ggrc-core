359b9d3b67a9f8293758e6c2a547f18a08759b7b
Svetlana Drozdova <drozdovase@gmail.com>
2018-08-14 14:57:10 +0300

    Add Mark Reviewed button

--- src/ggrc-client/js/components/object-review/object-review.js
+++ src/ggrc-client/js/components/object-review/object-review.js
@@ -5,6 +5,12 @@
 
 import template from './templates/object-review.mustache';
 import Review from '../../models/service-models/review';
+import Permission from '../../permission';
+import {isSnapshot} from '../../plugins/utils/snapshot-utils';
+import {createReviewInstance, saveReview} from '../../plugins/utils/object-review-utils';
+import {REFRESH_MAPPING, DESTINATION_UNMAPPED} from '../../events/eventTypes';
+import {getRole} from '../../plugins/utils/acl-utils';
+
 const tag = 'object-review';
 
 export default can.Component.extend({
@@ -20,28 +26,100 @@ export default can.Component.extend({
           return status.toLowerCase();
         },
       },
+      isReviewed: {
+        get() {
+          return this.attr('reviewStatus') === 'reviewed';
+        },
+      },
+      showButtons: {
+        get() {
+          return !this.attr('isReviewed') &&
+            !isSnapshot(this.attr('instance')) &&
+            this.attr('hasUpdatePermission');
+        },
+      },
+      hasUpdatePermission: {
+        get() {
+          const instance = this.attr('review') || this.attr('instance');
+
+          return Permission.is_allowed_for('update', instance);
+        },
+      },
     },
     instance: {},
     review: null,
     loading: false,
-    initReview() {
-      let review = this.attr('instance.review');
+    loadReview() {
+      const review = this.attr('instance.review');
 
-      if (!review) {
-        return;
+      if (review) {
+        this.attr('loading', true);
+
+        new Review(review)
+          .refresh()
+          .then((reviewInstance) => {
+            this.attr('review', reviewInstance);
+          })
+          .always(() => {
+            this.attr('loading', false);
+          });
       }
-
-      this.refreshReview(new Review(review));
     },
-    refreshReview(review) {
-      review.refresh().then((reviewInstance) => {
-        this.attr('review', reviewInstance);
+    getReviewInstance() {
+      const review = this.attr('review');
+
+      return review || createReviewInstance(this.attr('instance'));
+    },
+    markReviewed() {
+      const review = this.getReviewInstance();
+
+      this.updateAccessControlList(review);
+      this.changeReviewState(review, 'Reviewed');
+    },
+    changeReviewState(review, status) {
+      review.attr('status', status);
+      this.attr('loading', true);
+
+      return this.updateReview(review).then(() => {
+        this.attr('loading', false);
       });
+    },
+    updateReview(review) {
+      return saveReview(review, this.attr('instance'))
+        .then((reviewInstance) => {
+          this.attr('review', reviewInstance);
+        });
+    },
+    updateAccessControlList(review) {
+      const acl = review.attr('access_control_list');
+      const isCurrentUserReviewer = !!_.find(acl, (item) =>
+        item.person.id === GGRC.current_user.id);
+
+      if (!isCurrentUserReviewer) {
+        const reviewerRole = getRole('Review', 'Reviewer');
+
+        acl.push({
+          ac_role_id: reviewerRole.id,
+          person: {type: 'Person', id: GGRC.current_user.id},
+        });
+      }
     },
   },
   events: {
     inserted() {
-      this.viewModel.initReview();
+      this.viewModel.loadReview();
+    },
+    '{viewModel.instance} modelAfterSave'() {
+      this.viewModel.loadReview();
+    },
+    [`{viewModel.instance} ${REFRESH_MAPPING.type}`](instance, event) {
+      // check destinationType because REFRESH_MAPPING is also dispatched on modal 'hide'
+      if (event.destinationType) {
+        this.viewModel.loadReview();
+      }
+    },
+    [`{viewModel.instance} ${DESTINATION_UNMAPPED.type}`]() {
+      this.viewModel.loadReview();
     },
   },
 });
