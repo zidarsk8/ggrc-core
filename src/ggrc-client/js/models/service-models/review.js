4e4deeef3aff1dc763e32ba02484aa473c0b7da4
Svetlana Drozdova <drozdovase@gmail.com>
2018-08-14 14:18:33 +0300

    Add Review model

---
+++ src/ggrc-client/js/models/service-models/review.js
@@ -0,0 +1,25 @@
+/*
+ Copyright (C) 2018 Google Inc.
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
+import Cacheable from '../cacheable';
+import Stub from '../stub';
+
+export default Cacheable('CMS.Models.Review', {
+  root_object: 'review',
+  root_collection: 'reviews',
+  table_singular: 'review',
+  table_plural: 'reviews',
+
+  category: 'governance',
+  findOne: 'GET /api/reviews/{id}',
+  findAll: 'GET /api/reviews',
+  update: 'PUT /api/reviews/{id}',
+  destroy: 'DELETE /api/reviews/{id}',
+  create: 'POST /api/reviews',
+  attributes: {
+    reviewable: Stub,
+    last_reviewed_at: 'datetime',
+  },
+}, {});
