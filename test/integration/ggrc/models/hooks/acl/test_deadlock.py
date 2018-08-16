03e11db5ec44caa8c9534163293dbb5617f3460a
Alieh Rymasheuski <alieh.rymasheuski@gmail.com>
2018-08-16 18:25:30 +0300

    Separate SELECT and INSERT on ACL propagation
    INSERT INTO a SELECT FROM b puts read locks on b, which leads to
    deadlocks when the same statement is executed 3 times in a row by each
    parallel job with slight variations.
    
    SELECT, COMMIT and then INSERT effectively reduces the transaction
    isolation, because SELECTed data that stays in the app may go obsolete
    right after the new transaction is opened. For the case of ACL
    propagation this is good enough, because any change in the data isn't
    going to affect the result of the INSERT. The only possible issue is a
    race condition: req1/tx1 SELECTs data based on an ACL entry, parallel
    req2/tx1 drops the ACL entry, and req1/tx2 tries to INSERT a row that
    will violate FK constraint.

---
+++ test/integration/ggrc/models/hooks/acl/test_deadlock.py
@@ -0,0 +1,81 @@
+# Copyright (C) 2018 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""Test deadlocks on parallel propagation requests."""
+
+import flask
+import mock
+
+from ggrc import db
+from ggrc.models import all_models
+from ggrc.models.hooks.acl import propagation
+from integration.ggrc import TestCase
+from integration.ggrc.models import factories
+
+
+class TestDeadlocks(TestCase):
+  """Base test case for all propagation scenarios."""
+
+  def setUp(self):
+    super(TestDeadlocks, self).setUp()
+    pm_role = all_models.AccessControlRole.query.filter_by(
+        object_type="Program",
+        name="Program Managers",
+    ).one()
+    with factories.single_commit():
+      self.person = factories.PersonFactory()
+      self.program = factories.ProgramFactory()
+      self.audits = [factories.AuditFactory(program=self.program)
+                     for _ in range(3)]
+      self.rels = [
+          factories.RelationshipFactory(source=self.program, destination=audit)
+          for audit in self.audits
+      ]
+
+      self.parent_acl = factories.AccessControlListFactory(
+          ac_role=pm_role,
+          person=self.person,
+          object=self.program,
+      )
+    db.session.execute(
+        "DELETE FROM access_control_list WHERE parent_id IS NOT NULL",
+    )
+
+  def test_deadlock(self):
+    """No deadlocks occur if propagation for three Audits goes in parallel."""
+
+    new_conn = db.engine.begin
+
+    # Emulate connections from three parallel requests
+    with new_conn() as conn0, new_conn() as conn1, new_conn() as conn2:
+
+      with mock.patch("ggrc.access_control.utils.db.session.execute",
+                      conn0.execute):
+        flask.g.new_acl_ids = {}
+        flask.g.new_relationship_ids = {self.rels[0].id}
+        flask.g.deleted_objects = {}
+
+        propagation.propagate()
+
+      with mock.patch("ggrc.access_control.utils.db.session.execute",
+                      conn1.execute):
+        flask.g.new_acl_ids = {}
+        flask.g.new_relationship_ids = {self.rels[1].id}
+        flask.g.deleted_objects = {}
+
+        propagation.propagate()
+
+      with mock.patch("ggrc.access_control.utils.db.session.execute",
+                      conn2.execute):
+        flask.g.new_acl_ids = {}
+        flask.g.new_relationship_ids = {self.rels[2].id}
+        flask.g.deleted_objects = {}
+
+        propagation.propagate()
+
+    self.assertEqual(
+        all_models.AccessControlList.query.filter_by(
+            parent_id=self.parent_acl.id,
+        ).count(),
+        3,
+    )
