3a149c5dcde88ed5d8099018b6f296e3b2141e4c
Miha Zidar <zidarsk8@gmail.com>
2018-08-19 23:29:07 +0200

    Add base_id column to access control list table

---
+++ src/ggrc/migrations/versions/20180819204257_ee7ba3ba8aa8_add_base_id_to_access_control_list.py
@@ -0,0 +1,59 @@
+# Copyright (C) 2018 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""
+Add base_id to access control list
+
+Create Date: 2018-08-19 20:42:57.168436
+"""
+# disable Invalid constant name pylint warning for mandatory Alembic variables.
+# pylint: disable=invalid-name
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = 'ee7ba3ba8aa8'
+down_revision = 'c3ef0656a527'
+
+
+def _populate_base_id():
+  acl_table = sa.sql.table(
+      "access_control_list",
+      sa.sql.column('id', sa.Integer),
+      sa.sql.column('ac_role_id', sa.Integer),
+      sa.sql.column('parent_id', sa.Integer),
+      sa.sql.column('base_id', sa.Integer),
+  )
+  op.execute(acl_table.update().values(base_id=acl_table.c.id))
+
+
+def upgrade():
+  """Upgrade database schema and/or data, creating a new revision."""
+  op.add_column(
+      'access_control_list',
+      sa.Column('base_id', sa.Integer(), nullable=True)
+  )
+  op.create_foreign_key(
+      'fk_access_control_list_base_id',
+      'access_control_list',
+      'access_control_list',
+      ['base_id'],
+      ['id'],
+  )
+  _populate_base_id()
+
+
+def downgrade():
+  """Downgrade database schema and/or data back to the previous revision."""
+  op.drop_constraint(
+      'fk_access_control_list_base_id',
+      'access_control_list',
+      type_='foreignkey',
+  )
+  op.drop_index(
+      'fk_access_control_list_base_id',
+      table_name='access_control_list',
+  )
+  op.drop_column('access_control_list', 'base_id')
