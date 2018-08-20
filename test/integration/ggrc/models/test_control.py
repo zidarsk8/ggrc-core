0ac85135d4fa4e1528ed328d6770928a018b5f98
Aleh Natseuski <natsevsky@gmail.com>
2018-08-20 16:56:01 +0300

    Add review/reviewable tests

--- test/integration/ggrc/models/test_control.py
+++ test/integration/ggrc/models/test_control.py
@@ -2,9 +2,10 @@
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
 """Tests for control model."""
+import unittest
 
 from ggrc import db
-from ggrc.models import Control
+from ggrc.models import all_models
 from integration.ggrc import TestCase
 from integration.ggrc.models import factories
 from integration.ggrc import api_helper
@@ -26,35 +27,35 @@ class TestControl(TestCase):
     db.session.commit()
     self.assertIn(category, control.categories)
     # be really really sure
-    control = db.session.query(Control).get(control.id)
+    control = db.session.query(all_models.Control).get(control.id)
     self.assertIn(category, control.categories)
 
   def test_has_test_plan(self):
     """Check test plan setup to control."""
     control = factories.ControlFactory(test_plan="This is a test text")
-    control = db.session.query(Control).get(control.id)
+    control = db.session.query(all_models.Control).get(control.id)
     self.assertEqual(control.test_plan, "This is a test text")
 
   def test_set_end_date(self):
     """End_date can't to be updated."""
     control = factories.ControlFactory()
     self.api.put(control, {"end_date": "2015-10-10"})
-    control = db.session.query(Control).get(control.id)
+    control = db.session.query(all_models.Control).get(control.id)
     self.assertIsNone(control.end_date)
 
   def test_set_deprecated_status(self):
     """Deprecated status setup end_date."""
     control = factories.ControlFactory()
     self.assertIsNone(control.end_date)
-    self.api.put(control, {"status": Control.DEPRECATED})
-    control = db.session.query(Control).get(control.id)
+    self.api.put(control, {"status": all_models.Control.DEPRECATED})
+    control = db.session.query(all_models.Control).get(control.id)
     self.assertIsNotNone(control.end_date)
 
   def test_create_commentable(self):
     """Test if commentable fields are set on creation"""
     recipients = "Admin,Primary Contacts,Secondary Contacts"
     send_by_default = 0
-    response = self.api.post(Control, {
+    response = self.api.post(all_models.Control, {
         "control": {
             "title": "Control title",
             "context": None,
@@ -64,7 +65,7 @@ class TestControl(TestCase):
     })
     self.assertEqual(response.status_code, 201)
     control_id = response.json.get("control").get("id")
-    control = db.session.query(Control).get(control_id)
+    control = db.session.query(all_models.Control).get(control_id)
     self.assertEqual(control.recipients, recipients)
     self.assertEqual(control.send_by_default, send_by_default)
 
@@ -80,6 +81,20 @@ class TestControl(TestCase):
         "recipients": recipients,
         "send_by_default": send_by_default,
     })
-    control = db.session.query(Control).get(control.id)
+    control = db.session.query(all_models.Control).get(control.id)
     self.assertEqual(control.recipients, recipients)
     self.assertEqual(control.send_by_default, send_by_default)
+
+  @unittest.skip("Skipped until control not Reviewable")
+  def test_review_get(self):
+    """Test that review data is present in control get response"""
+    with factories.single_commit():
+      control = factories.ControlFactory()
+      review = factories.ReviewFactory(reviewable=control)
+      review_id = review.id
+
+    resp = self.api.get(all_models.Control, control.id)
+    self.assert200(resp)
+    resp_control = resp.json["control"]
+    self.assertIn("review", resp_control)
+    self.assertEquals(review_id, resp_control["review"]["id"])
