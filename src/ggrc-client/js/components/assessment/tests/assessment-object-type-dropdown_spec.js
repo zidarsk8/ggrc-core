7808f9c9ae8a9e6affd961349e47368de82810f5
Natalia Zarubina <nataly.zarubina@gmail.com>
2018-08-21 14:42:23 +0300

    Use snapshotable objects collection for assessment type dropdown

--- src/ggrc-client/js/components/assessment/tests/assessment-object-type-dropdown_spec.js
+++ src/ggrc-client/js/components/assessment/tests/assessment-object-type-dropdown_spec.js
@@ -16,7 +16,7 @@ describe('assessment-object-type-dropdown component', function () {
     viewModel = getComponentVM(Component);
   });
 
-  it('returns the types obtained from the Mappings', function () {
+  it('returns the grouped types', function () {
     let result;
 
     let objectTypes = {
@@ -30,7 +30,7 @@ describe('assessment-object-type-dropdown component', function () {
       },
     };
 
-    spyOn(Mappings, 'getMappingTypes').and.returnValue(objectTypes);
+    spyOn(Mappings, 'groupTypes').and.returnValue(objectTypes);
     result = viewModel.attr('objectTypes');
     expect(result).toEqual(objectTypes);
   });
@@ -51,74 +51,9 @@ describe('assessment-object-type-dropdown component', function () {
       {name: 'Bar'}, {name: 'Car'}, {name: 'Dar'}, {name: 'Zar'},
     ];
 
-    spyOn(Mappings, 'getMappingTypes').and.returnValue(objectTypes);
+    spyOn(Mappings, 'groupTypes').and.returnValue(objectTypes);
 
     result = viewModel.attr('objectTypes');
     expect(result.groupFoo.items).toEqual(expected);
   });
-
-  it('omits the all_objects group from result', function () {
-    let result;
-
-    let objectTypes = {
-      all_objects: {
-        models: ['Foo', 'Bar', 'Baz'],
-        name: 'FooBarBaz-type Objects',
-      },
-    };
-
-    spyOn(Mappings, 'getMappingTypes').and.returnValue(objectTypes);
-
-    result = viewModel.attr('objectTypes');
-    expect(result.all_objects).toBeUndefined();
-  });
-
-  it('omits the types not relevant to the AssessmentTemplate from result',
-    function () {
-      let result;
-
-      let objectTypes = {
-        groupFoo: {
-          name: 'Foo Objects',
-          items: [
-            {value: 'Contract'}, // this object type is relevant
-            {value: 'Assessment'},
-            {value: 'Audit'},
-            {value: 'CycleTaskGroupObjectTask'},
-          ],
-        },
-        groupBar: {
-          name: 'Bar Objects',
-          items: [
-            {value: 'Policy'}, // this object type is relevant
-            {value: 'TaskGroup'},
-            {value: 'TaskGroupTask'},
-          ],
-        },
-        groupBaz: {
-          name: 'Baz Objects',
-          items: [
-            {value: 'Workflow'},
-          ],
-        },
-      };
-
-      let expected = {
-        groupFoo: {
-          name: 'Foo Objects',
-          items: [{value: 'Contract'}],
-        },
-        groupBar: {
-          name: 'Bar Objects',
-          items: [{value: 'Policy'}],
-        },
-        // the groupBaz group, being empty, is expected to have been removed
-      };
-
-      spyOn(Mappings, 'getMappingTypes').and.returnValue(objectTypes);
-
-      result = viewModel.attr('objectTypes');
-      expect(result).toEqual(expected);
-    }
-  );
 });
