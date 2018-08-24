e1f7fbf16623196cf7345f3f0a87780556e5a9b1
Aleh Natseuski <natsevsky@gmail.com>
2018-08-24 18:09:02 +0300

    Add rest handlers mixins

---
+++ src/ggrc/models/mixins/rest_handable.py
@@ -0,0 +1,113 @@
+# Copyright (C) 2018 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""Contains mixins for handle REST events issued by signals.
+
+This allows adding handlers to model without boilerplate code
+"""
+
+from ggrc.services import signals
+
+
+class WithPutHandable(object):
+  """Mixin that adds PUT handler"""
+  __lazy_init__ = True
+
+  def handle_put(self):
+    """PUT handler"""
+    raise NotImplementedError
+
+  @classmethod
+  def init(cls, model):
+    """Init handlers"""
+    # pylint: disable=unused-variable,unused-argument
+    @signals.Restful.model_put.connect_via(model)
+    def model_put(*args, **kwargs):
+      """PUT handler"""
+      model.handle_put(kwargs["obj"])
+
+
+class WithPostHandable(object):
+  """Mixin that adds POST handler"""
+  __lazy_init__ = True
+
+  def handle_post(self):
+    """POST handler"""
+    raise NotImplementedError
+
+  @classmethod
+  def init(cls, model):
+    """Init handlers"""
+    # pylint: disable=unused-variable,unused-argument
+    @signals.Restful.model_posted.connect_via(model)
+    def model_posted(*args, **kwargs):
+      """POST handler"""
+      model.handle_post(kwargs["obj"])
+
+
+class WithRelationshipsHandable(object):
+  """Mixin that adds relationship POST/PUT/DELETE handlers"""
+  __lazy_init__ = True
+
+  def handle_relationship_post(self, counterparty):
+    """relationship POST handler"""
+    pass
+
+  def handle_relationship_put(self, counterparty):
+    """relationship PUT handler"""
+    pass
+
+  def handle_relationship_delete(self, counterparty):
+    """relationship DELETE handler"""
+    pass
+
+  def is_in_relationship(self, relationship):
+    endpoints = [relationship.source.type, relationship.destination.type]
+    return self.__name__ not in endpoints
+
+  @classmethod
+  def init(cls, model):
+    """Init handlers"""
+    # pylint: disable=unused-variable,unused-argument
+    from ggrc.models import all_models
+
+    def is_in_relationship(rel):
+      """Check if relationship related to model"""
+      endpoints = {rel.source.type, rel.destination.type}
+      return model.__name__ in endpoints
+
+    def get_obj_counterparty(rel):
+      """Get model object and its counterparty"""
+      counterparty = rel.source
+      obj = rel.destination
+      if rel.source.type == model.__name__:
+        counterparty = rel.destination
+        obj = rel.source
+      return obj, counterparty
+
+    @signals.Restful.model_posted.connect_via(all_models.Relationship)
+    def relationship_post(*args, **kwargs):
+      """relationship POST handler"""
+      relationship = kwargs["obj"]
+      if not is_in_relationship(relationship):
+        return
+      obj, counterparty = get_obj_counterparty(relationship)
+      model.handle_relationship_post(obj, counterparty)
+
+    @signals.Restful.model_put.connect_via(all_models.Relationship)
+    def relationship_updated(*args, **kwargs):
+      """relationship PUT handler"""
+      relationship = kwargs["obj"]
+      if not is_in_relationship(relationship):
+        return
+      obj, counterparty = get_obj_counterparty(relationship)
+      model.handle_relationship_put(obj, counterparty)
+
+    @signals.Restful.model_deleted.connect_via(all_models.Relationship)
+    def relationship_delete(*args, **kwargs):
+      """relationship DELETE handler"""
+      relationship = kwargs["obj"]
+      if not is_in_relationship(relationship):
+        return
+      obj, counterparty = get_obj_counterparty(relationship)
+      model.handle_relationship_delete(obj, counterparty)
