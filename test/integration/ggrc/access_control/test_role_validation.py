d0a20167df6aff4ab5529dca629c9e7bf06c102a
Alexey Karpenko <k-alexey@users.noreply.github.com>
2018-08-27 16:13:18 +0300

    Add integration API test

--- test/integration/ggrc/access_control/test_role_validation.py
+++ test/integration/ggrc/access_control/test_role_validation.py
@@ -2,9 +2,11 @@
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
 """Test Access Control List validation."""
+import ddt
 
 from ggrc import db
 from ggrc.models import all_models
+from ggrc.access_control.roleable import Roleable
 from integration.ggrc import TestCase, Api
 from integration.ggrc.models import factories
 
@@ -80,3 +82,53 @@ class TestAccessControlListValidation(TestCase):
       acl_obj_type = acl.object_type
       acr_obj_type = acl.ac_role.object_type
       self.assertEqual(acl_obj_type, acr_obj_type)
+
+
+@ddt.ddt
+class TestMaxACLValidation(TestCase):
+  """Test AccessControlList max roles validation."""
+
+  def setUp(self):
+    super(TestMaxACLValidation, self).setUp()
+    self.api = Api()
+    self.client.get("/login")
+
+  @ddt.data("Assignee", "Verifier")
+  def test_max_roles_validation(self, role):
+    """Test validation of max {} roles in object(OrgGroup)"""
+
+    def add_role(_acl, _id):
+      _acl += [{
+          "ac_role_id": _id,
+          "person": {
+              "type": "Person",
+              "id": factories.PersonFactory().id,
+          }
+      }]
+
+    og_admin = db.session.query(all_models.AccessControlRole.id).filter(
+        all_models.AccessControlRole.object_type == "OrgGroup",
+        all_models.AccessControlRole.name == "Admin"
+    ).one()[0]
+    og_role = db.session.query(all_models.AccessControlRole.id).filter(
+        all_models.AccessControlRole.object_type == "OrgGroup",
+        all_models.AccessControlRole.name == role
+    ).one()[0]
+    acl = []
+    add_role(acl, og_admin)
+    for _ in range(getattr(Roleable, "MAX_{}_NUM".format(role.upper()))):
+      add_role(acl, og_role)
+    data = {
+        "org_group": {
+            "title": "org_group title",
+            "context": None,
+            "access_control_list": acl,
+        }
+    }
+    response = self.api.post(all_models.OrgGroup, data)
+    self.assertEqual(response.status_code, 201)
+    self.assertEqual(all_models.OrgGroup.query.count(), 1)
+    add_role(acl, og_role)
+    response = self.api.post(all_models.OrgGroup, data)
+    self.assertEqual(response.status_code, 400)
+    self.assertEqual(all_models.OrgGroup.query.count(), 1)
