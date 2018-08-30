d782f7cb198d22e9caef67e6ef5f442d02ebabc3
Artsiom Valchanin <volchanin.dev@gmail.com>
2018-08-30 14:44:10 +0300

    Simplify task-list component event handlers

--- src/ggrc-client/js/components/workflow/task-list/tests/task-list_spec.js
+++ src/ggrc-client/js/components/workflow/task-list/tests/task-list_spec.js
@@ -3,12 +3,11 @@
  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 */
 
-import Cacheable from '../../../../models/cacheable';
 import Component from '../task-list';
 import {getComponentVM} from '../../../../../js_specs/spec_helpers';
 import Permission from '../../../../permission';
 import {REFRESH_RELATED} from '../../../../events/eventTypes';
-import * as businessModels from '../../../../models/business-models';
+import TaskGroupTask from '../../../../models/business-models/task-group-task';
 
 describe('task-list component', () => {
   let viewModel;
@@ -66,45 +65,23 @@ describe('task-list component', () => {
     });
 
     describe('when page is first', () => {
-      let origRelatedItemsType;
-
-      beforeAll(function () {
-        origRelatedItemsType = staticVmProps.relatedItemsType;
-      });
-
-      afterAll(function () {
-        staticVmProps.relatedItemsType = origRelatedItemsType;
-      });
-
       beforeEach(function () {
         viewModel.attr('paging.current', 1);
       });
 
       it('dispatches REFRESH_RELATED event for base instance with ' +
-      'relatedItemsType model', function () {
+      'TaskGroupTask model', function () {
         const dispatch = spyOn(viewModel.baseInstance, 'dispatch');
-        const type = 'ModelName';
-        staticVmProps.relatedItemsType = type;
         viewModel.updatePagingAfterCreate();
         expect(dispatch).toHaveBeenCalledWith({
           ...REFRESH_RELATED,
-          model: type,
+          model: 'TaskGroupTask',
         });
       });
     });
   });
 
   describe('updatePagingAfterDestroy() method', () => {
-    let origRelatedItemsType;
-
-    beforeAll(function () {
-      origRelatedItemsType = staticVmProps.relatedItemsType;
-    });
-
-    afterAll(function () {
-      staticVmProps.relatedItemsType = origRelatedItemsType;
-    });
-
     beforeEach(function () {
       viewModel.attr('baseInstance', {});
     });
@@ -125,14 +102,12 @@ describe('task-list component', () => {
 
     describe('if current page is first', () => {
       it('dispatches REFRESH_RELATED event for base instance with ' +
-      'relatedItemsType model', function () {
+      'TaskGroupTask model', function () {
         const dispatch = spyOn(viewModel.baseInstance, 'dispatch');
-        const type = 'ModelName';
-        staticVmProps.relatedItemsType = type;
         viewModel.updatePagingAfterDestroy();
         expect(dispatch).toHaveBeenCalledWith({
           ...REFRESH_RELATED,
-          model: type,
+          model: 'TaskGroupTask',
         });
       });
     });
@@ -145,32 +120,22 @@ describe('task-list component', () => {
       events = Component.prototype.events;
     });
 
-    describe('"{CMS.Models.${viewModel.relatedItemsType}} created"() event',
+    describe('"{CMS.Models.TaskGroupTask} created"() event',
       () => {
         let handler;
         let eventsScope;
 
         beforeEach(function () {
           eventsScope = {viewModel};
-          handler = events[
-            `{CMS.Models.${staticVmProps.relatedItemsType}} created`
-          ].bind(eventsScope);
+          handler = events['{CMS.Models.TaskGroupTask} created']
+            .bind(eventsScope);
         });
 
         describe('if passed instance has related items type then', () => {
           let instance;
-          let origRelatedItemsType;
 
           beforeEach(function () {
-            origRelatedItemsType = staticVmProps.relatedItemsType;
-            staticVmProps.relatedItemsType = 'TestType';
-            businessModels[staticVmProps.relatedItemsType] = can.Model.extend();
-            instance = new businessModels[staticVmProps.relatedItemsType];
-          });
-
-          afterEach(function () {
-            businessModels[staticVmProps.relatedItemsType] = undefined;
-            staticVmProps.relatedItemsType = origRelatedItemsType;
+            instance = new TaskGroupTask();
           });
 
           it('updates items of the page', function () {
@@ -181,38 +146,22 @@ describe('task-list component', () => {
         });
       });
 
-    describe('"{CMS.Models.${viewModel.relatedItemsType}} destroyed"() event',
+    describe('"{CMS.Models.TaskGroupTask} destroyed"() event',
       () => {
         let handler;
         let eventsScope;
 
         beforeEach(function () {
           eventsScope = {viewModel};
-          handler = events[
-            `{CMS.Models.${staticVmProps.relatedItemsType}} destroyed`
-          ].bind(eventsScope);
+          handler = events['{CMS.Models.TaskGroupTask} destroyed']
+            .bind(eventsScope);
         });
 
         describe('if passed instance has related items type then', () => {
           let instance;
-          let origRelatedItemsType;
-
-          beforeAll(function () {
-            origRelatedItemsType = staticVmProps.relatedItemsType;
-          });
-
-          afterAll(function () {
-            staticVmProps.relatedItemsType = origRelatedItemsType;
-          });
 
           beforeEach(function () {
-            staticVmProps.relatedItemsType = 'TestType';
-            businessModels[staticVmProps.relatedItemsType] = can.Model.extend();
-            instance = new businessModels[staticVmProps.relatedItemsType];
-          });
-
-          afterEach(function () {
-            businessModels[staticVmProps.relatedItemsType] = undefined;
+            instance = new TaskGroupTask();
           });
 
           it('updates items of the page', function () {
