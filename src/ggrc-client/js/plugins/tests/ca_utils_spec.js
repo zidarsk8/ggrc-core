e9da23a4a8a00bcc283d0ba33479b001d2e89229
Klim Shuplenkou <kshuplenkou@google.com>
2018-09-04 12:06:11 +0300

    Add common describe for ca-utils tests

--- src/ggrc-client/js/plugins/tests/ca_utils_spec.js
+++ src/ggrc-client/js/plugins/tests/ca_utils_spec.js
@@ -13,250 +13,252 @@ import {
   isUrlRequired,
 } from '../utils/ca-utils';
 
-describe('GGRC utils isEmptyCustomAttribute() method', function () {
-  describe('check undefined value', function () {
-    it('returns true for undefined', function () {
-      let result = isEmptyCA(undefined);
-      expect(result).toBe(true);
-    });
-  });
-
-  describe('check Rich Text value', function () {
-    it('returns true for empty div', function () {
-      let result = isEmptyCA('<div></div>', 'Rich Text');
-      expect(result).toBe(true);
-    });
-
-    it('returns true for div with a line break', function () {
-      let result = isEmptyCA('<div><br></div>', 'Rich Text');
-      expect(result).toBe(true);
-    });
-
-    it('returns true for div with a empty list', function () {
-      let result = isEmptyCA('<div><ul><li></li></ul></div>', 'Rich Text');
-      expect(result).toBe(true);
-    });
-
-    it('returns true for div with a empty paragraph', function () {
-      let result = isEmptyCA('<div><p></p></div>', 'Rich Text');
-      expect(result).toBe(true);
-    });
-
-    it('returns false for div with the text', function () {
-      let result = isEmptyCA('<div>Very important text!</div>', 'Rich Text');
-      expect(result).toBe(false);
-    });
-
-    it('returns false for not empty list', function () {
-      let result = isEmptyCA('<div><ul><li>One</li><li>Two</li></ul></div>',
-        'Rich Text');
-      expect(result).toBe(false);
-    });
-  });
-
-  describe('check Checkbox value', function () {
-    it('returns false for unchecked', function () {
-      let result = isEmptyCA('0', 'Checkbox');
-      expect(result).toBe(true);
-    });
-
-    it('returns true for checked', function () {
-      let result = isEmptyCA('1', 'Checkbox');
-      expect(result).toBe(false);
-    });
-  });
-
-  describe('check Text value', function () {
-    it('returns false for not empty', function () {
-      let result = isEmptyCA('some text', 'Text');
-      expect(result).toBe(false);
-    });
-
-    it('returns true for empty', function () {
-      let result = isEmptyCA('', 'Text');
-      expect(result).toBe(true);
-    });
-  });
-
-  describe('check Map:Person type', function () {
-    it('returns false for selected', function () {
-      let result = isEmptyCA('Person', 'Map:Person');
-      expect(result).toBe(false);
-    });
-
-    it('returns true for not selected', function () {
-      let result = isEmptyCA('', 'Map:Person');
-      expect(result).toBe(true);
-    });
-
-    it('returns true for not selected cav', function () {
-      let result = isEmptyCA('', 'Map:Person', {attribute_object: null});
-      expect(result).toBe(true);
-    });
-
-    it('returns false for selected cav', function () {
-      let result = isEmptyCA('', 'Map:Person', {attribute_object: 'Person'});
-      expect(result).toBe(false);
-    });
-  });
-
-  describe('check Date type', function () {
-    it('returns false for selected', function () {
-      let result = isEmptyCA('01/01/2016', 'Date');
-      expect(result).toBe(false);
-    });
-
-    it('returns true for not selected', function () {
-      let result = isEmptyCA('', 'Date');
-      expect(result).toBe(true);
-    });
-  });
-
-  describe('check Dropdown type', function () {
-    it('returns false for selected', function () {
-      let result = isEmptyCA('value', 'Dropdown');
-      expect(result).toBe(false);
-    });
-
-    it('returns true for not selected', function () {
-      let result = isEmptyCA('', 'Dropdown');
-      expect(result).toBe(true);
-    });
-  });
-
-  describe('check invalid type', function () {
-    it('returns false for invalid type', function () {
-      let result = isEmptyCA('some value', 'Invalid');
-      expect(result).toBe(false);
-    });
-  });
-});
-
-describe('GGRC util methods to validate requirements', function () {
-  let dropdownField;
-
-  beforeEach(function () {
-    dropdownField = new can.Map({
-      id: 2,
-      type: 'dropdown',
-      validationConfig: {
-        'nothing required': ddValidationMapToValue(),
-        'comment required': ddValidationMapToValue({
-          comment: true,
-        }),
-        'evidence required': ddValidationMapToValue({
-          attachment: true,
-        }),
-        'url required': ddValidationMapToValue({
-          url: true,
-        }),
-        'comment & evidence required': ddValidationMapToValue({
-          comment: true,
-          attachment: true,
-        }),
-        'comment & url required': ddValidationMapToValue({
-          comment: true,
-          url: true,
-        }),
-        'url & evidence required': ddValidationMapToValue({
-          url: true,
-          attachment: true,
-        }),
-        'comment, evidence, url required': ddValidationMapToValue({
-          comment: true,
-          attachment: true,
-          url: true,
-        }),
-        one: ddValidationMapToValue({
-          comment: true,
-          attachment: true,
-        }),
-        two: ddValidationMapToValue({
-          comment: true,
-          attachment: true,
-        }),
-      },
-      preconditions_failed: [],
-      validation: {
-        mandatory: false,
-      },
-      errorsMap: {
-        comment: false,
-        evidence: false,
-        url: false,
-      },
-    });
-  });
-
-  describe('check isCommentRequired() method', function () {
-    it('should return TRUE if comments required', function () {
-      ['one', 'two',
-        'comment required',
-        'comment & evidence required',
-        'comment & url required',
-        'comment, evidence, url required',
-      ].forEach((value) => {
-        dropdownField.attr('value', value);
-        expect(isCommentRequired(dropdownField)).toEqual(true);
+describe('ca-utils', function () {
+  describe('isEmptyCustomAttribute() method', function () {
+    describe('check undefined value', function () {
+      it('returns true for undefined', function () {
+        let result = isEmptyCA(undefined);
+        expect(result).toBe(true);
       });
     });
 
-    it('should return FALSE if comments NOT required', function () {
-      ['nothing required',
-        'evidence required',
-        'url required',
-        'url & evidence required',
-      ].forEach((value) => {
-        dropdownField.attr('value', value);
-        expect(isCommentRequired(dropdownField)).toEqual(false);
+    describe('check Rich Text value', function () {
+      it('returns true for empty div', function () {
+        let result = isEmptyCA('<div></div>', 'Rich Text');
+        expect(result).toBe(true);
+      });
+
+      it('returns true for div with a line break', function () {
+        let result = isEmptyCA('<div><br></div>', 'Rich Text');
+        expect(result).toBe(true);
+      });
+
+      it('returns true for div with a empty list', function () {
+        let result = isEmptyCA('<div><ul><li></li></ul></div>', 'Rich Text');
+        expect(result).toBe(true);
+      });
+
+      it('returns true for div with a empty paragraph', function () {
+        let result = isEmptyCA('<div><p></p></div>', 'Rich Text');
+        expect(result).toBe(true);
+      });
+
+      it('returns false for div with the text', function () {
+        let result = isEmptyCA('<div>Very important text!</div>', 'Rich Text');
+        expect(result).toBe(false);
+      });
+
+      it('returns false for not empty list', function () {
+        let result = isEmptyCA('<div><ul><li>One</li><li>Two</li></ul></div>',
+          'Rich Text');
+        expect(result).toBe(false);
+      });
+    });
+
+    describe('check Checkbox value', function () {
+      it('returns false for unchecked', function () {
+        let result = isEmptyCA('0', 'Checkbox');
+        expect(result).toBe(true);
+      });
+
+      it('returns true for checked', function () {
+        let result = isEmptyCA('1', 'Checkbox');
+        expect(result).toBe(false);
+      });
+    });
+
+    describe('check Text value', function () {
+      it('returns false for not empty', function () {
+        let result = isEmptyCA('some text', 'Text');
+        expect(result).toBe(false);
+      });
+
+      it('returns true for empty', function () {
+        let result = isEmptyCA('', 'Text');
+        expect(result).toBe(true);
+      });
+    });
+
+    describe('check Map:Person type', function () {
+      it('returns false for selected', function () {
+        let result = isEmptyCA('Person', 'Map:Person');
+        expect(result).toBe(false);
+      });
+
+      it('returns true for not selected', function () {
+        let result = isEmptyCA('', 'Map:Person');
+        expect(result).toBe(true);
+      });
+
+      it('returns true for not selected cav', function () {
+        let result = isEmptyCA('', 'Map:Person', {attribute_object: null});
+        expect(result).toBe(true);
+      });
+
+      it('returns false for selected cav', function () {
+        let result = isEmptyCA('', 'Map:Person', {attribute_object: 'Person'});
+        expect(result).toBe(false);
+      });
+    });
+
+    describe('check Date type', function () {
+      it('returns false for selected', function () {
+        let result = isEmptyCA('01/01/2016', 'Date');
+        expect(result).toBe(false);
+      });
+
+      it('returns true for not selected', function () {
+        let result = isEmptyCA('', 'Date');
+        expect(result).toBe(true);
+      });
+    });
+
+    describe('check Dropdown type', function () {
+      it('returns false for selected', function () {
+        let result = isEmptyCA('value', 'Dropdown');
+        expect(result).toBe(false);
+      });
+
+      it('returns true for not selected', function () {
+        let result = isEmptyCA('', 'Dropdown');
+        expect(result).toBe(true);
+      });
+    });
+
+    describe('check invalid type', function () {
+      it('returns false for invalid type', function () {
+        let result = isEmptyCA('some value', 'Invalid');
+        expect(result).toBe(false);
       });
     });
   });
 
-  describe('check isEvidenceRequired() method', function () {
-    it('should return TRUE if evidence required', function () {
-      ['one', 'two',
-        'evidence required',
-        'url & evidence required',
-        'comment & evidence required',
-        'comment, evidence, url required',
-      ].forEach((value) => {
-        dropdownField.attr('value', value);
-        expect(isEvidenceRequired(dropdownField)).toEqual(true);
+  describe('methods to validate requirements', function () {
+    let dropdownField;
+
+    beforeEach(function () {
+      dropdownField = new can.Map({
+        id: 2,
+        type: 'dropdown',
+        validationConfig: {
+          'nothing required': ddValidationMapToValue(),
+          'comment required': ddValidationMapToValue({
+            comment: true,
+          }),
+          'evidence required': ddValidationMapToValue({
+            attachment: true,
+          }),
+          'url required': ddValidationMapToValue({
+            url: true,
+          }),
+          'comment & evidence required': ddValidationMapToValue({
+            comment: true,
+            attachment: true,
+          }),
+          'comment & url required': ddValidationMapToValue({
+            comment: true,
+            url: true,
+          }),
+          'url & evidence required': ddValidationMapToValue({
+            url: true,
+            attachment: true,
+          }),
+          'comment, evidence, url required': ddValidationMapToValue({
+            comment: true,
+            attachment: true,
+            url: true,
+          }),
+          one: ddValidationMapToValue({
+            comment: true,
+            attachment: true,
+          }),
+          two: ddValidationMapToValue({
+            comment: true,
+            attachment: true,
+          }),
+        },
+        preconditions_failed: [],
+        validation: {
+          mandatory: false,
+        },
+        errorsMap: {
+          comment: false,
+          evidence: false,
+          url: false,
+        },
       });
     });
 
-    it('should return FALSE if evidence NOT required', function () {
-      ['nothing required',
-        'comment required',
-        'url required',
-        'comment & url required',
-      ].forEach((value) => {
-        dropdownField.attr('value', value);
-        expect(isEvidenceRequired(dropdownField)).toEqual(false);
+    describe('check isCommentRequired() method', function () {
+      it('should return TRUE if comments required', function () {
+        ['one', 'two',
+          'comment required',
+          'comment & evidence required',
+          'comment & url required',
+          'comment, evidence, url required',
+        ].forEach((value) => {
+          dropdownField.attr('value', value);
+          expect(isCommentRequired(dropdownField)).toEqual(true);
+        });
+      });
+
+      it('should return FALSE if comments NOT required', function () {
+        ['nothing required',
+          'evidence required',
+          'url required',
+          'url & evidence required',
+        ].forEach((value) => {
+          dropdownField.attr('value', value);
+          expect(isCommentRequired(dropdownField)).toEqual(false);
+        });
       });
     });
-  });
 
-  describe('check isURLRequired() method', function () {
-    it('should return TRUE if url required', function () {
-      ['url required',
-        'comment & url required',
-        'comment, evidence, url required',
-      ].forEach((value) => {
-        dropdownField.attr('value', value);
-        expect(isUrlRequired(dropdownField)).toEqual(true);
+    describe('check isEvidenceRequired() method', function () {
+      it('should return TRUE if evidence required', function () {
+        ['one', 'two',
+          'evidence required',
+          'url & evidence required',
+          'comment & evidence required',
+          'comment, evidence, url required',
+        ].forEach((value) => {
+          dropdownField.attr('value', value);
+          expect(isEvidenceRequired(dropdownField)).toEqual(true);
+        });
+      });
+
+      it('should return FALSE if evidence NOT required', function () {
+        ['nothing required',
+          'comment required',
+          'url required',
+          'comment & url required',
+        ].forEach((value) => {
+          dropdownField.attr('value', value);
+          expect(isEvidenceRequired(dropdownField)).toEqual(false);
+        });
       });
     });
 
-    it('should return FALSE if url NOT required', function () {
-      ['nothing required',
-        'comment required',
-        'evidence required',
-        'comment & evidence required',
-      ].forEach((value) => {
-        dropdownField.attr('value', value);
-        expect(isUrlRequired(dropdownField)).toEqual(false);
+    describe('check isURLRequired() method', function () {
+      it('should return TRUE if url required', function () {
+        ['url required',
+          'comment & url required',
+          'comment, evidence, url required',
+        ].forEach((value) => {
+          dropdownField.attr('value', value);
+          expect(isUrlRequired(dropdownField)).toEqual(true);
+        });
+      });
+
+      it('should return FALSE if url NOT required', function () {
+        ['nothing required',
+          'comment required',
+          'evidence required',
+          'comment & evidence required',
+        ].forEach((value) => {
+          dropdownField.attr('value', value);
+          expect(isUrlRequired(dropdownField)).toEqual(false);
+        });
       });
     });
   });
