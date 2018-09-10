f6d2676763b61a2989128aea3a0a5bab8e18ce66
Miha Zidar <zidarsk8@gmail.com>
2018-09-10 11:33:24 +0200

    wip

--- src/ggrc/access_control/roleable.py
+++ src/ggrc/access_control/roleable.py
@@ -7,10 +7,10 @@ from sqlalchemy import and_
 from sqlalchemy import orm
 from sqlalchemy.orm import remote
 from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.ext.hybrid import hybrid_property
 
 from ggrc import db
 from ggrc.access_control.list import AccessControlList
+from ggrc.access_control.people import AccessControlPeople
 from ggrc.access_control import role
 from ggrc.fulltext.attributes import CustomRoleAttr
 from ggrc.models import reflection
@@ -33,11 +33,24 @@ class Roleable(object):
   def __init__(self, *args, **kwargs):
     super(Roleable, self).__init__(*args, **kwargs)
     for ac_role in role.get_ac_roles_for(self.type).values():
-      print ac_role
       AccessControlList(
           object=self,
           ac_role=ac_role,
       )
+
+  @declared_attr
+  def _access_control_people(cls):  # pylint: disable=no-self-argument
+    """access_control_list"""
+    return db.relationship(
+        'AccessControlPeople',
+        primaryjoin=lambda: and_(
+            remote(AccessControlList.object_id) == cls.id,
+            remote(AccessControlList.object_type) == cls.__name__,
+            remote(AccessControlList.parent_id_nn) == 0,
+            remote(AccessControlPeople.ac_list_id) == AccessControlList.id
+        ),
+        viewonly=True,
+    )
 
   @declared_attr
   def _access_control_list(cls):  # pylint: disable=no-self-argument
@@ -53,9 +66,12 @@ class Roleable(object):
         backref='{0}_object'.format(cls.__name__),
         cascade='all, delete-orphan')
 
-  @hybrid_property
+  @property
   def access_control_list(self):
-    return self._access_control_list
+    return [
+        (acp.person_id, acp.acl_id)
+        for acp in self._access_control_people
+    ]
 
   @access_control_list.setter
   def access_control_list(self, values):
@@ -65,7 +81,15 @@ class Roleable(object):
         values: List of access control roles or dicts containing json
         representation of custom attribute values.
     """
-    pass  # TODO: acp
+    import ipdb; ipdb.set_trace()
+    # using ac_role_id instead of ac_role means we can't (easily) create a new
+    # role and map people to it in a single request.
+    role_map = {acl.ac_role.id: acl for acl in self._access_control_list}
+    for value in values:
+      AccessControlPeople(
+          acl_id=role_map[value["ac_role_id"]],
+          person_id=value["person"]["id"],
+      )
 
   def extend_access_control_list(self, values):
     """Extend access control list.
--- test/integration/ggrc/access_control/test_roleable.py
+++ test/integration/ggrc/access_control/test_roleable.py
@@ -5,9 +5,10 @@
 import ddt
 from ggrc import app
 from ggrc import db
-from ggrc.models import all_models
 from ggrc.access_control import roleable
+from ggrc.models import all_models
 from integration.ggrc import TestCase
+from integration.ggrc import api_helper
 from integration.ggrc.models import factories
 
 
@@ -18,7 +19,6 @@ class TestAccessControlRoleable(TestCase):
   def setUp(self):
     super(TestAccessControlRoleable, self).setUp()
     with factories.single_commit():
-      self.role = factories.AccessControlRoleFactory()
       self.person = factories.PersonFactory()
 
   @ddt.data(*[
@@ -51,12 +51,31 @@ class TestAccessControlRoleable(TestCase):
   def test_with_dict(self, acl_list):
     """Test access_control_list setter with a basic dict object
     This is the format the frontend uses"""
-    return  # TODO: acp
+    self.role = factories.AccessControlRoleFactory(object_type="Control")
+    import ipdb; ipdb.set_trace()
+    obj = all_models.Control(
+        title="New Control",
+    )
+    obj.access_control_list = acl_list(self)
+    self.assertIsNotNone(obj.access_control_list)
+    acl = obj.access_control_list[0]
+    self.assertIsNotNone(acl)
+    self.assertIsInstance(acl, all_models.AccessControlList)
+    self.assertEqual(acl.person.id, self.person.id)
+    self.assertEqual(acl.ac_role.id, self.role.id)
+    self.assertEqual(acl.object, obj)
 
   def test_with_dict_objs_multiple(self):
     """Test access_control_list setter without ids"""
-    return  # TODO: acp
 
+    def acl_query():
+      return db.session.query(
+          all_models.AccessControlList.person_id,
+          all_models.AccessControlList.ac_role_id
+      ).filter(
+          all_models.AccessControlList.object_id == obj.id,
+          all_models.AccessControlList.object_type == "Control"
+      ).all()
     person_1 = all_models.Person(name="Frodo", email="frodo@baggins.com")
     person_2 = all_models.Person(name="Bilbo", email="bilbo@baggins.com")
     person_3 = factories.PersonFactory(name="Merry", email="merry@buck.com")
@@ -69,21 +88,12 @@ class TestAccessControlRoleable(TestCase):
         "ac_role": role,
     }])
     db.session.commit()
-    obj_id = obj.id
     self.assertIsNotNone(obj.access_control_list)
     self.assertEqual(len(obj.access_control_list), 2)
     self.assertEqual(obj.access_control_list[0].person, person_1)
     self.assertEqual(obj.access_control_list[1].person, person_2)
 
-    acl_query = db.session.query(
-        all_models.AccessControlList.person_id,
-        all_models.AccessControlList.ac_role_id
-    ).filter(
-        all_models.AccessControlList.object_id == obj_id,
-        all_models.AccessControlList.object_type == "Control"
-    )
-
-    acls = acl_query.all()
+    acls = acl_query()
     self.assertItemsEqual([
         (person_1.id, self.role.id),
         (person_2.id, role.id)
@@ -102,8 +112,43 @@ class TestAccessControlRoleable(TestCase):
     }]
     db.session.commit()
 
-    acls = acl_query.all()
+    acls = acl_query()
     self.assertItemsEqual([
         (person_2.id, role.id),
         (person_3.id, role.id)
     ], acls)
+
+  def test_full_access_control_list(self):
+    """Test if access_control_list property filters out propagated roles
+
+       Before sending the access_control_list to the frontend, propagated roles
+       need to be filtered out to help prevent performance issues"""
+    with factories.single_commit():
+      # Create an object with one external and one propagated role
+      obj = factories.ControlFactory()
+      acl = factories.AccessControlListFactory(
+          object=obj,
+          ac_role=self.role,
+          person=self.person
+      )
+      factories.AccessControlListFactory(
+          object=obj,
+          ac_role=self.role,
+          person=self.person,
+          parent=acl
+      )
+    # full_access_control_list should have all rows:
+    self.assertEqual(len(obj.full_access_control_list), 2,
+                     "full_access_control_list doesn't include all roles")
+    # access_control_list should only have non propagated ones
+    self.assertEqual(len(obj.access_control_list), 1,
+                     "access_control_list doesn't include all the roles")
+    obj_id, acl_id = obj.id, acl.id
+    api = api_helper.Api()
+    response = api.get(all_models.Control, obj_id)
+    acl = response.json["control"]["access_control_list"]
+    # Check if the response filtered out the propagated access_control_role
+    self.assertEqual(len(acl), 1,
+                     "acl didn't filter out propagated roles correctly")
+    self.assertEqual(acl[0]["id"], acl_id,
+                     "acl didn't filter out propagated roles correctly")
