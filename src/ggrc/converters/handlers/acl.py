bded13fd301de35aea871faa737eb12d5eacca1c
Miha Zidar <zidarsk8@gmail.com>
2018-09-17 08:22:38 +0200

    Update ACL import handler

--- src/ggrc/converters/handlers/acl.py
+++ src/ggrc/converters/handlers/acl.py
@@ -13,30 +13,9 @@ class AccessControlRoleColumnHandler(handlers.UsersColumnHandler):
   def __init__(self, row_converter, key, **options):
     super(AccessControlRoleColumnHandler, self).__init__(
         row_converter, key, **options)
-    self.role = models.AccessControlRole.query.filter_by(
-        name=(options.get("attr_name") or self.display_name),
-        object_type=self.row_converter.obj.type
-    ).one()
-
-  def _add_people(self, people_list):
-    """Add people to AC list with the current role."""
-    for person in people_list:
-      models.AccessControlList(
-          object=self.row_converter.obj,
-          person=person,
-          ac_role=self.role
-      )
-
-  def _remove_people(self, people_list):
-    """Remove people from AC list with the current role."""
-    acl_email_map = {
-        acl.person.email: acl
-        for acl in self.row_converter.obj.access_control_list
-        if acl.ac_role == self.role
-    }
-    for person in people_list:
-      acl = acl_email_map[person.email]
-      self.row_converter.obj.access_control_list.remove(acl)
+    role_name = (options.get("attr_name") or self.display_name)
+    self.acl = [acl for acl in row_converter.obj._access_control_people
+                if acl.ac_role.name==role_name][0]
 
   def set_obj_attr(self):
     """Update current AC list with correct people values."""
@@ -44,16 +23,16 @@ class AccessControlRoleColumnHandler(handlers.UsersColumnHandler):
     if not value_is_correct:
       return
     list_old = {
-        acl.person
-        for acl in self.row_converter.obj.access_control_list
-        if acl.ac_role == self.role
+        acp.person
+        for acp in self.acl.access_control_people
     }
     if self.set_empty:
-      self._remove_people(list_old)
+      self.row_converter.obj._remove_acp(self.acl, list_old)
       return
+
     list_new = set(self.value)
-    self._add_people(list_new - list_old)
-    self._remove_people(list_old - list_new)
+    self.row_converter.obj._add_acp(list_new - list_old)
+    self.row_converter.obj._remove_acp(list_old - list_new)
 
   def get_value(self):
     """Get list of emails for people with the current AC role."""
