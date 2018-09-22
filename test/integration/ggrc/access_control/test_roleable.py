98aec74954e1ee5566533091c9069411023b7f86
Miha Zidar <zidarsk8@gmail.com>
2018-09-22 23:35:17 +0200

    Update roleable tests
    This removes some obsolete tests and refactors others to use access
    control people instead.

--- test/integration/ggrc/access_control/test_roleable.py
+++ test/integration/ggrc/access_control/test_roleable.py
@@ -19,6 +19,7 @@ class TestAccessControlRoleable(TestCase):
   def setUp(self):
     super(TestAccessControlRoleable, self).setUp()
     with factories.single_commit():
+      self.role = factories.AccessControlRoleFactory(object_type="Control")
       self.person = factories.PersonFactory()
 
   @ddt.data(*[
@@ -39,63 +40,61 @@ class TestAccessControlRoleable(TestCase):
           control_visible_roles,
       )
 
-  @ddt.data(lambda self: [{
-      "ac_role_id": self.role.id,
-      "person": {
-          "id": self.person.id
-      }
-  }], lambda self: [{
-      "person": self.person,
-      "ac_role": self.role
-  }])
-  def test_with_dict(self, acl_list):
+  def test_with_dict(self):
+    acl_list = [{
+        "ac_role_id": self.role.id,
+        "person": {
+            "id": self.person.id
+        }
+    }]
     """Test access_control_list setter with a basic dict object
     This is the format the frontend uses"""
-    self.role = factories.AccessControlRoleFactory(object_type="Control")
     obj = all_models.Control(
         title="New Control",
-        access_control_list=acl_list(self),
+        access_control_list=acl_list
     )
     self.assertIsNotNone(obj.access_control_list)
-    acl = obj.access_control_list[0]
+    person, acl = obj.access_control_list[0]
     self.assertIsNotNone(acl)
     self.assertIsInstance(acl, all_models.AccessControlList)
-    self.assertEqual(acl.person.id, self.person.id)
+    self.assertEqual(person.id, self.person.id)
     self.assertEqual(acl.ac_role.id, self.role.id)
     self.assertEqual(acl.object, obj)
 
   def test_with_dict_objs_multiple(self):
     """Test access_control_list setter without ids"""
+    role = factories.AccessControlRoleFactory(object_type="Control")
 
     def acl_query():
       return db.session.query(
-          all_models.AccessControlList.person_id,
-          all_models.AccessControlList.ac_role_id
+          all_models.AccessControlPeople.person_id,
+      ).join(
+          all_models.AccessControlList
       ).filter(
           all_models.AccessControlList.object_id == obj.id,
           all_models.AccessControlList.object_type == "Control"
       ).all()
-    person_1 = all_models.Person(name="Frodo", email="frodo@baggins.com")
-    person_2 = all_models.Person(name="Bilbo", email="bilbo@baggins.com")
+    person_1 = factories.PersonFactory(name="Frodo", email="frodo@baggins.com")
+    person_2 = factories.PersonFactory(name="Bilbo", email="bilbo@baggins.com")
     person_3 = factories.PersonFactory(name="Merry", email="merry@buck.com")
-    role = all_models.AccessControlRole(name="Hobbit")
-    obj = all_models.Control(title="Test Control", access_control_list=[{
-        "person": person_1,
-        "ac_role": self.role,
+    obj = all_models.Control(title="Test Control")
+    obj.access_control_list = [{
+        "person": {
+            "id": person_1.id,
+        },
+        "ac_role_id": role.id,
     }, {
-        "person": person_2,
-        "ac_role": role,
-    }])
+        "person": {
+            "id": person_2.id,
+        },
+        "ac_role_id": role.id,
+    }]
     db.session.commit()
-    self.assertIsNotNone(obj.access_control_list)
-    self.assertEqual(len(obj.access_control_list), 2)
-    self.assertEqual(obj.access_control_list[0].person, person_1)
-    self.assertEqual(obj.access_control_list[1].person, person_2)
 
     acls = acl_query()
     self.assertItemsEqual([
-        (person_1.id, self.role.id),
-        (person_2.id, role.id)
+        (person_1.id, ),
+        (person_2.id, ),
     ], acls)
 
     obj.access_control_list = [{
@@ -113,41 +112,6 @@ class TestAccessControlRoleable(TestCase):
 
     acls = acl_query()
     self.assertItemsEqual([
-        (person_2.id, role.id),
-        (person_3.id, role.id)
+        (person_2.id, ),
+        (person_3.id, ),
     ], acls)
-
-  def test_full_access_control_list(self):
-    """Test if access_control_list property filters out propagated roles
-
-       Before sending the access_control_list to the frontend, propagated roles
-       need to be filtered out to help prevent performance issues"""
-    with factories.single_commit():
-      # Create an object with one external and one propagated role
-      obj = factories.ControlFactory()
-      acl = factories.AccessControlListFactory(
-          object=obj,
-          ac_role=self.role,
-          person=self.person
-      )
-      factories.AccessControlListFactory(
-          object=obj,
-          ac_role=self.role,
-          person=self.person,
-          parent=acl
-      )
-    # full_access_control_list should have all rows:
-    self.assertEqual(len(obj.full_access_control_list), 2,
-                     "full_access_control_list doesn't include all roles")
-    # access_control_list should only have non propagated ones
-    self.assertEqual(len(obj.access_control_list), 1,
-                     "access_control_list doesn't include all the roles")
-    obj_id, acl_id = obj.id, acl.id
-    api = api_helper.Api()
-    response = api.get(all_models.Control, obj_id)
-    acl = response.json["control"]["access_control_list"]
-    # Check if the response filtered out the propagated access_control_role
-    self.assertEqual(len(acl), 1,
-                     "acl didn't filter out propagated roles correctly")
-    self.assertEqual(acl[0]["id"], acl_id,
-                     "acl didn't filter out propagated roles correctly")
