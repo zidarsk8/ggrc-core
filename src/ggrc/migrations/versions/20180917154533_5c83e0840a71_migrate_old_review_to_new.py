5225c9a46b4d371e7a134bd75685a852807dc539
Aleh Natseuski <natsevsky@gmail.com>
2018-09-24 13:52:00 +0300

    Fix object review migration aff filter by cycle_task_group_object_tasks title

--- src/ggrc/migrations/versions/20180917154533_5c83e0840a71_migrate_old_review_to_new.py
+++ src/ggrc/migrations/versions/20180917154533_5c83e0840a71_migrate_old_review_to_new.py
@@ -70,7 +70,8 @@ def get_object_mapping_info(conn, table, obj_type, review_status):
             r.source_type="{obj_type}" AND
             r.destination_type="CycleTaskGroupObjectTask"
           JOIN cycle_task_group_object_tasks ctgot ON
-            ctgot.id = r.destination_id
+            ctgot.id = r.destination_id AND
+            ctgot.title like 'Object review for%'
           JOIN cycles cy ON cy.id = ctgot.cycle_id
           JOIN workflows w ON w.id = cy.workflow_id
           JOIN cycle_task_groups ctg ON cy.id = ctg.cycle_id
@@ -95,7 +96,8 @@ def get_object_mapping_info(conn, table, obj_type, review_status):
           r.destination_type="{obj_type}" AND
           r.source_type="CycleTaskGroupObjectTask"
         JOIN cycle_task_group_object_tasks ctgot ON
-          ctgot.id = r.source_id
+          ctgot.id = r.source_id AND
+          ctgot.title like 'Object review for%'
         JOIN cycles cy ON cy.id = ctgot.cycle_id
         JOIN workflows w ON w.id = cy.workflow_id
         JOIN cycle_task_groups ctg ON cy.id = ctg.cycle_id
