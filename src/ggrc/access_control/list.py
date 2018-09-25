e454bebfb6f0abdb1f1bb1163536f3c012ee76b6
Miha Zidar <zidarsk8@gmail.com>
2018-09-25 12:11:21 +0200

    Add comments on special ACL fields

--- src/ggrc/access_control/list.py
+++ src/ggrc/access_control/list.py
@@ -25,21 +25,35 @@ class AccessControlList(base.ContextRBAC, mixins.Base, db.Model):
   object_id = db.Column(db.Integer, nullable=False)
   object_type = db.Column(db.String, nullable=False)
 
-  parent_id_nn = db.Column(
-      db.Integer,
-      nullable=False,
-      default="0",
-  )
+  # Base id always points to the top most parent of the acl propagation chain
+  # or to itself if there are no parents. This field is used to optimize
+  # permission queries by making sure a single extra join is needed to get to
+  # the base ACL entry (one without parents) to which access control people are
+  # mapped.
   base_id = db.Column(
       db.Integer,
       db.ForeignKey('access_control_list.id', ondelete='CASCADE'),
       nullable=True,
   )
+
+  # This field is a copy of parent_id but set to not nullable, so it can be
+  # used in a unique constraint. Uniqueness check will always pass if there is
+  # a NULL in the set.
+  parent_id_nn = db.Column(
+      db.Integer,
+      nullable=False,
+      default="0",
+  )
+
+  # Parent id field is just to keep the information about the entire chain of
+  # acl propagation. This field is only needed for acl deletion. So unmapping
+  # will remove the entire subtree of propagated acl entries.
   parent_id = db.Column(
       db.Integer,
       db.ForeignKey('access_control_list.id', ondelete='CASCADE'),
       nullable=True,
   )
+
   parent = db.relationship(
       lambda: AccessControlList,  # pylint: disable=undefined-variable
       foreign_keys=lambda: AccessControlList.parent_id,
