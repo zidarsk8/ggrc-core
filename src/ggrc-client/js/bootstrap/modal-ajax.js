9f6d8ec3d667aed91dfab8ce1b0d7e6bcb359625
pavelglebov <pg.incloud@gmail.com>
2018-10-09 16:02:57 +0300

    Refactor unreadable conditions

--- src/ggrc-client/js/bootstrap/modal-ajax.js
+++ src/ggrc-client/js/bootstrap/modal-ajax.js
@@ -413,7 +413,7 @@ import {changeUrl} from '../router';
     let that = this;
     let $el = this.$element;
     let shownevents;
-    let keyevents;
+
     if (!(shownevents = $._data($el[0], 'events').shown) ||
       $(shownevents).filter(function () {
         return $.inArray('arrange', this.namespace.split('.')) > -1;
@@ -431,10 +431,12 @@ import {changeUrl} from '../router';
     }
 
     // prevent form submissions when descendant elements are also modals.
-    if (!(keyevents = $._data($el[0], 'events').keypress) ||
-      $(keyevents).filter(function () {
-        return $.inArray('preventdoublesubmit', this.namespace.split('.')) > -1;
-      }).length < 1) {
+    let keypressEvents = $._data($el[0], 'events').keypress;
+    let hasPreventDblSubmitEvent = _.find(keypressEvents, (el) => {
+      return el.namespace.indexOf('preventdoublesubmit') > -1;
+    });
+
+    if (!hasPreventDblSubmitEvent) {
       $el.on('keypress.preventdoublesubmit', function (ev) {
         if (ev.which === 13 &&
           !$(document.activeElement).hasClass('create-form__input') &&
@@ -448,10 +450,13 @@ import {changeUrl} from '../router';
         }
       });
     }
-    if (!(keyevents = $._data($el[0], 'events').keyup) ||
-      $(keyevents).filter(function () {
-        return $.inArray('preventdoubleescape', this.namespace.split('.')) > -1;
-      }).length < 1) {
+
+    let keyupEvents = $._data($el[0], 'events').keyup;
+    let hasPreventDblEscEvent = _.find(keyupEvents, (el) => {
+      return el.namespace.indexOf('preventdoubleescape') > -1;
+    });
+
+    if (!hasPreventDblEscEvent) {
       $el.on('keyup.preventdoubleescape', function (ev) {
         if (ev.which === 27 && $(ev.target).closest('.modal').length) {
           $(ev.target).closest('.modal').attr('tabindex', -1).focus();
