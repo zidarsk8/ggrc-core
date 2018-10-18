97c96529a368c1b95fa8e7bbbe9fda64e99831d2
Maxim Belov <belovmd@gmail.com>
2018-10-18 14:20:38 +0300

    Add column handler for issue tracker fields export

--- src/ggrc/converters/handlers/issue_tracker.py
+++ src/ggrc/converters/handlers/issue_tracker.py
@@ -1,3 +1,14 @@
 # Copyright (C) 2018 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 """Handlers for Issue Tracker fields"""
+
+from ggrc.converters.handlers import handlers
+
+
+class IssueTrackerColumnHandler(handlers.ColumnHandler):
+  """Column handler used for Issue Tracker related fields.
+
+  This class provides method for Issue Tracker fields export.
+  """
+  def get_value(self):
+    return self.row_converter.issue_tracker.get(self.key, "")
