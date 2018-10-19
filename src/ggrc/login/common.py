9447b7e57403a8e2a4053de58931d7fbd5df42f6
Kate Zabelova <kzabelova@gmail.com>
2018-10-19 13:09:30 +0300

    Check with cache that user or role were created during login

--- src/ggrc/login/common.py
+++ src/ggrc/login/common.py
@@ -4,11 +4,32 @@
 """Handle the interface to GGRC models for all login methods.
 """
 
+import flask
+
+from ggrc import db
+from ggrc.utils.log_event import log_event
+
 
 def get_next_url(request, default_url):
   """Returns next url from requres or default url if it's not found."""
   if 'next' in request.args:
     next_url = request.args['next']
     return next_url
-  else:
-    return default_url
+  return default_url
+
+
+def commit_user_and_role(user):
+  """Commits and flushes user and its role after the login."""
+  db_user, db_role = None, None
+  if hasattr(flask.g, "user_cache"):
+    db_user = flask.g.user_cache.get(user.email, None)
+  if hasattr(flask.g, "user_creator_roles_cache"):
+    db_role = flask.g.user_creator_roles_cache.get(user.email, None)
+  if db_user or db_role:
+    db.session.flush()
+    if db_user:
+      log_event(db.session, db_user, db_user.id, flush=False)
+    if db_role and not db_user:
+      # log_event of user includes event of role creation
+      log_event(db.session, db_role, flush=False)
+    db.session.commit()
