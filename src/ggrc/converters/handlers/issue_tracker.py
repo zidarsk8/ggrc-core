86e304cf041850636bc4fcd94581e56b8e4df011
Maxim Belov <belovmd@gmail.com>
2018-10-19 17:37:36 +0300

    Add Priority Column Handler

--- src/ggrc/converters/handlers/issue_tracker.py
+++ src/ggrc/converters/handlers/issue_tracker.py
@@ -3,6 +3,9 @@
 """Handlers for Issue Tracker fields"""
 
 from ggrc.converters.handlers import handlers
+from ggrc.models.hooks.issue_tracker import \
+    issue_tracker_params_container as params_container
+from ggrc.converters import errors
 
 
 class IssueTrackerColumnHandler(handlers.ColumnHandler):
@@ -12,3 +15,23 @@ class IssueTrackerColumnHandler(handlers.ColumnHandler):
   """
   def get_value(self):
     return self.row_converter.issue_tracker.get(self.key, "")
+
+
+class PriorityColumnHandler(IssueTrackerColumnHandler):
+  """Column handler for Priority Issue Tracker field."""
+  def __init__(self, row_converter, key, **options):
+    self.valid_states = \
+        params_container.IssueTrackerParamsContainer.AVAILABLE_PRIORITIES
+    super(PriorityColumnHandler, self).__init__(row_converter, key, **options)
+
+  def parse_item(self):
+    value = self.raw_value.upper()
+    if value not in self.valid_states:
+      self.add_warning(errors.WRONG_VALUE, column_name=self.display_name)
+      return None
+    return value
+
+  def set_obj_attr(self):
+    if self.dry_run or not self.value:
+      return
+    self.row_converter.issue_tracker[self.key] = self.value
