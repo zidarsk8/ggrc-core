3ba7b168f915b08ac94c65a3c3e3149344cc955c
Maxim Belov <belovmd@gmail.com>
2018-10-19 17:38:43 +0300

    Add Severity Column Handler

--- src/ggrc/converters/handlers/issue_tracker.py
+++ src/ggrc/converters/handlers/issue_tracker.py
@@ -35,3 +35,23 @@ class PriorityColumnHandler(IssueTrackerColumnHandler):
     if self.dry_run or not self.value:
       return
     self.row_converter.issue_tracker[self.key] = self.value
+
+
+class SeverityColumnHandler(IssueTrackerColumnHandler):
+  """Column handler for Priority Issue Tracker field."""
+  def __init__(self, row_converter, key, **options):
+    self.valid_states = \
+      params_container.IssueTrackerParamsContainer.AVAILABLE_SEVERITIES
+    super(SeverityColumnHandler, self).__init__(row_converter, key, **options)
+
+  def parse_item(self):
+    value = self.raw_value.upper()
+    if value not in self.valid_states:
+      self.add_warning(errors.WRONG_VALUE, column_name=self.display_name)
+      return None
+    return value
+
+  def set_obj_attr(self):
+    if self.dry_run or not self.value:
+      return
+    self.row_converter.issue_tracker[self.key] = self.value
