52742da9b22934adb26297df9e5613446201ca46
Sultan Abilda <abildinsultan@gmail.com>
2018-10-26 16:50:10 +0300

    Added migration with update of audit evidence ACR for auditor.
    Fixed bug when Auditor can edit audit evidence.
    Commit includes changes in "ggrc.migrations.utils.propagate_tree()" function.
    Modified with_update parameter behaviour.

---
+++ src/ggrc/migrations/versions/20181023132410_70e734fea4a4_evidence_readonly_for_auditor.py
@@ -0,0 +1,37 @@
+# Copyright (C) 2018 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""
+Set readonly permissions to audit evidence for auditor
+
+Create Date: 2018-10-23 13:24:10.838296
+"""
+# disable Invalid constant name pylint warning for mandatory Alembic variables.
+# pylint: disable=invalid-name
+
+from ggrc.migrations.utils.acr_propagation import propagate_roles
+
+
+# revision identifiers, used by Alembic.
+revision = '70e734fea4a4'
+down_revision = 'b9e9cb977292'
+
+
+AUDITOR_PERMISSIONS = {
+    'Audit': {
+        'Auditors': {
+            'Relationship R': {
+                'Evidence R': {}
+            }
+        }
+    }
+}
+
+
+def upgrade():
+  """Upgrade database schema and/or data, creating a new revision."""
+  propagate_roles(AUDITOR_PERMISSIONS, with_update=True)
+
+
+def downgrade():
+  """Downgrade database schema and/or data back to the previous revision."""
