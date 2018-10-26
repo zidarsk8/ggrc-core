dbd1322614b1570a2406e54597d968f56e9e3c05
Alexey Karpenko <k-alexey@users.noreply.github.com>
2018-10-26 16:40:31 +0300

    Add exception

--- src/ggrc/models/background_task.py
+++ src/ggrc/models/background_task.py
@@ -216,6 +216,9 @@ def create_task(name, url, **kwargs):
         task.get_result()
     elif queued_callback:
       queued_callback(bg_task)
+    else:
+      raise ValueError("Either queued_callback should be provided "
+                       "or APP_ENGINE set to true.")
     return bg_task
 
 
