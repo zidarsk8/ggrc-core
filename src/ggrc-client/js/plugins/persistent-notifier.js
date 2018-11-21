db218de40dd5c7cd16f1d808cf87e253d51e02f6
Klim Shuplenkou <kshuplenkou@google.com>
2018-11-21 17:23:18 +0300

    Use 'this' instead of 'that'

--- src/ggrc-client/js/plugins/persistent-notifier.js
+++ src/ggrc-client/js/plugins/persistent-notifier.js
@@ -16,31 +16,30 @@ export default class PersistentNotifier {
   whenQueueEmpties() {}
 
   queue(dfd) {
-    let that = this;
     if (!dfd || !dfd.then) {
       throw new Error('Attempted to queue something other than a ' +
                       'Deferred or Promise');
     }
 
-    if (!_.includes(this.dfds, dfd)) {
+    if (!this.dfds.includes(dfd)) {
       this.dfds.push(dfd);
       dfd.always(function () {
-        let dfdIndex = that.dfds.indexOf(dfd);
+        let dfdIndex = this.dfds.indexOf(dfd);
         if (dfdIndex > -1) {
-          that.dfds.splice(dfdIndex, 1);
+          this.dfds.splice(dfdIndex, 1);
         }
 
-        if (that.dfds.length === 0) {
-          can.each(that.onEmptyCallbacksList, Function.prototype.call);
-          that.onEmptyCallbacksList = [];
-          that.whenQueueEmpties();
+        if (this.dfds.length === 0) {
+          this.onEmptyCallbacksList.forEach(Function.prototype.call);
+          this.onEmptyCallbacksList = [];
+          this.whenQueueEmpties();
         }
       });
     }
 
     let pendingCallbacks = this.onEmptyCallbacksList.length;
-    if (pendingCallbacks === 0 && that.dfds.length > 0) {
-      that.whileQueueHasElements();
+    if (pendingCallbacks === 0 && this.dfds.length > 0) {
+      this.whileQueueHasElements();
     }
   }
 
