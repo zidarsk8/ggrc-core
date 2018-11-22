204d09071c9d908b694220042e7077330828e2dd
Daniil Novikau <novikaud@gmail.com>
2018-11-22 13:04:24 +0300

    Extend functionality of 'clipboard-link' model

---
+++ src/ggrc-client/js/components/clipboard-link/clipboard-link.js
@@ -0,0 +1,25 @@
+/*
+    Copyright (C) 2018 Google Inc.
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+import Clipboard from 'clipboard';
+import {notifier} from '../../plugins/utils/notifiers-utils';
+
+export default can.Component.extend({
+  tag: 'clipboard-link',
+  viewModel: {
+    text: '',
+  },
+  template:
+    `<a type="button" data-clipboard-text="{{text}}">
+       <content/>
+     </a>`,
+  events: {
+    inserted(el, evnt) {
+      new Clipboard(el.find('a')[0]).on('success', () => {
+        notifier('info', 'Link has been copied to your clipboard.');
+      });
+    },
+  },
+});
