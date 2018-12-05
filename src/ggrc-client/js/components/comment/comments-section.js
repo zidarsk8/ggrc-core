52852e625a7c34bb37a317b88b03f8d7df960e26
Natalia Zarubina <nataly.zarubina@gmail.com>
2018-12-05 11:51:13 +0300

    Move access logic to wrapper component for comments

--- src/ggrc-client/js/components/comment/comments-section.js
+++ src/ggrc-client/js/components/comment/comments-section.js
@@ -7,6 +7,7 @@ import template from './comments-section.mustache';
 import './comment-data-provider';
 import './comment-add-form';
 import './mapped-comments';
+import Permission from '../../permission';
 
 export default can.Component.extend({
   tag: 'comments-section',
@@ -15,6 +16,12 @@ export default can.Component.extend({
     define: {
       notification: {
         value: 'Send Notifications',
+      },
+      isDeniedToAddComment: {
+        get() {
+          return !Permission.is_allowed_for('update', this.attr('instance'))
+            || this.attr('instance.archived');
+        },
       },
     },
     instance: null,
