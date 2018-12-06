6496b8bf72b49da965dcbb8d88745bb3a572c209
Svetlana Drozdova <drozdovase@gmail.com>
2018-12-06 18:03:17 +0300

    Move getPersonInfo to user-utils

--- src/ggrc-client/js/components/diff/instance-gca-diff.js
+++ src/ggrc-client/js/components/diff/instance-gca-diff.js
@@ -6,7 +6,7 @@
 import {REFRESH_PROPOSAL_DIFF} from '../../events/eventTypes';
 import DiffBaseVM from './diff-base-vm';
 import template from './templates/instance-diff-items.mustache';
-import {getPersonInfo} from '../../../js/plugins/ggrc_utils';
+import {getPersonInfo} from '../../../js/plugins/utils/user-utils';
 import {formatDate} from '../../../js/plugins/utils/date-utils';
 const tag = 'instance-gca-diff';
 
