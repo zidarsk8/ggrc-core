7580642eb8364b930b2d3fa74ea668b8ce385740
IlyaTsupryk <ilya.tsupryk@gmail.com>
2018-12-13 13:29:11 +0300

    Add Other Contacts ACR

---
+++ src/ggrc/migrations/utils/acr_propagation_constants_control_other_contacts.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2018 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""Module for keeping constants for ACR propagation rules
+DO NOT MODIFY THESE VALUES.
+These are just the common propagation dictionaries, that are used in different
+migration files.
+For migration consistency, do not update these values once they are merged.
+If a modification is needed feel free to use these and modify them inside the
+migration file, or add new common roles and propagation rules.
+"""
+
+COMMENT_DOCUMENT_RUD = {
+    "Relationship R": {
+        "Review RU": {},
+        "Proposal RU": {},
+        "Comment R": {},
+        "Document RU": {
+            "Relationship R": {
+                "Comment R": {},
+            }
+        },
+    },
+}
+
+GGRC_NEW_ROLES_PROPAGATION = {
+    "Control": {"Other Contacts": COMMENT_DOCUMENT_RUD},
+}
