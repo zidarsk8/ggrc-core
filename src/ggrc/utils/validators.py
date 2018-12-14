94b643c28fdc60ae02a9f11d6c38aa35fffa5ef2
Mikita Kulik <kuliknikita@gmail.com>
2018-12-14 13:49:56 +0300

    Add utils.valdators.py with method for GGRCQ header validation

---
+++ src/ggrc/utils/validators.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2018 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""Module with common validators for GGRC."""
+
+from flask import request
+from werkzeug import exceptions
+
+from ggrc import settings
+from ggrc.models import mixins
+
+
+def validate_object_type_ggrcq(mapper, content, target):
+  """Validate object_type actions for GGRCQ."""
+  from ggrc.models import get_model
+
+  del mapper, content  # Unused
+  headers = request.headers
+  model = get_model(target.object_type)
+  is_ggrc_action = all([
+      issubclass(model, mixins.ReadOnlyGGRC),
+      headers.get("X-Requested-By") != settings.GGRC_Q_ACTION_HEADER
+  ])
+
+  if is_ggrc_action:
+    raise exceptions.MethodNotAllowed()
