c092c0382e7262011586f19a8b780746d95deea2
Zakhar Mikhalenka <zakhar98@gmail.com>
2019-01-03 10:51:08 +0300

    Remove extraPageOptions from bootstrap.js

--- src/ggrc-client/js/entrypoints/dashboard/bootstrap.js
+++ src/ggrc-client/js/entrypoints/dashboard/bootstrap.js
@@ -6,7 +6,6 @@
 import {
   getPageType,
   isMyAssessments,
-  isMyWork,
   getPageInstance,
 } from '../../plugins/utils/current-page-utils';
 import {
@@ -28,30 +27,11 @@ const location = window.location.pathname;
 const isAssessmentsView = isMyAssessments();
 const modelName = instance.constructor.shortName;
 let defaults;
-let extraPageOptions;
 let widgetList;
 let widgetModels;
 
 RouterConfig.setupRoutes(routes);
 gapiClient.loadGapiClient();
-
-extraPageOptions = {
-  Program: {
-    page_title: function (controller) {
-      return 'GRC Program: ' + controller.options.instance.title;
-    },
-  },
-  Person: {
-    page_title: function (controller) {
-      const instance = controller.options.instance;
-      return isMyWork() ?
-        'GRC: My Work' :
-        'GRC Profile: ' +
-        (instance.name && instance.name.trim()) ||
-        (instance.email && instance.email.trim());
-    },
-  },
-};
 
 initWidgets();
 
@@ -63,7 +43,7 @@ if (!isAssessmentsView && getPageType() !== 'Workflow') {
   initCounts(widgetModels, instance.type, instance.id);
 }
 
-$area.cms_controllers_page_object(Object.assign({
+$area.cms_controllers_page_object({
   widget_descriptors: widgetList,
   default_widgets: defaults || [],
   instance: getPageInstance(),
@@ -73,6 +53,6 @@ $area.cms_controllers_page_object(Object.assign({
     return controller.options.instance.title;
   },
   current_user: GGRC.current_user,
-}, extraPageOptions[modelName]));
+});
 
 saveRecentlyViewedObject();
