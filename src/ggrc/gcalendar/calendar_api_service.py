8d224be603f86d30f269aef6bc99dce2dec7106c
Kate Zabelova <kzabelova@gmail.com>
2019-01-16 18:15:51 +0300

    Send flags on event update

--- src/ggrc/gcalendar/calendar_api_service.py
+++ src/ggrc/gcalendar/calendar_api_service.py
@@ -91,6 +91,9 @@ class CalendarApiService(object):
             'timeZone': kwargs.get('timezone', 'UTC'),
         },
         'attendees': [{'email': x} for x in attendees],
+        'sendNotifications': kwargs.get('send_notifications', False),
+        'guestsCanModify': kwargs.get('guests_can_modify', False),
+        'guestsCanInviteOthers': kwargs.get('guests_can_invite', False),
     }
     return self.calendar_service.events().update(
         calendarId=calendar_id, body=event, eventId=event_id).execute()
