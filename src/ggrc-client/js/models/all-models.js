8aa9e1892c6dd06643605faa0776e7ef5fc8ea23
Yury Chupyrkin <yuri.chupyrkin@gmail.com>
2019-01-28 15:52:56 +0300

    Update file headers

--- src/ggrc-client/js/models/all-models.js
+++ src/ggrc-client/js/models/all-models.js
@@ -1,3 +1,8 @@
+/*
+ Copyright (C) 2019 Google Inc.
+ Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+ */
+
 import * as businessModels from './business-models';
 import * as serviceModels from './service-models';
 import * as mappingModels from './mapping-models';
