d96c6fd5f10e81bcb28706cc78257b5dd700242a
Daniil Novikau <novikaud@gmail.com>
2019-02-04 16:16:50 +0300

    Move 'if_recurring_workflow' helper to 'mustache_helper' module

--- src/ggrc-client/js/mustache_helper.js
+++ src/ggrc-client/js/mustache_helper.js
@@ -985,6 +985,7 @@ Mustache.registerHelper('displayWidgetTab',
     return options.fn(options.contexts);
   }
 );
+
 Mustache.registerHelper('is_auditor', function (options) {
   const audit = getPageInstance();
   if (audit.type !== 'Audit') {
@@ -1022,4 +1023,21 @@ Mustache.registerHelper('isScopeModel', function (instance, options) {
   const modelName = Mustache.resolve(instance).type;
 
   return isScopeModel(modelName) ? options.fn(this) : options.inverse(this);
+});
+
+/*
+  if_recurring_workflow mustache helper
+
+  Given an object, it  determines if it's a workflow, and if it's a recurring
+  workflow or not.
+
+  @param object - the object we want to check
+  */
+Mustache.registerHelper('if_recurring_workflow', function (object, options) {
+  object = Mustache.resolve(object);
+  if (object.type === 'Workflow' &&
+      _.includes(['day', 'week', 'month'], object.unit)) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
 });
