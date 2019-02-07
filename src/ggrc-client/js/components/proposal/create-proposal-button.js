de094310d5d114cc12aadca879e7d3008294a44b
Roman Matusevich <roma.matusevich@gmail.com>
2019-02-07 16:25:20 +0300

    Hide "Propose Changes" button

--- src/ggrc-client/js/components/proposal/create-proposal-button.js
+++ src/ggrc-client/js/components/proposal/create-proposal-button.js
@@ -3,7 +3,9 @@
  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
  */
 
+import {isChangeableExternally} from '../../plugins/utils/ggrcq-utils';
 import template from './templates/create-proposal-button.stache';
+
 const tag = 'create-proposal-button';
 
 export default can.Component.extend({
@@ -19,7 +21,8 @@ export default can.Component.extend({
             return false;
           }
 
-          return instance.class.isProposable;
+          return instance.class.isProposable &&
+            !isChangeableExternally(instance);
         },
       },
     },
