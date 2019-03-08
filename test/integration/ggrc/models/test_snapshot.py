a1aebe7f7f8c08a4d15c8f1888bc919e3a6d878b
Miha Zidar <zidarsk8@gmail.com>
2019-03-08 01:48:09 +0100

    Remove person GCA from snapshot tests

--- test/integration/ggrc/models/test_snapshot.py
+++ test/integration/ggrc/models/test_snapshot.py
@@ -123,7 +123,6 @@ class TestSnapshotQueryApi(TestCase):
         {"title": "CA rich text", "attribute_type": "Rich Text"},
         {"title": "CA date", "attribute_type": "Date"},
         {"title": "CA checkbox", "attribute_type": "Checkbox"},
-        {"title": "CA person", "attribute_type": "Map:Person"},
         {"title": "CA dropdown", "attribute_type": "Dropdown",
          "multi_choice_options": "one,two,three,four,five"},
     ]
@@ -151,7 +150,6 @@ class TestSnapshotQueryApi(TestCase):
               "CA rich text",
               "CA date",
               "CA checkbox",
-              "CA person",
               "CA dropdown"
           ])
       }
@@ -160,7 +158,6 @@ class TestSnapshotQueryApi(TestCase):
           "CA rich text": "control<br><br>\nrich text",
           "CA date": "22/02/2022",
           "CA checkbox": "yes",
-          "CA person": ca_person,
           "CA dropdown": "one"
       }
 
