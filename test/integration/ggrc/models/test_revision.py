8b1d838097d95657963cd6e0d8fc71367eb40a2d
Miha Zidar <zidarsk8@gmail.com>
2019-03-08 01:51:05 +0100

    Remove person GCA from revision tests

--- test/integration/ggrc/models/test_revision.py
+++ test/integration/ggrc/models/test_revision.py
@@ -296,7 +296,6 @@ class TestRevisions(query_helper.WithQueryApi, TestCase):
       ("Dropdown", ""),
       ("Checkbox", "0"),
       ("Date", ""),
-      ("Map:Person", "Person"),
   )
   @ddt.unpack
   def test_revisions_with_empty_gcads(self, attribute_type, attribute_value):
@@ -309,7 +308,6 @@ class TestRevisions(query_helper.WithQueryApi, TestCase):
       ("Dropdown", ""),
       ("Checkbox", "0"),
       ("Date", ""),
-      ("Map:Person", "Person"),
   )
   @ddt.unpack
   def test_revisions_with_empty_lcads(self, attribute_type, attribute_value):
