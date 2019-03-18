8197ba7da6f194f1de9678eb38cd1761a3053170
Yury Ostroukh <0str0uchura@gmail.com>
2019-03-18 17:35:00 +0300

    Add redirects for Risk's proposable attrs

--- src/ggrc-client/js/components/redirects/role-attr-names-provider/role-attr-names-provider.js
+++ src/ggrc-client/js/components/redirects/role-attr-names-provider/role-attr-names-provider.js
@@ -4,22 +4,30 @@
 */
 
 const roleToLinkMap = {
-  Admin: 'owner',
-  'Control Operators': 'control_operator',
-  'Control Owners': 'control_owner',
-  'Principal Assignees': 'principal_assignee',
-  'Secondary Assignees': 'secondary_assignee',
-  'Other Contacts': 'other_contact',
+  Control: {
+    Admin: 'owner',
+    'Control Operators': 'control_operator',
+    'Control Owners': 'control_owner',
+    'Principal Assignees': 'principal_assignee',
+    'Secondary Assignees': 'secondary_assignee',
+    'Other Contacts': 'other_contact',
+  },
+  Risk: {
+    Admin: 'owner',
+    'Risk Owners': 'risk_owner',
+    'Other Contacts': 'other_contact',
+  },
 };
 
 const viewModel = can.Map.extend({
   define: {
     linkAttrName: {
       get() {
-        return roleToLinkMap[this.attr('roleName')];
+        return roleToLinkMap[this.attr('modelType')][this.attr('roleName')];
       },
     },
   },
+  modelType: '',
   roleName: '',
 });
 
