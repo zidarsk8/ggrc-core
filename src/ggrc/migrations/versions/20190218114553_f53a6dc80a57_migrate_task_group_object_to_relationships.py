29c293e91f5487aa35038f7a8fd6d4ed39153d23
MikalaiMikalalai <mikalaimikalalai@gmail.com>
2019-03-19 13:46:47 +0300

    Update migration chain

--- src/ggrc/migrations/versions/20190218114553_f53a6dc80a57_migrate_task_group_object_to_relationships.py
+++ src/ggrc/migrations/versions/20190218114553_f53a6dc80a57_migrate_task_group_object_to_relationships.py
@@ -23,7 +23,7 @@ from ggrc.migrations.utils\
 
 # revision identifiers, used by Alembic.
 revision = 'f53a6dc80a57'
-down_revision = '7d10655e87f9'
+down_revision = '0d7a3a0aa3da'
 
 
 def load_data(conn):
