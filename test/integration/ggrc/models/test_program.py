c39bffa94317d73762ba8aa412b88a112dec317c
Alexey Karpenko <k-alexey@users.noreply.github.com>
2019-04-02 11:27:06 +0300

    Add tests to revision, relationship, program

--- test/integration/ggrc/models/test_program.py
+++ test/integration/ggrc/models/test_program.py
@@ -48,3 +48,25 @@ class TestProgram(TestCase):
     self.assert200(response)
     response = self.api.delete(self.program)
     self.assert200(response)
+
+
+class TestMegaProgram(TestCase):
+  """Mega Program test cases"""
+
+  def setUp(self):
+    """Setup tests"""
+    self.api = api_helper.Api()
+
+  def test_is_mega_attr(self):
+    """Test is_mega attribute of program"""
+    with factories.single_commit():
+      program_child = factories.ProgramFactory()
+      program_parent = factories.ProgramFactory()
+      factories.RelationshipFactory(source=program_parent,
+                                    destination=program_child)
+    program_child_id = program_child.id
+    program_parent_id = program_parent.id
+    response = self.api.get(all_models.Program, program_child_id)
+    self.assertEqual(response.json["program"]["is_mega"], False)
+    response = self.api.get(all_models.Program, program_parent_id)
+    self.assertEqual(response.json["program"]["is_mega"], True)
