dc84b524bc6173dc5cedcf9184730945c5f319d0
Klim Shuplenkou <kshuplenkou@google.com>
2019-04-02 16:54:47 +0300

    Convert markdown in tree-item-attr

--- src/ggrc-client/js/components/tree/tree-item-attr.js
+++ src/ggrc-client/js/components/tree/tree-item-attr.js
@@ -5,6 +5,7 @@
 import {formatDate} from '../../plugins/utils/date-utils';
 import {getUserRoles} from '../../plugins/utils/user-utils';
 import template from './templates/tree-item-attr.stache';
+import {convertMarkdownToHtml} from '../../plugins/utils/markdown-utils';
 
 // attribute names considered "default" and representing a date
 const DATE_ATTRS = Object.freeze({
@@ -79,12 +80,18 @@ export default can.Component.extend({
           return formatDate(result, true);
         }
         if (attrName in RICH_TEXT_ATTRS) {
+          if (this.isMarkdown()) {
+            result = convertMarkdownToHtml(result);
+          }
           return result
             .replace(regexNewLines, '\n').replace(regexTags, ' ').trim();
         }
         return String(result);
       }
       return '';
+    },
+    isMarkdown() {
+      return !!this.attr('instance').constructor.isChangeableExternally;
     },
   },
 });
