9bfbae7878dcfe94c2adfac558cca1640e08256c
pavelglebov <pg.incloud@gmail.com>
2019-04-03 11:24:37 +0300

    Add mega-object model mixin

---
+++ src/ggrc-client/js/models/mixins/mega-object.js
@@ -0,0 +1,23 @@
+/*
+    Copyright (C) 2019 Google Inc.
+    Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+
+import Mixin from './mixin';
+
+/*
+  Mega object can be mapped to the same object type,
+  e.g. map Program to Mega Program
+*/
+export default Mixin({
+  isMegaObject: true,
+  'after:init'() {
+    this.tree_view_options.mega_attr_list = [{
+      attr_title: 'Map as',
+      attr_name: 'map_as',
+      order: 41,
+      disable_sorting: true,
+      mandatory: true,
+    }];
+  },
+}, {});
