f8d316929fba403acbf1f954caca72b9d1e05ba8
Yury Ostroukh <0str0uchura@gmail.com>
2019-05-13 12:12:50 +0300

    Enable Change Log for Control
    Update migration dates for Risk&Control.
    Update Change log message.

--- src/ggrc-client/js/models/business-models/risk.js
+++ src/ggrc-client/js/models/business-models/risk.js
@@ -21,6 +21,7 @@ export default Cacheable.extend({
     proposable,
     changeableExternally,
   ],
+  migrationDate: '06/27/2019',
   is_custom_attributable: true,
   isRoleable: true,
   attributes: {
