33e316dbe121bec1ac1572f9c5f67e33d03e8153
YuriChupyrkin <Yuri.Chupyrkin@gmail.com>
2019-05-17 15:37:39 +0300

    Import required components for export page

--- src/ggrc-client/js/entrypoints/export/index.js
+++ src/ggrc-client/js/entrypoints/export/index.js
@@ -11,6 +11,9 @@ import '../../components/import-export/export';
 import '../../components/object-list/object-list';
 import '../../components/tree_pagination/tree_pagination';
 import '../../components/page-header/page-header';
+import '../../components/questionnaire-link/questionnaire-link';
+import '../../components/custom-roles/custom-roles';
+import '../../components/mapped-counter/mapped-counter';
 
 import '../../common-handlers';
 
