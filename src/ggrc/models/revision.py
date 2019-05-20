d21a1e14f3ef00e50ad82034f521610cc3361226
mrR2D2 <alaksandr.lavoncyk@gmail.com>
2019-05-20 14:05:48 +0300

    Reduce DB queries for snapshots and revisions
    * Load snapshot along with revision to reduce number of DB queries.
    * Make snapshot's eager_query load revision with event's action since
      it's needed for revision description.
    * Make revision's eager_query load event's action for the same reason.
    * Remove "deferred" from snapshot's fields to reduce number of DB
      queries. There is no such a case where snapshot without at least one
      field is being used so no need for deferred.

--- src/ggrc/models/revision.py
+++ src/ggrc/models/revision.py
@@ -94,7 +94,8 @@ class Revision(before_flush_handleable.BeforeFlushHandleable,
     query = super(Revision, cls).eager_query(**kwargs)
     return query.options(
         orm.subqueryload('modified_by'),
-        orm.subqueryload('event'),  # used in description
+        # Event's action is loaded here since it is used in description.
+        orm.joinedload('event').load_only('action'),
     )
 
   def __init__(self, obj, modified_by_id, action, content):
