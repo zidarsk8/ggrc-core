e64deb8dfab714d1ee8474d4004c8d82419c4aba
Yury Ostroukh <0str0uchura@gmail.com>
2019-05-28 16:45:38 +0300

    Update unit tests

--- src/ggrc-client/js/components/workflow/cycle-task-group-object-task/tests/cycle-task-group-object-task_spec.js
+++ src/ggrc-client/js/components/workflow/cycle-task-group-object-task/tests/cycle-task-group-object-task_spec.js
@@ -6,7 +6,10 @@
 import Component from '../cycle-task-group-object-task';
 import * as WorkflowHelpers from '../../../../plugins/utils/workflow-utils';
 import * as CurrentPageUtils from '../../../../plugins/utils/current-page-utils';
-import {getComponentVM} from '../../../../../js_specs/spec_helpers';
+import {
+  getComponentVM,
+  spyProp,
+} from '../../../../../js_specs/spec_helpers';
 import Permission from '../../../../permission';
 
 describe('cycle-task-group-object-task component', function () {
@@ -16,31 +19,51 @@ describe('cycle-task-group-object-task component', function () {
     viewModel = getComponentVM(Component);
   });
 
-  describe('isEditDenied get() method', () => {
-    beforeEach(function () {
+  describe('isAllowedToUpdate get() method', () => {
+    beforeEach(() => {
+      viewModel.attr('instance', {});
       spyOn(Permission, 'is_allowed_for');
     });
 
+    it('returns true if it is allowed to update instance', () => {
+      Permission.is_allowed_for.
+        withArgs('update', viewModel.attr('instance')).and.returnValue(true);
+
+      expect(viewModel.attr('isAllowedToUpdate')).toBe(true);
+    });
+
+    it('returns false if it is not allowed to update instance', () => {
+      Permission.is_allowed_for
+        .withArgs('update', viewModel.attr('instance')).and.returnValue(false);
+
+      expect(viewModel.attr('isAllowedToUpdate')).toBe(false);
+    });
+  });
+
+  describe('isEditDenied get() method', () => {
+    let isAllowedToUpdate;
+
+    beforeEach(function () {
+      isAllowedToUpdate = spyProp(viewModel, 'isAllowedToUpdate');
+    });
+
     it('returns false if there is access to edit instance', () => {
-      Permission.is_allowed_for.and.returnValue(true);
+      isAllowedToUpdate.and.returnValue(true);
       viewModel.attr('instance', {is_in_history: false});
 
       expect(viewModel.attr('isEditDenied')).toBe(false);
     });
 
     describe('returns true', () => {
-      it('if there are no "update "permissions for the instance', function () {
-        Permission.is_allowed_for.and.returnValue(false);
-
+      it('if it is not allowed to update instance', function () {
+        isAllowedToUpdate.and.returnValue(false);
         const result = viewModel.attr('isEditDenied');
 
-        expect(Permission.is_allowed_for)
-          .toHaveBeenCalledWith('update', viewModel.attr('instance'));
         expect(result).toBe(true);
       });
 
       it('if the instance is in history', function () {
-        Permission.is_allowed_for.and.returnValue(true);
+        isAllowedToUpdate.and.returnValue(true);
         viewModel.attr('instance', {is_in_history: true});
 
         expect(viewModel.attr('isEditDenied')).toBe(true);
@@ -93,6 +116,40 @@ describe('cycle-task-group-object-task component', function () {
         viewModel.attr('instance'),
         event.state
       );
+    });
+  });
+
+  describe('showMapObjectsButton() getter', () => {
+    let spiedIsEditDenied;
+
+    beforeEach(() => {
+      viewModel.attr('instance', {status: 'Assigned'});
+      spiedIsEditDenied = spyProp(viewModel, 'isEditDenied');
+      spiedIsEditDenied.and.returnValue(false);
+    });
+
+    it('returns true by default', () => {
+      expect(viewModel.attr('showMapObjectsButton')).toBe(true);
+    });
+
+    describe('returns false', () => {
+      it('when edit functionality is denied', () => {
+        spiedIsEditDenied.and.returnValue(true);
+
+        expect(viewModel.attr('showMapObjectsButton')).toBe(false);
+      });
+
+      it('when instance has "Verified" status', () => {
+        viewModel.attr('instance.status', 'Verified');
+
+        expect(viewModel.attr('showMapObjectsButton')).toBe(false);
+      });
+
+      it('when instance has "Finished" status', () => {
+        viewModel.attr('instance.status', 'Finished');
+
+        expect(viewModel.attr('showMapObjectsButton')).toBe(false);
+      });
     });
   });
 });
