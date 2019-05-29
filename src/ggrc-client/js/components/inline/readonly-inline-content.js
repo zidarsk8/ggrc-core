2a153f4ddfffdd8ee728454a201b827044421089
Daniil Novikau <novikaud@gmail.com>
2019-05-29 11:45:22 +0300

    Validate ms values in actual options list

--- src/ggrc-client/js/components/inline/readonly-inline-content.js
+++ src/ggrc-client/js/components/inline/readonly-inline-content.js
@@ -13,13 +13,17 @@ export default can.Component.extend({
   leakScope: true,
   viewModel: can.Map.extend({
     withReadMore: false,
+    options: [],
     value: '',
     define: {
       items: {
         get() {
-          let value = this.attr('value');
-          if (value && typeof(value) === 'string') {
+          const options = this.attr('options');
+          const value = this.attr('value');
+
+          if (options.length && value && typeof(value) === 'string') {
             return value.split(',')
+              .filter((item) => _.includes(options, item))
               .map((item) => `<p><i class="fa fa-circle"></i>${item}</p>`)
               .join('');
           }
