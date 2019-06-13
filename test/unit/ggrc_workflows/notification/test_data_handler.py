bf4fadeba22ceee3d2df4fe9fe613f44344fca0f
Alexey Karpenko <k-alexey@users.noreply.github.com>
2019-06-13 16:33:03 +0300

    Test CycleTask and Workflow URL

--- test/unit/ggrc_workflows/notification/test_data_handler.py
+++ test/unit/ggrc_workflows/notification/test_data_handler.py
@@ -8,22 +8,21 @@
 import unittest
 from mock import patch
 
+from ggrc.gcalendar.utils import get_cycle_tasks_url_by_slug
 from ggrc_workflows import models
 from ggrc_workflows.notification import data_handler
 
 
 class GetCycleTaskUrlTestCase(unittest.TestCase):
-  """Tests for the get_cycle_task_url() function."""
+  """Tests for the get_cycle_tasks_url_by_slug() function."""
 
-  # pylint: disable=invalid-name, unused-argument
-  @patch(
-      u"ggrc_workflows.notification.data_handler.get_url_root",
-      return_value=u"http://www.foo.com/")
-  def test_generates_correct_url_for_task(self, *mocks):
+  @patch(u"ggrc.gcalendar.utils.get_url_root",
+         return_value=u"http://www.foo.com/")
+  def test_correct_url_for_task(self, *_):
     """The method should return a correct URL for the given Task."""
-    result = data_handler.get_cycle_task_url()
-
-    expected_url = u"http://www.foo.com/dashboard#!task"
+    result = get_cycle_tasks_url_by_slug("CYCLETASK-1")
+    expected_url = (u"http://www.foo.com/dashboard#!task"
+                    u"&query=%22task%20slug%22%3DCYCLETASK-1")
     self.assertEqual(result, expected_url)
 
 
@@ -33,9 +32,7 @@ class GetCycleTaskUrlTestCase(unittest.TestCase):
 class GetCycleUrlTestCase(unittest.TestCase):
   """Tests for the get_cycle_url() function."""
 
-  # pylint: disable=invalid-name, unused-argument
-
-  def test_generates_correct_url_for_active_cycle(self, *mocks):
+  def test_url_for_active_cycle(self, *_):
     """The method should return correct URL for active Cycles."""
     workflow = models.Workflow(id=111)
     cycle = models.Cycle(slug="CYCLE-22", workflow=workflow)
@@ -48,7 +45,7 @@ class GetCycleUrlTestCase(unittest.TestCase):
     )
     self.assertEqual(expected_url, cycle.cycle_url)
 
-  def test_generates_correct_url_for_inactive_cycle(self, *mocks):
+  def test_url_for_inactive_cycle(self, *_):
     """The method should return correct URL for inactive Cycles."""
     workflow = models.Workflow(id=111)
     cycle = models.Cycle(slug="CYCLE-22", workflow=workflow)
@@ -59,3 +56,16 @@ class GetCycleUrlTestCase(unittest.TestCase):
         u"&query=%22cycle%20slug%22%3D%22CYCLE-22%22"
     )
     self.assertEqual(expected_url, cycle.cycle_inactive_url)
+
+
+@patch(u"ggrc_workflows.notification.data_handler.get_url_root",
+       return_value=u"http://www.foo.com/")
+@patch(u"ggrc.access_control.role.get_ac_roles_for", return_value={})
+class GetWorkflowUrlTestCase(unittest.TestCase):
+  """Tests for the get_workflow_url() function."""
+
+  def test_correct_url_for_workflow(self, *_):
+    """The method should return correct URL for Workflow."""
+    workflow = models.Workflow(id=111)
+    expected_url = u"http://www.foo.com/workflows/111#current"
+    self.assertEqual(expected_url, data_handler.get_workflow_url(workflow))
