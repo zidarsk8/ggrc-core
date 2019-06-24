7a6aa3e778ea16fe9897290ba1499771704b2e39
Yury Ostroukh <0str0uchura@gmail.com>
2019-06-24 18:28:25 +0300

    Remove extra check

--- src/ggrc-client/js/controllers/modals/modals_controller.js
+++ src/ggrc-client/js/controllers/modals/modals_controller.js
@@ -338,10 +338,8 @@ export default canControl.extend({
       } else {
         instance.removeAttr('_suppress_errors');
       }
-      // Set the value if it isn't a search field
-      if (!el.hasClass('search-icon')) {
-        this.set_value_from_element(el);
-      }
+
+      this.set_value_from_element(el);
     },
 
   'input:not([data-lookup]), textarea keyup':
