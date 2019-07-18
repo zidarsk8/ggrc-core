ee324733a216d824f112077acc2574917832b42d
roman4hik <romanchikovevgeni@gmail.com>
2019-07-19 00:51:11 +0300

    Fix PR errors

--- test/integration/ggrc/models/test_log_json.py
+++ test/integration/ggrc/models/test_log_json.py
@@ -4,51 +4,73 @@
 """Integration tests for log_json method."""
 
 from types import NoneType
+import ddt
 
 from ggrc.models import all_models
 from integration.ggrc import TestCase
 from integration.ggrc.models import factories
 
 
+SET_ALL_MODELS = set(all_models.all_models)
+SET_MODELS_TO_EXCLUDE = {all_models.AttributeDefinitions,
+                         all_models.AttributeTemplates,
+                         all_models.AttributeTypes,
+                         all_models.Attributes,
+                         all_models.Namespaces,
+                         all_models.ObjectTemplates,
+                         all_models.ObjectTypes,
+                         all_models.ImportExport,
+                         all_models.Maintenance,
+                         all_models.Automapping,
+                         all_models.Revision
+                         }
+SET_MODELS_WITH_LOG_JSON_METHOD = (SET_ALL_MODELS -
+                                   SET_MODELS_TO_EXCLUDE)
+
+
+@ddt.ddt
 class TestLogJson(TestCase):
   """Test checks log_json method."""
+
+  ATTRS_TO_EXCLUDE = "modified_by"
+  ERROR_MESSAGE = "Model {} has problem with field {}, " \
+                  "after calling log_json, it == {}"
 
   @staticmethod
   def _prepare_create_param(person, list_attr):
     """Method builds params for creating model."""
     create_param = {}
     for attr in list_attr:
-      if attr == 'modified_by':
-        continue
       create_param[attr] = person
     return create_param
 
   def _check_json_representation(self, keys_attr, json_representation, model):
-    """Result of calling log_json should not contain Model's obj."""
+    """Result of calling log_json should not contain Person instance."""
     for item in keys_attr:
-      if item in json_representation:
+      if item in json_representation and item != self.ATTRS_TO_EXCLUDE:
         self.assertTrue(
             isinstance(json_representation[item], (dict, NoneType)),
-            msg="Model {} has problem with field {}, after calling "
-                "log_json, it == {}"
+            msg=self.ERROR_MESSAGE
             .format(model.__name__, item, json_representation[item])
         )
 
-  def test_log_json(self):
-    """Test checks work of log_json method for all models"""
+  @staticmethod
+  def _get_person_type_attrs(model):
+    """Get all model's person type attrs."""
+    result = []
+    for attr in dir(model):
+      if (attr.endswith("_by") and not attr[0].isupper() and attr[0] != '_'):
+        result.append(attr)
+    return result
+
+  @ddt.data(*SET_MODELS_WITH_LOG_JSON_METHOD)
+  def test_person_type_log_json(self, model):
+    """Test checks work of log_json method for person type attrs."""
     creator_email = "creator@example.com"
     creator = factories.PersonFactory(email=creator_email)
-    for model in all_models.all_models:
-      attr = [attr for attr in dir(model) if (attr.endswith("_by") and
-                                              not attr[0].isupper() and
-                                              attr[0] != '_')]
-      attr = self._prepare_create_param(creator, attr)
-      keys_attr = list(attr.keys())
-      if keys_attr:
-        try:
-          json_representation = model(**attr).log_json()
-        except AttributeError:
-          continue
-        self._check_json_representation(
-            keys_attr, json_representation, model
-        )
+    person_type_attrs = self._get_person_type_attrs(model)
+    attr_dict = self._prepare_create_param(creator, person_type_attrs)
+    json_representation = model(**attr_dict).log_json()
+    self._check_json_representation(
+        person_type_attrs, json_representation, model
+    )
