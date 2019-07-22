5a4c3360a8ce5f22b85fdca0574efd1482fcef4f
alyohea <alexsema1705@gmail.com>
2019-07-22 11:29:31 +0300

    Add test for monthly notification for secondary assignee

--- test/integration/ggrc_workflows/notifications/test_monthly_wf.py
+++ test/integration/ggrc_workflows/notifications/test_monthly_wf.py
@@ -5,9 +5,10 @@ from freezegun import freeze_time
 from mock import patch
 
 from ggrc.notifications import common
-from ggrc.models import Notification, Person
+from ggrc.models import all_models
 from ggrc_workflows import start_recurring_cycles
 from integration.ggrc import TestCase
+from integration.ggrc.access_control import acl_helper
 from integration.ggrc_workflows.generator import WorkflowsGenerator
 from integration.ggrc.api_helper import Api
 from integration.ggrc.generator import ObjectGenerator
@@ -26,10 +27,9 @@ class TestMonthlyWorkflowNotification(TestCase):
     self.object_generator = ObjectGenerator()
 
     self.random_objects = self.object_generator.generate_random_objects()
-    _, self.person_1 = self.object_generator.generate_person(
-        user_role="Administrator")
-    _, self.person_2 = self.object_generator.generate_person(
-        user_role="Administrator")
+    self.person_1 = self.create_user_with_role(role="Administrator")
+    self.person_2 = self.create_user_with_role(role="Administrator")
+    self.secondary_assignee = self.create_user_with_role(role="Reader")
     self.create_test_cases()
 
     def init_decorator(init):
@@ -39,25 +39,31 @@ class TestMonthlyWorkflowNotification(TestCase):
           self.created_at = dt.datetime.now()
       return new_init
 
-    Notification.__init__ = init_decorator(Notification.__init__)
+    all_models.Notification.__init__ = init_decorator(
+        all_models.Notification.__init__)
 
   @patch("ggrc.notifications.common.send_email")
   def test_auto_generate_cycle(self, mock_mail):
 
-    person_1_email = Person.query.get(self.person_1.id).email
     with freeze_time("2015-04-01"):
       _, wf = self.wf_generator.generate_workflow(self.monthly_workflow_1)
       self.wf_generator.activate_workflow(wf)
       _, notif_data = common.get_daily_notifications()
-      self.assertNotIn(person_1_email, notif_data)
+      contact = self.person_1
+      task_assignees = [contact, self.secondary_assignee]
+
+      for user in task_assignees:
+        self.assertNotIn(user.email, notif_data)
 
     with freeze_time("2015-04-02"):
       self.api.client.get("nightly_cron_endpoint")
       _, notif_data = common.get_daily_notifications()
-      self.assertNotIn(person_1_email, notif_data)
+      for user in task_assignees:
+        self.assertNotIn(user.email, notif_data)
       start_recurring_cycles()
       _, notif_data = common.get_daily_notifications()
-      self.assertNotIn(person_1_email, notif_data)
+      for user in task_assignees:
+        self.assertNotIn(user.email, notif_data)
 
     # cycle starts on monday - 6th, and not on 5th
     with freeze_time("2015-04-03"):
@@ -65,16 +71,21 @@ class TestMonthlyWorkflowNotification(TestCase):
       noop.login()
       start_recurring_cycles()
       _, notif_data = common.get_daily_notifications()
-      self.assertIn(person_1_email, notif_data)
-      self.assertIn("cycle_started", notif_data[person_1_email])
+      for user in task_assignees:
+        self.assertIn(user.email, notif_data)
 
-    with freeze_time("2015-04-15"):  # one day befor due date
+      # cycle started notifs available only for contact
+      self.assertIn("cycle_started", notif_data[contact.email])
+
+    with freeze_time("2015-04-15"):  # one day before due date
       _, notif_data = common.get_daily_notifications()
-      self.assertIn(person_1_email, notif_data)
+      for user in task_assignees:
+        self.assertIn(user.email, notif_data)
 
     with freeze_time("2015-04-25"):  # due date
       _, notif_data = common.get_daily_notifications()
-      self.assertIn(person_1_email, notif_data)
+      for user in task_assignees:
+        self.assertIn(user.email, notif_data)
 
   @patch("ggrc.notifications.common.send_email")
   def test_manual_generate_cycle(self, mock_mail):
@@ -83,21 +94,19 @@ class TestMonthlyWorkflowNotification(TestCase):
       _, wf = self.wf_generator.generate_workflow(self.monthly_workflow_1)
       self.wf_generator.activate_workflow(wf)
 
-      person_1 = Person.query.get(self.person_1.id)
-
-    with freeze_time("2015-04-03"):
-      _, notif_data = common.get_daily_notifications()
-
     with freeze_time("2015-04-03"):
       _, cycle = self.wf_generator.generate_cycle(wf)
+      contact = self.person_1
+      task_assignees = [contact, self.secondary_assignee]
       _, notif_data = common.get_daily_notifications()
-      person_1 = Person.query.get(self.person_1.id)
-      self.assertIn("cycle_started", notif_data[person_1.email])
 
-    with freeze_time("2015-05-03"):  # two days befor due date
+      # cycle started notifs available only for contact
+      self.assertIn("cycle_started", notif_data[contact.email])
+
+    with freeze_time("2015-05-03"):  # two days before due date
       _, notif_data = common.get_daily_notifications()
-      person_1 = Person.query.get(self.person_1.id)
-      self.assertIn(person_1.email, notif_data)
+      for user in task_assignees:
+        self.assertIn(user.email, notif_data)
 
   def create_test_cases(self):
     def person_dict(person_id):
@@ -106,6 +115,11 @@ class TestMonthlyWorkflowNotification(TestCase):
           "id": person_id,
           "type": "Person"
       }
+
+    task_secondary_assignee = all_models.AccessControlRole.query.filter(
+        all_models.AccessControlRole.name == "Task Secondary Assignees",
+        all_models.AccessControlRole.object_type == "TaskGroupTask",
+    ).one().id
 
     self.monthly_workflow_1 = {
         "title": "test monthly wf notifications",
@@ -121,12 +135,20 @@ class TestMonthlyWorkflowNotification(TestCase):
             "task_group_tasks": [{
                 "title": "task 1",
                 "description": "some task",
+                "access_control_list": [
+                    acl_helper.get_acl_json(task_secondary_assignee,
+                                            self.secondary_assignee.id),
+                ],
                 "contact": person_dict(self.person_1.id),
                 "start_date": dt.date(2015, 4, 5),
                 "end_date": dt.date(2015, 4, 25),
             }, {
                 "title": "task 2",
                 "description": "some task",
+                "access_control_list": [
+                    acl_helper.get_acl_json(task_secondary_assignee,
+                                            self.secondary_assignee.id),
+                ],
                 "contact": person_dict(self.person_1.id),
                 "start_date": dt.date(2015, 4, 10),
                 "end_date": dt.date(2015, 4, 21),
@@ -138,12 +160,20 @@ class TestMonthlyWorkflowNotification(TestCase):
             "task_group_tasks": [{
                 "title": "task 1 in tg 2",
                 "description": "some task",
+                "access_control_list": [
+                    acl_helper.get_acl_json(task_secondary_assignee,
+                                            self.secondary_assignee.id),
+                ],
                 "contact": person_dict(self.person_1.id),
                 "start_date": dt.date(2015, 4, 15),
                 "end_date": dt.date(2015, 4, 15),
             }, {
                 "title": "task 2 in tg 2",
                 "description": "some task",
+                "access_control_list": [
+                    acl_helper.get_acl_json(task_secondary_assignee,
+                                            self.secondary_assignee.id),
+                ],
                 "contact": person_dict(self.person_2.id),
                 "start_date": dt.date(2015, 4, 15),
                 "end_date": dt.date(2015, 4, 28),
