1d257d782edd192c090aa7f4b8fdf0ba34f03fc0
Denis Tamkovich <tamkovichdenis@gmail.com>
2019-07-29 13:51:09 +0300

    Add delete revisions for rels and proposals

--- src/ggrc/migrations/versions/20190716_a6b61113e372_remove_proposal_data_related_to_control.py
+++ src/ggrc/migrations/versions/20190716_a6b61113e372_remove_proposal_data_related_to_control.py
@@ -13,33 +13,79 @@ import sqlalchemy as sa
 
 from alembic import op
 
+from ggrc.migrations import utils
+
 
 # revision identifiers, used by Alembic.
 revision = 'a6b61113e372'
-down_revision = '17fbb17f7cec'
+down_revision = 'f00343450894'
 
 
-def remove_relationships(conn):
+def create_old_rel_del_revisions(conn, data):
+  """Remove old relationship revisions."""
+  old_rel_ids = [d.id for d in data]
+  if old_rel_ids:
+    for rel_id in old_rel_ids:
+      utils.add_to_objects_without_revisions(
+          conn, rel_id, "Relationship", "deleted"
+      )
+
+
+def remove_relationships(conn, data):
   """Remove old control relationships"""
-  conn.execute(
-      sa.text("DELETE FROM relationships WHERE "
-              "source_type = 'Control' AND destination_type = 'Proposal'"
-              ),
-  )
+  old_rel_ids = [d.id for d in data]
+  if old_rel_ids:
+    conn.execute(
+        sa.text("DELETE FROM relationships WHERE id IN :rel_ids"),
+        rel_ids=old_rel_ids
+    )
 
 
-def remove_proposal_and_comments(conn):
+def create_old_proposal_del_revisions(conn, data):
+  """Remove old proposal revisions."""
+  old_prop_ids = [d.id for d in data]
+  if old_prop_ids:
+    utils.add_to_objects_without_revisions_bulk(
+        conn, old_prop_ids, "Proposal", "deleted"
+    )
+
+
+def remove_proposal(conn):
   """Remove old proposals for controls"""
   conn.execute(
       sa.text("DELETE FROM proposals WHERE instance_type = 'Control'"),
   )
 
 
+def load_relationships_data(conn):
+  """Load relationships necessary data for migration"""
+  rel_data = conn.execute(
+      sa.text("SELECT id FROM relationships WHERE "
+              "(source_type = 'Control' AND destination_type = 'Proposal')"
+              "OR "
+              "(source_type = 'Proposal' AND destination_type = 'Control')"
+              ),
+  ).fetchall()
+  return rel_data
+
+
+def load_proposal_data(conn):
+  """Load proposals necessary data for migration"""
+  proposal_data = conn.execute(
+      sa.text("SELECT id FROM proposals WHERE instance_type = 'Control'"),
+  ).fetchall()
+  return proposal_data
+
+
 def upgrade():
   """Upgrade database schema and/or data, creating a new revision."""
   conn = op.get_bind()
-  remove_relationships(conn)
-  remove_proposal_and_comments(conn)
+  rel_data = load_relationships_data(conn)
+  proposal_data = load_proposal_data(conn)
+  create_old_rel_del_revisions(conn, rel_data)
+  create_old_proposal_del_revisions(conn, proposal_data)
+  remove_relationships(conn, rel_data)
+  remove_proposal(conn)
 
 
 def downgrade():
