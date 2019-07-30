f04dc1e9dc6e0292c4fbf1780affaf085b6c96ea
alyohea <alexsema1705@gmail.com>
2019-07-30 14:21:30 +0300

    Add unit tests for cad title modification functions

--- test/unit/ggrc/models/test_custom_attribute_definition.py
+++ test/unit/ggrc/models/test_custom_attribute_definition.py
@@ -5,8 +5,9 @@
 
 import unittest
 import ddt
-from mock import MagicMock
+from mock import MagicMock, Mock, patch
 
+from ggrc import views
 from ggrc.models import all_models
 from ggrc.access_control import role as acr
 
@@ -35,3 +36,53 @@ class TestCustomAttributeDefinition(unittest.TestCase):
     """Test if raises if title invalid"""
     with self.assertRaises(ValueError):
       self.cad.validate_title("title", title)
+
+
+@ddt.ddt
+class TestCustomAttributeDefinitionViews(unittest.TestCase):
+  # pylint: disable=protected-access
+  """Test cads title modification functions in ggrc.views.__init__"""
+  @ddt.data(
+      (True, "program", "person", False),
+      (True, "program", "program", False),
+      (False, "program", "person", False),
+      (False, "program", "program", True),
+  )
+  @ddt.unpack
+  def test_is_cad_definition(self, is_none, definition_type,
+                             expected_definition_type, expected):
+    """Test _is_cad_definition function"""
+    cad = None if is_none else Mock()
+    if cad:
+      cad.definition_type = definition_type
+
+    result = bool(views._is_cad_definition_type(cad, expected_definition_type))
+    self.assertEqual(result, expected)
+
+  @ddt.data(
+      (1, 1, [(None, {'custom_attribute_definitions': [
+          {
+              'id': 1,
+              'title': 'new title'
+          }]})]),
+      (1, 2, []),
+  )
+  @ddt.unpack
+  def test_modify_revision_content(self, cad_id, expected_cad_id, expected):
+    """Test _get_modified_revision_content function"""
+    cad = Mock(id=cad_id, title="new title")
+    content = {
+        "custom_attribute_definitions": [{
+            "id": expected_cad_id,
+            "title": "title"
+        }]
+    }
+
+    obj = Mock(resource_type="Program")
+    revision = all_models.Revision(obj, MagicMock(),
+                                   MagicMock(), content)
+
+    with patch("ggrc.views._get_revisions_by_type", return_value=[revision]):
+      result = [c for c in views._get_modified_revision_content(
+          cad, revision.resource_type)]
+      self.assertEqual(result, expected)
