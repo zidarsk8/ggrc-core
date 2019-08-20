25c5e033a24dc548165a4200e4d970870163fd88
Shurke <alsereda1992@gmail.com>
2019-08-20 12:52:03 +0300

    Remove duplicated warning message

--- src/ggrc/converters/pre_commit_checks.py
+++ src/ggrc/converters/pre_commit_checks.py
@@ -98,15 +98,32 @@ def check_assessment(row_converter):
     row_converter.add_error(errors.ARCHIVED_IMPORT_ERROR)
 
 
+def _is_done_or_end_state(obj):
+  """Check Assessment status.
+
+  Status should be 'Completed and verified', 'Verified',
+  'In Review' or 'Rework Needed'.
+
+  Args:
+    obj: db.Model instance.
+
+  Returns:
+     Boolean: indicated that obj in 'Completed and verified', 'Verified',
+    'In Review' or 'Rework Needed' status.
+  """
+  in_done_state = obj.status in {obj.DONE_STATE, obj.REWORK_NEEDED}
+  in_end_state = obj.status in obj.END_STATES and obj.verified
+  return in_done_state or in_end_state
+
+
 def secondary_check_assessment(row_converter):
   """Check Assessment after setup of secondary objects
 
   Assessment can't be imported with 'Completed and verified', 'Verified',
   'In Review', 'Rework Needed' state if don't have Verifier"""
   obj = row_converter.obj
-  if ((obj.status in {obj.DONE_STATE, obj.REWORK_NEEDED} or
-          (obj.status in obj.END_STATES and obj.verified)) and
-          not obj.verifiers):
+  if (_is_done_or_end_state(obj) and not obj.verifiers and
+          not row_converter.status_changing):
     row_converter.add_warning(errors.NO_VERIFIER_WARNING, status=obj.status)
     # In case of import new asmt with 'Rework Needed' status
     # we can't change it to default state because of validation
