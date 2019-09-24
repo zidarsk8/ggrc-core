276fcf13793c233caa23237858b628914986a66d
Mikita Kulik <kuliknikita@gmail.com>
2019-09-24 14:27:16 +0300

    BUMP VERSION 2.19.0

--- src/ggrc/settings/default.py
+++ src/ggrc/settings/default.py
@@ -59,7 +59,7 @@ except ImportError:
 # for more info) and if the version name were to exceed 30 characters, all
 # deployments would go to the same GAE app version. Please take that into
 # consideration when modifying this string.
-VERSION = "2.18.0-Pumpkin" + BUILD_NUMBER
+VERSION = "2.19.0-Pumpkin" + BUILD_NUMBER
 
 # Migration owner
 MIGRATOR = os.environ.get(
